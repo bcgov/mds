2870e222449499b189f206fe0eacbdfaeb0564cd
VivianCheungFW <vivian@freshworks.io>
2018-07-30 23:05:05 -0700

    edit home page and add dashboard page

--- functional-tests/src/test/groovy/pages/HomePage.groovy
+++ functional-tests/src/test/groovy/pages/HomePage.groovy
@@ -3,20 +3,19 @@ package pages
 
 import geb.Page
 
-class HomePage extends Page
-{
+class HomePage extends Page{
     static at = { title == "React App" }
     static content = {
-        clickMeButton (wait: true) {$("button", class:"ant-btn btn-center ant-btn-primary ant-btn-lg")}
+        header (wait:true) {$("h1")}
+        mineNameBox (wait:true) {$("input", placeholder:"Mine Name")}
+        createMineRecordButton (wait: true) {$("button").has("span", text:"Create Mine")}       
         toastMessage (wait: true) {$("div", class:"ant-notification-notice-message").text()}
-    
-    
+        dashboardButton {$("button").has("span",text:"Dashboard")}
     }
 
-    def createMineRecord(mineID,mineName){
-        createMineRecordButton.click()
-        mineIDBox = mineID
+    def createMineRecord(mineName){
         mineNameBox = mineName
-        submitButton.click()
+        createMineRecordButton.click()        
+         
     }
 }
--- functional-tests/src/test/groovy/spec/HomePageSpec.groovy
+++ functional-tests/src/test/groovy/spec/HomePageSpec.groovy
@@ -7,88 +7,71 @@ import spock.lang.*
 import pages.*
 
 //variables
-ID_GOOD = "Regional-123"
-ID_LONG = "Regional-12345678"
 
-NULL_STRING = ""
 
+NAME_NULL = ""
 NAME_GOOD = "Gibraltar"
-NAME_LONG = "RGN945v88nTk0LOUk5d5WlQgbl5209VjZMEOmGobwppXO7QPHflw5jaQHna7"
+NAME_LONG = "RGN945v88asdfasfnTk0LOUk5d5WlQgbl5209VjZMEOmGobwppXO7QPHflw5jaQHna7"
 
 MSG_SUCCEED = "Success"
 MSG_FAIL = "fail"
 
 @Title("MDS-createMineRecord")
-@Narrative("At homepage, I can create a mine record given valid mine ID and mine Name")
+@Narrative("At homepage, I can create a mine record given valid mine Name")
 @see("https://bcmines.atlassian.net/browse/MDS-63")
 @Stepwise
 class  HomePageTest extends GebReportingSpec {
-    def "Scenario 1: User is able to create a mine record given unique MINE-ID and a MIND-Name"(){
+    def "Scenario: User is able to create a mine record given a  valid MIND-Name"(){
         given: "I go to the homepage"
         to HomePage
 
-        when: "I input valie mine ID and name combination"
-        createMineRecord(ID_GOOD,NAME_GOOD)
+        then: "I can see the header'"
+        header.text === "Create A Mine Record"
 
-        and: "I should see the successful message"
-        toastMessage = MSG_SUCCEED
+
+        when: "I input valid mine name"
+        createMineRecord(NAME_GOOD)
+
+        then: "I should see the successful message"
+        toastMessage === MSG_SUCCEED
+
+        when: "I click the Dashboard button"
+        dashboardButton.click()
+
+        then: "I should see the Dashboard"
+        at Dashboard
+
+        and: "I should see the created record on the Dashboard"
+        header.text === "Mine Dashboard"
+        mineRecordExists(NAME_GOOD)
+
 
     }
 
-
-    def "Scenario 2: Error displayed when the given mine ID is more than 10 char"(){
-        given: "I go to the homepage"
-        to HomePage
-
-        when: "I input long mine ID"
-        createMineRecord(ID_LONG,NAME_GOOD)
-
-        and: "I should see an error message"
-        toastMessage = MSG_FAIL
-    }
-
-    def "Scenario 3: Error displayed when the given mine name is more than 50 char"(){
+/*
+    def "Scenario: Error displayed when the given mine name is more than 50 char"(){
         given: "I go to the homepage"
         to HomePage
 
         when: "I input long mine name"
-        createMineRecord(ID_GOOD,NAME_LONG)
+        createMineRecord(NAME_LONG)
 
         and: "I should see an error message"
         toastMessage = MSG_FAIL
     }
 
-    def "Scenario 4: Error displayed if mineID is null"(){
-        given: "I go to the homepage"
-        to HomePage
 
-        when: "I do not give a mine ID"
-        createMineRecord(NULL_STRING,NAME_GOOD)
-
-        and: "I should see an error message"
-        toastMessage = MSG_FAIL
-    }
-
-    def "Scenario 5: Error displayed if mine name is null"(){
+    def "Scenario: Error displayed if mine name is null"(){
         given: "I go to the homepage"
         to HomePage
 
         when: "I do not give a mine name"
-        createMineRecord(NAME_GOOD,NULL_STRING)
+        createMineRecord(NAME_NULL)
 
         and: "I should see an error message"
         toastMessage = MSG_FAIL
     }
 
+    */
 
-    def "Scenario 6: Error displayed if the given mineID already exists in the DB"(){
-        given: "I go to the homepage"
-        to HomePage
-
-        when: "I input a duplicate mine id"
-        createMineRecord(NAME_GOOD,NAME_LONG)
-
-        and: "I should see an error message"
-        toastMessage = MSG_FAIL
-    }
 }
