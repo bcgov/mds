9f1391f1f1668443ebd54bece86ed0e9409b7895
sam_l <samuel.yj.lee@gmail.com>
2018-07-31 13:20:24 -0700

    frontend validation for mine name

--- frontend/src/components/mine/CreateMineForm.js
+++ frontend/src/components/mine/CreateMineForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Card, Form, Input, Button } from 'antd';
+import { Card, Form, Input, Button, notification } from 'antd';
 
 import { createMineRecord } from '@/actionCreators/mineActionCreator';
 import * as router from '@/constants/routes';
@@ -15,8 +15,13 @@ export class CreateMineForm extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
     const mineName = this.refs.mineName.input.value;
-    this.props.createMineRecord(mineName);
-    // this.refs.createMineForm.reset();
+    if (!mineName) {
+      notification.error({message: "Must specify a mine name.", duration: 10});
+    } else if (mineName.length > 100) {
+      notification.error({message: "Specified name cannot exceed 100 characters.", duration: 10});
+    } else {
+      this.props.createMineRecord(mineName);
+    }
   }
 
   render() {
--- frontend/src/constants/API.js
+++ frontend/src/constants/API.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 export const BASE_URL = process.env.API_URL
-export const HELLO = '/hello'
-=======
-export const BASE_URL = 'http://0.0.0.0:5000'
 export const MINE = '/mine'
 export const MINE_LIST = '/mines'
->>>>>>> frontend create and view mine record with links
