44ecfef1191abce8cba02feaebe84c6ec4af2aac
sam_l <samuel.yj.lee@gmail.com>
2018-08-08 16:11:37 -0700

    add better defaults for config

--- python-backend/app/config.py
+++ python-backend/app/config.py
@@ -9,9 +9,9 @@ if ENV_FILE:
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev')
-    DB_HOST = os.environ.get('DB_HOST', '')
-    DB_USER = os.environ.get('DB_USER', '')
-    DB_PASS = os.environ.get('DB_PASS', '')
-    DB_PORT = os.environ.get('DB_PORT', '')
-    DB_NAME = os.environ.get('DB_NAME', '')
+    DB_HOST = os.environ.get('DB_HOST', 'localhost')
+    DB_USER = os.environ.get('DB_USER', 'user')
+    DB_PASS = os.environ.get('DB_PASS', 'pass')
+    DB_PORT = os.environ.get('DB_PORT', 5432)
+    DB_NAME = os.environ.get('DB_NAME', 'db_name')
     DB_URL = "postgresql://{0}:{1}@{2}:{3}/{4}".format(DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME)
