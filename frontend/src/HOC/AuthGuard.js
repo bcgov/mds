5de72fa7a7b17c631a6f47f9a6339c02df370677
mmumby <meredith.mumby@outlook.com>
2018-08-09 10:34:07 -0700

    changed url to be dev keycloak

--- frontend/src/HOC/AuthGuard.js
+++ frontend/src/HOC/AuthGuard.js
@@ -19,10 +19,12 @@ export const AuthGuard = (WrappedComponent) => {
         "realm": "mds",
         "url": "https://sso-test.pathfinder.gov.bc.ca/auth/",
         "ssl-required": "external",
-        "resource": "frontend-local",
+        // "resource": "frontend-local",
+        "resource": "frontend",
         "public-client": true,
         "confidential-port": 0,
-        "clientId": "frontend-local"
+        // "clientId": "frontend-local"
+         "clientId": "frontend"
       });
       keycloak.init({ onLoad: 'login-required' }).then((authenticated) => {
         this.setState({ keycloak: keycloak, authenticated: authenticated });
