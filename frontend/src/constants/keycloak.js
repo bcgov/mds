50f6ab45ab6bdae8f24fe5b03c1f26b9ca605acb
mmumby <meredith.mumby@outlook.com>
2018-08-09 13:54:00 -0700

    changed clientId

--- frontend/src/constants/keycloak.js
+++ frontend/src/constants/keycloak.js
@@ -2,10 +2,10 @@ export const KEYCLOAK = {
   "realm": "mds",
   "url": "https://sso-test.pathfinder.gov.bc.ca/auth/",
   "ssl-required": "external",
-  "resource": "frontend-local",
-  // "resource": "frontend",
+  // "resource": "frontend-local",
+  "resource": "frontend",
   "public-client": true,
   "confidential-port": 0,
-  "clientId": "frontend-local"
-  // "clientId": "frontend"
+  // "clientId": "frontend-local"
+  "clientId": "frontend"
 }
