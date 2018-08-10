ab94116aea1c5b9ddcf00c66fcb73060d2f44c2c
Gyanesh Mishra <gyan@freshworks.io>
2018-08-10 12:40:04 -0700

    change code coverage to be sonar compatible

--- python-backend/run_tests.sh
+++ python-backend/run_tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-python -m pytest --cov=app --cov-report xml tests/
+coverage erase
+coverage run --branch --source=app -m pytest
+coverage xml -i
 
 /opt/app-root/sonar-scanner-cli/bin/sonar-scanner "-Dsonar.host.url=${SONAR_HOST_URL}"
