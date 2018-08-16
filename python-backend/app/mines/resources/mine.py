49380a42364c88c3762112f190e2cea4b9ca8cca
sam_l <samuel.yj.lee@gmail.com>
2018-08-16 14:48:00 -0700

    allow for guid or mine no on mine/get

--- python-backend/app/mines/resources/mine.py
+++ python-backend/app/mines/resources/mine.py
@@ -13,9 +13,12 @@ class Mine(Resource):
 
     @jwt.requires_roles(["mds-mine-view"])
     def get(self, mine_no):
-        mine = MineIdentity.find_by_mine_no(mine_no)
-        if mine:
+        mine_by_no = MineIdentity.find_by_mine_no(mine_no)
+        if mine_by_no:
             return mine.json()
+        mine_by_guid = MineIdentity.find_by_mine_guid(mine_no)
+        if mine_by_guid:
+            return mine_by_guid.json()
         return {'message': 'Mine not found'}, 404
 
     @jwt.requires_roles(["mds-mine-create"])
