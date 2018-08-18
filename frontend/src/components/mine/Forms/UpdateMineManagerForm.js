a2afdfffa276f8ec295d9ecbfa5ee7ccc5499fd9
mmumby <meredith.mumby@outlook.com>
2018-08-17 19:54:26 -0700

    fixed import

--- frontend/src/components/mine/Forms/UpdateMineManagerForm.js
+++ frontend/src/components/mine/Forms/UpdateMineManagerForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Field, reduxForm } from 'redux-form'
-import RenderSelect from '../../reusables/RenderSearchSelect';
+import RenderSelect from '../../reusables/RenderSelect';
 import RenderDate from '../../reusables/RenderDate';
 import { Form, Button, Col, Row } from 'antd';
 import * as FORM from '@/constants/forms';
