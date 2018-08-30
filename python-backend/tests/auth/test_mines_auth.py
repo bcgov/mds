a84a5fbe33c0f18fb724fcdb77f3b59317a3642a
sam_l <samuel.yj.lee@gmail.com>
2018-08-30 13:48:41 -0700

    added management command to add mines and delete mines, also added fix for post on mine resource location not defined

--- python-backend/tests/auth/test_mines_auth.py
+++ python-backend/tests/auth/test_mines_auth.py
@@ -53,8 +53,8 @@ def test_get_mine_full_auth(test_client, auth_headers):
 def test_post_mine_no_auth(test_client):
     test_mine_data = {
         "name": "test_create_mine",
-        "latitude": "123.123",
-        "longitude": "49.49"
+        "latitude": "49.49",
+        "longitude": "123.124"
     }
     post_resp = test_client.post('/mine', data=test_mine_data, headers={})
     assert post_resp.status_code == 401
@@ -63,8 +63,8 @@ def test_post_mine_no_auth(test_client):
 def test_post_mine_view_only(test_client, auth_headers):
     test_mine_data = {
         "name": "test_create_mine",
-        "latitude": "123.123",
-        "longitude": "49.49"
+        "latitude": "49.49",
+        "longitude": "123.124"
     }
     post_resp = test_client.post('/mine', data=test_mine_data, headers=auth_headers['view_only_auth_header'])
     assert post_resp.status_code == 401
@@ -73,8 +73,8 @@ def test_post_mine_view_only(test_client, auth_headers):
 def test_post_mine_full_auth(test_client, auth_headers):
     test_mine_data = {
         "name": "test_create_mine",
-        "latitude": "123.123",
-        "longitude": "49.49"
+        "latitude": "49.49",
+        "longitude": "123.124"
     }
     post_resp = test_client.post('/mine', data=test_mine_data, headers=auth_headers['full_auth_header'])
     assert post_resp.status_code == 200
@@ -84,8 +84,8 @@ def test_post_mine_full_auth(test_client, auth_headers):
 def test_put_mine_no_auth(test_client):
     test_tenure_data = {
         "tenure_number_id": "1234568",
-        "latitude": "123.124",
-        "longitude": "49.478"
+        "latitude": "49.49",
+        "longitude": "123.123"
     }
     put_resp = test_client.put('/mine/' + TEST_MINE_NO, data=test_tenure_data, headers={})
     assert put_resp.status_code == 401
@@ -94,8 +94,8 @@ def test_put_mine_no_auth(test_client):
 def test_put_mine_view_only(test_client, auth_headers):
     test_tenure_data = {
         "tenure_number_id": "1234568",
-        "latitude": "123.124",
-        "longitude": "49.478"
+        "latitude": "49.49",
+        "longitude": "123.123"
     }
     put_resp = test_client.put('/mine/' + TEST_MINE_NO, data=test_tenure_data, headers=auth_headers['view_only_auth_header'])
     assert put_resp.status_code == 401
@@ -104,8 +104,8 @@ def test_put_mine_view_only(test_client, auth_headers):
 def test_put_mine_full_auth(test_client, auth_headers):
     test_tenure_data = {
         "tenure_number_id": "1234568",
-        "latitude": "123.124",
-        "longitude": "49.478"
+        "latitude": "49.49",
+        "longitude": "123.125"
     }
     put_resp = test_client.put('/mine/' + TEST_MINE_NO, data=test_tenure_data, headers=auth_headers['full_auth_header'])
     assert put_resp.status_code == 200
