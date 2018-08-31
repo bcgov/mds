1ff4cfe42d2a19140db2d6c2b8d17bc27505137b
mmumby <meredith.mumby@outlook.com>
2018-08-31 09:03:43 -0700

    added more styling

--- frontend/src/components/dashboard/MineList.js
+++ frontend/src/components/dashboard/MineList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { Button, Col, Row } from 'antd';
+import { Button, Col, Row, Divider } from 'antd';
 import * as router from '@/constants/routes';
 
 const propTypes = {
@@ -23,25 +23,25 @@ class MineList extends Component {
       <div>
         <Row type="flex">
           <Col span={4}><strong>MINE_NO</strong></Col>
-          <Col span={8}><strong>NAME</strong></Col>
-          <Col span={8}><strong>GUID</strong></Col>
+          <Col span={4}><strong>NAME</strong></Col>
           <Col span={4}><strong>ACTION</strong></Col>
         </Row>
+        <Divider style={{ height: '5px', backgroundColor: '003366'}}/>
         {mineIds.map((id) => {
           return (
             <div style={{ padding: "10px" }} key={id}>
               <Row type="flex">
                 <Col span={4}>{mines[id].mine_detail[0] ? mines[id].mine_detail[0].mine_no : "-"}</Col>
-                <Col span={8}>{mines[id].mine_detail[0] ? mines[id].mine_detail[0].mine_name : "-"}</Col>
-                <Col span={8}>{mines[id].guid}</Col>
+                <Col span={4}>{mines[id].mine_detail[0] ? mines[id].mine_detail[0].mine_name : "-"}</Col>
                 <Col span={4}>
                   <Link to={router.MINE_SUMMARY.dynamicRoute(id)}>
-                    <Button type="primary" size="small" >
-                      View
+                    <Button type="primary">
+                      View Mine
                     </Button>
                   </Link>
                 </Col>
               </Row>
+              <Divider />
             </div>
           )
         })}
