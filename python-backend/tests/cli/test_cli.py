3588d5d445cd2b502089b31dfca373cad58686a9
sam_l <samuel.yj.lee@gmail.com>
2018-09-04 10:39:05 -0700

    added cli test okay exit code

--- python-backend/tests/cli/test_cli.py
+++ python-backend/tests/cli/test_cli.py
@@ -1,8 +1,10 @@
 def test_create_mines_cli(test_client, cli_runner):
     result = cli_runner.invoke(args=['create_data', '10'])
+    assert result.exit_code == 0
     assert result.output == 'Created 10 random mines.\n'
 
 
 def test_delete_mines_cli(test_client, cli_runner):
     result = cli_runner.invoke(args=['delete_data'])
+    assert result.exit_code == 0
     assert result.output == 'Database has been cleared.\n'
