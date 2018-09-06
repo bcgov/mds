101baede52c756880eb6538eb1acd6c52642331e
samfresh <38572614+samfresh@users.noreply.github.com>
2018-09-06 16:07:25 -0700

    Feature/backend add points (#92)
    * added lat long into /mine/name, and added map as optional request args
    
    * remove unnesscary comma

--- python-backend/tests/mines/resources/test_mine_list_resource.py
+++ python-backend/tests/mines/resources/test_mine_list_resource.py
@@ -9,6 +9,13 @@ def test_get_mines(test_client, auth_headers):
     assert get_data['mines'][0]['guid'] == TEST_MINE_GUID
 
 
+def test_get_mines_with_map_true(test_client, auth_headers):
+    get_resp = test_client.get('/mines?map=true', headers=auth_headers['full_auth_header'])
+    get_data = json.loads(get_resp.data.decode())
+    assert get_resp.status_code == 200
+    assert get_data['mines'][0]['guid'] == TEST_MINE_GUID
+
+
 def test_get_mines_empty(test_client, auth_headers):
     get_resp = test_client.get('/mines?page=2', headers=auth_headers['full_auth_header'])
     get_data = json.loads(get_resp.data.decode())
