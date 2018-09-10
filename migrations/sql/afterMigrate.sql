e440afe4c0da7b05edb82fd2a40005aebb5f2049
Gyanesh Mishra <gyan@freshworks.io>
2018-09-10 15:30:23 -0700

    update crypto extension check

--- migrations/sql/afterMigrate.sql
+++ migrations/sql/afterMigrate.sql
@@ -1,5 +1,6 @@
 DROP DATABASE IF EXISTS mds_test;
 CREATE DATABASE mds_test WITH TEMPLATE mds;
+COMMIT;
 
 -- Grant all table permissions to the regular user
 GRANT ALL PRIVILEGES ON DATABASE mds TO mds;
