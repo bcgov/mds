bd8d57175855ecdf61cbc2b40eb7780ea28debbc
samfresh <samuel@freshworks.io>
2018-09-13 13:59:25 -0700

    added phone contact and email to backend

--- python-backend/app/mines/resources/location.py
+++ python-backend/app/mines/resources/location.py
@@ -6,10 +6,6 @@ from app.extensions import jwt
 
 
 class MineLocationResource(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('first_name', type=str)
-    parser.add_argument('surname', type=str)
-
     @jwt.requires_roles(["mds-mine-view"])
     def get(self, mine_location_guid):
         mine_location = MineLocation.find_by_mine_location_guid(mine_location_guid)
