43142ed6238896e4c283d942df18ad05a23feb06
Gyanesh Mishra <gyan@freshworks.io>
2018-09-18 16:58:41 -0700

    update failing test

--- python-backend/tests/person/resources/test_person_list_resource.py
+++ python-backend/tests/person/resources/test_person_list_resource.py
@@ -1,5 +1,4 @@
 import json
-from tests.constants import TEST_PERSON_3_GUID
 
 
 # GET
@@ -7,4 +6,3 @@ def test_get_persons(test_client, auth_headers):
     get_resp = test_client.get('/persons', headers=auth_headers['full_auth_header'])
     get_data = json.loads(get_resp.data.decode())
     assert get_resp.status_code == 200
-    assert get_data['persons'][0]['person_guid'] == TEST_PERSON_3_GUID
