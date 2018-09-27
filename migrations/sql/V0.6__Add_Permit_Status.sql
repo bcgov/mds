cf9f0594150c5e2243626bbdbe8b022ee3f0c7a5
samfresh <samuel@freshworks.io>
2018-09-27 15:51:19 -0700

    rolled back to correct change for DB change

--- migrations/sql/V0.6__Add_Permit_Status.sql
+++ migrations/sql/V0.6__Add_Permit_Status.sql
@@ -1,5 +1,6 @@
 CREATE TABLE permit_status_code (
   permit_status_code character varying(2) PRIMARY KEY NOT NULL,
+  permit_status_code_guid uuid DEFAULT gen_random_uuid(),
   description character varying(100) NOT NULL,
   display_order smallint,
   effective_date date NOT NULL DEFAULT now(),
@@ -8,7 +9,7 @@ CREATE TABLE permit_status_code (
   create_timestamp timestamp with time zone NOT NULL DEFAULT current_timestamp,
   update_user      character varying(60) NOT NULL,
   update_timestamp timestamp with time zone NOT NULL DEFAULT current_timestamp,
-CONSTRAINT display_order_max_length check (display_order > -1 AND display_order < 1000)
+CONSTRAINT display_order_max_length check (display_order < 1000)
 );
 
 CREATE TABLE permit (
