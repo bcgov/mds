60a940149057174345b2c0f102229b9c3b0b87ad
samfresh <samuel@freshworks.io>
2018-10-02 11:37:46 -0700

    changes to seed data, and ordered the permit information from mine endpoint

--- migrations/sql/afterMigrate__3__seed_data.sql
+++ migrations/sql/afterMigrate__3__seed_data.sql
@@ -6,10 +6,8 @@ INSERT INTO permit_status_code (
     update_user
     )
 VALUES
-    ('N', 'New permit', 10, 'system-mds', 'system-mds'),
-    ('A', 'Ammended Permit', 20, 'system-mds', 'system-mds'),
-    ('Z', 'Permit Not required', 30, 'system-mds', 'system-mds'),
-    ('M', 'Permit type not selected', 40, 'system-mds', 'system-mds')
+    ('O', 'Open permit', 10, 'system-mds', 'system-mds'),
+    ('C', 'Closed Permit', 20, 'system-mds', 'system-mds'),
 ON CONFLICT DO NOTHING;
 
 INSERT INTO party_type_code (
