a72cb4444d1fbedbe49c8a29b1cde00feb494a4f
samfresh <38572614+samfresh@users.noreply.github.com>
2018-10-10 13:53:09 -0700

    Got rid of parties/name endpoint and updated parties search list resource to be the new parties list endpoint (#151)

--- python-backend/tests/person/resources/test_person_list_resource.py
+++ python-backend/tests/person/resources/test_person_list_resource.py
@@ -1,5 +1,5 @@
 import json
-from tests.constants import TEST_PERSON_GUID, TEST_PERSON_2_GUID, TEST_PERSON_3_GUID
+from tests.constants import TEST_FIRST_NAME, TEST_FIRST_NAME_2, TEST_FIRST_NAME_3, TEST_PERSON_GUID, TEST_PERSON_2_GUID, TEST_PERSON_3_GUID
 
 
 # GET
@@ -8,3 +8,10 @@ def test_get_persons(test_client, auth_headers):
     get_data = json.loads(get_resp.data.decode())
     assert get_resp.status_code == 200
     assert get_data['parties'][0]['party_guid'] in [TEST_PERSON_GUID, TEST_PERSON_2_GUID, TEST_PERSON_3_GUID]
+
+
+def test_get_persons_by_list_search(test_client, auth_headers):
+    get_resp = test_client.get('/parties?type=per&search=first_name3', headers=auth_headers['full_auth_header'])
+    get_data = json.loads(get_resp.data.decode())
+    assert get_resp.status_code == 200
+    assert get_data['parties'][0]['first_name'] in [TEST_FIRST_NAME, TEST_FIRST_NAME_2, TEST_FIRST_NAME_3]
