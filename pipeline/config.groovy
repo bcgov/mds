830fa19d4224bc170dc406f4d0544d45c74241fd
Gyanesh Mishra <gyan@freshworks.io>
2018-10-11 16:18:00 -0700

    fix host

--- pipeline/config.groovy
+++ pipeline/config.groovy
@@ -71,6 +71,7 @@ app {
                         'params':[
                             'NAME':"mds-frontend",
                             'SUFFIX': "${app.build.suffix}",
+                            'BASE_PATH': "/${app.git.changeId}",
                             'VERSION':"${app.build.version}",
                             'SOURCE_CONTEXT_DIR': "frontend",
                             'SOURCE_REPOSITORY_URL': "${app.git.uri}",
