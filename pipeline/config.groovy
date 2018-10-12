16ba15d0bf1c771f7df96d93eb564c3eff20efd4
Gyanesh Mishra <gyan@freshworks.io>
2018-10-12 09:43:34 -0700

    add application suffix

--- pipeline/config.groovy
+++ pipeline/config.groovy
@@ -140,6 +140,7 @@ app {
                     'params':[
                             'NAME':"mds-frontend",
                             'SUFFIX': "${vars.deployment.suffix}",
+                            'APPLICATION_SUFFIX': "${vars.deployment.application_suffix}",
                             'TAG_NAME':"${app.deployment.version}",
                             'APPLICATION_DOMAIN': "${vars.modules.'mds-frontend'.HOST}",
                             'BASE_PATH': "${vars.modules.'mds-frontend'.PATH}",
@@ -157,7 +158,6 @@ app {
                             'NAME':"mds-python-backend",
                             'FLYWAY_NAME':"mds-flyway-migration-client",
                             'SUFFIX': "${vars.deployment.suffix}",
-                            'APPLICATION_SUFFIX': "${vars.deployment.application_suffix}",
                             'VERSION':"${app.deployment.version}",
                             'JWT_OIDC_WELL_KNOWN_CONFIG': "${vars.keycloak.known_config_url}",
                             'JWT_OIDC_AUDIENCE': "${vars.keycloak.clientId}",
