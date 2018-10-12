84bb1d86d611470b2970e33a597ad455303ce9cf
Gyanesh Mishra <gyan@freshworks.io>
2018-10-11 17:38:25 -0700

    try git vars for build

--- pipeline/config.groovy
+++ pipeline/config.groovy
@@ -71,7 +71,7 @@ app {
                         'params':[
                             'NAME':"mds-frontend",
                             'SUFFIX': "${app.build.suffix}",
-                            'BASE_PATH': "/${app.git.changeId}",
+                            'BASE_PATH': "${vars.modules.'mds-frontend'.PATH}",
                             'VERSION':"${app.build.version}",
                             'SOURCE_CONTEXT_DIR': "frontend",
                             'SOURCE_REPOSITORY_URL': "${app.git.uri}",
