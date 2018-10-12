20d9563ff1128cc3415269bac2814ae95acba55b
samfresh <38572614+samfresh@users.noreply.github.com>
2018-10-12 15:13:40 -0700

    Renamed party to person for tests, in constants and also within party testing files. (#165)
    Renamed party to person for tests, in constants and also within party testing files.
    These were just the tests that were not converted from 'person' to 'party'

--- python-backend/tests/party/models/test_mgr_appointment_model.py
+++ python-backend/tests/party/models/test_mgr_appointment_model.py
@@ -1,11 +1,11 @@
-from tests.constants import TEST_PERSON_GUID, TEST_MANAGER_GUID, TEST_MINE_GUID
+from tests.constants import TEST_PARTY_PER_GUID_1, TEST_MANAGER_GUID, TEST_MINE_GUID
 from app.api.party.models.party import MgrAppointment
 
 
 # Mgr Appointment Class Methods
 def test_mgr_model_find_by_person_guid(test_client, auth_headers):
-    mgr = MgrAppointment.find_by_party_guid(TEST_PERSON_GUID)
-    assert str(mgr[0].party_guid) == TEST_PERSON_GUID
+    mgr = MgrAppointment.find_by_party_guid(TEST_PARTY_PER_GUID_1)
+    assert str(mgr[0].party_guid) == TEST_PARTY_PER_GUID_1
 
 
 def test_mgr_model_find_by_mgr_appointment_guid(test_client, auth_headers):
