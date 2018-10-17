4a58631948be6937ab9edaa3dc67036550118f7c
VivianCheungFW <38731284+VivianCheungFW@users.noreply.github.com>
2018-10-17 12:41:50 -0700

    modify etl for manager based on meeting feedback (#172)

--- migrations/etl/0_dataCleanup.sql
+++ migrations/etl/0_dataCleanup.sql
@@ -1,3 +1,4 @@
+-- OPTIONAL:  This script is used to erase all the existing data in MDS 
 DROP TABLE IF EXISTS etl_profile, etl_manager, etl_permit;
 Truncate party cascade;
 Truncate mine_identity cascade;
