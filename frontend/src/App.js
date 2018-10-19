79749d75af00114992bf9bfee75db0b84e9542e5
Gyanesh Mishra <gyan@freshworks.io>
2018-10-18 17:04:02 -0700

    fix redux hot reloading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import configureStore from './store/configureStore';
 import Routes from './routes/Routes';
 import { hot } from 'react-hot-loader'
-
-export const store = configureStore();
 
 class App extends Component {
   render() {
     return(
-      <Provider store={store}>
         <BrowserRouter basename={process.env.BASE_PATH}>
           <Routes />
         </BrowserRouter>
-      </Provider>
     );
   }
 }
