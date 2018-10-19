50f29ee9a945c6084b1164091ee9b47fa1affa54
Gyanesh Mishra <gyan@freshworks.io>
2018-10-19 14:50:35 -0700

    Add local hosting for fcl

--- frontend/src/components/maps/MinePin.js
+++ frontend/src/components/maps/MinePin.js
@@ -51,15 +51,16 @@ export class MinePin extends Component {
   }
 
   componentWillMount() {
+    const fclURL = `${window.location.origin}${process.env.BASE_PATH}/public/FlareClusterLayer/fcl/FlareClusterLayer_v4.js`;
+
     loadModules(["esri/symbols/SimpleMarkerSymbol",
       "esri/renderers/ClassBreaksRenderer",
       "esri/symbols/SimpleLineSymbol",
-      "esri/symbols/SimpleFillSymbol",
       "esri/geometry/SpatialReference",
       "esri/PopupTemplate",
-      "https://flareclusterlayer.azurewebsites.net/fcl/FlareClusterLayer_v4.js"]).then((
+      fclURL]).then((
       [SimpleMarkerSymbol,
-        ClassBreaksRenderer, SimpleLineSymbol, SimpleFillSymbol,
+        ClassBreaksRenderer, SimpleLineSymbol,
       SpatialReference, PopupTemplate, FlareClusterLayer]) => {
       // create a new Graphic for every mine in the array or fetch the ID from the URL for a single mine.
       // data must be passed into this.points() and this.popupTemplate to associate the correct information with the correct lat/long.
@@ -81,63 +82,23 @@ export class MinePin extends Component {
         "type": "picture-marker"
       };
 
-      var renderer = new ClassBreaksRenderer({
+      const renderer = new ClassBreaksRenderer({
           defaultSymbol: defaultSym
       });
       renderer.field = "clusterCount";
 
-      var smSymbol = new SimpleMarkerSymbol({ size: 22, outline: new SimpleLineSymbol({ color: [221, 159, 34, 1] }), color: [255, 204, 102, 1] });
-      var mdSymbol = new SimpleMarkerSymbol({ size: 24, outline: new SimpleLineSymbol({ color: [82, 163, 204, 1] }), color: [102, 204, 255, 1] });
-      var lgSymbol = new SimpleMarkerSymbol({ size: 28, outline: new SimpleLineSymbol({ color: [41, 163, 41, 1] }), color: [51, 204, 51, 1] });
-      var xlSymbol = new SimpleMarkerSymbol({ size: 32, outline: new SimpleLineSymbol({ color: [200, 52, 59, 1] }), color: [250, 65, 74, 1] });
+      const smSymbol = new SimpleMarkerSymbol({ size: 22, outline: new SimpleLineSymbol({ color: [221, 159, 34, 1] }), color: [255, 204, 102, 1] });
+      const mdSymbol = new SimpleMarkerSymbol({ size: 24, outline: new SimpleLineSymbol({ color: [82, 163, 204, 1] }), color: [102, 204, 255, 1] });
+      const lgSymbol = new SimpleMarkerSymbol({ size: 28, outline: new SimpleLineSymbol({ color: [41, 163, 41, 1] }), color: [51, 204, 51, 1] });
+      const xlSymbol = new SimpleMarkerSymbol({ size: 32, outline: new SimpleLineSymbol({ color: [200, 52, 59, 1] }), color: [250, 65, 74, 1] });
 
       renderer.addClassBreakInfo(0, 19, smSymbol);
       renderer.addClassBreakInfo(20, 150, mdSymbol);
       renderer.addClassBreakInfo(151, 1000, lgSymbol);
       renderer.addClassBreakInfo(1001, Infinity, xlSymbol);
 
-      var areaRenderer;
-
-      // if area display mode is set. Create a renderer to display cluster areas. Use SimpleFillSymbols as the areas are polygons
-      var defaultAreaSym = new SimpleFillSymbol({
-          style: "solid",
-          color: [0, 0, 0, 0.2],
-          outline: new SimpleLineSymbol({ color: [0, 0, 0, 0.3] })
-      });
-
-      areaRenderer = new ClassBreaksRenderer({
-          defaultSymbol: defaultAreaSym
-      });
-      areaRenderer.field = "clusterCount";
-
-      var smAreaSymbol = new SimpleFillSymbol({ color: [255, 204, 102, 0.4], outline: new SimpleLineSymbol({ color: [221, 159, 34, 1], style: "dash" }) });
-      var mdAreaSymbol = new SimpleFillSymbol({ color: [102, 204, 255, 0.4], outline: new SimpleLineSymbol({ color: [82, 163, 204, 1], style: "dash" }) });
-      var lgAreaSymbol = new SimpleFillSymbol({ color: [51, 204, 51, 0.4], outline: new SimpleLineSymbol({ color: [41, 163, 41, 1], style: "dash" }) });
-      var xlAreaSymbol = new SimpleFillSymbol({ color: [250, 65, 74, 0.4], outline: new SimpleLineSymbol({ color: [200, 52, 59, 1], style: "dash" }) });
-
-      areaRenderer.addClassBreakInfo(0, 19, smAreaSymbol);
-      areaRenderer.addClassBreakInfo(20, 150, mdAreaSymbol);
-      areaRenderer.addClassBreakInfo(151, 1000, lgAreaSymbol);
-      areaRenderer.addClassBreakInfo(1001, Infinity, xlAreaSymbol);
-
-      // Set up another class breaks renderer to style the flares individually
-      var flareRenderer = new ClassBreaksRenderer({
-          defaultSymbol: renderer.defaultSymbol
-      });
-      flareRenderer.field = "clusterCount";
-
-      var smFlareSymbol = new SimpleMarkerSymbol({ size: 14, color: [255, 204, 102, 1], outline: new SimpleLineSymbol({ color: [221, 159, 34, 1] }) });
-      var mdFlareSymbol = new SimpleMarkerSymbol({ size: 14, color: [102, 204, 255, 1], outline: new SimpleLineSymbol({ color: [82, 163, 204, 1] }) });
-      var lgFlareSymbol = new SimpleMarkerSymbol({ size: 14, color: [51, 204, 51, 1], outline: new SimpleLineSymbol({ color: [41, 163, 41, 1] }) });
-      var xlFlareSymbol = new SimpleMarkerSymbol({ size: 14, color: [250, 65, 74, 1], outline: new SimpleLineSymbol({ color: [200, 52, 59, 1] }) });
-
-      flareRenderer.addClassBreakInfo(0, 19, smFlareSymbol);
-      flareRenderer.addClassBreakInfo(20, 150, mdFlareSymbol);
-      flareRenderer.addClassBreakInfo(151, 1000, lgFlareSymbol);
-      flareRenderer.addClassBreakInfo(1001, Infinity, xlFlareSymbol);
-
       // set up a popup template
-      var popupTemplate = new PopupTemplate({
+      const popupTemplate = new PopupTemplate({
         title: "{templateTitle}",
         content: "{templateContent}"
       });
@@ -150,22 +111,18 @@ export class MinePin extends Component {
         return {
           y : y,
           x: x,
-          id: id,
           templateTitle: templateInfo.title,
           templateContent: templateInfo.content,
-          facilityType: "Mine"}
+        }
       });
 
-      var options = {
+      const options = {
         id: "flare-cluster-layer",
         clusterRenderer: renderer,
-        areaRenderer: areaRenderer,
-        flareRenderer: flareRenderer,
         singlePopupTemplate: popupTemplate,
         spatialReference: new SpatialReference({ "wkid": 4326 }),
-        singleFlareTooltipProperty: "id",
         maxSingleFlareCount: 8,
-        clusterRatio: 100,
+        clusterRatio: 75,
         data: fclData
       }
 
