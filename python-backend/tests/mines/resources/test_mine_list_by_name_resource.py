a59589ba0f250425b0e5a1d5a6d1c9179cbcad1f
samfresh <38572614+samfresh@users.noreply.github.com>
2018-10-24 10:13:11 -0700

    Add permit no as a search filter in mine search. (#181)
    Added the permit no in the mine list search filter.

--- python-backend/tests/mines/resources/test_mine_list_by_name_resource.py
+++ python-backend/tests/mines/resources/test_mine_list_by_name_resource.py
@@ -28,3 +28,17 @@ def test_get_mines_by_list_search(test_client, auth_headers):
     }
     assert get_resp.status_code == 200
     assert get_data['mines'][0] == context
+
+
+def test_get_mines_by_list_search_by_permit_no(test_client, auth_headers):
+    get_resp = test_client.get('/mines/names?search=TEST56789012', headers=auth_headers['full_auth_header'])
+    get_data = json.loads(get_resp.data.decode())
+    context = {
+        'guid': TEST_MINE_GUID,
+        'mine_name': TEST_MINE_NAME,
+        'mine_no': TEST_MINE_NO,
+        'latitude': TEST_LAT_1,
+        'longitude': TEST_LONG_1
+    }
+    assert get_resp.status_code == 200
+    assert get_data['mines'][0] == context
