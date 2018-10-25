08ac45ad4fc5c1835e02b130500adaff7c95c963
Meredith Mumby <meredith@Merediths-MacBook-Pro.local>
2018-10-25 12:15:00 -0700

    in progress

--- frontend/src/selectors/mineSelectors.js
+++ frontend/src/selectors/mineSelectors.js
@@ -1,9 +1,37 @@
 import * as mineReducer from '@/reducers/mineReducer';
+import { createSelector } from 'reselect';
 
 export const getMines = (state) => mineReducer.getMines(state);
 export const getMineIds = (state) => mineReducer.getMineIds(state);
 export const getMineNames = (state) => mineReducer.getMineNames(state);
 export const getMinesPageData = (state) => mineReducer.getMinesPageData(state);
-export const getCurrentPermittees = (state) => mineReducer.getCurrentPermittees(state);
-export const getCurrentPermitteeIds = (state) => mineReducer.getCurrentPermitteeIds(state);
 export const getMineStatusOptions = (state) => mineReducer.getMineStatusOptions(state);
+
+
+export const getCurrentPermittees = createSelector(
+  [getMines, getMineGuid], 
+  (mines, mineGuid) => {
+    const permitteeObj = {};
+    if (mineGuid) {
+     mines[mineGuid].mine_permit.map((permit) => {
+        permitteeObj[permit.permittee[0].party_guid] = permit.permittee[0];
+      })
+    }
+    return permitteeObj;
+  }
+);
+
+export const getCurrentPermitteeIds = createSelector(
+  [getMines, getMineGuid], 
+  (mines, mineGuid) => {
+    const permitteeIds = [];
+    let unique;
+    if (mineGuid) {
+      mines[mineGuid].mine_permit.map((permit) => {
+        permitteeIds.push(permit.permittee[0].party_guid);
+      })
+    }
+    unique = [...new Set(permitteeIds)];
+    return unique;
+    }
+);
