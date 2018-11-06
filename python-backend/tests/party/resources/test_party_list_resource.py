8e9f18c0a96875bd5b5f951b160d45a02759cb6a
samfresh <samuel@freshworks.io>
2018-11-06 13:01:31 -0800

    Fix party search to include organization type
    There was an issue where party search was working with person type for party, first name and last name
    was working properly. Unfortunately the search was not working with organization. The issue was that
    the hybrid_property used to make 'name' for Party, was not working for queries, a .expression was used
    and sqlalchemy func with concat solved the issue.
    
    There were a couple of code clean up done, as the search was placed into the Party class, and a
    couple of test cases were added to test implicit and explicit search.

--- python-backend/tests/party/resources/test_party_list_resource.py
+++ python-backend/tests/party/resources/test_party_list_resource.py
@@ -1,5 +1,5 @@
 import json
-from tests.constants import TEST_PARTY_PER_FIRST_NAME_1, TEST_PARTY_ORG_GUID, TEST_PARTY_PER_FIRST_NAME_2, TEST_PARTY_PER_FIRST_NAME_3, TEST_PARTY_PER_GUID_1, TEST_PARTY_PER_GUID_2, TEST_PARTY_PER_GUID_3
+from tests.constants import TEST_PARTY_ORG_NAME, TEST_PARTY_PER_FIRST_NAME_1, TEST_PARTY_ORG_GUID, TEST_PARTY_PER_FIRST_NAME_2, TEST_PARTY_PER_FIRST_NAME_3, TEST_PARTY_PER_GUID_1, TEST_PARTY_PER_GUID_2, TEST_PARTY_PER_GUID_3
 
 
 # GET
@@ -10,8 +10,29 @@ def test_get_persons(test_client, auth_headers):
     assert get_data['parties'][0]['party_guid'] in [TEST_PARTY_PER_GUID_1, TEST_PARTY_PER_GUID_2, TEST_PARTY_PER_GUID_3, TEST_PARTY_ORG_GUID]
 
 
-def test_get_persons_by_list_search(test_client, auth_headers):
-    get_resp = test_client.get('/parties?type=per&search=first_name3', headers=auth_headers['full_auth_header'])
+def test_search_person_by_explicit(test_client, auth_headers):
+    get_resp = test_client.get('/parties?search=first_name3&type=per', headers=auth_headers['full_auth_header'])
     get_data = json.loads(get_resp.data.decode())
     assert get_resp.status_code == 200
     assert get_data['parties'][0]['first_name'] in [TEST_PARTY_PER_FIRST_NAME_1, TEST_PARTY_PER_FIRST_NAME_2, TEST_PARTY_PER_FIRST_NAME_3]
+
+
+def test_search_persons_by_implicit(test_client, auth_headers):
+    get_resp = test_client.get('/parties?search=first_name3', headers=auth_headers['full_auth_header'])
+    get_data = json.loads(get_resp.data.decode())
+    assert get_resp.status_code == 200
+    assert get_data['parties'][0]['first_name'] in [TEST_PARTY_PER_FIRST_NAME_1, TEST_PARTY_PER_FIRST_NAME_2, TEST_PARTY_PER_FIRST_NAME_3]
+
+
+def test_search_org_by_implicit(test_client, auth_headers):
+    get_resp = test_client.get('/parties?search=test_company', headers=auth_headers['full_auth_header'])
+    get_data = json.loads(get_resp.data.decode())
+    assert get_resp.status_code == 200
+    assert get_data['parties'][0]['party_name'] == TEST_PARTY_ORG_NAME
+
+
+def test_search_org_by_explicit(test_client, auth_headers):
+    get_resp = test_client.get('/parties?search=test_company&type=org', headers=auth_headers['full_auth_header'])
+    get_data = json.loads(get_resp.data.decode())
+    assert get_resp.status_code == 200
+    assert get_data['parties'][0]['party_name'] == TEST_PARTY_ORG_NAME
