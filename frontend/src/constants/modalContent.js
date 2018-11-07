cd637f5fda87fa7c8d7e6c78d0213cc00b6849b6
VivianCheungFW <vivian@freshworks.io>
2018-11-07 14:22:43 -0800

    undo change

--- frontend/src/constants/modalContent.js
+++ frontend/src/constants/modalContent.js
@@ -10,3 +10,4 @@ export const ORGANIZATION = 'ORG';
 
 export const PARTY_NOT_FOUND = `If the party is not in the list above, please add a new person or company below`;
 export const PERSON_NOT_FOUND = 'If the party is not in the list above, please add a new person below'
+
