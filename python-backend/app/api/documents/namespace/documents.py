7722e7dc41287d932a8857754363bd66fe6b22e7
Syrotuck, Jason <jasyrotuck@gmail.com>
2018-11-16 10:29:16 -0800

    typo in namespace paths

--- python-backend/app/api/documents/namespace/documents.py
+++ python-backend/app/api/documents/namespace/documents.py
@@ -5,5 +5,5 @@ from ..required.resources.required_documents import RequiredDocumentResource
 
 api = Namespace('documents', description='MDS records of documents, expected documents, and required documents')
 
-api.add_resource(MineExpectedDocumentResource, '/mines/expected', '/mine/expected/<string:mine_guid>')
+api.add_resource(MineExpectedDocumentResource, '/mines/expected', '/mines/expected/<string:mine_guid>')
 api.add_resource(RequiredDocumentResource, '/required/', '/required/<string:req_doc_guid>')
