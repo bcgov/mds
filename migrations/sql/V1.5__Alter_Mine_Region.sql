6e005a2e71224a650655d43aad0f0c360bf3f0cc
Gyanesh Mishra <gyan@freshworks.io>
2018-11-20 09:45:33 -0800

    remove non null constraint

--- migrations/sql/V1.5__Alter_Mine_Region.sql
+++ migrations/sql/V1.5__Alter_Mine_Region.sql
@@ -8,6 +8,6 @@ ALTER TABLE mine_region_code
 ALTER TABLE mine_region_code RENAME region_code TO mine_region_code;
 
 ALTER TABLE mine_detail
-    ADD mine_region character varying(2) NOT NULL DEFAULT '',
+    ADD mine_region character varying(2),
     ADD FOREIGN KEY(mine_region) REFERENCES mine_region_code(mine_region_code)
 ;
