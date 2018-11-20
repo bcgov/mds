cc609b3d4a29dd7e11e6ee8c20a93de30d14ee2a
VivianCheung <vivian@freshworks.io>
2018-11-19 22:22:22 -0800

    fix

--- functional-tests/src/test/groovy/modules/Tab_Contact.groovy
+++ functional-tests/src/test/groovy/modules/Tab_Contact.groovy
@@ -12,7 +12,7 @@ class Tab_Contact extends Module {
         updateManagerForm {module Form_UpdateManager}
         updatePermitteeForm {module Form_UpdatePermittee}
 
-        manager_name (wait:true) {$("td", 'data-label':"Mine Manager").find("a").find("p.p-large",0).text()}
+        manager_name (wait:true) {$("td", 'data-label':'Mine Manager').find("a").find("p.p-large",0).text()}
         manager_date (wait:true) {$("td", 'data-label':'Manager Since').find("p.p-large",1).text()}
         manager_null_screen (required: false){$("h1", text:"No assigned mine manager")}
 
