9a125d4bf732f804f3deb40c5fb6e5c256b358fb
Jason Syrotuck <jasyrotuck@gmail.com>
2018-11-23 13:11:20 -0800

    test on expected_document delete

--- python-backend/app/api/documents/expected/resources/documents.py
+++ python-backend/app/api/documents/expected/resources/documents.py
@@ -21,6 +21,8 @@ class ExpectedDocumentResource(Resource, UserMixin, ErrorMixin):
         if exp_doc_guid is None:
             return self.create_error_payload(404, 'Must provide a expected document guid.'), 404
         mine_exp_doc = ExpectedDocument.find_by_exp_document_guid(exp_doc_guid)
+        if mine_exp_doc is None:
+            return self.create_error_payload(404, 'Expected document not found'), 404
         return { 'expected_document' : mine_exp_doc.json() }
 
     @api.doc(params={'exp_doc_guid': 'Required: Mine number or guid. returns list of expected documents for the mine'})
--- python-backend/tests/documents/expected/resources/test_expected_documents_resource.py
+++ python-backend/tests/documents/expected/resources/test_expected_documents_resource.py
@@ -28,3 +28,13 @@ def test_put_expected_document_by_guid(test_client, auth_headers):
     get_data = json.loads(get_resp.data.decode())
     assert get_resp.status_code == 200
     assert get_data['expected_document']['exp_document_name'] == put_data['expected_document']['exp_document_name']
+
+
+def test_del_expected_document_by_guid(test_client, auth_headers):
+    del_resp = test_client.delete('/documents/expected/' + TEST_EXPECTED_DOCUMENT_GUID1 , headers=auth_headers['full_auth_header'])
+    del_data = json.loads(del_resp.data.decode())
+    assert del_resp.status_code == 200
+
+    get_resp = test_client.get('/documents/expected/' + TEST_EXPECTED_DOCUMENT_GUID1 , headers=auth_headers['full_auth_header'])
+    get_data = json.loads(get_resp.data.decode())
+    assert get_resp.status_code == 404
