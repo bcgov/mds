3937af1d7a7f07285013f9c1d9168dd2d345afe3
Jason Aitchison <JasonAitchison@sierrasystems.com>
2018-11-26 10:43:04 -0800

    field lengths

--- migrations/sql/V1.7_Add_Documents.sql
+++ migrations/sql/V1.7_Add_Documents.sql
@@ -1,13 +1,21 @@
 CREATE TABLE public.document_manager (
 	document_manager_id serial NOT NULL,
 	document_guid uuid NOT NULL DEFAULT gen_random_uuid(),
-	full_storage_path varchar NOT NULL,
+	full_storage_path varchar(100) NOT NULL,
 	uploade_date timestamptz NOT NULL,
-	file_display_name varchar NOT NULL,
-	path_display_name varchar NOT NULL,
+	file_display_name varchar(40) NOT NULL,
+	path_display_name varchar(100) NOT NULL,
 	create_user varchar(60) NOT NULL,
 	create_timestamp timestamptz NOT NULL DEFAULT now(),
 	update_user varchar(60) NOT NULL,
 	update_timestamp timestamptz NOT NULL DEFAULT now(),
 	CONSTRAINT document_manager_pk PRIMARY KEY (document_manager_id)
 );
+
+-- Permissions
+
+ALTER TABLE public.document_manager OWNER TO mds;
+GRANT ALL ON TABLE public.document_manager TO mds;
+
+-- Comment
+COMMENT ON TABLE document_manager IS 'documents representations on the file volumes stored in our table structure';
