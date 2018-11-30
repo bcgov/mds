2261d7149f99e9c783935cac09a28eb5ea2173a9
justinmacaulay <jmac@uvic.ca>
2018-11-30 14:31:49 -0800

    refactored tests

--- python-backend/tests/document_manager/resources/test_document_manager_resource.py
+++ python-backend/tests/document_manager/resources/test_document_manager_resource.py
@@ -18,71 +18,68 @@ def setup_info():
         file_upload_4 = (io.BytesIO(b'Test File'), 'test_file_4.sh'),
     )
 
-def test_happy_path_file_upload(test_client, auth_headers, tmpdir, setup_info):
-    test_dir = tmpdir.mkdir("mines")
-    test_dir_path = str(test_dir).split('/')
+def test_happy_path_file_upload(test_client, auth_headers, setup_info):
+    test_dir = os.mkdir(setup_info.get('base_path') + '/test')
+    test_dir_path = setup_info.get('base_path') + '/test'
     
     data = {
-        'folder': f'{test_dir_path[-2]}/{test_dir_path[-1]}/{setup_info.get("mine_guid")}/tailings',
+        'folder': f'test/mines/{setup_info.get("mine_guid")}/tailings',
         'file': setup_info.get('file_upload_1'),
-        'pretty_folder': f'mines/{setup_info.get("mine_no")}/tailings',
+        'pretty_folder': f'test/mines/{setup_info.get("mine_no")}/tailings',
     }
     
     post_resp = test_client.post('/document-manager', data=data, headers=auth_headers['full_auth_header'])
     post_data = json.loads(post_resp.data.decode())
     filename = post_data['document_manager_guids'][0]+'.pdf'
-    saved_file = open(f'{setup_info.get("base_path")}/{test_dir_path[-2]}/{test_dir_path[-1]}/{setup_info.get("mine_guid")}/tailings/{filename}') 
+    saved_file = open(f'{test_dir_path}/mines/{setup_info.get("mine_guid")}/tailings/{filename}') 
     
     assert post_resp.status_code == 200
     assert saved_file
 
-    test_dir_path = test_dir_path[:-1]
-    shutil.rmtree('/'.join(test_dir_path))
+    shutil.rmtree(test_dir_path)
 
-def test_happy_path_file_upload_two_files(test_client, auth_headers, tmpdir, setup_info):
-    test_dir = tmpdir.mkdir("mines")
-    test_dir_path = str(test_dir).split('/')
+def test_happy_path_file_upload_two_files(test_client, auth_headers, setup_info):
+    test_dir = os.mkdir(setup_info.get('base_path') + '/test')
+    test_dir_path = setup_info.get('base_path') + '/test'
     
     data = {
-        'folder': f'{test_dir_path[-2]}/{test_dir_path[-1]}/{setup_info.get("mine_guid")}/tailings',
+        'folder': f'test/mines/{setup_info.get("mine_guid")}/tailings',
         'file': [setup_info.get('file_upload_2'), setup_info.get('file_upload_1')] ,
-        'pretty_folder': f'mines/{setup_info.get("mine_no")}/tailings',
+        'pretty_folder': f'test/mines/{setup_info.get("mine_no")}/tailings',
     }
     
     post_resp = test_client.post('/document-manager', data=data, headers=auth_headers['full_auth_header'])
     post_data = json.loads(post_resp.data.decode())
     filename = post_data['document_manager_guids'][0]+'.docx'
     filename2 = post_data['document_manager_guids'][1]+'.pdf'
-    saved_file = open(f'{setup_info.get("base_path")}/{test_dir_path[-2]}/{test_dir_path[-1]}/{setup_info.get("mine_guid")}/tailings/{filename}')  
-    saved_file_2 = open(f'{setup_info.get("base_path")}/{test_dir_path[-2]}/{test_dir_path[-1]}/{setup_info.get("mine_guid")}/tailings/{filename2}')
+    saved_file = open(f'{test_dir_path}/mines/{setup_info.get("mine_guid")}/tailings/{filename}')  
+    saved_file_2 = open(f'{test_dir_path}/mines/{setup_info.get("mine_guid")}/tailings/{filename2}')
     
     assert post_resp.status_code == 200
     assert saved_file
     assert saved_file_2
     assert saved_file is not saved_file_2
 
-    test_dir_path = test_dir_path[:-1]
-    shutil.rmtree('/'.join(test_dir_path))
+    shutil.rmtree(test_dir_path)
     
 
-def test_bad_file_type(test_client, auth_headers, tmpdir, setup_info):
-    test_dir = tmpdir.mkdir("mines")
-    test_dir_path = str(test_dir).split('/')
+def test_bad_file_type(test_client, auth_headers, setup_info):
+    test_dir = os.mkdir(setup_info.get('base_path') + '/test')
+    test_dir_path = setup_info.get('base_path') + '/test'
     
     data = {
-        'folder': f'{test_dir_path[-2]}/{test_dir_path[-1]}/{setup_info.get("mine_guid")}/tailings',
+        'folder': f'test/mines/{setup_info.get("mine_guid")}/tailings',
         'file': [setup_info.get('file_upload_2'), setup_info.get('file_upload_3'), setup_info.get('file_upload_4')] ,
-        'pretty_folder': f'mines/{setup_info.get("mine_no")}/tailings',
+        'pretty_folder': f'test/mines/{setup_info.get("mine_no")}/tailings',
     }
 
     post_resp = test_client.post('/document-manager', data=data, headers=auth_headers['full_auth_header'])
     post_data = json.loads(post_resp.data.decode())
     filename = post_data['document_manager_guids'][0]+'.docx'
-    saved_file = open(f'{setup_info.get("base_path")}/{test_dir_path[-2]}/{test_dir_path[-1]}/{setup_info.get("mine_guid")}/tailings/{filename}')  
+    saved_file = open(f'{test_dir_path}/mines/{setup_info.get("mine_guid")}/tailings/{filename}')  
     
     assert len(post_data['errors']) == 2
     assert len(post_data['document_manager_guids']) == 1
     assert saved_file
     
-    test_dir_path = test_dir_path[:-1]
-    shutil.rmtree('/'.join(test_dir_path))
+    shutil.rmtree(test_dir_path)
