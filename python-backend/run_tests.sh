183a6e98cec2e0087ad53befde53d2cc4422e73a
justinmacaulay <jmac@uvic.ca>
2018-11-30 15:03:57 -0800

    added an index to document manager

--- python-backend/run_tests.sh
+++ python-backend/run_tests.sh
@@ -2,7 +2,7 @@
 set -exv -o pipefail
 
 coverage erase
-coverage run --branch --source=app -m pytest --basetemp=/app/document_uploads
+coverage run --branch --source=app -m pytest
 coverage xml -i
 
 /opt/app-root/sonar-scanner-cli/bin/sonar-scanner
