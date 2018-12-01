d163d2baafc8426b7b58d41a866067c2337e042f
Ryan Rondeau <nayr974@users.noreply.github.com>
2018-11-30 16:45:46 -0800

    Fix Tests

--- python-backend/app/api/documents/expected/models/document.py
+++ python-backend/app/api/documents/expected/models/document.py
@@ -15,28 +15,32 @@ from ....utils.models_mixins import AuditMixin, Base
 class ExpectedDocument(AuditMixin, Base):
     __tablename__ = 'mine_expected_document'
 
-    exp_document_guid = db.Column(UUID(as_uuid=True), primary_key=True, server_default=FetchedValue())
-    #Foreign Key Columns
+    exp_document_guid = db.Column(
+        UUID(as_uuid=True), primary_key=True, server_default=FetchedValue())
+    # Foreign Key Columns
     req_document_guid = db.Column(UUID(as_uuid=True), nullable=True)
-    mine_guid = db.Column(UUID(as_uuid=True), db.ForeignKey('mine_identity.mine_guid'))
-    #Data Columns
+    mine_guid = db.Column(UUID(as_uuid=True),
+                          db.ForeignKey('mine_identity.mine_guid'))
+    # Data Columns
     exp_document_name = db.Column(db.String(100), nullable=False)
     exp_document_description = db.Column(db.String(300))
 
-    due_date = db.Column(db.DateTime) 
-    received_date = db.Column(db.DateTime) 
-    active_ind = db.Column(db.Boolean, nullable=False, server_default=FetchedValue())
-    exp_document_status_guid = db.Column(UUID(as_uuid=True), nullable=True)
+    due_date = db.Column(db.DateTime)
+    received_date = db.Column(db.DateTime)
+    active_ind = db.Column(db.Boolean, nullable=False,
+                           server_default=FetchedValue())
+    exp_document_status_guid = db.Column(
+        UUID(as_uuid=True), nullable=True, server_default=FetchedValue())
 
     def json(self):
         return {
-            'exp_document_guid' : str(self.exp_document_guid),
-            'req_document_guid' : str(self.req_document_guid),
-            'mine_guid' : str(self.mine_guid),
-            'exp_document_name' : str(self.exp_document_name),
-            'exp_document_description' : str(self.exp_document_description),
-            'due_date' : str(self.due_date),
-            'received_date' : str(self.received_date),
+            'exp_document_guid': str(self.exp_document_guid),
+            'req_document_guid': str(self.req_document_guid),
+            'mine_guid': str(self.mine_guid),
+            'exp_document_name': str(self.exp_document_name),
+            'exp_document_description': str(self.exp_document_description),
+            'due_date': str(self.due_date),
+            'received_date': str(self.received_date),
             'exp_document_status_guid': str(self.exp_document_status_guid),
         }
 
@@ -59,8 +63,10 @@ class ExpectedDocument(AuditMixin, Base):
 
         if due_date_type == 'FIS':
 
-            fiscal_year_end = datetime(current_year, march, day, hour, minute, second)
-            due_date = fiscal_year_end + relativedelta(months=int(period_in_months))
+            fiscal_year_end = datetime(
+                current_year, march, day, hour, minute, second)
+            due_date = fiscal_year_end + \
+                relativedelta(months=int(period_in_months))
 
             return due_date
 
--- python-backend/app/api/documents/expected/resources/documents.py
+++ python-backend/app/api/documents/expected/resources/documents.py
@@ -13,7 +13,8 @@ from ....utils.resources_mixins import UserMixin, ErrorMixin
 
 class ExpectedDocumentResource(Resource, UserMixin, ErrorMixin):
     parser = reqparse.RequestParser()
-    parser.add_argument('document', type=dict, required=True, help='document to change', location="json")
+    parser.add_argument('document', type=dict, required=True,
+                        help='document to change', location="json")
 
     @api.doc(params={'exp_doc_guid': 'Required: Mine number or guid. returns list of expected documents for the mine'})
     @jwt.requires_roles(["mds-mine-view"])
@@ -23,7 +24,7 @@ class ExpectedDocumentResource(Resource, UserMixin, ErrorMixin):
         mine_exp_doc = ExpectedDocument.find_by_exp_document_guid(exp_doc_guid)
         if mine_exp_doc is None:
             return self.create_error_payload(404, 'Expected document not found'), 404
-        return { 'expected_document' : mine_exp_doc.json() }
+        return {'expected_document': mine_exp_doc.json()}
 
     @api.doc(params={'exp_doc_guid': 'Required: Mine number or guid. Updates expected document'})
     @jwt.requires_roles(["mds-mine-create"])
@@ -34,22 +35,30 @@ class ExpectedDocumentResource(Resource, UserMixin, ErrorMixin):
         exp_doc = ExpectedDocument.find_by_exp_document_guid(exp_doc_guid)
         if exp_doc is not None:
             data = self.parser.parse_args()
-            updated_doc =  data['document']
+            updated_doc = data['document']
             if str(exp_doc.exp_document_guid) != updated_doc['exp_document_guid']:
                 return self.create_error_payload(500, 'exp_document does not match guid provided'), 500
 
             exp_doc.exp_document_name = updated_doc.get('exp_document_name')
-            exp_doc.exp_document_description = updated_doc.get('exp_document_description')
-            exp_doc.due_date = updated_doc.get('due_date')
+            exp_doc.exp_document_description = updated_doc.get(
+                'exp_document_description')
+            if updated_doc.get('due_date') is not None:
+                exp_doc.due_date = updated_doc.get('due_date')
+
             exp_doc.received_date = updated_doc.get('received_date')
-            exp_doc.exp_document_description = updated_doc.get('exp_document_description')
-            exp_doc.exp_document_status_guid = updated_doc.get('exp_document_status_guid')
+            exp_doc.exp_document_description = updated_doc.get(
+                'exp_document_description')
+
+            if updated_doc.get('exp_document_status_guid') is not None:
+                if updated_doc.get('exp_document_status_guid') != 'None':
+                    exp_doc.exp_document_status_guid = updated_doc.get(
+                        'exp_document_status_guid')
 
             exp_doc.save()
-            return {'expected_document': exp_doc.json() }
+            return {'expected_document': exp_doc.json()}
 
-        return self.create_error_payload(404, f'expected_document with guid "{exp_doc_guid}" not found' ), 404
-    
+        return self.create_error_payload(404, f'expected_document with guid "{exp_doc_guid}" not found'), 404
+
     @api.doc(params={'exp_doc_guid': 'Required: Mine number or guid. Deletes expected document.'})
     @jwt.requires_roles(["mds-mine-create"])
     def delete(self, exp_doc_guid=None):
@@ -59,5 +68,5 @@ class ExpectedDocumentResource(Resource, UserMixin, ErrorMixin):
         if exp_doc is not None:
             exp_doc.active_ind = False
             exp_doc.save()
-            return {'status':200, 'message':'expected_document deleted successfully.'}
-        return self.create_error_payload(404, f'expected_document with guid "{exp_doc_guid}" not found' ), 404
+            return {'status': 200, 'message': 'expected_document deleted successfully.'}
+        return self.create_error_payload(404, f'expected_document with guid "{exp_doc_guid}" not found'), 404
