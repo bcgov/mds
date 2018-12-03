f7d842812a43ca6a4b1aa0871327d53be11ff535
Meredith Mumby <meredith@freshworks.io>
2018-12-03 13:41:20 -0800

    removed unused imports

--- frontend/src/tests/routes/Routes.spec.js
+++ frontend/src/tests/routes/Routes.spec.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Routes from "@/routes/Routes";
-// import * as routes from "@/constants/routes";
-// import * as String from "@/constants/strings";
 
 const props = {};
 
