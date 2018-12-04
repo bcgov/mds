c448bf160c1b10a9b72bd44d2f7b42e0b35cc80b
Jason Syrotuck <jasyrotuck@gmail.com>
2018-12-04 13:15:22 -0800

    test url changed to match code change

--- python-backend/tests/parties/party_appt/resources/test_mine_party_appt_type_resource.py
+++ python-backend/tests/parties/party_appt/resources/test_mine_party_appt_type_resource.py
@@ -4,7 +4,7 @@ from tests.constants import TEST_MINE_GUID, TEST_PARTY_PER_GUID_1, TEST_PARTY_PE
 
 # GET
 def test_get_mine_party_appt_type(test_client, auth_headers):
-    get_resp = test_client.get('/parties/mines/types', headers=auth_headers['full_auth_header'])
+    get_resp = test_client.get('/parties/mines/relationship-types', headers=auth_headers['full_auth_header'])
     get_data = json.loads(get_resp.data.decode())
     assert get_resp.status_code == 200
     assert len(get_data) == 2
