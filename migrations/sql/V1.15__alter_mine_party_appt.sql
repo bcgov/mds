dcb69d20146e102680374955449d99facd00ef85
Jason Syrotuck <jasyrotuck@gmail.com>
2018-12-05 11:58:22 -0800

    added start/end dates, and post endpoint.
    start of yapf formating for python

---
+++ migrations/sql/V1.15__alter_mine_party_appt.sql
@@ -0,0 +1,2 @@
+ALTER TABLE public.mine_party_appt ADD start_date date;
+ALTER TABLE public.mine_party_appt ADD end_date date;
--- python-backend/app/api/parties/party_appt/resources/mine_party_appt_resource.py
+++ python-backend/app/api/parties/party_appt/resources/mine_party_appt_resource.py
@@ -11,19 +11,55 @@ from ....constants import PARTY_STATUS_CODE
 from app.extensions import jwt, api
 from ....utils.resources_mixins import UserMixin, ErrorMixin
 
+
 class MinePartyApptResource(Resource, UserMixin, ErrorMixin):
     parser = reqparse.RequestParser()
+    parser.add_argument('mine_guid', type=str, help='guid of the mine.')
+    parser.add_argument('party_guid', type=str, help='guid of the party.')
+    parser.add_argument(
+        'mine_party_appt_type_code',
+        type=str,
+        help='code for the type of appointment.')
+    parser.add_argument('mine_tailings_storage_facility_guid', type=str)
+    parser.add_argument(
+        'start_date', type=lambda x: datetime.strptime(x, '%Y-%m-%d'))
+    parser.add_argument(
+        'end_date', type=lambda x: datetime.strptime(x, '%Y-%m-%d'))
 
-    @api.doc(params={'mine_party_appt_guid': 'mine party appointment serial id'})
+    @api.doc(
+        params={'mine_party_appt_guid': 'mine party appointment serial id'})
     @jwt.requires_roles(["mds-mine-view"])
     def get(self, mine_party_appt_guid=None):
         if mine_party_appt_guid:
             return MinePartyAppointment.find_by_guid(mine_party_appt_guid)
-        else: 
+        else:
             mine_guid = request.args.get('mine_guid', type=str)
             party_guid = request.args.get('party_guid', type=str)
-            mine_party_appt_type_code = request.args.get('mine_party_appt_type_code',type=str)
+            mine_party_appt_type_code = request.args.get(
+                'mine_party_appt_type_code', type=str)
 
-            results = MinePartyAppointment.find_by(mine_guid=mine_guid, party_guid=party_guid, mine_party_appt_type_code=mine_party_appt_type_code)
-            
+            results = MinePartyAppointment.find_by(
+                mine_guid=mine_guid,
+                party_guid=party_guid,
+                mine_party_appt_type_code=mine_party_appt_type_code)
             return list(map(lambda x: x.json(), results))
+
+    @api.doc(
+        params={'mine_party_appt_guid': 'mine party appointment serial id'})
+    @jwt.requires_roles(["mds-mine-view"])
+    def post(self, mine_party_appt_guid=None):
+        if mine_party_appt_guid:
+            return self.create_error_payload(
+                400, 'unexpected mine party appointment guid'), 400
+        data = self.parser.parse_args()
+        new_mpa = MinePartyAppointment(
+            mine_guid=data.get('mine_guid'),
+            party_guid=data.get('party_guid'),
+            mine_party_appt_type_code=data.get('mine_party_appt_type_code'),
+            mine_tailings_storage_facility_guid=data.get(
+                'mine_tailings_storage_facility_guid'),
+            start_date=data.get('start_date'),
+            end_date=data.get('end_date'),
+            **self.get_create_update_dict())
+        new_mpa.save()
+        return new_mpa.json()
