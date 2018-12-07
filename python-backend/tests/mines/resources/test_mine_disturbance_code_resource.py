b7e04d3f951b78485de79c045a72af2ff2514d57
Nathan Calvank <nathan@freshworks.io>
2018-12-07 11:41:55 -0800

    Standardize quote type
    This was also to trigger a rebuild.

--- python-backend/tests/mines/resources/test_mine_disturbance_code_resource.py
+++ python-backend/tests/mines/resources/test_mine_disturbance_code_resource.py
@@ -11,26 +11,26 @@ def test_get_all_mine_disturbance_types(test_client, auth_headers):
     get_data = json.loads(get_resp.data.decode())
     all_options = [
         {
-            "mine_disturbance_code": "SUR",
-            "description": "Surface",
+            'mine_disturbance_code': 'SUR',
+            'description': 'Surface',
             'mine_tenure_type_codes': ['COL', 'MIN', 'PLR', 'BCL'],
             'exclusive_ind': False
         },
         {
-            "mine_disturbance_code": "UND",
-            "description": "Underground",
+            'mine_disturbance_code': 'UND',
+            'description': 'Underground',
             'mine_tenure_type_codes': ['COL', 'MIN', 'PLR'],
             'exclusive_ind': False
         },
         {
-            "mine_disturbance_code": "CWA",
-            "description": "Coal Wash",
+            'mine_disturbance_code': 'CWA',
+            'description': 'Coal Wash',
             'mine_tenure_type_codes': ['COL'],
             'exclusive_ind': True
         },
         {
-            "mine_disturbance_code": "MIL",
-            "description": "Mill",
+            'mine_disturbance_code': 'MIL',
+            'description': 'Mill',
             'mine_tenure_type_codes': ['PLR'],
             'exclusive_ind': True
         }
