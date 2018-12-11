b9ac491332f6ea22efcffc676610e2b7deee76e2
Unknown <juliansubda@gmail.com>
2018-12-11 14:47:30 -0800

    MDS-163: Removed comment

--- migrations/sql/V1.21__Alter_Mine_Location_Add_Geoms.sql
+++ migrations/sql/V1.21__Alter_Mine_Location_Add_Geoms.sql
@@ -2,4 +2,3 @@
 ALTER TABLE mine_location ADD COLUMN geom GEOMETRY(POINT, 3005);
 UPDATE mine_location SET geom = ST_SetSRID(ST_MakePoint(longitude, latitude),3005) WHERE longitude IS NOT NULL AND latitude IS NOT NULL;
 CREATE INDEX mine_location_geom_idx ON mine_location USING GIST(geom);
---VACUUM ANALYZE mine_location;
