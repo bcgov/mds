63ee744fec6968779a92e50b3dde29634ef7461b
Meredith Mumby <meredith@freshworks.io>
2018-12-12 13:03:25 -0800

    refactored component from statefull to a stateless function

--- frontend/src/components/dashboard/MineList.js
+++ frontend/src/components/dashboard/MineList.js
@@ -1,11 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { Table } from "antd";
-import { ELLIPSE, RED_ELLIPSE } from "@/constants/assets";
 import * as router from "@/constants/routes";
 import * as String from "@/constants/strings";
 import NullScreen from "@/components/common/NullScreen";
+
+/**
+ * @class MineList - paginated list of mines
+ */
+
+const propTypes = {
+  mines: PropTypes.object.isRequired,
+  mineIds: PropTypes.array.isRequired,
+  mineRegionHash: PropTypes.object.isRequired,
+};
 
 const columns = [
   {
@@ -58,53 +67,36 @@ const columns = [
   },
 ];
 
-/**
- * @class MineList - paginated list of this.props.mines
- */
+const renderRowData = (mines, mineIds, mineRegionHash) =>
+  mineIds.map((id) => ({
+    key: id,
+    emptyField: String.EMPTY_FIELD,
+    mineName: mines[id].mine_detail[0] ? mines[id].mine_detail[0].mine_name : String.EMPTY_FIELD,
+    mineNo: mines[id].mine_detail[0] ? mines[id].mine_detail[0].mine_no : String.EMPTY_FIELD,
+    operationalStatus: mines[id].mine_status[0]
+      ? mines[id].mine_status[0].status_labels[0]
+      : String.EMPTY_FIELD,
+    permit: mines[id].mine_permit[0] ? mines[id].mine_permit : null,
+    region: mines[id].mine_detail[0].region_code
+      ? mineRegionHash[mines[id].mine_detail[0].region_code]
+      : String.EMPTY_FIELD,
+    commodity: String.EMPTY_FIELD,
+    tenure: String.EMPTY_FIELD,
+    TSF: mines[id].mine_tailings_storage_facility
+      ? mines[id].mine_tailings_storage_facility.length
+      : String.EMPTY_FIELD,
+  }));
 
-const propTypes = {
-  mines: PropTypes.object.isRequired,
-  mineIds: PropTypes.array.isRequired,
-  mineRegionHash: PropTypes.object.isRequired,
-};
-
-class MineList extends Component {
-  render() {
-    const data = this.props.mineIds.map((id) => ({
-      key: id,
-      emptyField: String.EMPTY_FIELD,
-      mineName: this.props.mines[id].mine_detail[0]
-        ? this.props.mines[id].mine_detail[0].mine_name
-        : String.EMPTY_FIELD,
-      mineNo: this.props.mines[id].mine_detail[0]
-        ? this.props.mines[id].mine_detail[0].mine_no
-        : String.EMPTY_FIELD,
-      operationalStatus: this.props.mines[id].mine_status[0]
-        ? this.props.mines[id].mine_status[0].status_labels[0]
-        : String.EMPTY_FIELD,
-      permit: this.props.mines[id].mine_permit[0] ? this.props.mines[id].mine_permit : null,
-      region: this.props.mines[id].mine_detail[0].region_code
-        ? this.props.mineRegionHash[this.props.mines[id].mine_detail[0].region_code]
-        : String.EMPTY_FIELD,
-      commodity: String.EMPTY_FIELD,
-      tenure: String.EMPTY_FIELD,
-      TSF: this.props.mines[id].mine_tailings_storage_facility
-        ? this.props.mines[id].mine_tailings_storage_facility.length
-        : String.EMPTY_FIELD,
-    }));
-
-    return (
-      <Table
-        align="center"
-        pagination={false}
-        columns={columns}
-        dataSource={data}
-        scroll={{ x: 1500 }}
-        locale={{ emptyText: <NullScreen type="no-results" /> }}
-      />
-    );
-  }
-}
+export const MineList = (props) => (
+  <Table
+    align="center"
+    pagination={false}
+    columns={columns}
+    dataSource={renderRowData(props.mines, props.mineIds, props.mineRegionHash)}
+    scroll={{ x: 1500 }}
+    locale={{ emptyText: <NullScreen type="no-results" /> }}
+  />
+);
 
 MineList.propTypes = propTypes;
 
