58ca89185dc0a1b7b5b6f6eab5f0091304b5359f
Nathan Calvank <nathan@freshworks.io>
2018-12-19 14:20:25 -0800

    Remove redundant PUT method
    PUT is the combination of a DELETE and a POST. This method does not
    updated in place, as would likely be expected. In order to make MineType
    consistent with Disturbance and Commodity, the redundant PUT method is
    being removed.

--- python-backend/app/api/mines/mine/resources/mine_type.py
+++ python-backend/app/api/mines/mine/resources/mine_type.py
@@ -41,48 +41,6 @@ class MineTypeResource(Resource, UserMixin, ErrorMixin):
 
     @api.expect(parser)
     @jwt.requires_roles(["mds-mine-create"])
-    def put(self, mine_type_guid=None):
-        data = self.parser.parse_args()
-
-        mine_guid = data['mine_guid']
-        mine_tenure_type_code = data['mine_tenure_type_code']
-
-        if not mine_type_guid:
-            self.raise_error(400, 'Error: Missing mine_type_guid.')
-
-        if not mine_guid:
-            self.raise_error(400, 'Error: Missing mine_guid.')
-
-        if not mine_tenure_type_code:
-            self.raise_error(400, 'Error: Missing mine_tenure_type_code.')
-
-        mine_type = MineType.find_by_guid(mine_type_guid)
-        if not mine_type:
-            return self.create_error_payload(404, 'Mine Type not found'), 404
-
-        if mine_type.mine_tenure_type_code == mine_tenure_type_code:
-            return mine_type.json()
-        else:
-            try:
-                MineType.expire_record(mine_type)
-            except exc.IntegrityError as e:
-                self.raise_error(400, 'Error: Unable to update mine_type.')
-
-            try:
-                new_mine_type = MineType.create_mine_type(
-                    mine_guid,
-                    mine_tenure_type_code,
-                    self.get_create_update_dict(),
-                    save=False
-                )
-                new_mine_type.save()
-            except exc.IntegrityError as e:
-                self.raise_error(400, 'Error: Unable to update mine_type.')
-
-            return new_mine_type.json()
-
-    @api.expect(parser)
-    @jwt.requires_roles(["mds-mine-create"])
     def delete(self, mine_type_guid=None):
         data = self.parser.parse_args()
 
