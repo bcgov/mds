94df46679ad0b56c0c57e7823e35c10e7043b70d
Jason Syrotuck <jasyrotuck@gmail.com>
2018-12-20 11:00:14 -0800

    Transaction wrap for first tsf_creation and exp documents
    yapf formatting (100)
    url helper for document service

--- python-backend/app/api/mines/tailings/resources/tailings.py
+++ python-backend/app/api/mines/tailings/resources/tailings.py
@@ -6,16 +6,17 @@ from flask import request, current_app, url_for
 from flask_restplus import Resource, reqparse
 from ..models.tailings import MineTailingsStorageFacility
 
-from app.extensions import jwt, api
+from app.extensions import jwt, api, db
 from ....utils.resources_mixins import UserMixin, ErrorMixin
+from ....utils.url import get_documents_svc_url
+from ....documents.namespace.documents import api as doc_api
 
 
 class MineTailingsStorageFacilityResource(Resource, UserMixin, ErrorMixin):
     parser = reqparse.RequestParser()
+    parser.add_argument('mine_guid', type=str, help='mine to create a new tsf on')
     parser.add_argument(
-        'mine_guid', type=str, help='mine to create a new tsf on')
-    parser.add_argument(
-        'tsf_name', type=str, help='Name of the tailings storage facility.')
+        'tsf_name', type=str, trim=True, help='Name of the tailings storage facility.')
 
     @api.doc(
         params={
@@ -25,56 +26,43 @@ class MineTailingsStorageFacilityResource(Resource, UserMixin, ErrorMixin):
     # @jwt.requires_roles(["mds-mine-view"])
     def get(self, mine_tailings_storage_facility_guid=None):
         if mine_tailings_storage_facility_guid:
-            tsf = MineTailingsStorageFacility.find_by_tsf_guid(
-                mine_tailings_storage_facility_guid)
+            tsf = MineTailingsStorageFacility.find_by_tsf_guid(mine_tailings_storage_facility_guid)
             if not tsf:
-                return self.create_error_payload(
-                    404, 'mine_tailings_storage_facility not found')
+                return self.create_error_payload(404, 'mine_tailings_storage_facility not found')
             return tsf.json()
         else:
             mine_guid = request.args.get('mine_guid', type=str)
-            mine_tsf_list = MineTailingsStorageFacility.find_by_mine_guid(
-                mine_guid)
-            if mine_tsf_list:
-                return {
-                    'mine_storage_tailings_facilities':
-                    list(map(lambda x: x.json(), mine_tsf_list))
-                }
-            else:
-                return self.create_error_payload(
-                    404, 'Mine_guid or tsf_guid must be provided')
+            mine_tsf_list = MineTailingsStorageFacility.find_by_mine_guid(mine_guid)
+            if mine_tsf_list is None:
+                return self.raise_error(404, 'Mine_guid or tsf_guid must be provided')
+            return {
+                'mine_storage_tailings_facilities': list(map(lambda x: x.json(), mine_tsf_list))
+            }
 
     @api.doc(params={'mine_guid': 'mine_guid that is to get a new TSF'})
-    @jwt.requires_roles(["mds-mine-view"])
     def post(self, mine_tailings_storage_facility_guid=None):
         if not mine_tailings_storage_facility_guid:
             data = self.parser.parse_args()
             mine_guid = data['mine_guid']
             # see if this would be the first TSF
-            mine_tsf_list = MineTailingsStorageFacility.find_by_mine_guid(
-                mine_guid)
+            mine_tsf_list = MineTailingsStorageFacility.find_by_mine_guid(mine_guid)
             is_mine_first_tsf = len(mine_tsf_list) == 0
             mine_tsf = MineTailingsStorageFacility(
                 mine_guid=mine_guid,
                 mine_tailings_storage_facility_name=data['tsf_name'],
                 **self.get_create_update_dict())
-            mine_tsf.save()
-
+            db.session.add(mine_tsf)
             if is_mine_first_tsf:
                 try:
-                    documents_url = current_app.config['DOCUMENT_MS_URL'] + \
-                        current_app.config['BASE_PATH'] + '/documents'
+                    req_documents_url = get_documents_svc_url('/required?category=MINE_TAILINGS')
+                    get_tsf_docs_resp = requests.get(req_documents_url, headers=request.headers)
 
-                    get_tsf_docs_resp = requests.get(
-                        documents_url + '/required?category=MINE_TAILINGS',
-                        headers=request.headers)
                     if get_tsf_docs_resp.status_code != 200:
-                        return self.create_error_payload(
-                            500, 'get_tsf_req_docs returned error' + str(
-                                get_tsf_docs_resp.status_code)), 500
+                        self.raise_error(
+                            500,
+                            'get_tsf_req_docs returned error' + str(get_tsf_docs_resp.status_code))
 
-                    tsf_required_documents = get_tsf_docs_resp.json(
-                    )['required_documents']
+                    tsf_required_documents = get_tsf_docs_resp.json()['required_documents']
                     new_expected_documents = []
                     for tsf_req_doc in tsf_required_documents:
                         new_expected_documents.append({
@@ -91,26 +79,22 @@ class MineTailingsStorageFacilityResource(Resource, UserMixin, ErrorMixin):
                             'document_due_date_period_months':
                             tsf_req_doc['req_document_due_date_period_months']
                         })
-
+                    #raise Exception(str(new_expected_documents) + str(request.headers))
                     doc_assignment_response = requests.post(
-                        documents_url + '/expected/mines/' + str(mine_guid),
+                        get_documents_svc_url('/expected/mines/' + str(mine_guid)),
                         headers=request.headers,
                         json={'documents': new_expected_documents})
                     if doc_assignment_response.status_code != 200:
-                        return self.create_error_payload(
-                            500, 'exp_doc_assignment_post returned error' +
-                            str(doc_assignment_response.status_code)), 500
-
-                except Exception as e:
-                    return self.create_error_payload(
-                        500, 'Error: {}'.format(e)), 500
+                        self.raise_error(500, "Error creating tsf expected documents")
+                except BaseException as e:
+                    db.session.rollback()
+                    self.raise_error(500, str(e + ", tsf not created"))
+            db.session.commit()
             return {
                 'mine_tailings_storage_facilities':
                 list(
-                    map(
-                        lambda x: x.json(),
-                        MineTailingsStorageFacility.find_by_mine_guid(
-                            mine_guid)))
+                    map(lambda x: x.json(),
+                        MineTailingsStorageFacility.find_by_mine_guid(mine_guid)))
             }
         else:
             return self.create_error_payload(404, 'unexpected tsf_guid')
