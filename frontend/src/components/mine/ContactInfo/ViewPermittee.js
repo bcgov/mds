0c1c0c008d86d155d641a202c49d48ca4ef6640a
Nathan Calvank <nathan@freshworks.io>
2018-12-21 15:29:32 -0800

    Remove duplicate permit_no from contact tab
    This wasn't mentioned in any discussions, but this is consistent with
    the requested changes for the Summary tab and Mine List view.

--- frontend/src/components/mine/ContactInfo/ViewPermittee.js
+++ frontend/src/components/mine/ContactInfo/ViewPermittee.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Card } from "antd";
 import { Link } from "react-router-dom";
+import { uniqBy, some } from "lodash";
 import * as router from "@/constants/routes";
 import ConditionalButton from "@/components/common/ConditionalButton";
 import { modalConfig } from "@/components/modalContent/config";
@@ -59,12 +60,16 @@ export class ViewPermittee extends Component {
   }
 
   render() {
-    const { permittees, permitteeIds, mine } = this.props;
+    const uniqPermitsPerParty = (permits, guid) =>
+      uniqBy(
+        permits.filter(({ permittee }) => some(permittee, ({ party_guid }) => party_guid === guid)),
+        "permit_no"
+      );
     return (
       <div>
         <Card>
           <table>
-            {permitteeIds.map((id) => (
+            {this.props.permitteeIds.map((id) => (
               <tbody key={id}>
                 <tr>
                   <th scope="col">
@@ -76,10 +81,10 @@ export class ViewPermittee extends Component {
                 </tr>
                 <tr>
                   <td data-label="Permittee">
-                    <p className="p-large">{permittees[id].party.name}</p>
+                    <p className="p-large">{this.props.permittees[id].party.name}</p>
                   </td>
                   <td data-label="Permittee Since">
-                    <p className="p-large">{permittees[id].effective_date}</p>
+                    <p className="p-large">{this.props.permittees[id].effective_date}</p>
                   </td>
                 </tr>
                 <tr>
@@ -93,23 +98,23 @@ export class ViewPermittee extends Component {
                 <tr>
                   <td data-label="Permit No.">
                     {this.props.mine.mine_permit[0]
-                      ? this.props.mine.mine_permit
-                          .filter(({ permittee }) => permittee[0].party_guid === id)
-                          .map(({ permit_no }) => (
+                      ? uniqPermitsPerParty(this.props.mine.mine_permit, id).map(
+                          ({ permit_no }) => (
                             <Link
                               to={router.MINE_SUMMARY.dynamicRoute(this.props.mine.guid, "permit")}
                               key={permit_no}
                             >
                               <p className="p-large">{permit_no}</p>
                             </Link>
-                          ))
+                          )
+                        )
                       : String.EMPTY_FIELD}
                   </td>
                   <td data-label="Phone Number (Ext)">
                     <p className="p-large">
-                      {permittees[id].party.phone_no} (
-                      {permittees[id].party.phone_ext
-                        ? permittees[id].party.phone_ext
+                      {this.props.permittees[id].party.phone_no} (
+                      {this.props.permittees[id].party.phone_ext
+                        ? this.props.permittees[id].party.phone_ext
                         : String.EMPTY_FIELD}
                       )
                     </p>
@@ -122,7 +127,7 @@ export class ViewPermittee extends Component {
                 </tr>
                 <tr>
                   <td data-label="Email">
-                    <p className="p-large">{permittees[id].party.email}</p>
+                    <p className="p-large">{this.props.permittees[id].party.email}</p>
                   </td>
                 </tr>
               </tbody>
@@ -134,7 +139,7 @@ export class ViewPermittee extends Component {
                 this.openModal(
                   event,
                   this.handlePermitteeSubmit,
-                  mine.mine_permit,
+                  this.props.mine.mine_permit,
                   this.props.handleChange,
                   this.props.handlePartySubmit,
                   ModalContent.UPDATE_PERMITTEE
