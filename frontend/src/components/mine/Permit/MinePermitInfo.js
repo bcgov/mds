7b2a4e1df6fac26e9e3bf7894808d1a067d6d7f4
Nathan Calvank <nathan@freshworks.io>
2018-12-21 11:10:49 -0800

    Add collapsible permit amendment history

--- frontend/src/components/mine/Permit/MinePermitInfo.js
+++ frontend/src/components/mine/Permit/MinePermitInfo.js
@@ -14,37 +14,52 @@ const propTypes = {
 const columns = [
   {
     title: "Permit No.",
-    width: 150,
+    // width: 150,
     dataIndex: "permitNo",
-    fixed: "left",
-  },
-  {
-    title: "Permittee",
-    width: 100,
-    dataIndex: "permittee",
+    // fixed: "left",
+    key: "parentPermitNo",
   },
   {
     title: "Status",
     dataIndex: "status",
-    width: 100,
+    key: "status",
+    // width: 100,
   },
   {
-    title: "First Issued",
-    dataIndex: "firstIssued",
-    width: 100,
-  },
-  {
-    title: "Last Amended",
-    dataIndex: "lastAmended",
-    width: 100,
+    title: "Permittee",
+    // width: 100,
+    dataIndex: "permittee",
+    key: "parentPermittee",
   },
   {
     title: "Authorization End Date",
     dataIndex: "authorizationEndDate",
-    width: 100,
+    key: "authorizationEndDate",
+    // width: 100,
+  },
+
+  {
+    title: "First Issued",
+    dataIndex: "firstIssued",
+    key: "firstIssued",
+    // width: 100,
+  },
+  {
+    title: "Last Amended",
+    dataIndex: "lastAmended",
+    key: "lastAmended",
+    // width: 100,
   },
 ];
 
+const childColumns = [
+  { title: "Permit No.", dataIndex: "permitNo", key: "permitNo" },
+  { title: "Date Issued", dataIndex: "issueDate", key: "issueDate" },
+  { title: "Permittee", dataIndex: "permittee", key: "permittee" },
+  { title: "Description", dataIndex: "description", key: "description" },
+];
+
+// Data Manipulation
 const groupPermits = (permits) =>
   permits.reduce((acc, permit) => {
     acc[permit.permit_no] = acc[permit.permit_no] || [];
@@ -62,23 +77,51 @@ const transformRowData = (permits) => {
     firstIssued: first.issue_date || String.EMPTY_FIELD,
     permittee: latest.permittee[0] ? latest.permittee[0].party.party_name : String.EMPTY_FIELD,
     authorizationEndDate: latest.expiry_date ? latest.expiry_date : String.EMPTY_FIELD,
+    amendmentHistory: permits.slice(1),
     status: String.EMPTY_FIELD,
   };
 };
 
-export const MinePermitInfo = (props) => {
-  const groupedPermits = Object.values(groupPermits(props.mine.mine_permit));
-  const rowData = groupedPermits.map(transformRowData);
+const transformChildRowData = (x) => ({
+  key: x.permit_guid,
+  permitNo: x.permit_no,
+  issueDate: x.issue_date,
+  permittee: x.permittee[0] ? x.permittee[0].party.party_name : String.EMPTY_FIELD,
+  description: String.EMPTY_FIELD,
+});
+
+// Components
+const amendmentHistory = (record) => {
+  const childRowData = record.amendmentHistory.map(transformChildRowData);
+  return (
+    <Table
+      align="center"
+      pagination={false}
+      columns={childColumns}
+      dataSource={childRowData}
+      // scroll={{ x: 1500 }}
+    />
+  );
+};
+
+const permitTable = (permits) => {
+  const rowData = permits.map(transformRowData);
   return (
     <Table
       align="center"
       pagination={false}
       columns={columns}
       dataSource={rowData}
-      scroll={{ x: 1500 }}
+      expandedRowRender={amendmentHistory}
+      // scroll={{ x: 1500 }}
       locale={{ emptyText: <NullScreen type="permit" /> }}
     />
   );
+};
+
+export const MinePermitInfo = (props) => {
+  const groupedPermits = Object.values(groupPermits(props.mine.mine_permit));
+  return permitTable(groupedPermits);
 };
 
 MinePermitInfo.propTypes = propTypes;
