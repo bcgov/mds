e1760045b0e9766d3a1369794c613db25fc101a9
Nathan Calvank <nathan@freshworks.io>
2018-12-21 16:38:40 -0800

    Clean up /customPropTypes

--- frontend/src/customPropTypes/mines.js
+++ frontend/src/customPropTypes/mines.js
@@ -1,6 +1,8 @@
 import { PropTypes, shape, arrayOf } from "prop-types";
 import { minePermit } from "@/customPropTypes/permits";
 
+// This file is anticipated to have multiple exports
+// eslint-disable-next-line import/prefer-default-export
 export const mine = shape({
   guid: PropTypes.string.isRequired,
   mine_permit: arrayOf(minePermit),
