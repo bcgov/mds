c468a97094d1a9e0c6b9b8f279cf53459f690de2
Nathan Calvank <nathan@freshworks.io>
2018-12-21 17:26:48 -0800

    Clean up tests

--- frontend/src/tests/components/maps/LocationPin.spec.js
+++ frontend/src/tests/components/maps/LocationPin.spec.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { LocationPin } from '@/components/maps/LocationPin';
-import * as MOCK from '@/tests/mocks/dataMocks';
+import React from "react";
+import { shallow } from "enzyme";
+import { LocationPin } from "@/components/maps/LocationPin";
+import * as MOCK from "@/tests/mocks/dataMocks";
 
 const props = {};
 
 const setupProps = () => {
   props.center = MOCK.COORDINATES;
-  props.map = {},
-  props.view = {}
+  props.map = {};
+  props.view = {};
 };
 
 beforeEach(() => {
   setupProps();
 });
 
-describe('LocationPin', () => {
-  it('renders properly', () => {
+describe("LocationPin", () => {
+  it("renders properly", () => {
     const component = shallow(<LocationPin {...props} />);
     expect(component).toMatchSnapshot();
   });
