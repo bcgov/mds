8a3f4a9d3891ccf39f777281facb0c0cb189c4bb
Gyanesh Mishra <gyan@freshworks.io>
2018-12-22 23:23:04 -0800

    update filename direction

--- nginx/nginx-start/env_var_substitution.sh
+++ nginx/nginx-start/env_var_substitution.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-# for file in ${NGINX_CONFIGURATION_PATH}/*
-# do
-#   envsubst < "$file" > "$file"
-# done
+for file in ${NGINX_CONFIGURATION_PATH}/*
+do
+  envsubst < $file > /tmp/$file
+  mv /tmp/$file $file
+done
 
-# for file in ${NGINX_DEFAULT_CONF_PATH}/*
-# do
-#   envsubst < "$file" > "$file"
-# done
+for file in ${NGINX_DEFAULT_CONF_PATH}/*
+do
+  envsubst < $file > /tmp/$file
+  mv /tmp/$file $file
+done
 
-# envsubst < "${NGINX_CONF_PATH}" > "${NGINX_CONF_PATH}"
+envsubst < ${NGINX_CONF_PATH} > /tmp/${NGINX_CONF_PATH}
+mv /tmp/${NGINX_CONF_PATH} ${NGINX_CONF_PATH}
+
+rm -rf /tmp/*
