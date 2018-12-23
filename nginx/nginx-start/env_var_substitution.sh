2b0079975b6adafeafc46679560fd07865f0c6f0
Gyanesh Mishra <gyan@freshworks.io>
2018-12-23 00:51:31 -0800

    update env vars in bc

--- nginx/nginx-start/env_var_substitution.sh
+++ nginx/nginx-start/env_var_substitution.sh
@@ -1,17 +1,20 @@
-# #!/bin/bash
+#!/bin/bash
 
-# for file in ${NGINX_CONFIGURATION_PATH}/*
-# do
-#   filename=$(basename $file)
-#   envsubst < $file > /tmp/$filename
-#   mv /tmp/$filename $file
-# done
+for file in ${NGINX_CONFIGURATION_PATH}/*
+do
+  filename=$(basename $file)
+  envsubst < $file > /tmp/$filename
+  mv /tmp/$filename $file
+done
 
-# for file in ${NGINX_DEFAULT_CONF_PATH}/*
-# do
-#   filename=$(basename $file)
-#   envsubst < $file > /tmp/$filename
-#   mv /tmp/$filename $file
-# done
+for file in ${NGINX_DEFAULT_CONF_PATH}/*
+do
+  filename=$(basename $file)
+  envsubst < $file > /tmp/$filename
+  mv /tmp/$filename $file
+done
 
-# rm -rf /tmp/*
+envsubst < ${NGINX_CONF_PATH} > /tmp/nginx.conf
+mv /tmp/nginx.conf ${NGINX_CONF_PATH}
+
+rm -rf /tmp/*
