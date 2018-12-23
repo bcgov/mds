180dfe8a539b6df5a56f487108d645eb032f3092
Gyanesh Mishra <gyan@freshworks.io>
2018-12-22 22:25:09 -0800

    add environment variable support

--- nginx/nginx-start/env_var_substitution.sh
+++ nginx/nginx-start/env_var_substitution.sh
@@ -2,12 +2,12 @@
 
 for file in /etc/nginx/conf.d/*
 do
-  envsubst < /etc/nginx/conf.d/$file > /etc/nginx/conf.d/$file
+  envsubst < $file > $file
 done
 
 for file in /etc/nginx/nginx.default.d/*
 do
-  envsubst < /etc/nginx/nginx.default.d/$file > /etc/nginx/nginx.default.d/$file
+  envsubst < $file > $file
 done
 
 envsubst < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf
