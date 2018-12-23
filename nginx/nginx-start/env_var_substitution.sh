b31b2d4e12416cf5c33fbd9f2e2f59349aa9d04b
Gyanesh Mishra <gyan@freshworks.io>
2018-12-23 01:04:59 -0800

    try global permissions

--- nginx/nginx-start/env_var_substitution.sh
+++ nginx/nginx-start/env_var_substitution.sh
@@ -4,6 +4,7 @@ for file in ${NGINX_CONFIGURATION_PATH}/*
 do
   filename=$(basename $file)
   envsubst < $file > /tmp/$filename
+  chmod a+rw /tmp/$filename
   mv /tmp/$filename $file
 done
 
@@ -11,10 +12,12 @@ for file in ${NGINX_DEFAULT_CONF_PATH}/*
 do
   filename=$(basename $file)
   envsubst < $file > /tmp/$filename
+  chmod a+rw /tmp/$filename
   mv /tmp/$filename $file
 done
 
 envsubst < ${NGINX_CONF_PATH} > /tmp/nginx.conf
+chmod a+rw /tmp/nginx.conf
 mv /tmp/nginx.conf ${NGINX_CONF_PATH}
 
 rm -rf /tmp/*
