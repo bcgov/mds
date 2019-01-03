1ae5c264fb4d086a733cc24da1069589373bb3da
Gyanesh Mishra <gyan@freshworks.io>
2019-01-03 09:01:32 -0800

    only nginx does compression

--- frontend/server.js
+++ frontend/server.js
@@ -24,24 +24,6 @@ const staticServe = express.static(`${__dirname}/build`, {
   maxAge: "1y",
 });
 
-const serveGzipped = (contentType) => (req, res, next) => {
-  const acceptedEncodings = req.acceptsEncodings();
-  if (acceptedEncodings.indexOf("gzip") === -1 || !fs.existsSync(`./build/${req.url}.gz`)) {
-    next();
-    return;
-  }
-
-  // update request's url
-  req.url = `${req.url}.gz`;
-
-  // set correct headers
-  res.set("Content-Encoding", "gzip");
-  res.set("Content-Type", contentType);
-
-  // let express.static take care of the updated request
-  next();
-};
-
 app.get(`${BASE_PATH}/env`, function(req, res) {
   res.json({
     backend: "mds-python-backend",
@@ -56,9 +38,6 @@ app.get(`${BASE_PATH}/env`, function(req, res) {
     keycloak_role_view: process.env.KEYCLOAK_ROLE_VIEW,
   });
 });
-
-app.get(`${BASE_PATH}/*.js`, serveGzipped("text/javascript"));
-app.get(`${BASE_PATH}/*.css`, serveGzipped("text/css"));
 
 app.use(`${BASE_PATH}/`, staticServe);
 app.use(`${BASE_PATH}*`, staticServe);
