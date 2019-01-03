b0757dc4513215bdfa5185e2aff9bc6364241a74
James Hollinger <jameshollinger.ee@gmail.com>
2019-01-03 10:07:37 -0800

    Updated decorators, added function property required_roles

--- python-backend/app/api/utils/access_decorators.py
+++ python-backend/app/api/utils/access_decorators.py
@@ -2,13 +2,18 @@ from functools import wraps
 from app.extensions import jwt
 
 def requires_role_view(func):
-    @wraps(func)
-    def wrapper(*args, **kwds):
-        return jwt.requires_roles(["mds-mine-view"])(func)(*args, **kwds) 
-    return wrapper
+    return role_decorator(func, "mds-mine-view")
 
 def requires_role_create(func):
+     return role_decorator(func, "mds-mine-create")
+
+def role_decorator(func, role):
     @wraps(func)
     def wrapper(*args, **kwds):
-        return jwt.requires_roles(["mds-mine-create"])(func)(*args, **kwds) 
+        return jwt.requires_roles([role])(func)(*args, **kwds)
+
+    wrapper.required_roles = [role]
+    if func.required_roles:
+        wrapper.required_roles.extend(func.required_roles)
+
     return wrapper
