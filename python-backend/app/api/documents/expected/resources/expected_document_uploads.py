6b6b05bf38c80d78feb2ba420eb118a47e150d30
James Hollinger <jameshollinger.ee@gmail.com>
2019-01-04 11:06:28 -0800

    testing

--- python-backend/app/api/documents/expected/resources/expected_document_uploads.py
+++ python-backend/app/api/documents/expected/resources/expected_document_uploads.py
@@ -76,7 +76,7 @@ class ExpectedDocumentUploadResource(Resource, UserMixin, ErrorMixin):
 
         response = requests.post(
             url=document_manager_URL, data=args, files=files, headers=headers)
-        return self.create_error_payload(666, response.json())
+        return self.create_error_payload(666, str(response))
         json_response = response.json()
 
         errors = json_response['errors']
