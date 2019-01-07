56936ce06257670e81f304190c603ba13f67e618
James Hollinger <jameshollinger.ee@gmail.com>
2019-01-07 12:26:18 -0800

    New unit test file

---
+++ python-backend/tests/auth/test_auth_decorators.py
@@ -0,0 +1,46 @@
+from flask_restplus import Resource, Namespace
+from app.api.utils.access_decorators import requires_role_mine_view, requires_role_mine_create
+
+api = Namespace('test')
+@api.route('/')
+class TestResource(Resource):
+    @requires_role_mine_view
+    def get(self):
+        return "Example view method"
+
+    @requires_role_mine_create
+    def post(self):
+        return "Example create method"
+
+
+
+# Test view role
+def test_get_no_auth(test_client):
+    resp = test_client.get('/test', headers={})
+    assert resp.status_code == 401
+
+
+def test_get_view_only(test_client, auth_headers):
+    resp = test_client.get('/test', headers=auth_headers['view_only_auth_header'])
+    assert resp.status_code == 200
+
+
+def test_get_full_auth(test_client, auth_headers):
+    resp = test_client.get('/test', headers=auth_headers['full_auth_header'])
+    assert resp.status_code == 200
+
+
+# Test create role
+def test_post_no_auth(test_client):
+    resp = test_client.post('/test', headers={})
+    assert resp.status_code == 401
+
+
+def test_post_view_only(test_client, auth_headers):
+    resp = test_client.post('/test', headers=auth_headers['view_only_auth_header'])
+    assert resp.status_code == 401
+
+
+def test_post_full_auth(test_client, auth_headers):
+    resp = test_client.post('/test', headers=auth_headers['full_auth_header'])
+    assert resp.status_code == 200
