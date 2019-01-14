ffa1bef198fa8e7dc65d107fe0b6121dbb0cb720
Jason Syrotuck <jasyrotuck@gmail.com>
2019-01-14 14:25:44 -0800

    better comment

--- migrations/sql/V1.35__add_user_table.sql
+++ migrations/sql/V1.35__add_user_table.sql
@@ -9,7 +9,7 @@ CREATE TABLE minespace_user (
 );
 
 
-COMMENT ON TABLE minespace_user IS 'A list of public mine proponents that can log into minespace and update mds information on mines listed in minespace_user_mds_mine_access';
+COMMENT ON TABLE minespace_user IS 'A list of public mine proponents email addresses that can log into minespace and update mds information on mines listed in minespace_user_mds_mine_access';
 
 CREATE TABLE minespace_user_mds_mine_access(
     user_id integer NOT NULL,
