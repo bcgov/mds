b1f04356073b74bbc42f8edab991f9ab7d81c6b4
Jason Syrotuck <jasyrotuck@gmail.com>
2019-01-16 11:23:45 -0800

    this doesnt work??

--- frontend/src/components/admin/NewMinespaceUser.js
+++ frontend/src/components/admin/NewMinespaceUser.js
@@ -1,14 +1,56 @@
-import React from "react";
-import AddMinespaceUser from "@/components/forms/AddMinespaceUser";
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import AddMinespaceUser from "@/components/Forms/AddMinespaceUser";
+import { createDropDownList } from "@/utils/helpers";
+import PropTypes from "prop-types";
+import CustomPropTypes from "@/customPropTypes";
+import getMineNameList from "@/reducers/mineReducer";
+import { updateExpectedDocument } from "@/actionCreators/mineActionCreator";
 /**
  * @class AdminDashboard houses everything related to admin tasks, this is a permission-based route.
  */
 
-export const NewMinespaceUser = () => (
-  <div>
-    <h3>Add BCEID User</h3>
-    <AddMinespaceUser />
-  </div>
-);
+const propTypes = {
+  updateExpectedDocument: PropTypes.func.isRequired,
+  mines: PropTypes.arrayOf(CustomPropTypes.mine),
+};
 
-export default NewMinespaceUser;
+const defaultProps = {
+  mines: [],
+};
+
+export class NewMinespaceUser extends Component {
+  componentDidMount() {
+    // this.props.fetchMineRecords();
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Add BCEID User</h3>
+        <AddMinespaceUser />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  mines: getMineNameList(state),
+});
+
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators(
+    {
+      updateExpectedDocument,
+    },
+    dispatch
+  );
+
+NewMinespaceUser.propTypes = propTypes;
+NewMinespaceUser.defaultProps = defaultProps;
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(NewMinespaceUser);
