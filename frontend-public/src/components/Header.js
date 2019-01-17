198c4e204903764b472781171175893f0e623885
justinmacaulay <justinkmacaulay@gmail.com>
2019-01-17 09:05:43 -0800

    finished styling and cleaned up scss.

--- frontend-public/src/components/Header.js
+++ frontend-public/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "antd";
+import { Button, Row, Col } from "antd";
 import { Link } from "react-router-dom";
 
 import { BC_GOV } from "@/constants/assets";
@@ -7,19 +7,25 @@ import { AuthenticationGuard } from "../HOC/AuthenticationGuard";
 import * as Route from "@/constants/routes";
 
 export const Header = () => (
-  <div className="header-wrapper">
-    <div className="header">
-      <img alt="bc_gov_logo" src={BC_GOV} />
-      <h1 className="header-h1">
-        <span className="header-title-bc">BC</span>
-        <span className="header-title"> MineSpace</span>
-      </h1>
-      <Link to={Route.DASHBOARD.route}>
-        <Button type="secondary" className="login-btn">
-          Log in
-        </Button>
-      </Link>
-    </div>
+  <div className="header">
+    <Row>
+      <Col xs={24} sm={16} md={16} lg={16}>
+        <img alt="bc_gov_logo" src={BC_GOV} />
+        <div className="inline align-bot">
+          <h1>
+            <span className="header-title-bc">BC</span>
+            <span className="header-title"> MineSpace</span>
+          </h1>
+        </div>
+      </Col>
+      <Col xs={24} sm={8} md={8} lg={8}>
+        <Link to={Route.DASHBOARD.route}>
+          <Button type="secondary" className="login-btn">
+            Log in
+          </Button>
+        </Link>
+      </Col>
+    </Row>
   </div>
 );
 
