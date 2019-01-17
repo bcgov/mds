b45abdf2359f931db4e2d190b9347f66f63c88a3
Ryan Rondeau <nayr974@users.noreply.github.com>
2019-01-17 11:26:32 -0800

    re-add commented out role

--- python-backend/app/api/mines/status/resources/status.py
+++ python-backend/app/api/mines/status/resources/status.py
@@ -8,10 +8,11 @@ from ....constants import MINE_STATUS_OPTIONS
 
 class MineStatusResource(Resource, UserMixin, ErrorMixin):
     @api.doc(params={'mine_status_guid': 'Mine status guid.'})
-    #@requires_role_mine_view
+    @requires_role_mine_view
     def get(self, mine_status_guid=None):
         if mine_status_guid:
             mine_status = MineStatus.find_by_mine_status_guid(mine_status_guid)
-            return mine_status.json() if mine_status else self.create_error_payload(404, 'Mine Status not found'), 404
+            return mine_status.json() if mine_status else self.create_error_payload(
+                404, 'Mine Status not found'), 404
         else:
             return {'options': MINE_STATUS_OPTIONS}
