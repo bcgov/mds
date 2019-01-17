5a6e6b8f320d3a084fa7b5f3ce90cf2e8439e841
Ryan Rondeau <nayr974@users.noreply.github.com>
2019-01-17 14:45:02 -0800

    Move insert to seed_data

--- migrations/sql/afterMigrate__1__seed_data.sql
+++ migrations/sql/afterMigrate__1__seed_data.sql
@@ -345,3 +345,28 @@ VALUES
     ('ZN', 'Zinc', TRUE, 'system-mds', 'system-mds'),
     ('ZR', 'Zirconium', TRUE, 'system-mds', 'system-mds')
 ON CONFLICT DO NOTHING;
+
+INSERT INTO mine_status_xref
+    (
+    mine_operation_status_code,
+    mine_operation_status_reason_code,
+    mine_operation_status_sub_reason_code,
+    create_user,
+    update_user
+    )
+VALUES
+    ('ABN', null, null, 'system-mds', 'system-mds'),
+    ('CLD', null, null, 'system-mds', 'system-mds'),
+    ('CLD', 'CM', null, 'system-mds', 'system-mds'),
+    ('CLD', 'REC', 'LTM', 'system-mds', 'system-mds'),
+    ('CLD', 'REC', 'LWT', 'system-mds', 'system-mds'),
+    ('CLD', 'REC', 'PRP', 'system-mds', 'system-mds'),
+    ('CLD', 'ORP', 'LTM', 'system-mds', 'system-mds'),
+    ('CLD', 'ORP', 'LWT', 'system-mds', 'system-mds'),
+    ('CLD', 'ORP', 'RNS', 'system-mds', 'system-mds'),
+    ('CLD', 'ORP', 'SVR', 'system-mds', 'system-mds'),
+    ('CLD', 'UN', null, 'system-mds', 'system-mds'),
+    ('NS', null, null, 'system-mds', 'system-mds'),
+    ('OP', 'YR', null, 'system-mds', 'system-mds'),
+    ('OP', 'SEA', null, 'system-mds', 'system-mds')
+ON CONFLICT DO NOTHING;
