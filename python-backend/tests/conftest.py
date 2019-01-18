d298933fc1cb4014afe2f3cb36080eb095998b22
James Hollinger <39168456+james-hollinger@users.noreply.github.com>
2019-01-18 13:19:07 -0800

    Feature/mds-1189 Refactor auth decorators (#427)
    * Added mine_admin role to decorators. Added proponent role
    
    * Added requires_any_of decorator and tests.

--- python-backend/tests/conftest.py
+++ python-backend/tests/conftest.py
@@ -52,6 +52,9 @@ def auth_headers(app):
     base_auth_token = _jwt.create_jwt(BASE_AUTH_CLAIMS, TOKEN_HEADER)
     full_auth_token = _jwt.create_jwt(FULL_AUTH_CLAIMS, TOKEN_HEADER)
     view_only_auth_token = _jwt.create_jwt(VIEW_ONLY_AUTH_CLAIMS, TOKEN_HEADER)
+    create_only_auth_token = _jwt.create_jwt(CREATE_ONLY_AUTH_CLAIMS, TOKEN_HEADER)
+    admin_only_auth_token = _jwt.create_jwt(ADMIN_ONLY_AUTH_CLAIMS, TOKEN_HEADER)
+    proponent_only_auth_token = _jwt.create_jwt(PROPONENT_ONLY_AUTH_CLAIMS, TOKEN_HEADER)
     return {
         'base_auth_header': {
             'Authorization': 'Bearer ' + base_auth_token
@@ -61,6 +64,15 @@ def auth_headers(app):
         },
         'view_only_auth_header': {
             'Authorization': 'Bearer ' + view_only_auth_token
+        },
+        'create_only_auth_header': {
+            'Authorization': 'Bearer ' + create_only_auth_token
+        },
+        'admin_only_auth_header': {
+            'Authorization': 'Bearer ' + admin_only_auth_token
+        },
+        'proponent_only_auth_header': {
+            'Authorization': 'Bearer ' + proponent_only_auth_token
         },
     }
 
