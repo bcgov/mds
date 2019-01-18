b259a0886ccfa121be60fe166daab6502a457890
justinmacaulay <justinkmacaulay@gmail.com>
2019-01-18 15:52:53 -0800

    fixed front end test.

--- frontend-public/src/tests/components/dashboard/UserDashboard.spec.js
+++ frontend-public/src/tests/components/dashboard/UserDashboard.spec.js
@@ -3,18 +3,24 @@ import { shallow } from "enzyme";
 import { UserDashboard } from "@/components/dashboard/UserDashboard";
 
 const props = {};
+const dispatchProps = {};
 
 const setupProps = () => {
   props.userInfo = { perferred_username: "MockName" };
 };
 
+const setupDispatchProps = () => {
+  dispatchProps.fetchUserMineInfo = jest.fn(() => Promise.resolve());
+};
+
 beforeEach(() => {
   setupProps();
+  setupDispatchProps();
 });
 
 describe("UserDashboard", () => {
   it("renders properly", () => {
-    const wrapper = shallow(<UserDashboard {...props} />);
+    const wrapper = shallow(<UserDashboard {...props} {...dispatchProps} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
