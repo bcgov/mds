d1cd9197816c0e3001583e96e1b6441402df700f
justinmacaulay <justinkmacaulay@gmail.com>
2019-01-18 13:57:08 -0800

    Added a null screen for users with no mines

--- frontend-public/src/components/dashboard/ProponentDashboard.js
+++ frontend-public/src/components/dashboard/ProponentDashboard.js
@@ -8,8 +8,10 @@ import { Link } from "react-router-dom";
 import { getUserInfo } from "@/selectors/authenticationSelectors";
 import { getUserMineInfo } from "@/selectors/userMineInfoSelector";
 import { fetchUserMineInfo } from "@/actionCreators/userDashboardActionCreator";
+import NullScreen from "@/components/common/NullScreen";
 import CustomPropTypes from "@/customPropTypes";
 import QuestionSidebar from "@/components/common/QuestionsSidebar";
+import Loading from "@/components/common/Loading";
 import * as routes from "@/constants/routes";
 
 const propTypes = {
@@ -21,41 +23,48 @@ const propTypes = {
 // This file is anticipated to use state
 // eslint-disable-next-line react/prefer-stateless-function
 export class ProponentDashboard extends Component {
+  state = { isLoaded: false };
+
   componentDidMount() {
-    this.props.fetchUserMineInfo();
+    this.props.fetchUserMineInfo().then(() => this.setState({ isLoaded: !this.state.isLoading }));
   }
 
   render() {
-    const mines = this.props.userMineInfo ? this.props.userMineInfo.mines : [];
+    if (!this.state.isLoaded) {
+      return <Loading />;
+    }
+    const { mines } = this.props.userMineInfo;
     return (
       <div className="user-dashboard-padding">
-        <Row gutter={16}>
-          <Col xs={1} sm={1} md={2} lg={4} />
-          <Col xs={22} sm={22} md={14} lg={12}>
-            <div>
-              <h1 className="user-title"> Welcome, {this.props.userInfo.preferred_username}.</h1>
-              <p className="large-padding-bot">
-                You are authorized to submit information for the following mines:
-              </p>
-            </div>
-            <ul className="user-mine-list">
-              {mines &&
-                mines.map((mine) => (
+        {mines.length > 0 && (
+          <Row gutter={16}>
+            <Col xs={1} sm={1} md={2} lg={4} />
+            <Col xs={22} sm={22} md={14} lg={12}>
+              <div>
+                <h1 className="user-title"> Welcome, {this.props.userInfo.preferred_username}.</h1>
+                <p className="large-padding-bot">
+                  You are authorized to submit information for the following mines:
+                </p>
+              </div>
+              <ul className="user-mine-list">
+                {mines.map((mine) => (
                   <li>
                     <Link to={routes.MINE_INFO.dynamicRoute(mine.guid)}>{mine.mine_name}</Link>
                   </li>
                 ))}
-            </ul>
-            <p className="large-padding-top">
-              Don't see the mine you are looking for? Contact{" "}
-              <a href="mailto:MDS@gov.bc.ca">MDS@gov.bc.ca</a> for assistance.
-            </p>
-          </Col>
-          <Col xs={22} sm={22} md={6} lg={4}>
-            <QuestionSidebar />
-          </Col>
-          <Col xs={1} sm={1} md={2} lg={4} />
-        </Row>
+              </ul>
+              <p className="large-padding-top">
+                Don't see the mine you are looking for? Contact{" "}
+                <a href="mailto:MDS@gov.bc.ca">MDS@gov.bc.ca</a> for assistance.
+              </p>
+            </Col>
+            <Col xs={22} sm={22} md={6} lg={4}>
+              <QuestionSidebar />
+            </Col>
+            <Col xs={1} sm={1} md={2} lg={4} />
+          </Row>
+        )}
+        {mines.length === 0 && <NullScreen type="no-mines" />}
       </div>
     );
   }
