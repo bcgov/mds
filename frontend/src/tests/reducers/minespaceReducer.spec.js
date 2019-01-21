340731e182df6476ff234014b24384da0e8874bc
Jason Syrotuck <jasyrotuck@gmail.com>
2019-01-21 13:37:03 -0800

    reducer test

---
+++ frontend/src/tests/reducers/minespaceReducer.spec.js
@@ -0,0 +1,38 @@
+import minespaceReducer from "@/reducers/minespaceReducer";
+import { storeMinespaceUserList } from "@/actions/minespaceActions";
+
+const baseExpectedValue = {
+  minespaceUsers: [],
+};
+
+const getBaseExpectedValue = () => JSON.parse(JSON.stringify(baseExpectedValue));
+
+describe("mineReducer", () => {
+  it("receives undefined", () => {
+    const expectedValue = getBaseExpectedValue();
+    const result = minespaceReducer(undefined, {});
+    expect(result).toEqual(expectedValue);
+  });
+
+  it("receives STORE_MINESPACE_USERS", () => {
+    const expectedValue = getBaseExpectedValue();
+    expectedValue.minespaceUsers = {
+      email: "test@email.com",
+      keycloak_guid: "",
+      user_id: "1",
+      mines: [],
+    };
+    const result = minespaceReducer(
+      undefined,
+      storeMinespaceUserList({
+        users: {
+          email: "test@email.com",
+          keycloak_guid: "",
+          user_id: "1",
+          mines: [],
+        },
+      })
+    );
+    expect(result).toEqual(expectedValue);
+  });
+});
