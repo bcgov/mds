94c37f512eb0288a44e286ba160401e5c1f88309
Gyanesh Mishra <gyan@freshworks.io>
2019-01-24 21:24:04 -0800

    update migration

--- migrations/sql/afterMigrate__99__create_test_db.sql
+++ migrations/sql/afterMigrate__99__create_test_db.sql
@@ -1,2 +1,13 @@
-DROP DATABASE IF EXISTS mds_test;
-CREATE DATABASE mds_test OWNER mds;
+DO
+$do$
+DECLARE
+BEGIN
+  CREATE EXTENSION IF NOT EXISTS dblink;
+  IF EXISTS (SELECT 1 FROM pg_database WHERE datname = 'mds_test') THEN
+    RAISE NOTICE 'Database already exists';
+  ELSE
+    PERFORM dblink_connect('dbname=postgres');
+    PERFORM dblink_exec('CREATE DATABASE mds_test OWNER mds');
+  END IF;
+END
+$do$;
