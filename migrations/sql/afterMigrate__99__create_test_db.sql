af48874af9fac7067077984696d05a1c4a3c34cd
Gyanesh Mishra <gyan@freshworks.io>
2019-01-24 21:27:17 -0800

    change connect parameters

--- migrations/sql/afterMigrate__99__create_test_db.sql
+++ migrations/sql/afterMigrate__99__create_test_db.sql
@@ -6,7 +6,7 @@ BEGIN
   IF EXISTS (SELECT 1 FROM pg_database WHERE datname = 'mds_test') THEN
     RAISE NOTICE 'Database already exists';
   ELSE
-    PERFORM dblink_connect('dbname=postgres');
+    PERFORM dblink_connect('');
     PERFORM dblink_exec('CREATE DATABASE mds_test OWNER mds');
   END IF;
 END
