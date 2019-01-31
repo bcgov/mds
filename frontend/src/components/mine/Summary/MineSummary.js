3ea6f15fa7dbd74820ab5fa953e5ef511cc25961
Jason Syrotuck <jasyrotuck@gmail.com>
2019-01-31 13:32:27 -0800

    better safety and missing values

--- frontend/src/components/mine/Summary/MineSummary.js
+++ frontend/src/components/mine/Summary/MineSummary.js
@@ -60,72 +60,71 @@ const renderPartyRelationship = (mine, partyRelationship, partyRelationshipTypes
   );
 };
 
-const renderSummaryPermit = (permit, partyRelationships) => {
-  if (partyRelationships.length === 0) return <div>{String.LOADING}</div>;
-  return (
-    <Col xs={24} sm={24} md={12} lg={8} xl={6} xxl={4} key={permit.permit_guid}>
-      {" "}
-      <PermitCard permit={permit} PartyRelationships={partyRelationships} />
-    </Col>
-  );
-};
+const renderSummaryPermit = (permit, partyRelationships) => (
+  <Col xs={24} sm={24} md={12} lg={8} xl={6} xxl={4} key={permit.permit_guid}>
+    {" "}
+    <PermitCard permit={permit} PartyRelationships={partyRelationships} />
+  </Col>
+);
 
-const renderSummaryTSF = (tsf, partyRelationships) => {
-  if (partyRelationships.length === 0) return <div>{String.LOADING}</div>;
-  return (
-    <Col
-      xs={24}
-      sm={24}
-      md={12}
-      lg={8}
-      xl={6}
-      xxl={4}
-      key={tsf.mine_tailings_storage_facility_guid}
-    >
-      {" "}
-      <TSFCard tailingsStorageFacility={tsf} PartyRelationships={partyRelationships} />
-    </Col>
-  );
-};
+const renderSummaryTSF = (tsf, partyRelationships) => (
+  <Col xs={24} sm={24} md={12} lg={8} xl={6} xxl={4} key={tsf.mine_tailings_storage_facility_guid}>
+    {" "}
+    <TSFCard tailingsStorageFacility={tsf} PartyRelationships={partyRelationships} />
+  </Col>
+);
 
 const isActive = (pr) =>
   (!pr.end_date || Date.parse(pr.end_date) >= new Date()) &&
   (!pr.start_date || Date.parse(pr.start_date) <= new Date());
 
 export const MineSummary = (props) => {
-  if (props.partyRelationships.length === 0) {
+  if (
+    props.partyRelationships.length === 0 &&
+    props.mine.mine_permit.length === 0 &&
+    props.mine.mine_tailings_storage_facility.length === 0 &&
+    !props.mineComplianceInfo
+  ) {
     return <NullScreen type="generic" />;
   }
 
   return (
     <div>
-      <Row gutter={16} type="flex" justify="center">
-        <Col span={18}>
-          <Row gutter={16}>
-            <Col span={24}>
-              <h4>MAIN CONTACTS</h4>
-              <Divider />
-            </Col>
-          </Row>
-          <Row gutter={16} type="flex">
-            {props.partyRelationships
-              .filter(isActive)
-              .filter((pr) => ["MMG", "PMT"].includes(pr.mine_party_appt_type_code))
-              .map((partyRelationship) =>
-                renderPartyRelationship(props.mine, partyRelationship, props.partyRelationshipTypes)
-              )}
-          </Row>
-          <Row gutter={16}>
-            <Col span={24}>
-              <div className="right">
-                <Link to={router.MINE_SUMMARY.dynamicRoute(props.mine.guid, "contact-information")}>
-                  See All Contacts
-                </Link>
-              </div>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
+      {props.partyRelationships && props.partyRelationships.length > 0 && (
+        <Row gutter={16} type="flex" justify="center">
+          <Col span={18}>
+            <Row gutter={16}>
+              <Col span={24}>
+                <h4>MAIN CONTACTS</h4>
+                <Divider />
+              </Col>
+            </Row>
+            <Row gutter={16} type="flex">
+              {props.partyRelationships
+                .filter(isActive)
+                .filter((pr) => ["MMG", "PMT"].includes(pr.mine_party_appt_type_code))
+                .map((partyRelationship) =>
+                  renderPartyRelationship(
+                    props.mine,
+                    partyRelationship,
+                    props.partyRelationshipTypes
+                  )
+                )}
+            </Row>
+            <Row gutter={16}>
+              <Col span={24}>
+                <div className="right">
+                  <Link
+                    to={router.MINE_SUMMARY.dynamicRoute(props.mine.guid, "contact-information")}
+                  >
+                    See All Contacts
+                  </Link>
+                </div>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      )}
       {props.mine.mine_permit && props.mine.mine_permit.length > 0 && (
         <Row gutter={16} type="flex" justify="center">
           <Col span={18}>
