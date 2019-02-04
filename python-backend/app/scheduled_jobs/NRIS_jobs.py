c29f098b84435295842588c84982d4a2a5123d93
justinmacaulay <justinkmacaulay@gmail.com>
2019-02-04 10:37:35 -0800

    added in exception handling around the NRIS cache.

--- python-backend/app/scheduled_jobs/NRIS_jobs.py
+++ python-backend/app/scheduled_jobs/NRIS_jobs.py
@@ -1,3 +1,4 @@
+import requests
 from app.extensions import cache, sched
 from app.api.nris_services import NRIS_service
 from app.api.mines.mine.models.mine import Mine
@@ -19,7 +20,7 @@ def _schedule_NRIS_jobs(app):
 # caches a list of mine numbers for all major mines and each major mine indavidually
 # to indicate whether of not it has been processed.
 def _cache_major_mines_list():
-    with sched.app.app_context():
+    with sched.app.app_context() as app:
         cache.set(NRIS_JOB_PREFIX + NRIS_MMLIST_JOB, 'True', timeout=TIMEOUT_24_HOURS)
         major_mines = Mine.query.unbound_unsafe().filter_by(major_mine_ind=True).all()
         major_mine_list = []
@@ -32,7 +33,7 @@ def _cache_major_mines_list():
 
 # Using the cached list of major mines procees them if they are not already set to true.
 def _cache_all_NRIS_major_mines_data():
-    with sched.app.app_context():
+    with sched.app.app_context() as app:
         major_mine_list = cache.get(NRIS_JOB_PREFIX + NRIS_MAJOR_MINE_LIST)
         if major_mine_list is None:
             return
@@ -40,5 +41,16 @@ def _cache_all_NRIS_major_mines_data():
         for mine in major_mine_list:
             if cache.get(NRIS_JOB_PREFIX + mine) == 'False':
                 cache.set(NRIS_JOB_PREFIX + mine, 'True', timeout=TIMEOUT_60_MINUTES)
-                data = NRIS_service._get_EMPR_data_from_NRIS(mine)
-                NRIS_service._process_NRIS_data(data, mine)
+                try:
+                    data = NRIS_service._get_EMPR_data_from_NRIS(mine)
+                except requests.exceptions.Timeout:
+                    pass
+                except requests.exceptions.HTTPError as errhttp:
+                    #log error
+                    pass
+                except TypeError as e:
+                    #log error
+                    pass
+
+                if data is not None and len(data) > 0:
+                    NRIS_service._process_NRIS_data(data, mine)
