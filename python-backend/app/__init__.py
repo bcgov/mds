22cf2d98380acb8b4d800c6a6e367d4f601ee6b8
justinmacaulay <justinkmacaulay@gmail.com>
2019-02-04 13:48:01 -0800

    remove APM and reschedule

--- python-backend/app/__init__.py
+++ python-backend/app/__init__.py
@@ -38,7 +38,8 @@ def create_app(test_config=None):
     register_extensions(app)
     register_routes(app)
     register_commands(app)
-    register_apm(app)
+    if app.config.get('ENVIRONMENT_NAME') == 'prod':
+        register_apm(app)
 
     return app
 
