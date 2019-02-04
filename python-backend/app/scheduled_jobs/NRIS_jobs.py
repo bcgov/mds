52611f9a42f26d135732c746f6fb0463f0f31398
justinmacaulay <justinkmacaulay@gmail.com>
2019-02-04 11:14:08 -0800

    changed job schedule for testing.

--- python-backend/app/scheduled_jobs/NRIS_jobs.py
+++ python-backend/app/scheduled_jobs/NRIS_jobs.py
@@ -8,13 +8,13 @@ from app.api.constants import NRIS_JOB_PREFIX, NRIS_MMLIST_JOB, NRIS_MAJOR_MINE_
 #the schedule of these jobs is set using server time (UTC)
 def _schedule_NRIS_jobs(app):
     app.apscheduler.add_job(
-        func=_cache_major_mines_list, trigger='cron', id='get_major_mine_list', hour=9, minute=0)
+        func=_cache_major_mines_list, trigger='cron', id='get_major_mine_list', hour=19, minute=45)
     app.apscheduler.add_job(
         func=_cache_all_NRIS_major_mines_data,
         trigger='cron',
         id='get_major_mine_NRIS_data',
-        hour=9,
-        minute=10)
+        hour=19,
+        minute=47)
 
 
 # caches a list of mine numbers for all major mines and each major mine indavidually
