9101574fc52bb60e485c786ffec13ac42ecd7838
James Hollinger <jameshollinger.ee@gmail.com>
2019-02-07 17:06:29 -0800

    Update model

--- python-backend/app/api/documents/expected/models/document_status.py
+++ python-backend/app/api/documents/expected/models/document_status.py
@@ -12,25 +12,24 @@ from ....utils.models_mixins import AuditMixin, Base
 
 
 class ExpectedDocumentStatus(AuditMixin, Base):
-    __tablename__ = 'mine_expected_document_status'
-    exp_document_status_guid = db.Column(
-        UUID(as_uuid=True), primary_key=True, server_default=FetchedValue())
+    __tablename__ = 'mine_expected_document_status_code'
+    exp_document_status_code = db.Column(db.String(3), primary_key=True, server_default=FetchedValue())
     description = db.Column(db.String(100), nullable=False)
     display_order = db.Column(db.Integer, nullable=False)
     active_ind = db.Column(db.Boolean, nullable=False, server_default=FetchedValue())
 
     def __repr__(self):
-        return '<ExpectedDocumentStatus %r>' % self.exp_document_status_guid
+        return '<ExpectedDocumentStatus %r>' % self.exp_document_status_code
 
     def json(self):
         return {
-            'value': str(self.exp_document_status_guid),
+            'value': str(self.exp_document_status_code),
             'label': str(self.description),
         }
 
     @classmethod
-    def find_by_expected_document_status(cls, _id):
-        return cls.query.filter_by(exp_document_status_guid=_id).first()
+    def find_by_expected_document_status(cls, code):
+        return cls.query.filter_by(exp_document_status_code=code).first()
 
     @classmethod
     def find_by_expected_document_description(cls, description):
