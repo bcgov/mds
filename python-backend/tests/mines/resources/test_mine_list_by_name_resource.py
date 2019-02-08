c26a29173014a8b7850194bdfec8ba8ba0765ac9
Nathan Calvank <nathan@freshworks.io>
2019-02-08 12:22:54 -0800

    Fix failing tests (hopefully)

--- python-backend/tests/mines/resources/test_mine_list_by_name_resource.py
+++ python-backend/tests/mines/resources/test_mine_list_by_name_resource.py
@@ -16,8 +16,8 @@ def test_get_mines_by_list(test_client, auth_headers):
     assert get_data['mines'][0] == context
 
 
-def test_get_mines_by_list_search_term_name(test_client, auth_headers):
-    get_resp = test_client.get('/mines/search?term=mine_name', headers=auth_headers['full_auth_header'])
+def test_get_mines_by_list_search_by_name(test_client, auth_headers):
+    get_resp = test_client.get('/mines/search?name=' + TEST_MINE_NAME, headers=auth_headers['full_auth_header'])
     get_data = json.loads(get_resp.data.decode())
     context = {
         'guid': TEST_MINE_GUID,
@@ -30,8 +30,22 @@ def test_get_mines_by_list_search_term_name(test_client, auth_headers):
     assert get_data['mines'][0] == context
 
 
-def test_get_mines_by_list_search_by_name(test_client, auth_headers):
-    get_resp = test_client.get('/mines/search?name=mine_name', headers=auth_headers['full_auth_header'])
+def test_get_mines_by_list_search_term_name(test_client, auth_headers):
+    get_resp = test_client.get('/mines/search?term=' + TEST_MINE_NAME, headers=auth_headers['full_auth_header'])
+    get_data = json.loads(get_resp.data.decode())
+    context = {
+        'guid': TEST_MINE_GUID,
+        'mine_name': TEST_MINE_NAME,
+        'mine_no': TEST_MINE_NO,
+        'latitude': TEST_LAT_1,
+        'longitude': TEST_LONG_1
+    }
+    assert get_resp.status_code == 200
+    assert get_data['mines'][0] == context
+
+
+def test_get_mines_by_list_search_by_mine_no(test_client, auth_headers):
+    get_resp = test_client.get('/mines/search?term=' + TEST_MINE_NO, headers=auth_headers['full_auth_header'])
     get_data = json.loads(get_resp.data.decode())
     context = {
         'guid': TEST_MINE_GUID,
