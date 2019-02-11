6ee9fbd2e2a3db20eb6378781b6327924dbbc4e1
James Hollinger <jameshollinger.ee@gmail.com>
2019-02-11 10:06:00 -0800

    Update migration based on feedback

--- migrations/sql/V2019.02.07.02.27__alter_expected_document_status.sql
+++ migrations/sql/V2019.02.07.02.27__alter_expected_document_status.sql
@@ -2,11 +2,11 @@ ALTER TABLE mine_expected_document DROP CONSTRAINT mine_expected_document_exp_do
 ALTER TABLE mine_expected_document DROP COLUMN exp_document_status_guid;
 ALTER TABLE mine_expected_document ADD COLUMN exp_document_status_code character varying(3) NOT NULL DEFAULT 'MIA';
 
-DROP TABLE mine_expected_document_status;
+DROP TABLE IF EXISTS mine_expected_document_status;
 CREATE TABLE mine_expected_document_status_code (
     exp_document_status_code character varying(3) PRIMARY KEY DEFAULT 'MIA',
     description character varying(100) NOT NULL,
-    display_order smallint NOT NULL,
+    display_order smallint,
     active_ind boolean DEFAULT true NOT NULL,
     create_user character varying(60) NOT NULL,
     create_timestamp timestamp with time zone DEFAULT now() NOT NULL,
