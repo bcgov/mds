1471573b3e10512c568bdfba9962bb6d2b94c2e7
justinmacaulay <justinkmacaulay@gmail.com>
2019-02-14 09:56:36 -0800

    Changed the schedule to run every 2 hours.

--- python-backend/app/scheduled_jobs/ETL_jobs.py
+++ python-backend/app/scheduled_jobs/ETL_jobs.py
@@ -1,15 +1,13 @@
-import requests
-from app.extensions import db, cache, sched
-from app.api.nris_services import NRIS_service
-from app.api.mines.mine.models.mine import Mine
-from app.api.constants import NRIS_JOB_PREFIX, NRIS_MMLIST_JOB, NRIS_MAJOR_MINE_LIST, TIMEOUT_24_HOURS, TIMEOUT_60_MINUTES
+from app.extensions import db, sched
+from app.api.utils.apm import register_apm
 
 
 #the schedule of these jobs is set using server time (UTC)
 def _schedule_ETL_jobs(app):
-    app.apscheduler.add_job(func=_run_ETL, trigger='cron', id='ETL', hour=10, minute=0)
+    app.apscheduler.add_job(func=_run_ETL, trigger='interval', id='ETL', hour=2)
 
 
+@register_apm
 def _run_ETL():
     with sched.app.app_context():
         db.session.execute('select transfer_mine_information();')
