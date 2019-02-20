5e0583106702fd1f5000d8d1bbe7db195523896b
Jason Syrotuck <jasyrotuck@gmail.com>
2019-02-20 14:59:06 -0800

    create for permit_amendments

--- python-backend/app/api/permits/permit_amendment/models/permit_amendment.py
+++ python-backend/app/api/permits/permit_amendment/models/permit_amendment.py
@@ -14,24 +14,26 @@ from ....utils.models_mixins import AuditMixin, Base
 class PermitAmendment(AuditMixin, Base):
     __tablename__ = 'permit_amendment'
     permit_amendment_id = db.Column(db.Integer, primary_key=True, server_default=FetchedValue())
-    permit_amendment_guid = db.Column(UUID(as_uuid=True))
+    permit_amendment_guid = db.Column(UUID(as_uuid=True), server_default=FetchedValue())
     permit_id = db.Column(db.Integer, db.ForeignKey('permit.permit_id'), nullable=False)
-    received_date = db.Column(db.DateTime, nullable=False, server_default=FetchedValue())
-    issue_date = db.Column(db.DateTime, nullable=False, server_default=FetchedValue())
-    authorization_end_date = db.Column(db.DateTime, nullable=False, server_default=FetchedValue())
+    received_date = db.Column(db.DateTime, nullable=False)
+    issue_date = db.Column(db.DateTime, nullable=False)
+    authorization_end_date = db.Column(db.DateTime, nullable=False)
     permit_amendment_status_code = db.Column(
         db.String(2), db.ForeignKey('permit_amendment_status_code.permit_amendment_status_code'))
     permit_amendment_type_code = db.Column(
         db.String(2), db.ForeignKey('permit_amendment_type_code.permit_amendment_type_code'))
 
+    mine_documents = db.relationship('PermitAmendmentDocumentXref', lazy='joined')
+
     def json(self):
         return {
             'permit_amendment_id':
-            self.permit_amendment_id,
+            str(self.permit_amendment_id),
             'permit_amendment_guid':
             str(self.permit_amendment_guid),
             'permit_id':
-            self.permit_id,
+            str(self.permit_id),
             'permit_amendment_status_code':
             self.permit_amendment_status_code,
             'permit_amendment_type_code':
@@ -41,8 +43,33 @@ class PermitAmendment(AuditMixin, Base):
             'issue_date':
             self.issue_date.isoformat() if self.issue_date else None,
             'authorization_end_date':
-            self.authorization_end_date.isoformat() if self.authorization_end_date else None
+            self.authorization_end_date.isoformat() if self.authorization_end_date else None,
+            'related_documents':
+            str(self.mine_documents.mine_document_guid) if self.mine_documents else []
         }
+
+    @classmethod
+    def create(cls,
+               permit,
+               received_date,
+               issue_date,
+               authorization_end_date,
+               user_kwargs,
+               permit_amendment_type_code='AMD',
+               permit_amendment_status_code='ACT',
+               save=False):
+        new = cls(
+            permit_id=permit.permit_id,
+            received_date=received_date,
+            issue_date=issue_date,
+            authorization_end_date=authorization_end_date,
+            permit_amendment_type_code=permit_amendment_type_code,
+            permit_amendment_status_code=permit_amendment_status_code,
+            **user_kwargs)
+        permit.permit_amendments.append(new)
+        if save:
+            db.session.commit()
+        return new
 
     @classmethod
     def find_by_permit_amendment_id(cls, _id):
@@ -72,23 +99,26 @@ class PermitAmendment(AuditMixin, Base):
 
     @validates('received_date')
     def validate_received_date(self, key, received_date):
-        if received_date.isoformat() == '9999-12-31':
-            raise AssertionError(
-                'Permit amendment received date should be set to null if not known.')
-        if received_date > datetime.today():
-            raise AssertionError('Permit amendment received date cannot be set to the future.')
+        if received_date:
+            if received_date.isoformat() == '9999-12-31':
+                raise AssertionError(
+                    'Permit amendment received date should be set to null if not known.')
+            if received_date > datetime.today():
+                raise AssertionError('Permit amendment received date cannot be set to the future.')
         return received_date
 
     @validates('issue_date')
     def validate_issue_date(self, key, issue_date):
-        if issue_date.isoformat() == '9999-12-31':
-            raise AssertionError('Permit amendment issue date should be set to null if not known.')
-        if issue_date > datetime.today():
-            raise AssertionError('Permit amendment issue date cannot be set to the future.')
+        if issue_date:
+            if issue_date.isoformat() == '9999-12-31':
+                raise AssertionError(
+                    'Permit amendment issue date should be set to null if not known.')
+            if issue_date > datetime.today():
+                raise AssertionError('Permit amendment issue date cannot be set to the future.')
         return issue_date
 
     @validates('authorization_end_date')
     def validate_authorization_end_date(self, key, authorization_end_date):
-        if authorization_end_date.isoformat() == '9999-12-31':
+        if authorization_end_date and authorization_end_date.isoformat() == '9999-12-31':
             raise AssertionError('Permit amendment end date should be set to null if not known.')
         return authorization_end_date
--- python-backend/app/api/permits/permit_amendment/resources/permit_amendment.py
+++ python-backend/app/api/permits/permit_amendment/resources/permit_amendment.py
@@ -1,12 +1,25 @@
-from flask_restplus import Resource
+from flask_restplus import Resource, reqparse
 from ...permit.models.permit import Permit
 from ..models.permit_amendment import PermitAmendment
+from ..models.permit_ammendment_document_xref import PermitAmendmentDocumentXref
 from app.extensions import api
 from ....utils.access_decorators import requires_role_mine_view
 from ....utils.resources_mixins import UserMixin, ErrorMixin
 
 
 class PermitAmendmentResource(Resource, UserMixin, ErrorMixin):
+    parser = reqparse.RequestParser()
+
+    parser.add_argument(
+        'received_date', type=lambda x: datetime.strptime(x, '%Y-%m-%d') if x else None)
+    parser.add_argument(
+        'issue_date', type=lambda x: datetime.strptime(x, '%Y-%m-%d') if x else None)
+    parser.add_argument(
+        'authorization_end_date', type=lambda x: datetime.strptime(x, '%Y-%m-%d') if x else None)
+    parser.add_argument('permit_amendment_type_code', type=str)
+    parser.add_argument('permit_amendment_status_code', type=str)
+    parser.add_argument('mine_document_guid', type=str)
+
     @api.doc(params={'permit_amendment_id': 'Permit amendment id.', 'permit_guid': 'Permit GUID'})
     @requires_role_mine_view
     def get(self, permit_guid=None, permit_amendment_id=None):
@@ -25,3 +38,30 @@ class PermitAmendmentResource(Resource, UserMixin, ErrorMixin):
                     return result
 
         return self.create_error_payload(404, 'Permit amendment(s) not found'), 404
+
+    @requires_role_mine_view
+    def post(self, permit_guid=None, permit_amendment_id=None):
+        if not permit_guid:
+            return self.create_error_payload(400, 'Permit_guid must be provided')
+        if permit_amendment_id:
+            return self.create_error_payload(400, 'unexpected permit_amendement_id')
+
+        permit = Permit.find_by_permit_guid(permit_guid)
+        if not permit:
+            return self.create_error_payload(404, 'permit does not exist')
+
+        data = self.parser.parse_args()
+
+        received_date = data.get('received_date')
+        issue_date = data.get('issue_date')
+        authorization_end_date = data.get('authorization_end_date')
+
+        new_pa = PermitAmendment.create(
+            permit,
+            received_date,
+            issue_date,
+            authorization_end_date,
+            self.get_create_update_dict(),
+            save=True)
+
+        return permit.json()
