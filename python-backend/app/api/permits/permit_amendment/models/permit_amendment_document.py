6ba9a0b0f09e392401ae321fefd97a23b625a1ca
Jason Syrotuck <jasyrotuck@gmail.com>
2019-02-22 15:43:35 -0800

    url and resource registration.

--- python-backend/app/api/permits/permit_amendment/models/permit_amendment_document.py
+++ python-backend/app/api/permits/permit_amendment/models/permit_amendment_document.py
@@ -4,17 +4,19 @@ import uuid
 from sqlalchemy.orm import validates
 from sqlalchemy.schema import FetchedValue
 from sqlalchemy.dialects.postgresql import UUID
-from ....utils.models_mixins import Base
+from ....utils.models_mixins import AuditMixin, Base
 
 from app.extensions import db
 
 
 class PermitAmendmentDocument(AuditMixin, Base):
-    __tablename__ = "permit_ammendment_document_xref"
-    permit_amendment_document_guid = db.Column(UUID(as_uuid=True), primary_key=True)
+    __tablename__ = "permit_amendment_document"
+    permit_amendment_document_guid = db.Column(
+        UUID(as_uuid=True), primary_key=True, server_default=FetchedValue())
     permit_amendment_id = db.Column(db.Integer,
                                     db.ForeignKey('permit_amendment.permit_amendment_id'))
     document_name = db.Column(db.String, nullable=False)
+    mine_guid = db.Column(UUID(as_uuid=True), nullable=False)
     document_manager_guid = db.Column(UUID(as_uuid=True))
     active_ind = db.Column(db.Boolean, nullable=False, server_default=FetchedValue())
 
@@ -22,11 +24,13 @@ class PermitAmendmentDocument(AuditMixin, Base):
 
     def json(self):
         return {
-            'document_name':self.document_name
-            'document_manager_guid':str(self.document_manager_guid) if self.document_manager_guid else None
+            'document_guid':
+            str(self.permit_amendment_document_guid),
+            'document_name':
+            self.document_name,
+            'document_manager_guid':
+            str(self.document_manager_guid) if self.document_manager_guid else None
         }
-
-
 
     @classmethod
     def find_by_permit_amendment_guid(cls, _guid):
