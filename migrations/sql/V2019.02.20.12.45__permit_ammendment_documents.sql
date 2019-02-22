9934965c307c5891e0edb317601f3ffbe3713e84
Jason Syrotuck <jasyrotuck@gmail.com>
2019-02-22 10:56:46 -0800

    permits domain manages it's own connection with the doc_manager

--- migrations/sql/V2019.02.20.12.45__permit_ammendment_documents.sql
+++ migrations/sql/V2019.02.20.12.45__permit_ammendment_documents.sql
@@ -1,11 +1,16 @@
-CREATE TABLE permit_ammendment_document_xref (
-    permit_ammendment_document_xref_id serial PRIMARY KEY,
-    mine_document_guid uuid NOT NULL,
-    permit_ammendment_id integer NOT NULL,
-    
+CREATE TABLE permit_amendment_document (
+    permit_amendment_document_guid uuid DEFAULT gen_random_uuid() NOT NULL,
+    permit_ammendment_id uuid NOT NULL,
+    active_ind boolean DEFAULT true NOT NULL,
+    create_user character varying(60) NOT NULL,
+    create_timestamp timestamp with time zone DEFAULT now() NOT NULL,
+    update_user character varying(60) NOT NULL,
+    update_timestamp timestamp with time zone DEFAULT now() NOT NULL,
+    document_name character varying(255) NOT NULL,
+    document_manager_guid uuid NOT NULL
+
     FOREIGN KEY (permit_ammendment_id) REFERENCES permit_amendment(permit_amendment_id) DEFERRABLE INITIALLY DEFERRED,
-    FOREIGN KEY (mine_document_guid) REFERENCES mine_document(mine_document_guid) DEFERRABLE INITIALLY DEFERRED
 );
 
 
-ALTER TABLE mine_expected_document_xref OWNER TO mds;
+ALTER TABLE permit_amendment_document OWNER TO mds;
