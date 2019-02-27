2da2b26e33fe1024950f327429f9d4afe4cf63b4
Jason Syrotuck <jasyrotuck@gmail.com>
2019-02-27 10:05:32 -0800

    update seed data to match

--- migrations/sql/afterMigrate__1__seed_data.sql
+++ migrations/sql/afterMigrate__1__seed_data.sql
@@ -352,3 +352,17 @@ VALUES
     ('ACT', 'Active', 10, 'system-mds', 'system-mds'),
     ('RMT', 'Remitted', 20, 'system-mds', 'system-mds')
 ON CONFLICT DO NOTHING;
+
+
+INSERT INTO mds_required_document (
+    req_document_name,
+    req_document_description,
+    req_document_category_guid,
+    req_document_due_date_type,
+    req_document_due_date_period_months,
+    create_user,
+    update_user
+    )
+VALUES
+    ('As Built Reports', '10.4.5(c)', '6ab98b9a-0e66-4f26-99de-e3c270dea7b6', 'FIS', 12, 'system-mds','system-mds')
+ON CONFLICT DO NOTHING; 
