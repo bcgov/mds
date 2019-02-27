578c7181e5adaaeec6e9b67fa44b82bce09d89d9
Nathan Calvank <nathan@freshworks.io>
2019-02-27 11:35:04 -0800

    Use named instead of positional arguments

--- python-backend/app/api/parties/party_appt/resources/mine_party_appt_resource.py
+++ python-backend/app/api/parties/party_appt/resources/mine_party_appt_resource.py
@@ -32,14 +32,14 @@ class MinePartyApptResource(Resource, UserMixin, ErrorMixin):
             mpa = MinePartyAppointment.find_by_mine_party_appt_guid(mine_party_appt_guid)
             if not mpa:
                 self.raise_error(404, 'Mine Party Appointment not found')
-            result = mpa.json(relationships)
+            result = mpa.json(relationships=relationships)
         else:
             mine_guid = request.args.get('mine_guid')
             party_guid = request.args.get('party_guid')
             types = request.args.getlist('types')  #list
             mpas = MinePartyAppointment.find_by(
                 mine_guid=mine_guid, party_guid=party_guid, mine_party_appt_type_codes=types)
-            result = list(map(lambda x: x.json(relationships), mpas))
+            result = list(map(lambda x: x.json(relationships=relationships), mpas))
         return result
 
     @api.doc(params={'mine_party_appt_guid': 'mine party appointment serial id'})
