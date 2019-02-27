80c9a184f1e1f951f574abdcd295e3aeffd68e1d
Gyanesh Mishra <gyan@freshworks.io>
2019-02-27 09:54:56 -0800

    update timer

--- python-backend/app/scheduled_jobs/NRIS_jobs.py
+++ python-backend/app/scheduled_jobs/NRIS_jobs.py
@@ -11,7 +11,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 # caches a list of mine numbers for all major mines and each major mine individually
 # to indicate whether of not it has been processed.
-@sched.task('cron', id='get_major_mine_list', hour=5, minute=50)
+@sched.task('cron', id='get_major_mine_list', hour=6, minute=30)
 @register_apm
 def _cache_major_mines_list():
     with sched.app.app_context():
@@ -28,7 +28,7 @@ def _cache_major_mines_list():
 
 
 # Using the cached list of major mines process them if they are not already set to true.
-@sched.task('cron', id='get_major_mine_NRIS_data', hour=5, minute=55)
+@sched.task('cron', id='get_major_mine_NRIS_data', hour=6, minute=35)
 @register_apm
 def _cache_all_NRIS_major_mines_data():
     major_mine_list = cache.get(NRIS_JOB_PREFIX + NRIS_MAJOR_MINE_LIST)
