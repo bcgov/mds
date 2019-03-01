4ff2718021713187b95d49d8caf63dd7b7eebe12
Jason Syrotuck <jasyrotuck@gmail.com>
2019-03-01 14:34:42 -0800

    create action creator

--- frontend/src/actionCreators/permitActionCreator.js
+++ frontend/src/actionCreators/permitActionCreator.js
@@ -8,7 +8,27 @@ import * as String from "@/constants/strings";
 import * as API from "@/constants/API";
 import { ENVIRONMENT } from "@/constants/environment";
 import { createRequestHeader } from "@/utils/RequestHeaders";
-import queryString from "query-string";
+
+export const createPermit = (payload) => (dispatch) => {
+  dispatch(request(reducerTypes.CREATE_PERMIT));
+  dispatch(showLoading("modal"));
+  return axios
+    .post(ENVIRONMENT.apiUrl + API.PERMIT(), payload, createRequestHeader())
+    .then((response) => {
+      notification.success({ message: "Successfully created a new permit", duration: 10 });
+      dispatch(success(reducerTypes.CREATE_PERMIT));
+      dispatch(hideLoading("modal"));
+      return response;
+    })
+    .catch((err) => {
+      notification.error({
+        message: err.response ? err.response.data.error.message : String.ERROR,
+        duration: 10,
+      });
+      dispatch(error(reducerTypes.CREATE_PERMIT));
+      dispatch(hideLoading("modal"));
+    });
+};
 
 export const fetchPermits = (params = {}) => (dispatch) => {
   dispatch(request(reducerTypes.GET_PERMITS));
