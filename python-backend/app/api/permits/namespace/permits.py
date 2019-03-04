ace2dc2540e02a3a47c90eeebbd88b9e59b5eeee
Jason Syrotuck <jasyrotuck@gmail.com>
2019-03-04 12:34:45 -0800

    endpoint to get permit statuses .

--- python-backend/app/api/permits/namespace/permits.py
+++ python-backend/app/api/permits/namespace/permits.py
@@ -1,12 +1,14 @@
 from flask_restplus import Namespace
 
 from ..permit.resources.permit import PermitResource
+from ..permit.resources.permit_status_code import PermitStatusCodeResource
 from ..permit_amendment.resources.permit_amendment import PermitAmendmentResource
 from ..permit_amendment.resources.permit_amendment_document import PermitAmendmentDocumentResource
 
 api = Namespace('permits', description='Permit related operations')
 
 api.add_resource(PermitResource, '', '/<string:permit_guid>')
+api.add_resource(PermitStatusCodeResource, '/status-codes')
 api.add_resource(PermitAmendmentResource, '/<string:permit_guid>/amendments',
                  '/amendments/<string:permit_amendment_guid>',
                  '/<string:permit_guid>/amendments/<string:permit_amendment_guid>')
