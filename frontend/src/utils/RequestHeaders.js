476eeec0a1dd2f4e5799880ecc316d232062a7a8
Nathan Calvank <nathan@freshworks.io>
2019-03-21 11:31:57 -0700

    Linting Errors fix: First batch
    Down to 100

--- frontend/src/utils/RequestHeaders.js
+++ frontend/src/utils/RequestHeaders.js
@@ -2,6 +2,8 @@
  * Utility class for various request Headers.
  */
 
+// This file is anticipated to have multiple exports
+// eslint-disable-next-line import/prefer-default-export
 export const createRequestHeader = () => ({
   headers: {
     "Access-Control-Allow-Origin": "*",
