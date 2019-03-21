534116b56b4998baf4592f3acfc940e2e1eb2534
Jason Syrotuck <jasyrotuck@gmail.com>
2019-03-21 12:57:41 -0700

    fixed imports.

--- python-backend/app/api/mines/mine/resources/mine_verified_status.py
+++ python-backend/app/api/mines/mine/resources/mine_verified_status.py
@@ -6,10 +6,10 @@ from app.extensions import api
 
 from app.api.utils.access_decorators import requires_role_mine_view, requires_role_mine_create, requires_role_mine_admin
 from app.api.utils.resources_mixins import UserMixin, ErrorMixin
-from app.api.mine.models.mine_verified_status import MineVerifiedStatus
+from app.api.mines.mine.models.mine_verified_status import MineVerifiedStatus
 
 
-class MineVerifiedStatus(Resource, UserMixin, ErrorMixin):
+class MineVerifiedStatusResource(Resource, UserMixin, ErrorMixin):
     parser = reqparse.RequestParser()
     parser.add_argument(
         'mine_guid', type=str, required=True, help='Unique identifier for the mine.')
@@ -37,8 +37,7 @@ class MineVerifiedStatus(Resource, UserMixin, ErrorMixin):
         mine_verified_status = MineVerifiedStatus(
             mine_guid=mine_guid,
             verifying_user=User().get_user_username,
-            verifying_timestamp=datetime.now
-        )
+            verifying_timestamp=datetime.now)
         mine_verified_status.save()
         return mine_verified_status.json()
 
@@ -51,8 +50,7 @@ class MineVerifiedStatus(Resource, UserMixin, ErrorMixin):
 
         mine_verified_status = MineVerifiedStatus.find_by_mine_guid(mine_guid)
         if not mine_verified_status:
-            raise BadRequest(
-                'Cannot find a verified status for the given mine')
+            raise BadRequest('Cannot find a verified status for the given mine')
 
         mine_verified_status.healthy = False
         mine_verified_status.save()
