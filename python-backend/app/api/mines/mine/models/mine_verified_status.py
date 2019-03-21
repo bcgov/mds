f6f6088ee57c0529f1ee4dad73f4b5ee97c0cdc0
Jason Syrotuck <jasyrotuck@gmail.com>
2019-03-21 10:44:08 -0700

    date columns auto updating.

--- python-backend/app/api/mines/mine/models/mine_verified_status.py
+++ python-backend/app/api/mines/mine/models/mine_verified_status.py
@@ -1,17 +1,37 @@
-import uuid
+import uuid, datetime
 
 from sqlalchemy.orm import validates
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.schema import FetchedValue
 from app.extensions import db
 from app.api.utils.models_mixins import AuditMixin, Base
+from app.api.utils.include.user_info import User
 
 
-class MineVerifiedStatus(AuditMixin, Base):
+class MineVerifiedStatus(Base):
     __tablename__ = 'mine_verified_status'
 
     mine_verified_status_id = db.Column(db.Integer, primary_key=True, server_default=FetchedValue())
     mine_guid = db.Column(UUID(as_uuid=True), db.ForeignKey('mine.mine_guid'))
-    active_ind = db.Column(db.Boolean, nullable=False)
-    verifying_user = db.Column(db.String, nullable=False)
-    verifying_timestamp = db.Column(db.DateTime, nullable=False)
+    healthy_ind = db.Column(db.Boolean, nullable=False, server_default=FetchedValue())
+
+    verifying_user = db.Column(
+        db.String,
+        nullable=False,
+        default=User().get_user_username,
+        onupdate=User().get_user_username)
+    verifying_timestamp = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.datetime.utcnow,
+        onupdate=datetime.datetime.now)
+    update_user = db.Column(
+        db.String(60),
+        nullable=False,
+        default=User().get_user_username,
+        onupdate=User().get_user_username)
+    update_timestamp = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.datetime.utcnow,
+        onupdate=datetime.datetime.now)
