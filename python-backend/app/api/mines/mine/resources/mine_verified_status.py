447003b61e0ef629caa2ec273b6fff5c9abb6553
Jason Syrotuck <jasyrotuck@gmail.com>
2019-03-21 11:40:32 -0700

    required flag

--- python-backend/app/api/mines/mine/resources/mine_verified_status.py
+++ python-backend/app/api/mines/mine/resources/mine_verified_status.py
@@ -1,5 +1,5 @@
 import uuid
-
+from werkzeug.exceptions import *
 from flask_restplus import Resource, reqparse
 from app.extensions import api
 from ....utils.access_decorators import requires_role_mine_create, requires_role_mine_admin
@@ -9,8 +9,8 @@ from ..models.mine_type import MineType
 
 class MineVerifiedStatus(Resource, UserMixin):
     parser = reqparse.RequestParser()
-    parser.add_argument('mine_guid', type=str,
-                        help='Unique identifier for the mine.')
+    parser.add_argument(
+        'mine_guid', type=str, required=True, help='Unique identifier for the mine.')
 
     @api.expect(parser)
     @requires_role_mine_create
@@ -20,9 +20,7 @@ class MineVerifiedStatus(Resource, UserMixin):
             self.raise_error(400, 'Error: Missing mine_guid.')
 
         try:
-            mine_verified_status = MineVerifiedStatus(
-
-            )
+            mine_verified_status = MineVerifiedStatus()
             mine_type.save()
         except exc.IntegrityError as e:
             self.raise_error(400, 'Error: Unable to create mine_type.')
@@ -34,7 +32,7 @@ class MineVerifiedStatus(Resource, UserMixin):
     def delete(self, mine_guid):
         mine_guid = self.parser.parse_args()['mine_guid']
         if not mine_guid:
-            self.raise_error(400, 'Error: Missing mine_guid.')
+            raise BadRequest('Mine_guid not provided')
 
         mine_verified_status = MineVerifiedStatus.find_by_mine_guid(mine_guid)
         if mine_verified_status is not None:
