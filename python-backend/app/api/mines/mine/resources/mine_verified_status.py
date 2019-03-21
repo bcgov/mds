1af45c3441c32474312f54c3cd7d3c98c0a57f9e
Jason Syrotuck <jasyrotuck@gmail.com>
2019-03-21 13:36:08 -0700

    mine_guid comes from url.

--- python-backend/app/api/mines/mine/resources/mine_verified_status.py
+++ python-backend/app/api/mines/mine/resources/mine_verified_status.py
@@ -4,6 +4,7 @@ from werkzeug.exceptions import *
 from flask_restplus import Resource, reqparse
 from app.extensions import api
 from flask import request
+from app.api.utils.include.user_info import User
 
 from app.api.utils.access_decorators import requires_role_mine_view, requires_role_mine_create, requires_role_mine_admin
 from app.api.utils.resources_mixins import UserMixin, ErrorMixin
@@ -12,8 +13,6 @@ from app.api.mines.mine.models.mine_verified_status import MineVerifiedStatus
 
 class MineVerifiedStatusResource(Resource, UserMixin, ErrorMixin):
     parser = reqparse.RequestParser()
-    parser.add_argument(
-        'mine_guid', type=str, required=True, help='Unique identifier for the mine.')
 
     @requires_role_mine_view
     def get(self):
@@ -31,21 +30,19 @@ class MineVerifiedStatusResource(Resource, UserMixin, ErrorMixin):
     @api.expect(parser)
     @requires_role_mine_create
     def put(self, mine_guid=None):
-        mine_guid = self.parser.parse_args()['mine_guid']
         if not mine_guid:
             raise BadRequest('Mine_guid not provided')
 
         mine_verified_status = MineVerifiedStatus(
             mine_guid=mine_guid,
-            verifying_user=User().get_user_username,
-            verifying_timestamp=datetime.now)
+            verifying_user=User().get_user_username(),
+            verifying_timestamp=datetime.now())
         mine_verified_status.save()
         return mine_verified_status.json()
 
     @api.expect(parser)
     @requires_role_mine_admin
     def delete(self, mine_guid=None):
-        mine_guid = self.parser.parse_args()['mine_guid']
         if not mine_guid:
             raise BadRequest('Mine_guid not provided')
 
