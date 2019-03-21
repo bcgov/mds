5d2861c93e7bdf81fa55801a22007f0494e0bd15
James Hollinger <jameshollinger.ee@gmail.com>
2019-03-21 15:31:14 -0700

    Update put and delete

--- python-backend/app/api/mines/mine/resources/mine_verified_status.py
+++ python-backend/app/api/mines/mine/resources/mine_verified_status.py
@@ -12,6 +12,9 @@ from app.api.mines.mine.models.mine_verified_status import MineVerifiedStatus
 
 
 class MineVerifiedStatusResource(Resource, UserMixin, ErrorMixin):
+    parser = reqparse.RequestParser()
+    parser.add_argument('healthy', type=bool)
+
     @requires_role_mine_view
     def get(self):
         user_id = request.args.get('user_id')
@@ -25,27 +28,28 @@ class MineVerifiedStatusResource(Resource, UserMixin, ErrorMixin):
             'unhealthy': [x.json() for x in statuses if not x.healthy_ind]
         }
 
+    @api.expect(parser)
     @requires_role_mine_create
     def put(self, mine_guid=None):
         if not mine_guid:
             raise BadRequest('Mine_guid not provided')
 
-        mine_verified_status = MineVerifiedStatus(
-            mine_guid=mine_guid,
-            verifying_user=User().get_user_username(),
-            verifying_timestamp=datetime.now())
+        healthy = self.parser.parse_args().get('healthy')
+        if healthy is None:
+            raise BadRequest('"healthy" parameter not provided')
+
+        mine_verified_status = MineVerifiedStatus.find_by_mine_guid(mine_guid)
+        if not mine_verified_status:
+            mine_verified_status = MineVerifiedStatus(
+                mine_guid=mine_guid,
+                verifying_user=User().get_user_username(),
+                verifying_timestamp=datetime.now())
+
+        mine_verified_status.healthy_ind = healthy
         mine_verified_status.save()
         return mine_verified_status.json()
 
     @requires_role_mine_admin
     def delete(self, mine_guid=None):
-        if not mine_guid:
-            raise BadRequest('Mine_guid not provided')
-
-        mine_verified_status = MineVerifiedStatus.find_by_mine_guid(mine_guid)
-        if not mine_verified_status:
-            raise BadRequest('Cannot find a verified status for the given mine')
-
-        mine_verified_status.healthy = False
-        mine_verified_status.save()
-        return ('', 204)
+        #TODO: Actually delete
+        return ('', 501)
