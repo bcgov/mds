d060435d7a0cf76ec132873fe648903fd9052376
Nathan Calvank <nathan@freshworks.io>
2019-03-25 15:54:23 -0700

    Add variance table
    This includes everything that should be needed for variances in the
    database other than the associated documents.

---
+++ migrations/sql/V2019.03.25.14.40__add_variance_table.sql
@@ -0,0 +1,33 @@
+CREATE TABLE IF NOT EXISTS variance
+(
+    variance_id           integer                PRIMARY KEY                           ,
+    compliance_article_id integer                                              NOT NULL,
+    mine_guid             uuid                                                 NOT NULL,
+    note                  character varying(300) DEFAULT ''::character varying NOT NULL,
+    issue_date            date                   DEFAULT '9999-12-31'::date    NOT NULL,
+    received_date         date                   DEFAULT '9999-12-31'::date    NOT NULL,
+    expiry_date           date                   DEFAULT '9999-12-31'::date    NOT NULL
+);
+
+CREATE SEQUENCE variance_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER TABLE variance_id_seq OWNER TO mds;
+ALTER SEQUENCE variance_id_seq OWNED BY variance.variance_id;
+ALTER TABLE ONLY variance ALTER COLUMN variance_id SET DEFAULT nextval('variance_id_seq'::regclass);
+
+
+-- Constraints
+ALTER TABLE ONLY variance
+    ADD CONSTRAINT variance_compliance_article_id_fkey FOREIGN KEY (compliance_article_id) REFERENCES compliance_article(compliance_article_id);
+ALTER TABLE ONLY variance
+    ADD CONSTRAINT variance_mine_guid_fkey FOREIGN KEY (mine_guid) REFERENCES mine(mine_guid);
+
+
+-- Comments
+COMMENT ON TABLE variance IS 'A request to be exempt from the rules of an HSRC code.';
+COMMENT ON COLUMN variance.expiry_date IS 'Expiry Date reflects the legal date a variance is no longer valid. 9999-12-31 implies the variance is still valid.';
