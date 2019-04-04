4d4a7c52edffc1ca3a3490c556bcbd9827e2b976
James Hollinger <39168456+james-hollinger@users.noreply.github.com>
2019-04-04 16:11:48 -0700

    Refactor Axios interceptors to handle error toast messages (#644)
    * Custom Axios object factory

--- frontend/src/actionCreators/partiesActionCreator.js
+++ frontend/src/actionCreators/partiesActionCreator.js
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { notification } from "antd";
 import { showLoading, hideLoading } from "react-redux-loading-bar";
 import { request, success, error } from "@/actions/genericActions";
@@ -9,11 +8,12 @@ import * as API from "@/constants/API";
 import { ENVIRONMENT } from "@/constants/environment";
 import { createRequestHeader } from "@/utils/RequestHeaders";
 import queryString from "query-string";
+import CustomAxios from "@/customAxios";
 
 export const createParty = (payload) => (dispatch) => {
   dispatch(request(reducerTypes.CREATE_PARTY));
   dispatch(showLoading("modal"));
-  return axios
+  return CustomAxios()
     .post(ENVIRONMENT.apiUrl + API.PARTY, payload, createRequestHeader())
     .then((response) => {
       dispatch(hideLoading("modal"));
@@ -22,10 +22,6 @@ export const createParty = (payload) => (dispatch) => {
       return response;
     })
     .catch((err) => {
-      notification.error({
-        message: err.response ? err.response.data.message : String.ERROR,
-        duration: 10,
-      });
       dispatch(error(reducerTypes.CREATE_PARTY));
       dispatch(hideLoading("modal"));
       throw new Error(err);
@@ -38,7 +34,7 @@ export const updateParty = (payload, partyGuid) => (dispatch) => {
     : payload.party_name;
   dispatch(request(reducerTypes.UPDATE_PARTY));
   dispatch(showLoading("modal"));
-  return axios
+  return CustomAxios()
     .put(`${ENVIRONMENT.apiUrl + API.PARTY}/${partyGuid}`, payload, createRequestHeader())
     .then((response) => {
       dispatch(hideLoading("modal"));
@@ -47,10 +43,6 @@ export const updateParty = (payload, partyGuid) => (dispatch) => {
       return response;
     })
     .catch((err) => {
-      notification.error({
-        message: err.response ? err.response.data.message : String.ERROR,
-        duration: 10,
-      });
       dispatch(error(reducerTypes.UPDATE_PARTY));
       dispatch(hideLoading("modal"));
       throw new Error(err);
@@ -60,67 +52,46 @@ export const updateParty = (payload, partyGuid) => (dispatch) => {
 export const fetchParties = (params = {}) => (dispatch) => {
   dispatch(request(reducerTypes.GET_PARTIES));
   dispatch(showLoading("modal"));
-  return axios
+  return CustomAxios()
     .get(ENVIRONMENT.apiUrl + API.PARTIES_LIST_QUERY(params), createRequestHeader())
     .then((response) => {
       dispatch(success(reducerTypes.GET_PARTIES));
       dispatch(partyActions.storeParties(response.data));
-      dispatch(hideLoading("modal"));
     })
-    .catch((err) => {
-      notification.error({
-        message: err.response ? err.response.data.error.message : String.ERROR,
-        duration: 10,
-      });
-      dispatch(error(reducerTypes.GET_PARTIES));
-      dispatch(hideLoading("modal"));
-    });
+    .catch(() => dispatch(error(reducerTypes.GET_PARTIES)))
+    .finally(() => dispatch(hideLoading("modal")));
 };
 
 export const fetchPartyById = (id) => (dispatch) => {
   dispatch(request(reducerTypes.GET_PARTY));
   dispatch(showLoading());
-  return axios
+  return CustomAxios()
     .get(`${ENVIRONMENT.apiUrl + API.PARTY}/${id}`, createRequestHeader())
     .then((response) => {
       dispatch(success(reducerTypes.GET_PARTY));
       dispatch(partyActions.storeParty(response.data, id));
-      dispatch(hideLoading());
     })
-    .catch((err) => {
-      notification.error({
-        message: err.response ? err.response.data.error.message : String.ERROR,
-        duration: 10,
-      });
-      dispatch(error(reducerTypes.GET_PARTY));
-      dispatch(hideLoading());
-    });
+    .catch(() => dispatch(error(reducerTypes.GET_PARTY)))
+    .finally(() => dispatch(hideLoading()));
 };
 
 export const fetchPartyRelationshipTypes = () => (dispatch) => {
   dispatch(request(reducerTypes.GET_PARTY_RELATIONSHIP_TYPES));
   dispatch(showLoading());
-  return axios
+  return CustomAxios()
     .get(`${ENVIRONMENT.apiUrl + API.PARTY}/mines/relationship-types`, createRequestHeader())
     .then((response) => {
       dispatch(success(reducerTypes.GET_PARTY_RELATIONSHIP_TYPES));
       dispatch(partyActions.storePartyRelationshipTypes(response.data));
-      dispatch(hideLoading());
     })
-    .catch((err) => {
-      notification.error({
-        message: err.response ? err.response.data.error.message : String.ERROR,
-        duration: 10,
-      });
-      dispatch(error(reducerTypes.GET_PARTY_RELATIONSHIP_TYPES));
-      dispatch(hideLoading());
-    });
+    .catch(() => dispatch(error(reducerTypes.GET_PARTY_RELATIONSHIP_TYPES)))
+    .finally(() => dispatch(hideLoading()));
 };
 
 export const addPartyRelationship = (payload) => (dispatch) => {
   dispatch(request(reducerTypes.ADD_PARTY_RELATIONSHIP));
   dispatch(showLoading());
-  return axios
+  return CustomAxios()
     .post(ENVIRONMENT.apiUrl + API.PARTY_RELATIONSHIP, payload, createRequestHeader())
     .then((response) => {
       notification.success({
@@ -128,23 +99,16 @@ export const addPartyRelationship = (payload) => (dispatch) => {
         duration: 10,
       });
       dispatch(success(reducerTypes.ADD_PARTY_RELATIONSHIP));
-      dispatch(hideLoading());
       return response;
     })
-    .catch((err) => {
-      notification.error({
-        message: err.response ? err.response.data.error.message : String.ERROR,
-        duration: 10,
-      });
-      dispatch(error(reducerTypes.ADD_PARTY_RELATIONSHIP));
-      dispatch(hideLoading());
-    });
+    .catch(() => dispatch(error(reducerTypes.ADD_PARTY_RELATIONSHIP)))
+    .finally(() => dispatch(hideLoading()));
 };
 
 export const updatePartyRelationship = (payload) => (dispatch) => {
   dispatch(request(reducerTypes.UPDATE_PARTY_RELATIONSHIP));
   dispatch(showLoading());
-  return axios
+  return CustomAxios()
     .put(
       `${ENVIRONMENT.apiUrl + API.PARTY_RELATIONSHIP}/${payload.mine_party_appt_guid}`,
       payload,
@@ -156,23 +120,16 @@ export const updatePartyRelationship = (payload) => (dispatch) => {
         duration: 10,
       });
       dispatch(success(reducerTypes.UPDATE_PARTY_RELATIONSHIP));
-      dispatch(hideLoading());
       return response;
     })
-    .catch((err) => {
-      notification.error({
-        message: err.response ? err.response.data.error.message : String.ERROR,
-        duration: 10,
-      });
-      dispatch(error(reducerTypes.UPDATE_PARTY_RELATIONSHIP));
-      dispatch(hideLoading());
-    });
+    .catch(() => dispatch(error(reducerTypes.UPDATE_PARTY_RELATIONSHIP)))
+    .finally(() => dispatch(hideLoading()));
 };
 
 export const fetchPartyRelationships = (parms) => (dispatch) => {
   dispatch(request(reducerTypes.FETCH_PARTY_RELATIONSHIPS));
   dispatch(showLoading());
-  return axios
+  return CustomAxios()
     .get(
       `${ENVIRONMENT.apiUrl + API.PARTY_RELATIONSHIP}?${queryString.stringify(parms)}`,
       createRequestHeader()
@@ -180,22 +137,15 @@ export const fetchPartyRelationships = (parms) => (dispatch) => {
     .then((response) => {
       dispatch(success(reducerTypes.FETCH_PARTY_RELATIONSHIPS));
       dispatch(partyActions.storePartyRelationships(response.data));
-      dispatch(hideLoading());
     })
-    .catch((err) => {
-      notification.error({
-        message: err.response ? err.response.data.error.message : String.ERROR,
-        duration: 10,
-      });
-      dispatch(error(reducerTypes.FETCH_PARTY_RELATIONSHIPS));
-      dispatch(hideLoading());
-    });
+    .catch(() => dispatch(error(reducerTypes.FETCH_PARTY_RELATIONSHIPS)))
+    .finally(() => dispatch(hideLoading()));
 };
 
 export const removePartyRelationship = (mine_party_appt_guid) => (dispatch) => {
   dispatch(request(reducerTypes.REMOVE_PARTY_RELATIONSHIP));
   dispatch(showLoading());
-  return axios
+  return CustomAxios(String.ERROR)
     .delete(
       `${ENVIRONMENT.apiUrl + API.PARTY_RELATIONSHIP}/${mine_party_appt_guid}`,
       createRequestHeader()
@@ -203,36 +153,28 @@ export const removePartyRelationship = (mine_party_appt_guid) => (dispatch) => {
     .then((response) => {
       notification.success({ message: "Successfully removed the contact", duration: 10 });
       dispatch(success(reducerTypes.REMOVE_PARTY_RELATIONSHIP));
-      dispatch(hideLoading());
       return response;
     })
-    .catch(() => {
-      notification.error({ message: String.ERROR, duration: 10 });
-      dispatch(error(reducerTypes.REMOVE_PARTY_RELATIONSHIP));
-      dispatch(hideLoading());
-    });
+    .catch(() => dispatch(error(reducerTypes.REMOVE_PARTY_RELATIONSHIP)))
+    .finally(() => dispatch(hideLoading()));
 };
 
 export const deleteParty = (party_guid) => (dispatch) => {
   dispatch(request(reducerTypes.DELETE_PARTY));
   dispatch(showLoading());
-  return axios
+  return CustomAxios(String.ERROR)
     .delete(`${ENVIRONMENT.apiUrl + API.PARTY}/${party_guid}`, createRequestHeader())
     .then((response) => {
       notification.success({ message: "Successfully removed the party", duration: 10 });
       dispatch(success(reducerTypes.DELETE_PARTY));
-      dispatch(hideLoading());
       return response;
     })
-    .catch(() => {
-      notification.error({ message: String.ERROR, duration: 10 });
-      dispatch(error(reducerTypes.DELETE_PARTY));
-      dispatch(hideLoading());
-    });
+    .catch(() => dispatch(error(reducerTypes.DELETE_PARTY)))
+    .finally(() => dispatch(hideLoading()));
 };
 
 export const downloadMineManagerHistory = (mineNo, { window, document }) =>
-  axios({
+  CustomAxios("")({
     method: "GET",
     url: `${ENVIRONMENT.apiUrl + API.MINE_MANAGER_HISTORY(mineNo)}`,
     responseType: "blob",
