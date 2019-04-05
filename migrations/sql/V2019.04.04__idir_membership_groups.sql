8a9940829d2d19281b15a1d2dcdd3641feb0ee61
Jason Syrotuck <jasyrotuck@gmail.com>
2019-04-05 13:31:50 -0700

    user should be system-mds

--- migrations/sql/V2019.04.04__idir_membership_groups.sql
+++ migrations/sql/V2019.04.04__idir_membership_groups.sql
@@ -6,9 +6,9 @@ INSERT INTO idir_membership
     update_user
 )
 VALUES
-    ('CN=EMPR MMRD Compliance and Enforcement,OU=Distribution Lists,OU=Exchange Objects,OU=Energy Mines and Petroleum Resources,OU=BCGOV,DC=idir,DC=BCGOV', 'true', 'mds','mds'),
-    ('CN=EMPR MMRD Excluded Managers,OU=Distribution Lists,OU=Exchange Objects,OU=Energy Mines and Petroleum Resources,OU=BCGOV,DC=idir,DC=BCGOV', 'true', 'mds','mds'),
-    ('CN=EMPR MMRD Health Safety and Permitting Branch,OU=Distribution Lists,OU=Exchange Objects,OU=Energy Mines and Petroleum Resources,OU=BCGOV,DC=idir,DC=BCGOV', 'true', 'mds','mds'),
-    ('CN=EMPR MCAD Policy,OU=Distribution Lists,OU=Exchange Objects,OU=Energy Mines and Petroleum Resources,OU=BCGOV,DC=idir,DC=BCGOV', 'true', 'mds','mds'),
-    ('CN=EMPR MMRD Supervisors,OU=Distribution Lists,OU=Exchange Objects,OU=Energy Mines and Petroleum Resources,OU=BCGOV,DC=idir,DC=BCGOV', 'true', 'mds','mds')
+    ('CN=EMPR MMRD Compliance and Enforcement,OU=Distribution Lists,OU=Exchange Objects,OU=Energy Mines and Petroleum Resources,OU=BCGOV,DC=idir,DC=BCGOV', 'true', 'system-mds','system-mds'),
+    ('CN=EMPR MMRD Excluded Managers,OU=Distribution Lists,OU=Exchange Objects,OU=Energy Mines and Petroleum Resources,OU=BCGOV,DC=idir,DC=BCGOV', 'true', 'system-mds','system-mds'),
+    ('CN=EMPR MMRD Health Safety and Permitting Branch,OU=Distribution Lists,OU=Exchange Objects,OU=Energy Mines and Petroleum Resources,OU=BCGOV,DC=idir,DC=BCGOV', 'true', 'system-mds','system-mds'),
+    ('CN=EMPR MCAD Policy,OU=Distribution Lists,OU=Exchange Objects,OU=Energy Mines and Petroleum Resources,OU=BCGOV,DC=idir,DC=BCGOV', 'true', 'system-mds','system-mds'),
+    ('CN=EMPR MMRD Supervisors,OU=Distribution Lists,OU=Exchange Objects,OU=Energy Mines and Petroleum Resources,OU=BCGOV,DC=idir,DC=BCGOV', 'true', 'system-mds','system-mds')
 ON CONFLICT DO NOTHING;
