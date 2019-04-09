5490f2a981ca0b08533b66cb6b1c0add28ca26cb
Jason Syrotuck <jasyrotuck@gmail.com>
2019-04-09 10:47:25 -0700

    force this to run on dev.

--- python-backend/app/__init__.py
+++ python-backend/app/__init__.py
@@ -41,6 +41,9 @@ def create_app(test_config=None):
     register_commands(app)
     register_scheduled_jobs(app)
 
+    sched.start()
+    _schedule_IDIR_jobs(app)
+    
     return app
 
 
@@ -61,6 +64,17 @@ def register_extensions(app):
     Compress(app)
 
     return None
+
+
+
+def register_scheduled_jobs(app):
+    if app.config.get('ENVIRONMENT_NAME') in ['test', 'prod']:
+        if not app.debug or os.environ.get("WERKZEUG_RUN_MAIN") == 'true':
+
+            _schedule_NRIS_jobs(app)
+            # This is here to prevent this from running in production until we are confident in the permit data.
+            if app.config.get('ENVIRONMENT_NAME') == 'test':
+                _schedule_ETL_jobs(app)
 
 
 def register_routes(app):
@@ -122,14 +136,3 @@ def register_routes(app):
                 'message': str(error)
             }
         }, getattr(error, 'code', 500)
-
-
-def register_scheduled_jobs(app):
-    if app.config.get('ENVIRONMENT_NAME') in ['test', 'prod']:
-        if not app.debug or os.environ.get("WERKZEUG_RUN_MAIN") == 'true':
-            sched.start()
-            _schedule_IDIR_jobs(app)
-            _schedule_NRIS_jobs(app)
-            # This is here to prevent this from running in production until we are confident in the permit data.
-            if app.config.get('ENVIRONMENT_NAME') == 'test':
-                _schedule_ETL_jobs(app)
