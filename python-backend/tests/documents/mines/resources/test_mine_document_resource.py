b97693164ae65dcdec6d08fb32ac7f86e2398afc
James Hollinger <jameshollinger.ee@gmail.com>
2019-04-12 16:32:44 -0700

    Refactor more docs

--- python-backend/tests/documents/mines/resources/test_mine_document_resource.py
+++ python-backend/tests/documents/mines/resources/test_mine_document_resource.py
@@ -1,15 +1,14 @@
 import json
-from tests.constants import TEST_MINE_GUID, TEST_REQUIRED_REPORT_CATEGORY_TAILINGS
+from tests.factories import MineFactory
 
 
 # GET
-def test_get_mine_documents_by_mine_guid(test_client, auth_headers):
+def test_get_mine_documents_by_mine_guid(test_client, db_session, auth_headers):
+    mine_documents_count = 5
+    mine = MineFactory(mine_expected_documents=1, mine_expected_documents__mine_documents=mine_documents_count)
+
     get_resp = test_client.get(
-        '/documents/mines/' + TEST_MINE_GUID,
-        headers=auth_headers['full_auth_header'])
+        '/documents/mines/' + str(mine.mine_guid), headers=auth_headers['full_auth_header'])
     get_data = json.loads(get_resp.data.decode())
     assert get_resp.status_code == 200
-    assert len(get_data['mine_documents']) > 0
-
-
-# GET
+    assert len(get_data['mine_documents']) == mine_documents_count
