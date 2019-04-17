9252e6759f6a49839c38065bf958fc0cf875ed8c
Nathan Calvank <nathan@freshworks.io>
2019-04-17 13:54:11 -0700

    MDS-1418 & MDS-1687 Feature/Sort Contacts Table & Refactor Party Resources for Restplus (#670)
    * Add sorting to table & read/write from URL
    
    * Update snapshots
    
    * Integrate sorting with clear filters
    
    * Integrate sorting with backend
    
    * Party Resource Clean-up WIP 1
    
    - Rename parties to records
    - Update error handling
    - Split resources based on endpoint
    - Use marshal_with
    - Get rid of /search
    
    * Split party address into a separate table
    
    * Update frontend tests
    
    * Update API docs
    
    * Remove appt_type sorting
    
    * Update API docs
    
    * Update backend tests
    
    * Copy trim use from PR #669
    
    * Update snapshots
    
    * Drop indexes that I removed
    
    * Protect PUT field(s) & move guid validation check
    
    * Add Xref suffix to PartyAddress
    
    * Create AddressFactory
    
    * Add Address model tests
    
    * Refactor name formatting for readability

--- frontend/src/components/dashboard/contactsHomePage/ContactList.js
+++ frontend/src/components/dashboard/contactsHomePage/ContactList.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { objectOf, string } from "prop-types";
+import { objectOf, string, func } from "prop-types";
 import { Link } from "react-router-dom";
 import { Table } from "antd";
-import { uniqBy, map, toArray } from "lodash";
+import { uniqBy, map, toArray, isEmpty } from "lodash";
 import * as router from "@/constants/routes";
 import * as Strings from "@/constants/strings";
 import NullScreen from "@/components/common/NullScreen";
@@ -15,15 +15,30 @@ import CustomPropTypes from "@/customPropTypes";
 const propTypes = {
   parties: objectOf(CustomPropTypes.party).isRequired,
   relationshipTypeHash: objectOf(string).isRequired,
+  handleSearch: func.isRequired,
+  sortField: string,
+  sortDir: string,
+};
+
+const defaultProps = {
+  sortField: null,
+  sortDir: null,
 };
 
 const columns = [
   {
     title: "Contact Name",
     dataIndex: "name",
-    render: (text, record) => (
-      <Link to={router.PARTY_PROFILE.dynamicRoute(record.key)}>{text}</Link>
-    ),
+    sortField: "party_name",
+    render: ([firstName = "", lastName = ""], record) => {
+      const comma = firstName ? ", " : "";
+      return (
+        <Link
+          to={router.PARTY_PROFILE.dynamicRoute(record.key)}
+        >{` ${lastName}${comma}${firstName}`}</Link>
+      );
+    },
+    sorter: true,
   },
   {
     title: "Role",
@@ -54,7 +69,9 @@ const transformRowData = (parties, relationshipTypeHash) =>
   toArray(parties).map((party) => ({
     key: party.party_guid,
     emptyField: Strings.EMPTY_FIELD,
-    name: party.name || Strings.EMPTY_FIELD,
+    name: party.first_name
+      ? [party.first_name, party.party_name]
+      : ["", party.name || Strings.EMPTY_FIELD],
     email: party.email === "Unknown" ? Strings.EMPTY_FIELD : party.email,
     phone: party.phone_no && party.phone_no !== "Unknown" ? party.phone_no : Strings.EMPTY_FIELD,
     role:
@@ -63,16 +80,37 @@ const transformRowData = (parties, relationshipTypeHash) =>
         : Strings.EMPTY_FIELD,
   }));
 
+const handleTableChange = (updateContactList) => (pagination, filters, sorter) => {
+  const params = isEmpty(sorter)
+    ? {
+        sort_field: undefined,
+        sort_dir: undefined,
+      }
+    : {
+        sort_field: sorter.column.sortField,
+        sort_dir: sorter.order.replace("end", ""),
+      };
+  updateContactList(params);
+};
+
+const applySortIndicator = (_columns, field, dir) =>
+  _columns.map((column) => ({
+    ...column,
+    sortOrder: column.sortField === field ? dir.concat("end") : false,
+  }));
+
 export const ContactList = (props) => (
   <Table
     align="left"
     pagination={false}
-    columns={columns}
+    columns={applySortIndicator(columns, props.sortField, props.sortDir)}
     dataSource={transformRowData(props.parties, props.relationshipTypeHash)}
     locale={{ emptyText: <NullScreen type="no-results" /> }}
+    onChange={handleTableChange(props.handleSearch)}
   />
 );
 
 ContactList.propTypes = propTypes;
+ContactList.defaultProps = defaultProps;
 
 export default ContactList;
--- python-backend/tests/factories.py
+++ python-backend/tests/factories.py
@@ -19,6 +19,7 @@ from app.api.mines.mine.models.mine_verified_status import MineVerifiedStatus
 from app.api.mines.status.models.mine_status import MineStatus
 from app.api.mines.tailings.models.tailings import MineTailingsStorageFacility
 from app.api.parties.party.models.party import Party
+from app.api.parties.party.models.address import Address
 from app.api.parties.party_appt.models.mine_party_appt import MinePartyAppointment
 from app.api.permits.permit.models.permit import Permit
 from app.api.permits.permit_amendment.models.permit_amendment import PermitAmendment
@@ -276,6 +277,18 @@ class MineVerifiedStatusFactory(BaseFactory):
     update_timestamp = TODAY
 
 
+class AddressFactory(BaseFactory):
+    class Meta:
+        model = Address
+
+    address_line_1 = factory.Faker('street_address')
+    suite_no = factory.Iterator([None, None, '123', '123'])
+    address_line_2 = factory.Iterator([None, 'Apt. 123', None, 'Apt. 123'])
+    city = factory.Faker('city')
+    sub_division_code = factory.LazyFunction(RandomSubDivisionCode)
+    post_code = factory.Faker('bothify', text='?#?#?#', letters='ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+
+
 class PartyFactory(BaseFactory):
     class Meta:
         model = Party
@@ -303,14 +316,9 @@ class PartyFactory(BaseFactory):
     effective_date = TODAY
     expiry_date = datetime.strptime('9999-12-31', '%Y-%m-%d')  # holdover till datetime refactor
     party_type_code = None
-    address_line_1 = factory.Faker('street_address')
-    suite_no = factory.Iterator([None, None, '123', '123'])
-    address_line_2 = factory.Iterator([None, 'Apt. 123', None, 'Apt. 123'])
-    city = factory.Faker('city')
-    sub_division_code = factory.LazyFunction(RandomSubDivisionCode)
-    post_code = factory.Faker('bothify', text='?#?#?#', letters='ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 
     mine_party_appt = []
+    address = factory.List([factory.SubFactory(AddressFactory) for _ in range(1)])
 
 
 class MinePartyAppointmentFactory(BaseFactory):
