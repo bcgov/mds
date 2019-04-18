c0847e35187f1ec8339d0889d4cf52c6a1431cfe
Jason Syrotuck <jasyrotuck@gmail.com>
2019-04-18 13:23:58 -0700

    small consistency changes.

--- python-backend/app/api/mines/subscription/models/subscription.py
+++ python-backend/app/api/mines/subscription/models/subscription.py
@@ -5,29 +5,31 @@ from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import validates
 
 from app.extensions import db
-from app.api.utils.models_mixins import  Base
+from app.api.utils.models_mixins import Base
 
 
-class Subscription( Base):
+class Subscription(Base):
     __tablename__ = 'subscription'
     subscription_id = db.Column(db.Integer, primary_key=True, server_default=FetchedValue())
-    mine_guid = db.Column(UUID(as_uuid=True),db.ForeignKey('mine.mine_guid'))
+    mine_guid = db.Column(UUID(as_uuid=True), db.ForeignKey('mine.mine_guid'))
     idir = db.Column(db.String, nullable=False)
 
+    mine = db.relationship("Mine", order_by='desc(Mine.mine_name)', lazy='joined')
+
     def __repr__(self):
-        return '<Subscription %r>' % self.user_id
+        return '<Subscription %r>' % self.subscription_id
 
     @classmethod
-    def find_subscription(cls, mine_guid):
+    def find_subscription_for_current_user_by_id(cls, mine_guid):
         user_name = User().get_user_username()
-        return Subscription.query.filter_by(mine_guid=uuid.UUID(mine_guid)).filter_by(
-            idir=user_name).first()
+        return Subscription.query.filter_by(mine_guid=mine_guid).filter_by(idir=user_name).first()
 
     @classmethod
-    def create_subscription(cls, mine_guid):
+    def create_for_current_user(cls, mine_guid, save=True):
         user_name = User().get_user_username()
-        subscription = cls(mine_guid=uuid.UUID(mine_guid), idir=user_name)
-        subscription.save()
+        subscription = cls(mine_guid=mine_guid, idir=user_name)
+        if save:
+            subscription.save(commit=False)
         return subscription
 
     @validates('mine_guid')
