4802230ba13cd3d6f0a6aab2092d2e1b7625bb7e
Jason Syrotuck <jasyrotuck@gmail.com>
2019-04-24 16:19:57 -0700

    missing network.

--- nris_api/app/config.py
+++ nris_api/app/config.py
@@ -10,7 +10,7 @@ class Config(object):
     DB_HOST = os.environ.get('DB_HOST', 'localhost')
     DB_USER = os.environ.get('DB_USER', 'user')
     DB_PASS = os.environ.get('DB_PASS', 'pass')
-    DB_PORT = os.environ.get('DB_PORT', 5555)
+    DB_PORT = os.environ.get('DB_PORT', 5432)
     DB_NAME = os.environ.get('DB_NAME', 'db_name')
     DB_URL = f"postgres://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
