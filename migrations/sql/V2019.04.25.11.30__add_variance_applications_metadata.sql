a503ed0e252b58076775df29cfeb66f28b625bd4
Nathan Calvank <nathan@freshworks.io>
2019-04-26 16:18:43 -0700

    Remove variance_no & make basic ORM/test changes
    Updated the models and tests to get the tests passing so that the
    database changes can be merged. The rest of the application-layer
    changes will be made in future PR(s) that focus on API functionality.

--- migrations/sql/V2019.04.25.11.30__add_variance_applications_metadata.sql
+++ migrations/sql/V2019.04.25.11.30__add_variance_applications_metadata.sql
@@ -1,40 +1,22 @@
 CREATE TABLE variance_application_status_code (
-    variance_application_status_code character varying(3)                   NOT NULL,
-    description                      character varying(100)                 NOT NULL,
-    active_ind                       boolean                  DEFAULT true  NOT NULL,
-    create_user                      character varying(60)                  NOT NULL,
-    create_timestamp                 timestamp with time zone DEFAULT now() NOT NULL,
-    update_user                      character varying(60)                  NOT NULL,
-    update_timestamp                 timestamp with time zone DEFAULT now() NOT NULL,
-
-    UNIQUE(variance_application_status_code)
+    variance_application_status_code character varying(3)                   NOT NULL PRIMARY KEY,
+    description                      character varying(100)                 NOT NULL            ,
+    active_ind                       boolean                  DEFAULT true  NOT NULL            ,
+    create_user                      character varying(60)                  NOT NULL            ,
+    create_timestamp                 timestamp with time zone DEFAULT now() NOT NULL            ,
+    update_user                      character varying(60)                  NOT NULL            ,
+    update_timestamp                 timestamp with time zone DEFAULT now() NOT NULL
 );
 
 ALTER TABLE variance_application_status_code OWNER TO mds;
 
 COMMENT ON TABLE variance_application_status_code IS 'The valid options for a variance / variance application.';
 
-INSERT INTO variance_application_status_code (
-    variance_application_status_code,
-    description,
-    create_user,
-    update_user
-)
-VALUES
-    ('REV', 'In Review', 'system-mds', 'system-mds'),
-    ('NAP', 'Not Applicable', 'system-mds', 'system-mds'),
-    ('APP', 'Approved', 'system-mds', 'system-mds'),
-    ('DEN', 'Denied', 'system-mds', 'system-mds');
-
-
 ALTER TABLE variance
 ADD COLUMN variance_application_status_code character varying(3) DEFAULT 'REV' NOT NULL,
 ADD COLUMN ohsc_ind                         boolean              DEFAULT FALSE NOT NULL,
 ADD COLUMN union_ind                        boolean              DEFAULT FALSE NOT NULL,
-ADD COLUMN inspector_id                     integer                                    ,
-ADD COLUMN variance_no                      integer                            NOT NULL;
-
-COMMENT ON COLUMN variance.variance_no IS 'A public, unique identifier for a mine variance.';
+ADD COLUMN inspector_id                     integer                                    ;
 
 ALTER TABLE variance
 ALTER COLUMN issue_date  DROP NOT NULL,
@@ -44,9 +26,6 @@ ALTER TABLE variance
 ALTER COLUMN issue_date    DROP DEFAULT,
 ALTER COLUMN expiry_date   DROP DEFAULT,
 ALTER COLUMN received_date DROP DEFAULT;
-
-ALTER TABLE variance
-ADD UNIQUE (mine_guid, variance_no);
 
 ALTER TABLE variance
 ADD CONSTRAINT variance_status_variance_application_status_code_fkey
@@ -79,27 +58,3 @@ ADD CONSTRAINT expiry_date_required_on_approved_variance_else_forbidden
         AND
         (NOT (expiry_date IS NOT NULL AND variance_application_status_code != 'APP') )
     );
-
-CREATE OR REPLACE FUNCTION add_incremented_variance_no() RETURNS trigger AS $$
-    DECLARE
-        max_mine_variance_no integer;
-    BEGIN
-        SELECT
-            CASE
-                WHEN (MAX(variance_no) IS NOT NULL)
-                THEN MAX(variance_no)
-                ELSE 0
-            END
-            FROM variance
-            WHERE mine_guid = NEW.mine_guid INTO max_mine_variance_no;
-
-        NEW.variance_no := max_mine_variance_no + 1;
-        RETURN NEW;
-    END;
-$$LANGUAGE plpgsql;
-
-CREATE TRIGGER new_variance_no
-  BEFORE INSERT
-  ON variance
-  FOR EACH ROW
-  EXECUTE PROCEDURE add_incremented_variance_no();
--- python-backend/app/api/mines/variances/models/variance.py
+++ python-backend/app/api/mines/variances/models/variance.py
@@ -6,6 +6,7 @@ from sqlalchemy.schema import FetchedValue
 from sqlalchemy.orm import validates
 from app.extensions import db
 
+from .variance_application_status_code import VarianceApplicationStatusCode
 from ....utils.models_mixins import AuditMixin, Base
 from ....documents.variances.models.variance import VarianceDocument
 
@@ -19,11 +20,18 @@ class Variance(AuditMixin, Base):
         nullable=False,
         server_default=FetchedValue())
     mine_guid = db.Column(UUID(as_uuid=True), db.ForeignKey('mine.mine_guid'), nullable=False)
+    variance_application_status_code = db.Column(
+        db.String,
+        db.ForeignKey('variance_application_status_code.variance_application_status_code'),
+        nullable=False,
+        server_default=FetchedValue())
+    ohsc_ind = db.Column(db.Boolean, nullable=False, server_default=FetchedValue())
+    union_ind = db.Column(db.Boolean, nullable=False, server_default=FetchedValue())
+    inspector_id = db.Column(db.Integer, db.ForeignKey('core_user.core_user_id'))
     note = db.Column(db.String, nullable=False, server_default=FetchedValue())
-    issue_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    received_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    expiry_date = db.Column(
-        db.DateTime, nullable=False, default=datetime.strptime('9999-12-31', '%Y-%m-%d'))
+    issue_date = db.Column(db.DateTime)
+    received_date = db.Column(db.DateTime, nullable=False)
+    expiry_date = db.Column(db.DateTime)
 
     documents = db.relationship('MineDocument', lazy='joined', secondary='variance_document_xref')
 
@@ -35,15 +43,21 @@ class Variance(AuditMixin, Base):
             cls,
             compliance_article_id,
             mine_guid,
+            received_date,
             # Optional Params
+            variance_application_status_code=None,
+            ohsc_ind=None,
+            union_ind=None,
             note=None,
             issue_date=None,
-            received_date=None,
             expiry_date=None,
             add_to_session=True):
         new_variance = cls(
             compliance_article_id=compliance_article_id,
             mine_guid=mine_guid,
+            variance_application_status_code=variance_application_status_code,
+            ohsc_ind=ohsc_ind,
+            union_ind=ohsc_ind,
             note=note,
             issue_date=issue_date,
             received_date=received_date,
