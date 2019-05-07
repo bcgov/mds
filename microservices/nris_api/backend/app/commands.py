2c6a5078b92ac7451ddfeba1608d3fe74cb9226f
Jason Syrotuck <jasyrotuck@gmail.com>
2019-05-07 13:01:54 -0700

    command to kick up oracle connection.

--- microservices/nris_api/backend/app/commands.py
+++ microservices/nris_api/backend/app/commands.py
@@ -7,6 +7,12 @@ from app.nris.models.test_model import Factorial
 def register_commands(app):
     @app.cli.command()
     def _test_oracle_db():
+
+        dsn_tns = cx_Oracle.makedsn(
+            Config.NRIS_DB_HOSTNAME, Config.NRIS_DB_PORT, service_name=Config.NRIS_DB_SERVICENAME)
+        oracle_db = cx_Oracle.connect(
+            user=Config.NRIS_DB_USER, password=Config.NRIS_DB_PASSWORD, dsn=dsn_tns)
+
         cursor = oracle_db.cursor()
 
         cursor.execute('select * from CORS.CORS_CV_ASSESSMENTS')
