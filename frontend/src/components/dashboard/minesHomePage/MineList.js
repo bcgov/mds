355965656f11ef422f130111cf5c530b6dd7fd3d
Gyanesh Mishra <gyan@freshworks.io>
2019-05-07 11:39:36 -0700

    process column data explicity before returning

--- frontend/src/components/dashboard/minesHomePage/MineList.js
+++ frontend/src/components/dashboard/minesHomePage/MineList.js
@@ -140,8 +140,8 @@ const columns = [
   },
 ];
 
-const transformRowData = (mines, mineIds, mineRegionHash, mineTenureHash, mineCommodityHash) =>
-  mineIds.map((id) => ({
+const transformRowData = (mines, mineIds, mineRegionHash, mineTenureHash, mineCommodityHash) => {
+  const result = mineIds.map((id) => ({
     key: id,
     emptyField: Strings.EMPTY_FIELD,
     mineName: mines[id].mine_name ? mines[id].mine_name : Strings.EMPTY_FIELD,
@@ -160,6 +160,8 @@ const transformRowData = (mines, mineIds, mineRegionHash, mineTenureHash, mineCo
       : Strings.EMPTY_FIELD,
     verified_status: mines[id].verified_status,
   }));
+  return result;
+};
 
 const handleTableChange = (updateMineList) => (pagination, filters, sorter) => {
   const params = isEmpty(sorter)
