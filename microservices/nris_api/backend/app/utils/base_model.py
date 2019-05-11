2b85865e58a7440f5c90835e75be7b420e13aeea
Jason Syrotuck <jasyrotuck@gmail.com>
2019-05-10 17:21:57 -0700

    try this

--- microservices/nris_api/backend/app/utils/base_model.py
+++ microservices/nris_api/backend/app/utils/base_model.py
@@ -1,10 +1,10 @@
-from app.extensions import db
+from app.extensions import migrate
 from flask import current_app
 
 
 class Base(db.Model):
     __abstract__ = True
     #get base metadata
-    metadata = current_app.extensions['migrate'].db.metadata
+    metadata = migrate.db.metadata
     #add custom metadata value
     metadata["schema"] = "nris"
