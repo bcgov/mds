1966fce29f46cf81b0fe27299edfd368d0e9ee1c
Gyanesh Mishra <gyan@freshworks.io>
2019-05-13 16:45:47 -0700

    refactor deploys

--- functional-tests/run_tests.sh
+++ functional-tests/run_tests.sh
@@ -1,18 +1,17 @@
 #! /bin/bash
 set -exv -o pipefail
 
-###FIREFOX HEADLESS IS THE ONLY BROWSER WHERE THE UPLOAD DOWNLOAD TEST CAN BE COMPLETED
+# Chrome Tests
 #./gradlew chromeTest -DchromeTest.single=CustomJUnitSpecRunner
 #./gradlew chromeHeadlessTest -DchromeHeadlessTest.single=CustomJUnitSpecRunner
-# ./gradlew firefoxHeadlessTest -DfirefoxHeadlessTest.single=CustomJUnitSpecRunner
-###Run both Core and MineSpace tests
-Xvfb :1 -screen 0 1024x768x24 &
-export DISPLAY=:1
-./gradlew firefoxHeadlessTest -DfirefoxHeadlessTest.single=CustomJUnitSpecRunner
-./gradlew firefoxHeadlessTest -DfirefoxHeadlessTest.single=CustomJUnitPublicSpecRunner
+
+###FIREFOX HEADLESS IS THE ONLY BROWSER WHERE THE UPLOAD DOWNLOAD TEST CAN BE COMPLETED
 
 # Config needed for firefoxHeadlessTest
 # For more info: https://github.com/BCDevOps/BDDStack/wiki/Running-firefoxHeadlessTest-in-CentOS
+Xvfb :1 -screen 0 1024x768x24 &
+export DISPLAY=:1
 
-# Firefox tests are currently disabled as the user base only uses chrome or IE
-#  ./gradlew clean -DfirefoxHeadlessTest.single=CustomJUnitSpecRunner firefoxHeadlessTest
+###Run both Core and MineSpace tests
+./gradlew firefoxHeadlessTest -DfirefoxHeadlessTest.single=CustomJUnitSpecRunner
+./gradlew firefoxHeadlessTest -DfirefoxHeadlessTest.single=CustomJUnitPublicSpecRunner
