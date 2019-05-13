0f46e058915e349f5c9208ba04fd4fe84ac1a0be
Jason Syrotuck <jasyrotuck@gmail.com>
2019-05-13 11:34:54 -0700

    comment about schema.

--- microservices/nris_api/backend/app/utils/base_model.py
+++ microservices/nris_api/backend/app/utils/base_model.py
@@ -13,7 +13,7 @@ Base = declarative_base(metadata=metadata, name='NRISBase')
 sqlalchemy.event.listen(
     Base.metadata, 'before_create',
     sqlalchemy.DDL("CREATE SCHEMA IF NOT EXISTS {schema}".format(schema=metadata.schema)))
-
+#TODO: flyway is doing this, but it won't hurt to have this year if nris db user has permission.
 
 class Base(db.Model):
     __abstract__ = True
