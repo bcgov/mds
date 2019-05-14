efd5dbf249922acb12b902f5fe471350a8e715f7
Gyanesh Mishra <gyan@freshworks.io>
2019-05-14 12:44:27 -0700

    update deploy template

--- openshift/tools/jenkins/build.sh
+++ openshift/tools/jenkins/build.sh
@@ -5,5 +5,4 @@ export OCP_PIPELINE_CLI_URL="https://raw.githubusercontent.com/BCDevOps/ocp-cd-p
 
 # Replace PR parameter with the latest Pull Request number that's making any changes
 # to the jenkins build/deploy.
-#curl -sSL "${OCP_PIPELINE_CLI_URL}" | bash -s build --config=config.groovy --pr=731
-curl -sSL "${OCP_PIPELINE_CLI_URL}" | bash -s build --config=config.groovy --pr=PR_NUMBER
+curl -sSL "${OCP_PIPELINE_CLI_URL}" | bash -s build --config=config.groovy --pr=731
