6505536024063ed048597d6418fe818b12824a0c
Gyanesh Mishra <gyan@freshworks.io>
2019-05-14 10:15:36 -0700

    update assemble

--- openshift/tools/jenkins/build.sh
+++ openshift/tools/jenkins/build.sh
@@ -3,4 +3,4 @@ set -e
 export OCP_PIPELINE_CLI_VERSION='0.0.6'
 export OCP_PIPELINE_CLI_URL="https://raw.githubusercontent.com/BCDevOps/ocp-cd-pipeline/v${OCP_PIPELINE_CLI_VERSION}/src/main/resources/pipeline-cli"
 
-curl -sSL "${OCP_PIPELINE_CLI_URL}" | bash -s build --config=config.groovy --pr=0
+curl -sSL "${OCP_PIPELINE_CLI_URL}" | bash -s build --config=config.groovy --pr=731
