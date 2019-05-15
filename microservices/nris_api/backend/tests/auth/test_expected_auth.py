76d4b8f9eab2cd42a1f174aa975cfb53abce2123
Jason Syrotuck <jasyrotuck@gmail.com>
2019-05-15 12:33:12 -0700

    adding testing and auth artifacts.

---
+++ microservices/nris_api/backend/tests/auth/test_expected_auth.py
@@ -0,0 +1,17 @@
+import pytest
+from app.api.utils.access_decorators import NRIS_VIEW
+
+from app.nris.resources.factorial_resource import FactorialResource
+
+
+@pytest.mark.parametrize("resource,method,expected_roles",
+                         [(FactorialResource, "get", [NRIS_VIEW])])
+def test_endpoint_auth(resource, method, expected_roles):
+    endpoint = getattr(resource, method, None)
+    assert endpoint != None, '{0} does not have a {1} method.'.format(resource, method.upper())
+
+    assigned_roles = getattr(endpoint, "required_roles", [])
+    assert set(expected_roles) == set(
+        assigned_roles
+    ), "For the {0} {1} method, expected the authorization flags {2}, but had {3} instead.".format(
+        resource.__name__, method.upper(), expected_roles, assigned_roles)
