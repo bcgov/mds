4a288f9259056295d023f40dae467def5d8f7373
justinmacaulay <justinkmacaulay@gmail.com>
2019-05-16 14:45:11 -0700

    migration and unused references

--- microservices/nris_api/backend/app/commands.py
+++ microservices/nris_api/backend/app/commands.py
@@ -2,13 +2,11 @@ import click
 
 from app.extensions import db, oracle_db
 from app.nris.models.test_model import Factorial
-from app.nris.etl.models.nris_raw_data import NRISRawData
 
 
 def register_commands(app):
     @app.cli.command()
     def _test_oracle_db():
-        NRISRawData = None
         cursor = oracle_db.cursor()
 
         cursor.execute('select * from CORS.CORS_CV_ASSESSMENTS')
