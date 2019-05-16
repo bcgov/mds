6730c75347eb0d76fb2f698d89a8d9d6ee3e6c23
Nathan Calvank <nathan@freshworks.io>
2019-05-16 11:04:06 -0700

    Add not null constraint to columns

--- migrations/sql/V2019.05.15.13.58__move_union_ind_and_hsrc_ind_to_mine.sql
+++ migrations/sql/V2019.05.15.13.58__move_union_ind_and_hsrc_ind_to_mine.sql
@@ -3,8 +3,8 @@ DROP COLUMN union_ind,
 DROP COLUMN ohsc_ind;
 
 ALTER TABLE mine
-ADD COLUMN union_ind boolean,
-ADD COLUMN ohsc_ind boolean;
+ADD COLUMN union_ind boolean DEFAULT FALSE NOT NULL,
+ADD COLUMN ohsc_ind  boolean DEFAULT FALSE NOT NULL;
 
 COMMENT ON COLUMN mine.union_ind IS 'Indicates if the mine has a union.';
 COMMENT ON COLUMN mine.ohsc_ind IS 'Indicates if the mine has an OHSC.';
