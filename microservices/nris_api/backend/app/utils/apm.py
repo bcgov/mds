80bf6d4649fbb5e4a52aafcd124ebb0ef38d4bad
Paul Dittaro <pdittaro@gmail.com>
2019-05-22 12:08:18 -0700

    Super removing instant client zip

--- microservices/nris_api/backend/app/utils/apm.py
+++ microservices/nris_api/backend/app/utils/apm.py
@@ -6,15 +6,13 @@ from flask import current_app
 def register_apm(func):
     """This function wraps a passed function with a call to the app's registered Elastic APM instance
 
-    Arguments:
-        func {Any} -- Function to be wrapped by calling 
-
-    Raises:
-        e: Client connection exception.
-
-    Returns:
-        func -- Wrapped function
+    :param func: Function to be wrapped by calling 
+    :type func: func
+    :raises e: Client connection exception.
+    :return: Wrapped function
+    :rtype: func
     """
+
     def wrapper(*args, **kwargs):
         client = None
         if current_app:
