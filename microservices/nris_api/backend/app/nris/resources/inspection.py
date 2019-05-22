35bc7a329394884283f91eb9d5801b749e8c46e2
Jason S <jasyrotuck@gmail.com>
2019-05-22 14:39:46 -0700

    returns count.

--- microservices/nris_api/backend/app/nris/resources/inspection.py
+++ microservices/nris_api/backend/app/nris/resources/inspection.py
@@ -8,10 +8,14 @@ from app.extensions import api, db
 from app.nris.utils.logger import get_logger
 from app.nris.utils.access_decorators import requires_role_nris_view
 
+from app.nris.models.inspection import Inspection
+
 
 @api.route('/inspection')
 class InspectionResource(Resource):
     @api.doc(params={'mine_guid': 'Core mine_guid to filter'})
     #@requires_role_nris_view
     def get(self):
-        return {}
+        inspections = Inspection.query.all()
+
+        return len(inspections)
