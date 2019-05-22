bad8a075bb1a2e7856df085fb18f44a01877a43c
Gyanesh Mishra <gyan@freshworks.io>
2019-05-22 13:37:06 -0700

    test environment templating

--- migrations/scripts/run_migrations.sh
+++ migrations/scripts/run_migrations.sh
@@ -3,6 +3,16 @@
 # Change directory to Flyway home
 cd $FLYWAY_HOME
 
+# Substitute environment variables in template files
+export DOLAR='$'
+if ls $FLYWAY_HOME/sql/*.tmpl > /dev/null 2>&1 ; then
+echo "---> substituting environment variables in templates"
+for file in $FLYWAY_HOME/sql/*.tmpl; do
+    target_file="$FLYWAY_HOME/sql/$(basename "$file" .tmpl)"
+    envsubst < "$file" > "$target_file"
+done
+fi
+
 # Run migrations for mds database
 ./flyway migrate
 
