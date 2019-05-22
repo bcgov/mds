43b57e1676e15ecccbda0915609f728649390c15
Jason S <jasyrotuck@gmail.com>
2019-05-22 15:54:03 -0700

    remove imports.

--- microservices/nris_api/backend/app/nris/resources/inspection.py
+++ microservices/nris_api/backend/app/nris/resources/inspection.py
@@ -1,11 +1,9 @@
 from flask import request
+from flask_restplus import Resource
 
-from flask_restplus import Resource, Namespace, reqparse, fields
-from datetime import datetime
 from werkzeug.exceptions import BadRequest, NotFound, InternalServerError
 
 from app.extensions import api, db
-from app.nris.utils.logger import get_logger
 from app.nris.utils.access_decorators import requires_role_nris_view
 
 from app.nris.models.inspection import Inspection, INSPECTION_RESPONSE_MODEL
