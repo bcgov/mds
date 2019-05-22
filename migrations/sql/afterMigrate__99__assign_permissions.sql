a93d93ad6dca858c62e03f0c7a054bcde2452f36
Gyanesh Mishra <gyan@freshworks.io>
2019-05-22 15:30:14 -0700

    update permission template

--- migrations/sql/afterMigrate__99__assign_permissions.sql
+++ migrations/sql/afterMigrate__99__assign_permissions.sql
@@ -1,8 +1,24 @@
+/**
+Allow MDS user to CRUD anything on the mds schema
+**/
 GRANT ALL PRIVILEGES ON DATABASE mds TO mds;
 GRANT ALL PRIVILEGES ON SCHEMA public TO mds;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO mds;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO mds;
 
+/**
+Allow NRIS user to CRUD anything on the nris schema
+**/
 GRANT ALL PRIVILEGES ON SCHEMA nris TO nris;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA nris TO nris;
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA nris TO nris;
+
+/**
+Allow logstash user to READ anything on the mds schema
+**/
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO logstash;
+
+/**
+Allow metabase user to READ anything on the mds schema
+**/
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO metabase;
