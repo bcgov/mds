d697edf36c082af96a1050e320bc278b9b421eb8
Ryan Rondeau <nayr974@users.noreply.github.com>
2019-05-22 14:26:25 -0700

    Update inspection.py

--- microservices/nris_api/backend/app/nris/models/inspection.py
+++ microservices/nris_api/backend/app/nris/models/inspection.py
@@ -1,10 +1,26 @@
 from datetime import datetime
-from app.extensions import db
+from app.extensions import db, api
+from flask_restplus import fields
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 from app.nris.utils.base_model import Base
 
 from app.nris.models.inspection_status import InspectionStatus
+
+INSPECTION_RESPONSE_MODEL = api.model(
+    'Inspection', {
+        'external_id': fields.Integer,
+        'inspection_date': fields.DateTime,
+        'completed_date': fields.DateTime,
+        'inspection_status_code': fields.String,
+        'business_area': fields.String,
+        'mine_no': fields.String,
+        'inspector_idir': fields.String,
+        'inspection_introduction': fields.String,
+        'inspection_preamble': fields.String,
+        'inspection_closing': fields.String,
+        'officer_notes': fields.String
+    })
 
 
 class Inspection(Base):
@@ -18,13 +34,13 @@ class Inspection(Base):
     inspection_status_code = association_proxy('inspection_status_code', 'inspection_status_code')
     business_area = db.Column(db.String(256))
     mine_no = db.Column(db.String(64))
-    documents = db.relationship(
-        'Document', lazy='selectin', secondary='nris.inspection_document_xref')
     inspector_idir = db.Column(db.String(256))
     inspection_introduction = db.Column(db.String())
     inspection_preamble = db.Column(db.String())
     inspection_closing = db.Column(db.String())
     officer_notes = db.Column(db.String())
+    documents = db.relationship(
+        'Document', lazy='selectin', secondary='nris.inspection_document_xref')
 
     def __repr__(self):
         return f'<Inspection inspection_id={self.inspection_status_code} inspection_status_description={self.inspection_status_description}>'
