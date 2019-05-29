4480e44ecbb7b5faf0a3f9ebdbaf21f8d6a00a17
Paul Dittaro <pdittaro@gmail.com>
2019-05-29 13:24:52 -0700

    Implementing lock on NRIS ETL scheduled job using redis

--- microservices/nris_api/backend/app/nris/scheduled_jobs/nris_jobs.py
+++ microservices/nris_api/backend/app/nris/scheduled_jobs/nris_jobs.py
@@ -1,7 +1,7 @@
-from app.extensions import db, sched
+from app.extensions import db, sched, cache
 from app.nris.utils.apm import register_apm
 from app.nris.etl.nris_etl import import_nris_xml, clean_nris_xml_import, etl_nris_data, clean_nris_data
-from app.constants import ETL, TIMEOUT_24_HOURS
+from app.constants import ETL, TIMEOUT_24_HOURS, NRIS_JOB_PREFIX, NRIS_ETL_JOB
 from flask import current_app
 from random import randint
 from time import sleep
@@ -12,35 +12,40 @@ import cx_Oracle
 
 
 def _schedule_nris_etl_jobs(app):
-    app.apscheduler.add_job(func=run_nightly_nris_etl, trigger='cron', id='ETL', hour=11, minute=0)
+    app.apscheduler.add_job(func=run_nightly_nris_etl,
+                            trigger='cron', id='ETL', hour=11, minute=0)
 
 
 @register_apm
 def run_nightly_nris_etl():
-    with sched.app.app_context():
-        """This nightly job initiates the ETL from NRIS into our app domain."""
+    """This nightly job initiates the ETL from NRIS into our app domain."""
 
-        sched.app.logger.info('Starting ETL process')
-        # TODO: More detailed logging
+    job_running = cache.get(NRIS_JOB_PREFIX + NRIS_ETL_JOB)
+    if job_running is None:
+        cache.set(NRIS_JOB_PREFIX + NRIS_ETL_JOB,
+                  'True', timeout=TIMEOUT_24_HOURS)
 
-        # Initiate Oracle ETL
-        try:
-            clean_nris_xml_import()
-            import_nris_xml()
-            sched.app.logger.info('XML Import completed')
-            # TODO: Insert update into status table
+        with sched.app.app_context():
 
-        except cx_Oracle.DatabaseError as e:
-            sched.app.logger.error("Error establishing connection to NRIS database: " + str(e))
-            return
-        except Exception as e:
-            sched.app.logger.error("Unexpected error with NRIS XML import: " + str(e))
-            raise
+            sched.app.logger.info('Starting ETL process')
+            # TODO: More detailed logging
 
-        try:
-            clean_nris_data()
-            etl_nris_data()
-            sched.app.logger.info('NRIS ETL Completed!')
+            # Initiate Oracle ETL
+            try:
+                clean_nris_xml_import()
+                import_nris_xml()
+                sched.app.logger.info('XML Import completed')
+                # TODO: Insert update into status table
 
-        except:
-            sched.app.logger.error("Unexpected error with NRIS ETL")
+                clean_nris_data()
+                etl_nris_data()
+                sched.app.logger.info('NRIS ETL Completed!')
+
+            except cx_Oracle.DatabaseError as e:
+                sched.app.logger.error(
+                    "Error establishing connection to NRIS database: " + str(e))
+            except Exception as e:
+                sched.app.logger.error(
+                    "Unexpected error with NRIS XML import: " + str(e))
+            finally:
+                cache.delete(NRIS_JOB_PREFIX + NRIS_ETL_JOB)
