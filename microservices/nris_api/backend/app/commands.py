6cdb340cd00e76d34b3442f078c76165489bcf59
Paul Dittaro <pdittaro@gmail.com>
2019-05-29 10:45:09 -0700

    Trimmed down requirements.txt

--- microservices/nris_api/backend/app/commands.py
+++ microservices/nris_api/backend/app/commands.py
@@ -2,26 +2,34 @@ import click
 
 from app.extensions import db, sched
 from app.nris.models.nris_raw_data import NRISRawData
-from app.nris.etl.nris_etl import import_nris_xml, etl_nris_data, clean_nris_data, clean_nris_xml_import
+from app.nris.etl.nris_etl import import_nris_xml, etl_nris_data, clean_nris_etl_data, clean_nris_xml_import
 from app.nris.scheduled_jobs import nris_jobs
 
 
 def register_commands(app):
     @app.cli.command()
-    def _import_nris_raw_data():
+    def import_nris_raw_data():
+        print("Importing Raw Data from NRIS...")
         import_nris_xml()
+        print("Import complete")
 
     @app.cli.command()
-    def _clean_nris_data():
-        clean_nris_data()
+    def clean_nris_data():
+        print("Cleaning NRIS Data...")
+        clean_nris_etl_data()
+        print("Cleanup complete")
 
     @app.cli.command()
-    def _clean_nris_raw_data():
+    def clean_nris_raw_data():
+        print("Cleaning NRIS Raw Data...")
         clean_nris_xml_import()
+        print("Cleanup complete")
 
     @app.cli.command()
-    def _run_nris_etl():
+    def run_nris_etl():
+        print("Running NRIS ETL...")
         etl_nris_data()
+        print("NRIS ETL complete")
 
     @sched.app.cli.command()
     def run_nightly_nris_etl():
--- microservices/nris_api/backend/app/nris/scheduled_jobs/nris_jobs.py
+++ microservices/nris_api/backend/app/nris/scheduled_jobs/nris_jobs.py
@@ -1,6 +1,6 @@
 from app.extensions import db, sched
 from app.nris.utils.apm import register_apm
-from app.nris.etl.nris_etl import import_nris_xml, clean_nris_xml_import, etl_nris_data, clean_nris_data
+from app.nris.etl.nris_etl import import_nris_xml, clean_nris_xml_import, etl_nris_data, clean_nris_etl_data
 from app.constants import ETL, TIMEOUT_24_HOURS
 from flask import current_app
 from random import randint
@@ -12,7 +12,8 @@ import cx_Oracle
 
 
 def _schedule_nris_etl_jobs(app):
-    app.apscheduler.add_job(func=run_nightly_nris_etl, trigger='cron', id='ETL', hour=11, minute=0)
+    app.apscheduler.add_job(func=run_nightly_nris_etl,
+                            trigger='cron', id='ETL', hour=11, minute=0)
 
 
 @register_apm
@@ -31,14 +32,16 @@ def run_nightly_nris_etl():
             # TODO: Insert update into status table
 
         except cx_Oracle.DatabaseError as e:
-            sched.app.logger.error("Error establishing connection to NRIS database: " + str(e))
+            sched.app.logger.error(
+                "Error establishing connection to NRIS database: " + str(e))
             return
         except Exception as e:
-            sched.app.logger.error("Unexpected error with NRIS XML import: " + str(e))
+            sched.app.logger.error(
+                "Unexpected error with NRIS XML import: " + str(e))
             raise
 
         try:
-            clean_nris_data()
+            clean_nris_etl_data()
             etl_nris_data()
             sched.app.logger.info('NRIS ETL Completed!')
 
