c79ea19fdc1c0e58765a44ae6ac5323bcb2b1dd6
justinmacaulay <justinkmacaulay@gmail.com>
2019-06-04 09:47:27 -0700

    fixed back end tests and removed constants.

--- python-backend/app/api/mines/status/resources/status.py
+++ python-backend/app/api/mines/status/resources/status.py
@@ -1,20 +1,18 @@
 from flask_restplus import Resource
-from flask import current_app
 from werkzeug.exceptions import BadRequest, NotFound
+
 from ..models.mine_status import MineStatus
 from app.api.mines.status.models.mine_status_xref import MineStatusXref
 from app.extensions import api
 from ....utils.access_decorators import requires_role_mine_view
 from ....utils.resources_mixins import UserMixin, ErrorMixin
 from app.api.mines.mine_api_models import MINE_STATUS_CODE_MODEL
-from ....constants import MINE_STATUS_OPTIONS
 
 
 class MineStatusListResource(Resource, UserMixin):
     @api.marshal_with(MINE_STATUS_CODE_MODEL, envelope='records')
     @requires_role_mine_view
     def get(self):
-        current_app.logger.info(MINE_STATUS_OPTIONS)
         return MineStatusXref.active_status_codes()
 
 
