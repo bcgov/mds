c425a7e2c66110c9852fde7f5ac2934427c6d0ae
Jason S <jasyrotuck@gmail.com>
2019-06-04 13:32:24 -0700

    return if found in cache.

--- python-backend/app/api/mines/compliance/resources/compliance.py
+++ python-backend/app/api/mines/compliance/resources/compliance.py
@@ -75,22 +75,22 @@ class MineComplianceSummaryResource(Resource, UserMixin, ErrorMixin):
     @requires_role_mine_view
     def get(self, mine_no):
         result = cache.get(NRIS_COMPLIANCE_DATA(mine_no))
+        if result is None: 
+            mine = Mine.find_by_mine_no_or_guid(mine_no)
+            if not mine:
+                raise NotFound("No mine record in CORE.")     
 
-        mine = Mine.find_by_mine_no_or_guid(mine_no)
-        if not mine:
-            raise NotFound("No mine record in CORE.")     
+            try:
+                raw_data = NRIS_API_service._get_NRIS_data_by_mine(request.headers.get('Authorization'),
+                                                                mine_no)
+            except requests.exceptions.Timeout:
+                current_app.logger.error(f'NRIS_API Connection Timeout <mine_no={mine_no}>')
+                raise
+            except requests.exceptions.HTTPError as e :
+                current_app.logger.error(f'NRIS_API Connection HTTPError <mine_no={mine_no}>, {str(e)}')
+                raise
 
-        try:
-            raw_data = NRIS_API_service._get_NRIS_data_by_mine(request.headers.get('Authorization'),
-                                                               mine_no)
-        except requests.exceptions.Timeout:
-            current_app.logger.error(f'NRIS_API Connection Timeout <mine_no={mine_no}>')
-            raise
-        except requests.exceptions.HTTPError as e :
-            current_app.logger.error(f'NRIS_API Connection HTTPError <mine_no={mine_no}>, {str(e)}')
-            raise
-
-        result = NRIS_API_service._process_NRIS_data(raw_data)
-        cache.set(NRIS_COMPLIANCE_DATA(mine_no), result, timeout=TIMEOUT_24_HOURS)
-        
-        return result
+            result = NRIS_API_service._process_NRIS_data(raw_data)
+            cache.set(NRIS_COMPLIANCE_DATA(mine_no), result, timeout=TIMEOUT_24_HOURS)
+    
+    return result
