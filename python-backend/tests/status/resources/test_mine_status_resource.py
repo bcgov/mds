3d78fd66dfb215cd6a7c984e9747bc205e47d2f3
JustinMacaulay <justinkmacaulay@gmail.com>
2019-06-06 11:54:29 -0700

    MDS-1803 refactor mine status (#768)

--- python-backend/tests/status/resources/test_mine_status_resource.py
+++ python-backend/tests/status/resources/test_mine_status_resource.py
@@ -1,13 +1,11 @@
 import json, uuid
 
-from app.api.constants import MINE_STATUS_OPTIONS
-
 
 # GET
 def test_get_mine_status_option(test_client, db_session, auth_headers):
     get_resp = test_client.get('/mines/status', headers=auth_headers['full_auth_header'])
     get_data = json.loads(get_resp.data.decode())
-    assert get_data == {'options': MINE_STATUS_OPTIONS}
+    assert len(get_data['records']) == 14
     assert get_resp.status_code == 200
 
 
@@ -15,5 +13,5 @@ def test_get_mine_status_not_found(test_client, db_session, auth_headers):
     get_resp = test_client.get(
         '/mines/status/' + str(uuid.uuid4()), headers=auth_headers['full_auth_header'])
     get_data = json.loads(get_resp.data.decode())
-    assert get_data['error']['message'] == 'Mine Status not found'
+    assert get_data['message'] is not None
     assert get_resp.status_code == 404
