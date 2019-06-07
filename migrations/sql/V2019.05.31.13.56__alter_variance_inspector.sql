215df7cba6f1afce96170570bbf91057a0c48d48
Ryan Rondeau <nayr974@users.noreply.github.com>
2019-06-07 16:58:31 -0700

    Update V2019.05.31.13.56__alter_variance_inspector.sql

--- migrations/sql/V2019.05.31.13.56__alter_variance_inspector.sql
+++ migrations/sql/V2019.05.31.13.56__alter_variance_inspector.sql
@@ -4,5 +4,7 @@ alter table variance
 ADD CONSTRAINT variance_inspector_party_fkey
     FOREIGN KEY (inspector_party_guid) REFERENCES party(party_guid) DEFERRABLE INITIALLY deferred;
 
+
+
 alter table variance
-drop constraint inspector_required_on_reviewed_application;
+drop constraint if exists inspector_required_on_reviewed_application;
