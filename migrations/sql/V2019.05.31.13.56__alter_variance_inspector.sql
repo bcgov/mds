05a1965ab20f1b4ad37cd7f7b815dbe1d524b5e9
Ryan Rondeau <nayr974@users.noreply.github.com>
2019-06-07 16:52:32 -0700

    Update V2019.05.31.13.56__alter_variance_inspector.sql

--- migrations/sql/V2019.05.31.13.56__alter_variance_inspector.sql
+++ migrations/sql/V2019.05.31.13.56__alter_variance_inspector.sql
@@ -6,9 +6,3 @@ ADD CONSTRAINT variance_inspector_party_fkey
 
 alter table variance
 drop constraint inspector_required_on_reviewed_application;
-
-alter table variance 
-ADD CONSTRAINT inspector_required_on_reviewed_application
-    CHECK (NOT (inspector_party_guid IS NULL AND (
-              variance_application_status_code = 'APP' OR variance_application_status_code = 'DEN'
-          ) ) );
