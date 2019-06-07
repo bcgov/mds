b20831197c66c7cc5ab86277661287f95c4293cc
Nathan Calvank <nathan@freshworks.io>
2019-06-07 16:09:46 -0700

    Use verbose method name

--- python-backend/tests/variances/resources/test_variance_application_status_code_resource.py
+++ python-backend/tests/variances/resources/test_variance_application_status_code_resource.py
@@ -9,7 +9,7 @@ from app.api.variances.models.variance_application_status_code import VarianceAp
 class TestGetVarianceApplicationStatusCode:
     """GET /variances/status-codes"""
 
-    def test_success(self, test_client, db_session, auth_headers):
+    def test_get_variance_application_status_codes(self, test_client, db_session, auth_headers):
         """Should return the correct number of records with a 200 response code"""
         get_resp = test_client.get(f'/variances/status-codes', headers=auth_headers['full_auth_header'])
         get_data = json.loads(get_resp.data.decode())
