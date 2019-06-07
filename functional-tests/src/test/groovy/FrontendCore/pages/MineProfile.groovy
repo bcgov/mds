56751004e24f2565e26b7e86646f47273e5578ca
thegentlemanphysicist <jon@freshworks.io>
2019-06-07 11:51:00 -0700

    Partially cleaned up tests 3

--- functional-tests/src/test/groovy/FrontendCore/pages/MineProfile.groovy
+++ functional-tests/src/test/groovy/FrontendCore/pages/MineProfile.groovy
@@ -28,27 +28,9 @@ class MineProfilePage extends Page {
         //Footer
         footer (wait:true) {$("div", class: "footer")}
 
-        // //Permit Tab objects
-        // newPermitButton (wait:true) {$("button", text: "Add a New Permit")}
-        newPermitForm { module Form_CreatePermit }
-        // //TODO: Remove magic string
-        // permitRow (wait:true) {$("tr").has("div", title: "Permit No.",text: "M-66666")}
-        // permitTitle (wait:true) {$("div", title: "Permit No.",text: "M-66666")}
-        // addEditButton (wait:true) {$("button").has("div", text: "Add/Edit")}
-        // editPermitStatusButton (wait:true) {$("button", text: "Edit permit status")}
-        // addAmendmentButton (wait:true) {$("button", text: "Add permit amendment")}
-        // editPermitFormStatusDropdown (wait:true) {$("div", id: "permit_status_code")}
-        // closedDropdownOption (wait:true) {$("li", text: "Closed")}
-        // submitEditPermitStatus (wait:true) {$("button", type: "submit")}
-        // amendmentDescriptions (wait:true) {$("div", title: "Description")}
-        // amendmentDescriptionSpecific (wait:true) {$("div", title: "Description").has(text:"A fancy description" )}
-        // openFileModalButton (wait:true) {$("button").has("img", alt: "Edit")}
-        // uploadField (wait:true) {$("input.filepond--browser")}
-        // uploadCompleteMessage (wait:true) {$("span.filepond--file-status-main", text:"Upload complete")}
-        // editPermitFileButton (wait:true) {$("button", text: "Edit initial permit for M-66666")}
-
+        //Permit Tab objects
+        newPermitForm { module Form_Create_And_Edit_Permit }
         downloadTestFileLink (wait:true) {$("a", text: Const.TEST_FILE_NAME)}
-        
 
     }
 
