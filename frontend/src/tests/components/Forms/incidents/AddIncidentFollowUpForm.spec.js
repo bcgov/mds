5884d28661a2a90a7c0fe33fbc66add5c5cc6662
Ryan Rondeau <nayr974@users.noreply.github.com>
2019-06-10 16:56:38 -0700

    Add Incident Form / Modal Snapshots

---
+++ frontend/src/tests/components/Forms/incidents/AddIncidentFollowUpForm.spec.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { AddIncidentFollowUpForm } from "@/components/Forms/incidents/AddIncidentFollowUpForm";
+
+const dispatchProps = {};
+const props = {};
+
+const setupDispatchProps = () => {
+  dispatchProps.handleSubmit = jest.fn();
+};
+
+const setupProps = () => {};
+
+beforeEach(() => {
+  setupDispatchProps();
+  setupProps();
+});
+
+describe("AddIncidentFollowUpForm", () => {
+  it("renders properly", () => {
+    const component = shallow(<AddIncidentFollowUpForm {...dispatchProps} {...props} />);
+    expect(component).toMatchSnapshot();
+  });
+});
