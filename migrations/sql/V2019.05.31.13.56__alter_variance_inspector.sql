1b6202fb99b2cab5dd0e9644bc5018a821860fab
Ryan Rondeau <nayr974@users.noreply.github.com>
2019-06-11 14:59:33 -0700

    Revert "Merge branch 'feature/MDS-1967-mwsl-nris-inspector-endpoint' into feature/MDS-1709-DO-Changes"
    This reverts commit 09accf3b251973886363d43dac5810cdc59f483a, reversing
    changes made to 82c96cefa86936a46410f0c7abc069ef67da446a.

--- migrations/sql/V2019.05.31.13.56__alter_variance_inspector.sql
+++ migrations/sql/V2019.05.31.13.56__alter_variance_inspector.sql
@@ -5,5 +5,6 @@ ADD CONSTRAINT variance_inspector_party_fkey
     FOREIGN KEY (inspector_party_guid) REFERENCES party(party_guid) DEFERRABLE INITIALLY deferred;
 
 
+
 alter table variance
 drop constraint if exists inspector_required_on_reviewed_application;
