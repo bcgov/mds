7ac61b0b73b1d310db7dcf2d830b5746b851fe4a
Nathan Calvank <nathan@freshworks.io>
2019-06-13 10:01:18 -0700

    MDS-2030 Feature/ Update Variances to accommodate user-requested workflow (#795)
    * Database changes & associated factory updates
    
    * Add category codes to variance document responses
    
    * Update variance document association process
    
    * Add parties_notified_ind to API
    
    * Add and update tests to cover new functionality
    
    * Add a bit of whitespace padding

--- python-backend/app/api/variances/response_models.py
+++ python-backend/app/api/variances/response_models.py
@@ -9,8 +9,13 @@ MINE_DOCUMENT_MODEL = api.model(
         'document_name': fields.String,
     })
 
-VARIANCE_DOCUMENT = api.inherit('VarianceDocumentModel', MINE_DOCUMENT_MODEL,
-                                {'created_at': fields.Date})
+VARIANCE_DOCUMENT = api.inherit(
+    'VarianceDocumentModel',
+    MINE_DOCUMENT_MODEL,
+    {
+        'created_at': fields.Date,
+        'variance_document_category_code': fields.String
+    })
 
 VARIANCE = api.model(
     'Variance', {
@@ -21,6 +26,7 @@ VARIANCE = api.model(
         'applicant_guid': fields.String,
         'inspector_party_guid': fields.String,
         'note': fields.String,
+        'parties_notified_ind': fields.Boolean,
         'issue_date': fields.Date,
         'received_date': fields.Date,
         'expiry_date': fields.Date,
@@ -41,5 +47,10 @@ PAGINATED_VARIANCE_LIST = api.inherit('VarianceList', PAGINATED_LIST, {
 
 VARIANCE_APPLICATION_STATUS_CODE = api.model('VarianceApplicationStatusCode', {
     'variance_application_status_code': fields.String,
+    'description': fields.String
+})
+
+VARIANCE_DOCUMENT_CATEGORY_CODE = api.model('VarianceDocumentCategoryCode', {
+    'variance_document_category_code': fields.String,
     'description': fields.String
 })
