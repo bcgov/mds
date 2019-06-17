69ee89e770134d98beaf8647150be49d08739851
justinmacaulay <justinkmacaulay@gmail.com>
2019-06-17 16:21:11 -0700

    changed to using an xref instead of a mine_documents table.

--- migrations/sql/V2019.06.12.15.12__create_mine_incident_document_and_type.sql
+++ migrations/sql/V2019.06.12.15.12__create_mine_incident_document_and_type.sql
@@ -10,23 +10,20 @@ CREATE TABLE IF NOT EXISTS mine_incident_document_type_code (
 
 ALTER TABLE mine_incident_document_type_code OWNER TO mds;
 
-CREATE TABLE IF NOT EXISTS mine_incident_document(
-    mine_incident_document_id        serial                                  NOT NULL PRIMARY KEY,
-    mine_incident_document_guid      uuid                                    NOT NULL            ,
-    mine_incident_id                 integer                                 NOT NULL            ,
-    mine_incident_document_type_code varchar(3)                              NOT NULL            ,
-    document_name                    varchar(50)                             NOT NULL            ,
-    document_manager_guid            uuid                                    NOT NULL            ,
-    active_ind                       boolean                  DEFAULT true   NOT NULL            ,
-    create_user                      varchar(60)                             NOT NULL            ,
-    create_timestamp                 timestamp with time zone DEFAULT now()  NOT NULL            ,
-    update_user                      varchar(60)                             NOT NULL            ,
-    update_timestamp                 timestamp with time zone DEFAULT now()  NOT NULL            ,
-    
+CREATE TABLE IF NOT EXISTS mine_incident_document_xref
+(
+    mine_incident_document_xref_guid uuid    DEFAULT gen_random_uuid() NOT NULL,
+    mine_document_guid               uuid                              NOT NULL,
+    mine_incident_id                 integer                           NOT NULL,
+    mine_incident_document_type_code varchar(3)                        NOT NULL,
+
+    FOREIGN KEY (mine_document_guid) REFERENCES mine_document(mine_document_guid) DEFERRABLE INITIALLY DEFERRED,
     FOREIGN KEY (mine_incident_id) REFERENCES mine_incident(mine_incident_id) DEFERRABLE INITIALLY DEFERRED,
     FOREIGN KEY (mine_incident_document_type_code) REFERENCES mine_incident_document_type_code(mine_incident_document_type_code) DEFERRABLE INITIALLY DEFERRED
 );
-ALTER TABLE mine_incident_document OWNER TO mds;
+
+ALTER TABLE mine_incident_document_xref OWNER TO mds;
+
 
 
 
