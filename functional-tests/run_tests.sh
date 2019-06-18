47f7525b07ba510becc24869f5e1e6f545c374f9
Gyanesh Mishra <gyan@freshworks.io>
2019-06-18 14:20:07 -0700

    update firefoxtestrun

--- functional-tests/run_tests.sh
+++ functional-tests/run_tests.sh
@@ -13,5 +13,5 @@ Xvfb :1 -screen 0 1920x1080x24 &
 export DISPLAY=:1
 
 ###Run both Core and MineSpace tests
-./gradlew firefoxHeadlessTest -DfirefoxHeadlessTest.single=CustomJUnitSpecRunner
-./gradlew firefoxHeadlessTest -DfirefoxHeadlessTest.single=CustomJUnitPublicSpecRunner
+./gradlew firefoxTest -DfirefoxTest.single=CustomJUnitSpecRunner
+./gradlew firefoxTest -DfirefoxTest.single=CustomJUnitPublicSpecRunner
