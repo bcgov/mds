5e8d5074425a50df990e4ed04bf61b209d196d49
Paul Dittaro <pdittaro@gmail.com>
2019-06-19 15:58:26 -0700

    Adding notifications on incident create

--- frontend/src/actionCreators/mineActionCreator.js
+++ frontend/src/actionCreators/mineActionCreator.js
@@ -390,6 +390,10 @@ export const createMineIncident = (mine_guid, payload) => (dispatch) => {
   return CustomAxios()
     .post(`${ENVIRONMENT.apiUrl}${API.MINE_INCIDENTS(mine_guid)}`, payload, createRequestHeader())
     .then((response) => {
+      notification.success({
+        message: "Successfully created incident.",
+        duration: 10,
+      });
       dispatch(success(reducerTypes.CREATE_MINE_INCIDENT));
       return response;
     })
@@ -419,6 +423,10 @@ export const updateMineIncident = (mineGuid, mineIncidentGuid, payload) => (disp
       createRequestHeader()
     )
     .then((response) => {
+      notification.success({
+        message: "Successfully updated incident.",
+        duration: 10,
+      });
       dispatch(success(reducerTypes.UPDATE_MINE_INCIDENT));
       return response;
     })
