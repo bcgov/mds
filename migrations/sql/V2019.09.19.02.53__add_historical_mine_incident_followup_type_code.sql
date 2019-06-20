5c5f8266a1e515e7dfb2e78d6ef333cb0e18bd72
Jason S <jasyrotuck@gmail.com>
2019-06-20 15:39:43 -0700

    different column

--- migrations/sql/V2019.09.19.02.53__add_historical_mine_incident_followup_type_code.sql
+++ migrations/sql/V2019.09.19.02.53__add_historical_mine_incident_followup_type_code.sql
@@ -1,5 +1,3 @@
-ALTER TABLE mine_incident_followup_investigation_type ADD COLUMN active_ind boolean default 'T'; 
-
 INSERT INTO mine_incident_followup_investigation_type
 	(
 	mine_incident_followup_investigation_type_code,
@@ -14,5 +12,5 @@ VALUES
 ON CONFLICT DO NOTHING;
 
 ALTER TABLE mine_incident ADD COLUMN mms_min_acc_no character varying(9); 
-ALTER TABLE mine_incident ADD COLUMN mms_recp_cd character varying(3);
+ALTER TABLE mine_incident ADD COLUMN mms_insp_cd character varying(3);
 ALTER TABLE mine_incident ADD COLUMN proponent_incident_no character varying(20);
