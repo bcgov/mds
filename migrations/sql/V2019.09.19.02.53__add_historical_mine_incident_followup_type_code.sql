47ce139af7e241d3b4f1a30c1a710a3b5620c330
Jason S <jasyrotuck@gmail.com>
2019-06-21 13:55:48 -0700

    column comment.

--- migrations/sql/V2019.09.19.02.53__add_historical_mine_incident_followup_type_code.sql
+++ migrations/sql/V2019.09.19.02.53__add_historical_mine_incident_followup_type_code.sql
@@ -13,4 +13,6 @@ ON CONFLICT DO NOTHING;
 
 ALTER TABLE mine_incident ADD COLUMN mine_incident_no character varying(10); 
 ALTER TABLE mine_incident ADD COLUMN mms_insp_cd character varying(3);
+comment on column mine_incident.mms_insp_cd is 'Copy from mms.mmsocc.insp_cd column, 3 letter inspector initials PK on table mms.mmsins';
+
 ALTER TABLE mine_incident ADD COLUMN proponent_incident_no character varying(20);
