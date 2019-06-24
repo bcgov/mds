fc436fb7e25f162a60bf6511f423e86874266847
Paul Dittaro <pdittaro@gmail.com>
2019-06-24 13:37:27 -0700

    Removing comments, adding todos

--- python-backend/tests/auth/test_auth_decorators.py
+++ python-backend/tests/auth/test_auth_decorators.py
@@ -2,6 +2,8 @@ from flask_restplus import Resource, Namespace
 from app.extensions import api as app_api
 from app.api.utils.access_decorators import *
 
+# TODO: Testing all new roles from Keycloak
+
 
 class DummyResource(Resource):
     @requires_role_view_all
@@ -33,22 +35,26 @@ def test_get_no_auth(test_client):
 
 
 def test_get_view_only(test_client, auth_headers):
-    resp = test_client.get('/test', headers=auth_headers['view_only_auth_header'])
+    resp = test_client.get(
+        '/test', headers=auth_headers['view_only_auth_header'])
     assert resp.status_code == 200
 
 
 def test_get_create_only(test_client, auth_headers):
-    resp = test_client.get('/test', headers=auth_headers['create_only_auth_header'])
+    resp = test_client.get(
+        '/test', headers=auth_headers['create_only_auth_header'])
     assert resp.status_code == 401
 
 
 def test_get_admin_only(test_client, auth_headers):
-    resp = test_client.get('/test', headers=auth_headers['admin_only_auth_header'])
+    resp = test_client.get(
+        '/test', headers=auth_headers['admin_only_auth_header'])
     assert resp.status_code == 401
 
 
 def test_get_proponent_only(test_client, auth_headers):
-    resp = test_client.get('/test', headers=auth_headers['proponent_only_auth_header'])
+    resp = test_client.get(
+        '/test', headers=auth_headers['proponent_only_auth_header'])
     assert resp.status_code == 401
 
 
@@ -59,22 +65,26 @@ def test_post_no_auth(test_client):
 
 
 def test_post_view_only(test_client, auth_headers):
-    resp = test_client.post('/test', headers=auth_headers['view_only_auth_header'])
+    resp = test_client.post(
+        '/test', headers=auth_headers['view_only_auth_header'])
     assert resp.status_code == 401
 
 
 def test_post_create_only(test_client, auth_headers):
-    resp = test_client.post('/test', headers=auth_headers['create_only_auth_header'])
+    resp = test_client.post(
+        '/test', headers=auth_headers['create_only_auth_header'])
     assert resp.status_code == 200
 
 
 def test_post_admin_only(test_client, auth_headers):
-    resp = test_client.post('/test', headers=auth_headers['admin_only_auth_header'])
+    resp = test_client.post(
+        '/test', headers=auth_headers['admin_only_auth_header'])
     assert resp.status_code == 401
 
 
 def test_post_proponent_only(test_client, auth_headers):
-    resp = test_client.post('/test', headers=auth_headers['proponent_only_auth_header'])
+    resp = test_client.post(
+        '/test', headers=auth_headers['proponent_only_auth_header'])
     assert resp.status_code == 401
 
 
@@ -85,22 +95,26 @@ def test_delete_no_auth(test_client):
 
 
 def test_delete_view_only(test_client, auth_headers):
-    resp = test_client.delete('/test', headers=auth_headers['view_only_auth_header'])
+    resp = test_client.delete(
+        '/test', headers=auth_headers['view_only_auth_header'])
     assert resp.status_code == 401
 
 
 def test_delete_create_only(test_client, auth_headers):
-    resp = test_client.delete('/test', headers=auth_headers['create_only_auth_header'])
+    resp = test_client.delete(
+        '/test', headers=auth_headers['create_only_auth_header'])
     assert resp.status_code == 401
 
 
 def test_delete_admin_only(test_client, auth_headers):
-    resp = test_client.delete('/test', headers=auth_headers['admin_only_auth_header'])
+    resp = test_client.delete(
+        '/test', headers=auth_headers['admin_only_auth_header'])
     assert resp.status_code == 200
 
 
 def test_proponent_admin_only(test_client, auth_headers):
-    resp = test_client.delete('/test', headers=auth_headers['proponent_only_auth_header'])
+    resp = test_client.delete(
+        '/test', headers=auth_headers['proponent_only_auth_header'])
     assert resp.status_code == 401
 
 
@@ -111,20 +125,24 @@ def test_put_no_auth(test_client):
 
 
 def test_put_view_only(test_client, auth_headers):
-    resp = test_client.put('/test', headers=auth_headers['view_only_auth_header'])
+    resp = test_client.put(
+        '/test', headers=auth_headers['view_only_auth_header'])
     assert resp.status_code == 200
 
 
 def test_put_create_only(test_client, auth_headers):
-    resp = test_client.put('/test', headers=auth_headers['create_only_auth_header'])
+    resp = test_client.put(
+        '/test', headers=auth_headers['create_only_auth_header'])
     assert resp.status_code == 401
 
 
 def test_put_admin_only(test_client, auth_headers):
-    resp = test_client.put('/test', headers=auth_headers['admin_only_auth_header'])
+    resp = test_client.put(
+        '/test', headers=auth_headers['admin_only_auth_header'])
     assert resp.status_code == 401
 
 
 def test_put_proponent_only(test_client, auth_headers):
-    resp = test_client.put('/test', headers=auth_headers['proponent_only_auth_header'])
+    resp = test_client.put(
+        '/test', headers=auth_headers['proponent_only_auth_header'])
     assert resp.status_code == 200
