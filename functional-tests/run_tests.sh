b60ca01868d736629f2eb9020bb6045496114ef0
Gyanesh Mishra <gyan@freshworks.io>
2019-06-25 17:46:36 -0700

    play  with gradle properties

--- functional-tests/run_tests.sh
+++ functional-tests/run_tests.sh
@@ -9,5 +9,5 @@ Xvfb :1 -screen 0 1920x1080x24 &
 export DISPLAY=:1
 
 ### Run both Core and MineSpace tests
-./gradlew firefoxHeadlessTest -DfirefoxHeadlessTest.single=CustomJUnitSpecRunner
-./gradlew firefoxHeadlessTest -DfirefoxHeadlessTest.single=CustomJUnitPublicSpecRunner
+gradle firefoxHeadlessTest -DfirefoxHeadlessTest.single=CustomJUnitSpecRunner
+gradle firefoxHeadlessTest -DfirefoxHeadlessTest.single=CustomJUnitPublicSpecRunner
