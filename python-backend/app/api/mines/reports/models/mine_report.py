da25612b9031bfad380ce57d439d017680decad1
justinmacaulay <justinkmacaulay@gmail.com>
2019-06-28 14:20:12 -0700

    models and migration for mine reports.

---
+++ python-backend/app/api/mines/reports/models/mine_report.py
@@ -0,0 +1,24 @@
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.schema import FetchedValue
+from sqlalchemy.ext.associationproxy import association_proxy
+
+from app.api.utils.models_mixins import Base
+from app.extensions import db
+
+
+class MineReport(Base):
+    __tablename__ = "mine_report"
+    mine_report_id = db.Column(db.Integer, primary_key=True, server_default=FetchedValue())
+    mine_report_guid = db.Column(UUID(as_uuid=True))
+    mine_report_definition_id = db.Column(
+        db.Integer, db.ForeignKey('mine_report_definition.mine_report_definition_id'))
+    mine_guid = db.Column(UUID(as_uuid=True), db.ForeignKey('mine.mine_guid'))
+    permit_id = db.Column(db.Integer, db.ForeignKey('permit.permit_id'))
+    due_date = db.Column(db.DateTime, nullable=False)
+    submission_year = db.Column(db.Integer)
+    deleted_ind = db.Column(db.Boolean, server_default=FetchedValue(), nullable=False)
+
+    mine_report_submissions = db.relationship('MineReportSubmission', lazy='joined')
+
+    def __repr__(self):
+        return '<MineReport %r>' % self.mine_report_guid
---
+++ python-backend/app/api/mines/reports/models/mine_report_submission.py
@@ -0,0 +1,24 @@
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.schema import FetchedValue
+from sqlalchemy.ext.associationproxy import association_proxy
+
+from app.api.utils.models_mixins import Base
+from app.extensions import db
+
+
+class MineReportSubmission(Base):
+    __tablename__ = "mine_report_submission"
+    mine_report_submission_id = db.Column(
+        db.Integer, primary_key=True, server_default=FetchedValue())
+    mine_report_submission_guid = db.Column(UUID(as_uuid=True))
+    mine_report_id = db.Column(db.Integer, db.ForeignKey('mine_report.mine_report_id'))
+    received_date = db.Column(db.DateTime)
+    mine_report_submission_status_code = db.Column(
+        db.String,
+        db.ForeignKey('mine_report_submission_status_code.mine_report_submission_status_code'))
+    active_ind = db.Column(db.Boolean, server_default=FetchedValue(), nullable=False)
+    documents = db.relationship(
+        'MineDocument', lazy='selectin', secondary='mine_report_document_xref')
+
+    def __repr__(self):
+        return '<MineReportSubmission %r>' % self.mine_report_submission_guid
