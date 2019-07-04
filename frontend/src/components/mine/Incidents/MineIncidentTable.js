14cf1fbb0f1059b7c5661c445dfcd9e379f77663
thegentlemanphysicist <jon@freshworks.io>
2019-07-04 08:48:53 -0700

    Fixed broken date sorting on the front end (#844)

--- frontend/src/components/mine/Incidents/MineIncidentTable.js
+++ frontend/src/components/mine/Incidents/MineIncidentTable.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Table, Button } from "antd";
-
+import moment from "moment";
 import _ from "lodash";
 
 import { BRAND_PENCIL } from "@/constants/assets";
@@ -43,7 +43,7 @@ const columns = (props) => [
   {
     title: "Incident Time",
     dataIndex: "incident_timestamp",
-    sorter: (a, b) => new Date(a.incident_timestamp) > new Date(b.incident_timestamp),
+    sorter: (a, b) => moment(a.incident_timestamp) > moment(b.incident_timestamp),
   },
   {
     title: "Reported By",
