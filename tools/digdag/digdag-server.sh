d88883c6028aef60fae728500ff021a87f071090
Gyanesh Mishra <gyan@freshworks.io>
2019-07-11 11:43:37 -0700

    add blocking call and  client creation

--- tools/digdag/digdag-server.sh
+++ tools/digdag/digdag-server.sh
@@ -3,4 +3,9 @@
 # Template any required environment variables for the property file
 envsubst < digdag.properties.tmpl > digdag.properties
 
-digdag server --database digdag-server --task-log digdag-logs --config digdag.properties
+digdag server --database digdag-server --task-log digdag-logs --config digdag.properties &
+
+digdag push best-project --project project -r "$(date +%Y-%m-%dT%H:%M:%S%z)" --endpoint 0.0.0.0:8080
+
+# Add blocking call to ensure container doesn't exit
+wait
