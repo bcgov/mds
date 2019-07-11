483d865aa07328df2924617f7205cd493c0e4f53
Gyanesh Mishra <gyan@freshworks.io>
2019-07-11 12:05:45 -0700

    add wait  for it

--- tools/digdag/digdag-server.sh
+++ tools/digdag/digdag-server.sh
@@ -5,6 +5,8 @@ envsubst < digdag.properties.tmpl > digdag.properties
 
 digdag server --database digdag-server --task-log digdag-logs --config digdag.properties &
 
+# Wait for server to start and create projects
+./wait-for-it.sh 0.0.0.0:8080 -- echo "Digdag Server running"
 digdag push best-project --project project -r "$(date +%Y-%m-%dT%H:%M:%S%z)" --endpoint 0.0.0.0:8080
 
 # Add blocking call to ensure container doesn't exit
