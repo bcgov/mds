8c914157b6acbc58cf2b1a96497d043e50dce132
Gyanesh Mishra <gyan@freshworks.io>
2019-07-11 21:12:30 -0700

    add service account and  correct config file

--- tools/digdag/project/tasks/__init__.py
+++ tools/digdag/project/tasks/__init__.py
@@ -1,4 +1,5 @@
 import yaml
+import os
 from kubernetes import client, config
 from openshift.dynamic import DynamicClient
 
@@ -7,23 +8,14 @@ class NrisETL(object):
 
     def run_job(self):
 
-        token = ""
-        with open('/var/run/secrets/kubernetes.io/serviceaccount/token', 'r') as token_file:
-            token = token_file.read()
-
-        # Create a configuration object
-        conf = client.Configuration()
-
-        # Specify the endpoint of your Kube cluster
-        conf.host = "https://pathfinder.gov.bc.ca"
-        conf.api_key = {"authorization": "Bearer " + token}
+        config_file = os.environ.get('KUBECONFIG', '/tmp/token')
 
         # api_client = client.ApiClient(conf)
         k8s_client = client.CoreV1Api(conf)
         dyn_client = DynamicClient(k8s_client)
 
         v1_jobs = dyn_client.resources.get(
-            api_version='route.openshift.io/v1', namespace='empr-mds-dev', kind='Job')
+            api_version='v1', namespace='empr-mds-dev', kind='Job', name='pi')
 
         job = """
         apiVersion: extensions/v1beta1
@@ -31,9 +23,9 @@ class NrisETL(object):
         metadata:
         name: nris_etl
         spec:
-        parallelism: 1    
-        completions: 1    
-        template:         
+        parallelism: 1
+        completions: 1
+        template:
             metadata:
             name: nris_etl
             labels:
