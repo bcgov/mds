763ea94dd13777b275744b5f252d2f39f99638be
Gyanesh Mishra <gyan@freshworks.io>
2019-07-12 21:19:20 -0700

    convert indentation to tabs

--- tools/digdag/project/tasks/__init__.py
+++ tools/digdag/project/tasks/__init__.py
@@ -5,54 +5,54 @@ from openshift.dynamic import DynamicClient
 
 class NrisETL(object):
 
-    def run_job(self):
+	def run_job(self):
 
-        kube_config = os.getenv('KUBECONFIG', '/app/kube_config')
+		kube_config = os.getenv('KUBECONFIG', '/app/kube_config')
 
-        # api_client = client.ApiClient(conf)
-        k8s_client = config.new_client_from_config(kube_config)
-        dyn_client = DynamicClient(k8s_client)
+		# api_client = client.ApiClient(conf)
+		k8s_client = config.new_client_from_config(kube_config)
+		dyn_client = DynamicClient(k8s_client)
 
-        v1_jobs = dyn_client.resources.get(api_version='v1', kind='Job')
+		v1_jobs = dyn_client.resources.get(api_version='v1', kind='Job')
 
-        job = """
+		job = """
 kind: Job
 metadata:
   labels:
-    app: digdag
+	app: digdag
   name: digdag-nris
   namespace: empr-mds-dev
 spec:
   template:
-    metadata:
-      labels:
-        job-name: digdag-nris
-    spec:
-      containers:
-      - command:
-        - echo
-        - IAmUp
-        image: mds-nris-backend:dev-pr-863
-        imagePullPolicy: Always
-        name: digdag-nris
-        resources: {}
-        terminationMessagePath: "/dev/termination-log"
-        terminationMessagePolicy: File
-      dnsPolicy: ClusterFirst
-      restartPolicy: Never
-      terminationGracePeriodSeconds: 30
+	metadata:
+	  labels:
+		job-name: digdag-nris
+	spec:
+	  containers:
+	  - command:
+		- echo
+		- IAmUp
+		image: mds-nris-backend:dev-pr-863
+		imagePullPolicy: Always
+		name: digdag-nris
+		resources: {}
+		terminationMessagePath: "/dev/termination-log"
+		terminationMessagePolicy: File
+	  dnsPolicy: ClusterFirst
+	  restartPolicy: Never
+	  terminationGracePeriodSeconds: 30
 """
 
-        job_data = yaml.load(job)
-				try:
-					resp = v1_jobs.create(body=job_data, namespace='empr-mds-dev')
-				except:
-					print("Job exists! Replace instead")
-        	resp = v1_jobs.replace(body=job_data, namespace='empr-mds-dev')
+		job_data = yaml.load(job)
+		try:
+			resp = v1_jobs.create(body=job_data, namespace='empr-mds-dev')
+		except:
+			print("Job exists! Replace instead")
+			resp = v1_jobs.replace(body=job_data, namespace='empr-mds-dev')
 
-				for event in v1_jobs.watch(namespace='empr-mds-dev'):
-    			print(event['object'])
+		for event in v1_jobs.watch(namespace='empr-mds-dev'):
+		print(event['object'])
 
-				print("Job finished")
-        # resp is a ResourceInstance object
-        print(resp.metadata)
+		print("Job finished")
+		# resp is a ResourceInstance object
+		print(resp.metadata)
