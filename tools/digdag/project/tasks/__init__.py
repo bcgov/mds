b83b76c6390aeb0e633d3bd0e53f1b193c22528c
Gyanesh Mishra <gyan@freshworks.io>
2019-07-12 21:31:48 -0700

    convert indentation to tabs

--- tools/digdag/project/tasks/__init__.py
+++ tools/digdag/project/tasks/__init__.py
@@ -5,17 +5,17 @@ from openshift.dynamic import DynamicClient
 
 class NrisETL(object):
 
-	def run_job(self):
+    def run_job(self):
 
-		kube_config = os.getenv('KUBECONFIG', '/app/kube_config')
+        kube_config = os.getenv('KUBECONFIG', '/app/kube_config')
 
-		# api_client = client.ApiClient(conf)
-		k8s_client = config.new_client_from_config(kube_config)
-		dyn_client = DynamicClient(k8s_client)
+        # api_client = client.ApiClient(conf)
+        k8s_client = config.new_client_from_config(kube_config)
+        dyn_client = DynamicClient(k8s_client)
 
-		v1_jobs = dyn_client.resources.get(api_version='v1', kind='Job')
+        v1_jobs = dyn_client.resources.get(api_version='v1', kind='Job')
 
-		job = """
+        job = """
 kind: Job
 metadata:
   labels:
@@ -43,16 +43,16 @@ spec:
 	  terminationGracePeriodSeconds: 30
 """
 
-		job_data = yaml.load(job)
-		try:
-			resp = v1_jobs.create(body=job_data, namespace='empr-mds-dev')
-		except:
-			print("Job exists! Replace instead")
-			resp = v1_jobs.replace(body=job_data, namespace='empr-mds-dev')
+        job_data = yaml.load(job)
+        try:
+            resp = v1_jobs.create(body=job_data, namespace='empr-mds-dev')
+        except:
+            print("Job exists! Replace instead")
+            resp = v1_jobs.replace(body=job_data, namespace='empr-mds-dev')
 
-		for event in v1_jobs.watch(namespace='empr-mds-dev'):
-		print(event['object'])
+        for event in v1_jobs.watch(namespace='empr-mds-dev'):
+            print(event['object'])
 
-		print("Job finished")
-		# resp is a ResourceInstance object
-		print(resp.metadata)
+        print("Job finished")
+        # resp is a ResourceInstance object
+        print(resp.metadata)
