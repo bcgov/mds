151f788dd636fa2e6d63252c2c3b89f22d23bfef
Paul Dittaro <pdittaro@gmail.com>
2019-07-16 15:34:39 -0700

    Keycloak gatekeeper OIDC proxy for Digdag

--- tools/digdag/digdag-init.sh
+++ tools/digdag/digdag-init.sh
@@ -2,12 +2,17 @@
 
 # Template any required environment variables for the property file
 envsubst < digdag.properties.tmpl > digdag.properties
+envsubst < gatekeeper.conf.tmpl > gatekeeper.conf
+
 
 digdag server --database digdag-server --task-log digdag-logs --config digdag.properties &
 
 # Wait for server to start and create projects
-./wait-for-it.sh 0.0.0.0:8080 -- echo "Digdag Server running"
-digdag push best-project --project project -r "$(date +%Y-%m-%dT%H:%M:%S%z)" --endpoint 0.0.0.0:8080
+./wait-for-it.sh 0.0.0.0:8081 -- echo "Digdag Server running"
 
+
+digdag push best-project --project project -r "$(date +%Y-%m-%dT%H:%M:%S%z)" --endpoint 0.0.0.0:8081
+
+keycloak-gatekeeper --config ./gatekeeper.conf
 # Add blocking call to ensure container doesn't exit
 wait
