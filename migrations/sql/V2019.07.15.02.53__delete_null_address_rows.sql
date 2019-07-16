a84cbf372631812f489483e0213c264aec7e0190
thegentlemanphysicist <jon@freshworks.io>
2019-07-16 09:27:58 -0700

    Delete migration works locally to remove unwanted null entries

--- migrations/sql/V2019.07.15.02.53__delete_null_address_rows.sql
+++ migrations/sql/V2019.07.15.02.53__delete_null_address_rows.sql
@@ -1,5 +1,5 @@
 DELETE FROM party_address_xref 
-WHERE address_id = (SELECT address_id FROM address
+WHERE address_id = ANY (SELECT address_id FROM address
 WHERE suite_no IS NULL AND
   address_line_1 IS NULL AND
   address_line_2 IS NULL AND
