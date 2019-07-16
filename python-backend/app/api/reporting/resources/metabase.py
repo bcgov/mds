6817509859a307cee33be9d10765c9ff11129531
Meredith Mumby <meredith@freshworks.io>
2019-07-16 14:20:03 -0700

    updated metabase resource to remove duplicates

--- python-backend/app/api/reporting/resources/metabase.py
+++ python-backend/app/api/reporting/resources/metabase.py
@@ -10,45 +10,32 @@ from app.api.utils.search import search_targets, append_result, execute_search, 
 from app.api.search.search_api_models import SEARCH_RESULT_RETURN_MODEL
 
 
+class Metabase():
+  @classmethod
+  def get_dashboard_url(cls, id):
+    payload = {
+      "resource": {"dashboard": id},
+      "params": {}
+    }
+    token = jwt.encode(payload, current_app.config['METABASE_EMBEDDING_SECRET_KEY'], algorithm="HS256")
+
+    return current_app.config['METABASE_SITE_URL'] + "/embed/dashboard/" + token.decode("utf8") 
+
 class CoreDashboardResource(Resource, UserMixin):
     @requires_role_view_all
     def get(self):
-        payload = {
-          "resource": {"dashboard": 136},
-          "params": {
-
-          }
-        }
-        token = jwt.encode(payload, current_app.config['METABASE_EMBEDDING_SECRET_KEY'], algorithm="HS256")
-
-        dashboard_url = current_app.config['METABASE_SITE_URL'] + "/embed/dashboard/" + token.decode("utf8")
-        return { 'dashboard_url': dashboard_url }
+      dashboard_url = Metabase.get_dashboard_url(136)
+      return { 'dashboard_url': dashboard_url }
 
 class LandingPageGraphOneResource(Resource, UserMixin):
     @requires_role_view_all
     def get(self):
-        payload = {
-          "resource": {"dashboard": 164},
-          "params": {
-
-          }
-        }
-        token = jwt.encode(payload, current_app.config['METABASE_EMBEDDING_SECRET_KEY'], algorithm="HS256")
-
-        dashboard_url = current_app.config['METABASE_SITE_URL'] + "/embed/dashboard/" + token.decode("utf8")
-        return { 'dashboard_url': dashboard_url }
+      dashboard_url = Metabase.get_dashboard_url(164)
+      return { 'dashboard_url': dashboard_url }
 
 class LandingPageGraphTwoResource(Resource, UserMixin):
     @requires_role_view_all
     def get(self):
-        payload = {
-          "resource": {"dashboard": 165},
-          "params": {
-
-          }
-        }
-        token = jwt.encode(payload, current_app.config['METABASE_EMBEDDING_SECRET_KEY'], algorithm="HS256")
-
-        dashboard_url = current_app.config['METABASE_SITE_URL'] + "/embed/dashboard/" + token.decode("utf8")
-        return { 'dashboard_url': dashboard_url }
+      dashboard_url = Metabase.get_dashboard_url(165)
+      return { 'dashboard_url': dashboard_url }
 
