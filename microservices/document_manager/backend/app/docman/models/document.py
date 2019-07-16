1594a8e6b3b629b9ac2a2bdc57b0120219c191d6
Jason S <jasyrotuck@gmail.com>
2019-07-16 15:37:18 -0700

    in serial key.

--- microservices/document_manager/backend/app/docman/models/document.py
+++ microservices/document_manager/backend/app/docman/models/document.py
@@ -10,13 +10,13 @@ from ...utils.models_mixins import AuditMixin, Base
 
 class Document(AuditMixin, Base):
     __tablename__ = 'document'
-    document_id = db.Column(db.Integer, primary_key=True, server_default=FetchedValue())
+    document_id = db.Column(db.Integer, primary_key=True)
     document_guid = db.Column(UUID(as_uuid=True), nullable=False)
-    full_storage_path = db.Column(db.String(150), nullable=False)
+    full_storage_path = db.Column(db.String(4096), nullable=False)
     upload_started_date = db.Column(db.DateTime, nullable=False)
     upload_completed_date = db.Column(db.DateTime, nullable=True)
-    file_display_name = db.Column(db.String(40), nullable=False)
-    path_display_name = db.Column(db.String(150), nullable=False)
+    file_display_name = db.Column(db.String(255), nullable=False)
+    path_display_name = db.Column(db.String(4096), nullable=False)
 
     def __repr__(self):
         return '<Document %r>' % self.document_id
