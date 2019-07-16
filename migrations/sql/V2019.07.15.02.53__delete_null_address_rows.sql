a0b83856313d3d7ee7af24af35efddaea2927456
thegentlemanphysicist <jon@freshworks.io>
2019-07-16 08:11:09 -0700

    made some attempts at database deletion

---
+++ migrations/sql/V2019.07.15.02.53__delete_null_address_rows.sql
@@ -0,0 +1,16 @@
+DELETE FROM party_address_xref 
+WHERE address_id = (SELECT address_id FROM address
+WHERE suite_no IS NULL AND
+  address_line_1 IS NULL AND
+  address_line_2 IS NULL AND
+  city              IS NULL AND
+  sub_division_code IS NULL AND
+  post_code   IS NULL);
+
+DELETE FROM address WHERE
+  suite_no IS NULL AND
+  address_line_1 IS NULL AND
+  address_line_2 IS NULL AND
+  city              IS NULL AND
+  sub_division_code IS NULL AND
+  post_code   IS NULL;
