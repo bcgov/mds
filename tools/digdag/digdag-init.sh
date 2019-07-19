4e2c55a6d74023614e9b6dcf653f67ce8c53a6aa
Gyanesh Mishra <gyan@freshworks.io>
2019-07-18 20:25:55 -0700

    rename dir and add global token

--- tools/digdag/digdag-init.sh
+++ tools/digdag/digdag-init.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-# Template any required environment variables for the property file
+# Export Openshift token for global access
+if [ -f /var/run/secrets/kubernetes.io/serviceaccount/token ]; then
+    export OC_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token);
+fi
+
+# Template any required environment variables for the property files
 envsubst < digdag.properties.tmpl > digdag.properties
 envsubst < gatekeeper.conf.tmpl > gatekeeper.conf
 
-
+# Run digdag server in the background
 digdag server --database digdag-server --task-log digdag-logs --config digdag.properties &
 
 # Wait for server to start and create projects
 ./wait-for-it.sh 0.0.0.0:8081 -- echo "Digdag Server running"
+digdag push etl-tasks --project etl -r "$(date +%Y-%m-%dT%H:%M:%S%z)" --endpoint 0.0.0.0:8081
 
-
-digdag push best-project --project project -r "$(date +%Y-%m-%dT%H:%M:%S%z)" --endpoint 0.0.0.0:8081
-
+# Run keycloak-gatekeeper for authenticated reverse proxy
 keycloak-gatekeeper --config ./gatekeeper.conf
-# Add blocking call to ensure container doesn't exit
+
+# Bring any background processes to front to block container exit
 wait
--- tools/digdag/project/scripts/fetch_oc_token.sh
+++
@@ -1,4 +0,0 @@
-TOKEN_FILE=/var/run/secrets/kubernetes.io/serviceaccount/token
-if [ -f $TOKEN_FILE ]; then
-    export OC_TOKEN=$(cat $TOKEN_FILE);
-fi
