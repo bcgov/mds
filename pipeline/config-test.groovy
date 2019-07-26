197b60664a970c64710bc4576d9eb1ec42ac465f
Jason Syrotuck <jasyrotuck@gmail.com>
2019-07-26 13:07:52 -0700

    Fix/docman nginx dc (#890)
    * missing vars
    
    * nginx dc var.

--- pipeline/config-test.groovy
+++ pipeline/config-test.groovy
@@ -151,6 +151,7 @@ app {
                             'PATH_PREFIX': "${vars.modules.'mds-nginx'.PATH}",
                             'CORE_SERVICE_URL': "${vars.modules.'mds-frontend'.HOST}",
                             'NRIS_API_SERVICE_URL': "${vars.modules.'mds-nris-backend'.HOST}",
+                            'DOCUMENT_MANAGER_SERVICE_URL': "${vars.modules.'mds-docman-backend'.HOST}",
                             'MINESPACE_SERVICE_URL': "${vars.modules.'mds-frontend-public'.HOST}",
                             'API_SERVICE_URL': "${vars.modules.'mds-python-backend'.HOST}",
                     ]
