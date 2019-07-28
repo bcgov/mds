f013e902a7e6ad1dfcb932016d109ac175c4226c
Gyanesh Mishra <gyan@freshworks.io>
2019-07-28 13:49:39 -0700

    add webhooks to jobs

---
+++ tools/digdag/etl/tasks/rocket_chat_webhook.py
@@ -0,0 +1,35 @@
+import requests
+import os
+import json
+
+webhook_url = os.getenv("WEBHOOK_URL", "https://sample.url")
+namespace = os.getenv("NAMESPACE", "empr-mds-dev")
+
+data = {
+  "status": "task_status",
+  "namespace": "namespace",
+  "message": "message"
+}
+
+emoji =	{
+  "RUNNING": "person_running",
+  "SUCCESS": "white_check_mark",
+  "FAIL": "x",
+  "UNDEFINED": "person_shrugging"
+}
+
+
+def send(task_name="", status="UNDEFINED"):
+    # Format message
+    data['status'] = status
+    data['namespace'] = namespace
+    data['message'] = f'''
+    Task status: {status} :{emoji[status]}:
+    Task name: {task_name}
+    Task namespace: {namespace}
+    '''
+
+    # Send webhook
+    headers = {'Content-type': 'application/json'}
+    requests.post(webhook_url, data=json.dumps(data), headers=headers)
+
