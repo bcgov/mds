593b645872c18e679af6b5d24474528f8c3cccb3
Nathan Calvank <nathan@freshworks.io>
2019-07-30 14:40:26 -0700

    Remove OOS todo comment

--- python-backend/tests/incidents/resources/test_mine_incident_followup_investigation_type.py
+++ python-backend/tests/incidents/resources/test_mine_incident_followup_investigation_type.py
@@ -12,6 +12,5 @@ class TestGetIncidentFollowupInvestigationTypeResource:
         get_resp = test_client.get(f'/incidents/followup-types', headers=auth_headers['full_auth_header'])
         get_data = json.loads(get_resp.data.decode())
         assert get_resp.status_code == 200
-        # TODO: Update get if we're only supposed to return active records
         assert len(MineIncidentFollowupInvestigationType.query.all()) > 0
         assert len(get_data['records']) == len(MineIncidentFollowupInvestigationType.query.all())
