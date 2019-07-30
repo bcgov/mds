be0482990b13a687201041827decb06915d79467
Jason S <jasyrotuck@gmail.com>
2019-07-30 14:10:21 -0700

    import change

--- python-backend/app/api/mines/documents/namespace/documents.py
+++ python-backend/app/api/mines/documents/namespace/documents.py
@@ -1,7 +1,7 @@
 from flask_restplus import Namespace
 
 from ..expected.resources.mine_documents import ExpectedMineDocumentResource
-from ..expected.resources.documents import ExpectedDocumentResource
+from ..expected.resources.expected_documents import ExpectedDocumentResource, ExpectedDocumentListResource
 from ..expected.resources.document_status import ExpectedDocumentStatusResource
 from ..expected.resources.expected_document_uploads import ExpectedDocumentUploadResource
 
@@ -10,9 +10,10 @@ api = Namespace('documents/expected',
                 description='MDS records of documents, expected documents, and required documents')
 
 api.add_resource(ExpectedMineDocumentResource, '/mines', '/mines/<string:mine_guid>')
-api.add_resource(ExpectedDocumentResource, '/', '/<string:exp_doc_guid>')
+api.add_resource(ExpectedDocumentListResource, '/')
+api.add_resource(ExpectedDocumentResource, '/<string:exp_doc_guid>')
 
-api.add_resource(ExpectedDocumentStatusResource, '/status', '/expected/status')
+api.add_resource(ExpectedDocumentStatusResource, '/status')
 
 api.add_resource(ExpectedDocumentUploadResource, '/<string:expected_document_guid>/document',
                  '/<string:expected_document_guid>/document/<string:mine_document_guid>')
