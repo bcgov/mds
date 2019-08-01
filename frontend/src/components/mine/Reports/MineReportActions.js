9698ba1ef5c8b8f72e31a5ae1318f2b7f20865d4
Ryan Rondeau <nayr974@users.noreply.github.com>
2019-08-01 13:02:08 -0700

    Merge

---
+++ frontend/src/components/mine/Reports/MineReportActions.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { Icon, Popconfirm, Button } from "antd";
+import PropTypes from "prop-types";
+import CustomPropTypes from "@/customPropTypes";
+import { BRAND_PENCIL } from "@/constants/assets";
+
+const propTypes = {
+  mineReport: PropTypes.objectOf(CustomPropTypes.mineReport).isRequired,
+  openEditReportModal: PropTypes.func.isRequired,
+  handleEditReport: PropTypes.func.isRequired,
+  // eslint-disable-next-line react/no-unused-prop-types
+  handleRemoveReport: PropTypes.func.isRequired,
+};
+
+const defaultProps = {};
+
+const DeleteButton = (state, props) =>
+  ({
+    hasFiles: (
+      <Popconfirm
+        placement="topLeft"
+        title={`Are you sure you want to delete the ${props.mineReport.submission_year} ${props.mineReport.report_name}?`}
+        onConfirm={() => props.handleRemoveReport(props.mineReport.mine_report_guid)}
+        okText="Delete"
+        cancelText="Cancel"
+      >
+        <Button ghost type="primary" size="small">
+          <Icon type="minus-circle" theme="outlined" />
+        </Button>
+      </Popconfirm>
+    ),
+    noFiles: (
+      <Popconfirm
+        placement="topLeft"
+        title={`Are you sure you want to delete the ${props.mineReport.submission_year} ${props.mineReport.report_name}?`}
+        onConfirm={() => props.handleRemoveReport(props.mineReport.mine_report_guid)}
+        okText="Delete"
+        cancelText="Cancel"
+      >
+        <Button ghost type="primary" size="small">
+          <Icon type="minus-circle" theme="outlined" />
+        </Button>
+      </Popconfirm>
+    ),
+  }[state]);
+
+const renderDeleteButton = (props) => {
+  const hasFiles =
+    props.mineReport.mine_report_submissions &&
+    props.mineReport.mine_report_submissions.length > 0 &&
+    props.mineReport.mine_report_submissions[props.mineReport.mine_report_submissions.length]
+      .documents.length > 0;
+  if (hasFiles) return DeleteButton("hasFiles", props);
+  return DeleteButton("noFiles", props);
+};
+
+export const MineReportActions = (props) => (
+  <div>
+    <Button
+      type="primary"
+      size="small"
+      ghost
+      onClick={(event) =>
+        props.openEditReportModal(event, props.handleEditReport, props.mineReport)
+      }
+    >
+      <img src={BRAND_PENCIL} alt="Edit Report" />
+    </Button>
+    {renderDeleteButton(props)}
+  </div>
+);
+
+MineReportActions.propTypes = propTypes;
+MineReportActions.defaultProps = defaultProps;
+
+export default MineReportActions;
