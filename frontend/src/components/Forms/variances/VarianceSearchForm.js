f51d5417ecf2efb1abbf0596a8adf49dad1ba292
thegentlemanphysicist <jon@freshworks.io>
2019-08-06 14:09:28 -0700

    updated comment

--- frontend/src/components/Forms/variances/VarianceSearchForm.js
+++ frontend/src/components/Forms/variances/VarianceSearchForm.js
@@ -27,8 +27,8 @@ const validate = (values) => {
       errors.issue_date_before = "Must be after issue date.";
     }
   }
-  if (values.expiry_date_after && values.expiry_date_max) {
-    if (Date.parse(values.expiry_date_after) > Date.parse(values.expiry_date_max)) {
+  if (values.expiry_date_after && values.expiry_date_before) {
+    if (Date.parse(values.expiry_date_after) > Date.parse(values.expiry_date_before)) {
       errors.issue_date_before = "Must be after expiry date.";
     }
   }
@@ -96,8 +96,8 @@ export class VarianceSearchForm extends Component {
               </Col>
               <Col md={12} xs={24}>
                 <Field
-                  id="expiry_date_max"
-                  name="expiry_date_max"
+                  id="expiry_date_before"
+                  name="expiry_date_before"
                   placeholder="Select Max Expiry Date"
                   component={renderConfig.DATE}
                 />
