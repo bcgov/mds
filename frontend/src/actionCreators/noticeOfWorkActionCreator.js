9295811d58eb65fd1e0d0baccf33b5cc5e1da3fe
Meredith Mumby <meredith@freshworks.io>
2019-08-08 15:01:46 -0700

    added all redux state for getting all noticeOfWorkApplications and single noticeOfWorkApplications

--- frontend/src/actionCreators/noticeOfWorkActionCreator.js
+++ frontend/src/actionCreators/noticeOfWorkActionCreator.js
@@ -1,37 +1,41 @@
-/* eslint-disable */
-import { error } from "@/actions/genericActions";
-import * as Strings from "@/constants/strings";
+import { showLoading, hideLoading } from "react-redux-loading-bar";
+import { error, request, success } from "@/actions/genericActions";
 import * as API from "@/constants/API";
 import { ENVIRONMENT } from "@/constants/environment";
 import { createRequestHeader } from "@/utils/RequestHeaders";
 import CustomAxios from "@/customAxios";
+import * as noticeOfWorkActions from "@/actions/noticeOfWorkActions";
+import * as reducerTypes from "@/constants/reducerTypes";
 
 // This file is anticipated to have multiple exports
 // eslint-disable-next-line import/prefer-default-export
 export const fetchNoticeOfWorkApplication = (applicationGuid) => (dispatch) => {
-  dispatch(request(reducerTypes.GET_MINE_REPORTS));
+  dispatch(request(reducerTypes.GET_NOTICE_OF_WORK_APPLICATIONS));
   dispatch(showLoading());
   return CustomAxios()
-    .get(`${ENVIRONMENT.apiUrl}${API.NOTICE_OF_WORK_APPLICATION()}`, createRequestHeader())
+    .get(
+      `${ENVIRONMENT.apiUrl}${API.NOTICE_OF_WORK_APPLICATION(applicationGuid)}`,
+      createRequestHeader()
+    )
     .then((response) => {
-      dispatch(success(reducerTypes.GET_MINE_REPORTS));
-      dispatch(mineReportActions.storeNoticeOfWorkApplication(response.data));
+      dispatch(success(reducerTypes.GET_NOTICE_OF_WORK_APPLICATIONS));
+      dispatch(noticeOfWorkActions.storeNoticeOfWorkApplication(response.data));
       return response;
     })
-    .catch(() => dispatch(error(reducerTypes.GET_MINE_REPORTS)))
+    .catch(() => dispatch(error(reducerTypes.GET_NOTICE_OF_WORK_APPLICATIONS)))
     .finally(() => dispatch(hideLoading()));
 };
 
 export const fetchNoticeOfWorkApplications = () => (dispatch) => {
-  dispatch(request(reducerTypes.GET_MINE_REPORTS));
+  dispatch(request(reducerTypes.GET_NOTICE_OF_WORK_APPLICATION));
   dispatch(showLoading());
   return CustomAxios()
-    .get(`${ENVIRONMENT.apiUrl}${API.NOTICE_OF_WORK_APPLICATION}`, createRequestHeader())
+    .get(`${ENVIRONMENT.apiUrl}${API.NOTICE_OF_WORK_APPLICATIONS}`, createRequestHeader())
     .then((response) => {
-      dispatch(success(reducerTypes.GET_MINE_REPORTS));
-      dispatch(mineReportActions.storeNoticeOfWorkApplication(response.data));
+      dispatch(success(reducerTypes.GET_NOTICE_OF_WORK_APPLICATION));
+      dispatch(noticeOfWorkActions.storeNoticeOfWorkApplication(response.data));
       return response;
     })
-    .catch(() => dispatch(error(reducerTypes.GET_MINE_REPORTS)))
+    .catch(() => dispatch(error(reducerTypes.GET_NOTICE_OF_WORK_APPLICATION)))
     .finally(() => dispatch(hideLoading()));
 };
