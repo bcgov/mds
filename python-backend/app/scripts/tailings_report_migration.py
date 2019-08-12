dae1e20f158996df2b5127a577befba40e2f8a31
Jason S <jasyrotuck@gmail.com>
2019-08-12 16:55:33 -0700

    load mine_report.

--- python-backend/app/scripts/tailings_report_migration.py
+++ python-backend/app/scripts/tailings_report_migration.py
@@ -9,13 +9,13 @@ connection = psycopg2.connect(host='postgres', port=5432, user='mds', password='
 
 src_table = etl.fromdb(
     connection,
-    'SELECT exp_doc.mine_guid, req_doc.req_document_name, exp_doc.due_date, exp_doc_x.mine_document_guid from mine_expected_document exp_doc \
+    'SELECT exp_doc.mine_guid, req_doc.req_document_name, exp_doc.due_date, exp_doc_x.mine_document_guid, exp_doc.create_user, exp_doc.create_timestamp, exp_doc.update_user, exp_doc.update_timestamp from mine_expected_document exp_doc \
     inner join mine_expected_document_xref exp_doc_x on exp_doc.exp_document_guid = exp_doc_x.exp_document_guid\
     inner join mds_required_document req_doc on req_doc.req_document_guid = exp_doc.req_document_guid'
 )
 
 req_document_crr_defintion_map = [
-    ['req_document_name', 'mine_report_defintion_id'],
+    ['req_document_name', 'mine_report_definition_id'],
     ['Summary of TSF and Dam Safety Recommendations', 28],
     ['ITRB Activities Report', 27],
     ['Register of Tailings Storage Facilities and Dams', 47],
@@ -34,4 +34,12 @@ req_document_crr_defintion_map = [
 
 table1 = etl.join(src_table, req_document_crr_defintion_map, 'req_document_name')
 table1 = etl.cutout(table1, 'req_document_name')
+
 print(table1)
+
+mine_report = etl.cutout(table1, 'mine_document_guid')
+mine_report = etl.addfield(mine_report, 'submission_year', 2019)
+print(mine_report)
+
+#truncate and load
+etl.todb(mine_report, connection, 'mine_report')
