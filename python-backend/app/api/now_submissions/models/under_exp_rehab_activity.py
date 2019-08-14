9e049d3c9a0b4c3dcd2be6aa0beefa1c75527179
Nathan Calvank <nathan@freshworks.io>
2019-08-14 10:34:37 -0700

    MDS-2230 Task/ Test coverage (#930)
    * Use absolute import paths & NOWApplicationFactory
    
    * Add relations tests
    
    * Add contacts relation to tests
    
    * Add existing placer activity relation to test
    
    * Add proposed placer activity relation to test
    
    * Add settling pond relations to tests
    
    * Add surface_bulk_sample_activity relation to tests
    
    * Add documents relation to tests
    
    * Add sand_grv_qry_activity relation to tests
    
    * Add under_exp_new_activity relation to tests
    
    * Add under_exp_rehab_activity relation to tests
    
    * Add under_exp_surface_activity relation to tests
    
    * Add water_source_activity relation to tests
    
    * Fix unique IDs that aren't generated in MDS
    
    These fields are required and must be unique, but they are not serial
    like the other IDs in MDS. The sequence ensures that there are no
    duplicate IDs generated.
    
    * Add NOW applications & relations to create-data

---
+++ python-backend/app/api/now_submissions/models/under_exp_rehab_activity.py
@@ -0,0 +1,21 @@
+from app.api.utils.models_mixins import Base
+from app.extensions import db
+
+
+class UnderExpRehabActivity(Base):
+    __tablename__ = "under_exp_rehab_activity"
+    __table_args__ = { "schema": "now_submissions" }
+    id = db.Column(db.Integer, primary_key=True)
+    messageid = db.Column(db.Integer, db.ForeignKey('now_submissions.application.messageid'))
+    type = db.Column(db.String)
+    incline = db.Column(db.Numeric(14,1))
+    inclineunits = db.Column(db.String)
+    quantity = db.Column(db.Integer)
+    length = db.Column(db.Numeric(14,1))
+    width = db.Column(db.Numeric(14,1))
+    height = db.Column(db.Numeric(14,1))
+    seq_no = db.Column(db.Integer)
+
+
+    def __repr__(self):
+        return '<UnderExpRehabActivity %r>' % self.id
