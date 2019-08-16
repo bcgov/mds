d5b4790e775e24aab8d36e12b121cf4c9bc527e4
bcgov-csnr-cd <bcgov-csnr-cd@gov.bc.ca>
2019-08-16 23:02:27 +0000

    Merge branch develop into master

--- python-backend/app/api/mines/reports/models/mine_report_submission.py
+++ python-backend/app/api/mines/reports/models/mine_report_submission.py
@@ -8,16 +8,18 @@ from app.extensions import db
 
 class MineReportSubmission(Base, AuditMixin):
     __tablename__ = "mine_report_submission"
-    mine_report_submission_id = db.Column(
-        db.Integer, primary_key=True, server_default=FetchedValue())
-    mine_report_submission_guid = db.Column(UUID(as_uuid=True))
+    mine_report_submission_id = db.Column(db.Integer,
+                                          primary_key=True,
+                                          server_default=FetchedValue())
+    mine_report_submission_guid = db.Column(UUID(as_uuid=True), server_default=FetchedValue())
     mine_report_id = db.Column(db.Integer, db.ForeignKey('mine_report.mine_report_id'))
     mine_report_submission_status_code = db.Column(
         db.String,
         db.ForeignKey('mine_report_submission_status_code.mine_report_submission_status_code'))
     submission_date = db.Column(db.DateTime)
-    documents = db.relationship(
-        'MineDocument', lazy='joined', secondary='mine_report_document_xref')
+    documents = db.relationship('MineDocument',
+                                lazy='selectin',
+                                secondary='mine_report_document_xref')
 
     def __repr__(self):
         return '<MineReportSubmission %r>' % self.mine_report_submission_guid
