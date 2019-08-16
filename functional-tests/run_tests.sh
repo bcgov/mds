fa59847170157fd2def0e4b278f50c98311748c0
Gyanesh Mishra <gyan@freshworks.io>
2019-08-15 23:39:28 -0700

    get deps

--- functional-tests/run_tests.sh
+++ functional-tests/run_tests.sh
@@ -3,6 +3,9 @@ set -exv -o pipefail
 
 # NOTE: FIREFOX HEADLESS IS THE ONLY BROWSER WHERE THE UPLOAD DOWNLOAD TEST CAN BE COMPLETED
 
+# Dry run project to cache gradle
+./gradlew -m
+
 ### Run both Core and MineSpace tests
-./gradlew firefoxTest -DfirefoxTest.single=CustomJUnitPublicSpecRunner
-./gradlew firefoxTest -DfirefoxTest.single=CustomJUnitSpecRunner
+./gradlew firefoxHeadlessTest -DfirefoxHeadlessTest.single=CustomJUnitPublicSpecRunner
+./gradlew firefoxHeadlessTest -DfirefoxHeadlessTest.single=CustomJUnitSpecRunner
