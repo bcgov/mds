86a521c9d04feb8d34ee01ffc81269f472f93cca
Paul Dittaro <pdittaro@gmail.com>
2019-08-22 14:39:01 -0700

    WIP Now ETL

--- tools/digdag/etl/tasks/now_etl.py
+++ tools/digdag/etl/tasks/now_etl.py
@@ -2,7 +2,10 @@ from models.pod import POD
 
 
 def run_job():
-    pod = POD(pod_name='digdag-now-etl', env_pod='mds-now-etl',
+    pod = POD(pod_name='digdag-now-etl',
+              env_pod='mds-now-etl',
+              image_namespace='empr-mds-tools',
               command=["python", "now_etls.py"])
+
     pod.create_pod()
     print("Job finished")
