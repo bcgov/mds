4daa9d03cd402bc07e210546a5a827e1af6416d8
bcgov-csnr-cd <bcgov-csnr-cd@gov.bc.ca>
2019-09-13 23:16:51 +0000

    Merge branch develop into master

--- python-backend/app/api/mines/reports/resources/mine_report_definition.py
+++ python-backend/app/api/mines/reports/resources/mine_report_definition.py
@@ -6,7 +6,7 @@ from werkzeug.exceptions import BadRequest, NotFound, InternalServerError
 
 from app.extensions import api, db
 from app.api.utils.resources_mixins import UserMixin
-from app.api.utils.access_decorators import requires_role_view_all
+from app.api.utils.access_decorators import requires_any_of, VIEW_ALL, MINESPACE_PROPONENT
 
 from app.api.mines.mine.models.mine import Mine
 from app.api.mines.reports.models.mine_report_definition import MineReportDefinition
@@ -21,6 +21,6 @@ from ...mine_api_models import MINE_REPORT_DEFINITION_MODEL
 class MineReportDefinitionListResource(Resource, UserMixin):
     @api.marshal_with(MINE_REPORT_DEFINITION_MODEL, envelope='records', code=200, as_list=True)
     @api.doc(description='returns the report definitions for possible reports.')
-    @requires_role_view_all
+    @requires_any_of([VIEW_ALL, MINESPACE_PROPONENT])
     def get(self):
         return MineReportDefinition.active()
