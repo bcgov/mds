1ab5cd44ae5fcff039da791ff72df1a7df5f1c3a
Paul Dittaro <pdittaro@gmail.com>
2019-09-16 13:40:08 -0700

    F

--- migrations/sql/V2019.09.18.12.46__remove_not_null_now_application_source.sql
+++ migrations/sql/V2019.09.18.12.46__remove_not_null_now_application_source.sql
@@ -1,1 +1,2 @@
-ALTER TABLE now_submissions.application ALTER COLUMN originating_system character varying(8) drop default drop not NULL ;
+ALTER TABLE now_submissions.application ALTER COLUMN originating_system drop default; 
+ALTER TABLE now_submissions.application ALTER COLUMN originating_system drop not NULL;
