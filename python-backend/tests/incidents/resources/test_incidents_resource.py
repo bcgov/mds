0d13a08c1bd0244babf1e9d119ccbdc6c6fc661d
Jason S <jasyrotuck@gmail.com>
2019-09-18 15:38:55 -0700

    more debug info on test.

--- python-backend/tests/incidents/resources/test_incidents_resource.py
+++ python-backend/tests/incidents/resources/test_incidents_resource.py
@@ -6,14 +6,17 @@ import random
 from tests.status_code_gen import *
 from tests.factories import MineIncidentFactory, MineFactory
 
+
 class TestGetIncidents:
     """GET /incidents"""
+
     # get all incidents
     def test_get_incidents(self, test_client, db_session, auth_headers):
         """Should return all records and a 200 response code"""
         batch_size = 20
         MineIncidentFactory.create_batch(size=batch_size)
-        get_resp = test_client.get('/incidents?per_page=25', headers=auth_headers['full_auth_header'])
+        get_resp = test_client.get('/incidents?per_page=25',
+                                   headers=auth_headers['full_auth_header'])
         get_data = json.loads(get_resp.data.decode())
         assert get_resp.status_code == 200
         assert len(get_data['records']) == batch_size
@@ -21,11 +24,12 @@ class TestGetIncidents:
     def test_get_incidents_status_filter(self, test_client, db_session, auth_headers):
         """Should respect incidents status query param"""
         batch_size = 20
-        status_pre="PRE";
+        status_pre = "PRE"
         MineIncidentFactory.create_batch(size=batch_size, status_code=status_pre)
         status_fin = "FIN"
         MineIncidentFactory.create_batch(size=batch_size, status_code=status_fin)
-        get_resp = test_client.get(f"/incidents?status={status_pre}", headers=auth_headers['full_auth_header'])
+        get_resp = test_client.get(f"/incidents?status={status_pre}",
+                                   headers=auth_headers['full_auth_header'])
         get_data = json.loads(get_resp.data.decode())
         assert get_resp.status_code == 200
         assert len(get_data['records']) == batch_size
@@ -37,11 +41,13 @@ class TestGetIncidents:
         MineIncidentFactory.create_batch(size=batch_size, determination_type_code="PEN")
         MineIncidentFactory.create_batch(size=batch_size, determination_type_code="DO")
         MineIncidentFactory.create_batch(size=batch_size, determination_type_code="NDO")
-        get_resp = test_client.get(f"/incidents?determination=DO%2CNDO", headers=auth_headers['full_auth_header'])
+        get_resp = test_client.get(f"/incidents?determination=DO%2CNDO",
+                                   headers=auth_headers['full_auth_header'])
         get_data = json.loads(get_resp.data.decode())
         assert get_resp.status_code == 200
-        assert len(get_data['records']) == 2*batch_size
-        assert all(map(lambda v: v['determination_type_code'] in ["DO","NDO"], get_data['records']))
+        assert len(get_data['records']) == 2 * batch_size
+        assert all(map(lambda v: v['determination_type_code'] in ["DO", "NDO"],
+                       get_data['records']))
 
     def test_get_incidents_codes_filter(self, test_client, db_session, auth_headers):
         """Should respect incidents codes query param"""
@@ -49,19 +55,27 @@ class TestGetIncidents:
         code2 = SampleDangerousOccurrenceSubparagraphs(2)
         code3 = SampleDangerousOccurrenceSubparagraphs(1)
         batch_size = 5
-        MineIncidentFactory.create_batch(size=batch_size, determination_type_code= 'DO', dangerous_occurrence_subparagraphs=code1)
-        MineIncidentFactory.create_batch(size=batch_size, determination_type_code= 'DO', dangerous_occurrence_subparagraphs=code2)
-        MineIncidentFactory.create_batch(size=batch_size, determination_type_code= 'DO', dangerous_occurrence_subparagraphs=code3)
+        MineIncidentFactory.create_batch(size=batch_size,
+                                         determination_type_code='DO',
+                                         dangerous_occurrence_subparagraphs=code1)
+        MineIncidentFactory.create_batch(size=batch_size,
+                                         determination_type_code='DO',
+                                         dangerous_occurrence_subparagraphs=code2)
+        MineIncidentFactory.create_batch(size=batch_size,
+                                         determination_type_code='DO',
+                                         dangerous_occurrence_subparagraphs=code3)
 
-        get_resp = test_client.get(f"/incidents?codes={code2[0].compliance_article_id}%2C{code2[1].compliance_article_id}",
-                                   headers=auth_headers['full_auth_header'])
+        get_resp = test_client.get(
+            f"/incidents?codes={code2[0].compliance_article_id}%2C{code2[1].compliance_article_id}",
+            headers=auth_headers['full_auth_header'])
         get_data = json.loads(get_resp.data.decode())
         assert get_resp.status_code == 200
         # The intersection of two lists
-        assert all(map(lambda v: set(v['dangerous_occurrence_subparagraph_ids'])
-                                 & set([code2[0].compliance_article_id, code2[1].compliance_article_id]),
-                       get_data['records']))
-
+        assert all(
+            map(
+                lambda v: set(v['dangerous_occurrence_subparagraph_ids'])
+                & set([code2[0].compliance_article_id, code2[1].compliance_article_id]),
+                get_data['records']))
 
     def test_get_incidents_year_filter(self, test_client, db_session, auth_headers):
         """Should respect incidents year query param"""
@@ -71,11 +85,13 @@ class TestGetIncidents:
         random_date_time = datetime.fromtimestamp(random_time_past)
         MineIncidentFactory(incident_timestamp=random_date_time)
         incident_year = str(random_date_time.year)
-        get_resp = test_client.get( f"/incidents?incident_year={incident_year}", headers=auth_headers['full_auth_header'])
+        get_resp = test_client.get(f"/incidents?incident_year={incident_year}",
+                                   headers=auth_headers['full_auth_header'])
         get_data = json.loads(get_resp.data.decode())
         assert get_resp.status_code == 200
         assert len(get_data['records']) == 1
-        assert all(map(lambda v: v['incident_timestamp'][0:4] == incident_year, get_data['records']))
+        assert all(map(lambda v: v['incident_timestamp'][0:4] == incident_year,
+                       get_data['records']))
 
     def test_get_incidents_major_filter(self, test_client, db_session, auth_headers):
         """Should respect incidents codes major mine indicator param"""
@@ -84,26 +100,25 @@ class TestGetIncidents:
         major_mine = MineFactory.create_batch(size=small_batch_size, major_mine_ind=True)
         MineFactory.create_batch(size=batch_size, major_mine_ind=False)
 
-        get_resp = test_client.get( f"/incidents?major=t", headers=auth_headers['full_auth_header'])
+        get_resp = test_client.get(f"/incidents?major=t", headers=auth_headers['full_auth_header'])
         get_data = json.loads(get_resp.data.decode())
         assert get_resp.status_code == 200
         assert len(get_data['records']) == small_batch_size
-        assert all(map(
-            lambda v: v['mine_name'] in [ mine.mine_name for mine in major_mine],
-            get_data['records']))
-
+        assert all(
+            map(lambda v: v['mine_name'] in [mine.mine_name for mine in major_mine],
+                get_data['records']))
 
     def test_get_incidents_search_filter(self, test_client, db_session, auth_headers):
         """Should respect incidents search query param"""
         MineFactory(mine_no='12345')
         MineFactory(mine_no="678910", mine_name='steve')
         MineFactory(mine_name="7891011")
-        get_resp = test_client.get(f'/incidents?search=78910', headers=auth_headers['full_auth_header'])
+        get_resp = test_client.get(f'/incidents?search=78910',
+                                   headers=auth_headers['full_auth_header'])
         get_data = json.loads(get_resp.data.decode())
         assert get_resp.status_code == 200
         assert len(get_data['records']) == 2
-        assert all(map(
-            lambda v: v['mine_name'] in ["7891011","steve"] , get_data['records']))
+        assert all(map(lambda v: v['mine_name'] in ["7891011", "steve"], get_data['records']))
 
     def test_get_incidents_region_filter(self, test_client, db_session, auth_headers):
         """Should respect incidents region query param"""
@@ -114,12 +129,12 @@ class TestGetIncidents:
         MineIncidentFactory.create_batch(size=batch_size)
         MineIncidentFactory(mine=mine_with_region_nw)
         MineIncidentFactory(mine=mine_with_region_sw)
-        get_resp = test_client.get(f'/incidents?region={region_code}', headers=auth_headers['full_auth_header'])
+        get_resp = test_client.get(f'/incidents?region={region_code}',
+                                   headers=auth_headers['full_auth_header'])
         get_data = json.loads(get_resp.data.decode())
         assert get_resp.status_code == 200
-        assert all(map(
-            lambda v: v['mine_name'] == mine_with_region_nw.mine_name,
-            get_data['records']))
+        assert all(
+            map(lambda v: v['mine_name'] == mine_with_region_nw.mine_name, get_data['records']))
 
     def test_get_incidents_date_sort(self, test_client, db_session, auth_headers):
         """Should respect incidents date sort"""
@@ -127,12 +142,13 @@ class TestGetIncidents:
         sort_dir = "desc"
         batch_size = 20
         MineIncidentFactory.create_batch(size=batch_size)
-        get_resp = test_client.get(f'/incidents?sort_field={sort_field}&sort_dir={sort_dir}', headers=auth_headers['full_auth_header'])
+        get_resp = test_client.get(f'/incidents?sort_field={sort_field}&sort_dir={sort_dir}',
+                                   headers=auth_headers['full_auth_header'])
         get_data = json.loads(get_resp.data.decode())
         assert get_resp.status_code == 200
         assert len(get_data['records']) == batch_size
-        assert all(get_data['records'][i]['incident_timestamp'] >= get_data['records'][i+1]['incident_timestamp']
-                   for i in range(len(get_data['records'])-1))
+        assert all(get_data['records'][i]['incident_timestamp'] >= get_data['records'][i + 1]
+                   ['incident_timestamp'] for i in range(len(get_data['records']) - 1))
 
     # TODO: When this incident_no is implemented a test on sorting by it will be needed
     # def test_get_incidents_incident_no_sort(self, test_client, db_session, auth_headers):
@@ -149,8 +165,8 @@ class TestGetIncidents:
         get_data = json.loads(get_resp.data.decode())
         assert get_resp.status_code == 200
         assert len(get_data['records']) == batch_size
-        assert all(get_data['records'][i]['determination_type_code'] <= get_data['records'][i + 1]['determination_type_code']
-                   for i in range(len(get_data['records']) - 1))
+        assert all(get_data['records'][i]['determination_type_code'] <= get_data['records'][i + 1]
+                   ['determination_type_code'] for i in range(len(get_data['records']) - 1))
 
     def test_get_incidents_status_sort(self, test_client, db_session, auth_headers):
         """Should respect incidents status sort"""
@@ -165,7 +181,7 @@ class TestGetIncidents:
         assert len(get_data['records']) == batch_size
         assert all(
             get_data['records'][i]['status_code'] >= get_data['records'][i + 1]['status_code']
-            for i in range(len(get_data['records']) - 1))
+            for i in range(len(get_data['records']) - 1)), get_data
 
     def test_get_incidents_mine_name_sort(self, test_client, db_session, auth_headers):
         """Should respect incidents mine_name sort"""
@@ -178,25 +194,25 @@ class TestGetIncidents:
         get_data = json.loads(get_resp.data.decode())
         assert get_resp.status_code == 200
         assert len(get_data['records']) == batch_size
-        assert all(
-            get_data['records'][i]['mine_name'] >= get_data['records'][i + 1]['mine_name']
-            for i in range(len(get_data['records']) - 1))
+        assert all(get_data['records'][i]['mine_name'] >= get_data['records'][i + 1]['mine_name']
+                   for i in range(len(get_data['records']) - 1)), get_data
 
-    def test_get_incidents_sort_and_filter_multiple_fields(self, test_client, db_session, auth_headers):
+    def test_get_incidents_sort_and_filter_multiple_fields(self, test_client, db_session,
+                                                           auth_headers):
         """Should respect incidents sort and filter by multiple fields"""
         batch_size = 20
-        status_pre="PRE";
+        status_pre = "PRE"
         MineIncidentFactory.create_batch(size=batch_size, status_code=status_pre)
         status_fin = "FIN"
         MineIncidentFactory.create_batch(size=batch_size, status_code=status_fin)
         sort_field = "mine_name"
         sort_dir = "desc"
-        get_resp = test_client.get(f'/incidents?sort_field={sort_field}&sort_dir={sort_dir}&status={status_pre}',
-                                   headers=auth_headers['full_auth_header'])
+        get_resp = test_client.get(
+            f'/incidents?sort_field={sort_field}&sort_dir={sort_dir}&status={status_pre}',
+            headers=auth_headers['full_auth_header'])
         get_data = json.loads(get_resp.data.decode())
         assert get_resp.status_code == 200
         assert len(get_data['records']) == batch_size
         assert all(map(lambda v: v['status_code'] == status_pre, get_data['records']))
-        assert all(
-            get_data['records'][i]['mine_name'] >= get_data['records'][i + 1]['mine_name']
-            for i in range(len(get_data['records']) - 1))
+        assert all(get_data['records'][i]['mine_name'] >= get_data['records'][i + 1]['mine_name']
+                   for i in range(len(get_data['records']) - 1))
