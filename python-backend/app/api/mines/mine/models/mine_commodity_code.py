9765255a4ff60c2670a36150b684a948b693e9f4
Jason S <jasyrotuck@gmail.com>
2019-09-18 11:31:53 -0700

    replace tenure constant with alchemy relationship.

--- python-backend/app/api/mines/mine/models/mine_commodity_code.py
+++ python-backend/app/api/mines/mine/models/mine_commodity_code.py
@@ -6,27 +6,35 @@ from app.api.constants import COMMODITY_CODES_CONFIG, METALS_AND_MINERALS
 from app.extensions import db
 
 
+class MineCommodityTenureType(Base):
+    __tablename__ = 'mine_commodity_tenure_type'
+    mine_commodity_code = db.Column(db.String,
+                                    db.ForeignKey('mine_commodity_code.mine_commodity_code'),
+                                    primary_key=True)
+    mine_tenure_type_code = db.Column(db.String,
+                                      db.ForeignKey('mine_tenure_type_code.mine_tenure_type_code'),
+                                      primary_key=True)
+
+
 class MineCommodityCode(AuditMixin, Base):
     __tablename__ = 'mine_commodity_code'
     mine_commodity_code = db.Column(db.String, nullable=False, primary_key=True)
     description = db.Column(db.String, nullable=False)
     active_ind = db.Column(db.Boolean, nullable=False, server_default=FetchedValue())
 
+    tenure_types = db.relationship('MineTenureTypeCode', secondary='mine_commodity_tenure_type')
+
     def __repr__(self):
         return '<MineCommodityCode %r>' % self.mine_commodity_code
 
-
     @classmethod
     def all_options(cls):
-        return list(map(
-            lambda x: {
-                'mine_commodity_code': x[0],
-                'description': x[1],
-                'mine_tenure_type_codes': COMMODITY_CODES_CONFIG[x[0]]['mine_tenure_type_codes'],
-                'exclusive_ind': COMMODITY_CODES_CONFIG[x[0]]['exclusive_ind']
-            },
-            cls.query
-               .with_entities(cls.mine_commodity_code, cls.description)
-               .filter_by(active_ind=True)
-               .all()
-        ))
+        return list(
+            map(
+                lambda x: {
+                    'mine_commodity_code': x.mine_commodity_code,
+                    'description': x.description,
+                    'mine_tenure_type_codes': [y.mine_tenure_type_code for y in x.tenure_types],
+                    'exclusive_ind': COMMODITY_CODES_CONFIG[x.mine_commodity_code]['exclusive_ind']
+                },
+                cls.query.filter_by(active_ind=True).all()))
