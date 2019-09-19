0f13b5cf763f5c5f25b26a662f0105f0e7ec96e5
Paul Dittaro <pdittaro@gmail.com>
2019-09-19 12:06:22 -0700

    Wrap print statements.

--- microservices/now_etls/NOW_import.py
+++ microservices/now_etls/NOW_import.py
@@ -50,7 +50,7 @@ def truncate_table(connection, tables):
 def join_mine_guids(connection, application_table):
     current_mines = etl.fromdb(
         connection, 'select distinct on (minenumber) mine_guid, mine_no as minenumber from public.mine order by minenumber, create_timestamp;')
-    etl.look(current_mines, style='simple')
+    print(etl.look(current_mines, style='simple'))
     application_table_guid_lookup = etl.join(
         application_table, current_mines, key='minenumber')
     return application_table_guid_lookup
@@ -68,12 +68,12 @@ def ETL_MMS_NOW_schema(connection, tables, schema, system_name):
                 table_plus_os = etl.addfield(
                     current_table, 'originating_system', system_name)
 
-                etl.look(table_plus_os, style='simple')
+                print(etl.look(table_plus_os, style='simple')
 
-                table_plus_os_guid = join_mine_guids(connection, table_plus_os)
+                table_plus_os_guid=join_mine_guids(connection, table_plus_os)
 
                 print("------------------")
-                etl.look(table_plus_os_guid, style='simple')
+                print(etl.look(table_plus_os_guid, style='simple'))
 
                 etl.appenddb(table_plus_os_guid, connection,
                              destination, schema='now_submissions', commit=False)
