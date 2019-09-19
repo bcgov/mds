c46aeb85a008aedb25f72b8f2baa43d1f0bf898c
Nathan Calvank <nathan@freshworks.io>
2019-09-19 12:48:01 -0700

    Basic POC of using esri-leaflet in a map component (#1001)
    * Load esri-leaflet example map in component
    
    * Add toggleable layers
    
    * hide overlay layer list

--- pipeline/config-dev.groovy
+++ pipeline/config-dev.groovy
@@ -98,6 +98,7 @@ app {
                             'BASE_PATH': "${vars.modules.'mds-frontend'.PATH}",
                             'NODE_ENV': "${vars.deployment.node_env}",
                             'MAP_PORTAL_ID': "${vars.deployment.map_portal_id}",
+                            'FN_LAYER_URL': "${vars.deployment.fn_layer_url}",
                             'KEYCLOAK_RESOURCE': "${vars.keycloak.resource}",
                             'KEYCLOAK_CLIENT_ID': "${vars.keycloak.clientId_core}",
                             'KEYCLOAK_URL': "${vars.keycloak.url}",
@@ -328,6 +329,7 @@ environments {
                 application_suffix = "-pr-${vars.git.changeId}"
                 node_env = "development"
                 map_portal_id = "e926583cd0114cd19ebc591f344e30dc"
+                fn_layer_url = "https://delivery.apps.gov.bc.ca/ext/sgw/geo.allgov"
                 elastic_enabled_core = 0
                 elastic_enabled_nris = 0
                 elastic_service_name = "MDS Dev"
