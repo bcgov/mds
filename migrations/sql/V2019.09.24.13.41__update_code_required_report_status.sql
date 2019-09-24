a89eba5b6445e3be82d11ae278d918bd2db0e6b3
justinmacaulay <justinkmacaulay@gmail.com>
2019-09-24 14:00:50 -0700

    new migrations

---
+++ migrations/sql/V2019.09.24.13.41__update_code_required_report_status.sql
@@ -0,0 +1,13 @@
+INSERT INTO mine_report_submission_status_code
+   (
+   mine_report_submission_status_code,
+   description,
+   display_order,
+   create_user,
+   update_user
+   )
+VALUES
+   ('NRQ', 'Not Requested', 10, 'system-mds', 'system-mds'),
+   ('REQ', 'Changes Requested', 20, 'system-mds', 'system-mds'),
+   ('REC', 'Changes Received', 30, 'system-mds', 'system-mds'),
+   ('ACC', 'Accepted', 40, 'system-mds', 'system-mds');
