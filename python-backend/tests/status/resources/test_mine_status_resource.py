938b6176ecd77282b68a8383015ad177084b456a
Jason S <jasyrotuck@gmail.com>
2019-09-26 10:33:19 -0700

    remove test on removed endpoint

--- python-backend/tests/status/resources/test_mine_status_resource.py
+++ python-backend/tests/status/resources/test_mine_status_resource.py
@@ -8,11 +8,3 @@ def test_get_mine_status_option(test_client, db_session, auth_headers):
     get_data = json.loads(get_resp.data.decode())
     assert len(get_data['records']) == len(MineStatusXref.active())
     assert get_resp.status_code == 200
-
-
-def test_get_mine_status_not_found(test_client, db_session, auth_headers):
-    get_resp = test_client.get(
-        '/mines/status/' + str(uuid.uuid4()), headers=auth_headers['full_auth_header'])
-    get_data = json.loads(get_resp.data.decode())
-    assert get_data['message'] is not None
-    assert get_resp.status_code == 404
