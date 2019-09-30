2c350431e3d467b33a0d46b24df54f8387fb5f45
Ryan <ryan_rondeau@live.ca>
2019-09-30 16:49:21 -0700

    Update V2019.09.24.13.22__add_upload_date_to_mine_document.sql

--- migrations/sql/V2019.09.24.13.22__add_upload_date_to_mine_document.sql
+++ migrations/sql/V2019.09.24.13.22__add_upload_date_to_mine_document.sql
@@ -1,3 +1,3 @@
-ALTER TABLE mine_document ADD COLUMN upload_date timestamp with time zone; 
+ALTER TABLE mine_document ADD COLUMN IF NOT EXISTS upload_date timestamp with time zone; 
 
 update mine_document set upload_date = create_timestamp where upload_date is null;
