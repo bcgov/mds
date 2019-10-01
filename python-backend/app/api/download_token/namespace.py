e3fcf82f28c0e6c135973a07446b9446d5392a80
Jason S <jasyrotuck@gmail.com>
2019-10-01 09:00:18 -0700

    import misname

--- python-backend/app/api/download_token/namespace.py
+++ python-backend/app/api/download_token/namespace.py
@@ -1,6 +1,6 @@
 from flask_restplus import Namespace
 
-from app.api.document_token.resources.download_token import DownloadTokenResource
+from app.api.download_token.resources.download_token import DownloadTokenResource
 
 api = Namespace('download-token', description='A for downloading files from the Document Manager.')
 
