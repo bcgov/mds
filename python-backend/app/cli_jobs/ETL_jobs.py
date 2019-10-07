6485285ae43ca0701af45eef54c98ad01c82b017
Jason S <jasyrotuck@gmail.com>
2019-10-07 15:39:21 -0700

    get companies addresses, next up parties.

--- python-backend/app/cli_jobs/ETL_jobs.py
+++ python-backend/app/cli_jobs/ETL_jobs.py
@@ -12,3 +12,15 @@ def run_ETL():
     db.session.execute('commit;')
     db.session.execute('select transfer_mine_status_information();')
     db.session.execute('commit;')
+
+
+@register_apm()
+def run_address_etl():
+    #update records where parties in ETL_PERMIT have addresses in address and were created by this migration
+    db.session.execute(
+        'update address set address_line_1 = m.addr1, city = m.city, sub_division_code = m.prov, post_code = m.post_cd, update_user = \'mms-migration\' from mms.mmscmp m right join ETL_PERMIT etl on concat(m.cmp_nm,m.tel_no) = etl.party_combo_id left join party p on p.party_guid = etl.party_guid where address.party_guid = p.party_guid and create_user = \'mms-migration\''
+    )
+    #create address where the ETL_PERMIT table has a party but that party doesn't have an address
+    db.session.execute(
+        'insert into address (party_guid, address_line_1, city, sub_division_code, post_code, create_user, update_user) select party_guid, addr1, city, prov, post_cd \'mms-migration\',\'mms-migration\' from mms.mmscmp m right join ETL_PERMIT etl on concat(m.cmp_nm,m.tel_no) = etl.party_combo_id inner join party p on p.party_guid = etl.party_guid;'
+    )
