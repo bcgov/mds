799aae5538032d31e6b82b5365f04947555c7ff6
Jason S <jasyrotuck@gmail.com>
2019-10-08 15:01:17 -0700

    address import sql.

--- python-backend/app/cli_jobs/ETL_jobs.py
+++ python-backend/app/cli_jobs/ETL_jobs.py
@@ -16,11 +16,26 @@ def run_ETL():
 
 @register_apm()
 def run_address_etl():
+    print('update existing address for contacts originating from permitee etl')
     #update records where parties in ETL_PERMIT have addresses in address and were created by this migration
     db.session.execute(
         'update address set address_line_1 = m.addr1, city = m.city, sub_division_code = m.prov, post_code = m.post_cd, update_user = \'mms-migration\' from mms.mmscmp m right join ETL_PERMIT etl on concat(m.cmp_nm,m.tel_no) = etl.party_combo_id left join party p on p.party_guid = etl.party_guid where address.party_guid = p.party_guid and create_user = \'mms-migration\''
     )
+
+    print('create addresses for contacts originating from permitee etl')
     #create address where the ETL_PERMIT table has a party but that party doesn't have an address
     db.session.execute(
-        'insert into address (party_guid, address_line_1, city, sub_division_code, post_code, create_user, update_user) select party_guid, addr1, city, prov, post_cd \'mms-migration\',\'mms-migration\' from mms.mmscmp m right join ETL_PERMIT etl on concat(m.cmp_nm,m.tel_no) = etl.party_combo_id inner join party p on p.party_guid = etl.party_guid;'
+        'insert into address (party_guid, address_line_1, city, sub_division_code, post_code, create_user, update_user) select DISTINCT p.party_guid, addr1, city, prov, m.post_cd, \'mms-migration\',\'mms-migration\' from mms.mmscmp m right join ETL_PERMIT etl on m.cmp_nm||m.tel_no = etl.party_combo_id inner join party p on p.party_guid= etl.party_guid where addr1 is not null or city is not null or post_cd is not null'
+    )
+
+    print('create addresses for contacts originating from mine manager etl')
+    #update records where parties in ETL_MANAGER have addresses in address and were created by this migration
+    db.session.execute(
+        'update address set address_line_1 = m.addr1, city = m.city, sub_division_code = m.prov, post_code = m.post_cd, update_user = \'mms-migration\' from ETL_MANAGER e left join mms.mmsccn m on e.person_combo_id = m.cid left join party p on p.party_guid = e.party_guid left join mine on mine.mine_guid = e.mine_guid where address.party_guid = p.party_guid and create_user = \'mms-migration\';'
+    )
+
+    print('create addresses for contacts originating from mine manager etl')
+    #create address where the ETL_PERMIT table has a party but that party doesn't have an address
+    db.session.execute(
+        'insert into address (party_guid, address_line_1, city, sub_division_code, post_code, create_user, update_user) select distinct p.party_guid, trim(m.street), trim(m.city), trim(m.prov), trim(m.post_cd), \'mms-migration\', \'mms-migration\' from ETL_MANAGER e left join mms.mmsccn m on e.person_combo_id = m.cid left join party p on p.party_guid = e.party_guid left join mine on mine.mine_guid = e.mine_guid where street is not null or city is not null or post_cd is not null;'
     )
