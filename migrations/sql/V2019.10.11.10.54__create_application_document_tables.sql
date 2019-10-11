e6502b504450685019416510e8589b99b9b26829
Ryan <ryan_rondeau@live.ca>
2019-10-11 16:22:19 -0700

    adding table comments and renaming files

--- migrations/sql/V2019.10.11.10.54__create_application_document_tables.sql
+++ migrations/sql/V2019.10.11.10.54__create_application_document_tables.sql
@@ -10,11 +10,16 @@ CREATE TABLE IF NOT EXISTS application_document_type (
 
 ALTER TABLE application_document_type OWNER TO mds;
 
+COMMENT ON TABLE application_document_type IS 'Definition of the type of application document (eg. Case File, Spatial File)';
+
 CREATE TABLE IF NOT EXISTS application_document_xref
 (
     application_document_xref uuid    DEFAULT gen_random_uuid() NOT NULL,
     mine_document_guid                uuid NOT NULL REFERENCES mine_document(mine_document_guid),
+	application_document_type_code    varchar(3) REFERENCES application_document_type(application_document_type_code),
     application_id                 	  integer NOT NULL REFERENCES application(application_id)
 );
 
 ALTER TABLE application_document_xref OWNER TO mds;
+
+COMMENT ON TABLE application_document_xref IS 'Documents relating to a NoW application.';
