d452e9c41bd77458db80c61e1154c477c463f709
Meredith Mumby <meredith@freshworks.io>
2019-10-16 14:30:40 -0700

    updated blating modal

---
+++ python-backend/app/api/now_applications/models/blasting_operation.py
@@ -0,0 +1,18 @@
+from sqlalchemy.schema import FetchedValue
+from app.extensions import db
+
+from app.api.utils.models_mixins import AuditMixin, Base
+
+
+class BlastingOperation(AuditMixin, Base):
+    __tablename__ = 'blasting_operation'
+    blasting_operation_id = db.Column(db.Integer, primary_key=True)
+    now_application_id = db.Column(db.Integer,
+                                   db.ForeignKey('now_application.now_application_id'),
+                                   nullable=False)
+   has_storage_explosive_on_site = db.Column(db.Boolean, nullable=False, server_default=FetchedValue())
+   explosive_permit_issued = db.Column(db.Boolean, nullable=False)
+   explosive_permit_number = db.Column(db.String)
+   explosive_permit_expiry_date = db.Column(db.DateTime)
+
+    now_application = db.relationship('NOWApplication')
