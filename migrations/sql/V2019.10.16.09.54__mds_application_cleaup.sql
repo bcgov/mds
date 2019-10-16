e88f693325557fafa64b70c0664f128d43cb3300
Jason S <jasyrotuck@gmail.com>
2019-10-16 15:39:34 -0700

    merge sql.

--- migrations/sql/V2019.10.16.09.54__mds_application_cleaup.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE water_supply ALTER COLUMN permit_application_id SET NOT NULL;
-
-ALTER TABLE placer_operation ALTER COLUMN is_underground SET DEFAULT 'false';
-ALTER TABLE placer_operation ALTER COLUMN is_underground SET NOT NULL;
-ALTER TABLE placer_operation ALTER COLUMN is_hand_operation SET DEFAULT 'false';
-ALTER TABLE placer_operation ALTER COLUMN is_hand_operation SET NOT NULL;
--- migrations/sql/V2019.10.16.13.19__more_application_table_updates.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE state_of_land ALTER COLUMN has_community_water_shed SET NOT NULL; 
-ALTER TABLE state_of_land ALTER COLUMN has_community_water_shed SET DEFAULT FALSE;
-ALTER TABLE state_of_land ALTER COLUMN has_archaeology_sites_affected SET NOT NULL; 
-ALTER TABLE state_of_land ALTER COLUMN has_archaeology_sites_affected SET DEFAULT FALSE;
