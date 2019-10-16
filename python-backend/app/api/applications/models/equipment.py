716948ee619f45de4d7cc3045319993ffa300180
Ryan <ryan_rondeau@live.ca>
2019-10-16 10:40:15 -0700

    wip

---
+++ python-backend/app/api/applications/models/equipment.py
@@ -0,0 +1,18 @@
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.schema import FetchedValue
+from sqlalchemy.ext.associationproxy import association_proxy
+
+from app.api.utils.models_mixins import Base
+from app.extensions import db
+
+
+class Equipment(Base):
+    __tablename__ = "equipment"
+
+    equipment_id = db.Column(db.Integer, primary_key=True, server_default=FetchedValue())
+    description = db.Column(db.String)
+    quantity = db.Column(db.Integer)
+    capacity = db.Column(db.String)
+
+    def __repr__(self):
+        return '<Equipment %r>' % self.equipment_id
