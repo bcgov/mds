6dfa16d27293a4b8af5f2cc0cbce90c502502237
Jason S <jasyrotuck@gmail.com>
2019-10-16 14:10:45 -0700

    xref with many to many and secondary key.

---
+++ python-backend/app/api/now_applications/models/placer_operation_application_xref.py
@@ -0,0 +1,23 @@
+import uuid, datetime
+
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import validates
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.schema import FetchedValue
+from app.extensions import db
+
+from app.api.utils.models_mixins import AuditMixin, Base
+
+
+class NOWApplicationPlacerXref(AuditMixin, Base):
+    __tablename__ = 'now_application_placer_xref'
+    now_application_id = db.Column(db.Integer,
+                                   db.ForeignKey('placer_operation.placer_operation_id'),
+                                   nullable=False)
+    placer_operation_id = db.Column(db.Integer,
+                                    db.ForeignKey('placer_operation.placer_operation_id'),
+                                    nullable=False)
+    is_existing_placer_activity = db.Column(db.Boolean, nullable=False)
+
+    NOWApplication = db.relationship('NOWApplication', backref='Placer_xref')
+    PlacerOperation = db.relationship('PlacerOperation', backref='NOW_xref')
