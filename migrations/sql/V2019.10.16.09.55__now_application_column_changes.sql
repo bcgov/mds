66ad2212fab488593555e1174a3aa129509cc968
Jason S <jasyrotuck@gmail.com>
2019-10-22 16:15:48 -0700

    merge two migration scripts.

--- migrations/sql/V2019.10.16.09.55__now_application_column_changes.sql
+++ migrations/sql/V2019.10.16.09.55__now_application_column_changes.sql
@@ -9,3 +9,25 @@ ALTER TABLE state_of_land ALTER COLUMN has_community_water_shed SET NOT NULL;
 ALTER TABLE state_of_land ALTER COLUMN has_community_water_shed SET DEFAULT FALSE;
 ALTER TABLE state_of_land ALTER COLUMN has_archaeology_sites_affected SET NOT NULL; 
 ALTER TABLE state_of_land ALTER COLUMN has_archaeology_sites_affected SET DEFAULT FALSE;
+
+ALTER TABLE state_of_land ALTER COLUMN has_community_water_shed SET NOT NULL; 
+ALTER TABLE state_of_land ALTER COLUMN has_community_water_shed SET DEFAULT FALSE;
+ALTER TABLE state_of_land ALTER COLUMN has_archaeology_sites_affected SET NOT NULL; 
+ALTER TABLE state_of_land ALTER COLUMN has_archaeology_sites_affected SET DEFAULT FALSE;
+
+ALTER TABLE camp ALTER COLUMN has_fuel_stored SET NOT NULL; 
+ALTER TABLE camp ALTER COLUMN has_fuel_stored SET DEFAULT FALSE;
+ALTER TABLE camp ALTER COLUMN has_fuel_stored_in_bulk SET NOT NULL; 
+ALTER TABLE camp ALTER COLUMN has_fuel_stored_in_bulk SET DEFAULT FALSE;
+ALTER TABLE camp ALTER COLUMN has_fuel_stored_in_barrels SET NOT NULL; 
+ALTER TABLE camp ALTER COLUMN has_fuel_stored_in_barrels SET DEFAULT FALSE;
+ALTER TABLE settling_pond ALTER COLUMN is_ponds_exfiltrated SET NOT NULL; 
+ALTER TABLE settling_pond ALTER COLUMN is_ponds_exfiltrated SET DEFAULT FALSE;
+ALTER TABLE settling_pond ALTER COLUMN is_ponds_recycled SET NOT NULL; 
+ALTER TABLE settling_pond ALTER COLUMN is_ponds_recycled SET DEFAULT FALSE;
+ALTER TABLE settling_pond ALTER COLUMN is_ponds_discharged SET NOT NULL; 
+ALTER TABLE settling_pond ALTER COLUMN is_ponds_discharged SET DEFAULT FALSE;
+ALTER TABLE blasting_operation ALTER COLUMN has_storage_explosive_on_site SET NOT NULL; 
+ALTER TABLE blasting_operation ALTER COLUMN has_storage_explosive_on_site SET DEFAULT FALSE;
+ALTER TABLE blasting_operation ALTER COLUMN explosive_permit_issued SET NOT NULL; 
+ALTER TABLE blasting_operation ALTER COLUMN explosive_permit_issued SET DEFAULT FALSE;
