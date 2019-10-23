4a48535331f21d15efcf4299e58ca1d6f5e9c028
Ryan <ryan_rondeau@live.ca>
2019-10-23 14:23:59 -0700

    Update now_import.py

--- python-backend/app/api/now_applications/now_import.py
+++ python-backend/app/api/now_applications/now_import.py
@@ -11,25 +11,30 @@ def Import(now_submission_message_id):
     if not now_sub:
         raise Exception('No NOW Submission with message_id')
     now_app = app_models.NOWApplication(mine_guid=now_sub.mine_guid)
-    import_now_details(now_app, now_sub)
+    _import_now_details(now_app, now_sub)
+
+    _import_camp_activities(now_app, now_sub)
+
     return now_app
 
+def _import_camp_activities(a, s):
+    
 
-def import_now_details(na, ns):
-    na.now_message_id = ns.messageid
-    na.now_tracking_number = ns.trackingnumber
+def _import_now_details(a, s):
+    a.now_message_id = s.messageid
+    a.now_tracking_number = s.trackingnumber
 
-    na.notice_of_work_type_code = code_lookup(app_models.NOWApplicationType,
-                                              ns.noticeofworktype).notice_of_work_type_code
+    a.notice_of_work_type_code = code_lookup(app_models.NOWApplicationType,
+                                              s.noticeofworktype).notice_of_work_type_code
 
-    na.now_application_status_code = code_lookup(app_models.NOWApplicationStatus,
-                                                 ns.status).now_application_status_code
-    na.submitted_date = ns.submitteddate
-    na.received_date = ns.receiveddate
-    na.latitude = ns.latitude
-    na.longitude = ns.longitude
-    na.property_name = ns.nameofproperty
-    na.tenure_number = ns.tenurenumbers
-    na.proposed_start_date = ns.proposedstartdate
-    na.proposed_end_date = ns.proposedenddate
-    na.save()
+    a.now_application_status_code = code_lookup(app_models.NOWApplicationStatus,
+                                                 s.status).now_application_status_code
+    a.submitted_date = s.submitteddate
+    a.received_date = s.receiveddate
+    a.latitude = s.latitude
+    a.longitude = s.longitude
+    a.property_name = s.nameofproperty
+    a.tenure_number = s.tenurenumbers
+    a.proposed_start_date = s.proposedstartdate
+    a.proposed_end_date = s.proposedenddate
+    a.save()
