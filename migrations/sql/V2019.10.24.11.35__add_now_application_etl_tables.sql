9ae4ca21b5bcc6870134880c7e36fde90b6f392e
Ryan <ryan_rondeau@live.ca>
2019-10-25 10:56:34 -0700

    update etl script

--- migrations/sql/V2019.10.24.11.35__add_now_application_etl_tables.sql
+++ migrations/sql/V2019.10.24.11.35__add_now_application_etl_tables.sql
@@ -1,13 +1,8 @@
-CREATE TABLE IF NOT EXISTS now_submissions.etl_placer_activity  (
-  placeractivityid integer NOT NULL PRIMARY KEY, 
-  activity_detail_id integer NOT NULL
+CREATE TABLE IF NOT EXISTS etl_activity_detail  (
+  activity_detail_id integer PRIMARY KEY REFERENCES activity_detail(activity_detail_id),
+  placeractivityid integer, 
+  settlingpondid integer
 );
-ALTER TABLE now_submissions.etl_placer_activity OWNER TO mds;
-
-CREATE TABLE IF NOT EXISTS now_submissions.etl_settling_pond  (
-  settlingpondid integer NOT NULL PRIMARY KEY, 
-  activity_detail_id integer NOT NULL
-);
-ALTER TABLE now_submissions.etl_settling_pond OWNER TO mds;
+ALTER TABLE etl_activity_detail OWNER TO mds;
 
 ALTER TABLE activity_summary_detail_xref ADD COLUMN is_existing boolean;
