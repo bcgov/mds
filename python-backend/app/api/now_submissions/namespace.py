25e4e9423ffd2bb185966280b5312ede4a090016
Meredith Mumby <meredith@freshworks.io>
2019-10-25 12:45:16 -0700

    created a new resource for now submission imports

--- python-backend/app/api/now_submissions/namespace.py
+++ python-backend/app/api/now_submissions/namespace.py
@@ -3,11 +3,13 @@ from flask_restplus import Namespace
 from app.api.now_submissions.resources.application_resource import ApplicationResource
 from app.api.now_submissions.resources.application_document_resource import ApplicationDocumentResource, ApplicationDocumentTokenResource
 from app.api.now_submissions.resources.application_list_resource import ApplicationListResource
+from app.api.now_submissions.resources.application_import_resource import ApplicationImportResource
 
 api = Namespace('now-submissions', description='Notice of Work operations')
 
 api.add_resource(ApplicationListResource, '/applications')
 api.add_resource(ApplicationResource, '/applications/<string:application_guid>')
+api.add_resource(ApplicationImportResource, '/<string:submission_guid>/import')
 api.add_resource(ApplicationDocumentResource,
                  '/applications/<string:application_guid>/document/<int:id>')
 api.add_resource(ApplicationDocumentTokenResource,
