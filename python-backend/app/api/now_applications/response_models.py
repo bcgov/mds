e5dd01e74bd23641b017ef1f0552c6047849ee67
Jason <jasyrotuck@gmail.com>
2019-10-30 14:24:18 -0700

    skip none.

--- python-backend/app/api/now_applications/response_models.py
+++ python-backend/app/api/now_applications/response_models.py
@@ -12,14 +12,32 @@ class Date(fields.Raw):
         return value.strftime("%Y-%m-%d") if value else None
 
 
+NOW_APPLICATION_ACTIVITY_SUMMARY_BASE = api.model(
+    'NOWApplicationActivitySummaryBase', {
+        'reclamation_description': fields.String,
+        'reclamation_cost': fields.Fixed,
+        'total_disturbed_area': fields.Fixed,
+        'total_disturbed_area_unit_type_code': fields.String
+    })
+
+NOW_APPLICATION_CAMP = api.inherit(
+    'NOWApplicationCamp', NOW_APPLICATION_ACTIVITY_SUMMARY_BASE, {
+        'camp_name': fields.String,
+        'camp_number_people': fields.Fixed,
+        'camp_number_structures': fields.Fixed,
+        'has_fuel_stored': fields.Boolean,
+        'has_fuel_stored_in_bulk': fields.Boolean,
+        'has_fuel_stored_in_barrels': fields.Boolean,
+    })
+
 NOW_APPLICATION_MODEL = api.model(
     'NOWApplication',
     {
         'now_application_guid': fields.String,
         'mine_guid': fields.String,
         'now_message_id': fields.String,
-        'notice_of_work_type_code': fields.String,                        ## code
-        'now_application_status_code': fields.String,                     ##code
+        'notice_of_work_type_code': fields.String,                   ## code
+        'now_application_status_code': fields.String,                ##code
         'submitted_date': Date,
         'received_date': Date,
         'now_application_guid': fields.String,
@@ -31,4 +49,5 @@ NOW_APPLICATION_MODEL = api.model(
         'description_of_land': fields.String,
         'proposed_start_date': Date,
         'proposed_end_date': Date,
+        'camps': fields.Nested(NOW_APPLICATION_CAMP, skip_none=True)
     })
