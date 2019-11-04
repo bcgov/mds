86993287b92deacadd90f43c64aa5db9d77dfd98
justinmacaulay <justinkmacaulay@gmail.com>
2019-11-04 15:53:58 -0800

    capitalization changes.

--- microservices/now_etls/mms_now_import.py
+++ microservices/now_etls/mms_now_import.py
@@ -69,13 +69,13 @@ def ETL_MMS_NOW_schema(connection, tables):
 
         sand_grv_qry_activity = etl.fromdb(
             connection,
-            "SELECT cid as mms_cid, recl_desc as sandgrvqryreclamation, recl_dol as SANDGRVQRYRECLAMATIONCOST, backslope as SANDGRVQRYRECLAMATIONBACKFILL, oper1_ind, oper2_ind, oper3_ind, alr_ind as SANDGRVQRYWITHINAGLANDRES, srb_ind as SANDGRVQRYLOCALGOVSOILREMBYLAW, pdist_ar as SANDGRVQRYDISTURBEDAREA, t_vol as SANDGRVQRYTIMBERVOLUME, edist_Ar as SANDGRVQRYTOTALEXISTDISTAREA, act1_ind, act2_ind, act3_ind, act4_ind, act1_ar, act2_ar, act3_ar, act4_ar, act1_vol, act2_vol, act3_vol, act4_vol from mms.mmssci_n where cid not in ('1610619201301', '1610619201301', '1610619201301', '1610619201301', '1641179201301', '1641180201301', '0900204199901', '1500363201301', '0101302201301', '1650080201302', '1200007201301', '1640974201301', '1101939201301', '0101702201901', '1630649201301', '0900127201301', '1641174201301', '1630124201301', '1620269201301', '1641128201301', '1650234201301', '1630742201501', '1101692201702', '0900221201501', '0101525201501', '0900168201501', '0900168201502', '0900204201701', '1641474201701', '1641366201601', '1620702201601', '1630215201701', '0101228201601', '1620574201601', '1641330201602', '1620818201701', '0200279201701', '1641971201701', '1650885201601', '1631026201701', '0101046201601', '0900133201701', '1641254201402', '1641200201701', '1621397201401', '1650395201601', '0900102201401', '0800688201601', '0400625201501', '0900063201702', '1640993201701', '0900220201501', '0900217201501', '0101046201401', '1620010201501', '1641329201601', '0400426201701', '0900133201702', '1621438201501', '1642069201901', '0500368201501', '1641330201601', '1630513201601', '1640772201601', '1101246201501', '1641430201601', '0900204201703', '0900016201701', '1630734201501', '0101327201401', '1610675201501', '1650528201501', '0900204201702', '1641476201701', '1641013201402', '1620494201401', '1650617201701', '0900168201701', '1630539201701', '0900063201701', '1000409201601', '1640150201601', '1641384201601', '0800407201602', '1641119201401', '1650861201401', '1640847201601', '1620498201701', '0800612201701', '1641179201601', '0900163201601', '0501122201602', '1101692201701', '1630080201701', '0603333201701', '0200279201401', '0900223201501', '0700568201701', '0500297201701', '1640851201701', '1101122201401', '1641354201501', '1650611201501', '0400724201502', '1641281201401', '0500910201401', '0101050201501', '0900168201601', '0600402201601', '0900127201401', '0101684201401', '1630484201601', '1650080201401', '1650294201401', '1650816201301', '1641252201401', '0900222201501', '1650832201401', '1650892201501', '1641311201501', '0101228201501', '1620501201501', '1630292201501', '1630419201401', '1620335201402', '0900218201501', '1641256201501', '1640870201401', '0400724201501', '1650898201501', '1650902201501', '0101058201801', '1640983201701', '1640863201701', '0101098201901', '0400704201801', '1650615201901', '0101231201901', '1641200201901', '1640850201701', '1610376201801', '1630712201701', '0400056201703', '0900197201902', '1641200201902', '1631059201901', '0101703201901', '1621123201901', '1621636201701', '0800029201701', '0200381201901', '1500530201701', '1642042201801', '0101058201901', '1650617201901', '0900145201701', '1650087201901', '1650294201901', '1610632201901', '1640982201701', '1620381201801', '1650753201901', '1650563201901', '1631030201801', '0700541201801', '1640235201802', '1620291201701', '1630272201701', '1631056201701', '1641424201801', '0400625201901', '1650616201901', '1641060201701', '1621123201801', '1101529201801', '1620530201801', '1630646201901', '1640943201801', '0501178201701', '0400851201801', '1620381201802', '1641250201701', '1620332201801', '1640892201901', '1620269201801', '1640560201701', '1640982201901', '1642066201901', '0800695201901', '1650808201901', '1640857201801', '0900102201901', '1200007201801', '0200505201801', '1620322201801', '1650981201801', '1650982201801', '1641134201701', '0300512201801', '0400667201801', '1650832201901', '0101719201901', '0100789201901', '0500444201901', '0800106201901', '0600234201901', '0501258201801', '1640377201901', '0500147201901', '1610729201801', '1610393201801', '1641060201901', '1620291201801', '0800547201901', '1101122201901', '1500446201901', '1640850201901', '1641134201901', '1641170201901', '1640851201901', '0900102201902', '1000409201901', '0500122201901', '0500452201901', '1630708201801', '1640857201901', '1101122201902', '1640993201901', '1631042201801', '1640214201901', '1620335201701', '0300597201801', '0400056201801', '1641474201702', '1640677201901', '1620269201802', '1641182201901', '1610679201801', '0501178201901', '1610679201702', '1640892201701', '0101684201701', '1610724201801', '1642051201901', '1642051201901') limit 10"
+            "SELECT cid as mms_cid, recl_desc as sandgrvqryreclamation, recl_dol as sandgrvqryreclamationcost, backslope as sandgrvqryreclamationbackfill, oper1_ind, oper2_ind, oper3_ind, alr_ind as SANDGRVQRYWITHINAGLANDRES, srb_ind as sandgrvqrylocalgovsoilrembylaw, pdist_ar as SANDGRVQRYDISTURBEDAREA, t_vol as SANDGRVQRYTIMBERVOLUME, edist_Ar as SANDGRVQRYTOTALEXISTDISTAREA, act1_ind, act2_ind, act3_ind, act4_ind, act1_ar, act2_ar, act3_ar, act4_ar, act1_vol, act2_vol, act3_vol, act4_vol from mms.mmssci_n where cid not in ('1610619201301', '1610619201301', '1610619201301', '1610619201301', '1641179201301', '1641180201301', '0900204199901', '1500363201301', '0101302201301', '1650080201302', '1200007201301', '1640974201301', '1101939201301', '0101702201901', '1630649201301', '0900127201301', '1641174201301', '1630124201301', '1620269201301', '1641128201301', '1650234201301', '1630742201501', '1101692201702', '0900221201501', '0101525201501', '0900168201501', '0900168201502', '0900204201701', '1641474201701', '1641366201601', '1620702201601', '1630215201701', '0101228201601', '1620574201601', '1641330201602', '1620818201701', '0200279201701', '1641971201701', '1650885201601', '1631026201701', '0101046201601', '0900133201701', '1641254201402', '1641200201701', '1621397201401', '1650395201601', '0900102201401', '0800688201601', '0400625201501', '0900063201702', '1640993201701', '0900220201501', '0900217201501', '0101046201401', '1620010201501', '1641329201601', '0400426201701', '0900133201702', '1621438201501', '1642069201901', '0500368201501', '1641330201601', '1630513201601', '1640772201601', '1101246201501', '1641430201601', '0900204201703', '0900016201701', '1630734201501', '0101327201401', '1610675201501', '1650528201501', '0900204201702', '1641476201701', '1641013201402', '1620494201401', '1650617201701', '0900168201701', '1630539201701', '0900063201701', '1000409201601', '1640150201601', '1641384201601', '0800407201602', '1641119201401', '1650861201401', '1640847201601', '1620498201701', '0800612201701', '1641179201601', '0900163201601', '0501122201602', '1101692201701', '1630080201701', '0603333201701', '0200279201401', '0900223201501', '0700568201701', '0500297201701', '1640851201701', '1101122201401', '1641354201501', '1650611201501', '0400724201502', '1641281201401', '0500910201401', '0101050201501', '0900168201601', '0600402201601', '0900127201401', '0101684201401', '1630484201601', '1650080201401', '1650294201401', '1650816201301', '1641252201401', '0900222201501', '1650832201401', '1650892201501', '1641311201501', '0101228201501', '1620501201501', '1630292201501', '1630419201401', '1620335201402', '0900218201501', '1641256201501', '1640870201401', '0400724201501', '1650898201501', '1650902201501', '0101058201801', '1640983201701', '1640863201701', '0101098201901', '0400704201801', '1650615201901', '0101231201901', '1641200201901', '1640850201701', '1610376201801', '1630712201701', '0400056201703', '0900197201902', '1641200201902', '1631059201901', '0101703201901', '1621123201901', '1621636201701', '0800029201701', '0200381201901', '1500530201701', '1642042201801', '0101058201901', '1650617201901', '0900145201701', '1650087201901', '1650294201901', '1610632201901', '1640982201701', '1620381201801', '1650753201901', '1650563201901', '1631030201801', '0700541201801', '1640235201802', '1620291201701', '1630272201701', '1631056201701', '1641424201801', '0400625201901', '1650616201901', '1641060201701', '1621123201801', '1101529201801', '1620530201801', '1630646201901', '1640943201801', '0501178201701', '0400851201801', '1620381201802', '1641250201701', '1620332201801', '1640892201901', '1620269201801', '1640560201701', '1640982201901', '1642066201901', '0800695201901', '1650808201901', '1640857201801', '0900102201901', '1200007201801', '0200505201801', '1620322201801', '1650981201801', '1650982201801', '1641134201701', '0300512201801', '0400667201801', '1650832201901', '0101719201901', '0100789201901', '0500444201901', '0800106201901', '0600234201901', '0501258201801', '1640377201901', '0500147201901', '1610729201801', '1610393201801', '1641060201901', '1620291201801', '0800547201901', '1101122201901', '1500446201901', '1640850201901', '1641134201901', '1641170201901', '1640851201901', '0900102201902', '1000409201901', '0500122201901', '0500452201901', '1630708201801', '1640857201901', '1101122201902', '1640993201901', '1631042201801', '1640214201901', '1620335201701', '0300597201801', '0400056201801', '1641474201702', '1640677201901', '1620269201802', '1641182201901', '1610679201801', '0501178201901', '1610679201702', '1640892201701', '0101684201701', '1610724201801', '1642051201901', '1642051201901') limit 10"
         )
 
         sand_grv_qry_activity_app_cols = etl.cut(sand_grv_qry_activity, 
-            'mms_cid', 'oper1_ind', 'oper2_ind', 'oper3_ind', 'SANDGRVQRYRECLAMATION',
-            'SANDGRVQRYRECLAMATIONCOST', 'SANDGRVQRYRECLAMATIONBACKFILL',
-            'SANDGRVQRYWITHINAGLANDRES', 'SANDGRVQRYLOCALGOVSOILREMBYLAW',
+            'mms_cid', 'oper1_ind', 'oper2_ind', 'oper3_ind', 'sandgrvqryreclamation',
+            'sandgrvqryreclamationcost', 'sandgrvqryreclamationbackfill',
+            'SANDGRVQRYWITHINAGLANDRES', 'sandgrvqrylocalgovsoilrembylaw',
             'SANDGRVQRYTOTALEXISTDISTAREA', 'SANDGRVQRYDISTURBEDAREA', 'SANDGRVQRYTIMBERVOLUME',
         )
 
@@ -85,9 +85,9 @@ def ETL_MMS_NOW_schema(connection, tables):
         print('-------------------------------------------------------')
 
         sand_grv_qry_activity = etl.cutout(sand_grv_qry_activity, 
-            'oper1_ind', 'oper2_ind', 'oper3_ind', 'SANDGRVQRYRECLAMATION',
-            'SANDGRVQRYRECLAMATIONCOST', 'SANDGRVQRYRECLAMATIONBACKFILL',
-            'SANDGRVQRYWITHINAGLANDRES', 'SANDGRVQRYLOCALGOVSOILREMBYLAW',
+            'oper1_ind', 'oper2_ind', 'oper3_ind', 'sandgrvqryreclamation',
+            'sandgrvqryreclamationcost', 'sandgrvqryreclamationbackfill',
+            'SANDGRVQRYWITHINAGLANDRES', 'sandgrvqrylocalgovsoilrembylaw',
             'SANDGRVQRYTOTALEXISTDISTAREA'
         )
         
@@ -277,22 +277,22 @@ def ETL_MMS_NOW_schema(connection, tables):
 
         cut_lines = etl.fromdb(
             connection,
-            f'SELECT cid as mms_cid, line_km as CutLinesExplGridTotalLineKms, t_vol as CutLinesExplGridTimberVolume, recl_desc as CutLinesReclamation, recl_dol as CutLinesReclamationCost, t_ar as CutLinesExplGridDisturbedArea from mms.mmssco_n'
+            f'SELECT cid as mms_cid, line_km as CutLinesExplGridTotalLineKms, t_vol as CutLinesExplGridTimberVolume, recl_desc as CutLinesReclamation, recl_dol as CutLinesReclamationcost, t_ar as CutLinesExplGridDisturbedArea from mms.mmssco_n'
         )
 
         applications = etl.outerjoin(applications, cut_lines, key='mms_cid')
 
         exploration_access = etl.fromdb(
             connection,
-            f'SELECT cid as mms_cid, recl_desc as ExpAccessReclamation, recl_dol as ExpAccessReclamationCost, act1_ind, act2_ind, act3_ind, act4_ind, act5_ind, act6_ind, act7_ind, act1_len, act2_len, act3_len, act4_len, act5_len, act6_len, act7_len, act1_ar, act2_ar, act3_ar, act4_ar, act5_ar, act6_ar, act7_ar, act1_vol, act2_vol, act3_vol, act4_vol, act5_vol, act6_vol, act7_vol from mms.mmssce_n'
+            f'SELECT cid as mms_cid, recl_desc as ExpAccessReclamation, recl_dol as ExpAccessReclamationcost, act1_ind, act2_ind, act3_ind, act4_ind, act5_ind, act6_ind, act7_ind, act1_len, act2_len, act3_len, act4_len, act5_len, act6_len, act7_len, act1_ar, act2_ar, act3_ar, act4_ar, act5_ar, act6_ar, act7_ar, act1_vol, act2_vol, act3_vol, act4_vol, act5_vol, act6_vol, act7_vol from mms.mmssce_n'
         )
 
         exploration_access_app_cols = etl.cut(exploration_access, [
-            'mms_cid', 'ExpAccessReclamation', 'ExpAccessReclamationCost'
+            'mms_cid', 'ExpAccessReclamation', 'ExpAccessReclamationcost'
         ])
 
         exploration_access = etl.cutout(exploration_access, [
-            'ExpAccessReclamation', 'ExpAccessReclamationCost'
+            'ExpAccessReclamation', 'ExpAccessReclamationcost'
         ])
 
         exploration_access_activity_detail = etl.fromdb(
@@ -411,15 +411,15 @@ def ETL_MMS_NOW_schema(connection, tables):
 
         exploration_surface_drill = etl.fromdb(
             connection,
-            f'SELECT cid as mms_cid, recl_desc as ExpSurfaceDrillReclamation, recl_dol as ExpSurfaceDrillReclamationCost, storage_desc as ExpSurfaceDrillReclCoreStorage, act1_ind, act2_ind, act3_ind, act4_ind, act5_ind, act6_ind, act7_ind, act8_ind, act1_cnt, act2_cnt, act3_cnt, act4_cnt, act5_cnt, act6_cnt, act7_cnt, act8_cnt, act1_ar, act2_ar, act3_ar, act4_ar, act5_ar, act6_ar, act7_ar, act8_ar, act1_vol, act2_vol, act3_vol, act4_vol, act5_vol, act6_vol, act7_vol, act8_vol from mms.mmsscd_n'
+            f'SELECT cid as mms_cid, recl_desc as ExpSurfaceDrillReclamation, recl_dol as ExpSurfaceDrillReclamationcost, storage_desc as ExpSurfaceDrillReclCoreStorage, act1_ind, act2_ind, act3_ind, act4_ind, act5_ind, act6_ind, act7_ind, act8_ind, act1_cnt, act2_cnt, act3_cnt, act4_cnt, act5_cnt, act6_cnt, act7_cnt, act8_cnt, act1_ar, act2_ar, act3_ar, act4_ar, act5_ar, act6_ar, act7_ar, act8_ar, act1_vol, act2_vol, act3_vol, act4_vol, act5_vol, act6_vol, act7_vol, act8_vol from mms.mmsscd_n'
         )
 
         exploration_surface_drill_app_cols = etl.cut(exploration_surface_drill, [
-            'mms_cid', 'ExpSurfaceDrillReclamation', 'ExpSurfaceDrillReclamationCost', 'ExpSurfaceDrillReclCoreStorage'
+            'mms_cid', 'ExpSurfaceDrillReclamation', 'ExpSurfaceDrillReclamationcost', 'ExpSurfaceDrillReclCoreStorage'
         ])
 
         exploration_surface_drill = etl.cutout(exploration_surface_drill, [
-            'ExpSurfaceDrillReclamation', 'ExpSurfaceDrillReclamationCost', 'ExpSurfaceDrillReclCoreStorage'
+            'ExpSurfaceDrillReclamation', 'ExpSurfaceDrillReclamationcost', 'ExpSurfaceDrillReclCoreStorage'
         ])
 
         exploration_surface_drill_activity_detail = etl.fromdb(
@@ -552,15 +552,15 @@ def ETL_MMS_NOW_schema(connection, tables):
 
         mech_trenching = etl.fromdb(
             connection,
-            f'SELECT cid as mms_cid, recl_desc as MechTrenchingReclamation, recl_dol as MechTrenchingReclamationCost, material_desc as surfacebulksamplereclsephandl, drainage_desc as surfacebulksamplerecldrainmiti, act1_ind, act2_ind, act1_cnt, act2_cnt, act1_ar, act2_ar, act1_vol, act2_vol from mms.mmsscb_n'
+            f'SELECT cid as mms_cid, recl_desc as MechTrenchingReclamation, recl_dol as MechTrenchingReclamationcost, material_desc as surfacebulksamplereclsephandl, drainage_desc as surfacebulksamplerecldrainmiti, act1_ind, act2_ind, act1_cnt, act2_cnt, act1_ar, act2_ar, act1_vol, act2_vol from mms.mmsscb_n'
         )
 
         mech_trenching_app_cols = etl.cut(mech_trenching, [
-            'mms_cid', 'MechTrenchingReclamation', 'MechTrenchingReclamationCost'
+            'mms_cid', 'MechTrenchingReclamation', 'MechTrenchingReclamationcost'
         ])
 
         mech_trenching = etl.cutout(mech_trenching, [
-            'MechTrenchingReclamation', 'MechTrenchingReclamationCost'
+            'MechTrenchingReclamation', 'MechTrenchingReclamationcost'
         ])
 
         mech_trenching_activity_detail = etl.fromdb(
@@ -602,15 +602,15 @@ def ETL_MMS_NOW_schema(connection, tables):
 
         under_exp_activity = etl.fromdb(
             connection,
-            f'SELECT cid as mms_cid, recl_desc as UnderExpReclamation, recl_dol as UnderExpReclamationCost, t_ar as UNDEREXPSURFACETOTALDISTAREA, t_vol as UNDEREXPSURFACETIMBERVOLUME, devr1_ind, devr2_ind, devr3_ind, devr4_ind, devr5_ind, devr6_ind, devr7_ind, devr8_ind, devr1_ct, devr2_ct, devr3_ct, devr4_ct, devr5_ct, devr6_ct, devr7_ct, devr8_ct, devn1_ind, devn2_ind, devn3_ind, devn4_ind, devn5_ind, devn6_ind, devn7_ind, devn8_ind, devn1_ct, devn2_ct, devn3_ct, devn4_ct, devn5_ct, devn6_ct, devn7_ct, devn8_ct, surf1_ind, surf2_ind, surf3_ind, surf4_ind, surf7_ind, surf8_ind, surf9_ind, surf10_ind, surf1_ct, surf2_ct, surf3_ct, surf4_ct, surf7_ct, surf8_ct, surf9_ct, surf10_ct, surf1_ar, surf2_ar, surf3_ar, surf4_ar, surf7_ar, surf8_ar, surf9_ar, surf10_ar, surf1_vol, surf2_vol, surf3_vol, surf4_vol, surf7_vol, surf8_vol, surf9_vol, surf10_vol from mms.mmsscg_n'
+            f'SELECT cid as mms_cid, recl_desc as UnderExpReclamation, recl_dol as UnderExpReclamationcost, t_ar as UNDEREXPSURFACETOTALDISTAREA, t_vol as UNDEREXPSURFACETIMBERVOLUME, devr1_ind, devr2_ind, devr3_ind, devr4_ind, devr5_ind, devr6_ind, devr7_ind, devr8_ind, devr1_ct, devr2_ct, devr3_ct, devr4_ct, devr5_ct, devr6_ct, devr7_ct, devr8_ct, devn1_ind, devn2_ind, devn3_ind, devn4_ind, devn5_ind, devn6_ind, devn7_ind, devn8_ind, devn1_ct, devn2_ct, devn3_ct, devn4_ct, devn5_ct, devn6_ct, devn7_ct, devn8_ct, surf1_ind, surf2_ind, surf3_ind, surf4_ind, surf7_ind, surf8_ind, surf9_ind, surf10_ind, surf1_ct, surf2_ct, surf3_ct, surf4_ct, surf7_ct, surf8_ct, surf9_ct, surf10_ct, surf1_ar, surf2_ar, surf3_ar, surf4_ar, surf7_ar, surf8_ar, surf9_ar, surf10_ar, surf1_vol, surf2_vol, surf3_vol, surf4_vol, surf7_vol, surf8_vol, surf9_vol, surf10_vol from mms.mmsscg_n'
         )
 
         under_exp_activity_app_cols = etl.cut(mech_trenching, [
-            'mms_cid', 'UnderExpReclamation', 'UnderExpReclamationCost', 'UNDEREXPSURFACETOTALDISTAREA', 'UNDEREXPSURFACETIMBERVOLUME'
+            'mms_cid', 'UnderExpReclamation', 'UnderExpReclamationcost', 'UNDEREXPSURFACETOTALDISTAREA', 'UNDEREXPSURFACETIMBERVOLUME'
         ])
 
         under_exp_activity = etl.cutout(mech_trenching, [
-            'UnderExpReclamation', 'UnderExpReclamationCost', 'UNDEREXPSURFACETOTALDISTAREA', 'UNDEREXPSURFACETIMBERVOLUME'
+            'UnderExpReclamation', 'UnderExpReclamationcost', 'UNDEREXPSURFACETOTALDISTAREA', 'UNDEREXPSURFACETIMBERVOLUME'
         ])
         
         under_exp_surface_activity_detail = etl.fromdb(
@@ -873,7 +873,7 @@ def ETL_MMS_NOW_schema(connection, tables):
 
         camps = etl.fromdb(
             connection,
-            f'SELECT cid as mms_cid, act1_ar as CampDisturbedArea, act1_vol as CampTimberVolume, act2_ar as BldgDisturbedArea, act2_vol as BldgTimberVolume, act3_ar as StgeDisturbedArea, act3_vol as StgeTimberVolume, recl_desc as CBSFReclamation, recl_dol as CBSFReclamationCost from mms.mmssca_n'
+            f'SELECT cid as mms_cid, act1_ar as CampDisturbedArea, act1_vol as CampTimberVolume, act2_ar as BldgDisturbedArea, act2_vol as BldgTimberVolume, act3_ar as StgeDisturbedArea, act3_vol as StgeTimberVolume, recl_desc as CBSFReclamation, recl_dol as CBSFReclamationcost from mms.mmssca_n'
         )
 
         applications = etl.outerjoin(applications, camps, key='mms_cid')
