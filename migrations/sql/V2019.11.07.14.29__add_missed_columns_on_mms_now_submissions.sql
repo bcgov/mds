3f6c9cfe2af39c6477a478bd06d529fb9f134d46
justinmacaulay <justinkmacaulay@gmail.com>
2019-11-07 15:35:09 -0800

    miss ordered the drop table statements.

--- migrations/sql/V2019.11.07.14.29__add_missed_columns_on_mms_now_submissions.sql
+++ migrations/sql/V2019.11.07.14.29__add_missed_columns_on_mms_now_submissions.sql
@@ -6,6 +6,6 @@ alter table MMS_NOW_Submissions.application add column PLACERDISTURBEDAREA numer
 alter table MMS_NOW_Submissions.application add column PLACERTIMBERVOLUME numeric(14,2);
 alter table MMS_NOW_Submissions.application add column PLACERTOTALEXISTDISTAREA numeric(14,2);
 
-DROP TABLE IF EXISTS MMS_NOW_Submissions.settling_pond;
 DROP TABLE IF EXISTS MMS_NOW_Submissions.existing_settling_pond_xref;
 DROP TABLE IF EXISTS MMS_NOW_Submissions.proposed_settling_pond_xref;
+DROP TABLE IF EXISTS MMS_NOW_Submissions.settling_pond;
