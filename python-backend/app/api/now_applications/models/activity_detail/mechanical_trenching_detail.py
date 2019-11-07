a3002bb39ae33f875180d82b66ff839eb5d9558e
Jason <jasyrotuck@gmail.com>
2019-11-07 11:34:18 -0800

    unintended removal

--- python-backend/app/api/now_applications/models/activity_detail/mechanical_trenching_detail.py
+++ python-backend/app/api/now_applications/models/activity_detail/mechanical_trenching_detail.py
@@ -9,6 +9,10 @@ from app.api.now_applications.models.activity_detail.activity_detail_base import
 
 
 class MechanicalTrenchingDetail(ActivityDetailBase):
+    __mapper_args__ = {
+        'polymorphic_identity': 'mechanical_trenching', ## type code
+    }
+
     ## NO TABLE FOR THIS TYPE
 
     def __repr__(self):
