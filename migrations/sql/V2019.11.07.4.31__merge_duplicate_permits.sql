7cd4cba3ecf4da461dc0b882bb2ea1b98847e31c
Ryan <ryan_rondeau@live.ca>
2019-11-08 12:33:25 -0800

    Update V2019.11.07.4.31__merge_duplicate_permits.sql

--- migrations/sql/V2019.11.07.4.31__merge_duplicate_permits.sql
+++ migrations/sql/V2019.11.07.4.31__merge_duplicate_permits.sql
@@ -5,11 +5,6 @@ GROUP BY permit_no, mine_guid
 HAVING count(permit_no) > 1;
 
 
-UPDATE permit_amendment SET permit_id=
-(select dp.permit_id from duplicate_permits dp join permit p on dp.permit_no = p.permit_no and dp.mine_guid=p.mine_guid where 
-p.permit_id=permit_id);
-
-
 SELECT permit_amendment_id, permit_id, 
 (select dp.permit_id from duplicate_permits dp join permit p on dp.permit_no = p.permit_no and dp.mine_guid=p.mine_guid where 
 p.permit_id=permit_amendment.permit_id) as new_permit_id
@@ -22,5 +17,5 @@ WHERE permit_amendment_id IN
 UPDATE permit_amendment SET permit_id=(select new_permit_id from duplicate_permit_mapping dp WHERE permit_amendment.permit_amendment_id=dp.permit_amendment_id)
 WHERE permit_amendment_id IN (select permit_amendment_id from duplicate_permit_mapping);
 
-
+DELETE FROM mine_party_appt where permit_guid in (select p.permit_id from permit p left join permit_amendment pa on p.permit_id=pa.permit_id where pa.permit_id is null);
 DELETE FROM permit where permit_id in (select p.permit_id from permit p left join permit_amendment pa on p.permit_id=pa.permit_id where pa.permit_id is null);
