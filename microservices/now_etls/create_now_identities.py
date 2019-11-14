0da238ca1bf4ead0b1dcd525264b115bc70b7091
Ryan <ryan_rondeau@live.ca>
2019-11-14 01:01:27 -0800

    Update create_now_identities.py

--- microservices/now_etls/create_now_identities.py
+++ microservices/now_etls/create_now_identities.py
@@ -12,7 +12,7 @@ def create_and_update_now_identities(connection):
         print('INSERT_NOW_SUBMISSION_IDENTITIES')
         INSERT_NOW_SUBMISSION_IDENTITIES = """
         INSERT INTO public.now_application_identity
-        SELECT gen_random_uuid(), null, messageid, null, 'now-etl-mds', now(), 'now-etl-mds', now(), (SELECT mine_guid FROM public.mine WHERE public.mine.mine_no=now_submissions.application.minenumber) as mine_guid
+        SELECT gen_random_uuid(), null, messageid, null, 'now-etl-mds', now(), 'now-etl-mds', now(), (SELECT mine_guid FROM public.mine WHERE public.mine.mine_no=now_submissions.application.minenumber limit 1) as mine_guid
         FROM now_submissions.application
         WHERE EXISTS (SELECT mine_guid FROM public.mine WHERE public.mine.mine_no=now_submissions.application.minenumber)
         AND NOT EXISTS (SELECT messageid from public.now_application_identity);
@@ -30,7 +30,7 @@ def create_and_update_now_identities(connection):
         print('INSERT_NOW_IDENTITIES_FOR_MMS_ONLY_APPLICATIONS')
         INSERT_NOW_IDENTITIES_FOR_MMS_ONLY_APPLICATIONS = """
         INSERT INTO public.now_application_identity
-        SELECT gen_random_uuid(), null, messageid, null, 'now-etl-mds', now(), 'now-etl-mds', now(), (SELECT mine_guid FROM public.mine WHERE public.mine.mine_no=mms_now_submissions.application.minenumber) as mine_guid
+        SELECT gen_random_uuid(), null, messageid, null, 'now-etl-mds', now(), 'now-etl-mds', now(), (SELECT mine_guid FROM public.mine WHERE public.mine.mine_no=mms_now_submissions.application.minenumber limit 1) as mine_guid
         FROM mms_now_submissions.application
         WHERE EXISTS (SELECT mine_guid FROM public.mine WHERE public.mine.mine_no=mms_now_submissions.application.minenumber)
         AND NOT EXISTS (SELECT mms_cid from public.now_application_identity);
