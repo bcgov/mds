b4b4babec9773615ec16c14e9acbb9a64f1e0f62
Jason <jasyrotuck@gmail.com>
2019-11-13 20:47:36 -0800

    don't import non-existent file.

--- python-backend/app/api/mms_now_submissions/models/__init__.py
+++ python-backend/app/api/mms_now_submissions/models/__init__.py
@@ -7,7 +7,6 @@ from .exp_surface_drill_activity import *
 from .mech_trenching_activity import *
 from .placer_activity import *
 from .sand_grv_qry_activity import *
-from .settling_pond import *
 from .surface_bulk_sample_activity import *
 from .under_exp_new_activity import *
 from .under_exp_rehab_activity import *
