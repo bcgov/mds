4dba84d1394e4d8ba7fb04ee6476ca976d80cd67
Meredith Mumby <meredith@freshworks.io>
2019-11-20 15:29:10 -0800

    added base quary for getting basic mine data and turning it to a csv

--- python-backend/app/api/mines/mine/resources/mine_csv_resource.py
+++ python-backend/app/api/mines/mine/resources/mine_csv_resource.py
@@ -1,17 +1,33 @@
 from flask import request, Response
 from flask_restplus import Resource, reqparse
+from sqlalchemy import cast, String
+from sqlalchemy.sql.functions import coalesce
 
 from ..models.mine import Mine
+
 from app.extensions import api
 from ....utils.access_decorators import requires_role_view_all
-# from ....utils.resources_mixins import UserMixin, ErrorMixin
 
 
 class MineCSVResource(Resource):
-    parser = reqparse.RequestParser()
-
     @api.doc(description='Returns a CSV of basic mine info.')
     @requires_role_view_all
     def get(self):
-        csv = Mine.to_csv(['mine_guid', 'mine_name', 'major_mine_ind', 'mine_region'])
-        return Response(csv, mimetype='text/csv')
+        q = Mine.query.with_entities(cast(Mine.mine_guid, String), coalesce(cast(Mine.mine_name, String), ''), cast(Mine.mine_no, String), cast(Mine.major_mine_ind, String), coalesce(cast(Mine.mine_region, String), '')).limit(10).all()
+        rows = ["\""+'","'.join(r)+"\"" for r in q]
+        csv = '\n'.join(rows)
+
+
+        return Response(csv, mimetype='text/csv')        
+        # return Response(q, mimetype='text/csv')
+
+
+    #         @classmethod
+    # def to_csv(cls, records, columns):
+    #     rows = [','.join(columns)]
+    #     for record in records:
+    #         row = []
+    #         for column in columns:
+    #             row.append(str(getattr(record, column)))
+    #         rows.append(','.join(row))
+    #     return '\n'.join(rows)
