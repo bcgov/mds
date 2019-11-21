71e3a474e8d2d2c68a8a80afe62f95f5ad568549
Meredith Mumby <meredith@freshworks.io>
2019-11-21 11:19:10 -0800

    added a postgres view

--- python-backend/app/api/mines/mine/models/mine_csv_view.py
+++ python-backend/app/api/mines/mine/models/mine_csv_view.py
@@ -11,23 +11,23 @@ from sqlalchemy.ext.hybrid import hybrid_property
 
 
 class MineCSVView(Base):
-    __tablename__ = ""
+    __tablename__ = "bcgw_view"
     mine_guid = db.Column(UUID(as_uuid=True), primary_key=True)
     mine_name = db.Column(db.String(60))
     mine_no = db.Column(db.String(10))
     mine_region = db.Column(db.String)
     major_mine_ind = db.Column(db.String)
     operating_status = db.Column(db.String)
-    mine_operation_status_codes = db.Column(db.String)
-    status_since = db.Column(db.String)
-    mine_tenure_type = db.Column(db.String)
-    mine_tenure_type_code = db.Column(db.String)
-    mine_commodity = db.Column(db.String)
-    mine_commodity_code = db.Column(db.String)
-    mine_disturbance = db.Column(db.String)
-    mine_disturbance_code = db.Column(db.String)
+    operating_status_code = db.Column(db.String)
+    effective_date = db.Column(db.String)
+    tenure = db.Column(db.String)
+    tenure_code = db.Column(db.String)
+    commodity = db.Column(db.String)
+    commodity_code = db.Column(db.String)
+    disturbance = db.Column(db.String)
+    disturbance_code = db.Column(db.String)
     permit_no = db.Column(db.String)
-    permittee_name = db.Column(db.String)
+    permittee_party_name = db.Column(db.String)
 
     def csv_row(self):
         model = inspect(self.__class__)
