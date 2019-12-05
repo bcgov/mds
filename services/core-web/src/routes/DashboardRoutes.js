251df2ac0321751b988d15c35c2a6bc433f1acc8
Ryan <ryan_rondeau@live.ca>
2019-12-04 16:45:04 -0800

    update route component

--- services/core-web/src/routes/DashboardRoutes.js
+++ services/core-web/src/routes/DashboardRoutes.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import * as routes from "@/constants/routes";
 import PageNotFound from "@/components/common/PageNotFound";
 
 const DashboardRoutes = () => (
   <Switch>
+    <Route
+      exact
+      path={routes.MINE_SUMMARY.route}
+      render={({ match }) => <Redirect to={routes.MINE_GENERAL.dynamicRoute(match.params.id)} />}
+    />
     <Route path={routes.MINE_SUMMARY.route} component={routes.MINE_SUMMARY.component} />
     {Object.values(routes).map((route) => (
       <Route exact path={route.route} component={route.component} />
