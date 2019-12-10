0169db52fcaf28d9a238a130f0fa55a0f2f965d7
Ryan <ryan_rondeau@live.ca>
2019-12-10 09:13:26 -0800

    Fix depth bug

--- services/core-api/app/api/now_applications/transmogrify_now.py
+++ services/core-api/app/api/now_applications/transmogrify_now.py
@@ -323,7 +323,7 @@ def _transmogrify_placer_operations(now_app, now_sub, mms_now_sub):
                 timber_volume=proposed.timbervolume,
                 width=getattr(proposed, 'width', None),
                 length=getattr(proposed, 'length', None),
-                depth=proposed.depth,
+                depth=getattr(existing, 'depth', None),
                 quantity=proposed.quantity)
 
             etl_detail = app_models.ETLActivityDetail(placeractivityid=proposed.placeractivityid)
@@ -350,7 +350,7 @@ def _transmogrify_placer_operations(now_app, now_sub, mms_now_sub):
                     timber_volume=existing.timbervolume,
                     width=getattr(existing, 'width', None),
                     length=getattr(existing, 'length', None),
-                    depth=existing.depth,
+                    depth=getattr(existing, 'depth', None),
                     quantity=existing.quantity)
 
                 etl_detail = app_models.ETLActivityDetail(
