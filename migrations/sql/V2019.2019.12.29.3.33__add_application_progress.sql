dcc59e938614443ae5916eb20ed405b47ec47fc5
Meredith Mumby <meredith@freshworks.io>
2019-12-12 13:42:37 -0800

    updated migration script, deleted unused code

--- migrations/sql/V2019.2019.12.29.3.33__add_application_progress.sql
+++ migrations/sql/V2019.2019.12.29.3.33__add_application_progress.sql
@@ -1,33 +1,3 @@
-
-
--- CREATE TABLE IF NOT EXISTS now_application_progress_status (
---   application_progress_status_code character varying(3) PRIMARY KEY,
---   description character varying(100) NOT NULL,
---   active_ind boolean DEFAULT true NOT NULL,
---   create_user character varying(60) NOT NULL,
---   create_timestamp timestamp with time zone DEFAULT now() NOT NULL,
---   update_user character varying(60) NOT NULL,
---   update_timestamp timestamp with time zone DEFAULT now() NOT NULL
--- );
--- ALTER TABLE now_application_progress_status OWNER TO mds;
-
--- CREATE TABLE IF NOT EXISTS now_application_progress (
---   application_progress_id SERIAL PRIMARY KEY REFERENCES now_application(now_application_id),
---   now_application_id integer,
---   application_progress_status_code character varying(3) REFERENCES now_application_progress_status(application_progress_status_code),
---   active_ind boolean,
---   end_date character varying(60),
---   start_date timestamp with time zone DEFAULT now() NOT NULL,
---   created_by character varying(60) NOT NULL,
---   create_user character varying(60) NOT NULL,
---   create_timestamp timestamp with time zone DEFAULT now() NOT NULL,
---   update_user character varying(60) NOT NULL,
---   update_timestamp timestamp with time zone DEFAULT now() NOT NULL
--- );
-
--- ALTER TABLE now_application_progress OWNER TO mds;
--- COMMENT ON TABLE now_application_progress IS 'Records tracking the progress of a Notice of Work application';
-
 CREATE TABLE IF NOT EXISTS now_application_progress_status (
   application_progress_status_code character varying(3)PRIMARY KEY,
   description character varying(100) NOT NULL,
