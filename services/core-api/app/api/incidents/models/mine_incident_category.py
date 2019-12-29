90a39eec5bb01acfc7397fc6fb5063a0b4f0e072
Luke <lukegamitchell@gmail.com>
2019-12-29 11:31:26 -0800

    sort incident categories by new column, display_order

--- services/core-api/app/api/incidents/models/mine_incident_category.py
+++ services/core-api/app/api/incidents/models/mine_incident_category.py
@@ -11,8 +11,9 @@ class MineIncidentCategory(AuditMixin, Base):
     __tablename__ = 'mine_incident_category'
     mine_incident_category_code = db.Column(db.String(3), primary_key=True)
     description = db.Column(db.String, nullable=False)
+    display_order = db.Column(db.Integer, nullable=False)
     active_ind = db.Column(db.Boolean, nullable=False, server_default=FetchedValue())
 
     @classmethod
     def active(cls):
-        return cls.query.filter_by(active_ind=True).order_by(cls.description).all()
+        return cls.query.filter_by(active_ind=True).order_by(cls.display_order).all()
