5b3824f75bd8a6f373a8e0e02c4ddba3603b1799
Luke <lukegamitchell@gmail.com>
2020-01-04 22:25:37 -0800

    create form input component for updating lead inspector

--- services/core-web/src/components/Forms/noticeOfWork/UpdateNOWLeadInspectorForm.js
+++ services/core-web/src/components/Forms/noticeOfWork/UpdateNOWLeadInspectorForm.js
@@ -1,44 +1,45 @@
-// import React from "react";
-// import PropTypes from "prop-types";
-// import { Field, reduxForm } from "redux-form";
-// import { Form, Col, Row } from "antd";
-// import * as FORM from "@/constants/forms";
-// import { required } from "@/utils/Validate";
-// import { renderConfig } from "@/components/common/config";
-// import CustomPropTypes from "@/customPropTypes";
+import React from "react";
+import { Field, reduxForm } from "redux-form";
+import { Form } from "antd";
+import PropTypes from "prop-types";
+import * as FORM from "@/constants/forms";
+import { required } from "@/utils/Validate";
+import { renderConfig } from "@/components/common/config";
+import CustomPropTypes from "@/customPropTypes";
 
-// const propTypes = {
-//   initialValues: PropTypes.objectOf(PropTypes.any).isRequired,
-//   inspectors: CustomPropTypes.groupOptions.isRequired,
-// };
+const propTypes = {
+  inspectors: CustomPropTypes.groupOptions.isRequired,
+  setLeadInspectorPartyGuid: PropTypes.func.isRequired,
+};
 
-// export const UpdateNOWLeadInspectorForm = (props) => (
-//   <div>
-//     <Form layout="vertical">
-//       <Row gutter={48}>
-//         <Col>
-//           <Form.Item>
-//             <Field
-//               id="lead_inspector_party_guid"
-//               name="lead_inspector_party_guid"
-//               label="Lead Inspector*"
-//               placeholder="Start typing lead inspector name"
-//               component={renderConfig.GROUPED_SELECT}
-//               validate={[required]}
-//               data={props.inspectors}
-//             />
-//           </Form.Item>
-//         </Col>
-//       </Row>
-//     </Form>
-//   </div>
-// );
+const UpdateNOWLeadInspectorForm = (props) => {
+  return (
+    <div>
+      <Form layout="vertical">
+        <Form.Item>
+          <Field
+            id="lead_inspector_party_guid"
+            name="lead_inspector_party_guid"
+            label="Lead Inspector"
+            component={renderConfig.GROUPED_SELECT}
+            placeholder="Start typing lead inspector name"
+            validate={[required]}
+            data={props.inspectors}
+            onSelect={props.setLeadInspectorPartyGuid}
+            doNotPinDropdown
+          />
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
 
-// UpdateNOWLeadInspectorForm.propTypes = propTypes;
+UpdateNOWLeadInspectorForm.propTypes = propTypes;
 
-// export default reduxForm({
-//   form: FORM.UPDATE_NOW_LEAD_INSPECTOR,
-//   destroyOnUnmount: false,
-//   forceUnregisterOnUnmount: true,
-//   touchOnBlur: true,
-// })(UpdateNOWLeadInspectorForm);
+export default reduxForm({
+  form: FORM.UPDATE_NOW_LEAD_INSPECTOR,
+  destroyOnUnmount: false,
+  forceUnregisterOnUnmount: true,
+  touchOnBlur: true,
+  enableReinitialize: true,
+})(UpdateNOWLeadInspectorForm);
