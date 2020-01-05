22ca10540657da001ce07f80b8f7156521fac86a
Luke <lukegamitchell@gmail.com>
2020-01-04 22:31:06 -0800

    create component for assigning lead inspector in verification step

---
+++ services/core-web/src/components/noticeOfWork/applications/verification/VerifyNOWMineConfirmation.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { bindActionCreators } from "redux";
+import { Button, Result, Col, Row } from "antd";
+import { connect } from "react-redux";
+import { getFormValues } from "redux-form";
+import PropTypes from "prop-types";
+import * as FORM from "@/constants/forms";
+import CustomPropTypes from "@/customPropTypes";
+import UpdateNOWLeadInspectorForm from "@/components/Forms/noticeOfWork/UpdateNOWLeadInspectorForm";
+
+const propTypes = {
+  noticeOfWork: CustomPropTypes.nowApplication.isRequired,
+  inspectors: CustomPropTypes.groupOptions.isRequired,
+  updateLeadInspectorFormValues: PropTypes.objectOf(PropTypes.any),
+  setLeadInspectorPartyGuid: PropTypes.func.isRequired,
+  handleUpdateLeadInspector: PropTypes.func.isRequired,
+};
+
+const defaultProps = {
+  updateLeadInspectorFormValues: {},
+};
+
+const invalidUpdateLeadInspectorPayload = (values) => !values.lead_inspector_party_guid;
+
+const VerifyNOWMineConfirmation = (props) => {
+  return (
+    <div className="tab__content">
+      <Row>
+        <Col lg={{ span: 12, offset: 6 }} md={{ span: 16, offset: 4 }} xs={{ span: 20, offset: 2 }}>
+          <Result
+            status="success"
+            title="Successfully Confirmed Mine"
+            subTitle="Please assign a Lead Inspector to continue to Technical Review."
+            extra={[
+              <UpdateNOWLeadInspectorForm
+                initialValues={{
+                  lead_inspector_party_guid: props.noticeOfWork.lead_inspector_party_guid,
+                }}
+                inspectors={props.inspectors}
+                setLeadInspectorPartyGuid={props.setLeadInspectorPartyGuid}
+              />,
+              <Button
+                type="primary"
+                onClick={props.handleUpdateLeadInspector}
+                disabled={invalidUpdateLeadInspectorPayload(props.updateLeadInspectorFormValues)}
+              >
+                Ready for Technical Review
+              </Button>,
+            ]}
+          />
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  updateLeadInspectorFormValues: getFormValues(FORM.UPDATE_NOW_LEAD_INSPECTOR)(state) || {},
+});
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({}, dispatch);
+
+VerifyNOWMineConfirmation.propTypes = propTypes;
+
+VerifyNOWMineConfirmation.defaultProps = defaultProps;
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(VerifyNOWMineConfirmation);
