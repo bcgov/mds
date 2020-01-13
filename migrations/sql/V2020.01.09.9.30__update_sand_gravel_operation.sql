46d3c842cd27ac2885f399bcf59a2bd276f9d991
Meredith Mumby <meredith@freshworks.io>
2020-01-13 14:31:31 -0800

    updated seed file and migration script to change column name

--- migrations/sql/V2020.01.09.9.30__update_sand_gravel_operation.sql
+++ migrations/sql/V2020.01.09.9.30__update_sand_gravel_operation.sql
@@ -8,15 +8,16 @@ ADD FOREIGN KEY (average_overburden_depth_unit_type_code) REFERENCES unit_type(u
 ADD FOREIGN KEY (average_top_soil_depth_unit_type_code) REFERENCES unit_type(unit_type_code) DEFERRABLE INITIALLY DEFERRED;
 
 
-UPDATE unit_type set unit = 't' where unit_type_code = 'MTN';
-UPDATE unit_type set unit = 'm3' where unit_type_code = 'MEC';
-UPDATE unit_type set unit = 'ha' where unit_type_code = 'HA';
-UPDATE unit_type set unit = 'deg' where unit_type_code = 'DEG';
-UPDATE unit_type set unit = '%' where unit_type_code = 'PER';
-UPDATE unit_type set unit = 'm' where unit_type_code = 'MTR';
+ALTER TABLE unit_type RENAME COLUMN unit TO short_description;
+UPDATE unit_type set short_description = 't' where unit_type_code = 'MTN';
+UPDATE unit_type set short_description = 'm3' where unit_type_code = 'MEC';
+UPDATE unit_type set short_description = 'ha' where unit_type_code = 'HA';
+UPDATE unit_type set short_description = 'deg' where unit_type_code = 'DEG';
+UPDATE unit_type set short_description = '%' where unit_type_code = 'PER';
+UPDATE unit_type set short_description = 'm' where unit_type_code = 'MTR';
 
 INSERT INTO unit_type
-(unit_type_code, unit, description, active_ind, create_user, update_user)
+(unit_type_code, short_description, description, active_ind, create_user, update_user)
 VALUES
   ('KMT', 'km', 'Kilometer ', true, 'system-mds', 'system-mds')
 on conflict do nothing;
