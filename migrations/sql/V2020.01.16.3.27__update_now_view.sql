c2ca670483c07eb332fcf0ad997bbbfca8c18785
Meredith Mumby <meredith@freshworks.io>
2020-01-17 12:54:59 -0800

    updated

--- migrations/sql/V2020.01.16.3.27__update_now_view.sql
+++ migrations/sql/V2020.01.16.3.27__update_now_view.sql
@@ -2,9 +2,11 @@ DROP VIEW notice_of_work_view;
 CREATE OR REPLACE VIEW notice_of_work_view
 	AS
 SELECT nid.now_application_guid,
-m.mine_guid, 
+m.mine_guid,
 m.mine_no,
 nid.now_number,
+app.lead_inspector_party_guid,
+concat_ws (' ', p.first_name, p.party_name) AS lead_inspector_name,
 COALESCE(nowt.description,  sub.noticeofworktype, msub.noticeofworktype) as notice_of_work_type_description,
 COALESCE(sub.status, nows.description) as now_application_status_description,
 COALESCE(app.received_date, sub.receiveddate, msub.receiveddate) as received_date,
@@ -14,5 +16,6 @@ JOIN mine m on nid.mine_guid = m.mine_guid
 LEFT JOIN now_submissions.application sub on nid.messageid = sub.messageid
 LEFT JOIN mms_now_submissions.application msub on nid.mms_cid = msub.mms_cid
 LEFT JOIN now_application app on nid.now_application_id=app.now_application_id
+LEFT JOIN party p on app.lead_inspector_party_guid=p.party_guid
 LEFT JOIN notice_of_work_type nowt on app.notice_of_work_type_code=nowt.notice_of_work_type_code
 LEFT JOIN now_application_status nows on app.now_application_status_code=nows.now_application_status_code;
