c6811977ccfb367a7c5de11b654c9a382b49f755
Ryan <ryan_rondeau@live.ca>
2020-01-28 13:39:09 -0800

    remote python file

--- services/digdag/etl/tasks/reporting_database_restore.py
+++
@@ -1,8 +0,0 @@
-from models.pod import POD
-import os
-from util.env_builder import EnvBuilder
-
-def run_job():
-    pod = POD(pod_name='digdag-reporting-database-restore', env_pod='mds-database-backup')
-    pod.create_pod()
-    print("Job finished")
