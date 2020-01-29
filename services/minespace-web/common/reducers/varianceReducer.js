c1dcf75f24c14109808b33c84aa4662e73b03c00
justinmacaulay <justinkmacaulay@gmail.com>
2020-01-29 14:09:29 -0800

    added missed thinsgs to minespace.

--- services/minespace-web/common/reducers/varianceReducer.js
+++ services/minespace-web/common/reducers/varianceReducer.js
@@ -1,5 +1,5 @@
-import * as actionTypes from "@/constants/actionTypes";
-import { VARIANCES } from "@/constants/reducerTypes";
+import * as actionTypes from "../constants/actionTypes";
+import { VARIANCES } from "../constants/reducerTypes";
 
 const initialState = {
   variances: [],
