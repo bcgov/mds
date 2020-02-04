286ff645c5cf5e6d259cd80cfa1b48264799c522
Luke Mitchell <exzizt@users.noreply.github.com>
2020-02-04 13:28:52 -0800

    update gutters for Overview tab and LandingPage content

--- services/minespace-web/src/components/dashboard/mine/overview/Overview.js
+++ services/minespace-web/src/components/dashboard/mine/overview/Overview.js
@@ -52,7 +52,7 @@ export class Overview extends Component {
       : null;
     const mineManager = mineManagers && mineManagers.length > 0 ? mineManagers[0].party : null;
     return (
-      <Row>
+      <Row gutter={[0, 16]}>
         <Col lg={{ span: 14 }} xl={{ span: 16 }}>
           <Title level={4}>Overview</Title>
           <Paragraph>
@@ -100,7 +100,7 @@ export class Overview extends Component {
                 : Strings.NONE}
             </Descriptions.Item>
           </Descriptions>
-          <Row>
+          <Row gutter={[16, 16]}>
             <Col xl={{ span: 11 }} xxl={{ span: 10 }}>
               <ContactCard
                 title="Mine Manager"
@@ -115,114 +115,120 @@ export class Overview extends Component {
           </Row>
         </Col>
         <Col lg={{ span: 9, offset: 1 }} xl={{ offset: 1, span: 7 }}>
-          <Card title="Regional Ministry Contacts">
-            <Paragraph>
-              <Text strong className="ministry-contact-title">
-                Senior Health, Safety and Environment Inspector
-              </Text>
-              <br />
-              <Text>{this.state.contacts.safety.name}</Text>
-              <br />
-              <Text>{this.state.contacts.safety.phone}</Text>
-              <br />
-              <Text>
-                <a href={`mailto:${this.state.contacts.safety.email}`}>
-                  {this.state.contacts.safety.email}
-                </a>
-              </Text>
-            </Paragraph>
-            <Paragraph>
-              <Text strong className="ministry-contact-title">
-                Senior Permitting Inspector
-              </Text>
-              <br />
-              {// There is only one contact who doesn't have a name, so conditionally render this field.
-              this.state.contacts.permitting.name && (
-                <Text>
-                  {this.state.contacts.permitting.name}
+          <Row gutter={[0, 16]}>
+            <Col>
+              <Card title="Regional Ministry Contacts">
+                <Paragraph>
+                  <Text strong className="ministry-contact-title">
+                    Senior Health, Safety and Environment Inspector
+                  </Text>
                   <br />
-                </Text>
-              )}
-              <Text>{this.state.contacts.permitting.phone}</Text>
-              <br />
-              <Text>
-                <a href={`mailto:${this.state.contacts.permitting.email}`}>
-                  {this.state.contacts.permitting.email}
-                </a>
-              </Text>
-            </Paragraph>
-            <Paragraph>
-              <Text strong className="ministry-contact-title">
-                Regional Director
-              </Text>
-              <br />
-              <Text>{this.state.contacts.director.name}</Text>
-              <br />
-              <Text>{this.state.contacts.director.phone}</Text>
-              <br />
-              <Text>
-                <a href={`mailto:${this.state.contacts.director.email}`}>
-                  {this.state.contacts.director.email}
-                </a>
-              </Text>
-            </Paragraph>
-            <Paragraph>
-              <Text strong className="ministry-contact-title">
-                Regional Office
-              </Text>
-              <br />
-              <Text>{this.state.contacts.office.phone}</Text>
-              <br />
-              <Text>
-                <a href={`mailto:${this.state.contacts.office.email}`}>
-                  {this.state.contacts.office.email}
-                </a>
-              </Text>
-            </Paragraph>
-          </Card>
-          <Card title="General Ministry Contacts">
-            {this.props.mine.major_mine_ind && (
-              <Paragraph>
-                <Text strong className="ministry-contact-title">
-                  Major Mines Office
-                </Text>
-                <br />
-                <Text>
-                  <a href={`mailto:${Contacts.MM_OFFICE.email}`}>{Contacts.MM_OFFICE.email}</a>
-                </Text>
-              </Paragraph>
-            )}
-            <Paragraph>
-              <Text strong className="ministry-contact-title">
-                Chief Inspector of Mines
-              </Text>
-              <br />
-              <Text>{Contacts.CHIEF_INSPECTOR.name}</Text>
-              <br />
-              <Text>{Contacts.CHIEF_INSPECTOR.phone}</Text>
-              <br />
-              <Text>
-                <a href={`mailto:${Contacts.CHIEF_INSPECTOR.email}`}>
-                  {Contacts.CHIEF_INSPECTOR.email}
-                </a>
-              </Text>
-            </Paragraph>
-            <Paragraph>
-              <Text strong className="ministry-contact-title">
-                Executive Lead (Authorizations)
-              </Text>
-              <br />
-              <Text>{Contacts.EXEC_LEAD_AUTH.name}</Text>
-              <br />
-              <Text>{Contacts.EXEC_LEAD_AUTH.phone}</Text>
-              <br />
-              <Text>
-                <a href={`mailto:${Contacts.EXEC_LEAD_AUTH.email}`}>
-                  {Contacts.EXEC_LEAD_AUTH.email}
-                </a>
-              </Text>
-            </Paragraph>
-          </Card>
+                  <Text>{this.state.contacts.safety.name}</Text>
+                  <br />
+                  <Text>{this.state.contacts.safety.phone}</Text>
+                  <br />
+                  <Text>
+                    <a href={`mailto:${this.state.contacts.safety.email}`}>
+                      {this.state.contacts.safety.email}
+                    </a>
+                  </Text>
+                </Paragraph>
+                <Paragraph>
+                  <Text strong className="ministry-contact-title">
+                    Senior Permitting Inspector
+                  </Text>
+                  <br />
+                  {// There is only one contact who doesn't have a name, so conditionally render this field.
+                  this.state.contacts.permitting.name && (
+                    <Text>
+                      {this.state.contacts.permitting.name}
+                      <br />
+                    </Text>
+                  )}
+                  <Text>{this.state.contacts.permitting.phone}</Text>
+                  <br />
+                  <Text>
+                    <a href={`mailto:${this.state.contacts.permitting.email}`}>
+                      {this.state.contacts.permitting.email}
+                    </a>
+                  </Text>
+                </Paragraph>
+                <Paragraph>
+                  <Text strong className="ministry-contact-title">
+                    Regional Director
+                  </Text>
+                  <br />
+                  <Text>{this.state.contacts.director.name}</Text>
+                  <br />
+                  <Text>{this.state.contacts.director.phone}</Text>
+                  <br />
+                  <Text>
+                    <a href={`mailto:${this.state.contacts.director.email}`}>
+                      {this.state.contacts.director.email}
+                    </a>
+                  </Text>
+                </Paragraph>
+                <Paragraph>
+                  <Text strong className="ministry-contact-title">
+                    Regional Office
+                  </Text>
+                  <br />
+                  <Text>{this.state.contacts.office.phone}</Text>
+                  <br />
+                  <Text>
+                    <a href={`mailto:${this.state.contacts.office.email}`}>
+                      {this.state.contacts.office.email}
+                    </a>
+                  </Text>
+                </Paragraph>
+              </Card>
+            </Col>
+            <Col>
+              <Card title="General Ministry Contacts">
+                {this.props.mine.major_mine_ind && (
+                  <Paragraph>
+                    <Text strong className="ministry-contact-title">
+                      Major Mines Office
+                    </Text>
+                    <br />
+                    <Text>
+                      <a href={`mailto:${Contacts.MM_OFFICE.email}`}>{Contacts.MM_OFFICE.email}</a>
+                    </Text>
+                  </Paragraph>
+                )}
+                <Paragraph>
+                  <Text strong className="ministry-contact-title">
+                    Chief Inspector of Mines
+                  </Text>
+                  <br />
+                  <Text>{Contacts.CHIEF_INSPECTOR.name}</Text>
+                  <br />
+                  <Text>{Contacts.CHIEF_INSPECTOR.phone}</Text>
+                  <br />
+                  <Text>
+                    <a href={`mailto:${Contacts.CHIEF_INSPECTOR.email}`}>
+                      {Contacts.CHIEF_INSPECTOR.email}
+                    </a>
+                  </Text>
+                </Paragraph>
+                <Paragraph>
+                  <Text strong className="ministry-contact-title">
+                    Executive Lead (Authorizations)
+                  </Text>
+                  <br />
+                  <Text>{Contacts.EXEC_LEAD_AUTH.name}</Text>
+                  <br />
+                  <Text>{Contacts.EXEC_LEAD_AUTH.phone}</Text>
+                  <br />
+                  <Text>
+                    <a href={`mailto:${Contacts.EXEC_LEAD_AUTH.email}`}>
+                      {Contacts.EXEC_LEAD_AUTH.email}
+                    </a>
+                  </Text>
+                </Paragraph>
+              </Card>
+            </Col>
+          </Row>
         </Col>
       </Row>
     );
--- services/minespace-web/src/components/pages/LandingPage.js
+++ services/minespace-web/src/components/pages/LandingPage.js
@@ -83,48 +83,54 @@ export const LandingPage = () => (
         </Row>
       </Col>
       <Col sm={{ span: 24 }} xl={{ span: 9, offset: 1 }} xxl={{ span: 8, offset: 3 }}>
-        <Card title="Do you have a BCeID?">
-          <Row>
-            <Col>
-              <Paragraph>
-                In order to access MineSpace, you need to register for a Business or Personal BCeID.
-                It can take several weeks to process the request, so give yourself plenty of lead
-                time.
-              </Paragraph>
-              <Paragraph>
-                Once you have your BCeID, you can add employees and delegates. You can request that
-                anyone added to your Business BCeID account be given access to MineSpace.
-              </Paragraph>
-            </Col>
-          </Row>
-          <Row type="flex" justify="center">
-            <Col>
-              <a
-                href="https://www.bceid.ca/register/business/getting_started/getting_started.aspx"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Button type="primary" size="large">
-                  Get a BCeID
-                </Button>
-              </a>
-            </Col>
-          </Row>
-        </Card>
-        <Card title="Questions?">
-          <Row>
-            <Col>
-              <Paragraph>
-                Please let us know about any questions or comments you have regarding your
-                experience using MineSpace.
-              </Paragraph>
-              <Paragraph>
-                Email us at&nbsp;
-                <a href={`mailto:${Strings.MDS_EMAIL}`}>{Strings.MDS_EMAIL}</a>.
-              </Paragraph>
-            </Col>
-          </Row>
-        </Card>
+        <Row gutter={[0, 16]}>
+          <Col>
+            <Card title="Do you have a BCeID?">
+              <Row>
+                <Col>
+                  <Paragraph>
+                    In order to access MineSpace, you need to register for a Business or Personal
+                    BCeID. It can take several weeks to process the request, so give yourself plenty
+                    of lead time.
+                  </Paragraph>
+                  <Paragraph>
+                    Once you have your BCeID, you can add employees and delegates. You can request
+                    that anyone added to your Business BCeID account be given access to MineSpace.
+                  </Paragraph>
+                </Col>
+              </Row>
+              <Row type="flex" justify="center">
+                <Col>
+                  <a
+                    href="https://www.bceid.ca/register/business/getting_started/getting_started.aspx"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Button type="primary" size="large">
+                      Get a BCeID
+                    </Button>
+                  </a>
+                </Col>
+              </Row>
+            </Card>
+          </Col>
+          <Col>
+            <Card title="Questions?">
+              <Row>
+                <Col>
+                  <Paragraph>
+                    Please let us know about any questions or comments you have regarding your
+                    experience using MineSpace.
+                  </Paragraph>
+                  <Paragraph>
+                    Email us at&nbsp;
+                    <a href={`mailto:${Strings.MDS_EMAIL}`}>{Strings.MDS_EMAIL}</a>.
+                  </Paragraph>
+                </Col>
+              </Row>
+            </Card>
+          </Col>
+        </Row>
       </Col>
     </Row>
   </div>
