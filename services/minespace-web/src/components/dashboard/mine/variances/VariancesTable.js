766353417b561aa4ee6edf39d41a9f8f91a41500
Meredith Mumby <meredith@freshworks.io>
2020-02-07 12:03:25 -0800

    updated link

--- services/minespace-web/src/components/dashboard/mine/variances/VariancesTable.js
+++ services/minespace-web/src/components/dashboard/mine/variances/VariancesTable.js
@@ -137,7 +137,9 @@ export class VariancesTable extends Component {
                     <LinkButton
                       title={text}
                       key={file.mine_document_guid}
-                      onClick={downloadFileFromDocumentManager(file)}
+                      onClick={() => {
+                        downloadFileFromDocumentManager(file);
+                      }}
                     >
                       {truncateFilename(file.document_name)}
                     </LinkButton>
