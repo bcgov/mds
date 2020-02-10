b476478eeb3fdccec06ccfbb76496fe0ba759849
Luke Mitchell <lukegamitchell@gmail.com>
2020-02-10 11:43:12 -0800

    Refactor Warning Banner style, composition, etc

--- services/core-web/src/components/common/WarningBanner.js
+++ services/core-web/src/components/common/WarningBanner.js
@@ -1,91 +1,81 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Alert } from "antd";
+import { Alert, Row, Col, Typography } from "antd";
 import { WARNING } from "@/constants/assets";
+
+const { Text } = Typography;
 
 const propTypes = {
   type: PropTypes.oneOf(["IE", "test", "mobile"]).isRequired,
   onClose: PropTypes.func.isRequired,
 };
 
-const renderIEBanner = () => (
-  <div className="inline-flex flex-center">
-    <img src={WARNING} alt="warning" className="padding-large--right" />
-    <div>
-      <h2>Internet Explorer is not a supported browser.</h2>
-      <p>If you continue with this browser the application may not work as intended.</p>
-      <p>
-        Please use a supported browser:
-        <a href="https://www.google.com/chrome" target="_blank" rel="noopener noreferrer">
-          {" "}
-          Chrome,
-        </a>
-        <a
-          href="https://www.mozilla.org/en-US/firefox/new"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {" "}
-          Firefox,
-        </a>
-        <a href="https://support.apple.com/en-ca/safari" target="_blank" rel="noopener noreferrer">
-          {" "}
-          Safari{" "}
-        </a>
-      </p>
-    </div>
-  </div>
-);
+const testWarningData = {
+  message: "This is the Core test site",
+  description:
+    "Features and layout could differ from the live production site and data entered in this system will not be saved.",
+};
 
-const renderTestBanner = () => (
-  <div className="inline-flex flex-center">
-    <img src={WARNING} alt="warning" className="padding-large--right" />
-    <div>
-      <h2>WARNING: This is the Core test Site</h2>
-      <p>
-        Features and layout will be constantly changing and will differ from the live production
-        site.{" "}
-      </p>
-      <p>Data entered in this system will not be saved.</p>
-    </div>
-  </div>
-);
+const mobileWarningData = {
+  message: "You are using a mobile device",
+  description: "The application may not work as intended in a mobile environment.",
+};
 
-const renderMobileWarningBanner = () => (
-  <div className="inline-flex flex-center">
-    <img src={WARNING} alt="warning" className="padding-large--right" />
-    <div>
-      <h2>You are currently using a mobile device to access this content.</h2>
-      <p> Be aware that the system may not work as expected in a mobile environment.</p>
-    </div>
-  </div>
-);
+const ieWarningData = {
+  message: "Internet Explorer is not supported",
+  description: (
+    <span>
+      The application may not work as intended. Please use a supported browser (
+      <a href="https://www.google.com/chrome" target="_blank" rel="noopener noreferrer">
+        Chrome,
+      </a>
+      &nbsp;
+      <a href="https://www.mozilla.org/en-US/firefox/new" target="_blank" rel="noopener noreferrer">
+        Firefox
+      </a>
+      , or&nbsp;
+      <a href="https://support.apple.com/en-ca/safari" target="_blank" rel="noopener noreferrer">
+        Safari
+      </a>
+      ).
+    </span>
+  ),
+};
 
-const WarningBanner = (props) => (
-  <div>
-    {props.type === "IE" && (
-      <Alert
-        style={{ backgroundColor: "#F3CD65" }}
-        message={renderIEBanner()}
-        type="warning"
-        closable
-        onClose={props.onClose}
-      />
-    )}
-    {props.type === "test" && (
-      <Alert style={{ backgroundColor: "#F3CD65" }} message={renderTestBanner()} type="warning" />
-    )}
-    {props.type === "mobile" && (
-      <Alert
-        style={{ backgroundColor: "#F3CD65" }}
-        message={renderMobileWarningBanner()}
-        type="warning"
-        closable
-        onClose={props.onClose}
-      />
-    )}
-  </div>
-);
+const WarningBanner = (props) => {
+  let warningData;
+  switch (props.type) {
+    case "test":
+      warningData = testWarningData;
+      break;
+    case "mobile":
+      warningData = mobileWarningData;
+      break;
+    case "IE":
+      warningData = ieWarningData;
+      break;
+  }
+
+  return (
+    warningData && (
+      <Row>
+        <Col>
+          <Alert
+            className="warning-banner"
+            message={<Text strong>{warningData.message}</Text>}
+            description={<Text>{warningData.description}</Text>}
+            type="warning"
+            closable
+            onClose={props.onClose}
+            showIcon
+            icon={<img src={WARNING} alt="Warning" />}
+          />
+        </Col>
+      </Row>
+    )
+  );
+};
 
 WarningBanner.propTypes = propTypes;
+
 export default WarningBanner;
