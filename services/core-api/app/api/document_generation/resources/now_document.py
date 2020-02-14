995ae06634d34067a3b0b40e0569f163516bc794
Luke Mitchell <lukegamitchell@gmail.com>
2020-02-14 15:25:22 -0800

    Tidy

--- services/core-api/app/api/document_generation/resources/now_document.py
+++ services/core-api/app/api/document_generation/resources/now_document.py
@@ -1,25 +1,20 @@
 import os
 import uuid
-from flask import Response, json, current_app, send_file, jsonify, request
+from flask import current_app, send_file, request
 from flask_restplus import Resource, fields
-from werkzeug.exceptions import NotFound, BadRequest, InternalServerError, NotImplemented
-from app.extensions import api
-from app.api.utils.resources_mixins import UserMixin
-from app.api.utils.access_decorators import requires_role_edit_permit
-from app.api.utils.custom_reqparser import CustomReqparser
-
-from app.api.constants import TIMEOUT_5_MINUTES
+from werkzeug.exceptions import NotFound, BadRequest, NotImplemented
 from app.extensions import api, cache
-from app.api.now_submissions.models.application import Application
-from app.api.now_submissions.response_models import APPLICATION
-from app.api.utils.access_decorators import requires_role_view_all
 from app.api.utils.resources_mixins import UserMixin
+from app.api.utils.access_decorators import requires_role_view_all, requires_role_edit_permit
+from app.api.utils.custom_reqparser import CustomReqparser
+from app.api.constants import TIMEOUT_5_MINUTES, NOW_DOCUMENT_TEMPLATE_TYPES
 
-DOCUMENT_TYPES = {
-    'CAL': 'Client Acknowledgment Letter Template (NoW).docx',
-    'RJL': 'Rejection Letter Template (NoW).docx',
-    'WDL': 'Withdrawl Letter Template (NoW).docx'
-}
+NOW_DOCUMENT_DOWNLOAD_TOKEN_MODEL = api.model('NoticeOfWorkDocumentDownloadToken',
+                                              {'token_guid': fields.String})
+
+
+def NOW_DOCUMENT_DOWNLOAD_TOKEN(token_guid):
+    return f'now-document-types:download-token:{token_guid}'
 
 
 class NoticeOfWorkDocumentGenerationResource(Resource, UserMixin):
@@ -33,7 +28,8 @@ class NoticeOfWorkDocumentGenerationResource(Resource, UserMixin):
     @requires_role_edit_permit
     def post(self, document_type_code):
         data = self.parser.parse_args()
-        if document_type_code not in DOCUMENT_TYPES:
+
+        if document_type_code not in NOW_DOCUMENT_TEMPLATE_TYPES:
             raise NotFound('Document type code not found')
 
         raise NotImplemented('This endpoint is under construction')
@@ -45,27 +41,21 @@ class NoticeOfWorkDocumentResource(Resource, UserMixin):
         params={'document_type_code': 'The code indicating the type of document to return.'})
     def get(self, document_type_code):
         token_guid = request.args.get('token', '')
-        token_data = cache.get(DOWNLOAD_TOKEN(token_guid))
-        cache.delete(DOWNLOAD_TOKEN(token_guid))
+        token_data = cache.get(NOW_DOCUMENT_DOWNLOAD_TOKEN(token_guid))
+        cache.delete(NOW_DOCUMENT_DOWNLOAD_TOKEN(token_guid))
 
         if not token_data:
             raise BadRequest('Valid token required for download')
 
-        if document_type_code not in DOCUMENT_TYPES:
+        if document_type_code not in NOW_DOCUMENT_TEMPLATE_TYPES:
             raise NotFound('Document type code not found')
 
-        file_name = DOCUMENT_TYPES[document_type_code]
+        file_name = NOW_DOCUMENT_TEMPLATE_TYPES[document_type_code]
+
         return send_file(
             os.path.join(current_app.root_path, 'document_templates', 'now', file_name),
             as_attachment=True,
             attachment_filename=file_name)
-
-
-DOWNLOAD_TOKEN_MODEL = api.model('DownloadToken', {'token_guid': fields.String})
-
-
-def DOWNLOAD_TOKEN(token_guid):
-    return f'now-document-generation:download-token:{token_guid}'
 
 
 class NoticeOfWorkDocumentTokenResource(Resource, UserMixin):
@@ -75,15 +65,15 @@ class NoticeOfWorkDocumentTokenResource(Resource, UserMixin):
             'document_type_code':
             'The code indicating the type of document to retrieve a download token for.'
         })
-    @api.marshal_with(DOWNLOAD_TOKEN_MODEL, code=200)
+    @api.marshal_with(NOW_DOCUMENT_DOWNLOAD_TOKEN_MODEL, code=200)
     @requires_role_view_all
     def get(self, document_type_code):
-        if document_type_code not in DOCUMENT_TYPES:
+        if document_type_code not in NOW_DOCUMENT_TEMPLATE_TYPES:
             raise NotFound('Document type code not found')
 
         token_guid = uuid.uuid4()
         cache.set(
-            DOWNLOAD_TOKEN(token_guid), {'document_type_code': document_type_code},
+            NOW_DOCUMENT_DOWNLOAD_TOKEN(token_guid), {'document_type_code': document_type_code},
             TIMEOUT_5_MINUTES)
 
         return {'token_guid': token_guid}
