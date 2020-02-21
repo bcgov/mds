0bd85de72a936bf1b58836b0dbe120affb983696
justinmacaulay <justinkmacaulay@gmail.com>
2020-02-21 08:23:59 -0800

    changed the notice of work document URL.

--- services/core-web/common/constants/API.js
+++ services/core-web/common/constants/API.js
@@ -146,14 +146,14 @@ export const MINE_REPORT_COMMENT = (mineGuid, reportGuid, commentGuid) =>
 export const MINE_REPORT_STATUS = "/mines/reports/status-codes";
 
 // Notice Of Work
-export const NOTICE_OF_WORK_APPLICATIONS = (params = {}) =>
+export const NOTICE_OF_WORK_APPLICATION_LIST = (params = {}) =>
   `/now-applications?${queryString.stringify(params)}`;
 export const NOTICE_OF_WORK_APPLICATION = (applicationGuid) =>
   `/now-applications/${applicationGuid}`;
 export const NOTICE_OF_WORK_DOCUMENT_FILE_GET_URL = (id, applicationGuid, token = {}) =>
-  `${NOTICE_OF_WORK_APPLICATION(applicationGuid)}/document/${id}?${queryString.stringify(token)}`;
+  `/now-submissions/${applicationGuid}/document/${id}?${queryString.stringify(token)}`;
 export const NOTICE_OF_WORK_DOCUMENT_TOKEN_GET_URL = (id, applicationGuid) =>
-  `${NOTICE_OF_WORK_APPLICATION(applicationGuid)}/document/${id}/token`;
+  `/now-submissions/${applicationGuid}/document/${id}/token`;
 export const NOTICE_OF_WORK_APPLICATION_IMPORT = (applicationGuid) =>
   `/now-applications/${applicationGuid}/import`;
 export const NOTICE_OF_WORK_ACTIVITY_TYPE_OPTIONS = "/now-applications/activity-types";
