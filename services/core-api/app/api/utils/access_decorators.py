da13ea55061648a85b102a72f8c95bd91b6d48f9
Luke Mitchell <exzizt@users.noreply.github.com>
2020-02-21 19:16:07 -0800

    MDS-2644: Return 403 (Forbidden) instead of 401 (Unauthorized) for API calls which lack required permissions (#1214)
    Returns 403 (Forbidden) instead of 401 (Unauthorized) for API calls which lack required permissions (fixed now in Core and MineSpace)
    Improves the styling of the Mine Manager card to handle long emails
    image
    Uses the "required" styling for a field on CRR instead of putting the "*" in the label string

--- services/core-api/app/api/utils/access_decorators.py
+++ services/core-api/app/api/utils/access_decorators.py
@@ -1,5 +1,7 @@
 from functools import wraps
 from app.extensions import jwt
+from flask_jwt_oidc.exceptions import AuthError
+from werkzeug.exceptions import Forbidden
 
 VIEW_ALL = "core_view_all"
 MINE_EDIT = "core_edit_mines"
@@ -49,7 +51,10 @@ def requires_any_of(roles):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwds):
-            return jwt.has_one_of_roles(roles)(func)(*args, **kwds)
+            try:
+                return jwt.has_one_of_roles(roles)(func)(*args, **kwds)
+            except AuthError as e:
+                raise Forbidden(e.error['description'])
 
         wrapper.required_roles = _combine_role_flags(func, roles)
         return wrapper
