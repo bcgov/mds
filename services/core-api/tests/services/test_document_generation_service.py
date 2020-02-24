e5a42f6a1ecab8eabfef1af57e03c6fda210fb8b
Jason <jasyrotuck@gmail.com>
2020-02-24 10:11:14 -0800

    status code check

--- services/core-api/tests/services/test_document_generation_service.py
+++ services/core-api/tests/services/test_document_generation_service.py
@@ -13,6 +13,8 @@ def test_missing_template_returns_fail(test_client, db_session, auth_headers):
         file_download_resp = DocumentGeneratorService.generate_document_and_stream_response(
             template_path, {'mine_no': '1234456'})
 
+        assert file_download_resp.status_code == 200
+
         assert file_download_resp.headers['Content-Transfer-Encoding'] == 'binary'
         assert file_download_resp.headers[
             'Content-Disposition'] == 'attachment; filename=Rejection Letter Template (NoW).pdf'
