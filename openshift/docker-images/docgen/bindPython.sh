223751bb70dcd58f4bd5f437351e00effa487ce7
Paul Dittaro <pdittaro@gmail.com>
2020-02-25 09:28:34 -0800

    Change to alpine, add bindpython logic

---
+++ openshift/docker-images/docgen/bindPython.sh
@@ -0,0 +1,15 @@
+  
+#!/bin/bash -
+
+# bindPython.sh
+# Copyright (C) 2019 Nathan Albridge
+
+encodings_root=$(find /usr/lib/python* -type d -name 'encodings')
+python_root=$(dirname $encodings_root)
+
+cp /app/python /usr/lib/libreoffice/program
+ln -sf /usr/bin/python3 /usr/lib/libreoffice/program/python.bin
+ln -sf ${python_root} /usr/lib/libreoffice/program/python-core
+chmod a+rx /usr/lib/libreoffice/program/python
+
+exit 
