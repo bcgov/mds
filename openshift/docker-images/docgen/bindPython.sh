12e9083a8da01906fcee5b0d0921e3518cc79e68
Paul Dittaro <pdittaro@gmail.com>
2020-02-25 09:43:26 -0800

    Change to approot for bind Python

--- openshift/docker-images/docgen/bindPython.sh
+++ openshift/docker-images/docgen/bindPython.sh
@@ -7,7 +7,7 @@
 encodings_root=$(find /usr/lib/python* -type d -name 'encodings')
 python_root=$(dirname $encodings_root)
 
-cp /app/python /usr/lib/libreoffice/program
+cp  ${APP_ROOT}/python /usr/lib/libreoffice/program
 ln -sf /usr/bin/python3 /usr/lib/libreoffice/program/python.bin
 ln -sf ${python_root} /usr/lib/libreoffice/program/python-core
 chmod a+rx /usr/lib/libreoffice/program/python
