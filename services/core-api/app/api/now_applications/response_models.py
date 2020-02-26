42f3ce96249ec284689cb965af7cb367da66e6fc
Jason <jasyrotuck@gmail.com>
2020-02-26 14:37:02 -0800

    don't show empty context-value.

--- services/core-api/app/api/now_applications/response_models.py
+++ services/core-api/app/api/now_applications/response_models.py
@@ -11,13 +11,14 @@ DOCUMENT_TEMPLATE_FIELD_MODE = api.model(
         "type": fields.String,
         "placeholder": fields.String,
         "required": fields.Boolean(default=False),
+        "context-value": fields.String,
         "read-only": fields.Boolean(default=False),
     })
 
 DOCUMENT_TEMPLATE_MODEL = api.model(
     'DocumentTemplateModel', {
         'document_template_code': fields.String,
-        'form_spec': fields.List(fields.Nested(DOCUMENT_TEMPLATE_FIELD_MODE))
+        'form_spec': fields.List(fields.Nested(DOCUMENT_TEMPLATE_FIELD_MODE, skip_none=True))
     })
 
 
