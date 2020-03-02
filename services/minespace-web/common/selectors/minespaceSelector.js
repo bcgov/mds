11926a40f4667e030bfbc74498aaf5ecbaba9494
Luke Mitchell <lukegamitchell@gmail.com>
2020-03-01 21:25:22 -0800

    Fix common folder formatting issue

--- services/minespace-web/common/selectors/minespaceSelector.js
+++ services/minespace-web/common/selectors/minespaceSelector.js
@@ -1,5 +1,5 @@
-import * as minespaceReducer from "../reducers/minespaceReducer";
 import { createSelector } from "reselect";
+import * as minespaceReducer from "../reducers/minespaceReducer";
 
 export const getMinespaceUsers = (state) => minespaceReducer.getMinespaceUsers(state);
 
