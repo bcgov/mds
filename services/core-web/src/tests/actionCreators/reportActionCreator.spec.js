407aba3bf35a99d6e8c7d6360d81a051660b7e9f
Luke Mitchell <lukegamitchell@gmail.com>
2020-03-12 09:54:15 -0700

    Add new tests

---
+++ services/core-web/src/tests/actionCreators/reportActionCreator.spec.js
@@ -0,0 +1,67 @@
+import MockAdapter from "axios-mock-adapter";
+import axios from "axios";
+import { fetchMineReports, fetchReports } from "@common/actionCreators/reportActionCreator";
+import * as genericActions from "@common/actions/genericActions";
+import { ENVIRONMENT } from "@common/constants/environment";
+import * as API from "@common/constants/API";
+import * as MOCK from "@/tests/mocks/dataMocks";
+
+const dispatch = jest.fn();
+const requestSpy = jest.spyOn(genericActions, "request");
+const successSpy = jest.spyOn(genericActions, "success");
+const errorSpy = jest.spyOn(genericActions, "error");
+const mockAxios = new MockAdapter(axios);
+
+beforeEach(() => {
+  mockAxios.reset();
+  dispatch.mockClear();
+  requestSpy.mockClear();
+  successSpy.mockClear();
+  errorSpy.mockClear();
+});
+
+describe("`fetchMineReports` action creator", () => {
+  const mineGuid = "1234567";
+
+  const url = `${ENVIRONMENT.apiUrl}${API.MINE_REPORTS(mineGuid)}`;
+  it("Request successful, dispatches `success` with correct response", () => {
+    const mockResponse = { data: { success: true } };
+    mockAxios.onGet(url).reply(200, mockResponse);
+    return fetchMineReports(mineGuid)(dispatch).then(() => {
+      expect(requestSpy).toHaveBeenCalledTimes(1);
+      expect(successSpy).toHaveBeenCalledTimes(1);
+      expect(dispatch).toHaveBeenCalledTimes(5);
+    });
+  });
+
+  it("Request failure, dispatches `error` with correct response", () => {
+    mockAxios.onGet(url, MOCK.createMockHeader()).reply(418, MOCK.ERROR);
+    return fetchMineReports(mineGuid)(dispatch).then(() => {
+      expect(requestSpy).toHaveBeenCalledTimes(1);
+      expect(errorSpy).toHaveBeenCalledTimes(1);
+      expect(dispatch).toHaveBeenCalledTimes(4);
+    });
+  });
+});
+
+describe("`fetchReports` action creator", () => {
+  const url = `${ENVIRONMENT.apiUrl}${API.REPORTS()}`;
+  it("Request successful, dispatches `success` with correct response", () => {
+    const mockResponse = { data: { success: true } };
+    mockAxios.onGet(url).reply(200, mockResponse);
+    return fetchReports()(dispatch).then(() => {
+      expect(requestSpy).toHaveBeenCalledTimes(1);
+      expect(successSpy).toHaveBeenCalledTimes(1);
+      expect(dispatch).toHaveBeenCalledTimes(5);
+    });
+  });
+
+  it("Request failure, dispatches `error` with correct response", () => {
+    mockAxios.onGet(url, MOCK.createMockHeader()).reply(418, MOCK.ERROR);
+    return fetchReports()(dispatch).then(() => {
+      expect(requestSpy).toHaveBeenCalledTimes(1);
+      expect(errorSpy).toHaveBeenCalledTimes(1);
+      expect(dispatch).toHaveBeenCalledTimes(4);
+    });
+  });
+});
