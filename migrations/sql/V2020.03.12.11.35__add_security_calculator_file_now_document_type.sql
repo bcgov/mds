49245a92ac9985a10b168c7972287ca8667c1640
Luke Mitchell <lukegamitchell@gmail.com>
2020-03-12 13:22:43 -0700

    Rename/add to SQL file: create security_amount column in now_application

--- migrations/sql/V2020.03.12.11.35__add_security_calculator_file_now_document_type.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO now_application_document_type
-(now_application_document_type_code, description, active_ind, create_user, update_user)
-VALUES
-	('SBC', 'Security Calculation', true, 'system-mds', 'system-mds')
-on conflict do nothing;
