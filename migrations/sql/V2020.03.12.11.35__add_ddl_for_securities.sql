4e13501ab4a55dc6e76b7a16f2cb470c6d1e378a
Luke Mitchell <lukegamitchell@gmail.com>
2020-03-12 14:59:56 -0700

    Update ddl for securities

--- migrations/sql/V2020.03.12.11.35__add_ddl_for_securities.sql
+++ migrations/sql/V2020.03.12.11.35__add_ddl_for_securities.sql
@@ -2,9 +2,9 @@
 -- Add a new NoW document type for the security calculation document
 INSERT INTO now_application_document_type
 (now_application_document_type_code, description, active_ind, create_user, update_user)
-VALUES
-	('SCA', 'Security Calculation', true, 'system-mds', 'system-mds')
+VALUES ('SCD', 'Security Document', true, 'system-mds', 'system-mds')
 ON CONFLICT DO NOTHING;
 
--- Add a new column to NoW Application for the security amount
-ALTER TABLE now_application ADD COLUMN security_amount NUMERIC(10, 2);
+-- Add new columns to NoW Application for the security total and received date
+ALTER TABLE now_application ADD COLUMN security_total NUMERIC(10, 2);
+ALTER TABLE now_application ADD COLUMN security_received_date DATE;
