66f8530118c79e4ff921638890204693753e863a
unknown <unknown@example.com>
2020-03-16 12:15:11 -0700

    created 31000 mms only records in test. and 0 on second run.

--- tasks/now-etls/create_now_identities.py
+++ tasks/now-etls/create_now_identities.py
@@ -30,9 +30,8 @@ def create_and_update_now_identities(connection):
         print('INSERT_NOW_IDENTITIES_FOR_MMS_ONLY_APPLICATIONS')
         INSERT_NOW_IDENTITIES_FOR_MMS_ONLY_APPLICATIONS = """
         INSERT INTO public.now_application_identity
-        SELECT gen_random_uuid(), null, null, mms_cid, 'now-etl-mds', now(), 'now-etl-mds', now(), (SELECT mine_guid FROM public.mine WHERE public.mine.mine_no=mms_now_submissions.application.minenumber limit 1) as mine_guid
-        FROM mms_now_submissions.application
-        WHERE EXISTS (SELECT mine_guid FROM public.mine WHERE public.mine.mine_no=mms_now_submissions.application.minenumber)
-        AND NOT EXISTS (SELECT nai.mms_cid from public.now_application_identity nai inner join mms_now_submissions.application mms_nsa on nai.mms_cid=mms_nsa.mms_cid);
+        SELECT gen_random_uuid(), null, null, mms_cid, 'now-etl-mds', now(), 'now-etl-mds', now(), m.mine_guid
+        FROM mms_now_submissions.application mnsa inner join public.mine m on m.mine_no = mnsa.minenumber
+        where mms_cid NOT IN (SELECT distinct nai.mms_cid from public.now_application_identity nai where nai.mms_cid  is not null)
         """
         cursor.execute(INSERT_NOW_IDENTITIES_FOR_MMS_ONLY_APPLICATIONS)
