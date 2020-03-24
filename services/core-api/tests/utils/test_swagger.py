34c97143220d2962e0d6521ef963e5a3ac910ed1
unknown <unknown@example.com>
2020-03-24 11:28:48 -0700

    clean up

--- services/core-api/tests/utils/test_swagger.py
+++ services/core-api/tests/utils/test_swagger.py
@@ -1,13 +1,9 @@
-
 import json
-
-from tests.factories import BondFactory, MineFactory, PermitFactory, PartyFactory
-from app.api.now_applications.resources.now_application_list_resource import PAGE_DEFAULT, PER_PAGE_DEFAULT
 
 
 class TestSwagger:
     """GET /"""
-    def test_swagger_returns_success(self, test_client, db_session, auth_headers):
+    def test_swagger_returns_success(self, test_client, db_session):
         """Should return the correct records with a 200 response code"""
         get_resp = test_client.get('/swagger.json')
         assert get_resp.status_code == 200, get_resp.response
