1964f1963ebe1e9c6388a29c8f63a82362a70bd6
unknown <unknown@example.com>
2020-03-24 11:15:01 -0700

    wrong model to marshal with.

--- services/core-api/app/api/parties/party/resources/sub_division_code_resource.py
+++ services/core-api/app/api/parties/party/resources/sub_division_code_resource.py
@@ -5,11 +5,12 @@ from app.api.utils.access_decorators import requires_role_view_all
 from app.api.utils.resources_mixins import UserMixin
 
 from app.api.parties.party.models.sub_division_code import SubDivisionCode
+from app.api.parties.response_models import SUB_DIVISION_CODE_MODEL
 
 
 class SubDivisionCodeResource(Resource, UserMixin):
     @api.doc(params={})
     @requires_role_view_all
-    @api.marshal_with(SubDivisionCode, envelope='records', code=200)
+    @api.marshal_with(SUB_DIVISION_CODE_MODEL, envelope='records', code=200)
     def get(self):
         return SubDivisionCode.get_active()
