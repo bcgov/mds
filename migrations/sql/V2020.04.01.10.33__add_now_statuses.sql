7eda1c0d188f8376fa6386bdbc2b885bf67f46fb
Luke Mitchell <lukegamitchell@gmail.com>
2020-04-02 16:04:57 -0700

    Fix OOP

--- migrations/sql/V2020.04.01.10.33__add_now_statuses.sql
+++ migrations/sql/V2020.04.01.10.33__add_now_statuses.sql
@@ -1,11 +1,6 @@
 -- Create a new column for defining the desired display order of the statuses
 ALTER TABLE now_application_status ADD COLUMN display_order integer;
 
--- Delete all of the old Core statuses (Accepted, Under Review, and Withdrawn)
-DELETE FROM now_application_status WHERE now_application_status_code = 'ACC';
-DELETE FROM now_application_status WHERE now_application_status_code = 'UNR';
-DELETE FROM now_application_status WHERE now_application_status_code = 'WDN';
-
 -- Create new statuses (from MMS)
 INSERT INTO now_application_status
 (now_application_status_code, description, display_order, active_ind, create_user, update_user)
@@ -21,3 +16,11 @@ VALUES
 	('REJ', 'Rejected', 80, true, 'system-mds', 'system-mds'),
 	('CLO', 'Closed', 40, true, 'system-mds', 'system-mds')
 ON CONFLICT DO NOTHING;
+
+-- Set all of the current NoW statuses to the new default status (Submitted)
+UPDATE now_application SET now_application_status_code = 'SUB';
+
+-- Delete all of the old Core statuses (Accepted, Under Review, and Withdrawn)
+DELETE FROM now_application_status WHERE now_application_status_code = 'ACC';
+DELETE FROM now_application_status WHERE now_application_status_code = 'UNR';
+DELETE FROM now_application_status WHERE now_application_status_code = 'WDN';
