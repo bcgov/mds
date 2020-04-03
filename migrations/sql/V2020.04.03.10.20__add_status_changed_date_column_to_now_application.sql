7e2435dd8967198fe8460b5e4f845e61d2ad7eab
Luke Mitchell <lukegamitchell@gmail.com>
2020-04-03 15:13:29 -0700

    Fix query for future?

--- migrations/sql/V2020.04.03.10.20__add_status_changed_date_column_to_now_application.sql
+++ migrations/sql/V2020.04.03.10.20__add_status_changed_date_column_to_now_application.sql
@@ -2,4 +2,6 @@
 ALTER TABLE now_application ADD COLUMN IF NOT EXISTS status_updated_date DATE DEFAULT NOW() NOT NULL;
 
 -- Set each application's initial "status updated" date to be its submitted date
-UPDATE now_application now1 SET status_updated_date = (SELECT submitted_date FROM now_application now2 WHERE now1.now_application_id = now2.now_application_id);
+UPDATE now_application now1 SET status_updated_date =
+    (SELECT submitted_date FROM now_application now2 WHERE now1.now_application_id = now2.now_application_id)
+WHERE status_updated_date IS NULL;
