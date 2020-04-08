65174be3ba20ce85efaa626052dd750b0a96328b
Luke Mitchell <lukegamitchell@gmail.com>
2020-04-08 15:07:45 -0700

    Add new codes/regional contacts to seed data, fix typo (propnent --> proponent) in document templates, other

--- migrations/sql/V2020.04.08.10.14__create_regional_contacts.sql
+++ migrations/sql/V2020.04.08.10.14__create_regional_contacts.sql
@@ -15,21 +15,6 @@ ALTER TABLE regional_contact_type OWNER TO mds;
 
 COMMENT ON TABLE regional_contact_type IS 'The types of regional contacts.';
 
-INSERT INTO regional_contact_type
-    (
-    regional_contact_type_code,
-    description,
-    display_order,
-    create_user,
-    update_user
-    )
-VALUES
-    ('SHI', 'Senior Health, Safety and Environment Inspector', 30, 'system-mds', 'system-mds'),
-    ('SPI', 'Senior Permitting Inspector', 40, 'system-mds', 'system-mds'),
-    ('ROE', 'Regional Office', 20, 'system-mds', 'system-mds'),
-    ('RDR', 'Regional Director', 10, 'system-mds', 'system-mds')
-ON CONFLICT DO NOTHING;
-
 CREATE TABLE IF NOT EXISTS regional_contact
 (
     regional_contact_type_code          character varying(3)                     NOT NULL,
@@ -48,21 +33,3 @@ CREATE TABLE IF NOT EXISTS regional_contact
 ALTER TABLE regional_contact OWNER TO mds;
 
 COMMENT ON TABLE regional_contact IS 'Contains regional contacts information.';
-
-INSERT INTO regional_contact
-    (
-    regional_contact_type_code,
-    mine_region_code,
-    email,
-    phone_number,
-    fax_number,
-    mailing_address_line_1,
-    mailing_address_line_2
-    )
-VALUES
-    ('ROE', 'NE', 'MMD-PrinceGeorge@gov.bc.ca', '250 565-4240', '250 565-4328', '350-1011 4th Avenue', 'Prince George, B.C. V2L 3H9'),
-    ('ROE', 'NW', 'MMD-Smithers@gov.bc.ca', '250 847-7383', '250 847-7603', '2nd Floor, 3726 Alfred Avenue', 'Smithers, B.C. V0J 2N0'),
-    ('ROE', 'SC', 'MMD-Kamloops@gov.bc.ca', '250 371-3912', NULL, '2nd Floor, 441 Columbia Street', 'Kamloops, B.C. V2C 2T3'),
-    ('ROE', 'SE', 'MMD-Cranbrook@gov.bc.ca', '250 417-6134', NULL, '202-100 Cranbrook Street North', 'Cranbrook, B.C. V1C 3P9'),
-    ('ROE', 'SW', 'SouthwestMinesDivision@gov.bc.ca', '778 698-3649', '250 953-3878', 'PO Box 9395, STN PROV GOVT', 'Victoria, B.C. V8W 9M9')
-ON CONFLICT DO NOTHING;
--- migrations/sql/afterMigrate__1__seed_data.sql
+++ migrations/sql/afterMigrate__1__seed_data.sql
@@ -662,14 +662,14 @@ UPDATE document_template SET form_spec_json = '[
       "id": "proponent_address",
       "label": "Proponent Address",
       "type": "FIELD",
-      "placeholder": "Enter the propnent''s address",
+      "placeholder": "Enter the proponent''s address",
       "required": true
     },
     {
       "id": "proponent_name",
       "label": "Proponent Name",
       "type": "FIELD",
-      "placeholder": "Enter the propnent''s name",
+      "placeholder": "Enter the proponent''s name",
       "required": false
     },
     {
@@ -729,14 +729,14 @@ UPDATE document_template SET form_spec_json = '[
       "id": "proponent_address",
       "label": "Proponent Address",
       "type": "FIELD",
-      "placeholder": "Enter the propnent''s address",
+      "placeholder": "Enter the proponent''s address",
       "required": true
     },
     {
       "id": "proponent_name",
       "label": "Proponent Name",
       "type": "FIELD",
-      "placeholder": "Enter the propnent''s name",
+      "placeholder": "Enter the proponent''s name",
       "required": false
     },
     {
@@ -796,14 +796,14 @@ UPDATE document_template SET form_spec_json = '[
       "id": "proponent_address",
       "label": "Proponent Address",
       "type": "FIELD",
-      "placeholder": "Enter the propnent''s address",
+      "placeholder": "Enter the proponent''s address",
       "required": true
     },
     {
       "id": "proponent_name",
       "label": "Proponent Name",
       "type": "FIELD",
-      "placeholder": "Enter the propnent''s name",
+      "placeholder": "Enter the proponent''s name",
       "required": false
     },
     {
@@ -969,4 +969,37 @@ VALUES
     ('CSL', 'Confiscation of Security Letter', 'system-mds', 'system-mds'),
     ('REL', 'Reminder Letter', 'system-mds', 'system-mds'),
     ('AKL', 'Acknowledgement Letter', 'system-mds', 'system-mds')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO regional_contact_type
+    (
+    regional_contact_type_code,
+    description,
+    display_order,
+    create_user,
+    update_user
+    )
+VALUES
+    ('SHI', 'Senior Health, Safety and Environment Inspector', 30, 'system-mds', 'system-mds'),
+    ('SPI', 'Senior Permitting Inspector', 40, 'system-mds', 'system-mds'),
+    ('ROE', 'Regional Office', 20, 'system-mds', 'system-mds'),
+    ('RDR', 'Regional Director', 10, 'system-mds', 'system-mds')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO regional_contact
+    (
+    regional_contact_type_code,
+    mine_region_code,
+    email,
+    phone_number,
+    fax_number,
+    mailing_address_line_1,
+    mailing_address_line_2
+    )
+VALUES
+    ('ROE', 'NE', 'MMD-PrinceGeorge@gov.bc.ca', '250 565-4240', '250 565-4328', '350-1011 4th Avenue', 'Prince George, B.C. V2L 3H9'),
+    ('ROE', 'NW', 'MMD-Smithers@gov.bc.ca', '250 847-7383', '250 847-7603', '2nd Floor, 3726 Alfred Avenue', 'Smithers, B.C. V0J 2N0'),
+    ('ROE', 'SC', 'MMD-Kamloops@gov.bc.ca', '250 371-3912', NULL, '2nd Floor, 441 Columbia Street', 'Kamloops, B.C. V2C 2T3'),
+    ('ROE', 'SE', 'MMD-Cranbrook@gov.bc.ca', '250 417-6134', NULL, '202-100 Cranbrook Street North', 'Cranbrook, B.C. V1C 3P9'),
+    ('ROE', 'SW', 'SouthwestMinesDivision@gov.bc.ca', '778 698-3649', '250 953-3878', 'PO Box 9395, STN PROV GOVT', 'Victoria, B.C. V8W 9M9')
 ON CONFLICT DO NOTHING;
