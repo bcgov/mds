9c3f16346ae070d922768c3e33a1e01436924c7d
unknown <unknown@example.com>
2020-04-22 17:05:49 -0700

    no delete.

--- migrations/sql/V2020.04.22.16.42__squash_dup_permits_cleaup.sql
+++ migrations/sql/V2020.04.22.16.42__squash_dup_permits_cleaup.sql
@@ -6,8 +6,8 @@ ALTER TABLE public.mine_party_appt DROP COLUMN permit_guid;
 --delete newly orphaned records
 --none of these duplicate records have had their permittee's changed
 ALTER TABLE permit DROP CONSTRAINT if exists permit_mine_guid_fkey;
-delete from mine_party_appt where permit_id not in (select permit_id from public.mine_permit_xref);
-delete from permit_amendment where permit_id not in (select permit_id from mine_permit_xref);
-DELETE FROM permit where permit_id not in (select permit_id from mine_permit_xref);
-
 ALTER TABLE permit DROP COLUMN mine_guid
+--run this adhoc after validation
+-- delete from mine_party_appt where permit_id not in (select permit_id from public.mine_permit_xref);
+-- delete from permit_amendment where permit_id not in (select permit_id from mine_permit_xref);
+-- DELETE FROM permit where permit_id not in (select permit_id from mine_permit_xref);
