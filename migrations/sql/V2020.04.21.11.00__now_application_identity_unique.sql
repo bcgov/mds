a745366017cdc6ae15790718569a090dc1460511
Luke Mitchell <lukegamitchell@gmail.com>
2020-04-22 19:07:30 -0700

    Fix SQL syntax error

--- migrations/sql/V2020.04.21.11.00__now_application_identity_unique.sql
+++ migrations/sql/V2020.04.21.11.00__now_application_identity_unique.sql
@@ -1,4 +1,4 @@
-ALTER TABLE now_application_identity ADD CONSTRAINT now_application_identity_messageid_unique UNIQUE messageid;
-ALTER TABLE now_application_identity ADD CONSTRAINT now_application_identity_mms_cid_unique UNIQUE mms_cid;
-ALTER TABLE now_application_identity ADD CONSTRAINT now_application_identity_now_application_id_unique UNIQUE now_application_id;
-ALTER TABLE now_application_identity ADD CONSTRAINT now_application_identity_now_number_unique UNIQUE now_number;
+ALTER TABLE now_application_identity ADD CONSTRAINT now_application_identity_messageid_unique UNIQUE (messageid);
+ALTER TABLE now_application_identity ADD CONSTRAINT now_application_identity_mms_cid_unique UNIQUE (mms_cid);
+ALTER TABLE now_application_identity ADD CONSTRAINT now_application_identity_now_application_id_unique UNIQUE (now_application_id);
+ALTER TABLE now_application_identity ADD CONSTRAINT now_application_identity_now_number_unique UNIQUE (now_number);
