5381c9f14ac7ef7d5fa8db767c04f8432fb5502f
unknown <unknown@example.com>
2020-04-30 11:09:17 -0700

    don't need any of this.

--- migrations/sql/V2020.04.24.13.42__squash_dup_permit_permit_no.sql
+++
@@ -1,4 +0,0 @@
---delete orphaned and empty permit records
-DELETE FROM permit where permit_id not in (select permit_id from mine_permit_xref);
-
-ALTER TABLE public.permit ADD CONSTRAINT permit_permit_no_unique UNIQUE (permit_no);
