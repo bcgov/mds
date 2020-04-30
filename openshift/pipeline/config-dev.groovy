6178e6d48276b732633c7a5e97618fa8ff50608b
unknown <unknown@example.com>
2020-04-30 12:51:00 -0700

    revert specs.

--- openshift/pipeline/config-dev.groovy
+++ openshift/pipeline/config-dev.groovy
@@ -324,7 +324,7 @@ app {
 environments {
     'dev' {
         vars {
-            DB_PVC_SIZE = '10Gi'
+            DB_PVC_SIZE = '5Gi'
             DOCUMENT_PVC_SIZE = '1Gi'
             BACKUP_VERIFICATION_PVC_SIZE = '200Mi'
             LOG_PVC_SIZE = '1Gi'
@@ -368,7 +368,7 @@ environments {
                 }
                 python {
                     cpu_request = "50m"
-                    cpu_limit = "200m"
+                    cpu_limit = "100m"
                     memory_request = "256Mi"
                     memory_limit = "512Mi"
                     uwsgi_threads = 4
@@ -377,8 +377,8 @@ environments {
                     replica_max = 1
                 }
                 python_lite {
-                    cpu_request = "20m"
-                    cpu_limit = "200m"
+                    cpu_request = "10m"
+                    cpu_limit = "100m"
                     memory_request = "128Mi"
                     memory_limit = "256Mi"
                     uwsgi_threads = 2
@@ -387,10 +387,10 @@ environments {
                     replica_max = 1
                 }
                 postgres {
-                    cpu_request = "20m"
-                    cpu_limit = "200m"
-                    memory_request = "1Gi"
-                    memory_limit = "2Gi"
+                    cpu_request = "50m"
+                    cpu_limit = "100m"
+                    memory_request = "512Mi"
+                    memory_limit = "1Gi"
                 }
                 redis {
                     cpu_request = "10m"
