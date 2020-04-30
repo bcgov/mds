875e482d57170ec41dac891ba814fbd57f972bf7
Luke Mitchell <lukegamitchell@gmail.com>
2020-04-30 11:21:43 -0700

    Fix bugs in Provincial NoW searching, sorting, and filtering
    I can't wait until we have this page match the other provincial pages!

--- services/core-web/src/components/dashboard/noticeOfWorkHomePage/NoticeOfWorkSearch.js
+++ services/core-web/src/components/dashboard/noticeOfWorkHomePage/NoticeOfWorkSearch.js
@@ -1,30 +1,48 @@
 import React from "react";
 import { Row, Col } from "antd";
 import PropTypes from "prop-types";
+import { isEmpty } from "lodash";
 import NoticeOfWorkSearchForm from "@/components/Forms/noticeOfWork/NoticeOfWorkSearchForm";
 
 /**
  * @class NoticeOfWorkSearch supports searching for a filtered list of notice of Work applications.
  */
+
 const propTypes = {
   handleSearch: PropTypes.func.isRequired,
-  initialValues: PropTypes.objectOf(PropTypes.string).isRequired,
+  searchParams: PropTypes.objectOf(PropTypes.any).isRequired,
+  initialValues: PropTypes.objectOf(PropTypes.any).isRequired,
 };
 
-export const NoticeOfWorkSearch = (props) => (
-  <div>
-    <Row>
-      <Col md={{ span: 12, offset: 6 }} xs={{ span: 20, offset: 2 }}>
-        <span className="advanced-search__container">
-          <NoticeOfWorkSearchForm
-            onSubmit={props.handleSearch}
-            initialValues={props.initialValues}
-          />
-        </span>
-      </Col>
-    </Row>
-  </div>
-);
+export const NoticeOfWorkSearch = (props) => {
+  const handleSubmit = (handleSearch) => (values) => {
+    const params = {
+      ...props.searchParams,
+      ...values,
+    };
+
+    if (isEmpty(values)) {
+      Object.keys(props.initialValues).map((key) => delete params[key]);
+    }
+
+    handleSearch(params);
+  };
+
+  return (
+    <div>
+      <Row>
+        <Col md={{ span: 12, offset: 6 }} xs={{ span: 20, offset: 2 }}>
+          <span className="advanced-search__container">
+            <NoticeOfWorkSearchForm
+              onSubmit={handleSubmit(props.handleSearch)}
+              initialValues={props.initialValues}
+            />
+          </span>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 NoticeOfWorkSearch.propTypes = propTypes;
 
