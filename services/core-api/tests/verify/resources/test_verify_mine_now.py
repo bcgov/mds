1615e8d9abf4c15c29ad6a77dfe2b64fb06dd717
Ryan <ryan_rondeau@live.ca>
2020-05-01 15:10:59 -0700

    Add new verify endpoint

--- services/core-api/tests/verify/resources/test_verify_mine_now.py
+++ services/core-api/tests/verify/resources/test_verify_mine_now.py
@@ -6,11 +6,10 @@ from tests.now_application_factories import NOWApplicationIdentityFactory
 from tests.now_submission_factories import NOWSubmissionFactory
 
 
-class TestVerifyPermitMine:
-    """GET verify/permit/mine"""
-    def test_get_verify_permit_mine(self, test_client, db_session, auth_headers):
+class TestVerifyMineNoW:
+    """GET verify/mine/now"""
+    def test_get_verify_mine_now(self, test_client, db_session, auth_headers):
         mine = MineFactory(operating=True)
-        #by default, authorization_end_date in the PermitAmendmentFactory is >30days
         permit = PermitFactory(permit_no="CX-1", mine=mine, permit_amendments=1)
         now_app = NOWApplicationIdentityFactory(mine=mine)
         permit.permit_amendments[0].now_identity = now_app
@@ -18,7 +17,7 @@ class TestVerifyPermitMine:
         now_sub = NOWSubmissionFactory()
 
         get_resp = test_client.get(
-            f'/verify/permit/now?a_PermitNumber={permit.permit_no}',
+            f'/verify/mine/now?a_MineNumber={mine.mine_no}',
             headers=auth_headers['full_auth_header'])
         get_data = json.loads(get_resp.data.decode())
         assert get_resp.status_code == 200
