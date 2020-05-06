86d0454c06162993baee019ac97385012e6af06a
unknown <unknown@example.com>
2020-05-06 12:34:31 -0700

    zombie files.

--- migrations/sql/V2020.04.22.16.42__squash_dup_permits_cleaup.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE public.mine_party_appt ADD CONSTRAINT mine_party_appt_permit_party_xref_fk FOREIGN KEY (permit_id, mine_guid) REFERENCES mine_permit_xref(permit_id, mine_guid);
-
---clean up 
-ALTER TABLE public.mine_party_appt DROP CONSTRAINT mine_party_appt_permit_party_fk; 
-ALTER TABLE public.mine_party_appt DROP COLUMN permit_guid;
---none of these duplicate records have had their permittee's changed
-ALTER TABLE permit DROP CONSTRAINT if exists permit_mine_guid_fkey;
-ALTER TABLE permit DROP COLUMN mine_guid
