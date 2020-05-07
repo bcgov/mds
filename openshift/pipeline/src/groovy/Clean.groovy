91c202468165baca32ec94a115de2c3d1701db5b
Paul Dittaro <pdittaro@gmail.com>
2020-05-07 10:29:46 -0700

    Better conditional

--- openshift/pipeline/src/groovy/Clean.groovy
+++ openshift/pipeline/src/groovy/Clean.groovy
@@ -52,7 +52,7 @@ config.app.namespaces.each {String key, Map env->
         // Delete ImageStreamTags associated to a single PR (most others)
         Map ret = oc(['get','istag', """-o=jsonpath='{.items[?(@.tag.name=="build-pr-${config.app.git.changeId}")].metadata.name}'""", '--ignore-not-found=true', '-n', "${env.namespace}"])
         out = ret['out']
-        if (out != ''){
+        if (out && !out.allWhitespace){
             tags = out.split()
             println "Tags: ${tags}"
             tags.each { tag ->
