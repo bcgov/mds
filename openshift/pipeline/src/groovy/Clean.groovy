3c88d6e834f533e28d1a7eab29e4f50ae89d6654
Paul Dittaro <pdittaro@gmail.com>
2020-05-07 10:51:57 -0700

    Output tracing

--- openshift/pipeline/src/groovy/Clean.groovy
+++ openshift/pipeline/src/groovy/Clean.groovy
@@ -52,6 +52,7 @@ config.app.namespaces.each {String key, Map env->
         // Delete ImageStreamTags associated to a single PR (most others)
         Map ret = oc(['get','istag', """-o=jsonpath='{.items[?(@.tag.name=="build-pr-${config.app.git.changeId}")].metadata.name}'""", '--ignore-not-found=true', '-n', "${env.namespace}"])
         out = ret['out']
+        println "out: ${out}"
         if (out && !out.allWhitespace){
             tags = out.split()
             println "Tags: ${tags}"
