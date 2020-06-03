e9d0ae7a8ded0d93777e460303eb3f43cc04aac6
jsyro <jasyrotuck@gmail.com>
2020-06-03 14:00:50 -0700

    case statement for new run where etl table doesn't exist.

--- migrations/sql/V2020.05.05.16.42__3_permit_data_transform_rm_mine_guid.sql
+++ migrations/sql/V2020.05.05.16.42__3_permit_data_transform_rm_mine_guid.sql
@@ -1,9 +1,21 @@
 alter table permit drop column mine_guid CASCADE;
 delete from permit where permit_id not in (select permit_id from permit_amendment);
 
-update ETL_PERMIT e
-set permit_guid = (
-    select permit_guid 
-    from permit 
-    where permit.permit_no = e.permit_no
-);
+DO $$   
+    BEGIN 
+        IF EXISTS
+            ( SELECT 1
+                FROM   information_schema.tables 
+                WHERE  table_schema = 'public'
+                AND    table_name = 'etl_permit'
+            )
+        then
+            update ETL_PERMIT e
+            set permit_guid = (
+                select permit_guid 
+                from permit 
+                where permit.permit_no = e.permit_no
+            );
+        END IF;
+    END
+$$ ;
