3f68d8fe758a1b863e88e09ad8c02228338f286d
Luke Mitchell <lukegamitchell@gmail.com>
2020-06-09 08:34:02 -0700

    Tidy/document/logger docman POST

--- services/document-manager/backend/app/docman/resources/document.py
+++ services/document-manager/backend/app/docman/resources/document.py
@@ -10,6 +10,7 @@ from app.docman.models.document import Document
 from app.extensions import api, cache
 from app.utils.access_decorators import requires_any_of, MINE_EDIT, VIEW_ALL, MINESPACE_PROPONENT, EDIT_PARTY, EDIT_PERMIT, EDIT_DO, EDIT_VARIANCE
 from app.constants import FILE_UPLOAD_SIZE, FILE_UPLOAD_OFFSET, FILE_UPLOAD_PATH, DOWNLOAD_TOKEN, TIMEOUT_24_HOURS, TUS_API_VERSION, TUS_API_SUPPORTED_VERSIONS, FORBIDDEN_FILETYPES
+from app.config import Config
 
 
 @api.route('/documents')
@@ -33,8 +34,9 @@ class DocumentListResource(Resource):
         if request.headers.get('Tus-Resumable') is None:
             raise BadRequest('Received file upload for unsupported file transfer protocol')
 
+        # Validate the file size
         file_size = request.headers.get('Upload-Length')
-        max_file_size = current_app.config['MAX_CONTENT_LENGTH']
+        max_file_size = Config.MAX_CONTENT_LENGTH
         if not file_size:
             raise BadRequest('Received file upload of unspecified size')
         file_size = int(file_size)
@@ -42,6 +44,7 @@ class DocumentListResource(Resource):
             raise RequestEntityTooLarge(
                 f'The maximum file upload size is {max_file_size/1024/1024}MB.')
 
+        # Validate the file name and file extension
         data = self.parser.parse_args()
         filename = data.get('filename') or request.headers.get('Filename')
         if not filename:
@@ -49,28 +52,35 @@ class DocumentListResource(Resource):
         if filename.endswith(FORBIDDEN_FILETYPES):
             raise BadRequest('File type is forbidden')
 
+        # Create the path string for this file
         document_guid = str(uuid.uuid4())
-        base_folder = current_app.config['UPLOADED_DOCUMENT_DEST']
+        base_folder = Config.UPLOADED_DOCUMENT_DEST
         folder = data.get('folder') or request.headers.get('Folder')
         folder = os.path.join(base_folder, folder)
         file_path = os.path.join(folder, document_guid)
         pretty_folder = data.get('pretty_folder') or request.headers.get('Pretty-Folder')
         pretty_path = os.path.join(base_folder, pretty_folder, filename)
+        current_app.logger.info(f'folder: {folder}')
+        current_app.logger.info(f'file_path: {file_path}')
+        current_app.logger.info(f'pretty_path: {pretty_path}')
 
+        # Create an empty file at this path
         try:
             if not os.path.exists(folder):
                 os.makedirs(folder)
-            with open(file_path, "wb") as f:
+            with open(file_path, 'wb') as f:
                 f.seek(file_size - 1)
-                f.write(b"\0")
+                f.write(b'\0')
         except IOError as e:
             current_app.logger.error(e)
             raise InternalServerError('Unable to create file')
 
+        # Cache this file POST data to be used in future PATCH requests
         cache.set(FILE_UPLOAD_SIZE(document_guid), file_size, TIMEOUT_24_HOURS)
         cache.set(FILE_UPLOAD_OFFSET(document_guid), 0, TIMEOUT_24_HOURS)
         cache.set(FILE_UPLOAD_PATH(document_guid), file_path, TIMEOUT_24_HOURS)
 
+        # Create document record
         document_info = Document(
             document_guid=document_guid,
             full_storage_path=file_path,
@@ -80,15 +90,16 @@ class DocumentListResource(Resource):
         )
         document_info.save()
 
+        # Create and send response
         response = make_response(jsonify(document_manager_guid=document_guid), 201)
         response.headers['Tus-Resumable'] = TUS_API_VERSION
         response.headers['Tus-Version'] = TUS_API_SUPPORTED_VERSIONS
-        response.headers[
-            'Location'] = f'{current_app.config["DOCUMENT_MANAGER_URL"]}/documents/{document_guid}'
+        response.headers['Location'] = f'{Config.DOCUMENT_MANAGER_URL}/documents/{document_guid}'
         response.headers['Upload-Offset'] = 0
         response.headers[
-            'Access-Control-Expose-Headers'] = "Tus-Resumable,Tus-Version,Location,Upload-Offset"
+            'Access-Control-Expose-Headers'] = 'Tus-Resumable,Tus-Version,Location,Upload-Offset'
         response.autocorrect_location_header = False
+        current_app.logger.info(f'response:\n{response.__dict__}')
         return response
 
     def get(self):
@@ -121,12 +132,12 @@ class DocumentResource(Resource):
     def patch(self, document_guid):
         file_path = cache.get(FILE_UPLOAD_PATH(document_guid))
         if file_path is None or not os.path.lexists(file_path):
-            raise NotFound('PATCH sent for a upload that does not exist')
+            raise NotFound('PATCH sent for a file that does not exist')
 
         request_offset = int(request.headers.get('Upload-Offset', 0))
         file_offset = cache.get(FILE_UPLOAD_OFFSET(document_guid))
         if request_offset != file_offset:
-            raise Conflict("Offset in request does not match uploaded file's offset")
+            raise Conflict('Offset in request does not match uploaded file\'s offset')
 
         chunk_size = request.headers.get('Content-Length')
         if chunk_size is None:
@@ -137,10 +148,10 @@ class DocumentResource(Resource):
         file_size = cache.get(FILE_UPLOAD_SIZE(document_guid))
         if new_offset > file_size:
             raise RequestEntityTooLarge(
-                'The uploaded chunk would put the file above its declared file size.')
+                'The uploaded chunk would put the file above its declared file size')
 
         try:
-            with open(file_path, "r+b") as f:
+            with open(file_path, 'r+b') as f:
                 f.seek(file_offset)
                 f.write(request.data)
         except IOError as e:
@@ -165,7 +176,7 @@ class DocumentResource(Resource):
         response.headers['Tus-Version'] = TUS_API_SUPPORTED_VERSIONS
         response.headers['Upload-Offset'] = new_offset
         response.headers[
-            'Access-Control-Expose-Headers'] = "Tus-Resumable,Tus-Version,Upload-Offset"
+            'Access-Control-Expose-Headers'] = 'Tus-Resumable,Tus-Version,Upload-Offset'
         return response
 
     @requires_any_of(
@@ -178,28 +189,28 @@ class DocumentResource(Resource):
         if file_path is None or not os.path.lexists(file_path):
             raise NotFound('File does not exist')
 
-        response = make_response("", 200)
+        response = make_response('', 200)
         response.headers['Tus-Resumable'] = TUS_API_VERSION
         response.headers['Tus-Version'] = TUS_API_SUPPORTED_VERSIONS
         response.headers['Upload-Offset'] = cache.get(FILE_UPLOAD_OFFSET(document_guid))
         response.headers['Upload-Length'] = cache.get(FILE_UPLOAD_SIZE(document_guid))
         response.headers['Cache-Control'] = 'no-store'
         response.headers[
-            'Access-Control-Expose-Headers'] = "Tus-Resumable,Tus-Version,Upload-Offset,Upload-Length,Cache-Control"
+            'Access-Control-Expose-Headers'] = 'Tus-Resumable,Tus-Version,Upload-Offset,Upload-Length,Cache-Control'
         return response
 
     def options(self, document_guid):
         response = make_response('', 200)
 
-        if request.headers.get('Access-Control-Request-Method', None) is not None:
+        if request.headers.get('Access-Control-Request-Method') is not None:
             # CORS request, return 200
             return response
 
         response.headers['Tus-Resumable'] = self.tus_api_version
         response.headers['Tus-Version'] = self.tus_api_supported_versions
-        response.headers['Tus-Extension'] = "creation"
+        response.headers['Tus-Extension'] = 'creation'
         response.headers['Tus-Max-Size'] = self.max_file_size
         response.headers[
-            'Access-Control-Expose-Headers'] = "Tus-Resumable,Tus-Version,Tus-Extension,Tus-Max-Size"
+            'Access-Control-Expose-Headers'] = 'Tus-Resumable,Tus-Version,Tus-Extension,Tus-Max-Size'
         response.status_code = 204
         return response
