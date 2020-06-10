6aed7ff968a09aa1dfb3e27cc8d139fca03bc65a
Luke Mitchell <lukegamitchell@gmail.com>
2020-06-10 13:06:58 -0700

    Stream objects from S3 instead of buffering then sending entire object

--- services/document-manager/backend/app/services/object_store_storage_service.py
+++ services/document-manager/backend/app/services/object_store_storage_service.py
@@ -1,7 +1,6 @@
 import boto3
 import io
-from flask import send_file
-
+from flask import send_file, Response, current_app
 from app.config import Config
 
 
@@ -17,7 +16,26 @@ class ObjectStoreStorageService():
             endpoint_url=f'https://{Config.OBJECT_STORE_HOST}')
 
     def download_file(self, path, display_name, as_attachment):
-        buffer = io.BytesIO()
-        self._client.download_fileobj(Config.OBJECT_STORE_BUCKET, path, buffer)
-        buffer.seek(0)
-        return send_file(buffer, attachment_filename=display_name, as_attachment=as_attachment)
+        def generate(result):
+            for chunk in iter(lambda: result['Body'].read(2048), b''):
+                yield chunk
+
+        s3_response = self._client.get_object(Bucket=Config.OBJECT_STORE_BUCKET, Key=path)
+        resp = Response(
+            generate(s3_response),
+            mimetype='application/pdf' if '.pdf' in display_name.lower() else 'application/zip',
+            headers={
+                'Content-Disposition':
+                ('attachment; ' if as_attachment else '') + ('filename=' + display_name)
+            })
+        return resp
+
+    # NOTE: Leaving this old method in here for reference and in case we want to use it in the future.
+    # def download_file(self, path, display_name, as_attachment):
+    #     buffer = io.BytesIO()
+    #     self._client.download_fileobj(
+    #         Config.OBJECT_STORE_BUCKET, path, buffer, Config=TransferConfig())
+    #     buffer.seek(0)
+    #     resp = send_file(buffer, attachment_filename=display_name, as_attachment=as_attachment)
+    #     current_app.logger.info(f'resp:\n{resp.__dict__}')
+    #     return resp
