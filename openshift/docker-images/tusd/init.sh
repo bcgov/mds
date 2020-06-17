7783e33b5cec870df5f51595bf5df7ff0f6d596d
Paul Dittaro <pdittaro@gmail.com>
2020-06-17 10:38:20 -0700

    Verbose TUSD logging

--- openshift/docker-images/tusd/init.sh
+++ openshift/docker-images/tusd/init.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-tusd -base-path="/files/" -behind-proxy -s3-endpoint=${S3_ENDPOINT} -s3-bucket=${S3_BUCKET_ID} -s3-object-prefix=${S3_PREFIX}
+tusd -base-path="/files/" -behind-proxy -s3-endpoint=${S3_ENDPOINT} -s3-bucket=${S3_BUCKET_ID} -s3-object-prefix=${S3_PREFIX} -verbose
