31040c831fbdbd1f7c40a84e4fa0de44ecc6951a
Meredith Mumby <meredith@freshworks.io>
2020-06-18 14:05:50 -0700

    updated common folders

--- services/minespace-web/common/selectors/staticContentSelectors.js
+++ services/minespace-web/common/selectors/staticContentSelectors.js
@@ -536,6 +536,10 @@ export const getDropdownNoticeOfWorkApplicationPermitTypeOptions = createSelecto
   createDropDownList,
   ["description", "now_application_permit_type_code", "active_ind"]
 );
+export const getNoticeOfWorkApplicationPermitTypeOptionsHash = createSelector(
+  [getDropdownNoticeOfWorkApplicationPermitTypeOptions],
+  createLabelHash
+);
 
 export const getDropdownNoticeOfWorkApplicationReviewTypeOptions = createSelectorWrapper(
   getNoticeOfWorkApplicationReviewOptions,
