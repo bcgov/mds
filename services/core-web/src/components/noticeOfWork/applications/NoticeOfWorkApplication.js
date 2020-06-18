59b0c9e679959cd1eabcefb39c7d1f07280be45b
Meredith Mumby <meredith@freshworks.io>
2020-06-18 14:03:40 -0700

    added back in conditional rendering of content

--- services/core-web/src/components/noticeOfWork/applications/NoticeOfWorkApplication.js
+++ services/core-web/src/components/noticeOfWork/applications/NoticeOfWorkApplication.js
@@ -185,10 +185,17 @@ export class NoticeOfWorkApplication extends Component {
   renderOriginalValues = (path) => {
     const prevValue = get(this.props.originalNoticeOfWork, path);
     const currentValue = get(this.props.noticeOfWork, path);
-    // only show edited if user has updated the field
+    // cases for isEdited:
+    // activities can be added, prevValue === undefined, currentValue === null, thus prevValue !== currentValue - but field has not been edited.
+    // prevValue !== undefined || prevValue !==  null, but currentValue has been changed to null, thus is has been edited
+    // prevValue !== currentValue, due to other value changes that are not null or undefined
     const isNewValue = isUndefined(prevValue) && !isNull(currentValue);
-    const edited = isNewValue && prevValue !== currentValue;
-
+    const isPrevValue =
+      !isUndefined(prevValue) &&
+      !isNull(prevValue) &&
+      (isNull(currentValue) || isUndefined(currentValue));
+    const hasBeenEdited = isNewValue || isPrevValue;
+    const edited = hasBeenEdited && prevValue !== currentValue;
     const getValue = () => {
       if (prevValue === true) {
         return "Yes";
