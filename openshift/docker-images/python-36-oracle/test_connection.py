9dc5f774c4034ced7e8b0312566d184818c2514c
Luke Mitchell <lukegamitchell@gmail.com>
2020-06-30 13:53:43 -0700

    Comment out init oracle client by default

--- openshift/docker-images/python-36-oracle/test_connection.py
+++ openshift/docker-images/python-36-oracle/test_connection.py
@@ -8,7 +8,7 @@ dsn = 'mds_nris_prod_1'
 # dsn = 'mds_nris_test_1'
 
 # cx_Oracle.init_oracle_client(config_dir='/opt/app-root/oracle_bin/instantclient/network/admin')
-cx_Oracle.init_oracle_client(lib_dir='/opt/app-root/oracle_bin/instantclient', config_dir='/opt/app-root/oracle_bin/instantclient/network/admin')
+# cx_Oracle.init_oracle_client(lib_dir='/opt/app-root/oracle_bin/instantclient', config_dir='/opt/app-root/oracle_bin/instantclient/network/admin')
 
 oracle_db = cx_Oracle.connect(user='PROXY_NRIS_MDS_TEMP', password='bgiu68hdl0dsecesqc0g', dsn=dsn, encoding="UTF-8")
 
