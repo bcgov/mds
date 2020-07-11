20a1450c53a53c3d5d402606c3f05181d7e8ea26
Luke Mitchell <lukegamitchell@gmail.com>
2020-07-11 13:06:28 -0700

    More work, wip

--- services/document-manager/backend/app/docman/resources/admin.py
+++ services/document-manager/backend/app/docman/resources/admin.py
@@ -1,14 +1,14 @@
-import os
-import sys
-from app.services.object_store_storage_service import ObjectStoreStorageService
+import numpy
+import uuid
 
 from werkzeug.exceptions import BadRequest
 from flask import current_app
 from flask_restplus import Resource, reqparse
 
-from app.docman.models.document import Document
 from app.extensions import api
+from app.docman.models.document import Document
 from app.utils.access_decorators import requires_any_of, MINE_ADMIN
+from app.services.object_store_storage_service import ObjectStoreStorageService
 
 
 @api.route('/admin/transfer-file-system-to-object-store')
@@ -26,21 +26,28 @@ class TransferFileSystemToObjectStore(Resource):
         if (secret != 'd053c125-eb4b-44ba-8560-e83d716dfb0c'):
             raise BadRequest()
 
-        # Initiate the transfer process
-        # docs = Document.query.filter_by(object_store_path=None).all()
+        # Get the documents that aren't stored on the object store
+        docs = Document.query.filter_by(object_store_path=None).filter(
+            Document.document_id >= 70).order_by(Document.document_id).all()
 
-        # def chunks(lst, n):
-        #     for i in range(0, len(lst), n):
-        #         yield lst[i:i + n]
+        if len(docs) == 0:
+            return 'No documents need to be transferred', 201
 
-        # docs_chunks = chunks(docs, 4)
-        # # current_app.logger.info(f'docs_chunks len: {len(docs_chunks)}')
+        transfer_id = str(uuid.uuid4())
 
-        # for i, chunk in enumerate(docs_chunks):
-        #     # current_app.logger.info(f'chunk_index: {i}')
-        #     transfer_docs.delay(chunk, i)
+        # Split the list of documents to transfer into N chunks to upload in parallel
+        UPLOAD_CHUNKS = 1
+        docs_chunks = numpy.array_split(docs, UPLOAD_CHUNKS)
+        docs_chunks = [x.tolist() for x in docs_chunks if len(x) > 0]
+        message = f'{transfer_id}: {len(docs)} files will be transferred in {len(docs_chunks)} chunks of size {len(docs_chunks[0])}'
+        current_app.logger.info(message)
+        current_app.logger.info(docs_chunks)
 
-        result = transfer_docs.delay()
-        result.wait()
+        # Start the transfer tasks
+        for i, chunk in enumerate(docs_chunks):
+            chunk_doc_data = [doc.document_id for doc in chunk]
+            current_app.logger.info(
+                f'{transfer_id}: Beginning transfer for chunk #{i}: {chunk_doc_data}')
+            transfer_docs.delay(transfer_id, chunk_doc_data, i)
 
-        return 'Initiated transfer', 201
+        return message, 201
--- services/document-manager/backend/app/utils/tasks.py
+++ services/document-manager/backend/app/utils/tasks.py
@@ -1,24 +1,44 @@
 from app.services.object_store_storage_service import ObjectStoreStorageService
-
 from app.docman.models.document import Document
-
 from app import make_celery
 
 celery = make_celery()
 
 
+class TaskFailure(Exception):
+    pass
+
+
 @celery.task
-def transfer_docs():
-    docs = Document.query.filter_by(object_store_path=None).all()
-    # print(f'Chunk {chunk_index} transferring {len(docs)} docs')
-    failures = []
+def transfer_docs(transfer_id, document_ids, chunk_index):
+    print(f'{transfer_id}: Chunk {chunk_index} transferring {len(document_ids)} docs')
+
+    # Get the documents to transfer
+    # TODO: If this task was resumed, could also filter the list by documents that don't have object_store_path set, and/or, verify checksums.
+    docs = Document.query.filter(Document.document_id.in_(document_ids)).all()
+    print(docs)
+
+    # Transfer the documents
+    errors = []
     for i, doc in enumerate(docs):
         try:
-            # print(f'Transferring doc #{i} from chunk {chunk_index}')
+            print(
+                f'{transfer_id}: Starting transfer of document #{i} with ID {doc.document_id} from chunk {chunk_index}'
+            )
             key = ObjectStoreStorageService().upload_file(file_name=doc.full_storage_path)
             doc.object_store_path = key
             doc.save()
+
         except Exception as e:
-            print(f'Failed to transfer document: {e}')
-            failures.append(doc)
-    # print(f'Chunk {chunk_index} failures: {failures}')
+            message = f'{transfer_id}: Failed to transfer document with ID {doc.document_id}: {e}'
+            print(message)
+            errors.append([message, doc.json()])
+
+    # Determine if the task failed
+    print(f'{transfer_id}: Chunk {chunk_index} completed with {len(errors)} errors')
+    if (len(errors) > 0):
+        raise TaskFailure(
+            f'{transfer_id}: Failed to successfully complete the transfer with the following errors:\n{errors}'
+        )
+
+    return f'{transfer_id}: Successfully completed the transfer'
