e918106e8c82867c76be8cc3a8ab1bd72c79a7e5
Luke Mitchell <lukegamitchell@gmail.com>
2020-07-12 10:42:31 -0700

    Don't break up log by worker thread (keep in same file)

--- services/document-manager/backend/init.sh
+++ services/document-manager/backend/init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir -p /var/log/celery /var/run/celery
-celery worker -A app.utils.tasks --detach --loglevel info --logfile=/var/log/celery/%n%I.log --pidfile=/var/run/celery/%n.pid
+celery worker -A app.utils.tasks --detach --loglevel info --logfile=/var/log/celery/%n.log --pidfile=/var/run/celery/%n.pid
 flask run
