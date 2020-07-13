0c14a9724ef369bb8387f78ac6b5ca27b2439ef4
Luke Mitchell <lukegamitchell@gmail.com>
2020-07-13 15:36:51 -0700

    update configs

--- openshift/docker-images/python-36-docman/init.sh
+++ openshift/docker-images/python-36-docman/init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd /opt/app-root/src
-celery worker -A app.utils.tasks --detach --loglevel=info --logfile=celery.log --pidfile=celery.pid --concurrency=8
+celery worker -A app.utils.tasks --detach --loglevel=info --logfile=celery.log --pidfile=celery.pid --concurrency=1
