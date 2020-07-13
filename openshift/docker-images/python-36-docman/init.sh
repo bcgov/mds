52c6ce2425880c9266412220b32ed31f6bac0dcc
Luke Mitchell <lukegamitchell@gmail.com>
2020-07-13 11:21:37 -0700

    debug config

--- openshift/docker-images/python-36-docman/init.sh
+++ openshift/docker-images/python-36-docman/init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 mkdir -p /var/log/celery /var/run/celery
-celery worker -A app.utils.tasks --detach --loglevel=info --logfile=/var/log/celery/celery.log --pidfile=/var/run/celery/celery.pid --concurrency=8
+# celery worker -A app.utils.tasks --detach --loglevel=info --logfile=/var/log/celery/celery.log --pidfile=/var/run/celery/celery.pid --concurrency=8
