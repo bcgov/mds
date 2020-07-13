463986414f40eb1d653e633bb3d413f25b3af629
Luke Mitchell <lukegamitchell@gmail.com>
2020-07-13 11:13:57 -0700

    Remove flask run for openshift dockerfile

--- openshift/docker-images/python-36-docman/init.sh
+++ openshift/docker-images/python-36-docman/init.sh
@@ -2,4 +2,3 @@
 
 mkdir -p /var/log/celery /var/run/celery
 celery worker -A app.utils.tasks --detach --loglevel=info --logfile=/var/log/celery/celery.log --pidfile=/var/run/celery/celery.pid --concurrency=8
-flask run
