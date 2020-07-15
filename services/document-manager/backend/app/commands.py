c9a346fc5816c1a47eeebdb10cd649ad0f812bf9
Paul Dittaro <pdittaro@gmail.com>
2020-07-15 09:29:35 -0700

    Moving transfer logic into 'click' command, WIP
    Signed-off-by: Paul Dittaro <pdittaro@gmail.com>

--- services/document-manager/backend/app/commands.py
+++ services/document-manager/backend/app/commands.py
@@ -1,2 +1,9 @@
+from app.services.transfer_files import transfer_local_files_to_object_store
+
+
 def register_commands(app):
-    pass
+    @app.cli.command()
+    def transfer_files():
+        print("Beginning transfer of files to object store...")
+        transfer_local_files_to_object_store()
+        print("Transfer task created.")
