58a691dd87936ef409e32874fdb2dca2b517ea3c
Ryan Rondeau <ryan_rondeau@live.ca>
2020-07-17 15:11:18 -0700

    Update mine_party_appt.py

--- services/core-api/app/api/parties/party_appt/models/mine_party_appt.py
+++ services/core-api/app/api/parties/party_appt/models/mine_party_appt.py
@@ -68,7 +68,7 @@ class MinePartyAppointment(AuditMixin, Base):
     def save(self, commit=True):
         current_app.logger.warn('start save')
         current_app.logger.warn(self.__dict__)
-        if not (self.permit or self.permit_id or self.mine_guid or self.mine):
+        if self.permit_id == None and self.mine_guid == None:
             raise AssertionError("Must have a related permit or mine")
         if self.mine_party_appt_type_code == "PMT" and (self.mine_guid or self.mine) is not None:
             raise AssertionError("Permittees are not related to mines")
