48facfd541a6de0e20660378fb0e1d313e682ba5
jsyro <jasyrotuck@gmail.com>
2020-07-17 14:40:42 -0700

    push up logging.

--- services/core-api/app/api/parties/party_appt/models/mine_party_appt.py
+++ services/core-api/app/api/parties/party_appt/models/mine_party_appt.py
@@ -64,6 +64,7 @@ class MinePartyAppointment(AuditMixin, Base):
         return
 
     def save(self, commit=True):
+        current_app.logger.warn(self.__dict__)
         if not (self.permit or self.permit_id or self.mine_guid or self.mine):
             raise AssertionError("Must have a related permit or mine")
         if self.mine_party_appt_type_code == "PMT" and (self.mine_guid or self.mine) is not None:
