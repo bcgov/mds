0bf98f5f6a627e37a967d04f5bc402693490d8db
justinmacaulay <justinkmacaulay@gmail.com>
2020-07-29 09:13:55 -0700

    updated string lengths on all tables.

--- services/nris-api/backend/app/nris/models/document.py
+++ services/nris-api/backend/app/nris/models/document.py
@@ -19,16 +19,18 @@ DOCUMENT_RESPONSE_MODEL = api.model(
 
 class Document(Base):
     __tablename__ = "document"
-    __table_args__ = {'comment': 'A document is any type of additional documentation that has been generated during the INSPECTION process and is attached as part of the INSPECTION record. An example would be a photograph that was taken of the effluent being released during the inspection.'}
+    __table_args__ = {
+        'comment':
+        'A document is any type of additional documentation that has been generated during the INSPECTION process and is attached as part of the INSPECTION record. An example would be a photograph that was taken of the effluent being released during the inspection.'
+    }
     document_id = db.Column(db.Integer, primary_key=True)
     external_id = db.Column(db.Integer)
     document_date = db.Column(db.DateTime)
-    document_type_id = db.Column(
-        db.Integer, db.ForeignKey('document_type.document_type_id'))
+    document_type_id = db.Column(db.Integer, db.ForeignKey('document_type.document_type_id'))
     document_type_rel = db.relationship("DocumentType", lazy='selectin')
     document_type = association_proxy('document_type_rel', 'document_type')
-    file_name = db.Column(db.String(1024))
-    comment = db.Column(db.String(1024))
+    file_name = db.Column(db.String(10485760))
+    comment = db.Column(db.String(10485760))
 
     def __repr__(self):
         return f'<Document document_id={self.document_id} file_name={self.file_name}>'
