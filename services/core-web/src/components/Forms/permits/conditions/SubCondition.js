0c91823f783fcac6e2ece4197f5adb09a3aff9e7
Meredith Mumby <meredith@freshworks.io>
2020-08-19 10:26:58 -0700

    Delete conditions - modal shows what is being deleted (#1418)

--- services/core-web/src/components/Forms/permits/conditions/SubCondition.js
+++ services/core-web/src/components/Forms/permits/conditions/SubCondition.js
@@ -1,94 +1,123 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { Col, Row, Popconfirm, Button } from "antd";
-import { EDIT_OUTLINE_VIOLET, TRASHCAN } from "@/constants/assets";
+import { Col, Row, Button } from "antd";
+import { maxBy } from "lodash";
+import { TRASHCAN } from "@/constants/assets";
 import AuthorizationWrapper from "@/components/common/wrappers/AuthorizationWrapper";
 import * as Permission from "@/constants/permissions";
-import AddButton from "@/components/common/AddButton";
 import Condition from "@/components/Forms/permits/conditions/Condition";
 import AddCondition from "@/components/Forms/permits/conditions/AddCondition";
 import SubConditionForm from "./SubConditionForm";
-import { maxBy } from "lodash";
 
 const propTypes = {
-    condition: PropTypes.objectOf(PropTypes.any),
-    new: PropTypes.bool,
-    handleSubmit: PropTypes.func,
-    handleCancel: PropTypes.func,
-    handleDelete: PropTypes.func,
-    initialValues: PropTypes.objectOf(PropTypes.any),
+  condition: PropTypes.objectOf(PropTypes.any),
+  new: PropTypes.bool,
+  handleSubmit: PropTypes.func,
+  handleCancel: PropTypes.func,
+  handleDelete: PropTypes.func,
+  initialValues: PropTypes.objectOf(PropTypes.any),
+  isViewOnly: PropTypes.bool,
 };
 
 const defaultProps = {
-    condition: {
-        step: '',
-        condition: '',
-        sub_conditions: []
-    },
-    new: false,
-    handleSubmit: () => { },
-    handleCancel: () => { },
-    handleDelete: () => { },
-    initialValues: {}
+  condition: {
+    step: "",
+    condition: "",
+    sub_conditions: [],
+  },
+  new: false,
+  handleSubmit: () => {},
+  handleCancel: () => {},
+  handleDelete: () => {},
+  initialValues: {},
+  isViewOnly: false,
 };
 
 const SubCondition = (props) => {
-    const [isEditing, setIsEditing] = useState(props.new);
-    return (
+  // eslint-disable-next-line no-unused-vars
+  const [isEditing, setIsEditing] = useState(props.new);
+  return (
+    <>
+      {props.condition.display_order !== 1 && (
         <>
-            {props.condition.display_order !== 1 && <><Row gutter={32}><Col>&nbsp;</Col></Row><Row gutter={32}><Col>&nbsp;</Col></Row></>}
-            <Row gutter={32}>
-                {!isEditing && <><Col span={1} />
-                    <Col span={1}>
-                        {!isEditing && props.condition.step}
-                    </Col></>}
-                <Col span={20}>
-                    <Row>
-                        <Col>{!isEditing && props.condition.condition}</Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            {isEditing && (<SubConditionForm onCancel={props.handleCancel} onSubmit={props.handleSubmit} initialValues={props.initialValues} />)}
-                        </Col>
-                    </Row>
-                </Col>
-                <Col span={2} className="float-right">
-                    {!isEditing &&
-                        (
-                            <AuthorizationWrapper permission={Permission.ADMIN}>
-                                <Popconfirm
-                                    placement="topLeft"
-                                    title="Are you sure you want to delete this condition?"
-                                    onConfirm={() => props.handleDelete(props.condition.permit_condition_guid)}
-                                    okText="Delete"
-                                    cancelText="Cancel"
-                                >
-                                    <Button ghost size="small" type="primary">
-                                        <img name="remove" src={TRASHCAN} alt="Remove Condition" />
-                                    </Button>
-                                </Popconfirm>
-                            </AuthorizationWrapper>
-                        )}
-                </Col>
-            </Row>
-            {props.condition.sub_conditions.map((condition) => <Condition condition={condition} handleDelete={props.handleDelete} />)}
-            {props.condition.sub_conditions.length === 0 && <Row gutter={32}><Col>&nbsp;</Col></Row>}
-            {!isEditing && (
-                <Row gutter={32}>
-                    <Col span={22} offset={2}>
-                        <AddCondition initialValues={
-                            {
-                                condition_category_code: props.condition.condition_category_code,
-                                condition_type_code: 'LIS',
-                                display_order: props.condition.sub_conditions.length === 0 ? 1 : maxBy(props.condition.sub_conditions, 'display_order').display_order + 1,
-                                parent_permit_condition_id: props.condition.permit_condition_id,
-                                permit_amendment_id: props.condition.permit_amendment_id
-                            }} />
-                    </Col>
-                </Row>
-            )}
+          <Row gutter={32}>
+            <Col>&nbsp;</Col>
+          </Row>
+          <Row gutter={32}>
+            <Col>&nbsp;</Col>
+          </Row>
         </>
-    )
+      )}
+      <Row gutter={32}>
+        {!isEditing && (
+          <>
+            <Col span={1} />
+            <Col span={props.isViewOnly ? 2 : 1}>{!isEditing && props.condition.step}</Col>
+          </>
+        )}
+        <Col span={props.isViewOnly ? 19 : 20}>
+          <Row>
+            <Col>{!isEditing && props.condition.condition}</Col>
+          </Row>
+          <Row>
+            <Col>
+              {isEditing && (
+                <SubConditionForm
+                  onCancel={props.handleCancel}
+                  onSubmit={props.handleSubmit}
+                  initialValues={props.initialValues}
+                />
+              )}
+            </Col>
+          </Row>
+        </Col>
+        <Col span={2} className="float-right">
+          {!isEditing && !props.isViewOnly && (
+            <AuthorizationWrapper permission={Permission.ADMIN}>
+              <Button
+                ghost
+                size="small"
+                type="primary"
+                onClick={() => props.handleDelete(props.condition)}
+              >
+                <img name="remove" src={TRASHCAN} alt="Remove Condition" />
+              </Button>
+            </AuthorizationWrapper>
+          )}
+        </Col>
+      </Row>
+      {props.condition.sub_conditions.map((condition) => (
+        <Condition
+          condition={condition}
+          handleDelete={props.handleDelete}
+          isViewOnly={props.isViewOnly}
+        />
+      ))}
+      {props.condition.sub_conditions.length === 0 && (
+        <Row gutter={32}>
+          <Col>&nbsp;</Col>
+        </Row>
+      )}
+      {!isEditing && !props.isViewOnly && (
+        <Row gutter={32}>
+          <Col span={22} offset={2}>
+            <AddCondition
+              initialValues={{
+                condition_category_code: props.condition.condition_category_code,
+                condition_type_code: "LIS",
+                display_order:
+                  props.condition.sub_conditions.length === 0
+                    ? 1
+                    : maxBy(props.condition.sub_conditions, "display_order").display_order + 1,
+                parent_permit_condition_id: props.condition.permit_condition_id,
+                permit_amendment_id: props.condition.permit_amendment_id,
+              }}
+            />
+          </Col>
+        </Row>
+      )}
+    </>
+  );
 };
 
 SubCondition.propTypes = propTypes;
