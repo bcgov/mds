e2db04089e16b2554367dea8d1bd30350c07e42a
justinmacaulay <justinkmacaulay@gmail.com>
2020-08-31 16:18:16 -0700

    testing

--- migrations/sql/V2020.08.26.13.15__update_permit_add_project_id.sql
+++ migrations/sql/V2020.08.26.13.15__update_permit_add_project_id.sql
@@ -1,4 +1,4 @@
 ALTER TABLE permit ADD COLUMN project_id varchar;
 
-ALTER TABLE bond drop column project_id cascade;
+ALTER TABLE bond drop column project_id;
 ALTER TABLE reclamation_invoice drop column project_id;
