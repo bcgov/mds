472c1f6b6df4e7eeb0222f0c238b46e546fcd566
Ryan Rondeau <ryan_rondeau@live.ca>
2020-09-03 15:29:05 -0700

    Update V2020.09.03.14.10__etl_permit_security_adjustment.sql

--- migrations/sql/V2020.09.03.14.10__etl_permit_security_adjustment.sql
+++ migrations/sql/V2020.09.03.14.10__etl_permit_security_adjustment.sql
@@ -1,2 +1,14 @@
-ALTER TABLE ETL_PERMIT
-ADD COLUMN IF NOT EXISTS security_adjustment numeric;
+DO $$   
+    BEGIN 
+        IF EXISTS
+            ( SELECT 1
+                FROM   information_schema.tables 
+                WHERE  table_schema = 'public'
+                AND    table_name = 'etl_permit'
+            )
+        then
+            ALTER TABLE ETL_PERMIT
+            ADD COLUMN IF NOT EXISTS security_adjustment numeric;
+        END IF;
+    END
+$$ ;
