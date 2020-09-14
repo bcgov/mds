e6ae74aada1b7e80fe38786593265c2e2cb3f047
Paul Dittaro <pdittaro@gmail.com>
2020-09-14 16:10:35 -0700

    Wrap in procedure
    Signed-off-by: Paul Dittaro <pdittaro@gmail.com>

--- migrations/sql/V2020.09.14.15.52__reimport_fdw_schemas.sql
+++ migrations/sql/V2020.09.14.15.52__reimport_fdw_schemas.sql
@@ -1,19 +1,28 @@
 -- Drop and recreate FDW schemas
+DO
+$$
+BEGIN
+
 IF EXISTS(select from pg_foreign_server where srvname = 'oracle_fdw') THEN
-    DROP SCHEMA IF EXISTS mms;
+    DROP SCHEMA IF EXISTS mms CASCADE;
     CREATE SCHEMA mms;
     IMPORT FOREIGN SCHEMA "MMSADMIN" FROM SERVER oracle_fdw INTO mms;
     GRANT USAGE ON SCHEMA mms TO fdw_reader;
     GRANT SELECT ON ALL TABLES IN SCHEMA mms TO fdw_reader;
     GRANT fdw_reader to "mds";
 
-    DROP SCHEMA IF EXISTS mms_now_nros;
+    DROP SCHEMA IF EXISTS mms_now_nros CASCADE;
+    CREATE SCHEMA mms_now_nros;
     IMPORT FOREIGN SCHEMA "APP_MMS_INTERFACE" FROM SERVER oracle_fdw INTO mms_now_nros;
     GRANT USAGE ON SCHEMA mms_now_nros TO fdw_reader;
     GRANT SELECT ON ALL TABLES IN SCHEMA mms_now_nros TO fdw_reader;
 
-    DROP SCHEMA IF EXISTS mms_now_vfcbc;
+    DROP SCHEMA IF EXISTS mms_now_vfcbc CASCADE;
+    CREATE SCHEMA mms_now_vfcbc;
     IMPORT FOREIGN SCHEMA "VFCMMSINTERFACE" FROM SERVER oracle_fdw INTO mms_now_vfcbc;
     GRANT USAGE ON SCHEMA mms_now_vfcbc TO fdw_reader;
     GRANT SELECT ON ALL TABLES IN SCHEMA mms_now_vfcbc TO fdw_reader;
 END IF;
+
+END
+$$;
