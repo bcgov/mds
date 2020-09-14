b0dd3418629066b35222aa8e3ae6473a07cc6501
Paul Dittaro <pdittaro@gmail.com>
2020-09-14 16:02:27 -0700

    Remove double grant
    Signed-off-by: Paul Dittaro <pdittaro@gmail.com>

--- migrations/sql/V2020.09.14.15.52__reimport_fdw_schemas.sql
+++ migrations/sql/V2020.09.14.15.52__reimport_fdw_schemas.sql
@@ -5,16 +5,15 @@ IF EXISTS(select from pg_foreign_server where srvname = 'oracle_fdw') THEN
     IMPORT FOREIGN SCHEMA "MMSADMIN" FROM SERVER oracle_fdw INTO mms;
     GRANT USAGE ON SCHEMA mms TO fdw_reader;
     GRANT SELECT ON ALL TABLES IN SCHEMA mms TO fdw_reader;
+    GRANT fdw_reader to "mds";
 
     DROP SCHEMA IF EXISTS mms_now_nros;
     IMPORT FOREIGN SCHEMA "APP_MMS_INTERFACE" FROM SERVER oracle_fdw INTO mms_now_nros;
     GRANT USAGE ON SCHEMA mms_now_nros TO fdw_reader;
     GRANT SELECT ON ALL TABLES IN SCHEMA mms_now_nros TO fdw_reader;
-    GRANT fdw_reader to "mds";
 
     DROP SCHEMA IF EXISTS mms_now_vfcbc;
     IMPORT FOREIGN SCHEMA "VFCMMSINTERFACE" FROM SERVER oracle_fdw INTO mms_now_vfcbc;
     GRANT USAGE ON SCHEMA mms_now_vfcbc TO fdw_reader;
     GRANT SELECT ON ALL TABLES IN SCHEMA mms_now_vfcbc TO fdw_reader;
-    GRANT fdw_reader to "mds";
 END IF;
