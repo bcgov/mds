ed75cc8bac6eaeba35e1502009e025f01ce6f8b3
Meredith Mumby <meredith@freshworks.io>
2020-09-17 08:29:17 -0700

    linting

--- services/core-web/src/components/Forms/noticeOfWork/UpdateNOWStatusForm.js
+++ services/core-web/src/components/Forms/noticeOfWork/UpdateNOWStatusForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
-import { Form } from '@ant-design/compatible';
-import '@ant-design/compatible/assets/index.css';
+import { Form } from "@ant-design/compatible";
+import "@ant-design/compatible/assets/index.css";
 import PropTypes from "prop-types";
 import { required } from "@common/utils/Validate";
 import * as FORM from "@/constants/forms";
