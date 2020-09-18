8070bfbf718e38cd3598d2f41a313d7866332bfe
Meredith Mumby <meredith@freshworks.io>
2020-09-18 12:52:58 -0700

    remove tooltip when leaving button

--- services/core-web/src/components/Forms/MineRecordForm.js
+++ services/core-web/src/components/Forms/MineRecordForm.js
@@ -300,6 +300,7 @@ export class MineRecordForm extends Component {
                           ]
                         : null
                     }
+                    validate={[required]}
                   />
                 </Col>
               </Row>
