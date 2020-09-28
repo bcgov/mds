40cda2a0dcfec69abb04b9844a5d12895e2040a7
jsyro <jasyrotuck@gmail.com>
2020-09-28 16:19:07 -0700

    wasn't using most recent version of view.

--- migrations/sql/R__view_bonds.sql
+++ migrations/sql/R__view_bonds.sql
@@ -3,30 +3,104 @@ DROP VIEW IF EXISTS bond_view;
 
 CREATE VIEW bond_view AS
 SELECT *
+FROM (
+SELECT bond.bond_id,
+       amount,
+       bond.bond_type_code,
+       bond_type.description as "bond_type_description",
+       bond.bond_status_code,
+       bond_status.description as "bond_status_description",
+       issue_date,
+       note,
+       payer_party_guid,
+       party_name AS "payer_name",
+       institution_city,
+       institution_name,
+       institution_postal_code,
+       institution_province,
+       institution_street,
+       permit_guid,
+       permit_no,
+       project_id,
+       reference_number,
+       bond.update_timestamp,
+       bond.update_user,
+       'Yes' AS "is_current_record",
+       'CORE' as "source"
+FROM bond,
+     party,
+     permit,
+     bond_permit_xref,
+     bond_type,
+     bond_status
+WHERE bond.payer_party_guid = party.party_guid
+    AND permit.permit_id = bond_permit_xref.permit_id
+    AND bond.bond_id = bond_permit_xref.bond_id
+    AND bond.bond_type_code = bond_type.bond_type_code
+    AND bond.bond_status_code = bond_status.bond_status_code
+UNION ALL
+SELECT bond_id,
+       amount,
+       bond_history.bond_type_code,
+       bond_type.description as "bond_type_description",
+       bond_history.bond_status_code,
+       bond_status.description as "bond_status_description",
+       issue_date,
+       note,
+       payer_party_guid,
+       payer_name,
+       institution_city,
+       institution_name,
+       institution_postal_code,
+       institution_province,
+       institution_street,
+       permit_guid,
+       permit_no,
+       null,
+       reference_number,
+       bond_history.update_timestamp,
+       bond_history.update_user,
+       'No' AS "is_current_record",
+       'CORE_HIST' as "source"
+FROM bond_history,
+     bond_type,
+     bond_status
+WHERE bond_history.bond_type_code = bond_type.bond_type_code
+    AND bond_history.bond_status_code = bond_status.bond_status_code as bond_data
+ORDER BY bond_id,
+         is_current_record DESC;
+
+
+DROP VIEW IF EXISTS bond_view;
+
+
+CREATE VIEW bond_view AS
+SELECT *
 FROM
-    (SELECT bond.bond_id,
-            amount,
-            bond.bond_type_code,
-            bond_type.description as "bond_type_description",
-            bond.bond_status_code,
-            bond_status.description as "bond_status_description",
-            issue_date,
-            note,
-            payer_party_guid,
-            party_name AS "payer_name",
-            institution_city,
-            institution_name,
-            institution_postal_code,
-            institution_province,
-            institution_street,
-            permit_guid,
-            permit_no,
-            project_id,
-            reference_number,
-            bond.update_timestamp,
-            bond.update_user,
-            'Yes' AS "is_current_record",
-            'CORE' as "source"
+    ( SELECT bond.bond_id,
+             amount,
+             bond.bond_type_code,
+             bond_type.description as "bond_type_description",
+             bond.bond_status_code,
+             bond_status.description as "bond_status_description",
+             issue_date,
+             note,
+             closed_date,
+             closed_note,
+             payer_party_guid,
+             party_name AS "payer_name",
+             institution_city,
+             institution_name,
+             institution_postal_code,
+             institution_province,
+             institution_street,
+             permit_guid,
+             permit_no,
+             permit.project_id,
+             reference_number,
+             bond.update_timestamp,
+             bond.update_user,
+             'Yes' AS "is_current_record"
      FROM bond,
           party,
           permit,
@@ -38,7 +112,7 @@ FROM
          AND bond.bond_id = bond_permit_xref.bond_id
          AND bond.bond_type_code = bond_type.bond_type_code
          AND bond.bond_status_code = bond_status.bond_status_code
-     UNION ALL SELECT bond_id,
+     UNION ALL SELECT bond_history.bond_id,
                       amount,
                       bond_history.bond_type_code,
                       bond_type.description as "bond_type_description",
@@ -46,6 +120,8 @@ FROM
                       bond_status.description as "bond_status_description",
                       issue_date,
                       note,
+                      closed_date,
+                      closed_note,
                       payer_party_guid,
                       payer_name,
                       institution_city,
@@ -53,19 +129,22 @@ FROM
                       institution_postal_code,
                       institution_province,
                       institution_street,
-                      permit_guid,
-                      permit_no,
-                      null,
+                      permit.permit_guid,
+                      permit.permit_no,
+                      permit.project_id,
                       reference_number,
                       bond_history.update_timestamp,
                       bond_history.update_user,
-                      'No' AS "is_current_record",
-                      'CORE_HIST' as "source"
+                      'No' AS "is_current_record"
      FROM bond_history,
           bond_type,
-          bond_status
+          bond_status,
+          permit,
+          bond_permit_xref
      WHERE bond_history.bond_type_code = bond_type.bond_type_code
          AND bond_history.bond_status_code = bond_status.bond_status_code
+         AND permit.permit_id = bond_permit_xref.permit_id
+         AND bond_history.bond_id = bond_permit_xref.bond_id
      UNION SELECT NULL,
                   sec_amt,
                   case
@@ -109,6 +188,6 @@ FROM
              (select mms_sec_cid
               from bond
               where mms_sec_cid is not null)
-         and sec_cid is not null) as bond_data
+         and sec_cid is not null) AS bond_data
 ORDER BY bond_id,
          is_current_record DESC;
