f474cf6fe8b49160435a8457c6c509c48eeeb9b5
jsyro <jasyrotuck@gmail.com>
2020-09-30 12:27:43 -0700

    formatting and comments.

--- migrations/sql/R__view_bonds.sql
+++ migrations/sql/R__view_bonds.sql
@@ -3,104 +3,31 @@ DROP VIEW IF EXISTS bond_view;
 
 CREATE VIEW bond_view AS
 SELECT *
-FROM (
-SELECT bond.bond_id,
-       amount,
-       bond.bond_type_code,
-       bond_type.description as "bond_type_description",
-       bond.bond_status_code,
-       bond_status.description as "bond_status_description",
-       issue_date,
-       note,
-       payer_party_guid,
-       party_name AS "payer_name",
-       institution_city,
-       institution_name,
-       institution_postal_code,
-       institution_province,
-       institution_street,
-       permit_guid,
-       permit_no,
-       project_id,
-       reference_number,
-       bond.update_timestamp,
-       bond.update_user,
-       'Yes' AS "is_current_record",
-       'CORE' as "source"
-FROM bond,
-     party,
-     permit,
-     bond_permit_xref,
-     bond_type,
-     bond_status
-WHERE bond.payer_party_guid = party.party_guid
-    AND permit.permit_id = bond_permit_xref.permit_id
-    AND bond.bond_id = bond_permit_xref.bond_id
-    AND bond.bond_type_code = bond_type.bond_type_code
-    AND bond.bond_status_code = bond_status.bond_status_code
-UNION ALL
-SELECT bond_id,
-       amount,
-       bond_history.bond_type_code,
-       bond_type.description as "bond_type_description",
-       bond_history.bond_status_code,
-       bond_status.description as "bond_status_description",
-       issue_date,
-       note,
-       payer_party_guid,
-       payer_name,
-       institution_city,
-       institution_name,
-       institution_postal_code,
-       institution_province,
-       institution_street,
-       permit_guid,
-       permit_no,
-       null,
-       reference_number,
-       bond_history.update_timestamp,
-       bond_history.update_user,
-       'No' AS "is_current_record",
-       'CORE_HIST' as "source"
-FROM bond_history,
-     bond_type,
-     bond_status
-WHERE bond_history.bond_type_code = bond_type.bond_type_code
-    AND bond_history.bond_status_code = bond_status.bond_status_code as bond_data
-ORDER BY bond_id,
-         is_current_record DESC;
-
-
-DROP VIEW IF EXISTS bond_view;
-
-
-CREATE VIEW bond_view AS
-SELECT *
 FROM
-    ( SELECT bond.bond_id,
-             amount,
-             bond.bond_type_code,
-             bond_type.description as "bond_type_description",
-             bond.bond_status_code,
-             bond_status.description as "bond_status_description",
-             issue_date,
-             note,
-             closed_date,
-             closed_note,
-             payer_party_guid,
-             party_name AS "payer_name",
-             institution_city,
-             institution_name,
-             institution_postal_code,
-             institution_province,
-             institution_street,
-             permit_guid,
-             permit_no,
-             permit.project_id,
-             reference_number,
-             bond.update_timestamp,
-             bond.update_user,
-             'Yes' AS "is_current_record"
+    (SELECT bond.bond_id,
+            amount,
+            bond.bond_type_code,
+            bond_type.description as "bond_type_description",
+            bond.bond_status_code,
+            bond_status.description as "bond_status_description",
+            issue_date,
+            note,
+            closed_date,
+            closed_note,
+            payer_party_guid,
+            party_name AS "payer_name",
+            institution_city,
+            institution_name,
+            institution_postal_code,
+            institution_province,
+            institution_street,
+            permit_guid,
+            permit_no,
+            permit.project_id,
+            reference_number,
+            bond.update_timestamp,
+            bond.update_user,
+            'Yes' AS "is_current_record"
      FROM bond,
           party,
           permit,
@@ -111,7 +38,8 @@ FROM
          AND permit.permit_id = bond_permit_xref.permit_id
          AND bond.bond_id = bond_permit_xref.bond_id
          AND bond.bond_type_code = bond_type.bond_type_code
-         AND bond.bond_status_code = bond_status.bond_status_code
+         AND bond.bond_status_code = bond_status.bond_status_code 
+    --HISTORY VALUES
      UNION ALL SELECT bond_history.bond_id,
                       amount,
                       bond_history.bond_type_code,
@@ -144,44 +72,45 @@ FROM
      WHERE bond_history.bond_type_code = bond_type.bond_type_code
          AND bond_history.bond_status_code = bond_status.bond_status_code
          AND permit.permit_id = bond_permit_xref.permit_id
-         AND bond_history.bond_id = bond_permit_xref.bond_id
-     UNION SELECT NULL,
-                  sec_amt,
-                  case
-                      when TRIM(sec_typ) = 'Asset Security Agreement' then 'ASA'
-                      when TRIM(sec_typ) = 'Cash' then 'CAS'
-                      when TRIM(sec_typ) = 'Confiscation' then 'CAS'
-                      when TRIM(sec_typ) = 'Letter of Credit' then 'ILC'
-                      when TRIM(sec_typ) = 'Qualified Env. Trust' then 'QET'
-                      when TRIM(sec_typ) = 'Safekeeping Agreement' then 'SAG'
-                      when TRIM(sec_typ) = 'Receipt and Agreement' then 'SAG'
-                      when TRIM(sec_typ) = 'Surety Bond' then 'SBO'
-                      when TRIM(sec_typ) = 'Recl. Fund' then 'STR'
-                      when TRIM(sec_typ) = 'Performance Bond' then 'PFB'
-                  end as bond_type_code,
-                  TRIM(descript) as "bond_type_description",
-                  CASE
-                      WHEN "status" = 'E' THEN 'REL'
-                      when "status" = 'C' then 'CON'
-                      ELSE 'ACT'
-                  end as bond_status_code,
-                  null,
-                  cnt_dt,
-                  CONCAT(TRIM(comment1),' ', TRIM(comment2)) as "comment",
-                  null::uuid,
-                  cmp_nm AS "payer_name",
-                  CONCAT(iaddr2,iaddr3) as institution_city,
-                  invloc as institution_name,
-                  null as institution_province,
-                  iaddr1 as institution_street,
-                  ipost_cd as institution_postal_code,
-                  null,
-                  permit_no,
-                  project_no,
-                  "descript" as reference_number,
-                  null as "update_timestamp",
-                  null as "update_user",
-                  'Yes' AS "is_current_record",
+         AND bond_history.bond_id = bond_permit_xref.bond_id 
+    -- SES RECORDS that couldn't be imported
+     UNION SELECT NULL, 
+                  sec_amt, 
+                  case 
+                      when TRIM(sec_typ) = 'Asset Security Agreement' then 'ASA' 
+                      when TRIM(sec_typ) = 'Cash' then 'CAS' 
+                      when TRIM(sec_typ) = 'Confiscation' then 'CAS' 
+                      when TRIM(sec_typ) = 'Letter of Credit' then 'ILC' 
+                      when TRIM(sec_typ) = 'Qualified Env. Trust' then 'QET' 
+                      when TRIM(sec_typ) = 'Safekeeping Agreement' then 'SAG' 
+                      when TRIM(sec_typ) = 'Receipt and Agreement' then 'SAG' 
+                      when TRIM(sec_typ) = 'Surety Bond' then 'SBO' 
+                      when TRIM(sec_typ) = 'Recl. Fund' then 'STR' 
+                      when TRIM(sec_typ) = 'Performance Bond' then 'PFB' 
+                  end as bond_type_code, 
+                  TRIM(descript) as "bond_type_description", 
+                  CASE 
+                      WHEN "status" = 'E' THEN 'REL' 
+                      when "status" = 'C' then 'CON' 
+                      ELSE 'ACT' 
+                  end as bond_status_code, 
+                  null, 
+                  cnt_dt, 
+                  CONCAT(TRIM(comment1),' ', TRIM(comment2)) as "comment", 
+                  null::uuid, 
+                  cmp_nm AS "payer_name", 
+                  CONCAT(iaddr2,iaddr3) as institution_city, 
+                  invloc as institution_name, 
+                  null as institution_province, 
+                  iaddr1 as institution_street, 
+                  ipost_cd as institution_postal_code, 
+                  null, 
+                  permit_no, 
+                  project_no, 
+                  "descript" as reference_number, 
+                  null as "update_timestamp", 
+                  null as "update_user", 
+                  'Yes' AS "is_current_record", 
                   'MMS_FDW' as "source"
      FROM mms.secsec
      where sec_cid not in
