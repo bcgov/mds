b7bf64e10f8bb9c03fd1e3bfdc35280eaa8b179f
Meredith Mumby <meredith@freshworks.io>
2020-10-13 13:28:17 -0700

    updated sql

--- migrations/sql/V2020.10.06.15.40__update_bond_document_categories.sql
+++ migrations/sql/V2020.10.06.15.40__update_bond_document_categories.sql
@@ -1,2 +1,3 @@
 UPDATE bond_document_type set description = 'No Interest Payable Form' where bond_document_type_code = 'NIA';
+delete from mine_documents where bond_document_type_code = 'BAL';
 DELETE FROM bond_document_type WHERE bond_document_type_code = 'BAL';
