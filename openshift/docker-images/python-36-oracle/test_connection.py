e6d46c624ed6815c29deb881203ffe35d53f33f8
Luke Mitchell <lukegamitchell@gmail.com>
2020-10-19 19:10:46 -0700

    Update test_connection.py

--- openshift/docker-images/python-36-oracle/test_connection.py
+++ openshift/docker-images/python-36-oracle/test_connection.py
@@ -5,17 +5,17 @@ import os
 
 print('test_connection.py')
 
-# dsn = 'mds_nris_test'
-dsn = cx_Oracle.makedsn(
-    os.getenv('NRIS_DB_HOSTNAME'),
-    os.getenv('NRIS_DB_PORT'),
-    service_name=os.getenv('NRIS_DB_SERVICENAME'))
-# oracle_db = cx_Oracle.connect(user='luke_test', password='luketest12#', dsn=dsn, encoding="UTF-8")
-oracle_db = cx_Oracle.connect(
-    user=os.getenv('NRIS_DB_USER'),
-    password=os.getenv('NRIS_DB_PASSWORD'),
-    dsn=dsn,
-    encoding="UTF-8")
+dsn = 'mds_nris_test'
+# dsn = cx_Oracle.makedsn(
+#     os.getenv('NRIS_DB_HOSTNAME'),
+#     os.getenv('NRIS_DB_PORT'),
+#     service_name=os.getenv('NRIS_DB_SERVICENAME'))
+oracle_db = cx_Oracle.connect(user='luke_test', password='luketest12#', dsn=dsn, encoding="UTF-8")
+# oracle_db = cx_Oracle.connect(
+#     user=os.getenv('NRIS_DB_USER'),
+#     password=os.getenv('NRIS_DB_PASSWORD'),
+#     dsn=dsn,
+#     encoding="UTF-8")
 
 print('connected')
 
