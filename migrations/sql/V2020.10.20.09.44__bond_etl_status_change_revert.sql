3a4a7c9b8e287755aa23a11546035e860b1c8a07
jsyro <jasyrotuck@gmail.com>
2020-10-20 15:46:42 -0700

    only fix data is ETL has run.

--- migrations/sql/V2020.10.20.09.44__bond_etl_status_change_revert.sql
+++ migrations/sql/V2020.10.20.09.44__bond_etl_status_change_revert.sql
@@ -1,18 +1,32 @@
--- 'E' records shoud be active (no history)
+DO $$
+    BEGIN
+        IF EXISTS
+            ( SELECT 1
+              FROM   information_schema.tables
+              WHERE  table_schema = 'public'
+              AND    table_name = 'etl_permit'
+            )
+        then
 
-delete
-from bond_history
-where bond_id in
-        (select core_bond_id
-         from etl_bond
-         where status = 'E');
+        -- 'E' records shoud be active (no history)
 
--- 'E' records were set to 'REL' should be 'ACT'
+        delete
+        from bond_history
+        where bond_id in
+                (select core_bond_id
+                from etl_bond
+                where status = 'E');
 
-update bond
-set bond_status_code = 'ACT'
-where bond_status_code = 'REL'
-    and sec_cid in
-        (select sec_cid
-         from etl_bond
-         where "status" = 'E')
+        -- 'E' records were set to 'REL' should be 'ACT'
+
+        update bond
+        set bond_status_code = 'ACT'
+        where bond_status_code = 'REL'
+            and sec_cid in
+                (select sec_cid
+                from etl_bond
+                where "status" = 'E')
+
+                 END IF ;
+    END
+   $$ ;
