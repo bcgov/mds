08d268f05c94273b5751c115b25e57a2dea2de8d
mhuseinov <mekhti.huseinov.ssi@gmail.com>
2020-10-21 14:32:32 -0700

    MDS-2798: updated alembic configuration; added migration template for now document job tables

---
+++ services/document-manager/backend/migrations/versions/4a4950ed26bd_create_now_document_job_tables.py
@@ -0,0 +1,24 @@
+"""create now document job tables
+
+Revision ID: 4a4950ed26bd
+Revises: 561823a58bc6
+Create Date: 2020-10-21 14:31:24.780205
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4a4950ed26bd'
+down_revision = '561823a58bc6'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
