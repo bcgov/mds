7e53127a90cf7eb4f52de967f8161d09455b4cb8
Meredith Mumby <meredith@freshworks.io>
2020-10-21 13:24:01 -0700

    updated migration script to remove site operator and rename mine operator

--- migrations/sql/V2020.10.15.14.53__add_party_type_land_owner.sql
+++ migrations/sql/V2020.10.15.14.53__add_party_type_land_owner.sql
@@ -10,6 +10,7 @@ INSERT INTO mine_party_appt_type_code
     grouping_level
     )
 VALUES
-    ('LDO', 'Land Owner', 260, 'system-mds', 'system-mds', 'true', 'false', 1),
-    ('STO', 'Site Operator', 270, 'system-mds', 'system-mds', 'true', 'false', 1)
+    ('LDO', 'Land Owner', 260, 'system-mds', 'system-mds', 'true', 'false', 1)
 ON CONFLICT DO NOTHING;
+
+UPDATE mine_party_appt_type_code SET description = 'Site Operator' WHERE mine_party_appt_type_code = 'MOR';
