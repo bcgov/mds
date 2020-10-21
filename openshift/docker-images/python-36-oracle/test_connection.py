ff9c163efd008a5f7873fb99d00810082ace0fca
Luke Mitchell <lukegamitchell@gmail.com>
2020-10-21 08:47:18 -0700

    Fix typo

--- openshift/docker-images/python-36-oracle/test_connection.py
+++ openshift/docker-images/python-36-oracle/test_connection.py
@@ -10,7 +10,7 @@ print('test_connection.py')
 #     os.getenv('NRIS_DB_PORT'),
 #     service_name=os.getenv('NRIS_DB_SERVICENAME'))
 
-dsn = f'(DESCRIPTION=(ADDRESS=(PROTOCOL=TCPS)(HOST=nrc{os.getenv("NRIS_DB_HOSTNAME")})(PORT={os.getenv("NRIS_DB_PORT")}))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME={os.getenv("NRIS_DB_SERVICENAME")}))(SECURITY=(ssl_server_cert_dn="{os.getenv("NRIS_SERVER_CERT_DN")}")))'
+dsn = f'(DESCRIPTION=(ADDRESS=(PROTOCOL=TCPS)(HOST={os.getenv("NRIS_DB_HOSTNAME")})(PORT={os.getenv("NRIS_DB_PORT")}))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME={os.getenv("NRIS_DB_SERVICENAME")}))(SECURITY=(ssl_server_cert_dn="{os.getenv("NRIS_SERVER_CERT_DN")}")))'
 
 oracle_db = cx_Oracle.connect(
     user=os.getenv('NRIS_DB_USER'), password=os.getenv('NRIS_DB_PASSWORD'), dsn=dsn)
