a7754b3e678bb824eb51302a98b6b1306e1ddfbd
Ryan <ryan_rondeau@live.ca>
2020-10-22 16:30:03 -0700

    update styling; fix bugs

--- services/core-api/app/api/utils/models_mixins.py
+++ services/core-api/app/api/utils/models_mixins.py
@@ -137,7 +137,7 @@ class Base(db.Model):
                     current_app.logger.debug(
                         f'COMBOBREAKER!!! {_edit_key} not in {rel_class} edit groups {rel_class._edit_groups}'
                     )
-                    #continue
+                    continue
                 current_app.logger.debug(depth * ' ' + f'recursively updating {k}')
                 existing_obj = getattr(self, k)
                 if existing_obj is None:
@@ -220,6 +220,7 @@ class Base(db.Model):
                 if (type(col.type) == UUID):
                     #UUID does not implement python_type, manual check
                     assert isinstance(v, (UUID, str))
+                    setattr(self, k, v)
                 else:
                     py_type = col.type.python_type
 
