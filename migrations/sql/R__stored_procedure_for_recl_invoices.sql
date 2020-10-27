19c76e27f2ab2fdea67e173578c1a82e3519bf84
jsyro <jasyrotuck@gmail.com>
2020-10-27 14:51:55 -0700

    just add ses pk, can be removed later.

--- migrations/sql/R__stored_procedure_for_recl_invoices.sql
+++ migrations/sql/R__stored_procedure_for_recl_invoices.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION mms_etl_bond_data() RETURNS void AS $$
+CREATE OR REPLACE FUNCTION mms_etl_reclamation_invoice_data() RETURNS void AS $$
 BEGIN
 declare
 
@@ -7,37 +7,6 @@ declare
         tmp2 integer;
         tmp3 integer;
   begin
-	CREATE TABLE IF NOT EXISTS ETL_RECLAMATION_INVOICES(
-		core_reclamation_invoice_id integer,
-		permit_id integer,
-		sec_cid varchar,
-		inv_cid varchar UNIQUE,
-		paid_dt TIMESTAMP,
-		contractor_nm varchar,
-		amount numeric(12,2) NOT NULL,
-		descript varchar
-	);
-
-	INSERT INTO ETL_RECLAMATION_INVOICES(
-		permit_id,
-		inv_cid,
-		paid_dt,
-		contractor_nm,
-		amount,
-		descript
-	)
-	SELECT DISTINCT ON inv.inv_cid
-		bpx.permit_id,
-		inv.inv_cid,
-		inv.paid_dt,
-		inv.contractor_nm,
-		inv.amount,
-		inv.descript
-	FROM mms.secinv inv
-	inner join bond b on b.sec_cid = inv.sec_cid
-	inner join bond_permit_xref bpx on bpx.bond_id = b.bond_id;
-
-
 	INSERT INTO reclamation_invoices (
 		mms_inv_cid,
 		permit_id,
@@ -49,7 +18,7 @@ declare
 		update_user,
 		)
 	select
-		DISTINCT ON (permit_id, paid_amt, contractor_nm)
+		inv.inv_cid
 		bpx.permit_id,
 		inv.paid_amt,
 		inv.contractor_nm,
@@ -57,11 +26,13 @@ declare
 		inv.descript,
 		'bond_etl',
 		'bond_etl',
-	from ETL_RECLAMATION_INVOICES e_inv
-	where
-		e_inv.paid_amt != 0
+	FROM mms.secinv inv
+	inner join bond b on b.sec_cid = inv.sec_cid
+	inner join bond_permit_xref bpx on bpx.bond_id = b.bond_id;
+	WHERE
+		inv.paid_amt != 0
 		AND
-		e_inv.core_reclamation_invoice_id is not null
+		inv.mms_inv_cid not in (select distinct mms_inv_cid from reclamation_invoices and mms_inv_cid is not null);
 
 END;
 END;
--- migrations/sql/V2020.10.26.13.04__add_paid_dt_reclamation_invoices.sql
+++ migrations/sql/V2020.10.26.13.04__add_paid_dt_reclamation_invoices.sql
@@ -1,2 +1,5 @@
 
 ALTER TABLE reclamation_invoice ADD COLUMN paid_date TIMESTAMP;
+
+
+ALTER TABLE reclamation_invoice ADD COLUMN mms_inv_cid varchar UNIQUE;
