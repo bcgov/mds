5bda9bb2be836c1289387ed0d8f636f63402b48f
mhuseinov <mekhti.huseinov.ssi@gmail.com>
2020-11-02 16:51:22 -0800

    MDS-2798: celery on openshift

--- services/document-manager/backend/app.sh
+++ services/document-manager/backend/app.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+cd /app
+celery worker -A app.tasks.celery --detach --loglevel=info --logfile=/var/log/celery/celery.log --pidfile=/var/run/celery/celery.pid --concurrency=1
+
 uwsgi uwsgi.ini
