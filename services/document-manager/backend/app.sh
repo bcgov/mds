32a9d8c203999f02022d9d8b59494b5b0552fa23
mhuseinov <mekhti.huseinov.ssi@gmail.com>
2020-11-04 11:06:24 -0800

    MDS-2798: celery on openshift

--- services/document-manager/backend/app.sh
+++ services/document-manager/backend/app.sh
@@ -2,6 +2,6 @@
 # next line works for couple of minutes and then pod gets killed: Crash Loop Back-off
 # celery worker -A app.tasks.celery --detach --loglevel=info --concurrency=1
 
-# python celery_start.py &
-
 uwsgi uwsgi.ini
+
+python celery_start.py
--- services/document-manager/backend/celery_start.py
+++ services/document-manager/backend/celery_start.py
@@ -1,4 +1,4 @@
 import subprocess
 
 worker_cmd = "celery worker -A app.tasks.celery --detach --loglevel=info --concurrency=1"
-subprocess.Popen(worker_cmd, cwd='/opt/app-root/src', close_fds=True, shell=True)
+subprocess.Popen(worker_cmd, cwd='.', close_fds=True, shell=True)
