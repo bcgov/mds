b4005cf6812972a552ea84f5448cb12ccf92bdc9
jsyro <jasyrotuck@gmail.com>
2020-11-10 13:24:15 -0800

    update route.

--- services/core-api/app/api/now_applications/namespace.py
+++ services/core-api/app/api/now_applications/namespace.py
@@ -34,12 +34,13 @@ api.add_resource(NOWApplicationDocumentResource,
 api.add_resource(NOWApplicationDocumentGenerateResource,
                  '/application-document-types/<string:document_type_code>/generate')
 api.add_resource(NOWApplicationDelayListResource, '/<string:now_application_guid>/delays')
+api.add_resource(NOWApplicationDelayResource,
+                 '/<string:now_application_guid>/delays/<string:now_application_delay_guid> ')
 
 # now static content
 api.add_resource(NOWActivityTypeResource, '/activity-types')
 api.add_resource(NOWApplicationTypeResource, '/application-types')
 api.add_resource(NOWApplicationDelayTypeResource, '/delay-reasons')
-api.add_resource(NOWApplicationDelayResource, '/delay/<string:now_application_delay_guid> ')
 api.add_resource(NOWApplicationStatusCodeResource, '/application-status-codes')
 api.add_resource(UnitTypeResource, '/unit-types')
 api.add_resource(NOWApplicationDocumentTypeListResource, '/application-document-types')
