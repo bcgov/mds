85e6d33e8449c1db92ae00160b0c4f70773cea4c
Paul Dittaro <pdittaro@gmail.com>
2020-11-17 09:16:12 -0800

    Revert original changes
    Signed-off-by: Paul Dittaro <pdittaro@gmail.com>

--- services/digdag/etl/tasks/nris_data_import.py
+++ services/digdag/etl/tasks/nris_data_import.py
@@ -1,19 +1,6 @@
 from models.pod import POD
 
-from util.env_builder import EnvBuilder
-
-
 def run_job():
-
-    builder = EnvBuilder()
-    builder.add_value(key='APP_FILE', value='/opt/app-root/src/run_etl.py')
-    env = builder.to_json()
-
-    pod = POD(
-        pod_name='digdag-nris-job',
-        env_pod='mds-nris-backend',
-        env=env,
-        command=["/usr/libexec/s2i/run"])
-
+    pod = POD(pod_name='digdag-nris-job', env_pod='mds-nris-backend', command=["flask","run-nris-etl-job"])
     pod.create_pod()
     print("Job finished")
