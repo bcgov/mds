ca0b0594daef06cfcbfb634b970c80b1f2eb417b
Luke Mitchell <lukegamitchell@gmail.com>
2020-12-11 11:02:26 -0800

    Change text relevant to Ministry name change (EMPR --> EMLI)

---
+++ migrations/sql/V2020.12.11.08.53__update_empr_to_emli.sql
@@ -0,0 +1,8 @@
+-- Changed "EMPR staff" to "Ministry staff"
+COMMENT ON TABLE mine_verified_status IS 'To aid the assurance of a mine''s data quality, this table is used to show whether or not the mine is verified to be correct (such as mine name, associated parties, etc.). This is determined by Ministry staff and a flag is set within the Core application.';
+
+-- Changed "business with EMPR" to "business with the Ministry"
+COMMENT ON TABLE party IS 'Party references an individual or an organization that does business with the Ministry, and any relevant contact information, such as address.';
+
+-- Changed from "lookup table for the types of EMPR reactions to a given mine_incident."
+COMMENT ON TABLE mine_incident_followup_type IS 'Lookup table for the types of Ministry reactions to a given mine incident.';
--- services/core-api/app/api/incidents/resources/mine_incident_followup_types.py
+++ services/core-api/app/api/incidents/resources/mine_incident_followup_types.py
@@ -1,22 +1,15 @@
-from flask_restplus import Resource, reqparse, fields, inputs
-from flask import request
-from datetime import datetime
-from werkzeug.exceptions import BadRequest, NotFound, InternalServerError
+from flask_restplus import Resource
 
 from app.extensions import api
-from app.api.utils.resources_mixins import UserMixin
-from app.api.utils.access_decorators import requires_role_view_all, requires_role_mine_edit
-
+from app.api.utils.access_decorators import requires_role_view_all
 from app.api.incidents.response_models import MINE_INCIDENT_FOLLOWUP_INVESTIGATION_TYPE_MODEL
 from app.api.incidents.models.mine_incident_followup_investigation_type import MineIncidentFollowupInvestigationType
 
 
-class MineIncidentFollowupTypeResource(Resource, UserMixin):
-    @api.marshal_with(MINE_INCIDENT_FOLLOWUP_INVESTIGATION_TYPE_MODEL,
-                      envelope='records',
-                      code=200,
-                      as_list=True)
-    @api.doc(description='Returns the possible EMPR followup action for mine incidents')
+class MineIncidentFollowupTypeResource(Resource):
+    @api.marshal_with(
+        MINE_INCIDENT_FOLLOWUP_INVESTIGATION_TYPE_MODEL, envelope='records', code=200, as_list=True)
+    @api.doc(description='Returns the possible followup action for mine incidents')
     @requires_role_view_all
     def get(self):
         return MineIncidentFollowupInvestigationType.query.all()
