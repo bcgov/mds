7269b1ee34d2907aa212a71d4ec4707316295ee3
justinmacaulay <justinkmacaulay@gmail.com>
2020-12-14 11:59:17 -0800

    change where the permit number creation occurs.

--- services/core-api/app/api/now_applications/resources/now_application_status_resource.py
+++ services/core-api/app/api/now_applications/resources/now_application_status_resource.py
@@ -72,13 +72,13 @@ class NOWApplicationStatusResource(Resource, UserMixin):
                 if permit.permit_status_code == 'D':
                     permit.permit_status_code = 'O'
                     permit_amendment.permit_amendment_status_code = 'OGP'
+                    #assign permit_no
+                    permit.assign_permit_no(
+                    now_application_identity.now_application.notice_of_work_type_code[0])
 
                 if permit_amendment.permit_amendment_status_code == 'DFT':
                     permit_amendment.permit_amendment_status_code = 'ACT'
-
-                #assign permit_no
-                permit.assign_permit_no(
-                    now_application_identity.now_application.notice_of_work_type_code[0])
+                
 
                 permit.save()
 
