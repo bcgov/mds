e8e97d1de069095d781d69f5cced9fa21000521e
justinmacaulay <justinkmacaulay@gmail.com>
2020-12-17 09:25:50 -0800

    missed debug

--- services/core-api/app/api/now_applications/resources/now_application_export_resource.py
+++ services/core-api/app/api/now_applications/resources/now_application_export_resource.py
@@ -111,7 +111,6 @@ class NOWApplicationExportResource(Resource, UserMixin):
             def get_address(contact):
                 if  not contact.get('party', None) or not contact['party'].get('address', None) or not len(contact['party']['address']) > 0:
                     return ''
-                current_app.logger.debug(contact['party']['address'])
                 address = contact['party']['address'][0]
                 address_string = ''
                 if address.get('suite_no', None):
