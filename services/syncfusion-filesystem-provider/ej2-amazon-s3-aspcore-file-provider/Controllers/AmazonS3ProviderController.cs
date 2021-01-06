6931aafe1bd6c651d503e522bcec4fb6d20cb63e
Luke Mitchell <lukegamitchell@gmail.com>
2021-01-06 10:32:33 -0800

    Delete file/folder modification actions

--- services/syncfusion-filesystem-provider/ej2-amazon-s3-aspcore-file-provider/Controllers/AmazonS3ProviderController.cs
+++ services/syncfusion-filesystem-provider/ej2-amazon-s3-aspcore-file-provider/Controllers/AmazonS3ProviderController.cs
@@ -32,46 +32,17 @@ namespace EJ2AmazonS3ASPCoreFileProvider.Controllers
         [Route("AmazonS3FileOperations")]
         public object AmazonS3FileOperations([FromBody] FileManagerDirectoryContent args)
         {
-            if (args.Action == "delete" || args.Action == "rename")
-            {
-                if ((args.TargetPath == null) && (args.Path == ""))
-                {
-                    FileManagerResponse response = new FileManagerResponse();
-                    ErrorDetails er = new ErrorDetails
-                    {
-                        Code = "401",
-                        Message = "Restricted to modify the root folder."
-                    };
-                    response.Error = er;
-                    return this.operation.ToCamelCase(response);
-                }
-            }
             switch (args.Action)
             {
                 case "read":
                     // reads the file(s) or folder(s) from the given path.
                     return this.operation.ToCamelCase(this.operation.GetFiles(args.Path, false, args.Data));
-                case "delete":
-                    // deletes the selected file(s) or folder(s) from the given path.
-                    return this.operation.ToCamelCase(this.operation.Delete(args.Path, args.Names, args.Data));
-                case "copy":
-                    // copies the selected file(s) or folder(s) from a path and then pastes them into a given target path.
-                    return this.operation.ToCamelCase(this.operation.Copy(args.Path, args.TargetPath, args.Names, args.RenameFiles, args.TargetData, args.Data));
-                case "move":
-                    // cuts the selected file(s) or folder(s) from a path and then pastes them into a given target path.
-                    return this.operation.ToCamelCase(this.operation.Move(args.Path, args.TargetPath, args.Names, args.RenameFiles, args.TargetData, args.Data));
                 case "details":
                     // gets the details of the selected file(s) or folder(s).
                     return this.operation.ToCamelCase(this.operation.Details(args.Path, args.Names, args.Data));
-                case "create":
-                    // creates a new folder in a given path.
-                    return this.operation.ToCamelCase(this.operation.Create(args.Path, args.Name, args.Data));
                 case "search":
                     // gets the list of file(s) or folder(s) from a given path based on the searched key string.
                     return this.operation.ToCamelCase(this.operation.Search(args.Path, args.SearchString, args.ShowHiddenItems, args.CaseSensitive));
-                case "rename":
-                    // renames a file or folder.
-                    return this.operation.ToCamelCase(this.operation.Rename(args.Path, args.Name, args.NewName, false, args.Data));
             }
             return null;
         }
