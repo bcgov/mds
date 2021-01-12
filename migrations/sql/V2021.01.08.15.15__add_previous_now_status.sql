a22095e2e4fefb3d3b7e5f23473b4cc86367319e
Meredith Mumby <meredith@freshworks.io>
2021-01-12 10:47:39 -0800

    added FK to previous application status code

--- migrations/sql/V2021.01.08.15.15__add_previous_now_status.sql
+++ migrations/sql/V2021.01.08.15.15__add_previous_now_status.sql
@@ -1,4 +1,5 @@
 Alter table now_application add column previous_application_status_code character varying(3);
+ALTER TABLE ONLY now_application ADD CONSTRAINT previous_application_status_code_fkey FOREIGN KEY (previous_application_status_code) REFERENCES now_application_status(now_application_status_code);
 
 UPDATE now_application_status SET active_ind = false WHERE now_application_status_code = 'REI';
 UPDATE now_application_status SET active_ind = false WHERE now_application_status_code = 'PCO';
