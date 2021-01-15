03317f16efc892f7c9fedb50713592bc8bdded83
Meredith Mumby <meredith@freshworks.io>
2021-01-14 16:00:57 -0800

    updated integers on activity_detail to be numberic

---
+++ migrations/sql/V2021.01.14.15.10__update_now_integer_to_numeric.sql
@@ -0,0 +1,8 @@
+ALTER TABLE activity_detail
+ALTER COLUMN length TYPE numeric(14,2),
+ALTER COLUMN width TYPE numeric(14,2),
+ALTER COLUMN depth TYPE numeric(14,2),
+ALTER COLUMN height TYPE numeric(14,2),
+ALTER COLUMN quantity TYPE numeric(14,2),
+ALTER COLUMN water_quantity TYPE numeric(14,2),
+ALTER COLUMN cut_line_length TYPE numeric(14,2);
