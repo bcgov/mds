4a507b0363da76c8e747f0e3dc8c2c225e96ea97
Ryan Rondeau <ryan_rondeau@live.ca>
2021-01-15 10:50:49 -0800

    Update V2021.01.12.14.09__remove_additional_conditions_category.sql

--- migrations/sql/V2021.01.12.14.09__remove_additional_conditions_category.sql
+++ migrations/sql/V2021.01.12.14.09__remove_additional_conditions_category.sql
@@ -1,3 +1,4 @@
 DELETE from permit_conditions where condition_category_code = 'ADC';
+DELETE FROM mine_report_submission WHERE mine_report_id IN (SELECT mine_report_id FROM mine_report where permit_condition_category_code = 'ADC');
 DELETE from mine_report where permit_condition_category_code = 'ADC';
 DELETE from permit_condition_category where condition_category_code = 'ADC';
