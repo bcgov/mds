ca9d217e7cdf9453141e1a8836453b0af08fff3b
Luke Mitchell <lukegamitchell@gmail.com>
2021-01-18 09:56:05 -0800

    More safely combine final app package docs

--- services/core-web/src/components/noticeOfWork/applications/permitGeneration/NOWPermitGeneration.js
+++ services/core-web/src/components/noticeOfWork/applications/permitGeneration/NOWPermitGeneration.js
@@ -257,18 +257,26 @@ export class NOWPermitGeneration extends Component {
   };
 
   getFinalApplicationPackage = (noticeOfWork) => {
-    const documents = noticeOfWork.filtered_submission_documents
-      .filter(({ is_final_package }) => is_final_package)
-      .map((doc) => ({
-        document_info: getDocumentInfo(doc),
-      }));
-    return documents.concat(
-      noticeOfWork.documents
-        .filter(({ is_final_package }) => is_final_package)
+    let documents = [];
+    let filteredSubmissionDocuments = noticeOfWork?.filtered_submission_documents;
+    let requestedDocuments = noticeOfWork?.documents;
+    if (!isEmpty(filteredSubmissionDocuments)) {
+      filteredSubmissionDocuments = filteredSubmissionDocuments
+        ?.filter(({ is_final_package }) => is_final_package)
         .map((doc) => ({
           document_info: getDocumentInfo(doc),
-        }))
-    );
+        }));
+      documents = filteredSubmissionDocuments;
+    }
+    if (!isEmpty(requestedDocuments)) {
+      requestedDocuments = requestedDocuments
+        ?.filter(({ is_final_package }) => is_final_package)
+        .map((doc) => ({
+          document_info: getDocumentInfo(doc),
+        }));
+      documents = [...documents, ...requestedDocuments];
+    }
+    return documents;
   };
 
   handlePermitGenSubmit = () => {
