48269a472eff32d90e8812a69659dc8e6e916c4e
Jason Sy <jasyrotuck@gmail.com>
2021-01-27 13:54:40 -0800

    remove unused imports

--- services/core-api/app/api/mines/permits/permit_amendment/resources/permit_amendment_issue_vc.py
+++ services/core-api/app/api/mines/permits/permit_amendment/resources/permit_amendment_issue_vc.py
@@ -1,20 +1,10 @@
-import decimal
-import uuid
-import requests
-import json
-
 from datetime import datetime
 from flask import request, current_app
 from flask_restplus import Resource, reqparse
-from werkzeug.datastructures import FileStorage
-from werkzeug.exceptions import BadRequest, InternalServerError, NotFound
-from sqlalchemy.exc import DBAPIError
+from werkzeug.exceptions import BadRequest
 
 from app.api.mines.permits.permit_amendment.models.permit_amendment import PermitAmendment
 
-from app.api.services.document_manager_service import DocumentManagerService
-
-from app.extensions import api, db
 from app.api.utils.access_decorators import requires_role_mine_admin
 from app.api.utils.resources_mixins import UserMixin
 
@@ -29,5 +19,6 @@ class PermitAmendmentIssueVCResource(Resource, UserMixin):
 
         response = OrgBookIssuerService().issue_permit_amendment_vc(permit_amendment)
         if not response:
-            return "Credential Not Issued, ensure permittee is associated with OrgBook Entity", 400
+            raise BadRequest(
+                "Credential Not Issued, ensure permittee is associated with OrgBook Entity")
         return
