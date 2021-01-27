c06ba70c09b474427c7c5201a1b7af93543426fb
Jason Sy <jasyrotuck@gmail.com>
2021-01-27 13:57:17 -0800

    add context to popconfirm

--- services/core-web/src/components/mine/Permit/MinePermitTable.js
+++ services/core-web/src/components/mine/Permit/MinePermitTable.js
@@ -425,7 +425,7 @@ const childColumns = [
         <AuthorizationWrapper permission={Permission.ADMIN}>
           <Popconfirm
             placement="topLeft"
-            title={`Are you sure you want to Issue this permit as a Verifiable Credential to OrgBook ${record.email}?`}
+            title={`Are you sure you want to Issue this permit as a Verifiable Credential to OrgBook entity: ${record.permit.permittee}?`}
             onConfirm={(event) =>
               record.handlePermitAmendmentIssueVC(event, text.amendment, record.permit)
             }
