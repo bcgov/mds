667b2b25a38f0ef1f99e322dc970426cf6e4f3b5
Jason Sy <jasyrotuck@gmail.com>
2021-01-27 13:55:51 -0800

    needed this one.

--- services/core-api/app/api/mines/permits/permit_amendment/resources/permit_amendment_issue_vc.py
+++ services/core-api/app/api/mines/permits/permit_amendment/resources/permit_amendment_issue_vc.py
@@ -5,6 +5,7 @@ from werkzeug.exceptions import BadRequest
 
 from app.api.mines.permits.permit_amendment.models.permit_amendment import PermitAmendment
 
+from app.extensions import api
 from app.api.utils.access_decorators import requires_role_mine_admin
 from app.api.utils.resources_mixins import UserMixin
 
