987581dc0b367b282f30228d965dad9d29b3efc6
bcgov-csnr-cd <bcgov-csnr-cd@gov.bc.ca>
2021-01-29 14:28:20 +0000

    Merge branch develop into master

--- services/core-api/app/api/parties/party/models/party.py
+++ services/core-api/app/api/parties/party/models/party.py
@@ -1,55 +1,58 @@
 from datetime import datetime
 import re
-import uuid
 
-from sqlalchemy import func
+from sqlalchemy import func, case, and_
 from sqlalchemy.schema import FetchedValue
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import validates
-from app.extensions import db
 from werkzeug.exceptions import BadRequest
 
+from app.extensions import db
 from app.api.utils.models_mixins import SoftDeleteMixin, AuditMixin, Base
 from app.api.parties.party.models.address import Address
+
+MAX_NAME_LENGTH = 100
 
 
 class Party(SoftDeleteMixin, AuditMixin, Base):
     __tablename__ = 'party'
+
     party_guid = db.Column(UUID(as_uuid=True), primary_key=True, server_default=FetchedValue())
-    first_name = db.Column(db.String, nullable=True)
-    middle_name = db.Column(db.String, nullable=True)
+    first_name = db.Column(db.String)
     party_name = db.Column(db.String, nullable=False)
-    phone_no = db.Column(db.String, nullable=False)
-    phone_ext = db.Column(db.String, nullable=True)
-    email = db.Column(db.String, nullable=True)
+    phone_no = db.Column(db.String)
+    phone_ext = db.Column(db.String)
+    email = db.Column(db.String)
     party_type_code = db.Column(db.String, db.ForeignKey('party_type_code.party_type_code'))
+    address = db.relationship('Address', lazy='joined')
+    job_title = db.Column(db.String)
+    postnominal_letters = db.Column(db.String)
+    idir_username = db.Column(db.String)
+    signature = db.Column(db.String)
 
     mine_party_appt = db.relationship('MinePartyAppointment', lazy='joined')
-    address = db.relationship('Address', lazy='joined')
-    job_title = db.Column(db.String, nullable=True)
-    postnominal_letters = db.Column(db.String, nullable=True)
-    idir_username = db.Column(db.String, nullable=True)
-    signature = db.Column(db.String, nullable=True)
+
     now_party_appt = db.relationship(
         'NOWPartyAppointment',
         lazy='selectin',
         primaryjoin=
-        "and_(NOWPartyAppointment.party_guid == Party.party_guid, NOWPartyAppointment.deleted_ind==False)"
+        'and_(NOWPartyAppointment.party_guid == Party.party_guid, NOWPartyAppointment.deleted_ind==False)'
     )
 
     business_role_appts = db.relationship(
         'PartyBusinessRoleAppointment',
         lazy='dynamic',
         primaryjoin=
-        "and_(Party.party_guid == PartyBusinessRoleAppointment.party_guid, PartyBusinessRoleAppointment.deleted_ind==False)",
+        'and_(Party.party_guid == PartyBusinessRoleAppointment.party_guid, PartyBusinessRoleAppointment.deleted_ind==False)',
     )
+
     party_orgbook_entity = db.relationship(
         'PartyOrgBookEntity', backref='party_orgbook_entity', uselist=False, lazy='select')
 
     @hybrid_property
     def name(self):
-        return self.first_name + ' ' + self.party_name if self.first_name else self.party_name
+        return f'{self.first_name} {self.party_name}' if self.first_name and self.party_type_code == 'PER' else self.party_name
 
     @hybrid_property
     def phone(self):
@@ -69,10 +72,6 @@ class Party(SoftDeleteMixin, AuditMixin, Base):
             if (not x.end_date or x.end_date > datetime.utcnow().date())
         ]
 
-    @name.expression
-    def name(cls):
-        return func.concat(cls.first_name, ' ', cls.party_name)
-
     def __repr__(self):
         return '<Party %r>' % self.party_guid
 
@@ -91,6 +90,7 @@ class Party(SoftDeleteMixin, AuditMixin, Base):
             'postnominal_letters': self.postnominal_letters,
             'idir_username': self.idir_username
         }
+
         if self.party_type_code == 'PER':
             context.update({
                 'first_name': self.first_name,
@@ -100,15 +100,20 @@ class Party(SoftDeleteMixin, AuditMixin, Base):
             context.update({
                 'mine_party_appt': [item.json() for item in self.mine_party_appt],
             })
+
         return context
 
+    @name.expression
+    def name(cls):
+        return case([
+            (and_(cls.first_name != None, cls.party_type_code
+                  == 'PER'), f'{cls.first_name} {cls.party_name}'),
+        ],
+                    else_=cls.party_name)
+
     @classmethod
-    def find_by_party_guid(cls, _id):
-        try:
-            uuid.UUID(_id)
-        except ValueError:
-            raise BadRequest('Invalid Party guid')
-        return cls.query.filter_by(party_guid=_id, deleted_ind=False).first()
+    def find_by_party_guid(cls, party_guid):
+        return cls.query.filter_by(party_guid=party_guid, deleted_ind=False).one_or_none()
 
     @classmethod
     def find_by_name(cls, party_name, first_name=None):
@@ -133,33 +138,20 @@ class Party(SoftDeleteMixin, AuditMixin, Base):
                 cls.deleted_ind == False).limit(query_limit)
 
     @classmethod
-    def create(
-            cls,
-                                                 # Required fields
-            party_name,
-            phone_no,
-            party_type_code,
-                                                 # Optional fields
-            address_type_code=None,
-                                                 # Nullable fields
-            email=None,
-            first_name=None,
-            phone_ext=None,
-            suite_no=None,
-            address_line_1=None,
-            address_line_2=None,
-            city=None,
-            sub_division_code=None,
-            post_code=None,
-            add_to_session=True):
+    def create(cls,
+               party_name,
+               phone_no,
+               party_type_code,
+               email=None,
+               first_name=None,
+               phone_ext=None,
+               add_to_session=True):
         party = cls(
-                                                 # Required fields
             party_name=party_name,
             phone_no=phone_no,
             party_type_code=party_type_code,
-                                                 # Optional fields
             email=email,
-            first_name=first_name,
+            first_name=first_name if party_type_code == 'PER' else None,
             phone_ext=phone_ext)
         if add_to_session:
             party.save(commit=False)
@@ -176,17 +168,19 @@ class Party(SoftDeleteMixin, AuditMixin, Base):
     @validates('first_name')
     def validate_first_name(self, key, first_name):
         if self.party_type_code == 'PER' and not first_name:
-            raise AssertionError('Person first name is not provided.')
-        if first_name and len(first_name) > 100:
-            raise AssertionError('Person first name must not exceed 100 characters.')
+            raise AssertionError('First name is not provided.')
+        if self.party_type_code == 'ORG' and first_name:
+            raise AssertionError('Organizations cannot have a first name.')
+        if first_name and len(first_name) > MAX_NAME_LENGTH:
+            raise AssertionError(f'First name must not exceed {MAX_NAME_LENGTH} characters.')
         return first_name
 
     @validates('party_name')
     def validate_party_name(self, key, party_name):
         if not party_name:
             raise AssertionError('Party name is not provided.')
-        if len(party_name) > 100:
-            raise AssertionError('Party name must not exceed 100 characters.')
+        if len(party_name) > MAX_NAME_LENGTH:
+            raise AssertionError(f'Party name must not exceed {MAX_NAME_LENGTH} characters.')
         return party_name
 
     @validates('phone_no')
