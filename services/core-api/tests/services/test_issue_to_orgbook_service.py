ea3fb2c428d7367a3a4d9fe8f1e5698c7e114dd2
Jason Sy <jasyrotuck@gmail.com>
2021-01-29 17:01:23 -0800

    rename

--- services/core-api/tests/services/test_issue_to_orgbook_service.py
+++ services/core-api/tests/services/test_issue_to_orgbook_service.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from flask import current_app
 from unittest.mock import MagicMock, patch, PropertyMock
 
-from app.api.services.issue_to_orgbook_service import OrgBookIssuerControllerService
+from app.api.services.issue_to_orgbook_service import OrgBookIssuerService
 
 from app.api.parties.party.models.party_orgbook_entity import PartyOrgBookEntity
 
@@ -14,7 +14,7 @@ def test_issue_vc_without_orgbook_association_does_nothing(test_client, db_sessi
     MinePartyAppointmentFactory(permit=permit, permittee=True)
     with current_app.test_request_context() as a, patch(
             'app.api.services.issue_to_orgbook_service.requests.post') as mock:
-        service = OrgBookIssuerControllerService()
+        service = OrgBookIssuerService()
         service.issue_permit_amendment_vc(permit.permit_amendments[0])
         mock.assert_not_called()
 
@@ -33,6 +33,6 @@ def test_issue_vc_with_orgbook_association_sends_post(test_client, db_session, a
     with current_app.test_request_context() as a, patch(
             'app.api.services.issue_to_orgbook_service.requests.post',
             side_effect={MockResponse()}) as mock:
-        service = OrgBookIssuerControllerService()
+        service = OrgBookIssuerService()
         service.issue_permit_amendment_vc(permit.permit_amendments[0])
         mock.assert_called_once()
