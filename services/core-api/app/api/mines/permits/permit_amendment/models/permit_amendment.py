6aa0b5fdad880a86122c77bf7e95d8ed8af3f4b7
Jason Sy <jasyrotuck@gmail.com>
2021-01-29 16:34:04 -0800

    typ

--- services/core-api/app/api/mines/permits/permit_amendment/models/permit_amendment.py
+++ services/core-api/app/api/mines/permits/permit_amendment/models/permit_amendment.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date
 
 import uuid
 
@@ -92,7 +92,7 @@ class PermitAmendment(SoftDeleteMixin, AuditMixin, Base):
 
         used_by_major_mine = any([m.mine.major_mine_ind for m in self.all_mine_permit_xref])
         if used_by_major_mine:
-            if self.issue_date >= datetime(2020, 7, 17):
+            if self.issue_date >= date(2020, 7, 17):
                 return 'Chief Permitting Officer'
             else:
                 return 'Chief Inspector of Mines'
