6d955dbd6eb2db2af83162f4cd88030cc9261fdc
jon-funk <johnthomasfunk@gmail.com>
2021-02-18 15:02:05 -0800

    Redirect traffic to port 3000 for poc

--- terraform/modules/variables.tf
+++ terraform/modules/variables.tf
@@ -42,7 +42,7 @@ variable "app_image" {
 
 variable "app_port" {
   description = "Port exposed by the docker image to redirect traffic to"
-  default     = 80
+  default     = 3000
 }
 
 variable "app_count" {
