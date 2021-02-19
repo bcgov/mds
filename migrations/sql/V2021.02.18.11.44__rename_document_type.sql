241eee943bef757300b843c2f8f55c9d5b0f1310
Luke Mitchell <lukegamitchell@gmail.com>
2021-02-19 13:41:12 -0800

    Revert "NTRs have no form spec"
    This reverts commit 0532ac9478adf59cefc88e3ad89a18cccd4159c5.

--- migrations/sql/V2021.02.18.11.44__rename_document_type.sql
+++ migrations/sql/V2021.02.18.11.44__rename_document_type.sql
@@ -1,3 +1,2 @@
 UPDATE now_application_document_type SET description = 'Notice of Work Form' WHERE now_application_document_type_code = 'NTR';
 UPDATE document_template SET template_file_path = 'templates/now/Notice of Work Form.docx' WHERE document_template_code = 'NTR';
-UPDATE document_template SET form_spec_json = '' WHERE document_template_code = 'NTR';
