c79b5ed82531ee3915ade9cea02a2af63792a873
mhuseinov <61513701+mhuseinov@users.noreply.github.com>
2021-02-23 09:26:26 -0800

    Feature/mds 3374 unit type in core (#1662)
    * Create V2021.02.18.11.44__rename_document_type.sql
    
    * Change document name from Technical Review --> Notice of Work Form
    
    * MDS-3374: added water supply units to Edited application pdf
    
    * MDS-3374: access road snapshot
    
    * MDS-3374: snapshot test
    
    * MDS-3374: fix for editable value unit column
    
    * MDS-3374: updates for editable table, updated undergraound exploration default unit_type_code
    
    * MDS-3374: updated Placer Operations
    
    * MDS-3374: make Estimate Rate required field on Water supply
    
    * MDS-3374: added space to titles, upadated tests
    
    * MDS-3374: updated cut lines title

---
+++ migrations/sql/V2021.02.18.11.44__rename_document_type.sql
@@ -0,0 +1,2 @@
+UPDATE now_application_document_type SET description = 'Notice of Work Form' WHERE now_application_document_type_code = 'NTR';
+UPDATE document_template SET template_file_path = 'templates/now/Notice of Work Form.docx' WHERE document_template_code = 'NTR';
