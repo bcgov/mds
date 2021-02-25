a302adb7fe633a6c059204caf2085bbebb7a764c
mhuseinov <61513701+mhuseinov@users.noreply.github.com>
2021-02-25 11:36:44 -0800

    Feature/mds 3374 unit type in core (#1663)
    * Create V2021.02.18.11.44__rename_document_type.sql
    
    * Change document name from Technical Review --> Notice of Work Form
    
    * MDS-3374: added water supply units to Edited application pdf
    
    * MDS-3374: access road snapshot
    
    * MDS-3374: snapshot test
    
    * MDS-3374: fix for editable value unit column
    
    * MDS-3374: updates for editable table, updated undergraound exploration default unit_type_code
    
    * MDS-3374: updated Placer Operations
    
    * MDS-3374: make Estimate Rate required field on Water supply
    
    * MDS-3374: added space to titles, upadated tests
    
    * MDS-3374: updated cut lines title
    
    * MDS-3374: set default values for length, width, height, migration for historical data
    
    * MDS-3374: updated migration file name

--- services/core-api/app/api/now_applications/models/activity_detail/placer_operation_detail.py
+++ services/core-api/app/api/now_applications/models/activity_detail/placer_operation_detail.py
@@ -17,3 +17,8 @@ class PlacerOperationDetail(ActivityDetailBase):
 
     def __repr__(self):
         return '<PlacerOperationDetail %r>' % self.activity_detail_id
+
+    def __init__(self, **kwargs):
+        self.length_unit_type_code = 'MTR'
+        self.width_unit_type_code = 'MTR'
+        super(PlacerOperationDetail, self).__init__(**kwargs)
