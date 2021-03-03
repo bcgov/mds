0f28942a387bed8c33b41f252994531214f86914
Meredith Mumby <meredith@freshworks.io>
2021-03-03 12:00:58 -0800

    updated sql

--- migrations/sql/V2021.03.01.15.00__update_proposed_reclamation_placer.sql
+++ migrations/sql/V2021.03.01.15.00__update_proposed_reclamation_placer.sql
@@ -1,3 +1,5 @@
 ALTER TABLE placer_operation ADD COLUMN planned_reclamation numeric(14,2);
 
-UPDATE placer_operation po SET planned_reclamation = (SELECT total_disturbed_area FROM activity_summary as WHERE po.activity_summary_id = as.activity_summary_id);
+UPDATE placer_operation po SET planned_reclamation = 
+(SELECT total_disturbed_area FROM activity_summary as 
+WHERE po.activity_summary_id = as.activity_summary_id);
