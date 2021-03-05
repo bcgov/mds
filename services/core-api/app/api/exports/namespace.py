72174aa8461bb673d26be4b9a5f7499953871170
Luke Mitchell <lukegamitchell@gmail.com>
2021-03-05 10:16:57 -0800

    Add GIS export endpoint to namespace

--- services/core-api/app/api/exports/namespace.py
+++ services/core-api/app/api/exports/namespace.py
@@ -2,10 +2,12 @@ from flask_restplus import Namespace
 
 from app.api.exports.mines.resources.mine_summary_resource import MineSummaryResource
 from app.api.exports.mines.resources.mine_summary_csv_resource import MineSummaryCSVResource
+from app.api.exports.now_application.resources.now_application_gis_export_resource import NowApplicationGisExport
 from app.api.exports.static_content.resources.core_static_content_resource import StaticContentResource
 
 api = Namespace('exports', description='Data dumps')
 
 api.add_resource(MineSummaryCSVResource, '/mine-summary-csv')
 api.add_resource(MineSummaryResource, '/mine-summary')
+api.add_resource(NowApplicationGisExport, '/now-application-gis-export')
 api.add_resource(StaticContentResource, '/core-static-content')
