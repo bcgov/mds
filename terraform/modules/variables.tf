063f11b99664c3dc0d88425e6c07d629c06062f0
jon-funk <johnthomasfunk@gmail.com>
2021-03-09 09:37:01 -0800

    Increase fargate resources

--- terraform/modules/variables.tf
+++ terraform/modules/variables.tf
@@ -72,12 +72,12 @@ variable "health_check_path" {
 
 variable "fargate_cpu" {
   description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
-  default     = 512
+  default     = 2048
 }
 
 variable "fargate_memory" {
   description = "Fargate instance memory to provision (in MiB)"
-  default     = 1024
+  default     = 4096
 }
 
 variable "budget_amount" {
