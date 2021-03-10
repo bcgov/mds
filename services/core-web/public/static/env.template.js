44f70ab62f569637b9cf95816241fc8a63f54e04
Paul Dittaro <pdittaro@gmail.com>
2021-03-10 11:03:17 -0800

    Change fetchenv to grab from Window
    Signed-off-by: Paul Dittaro <pdittaro@gmail.com>

--- services/core-web/public/static/env.template.js
+++ services/core-web/public/static/env.template.js
@@ -1,5 +1,22 @@
 (function(window) {
   window.env = window.env || {};
   // Environment variables
-  window.env.apiUrl = "${API_URL}";
+  window.env = window.env || {};
+  // Environment variables
+  window.env.environment = "${ENVIRONMENT}" || "development";
+  window.env.basePath = "${BASE_PATH}";
+  window.env.assetPath = "${ASSET_PATH}" || "/";
+
+  window.env.keycloakUrl = "${KEYCLOAK_URL}" || "https://test.oidc.gov.bc.ca/auth";
+  window.env.keycloakResource = "${KEYCLOAK_RESOURCE}" || "mines-application-local";
+  window.env.keycloakClientId = "${KEYCLOAK_CLIENT_ID}" || "mines-application-local";
+  window.env.keycloakIdpHint = "${KEYCLOAK_IDP_HINT}" || "test";
+
+  window.env.apiUrl = "${API_URL}" || "http://localhost:5000";
+  window.env.docManUrl = "${DOCUMENT_MANAGER_URL}" || "http://localhost:5001";
+  window.env.filesystemProviderUrl =
+    "${FILESYSTEM_PROVIDER_URL}" || "http://localhost:62870/file-api/AmazonS3Provider/";
+  window.env.matomoUrl = "${MATOMO_URL}" || "https://matomo-empr-mds-test.pathfinder.gov.bc.ca/";
+  window.env.firstNationsLayerUrl =
+    "${FN_LAYER_URL}" || "https://delivery.apps.gov.bc.ca/ext/sgw/geo.allgov";
 })(this);
