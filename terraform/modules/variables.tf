06f71133de4ae7a964ec09459d2655581b9a127b
jon-funk <johnthomasfunk@gmail.com>
2021-03-12 12:44:25 -0800

    Change alb port mapping to 80

--- terraform/modules/variables.tf
+++ terraform/modules/variables.tf
@@ -53,7 +53,7 @@ variable "app_image" {
 
 variable "app_port" {
   description = "Port exposed by the docker image to redirect traffic to"
-  default     = 3000
+  default     = 80
 }
 
 variable "app_count" {
