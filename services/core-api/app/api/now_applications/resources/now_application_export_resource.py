2f2bdf16988cb4b7f60da3236839c2122a15992a
Luke Mitchell <lukegamitchell@gmail.com>
2021-04-14 15:34:19 -0700

    Formatting, only render applicable + non-empty sections

--- services/core-api/app/api/now_applications/resources/now_application_export_resource.py
+++ services/core-api/app/api/now_applications/resources/now_application_export_resource.py
@@ -28,54 +28,102 @@ EMPTY_FIELD = '-'
 
 CURRENCY_FIELDS = ['reclamation_cost']
 
-IGNORE_EMPTY_FIELDS = ['details', 'equipment']
-
 EXCLUDED_APPLICATION_DOCUMENT_TYPES = []
 
 ORIGINAL_NOW_FIELD_PATHS = [
-    'property_name', 'mine_no', 'mine_region', 'latitude', 'description_of_land', 'longitude', 'work_plan',
-    'type_of_application', 'notice_of_work_type_code', 'application_permit_type_code',
-    'proposed_start_date', 'crown_grant_or_district_lot_numbers', 'proposed_end_date',
-    'tenure_number', 'directions_to_site', 'has_req_access_authorizations',
-    'has_surface_disturbance_outside_tenure', 'req_access_authorization_numbers', 'is_access_gated',
-    'has_key_for_inspector', 'state_of_land.present_land_condition_description',
-    'state_of_land.means_of_access_description', 'state_of_land.physiography_description',
-    'state_of_land.old_equipment_description', 'state_of_land.has_community_water_shed',
-    'state_of_land.type_of_vegetation_description', 'state_of_land.has_activity_in_park',
-    'state_of_land.is_on_private_land', 'state_of_land.has_auth_lieutenant_gov_council',
+    'property_name',
+    'mine_no',
+    'mine_region',
+    'latitude',
+    'description_of_land',
+    'longitude',
+    'work_plan',
+    'type_of_application',
+    'notice_of_work_type_code',
+    'application_permit_type_code',
+    'proposed_start_date',
+    'crown_grant_or_district_lot_numbers',
+    'proposed_end_date',
+    'tenure_number',
+    'directions_to_site',
+    'has_req_access_authorizations',
+    'has_surface_disturbance_outside_tenure',
+    'req_access_authorization_numbers',
+    'is_access_gated',
+    'has_key_for_inspector',
+    'state_of_land.present_land_condition_description',
+    'state_of_land.means_of_access_description',
+    'state_of_land.physiography_description',
+    'state_of_land.old_equipment_description',
+    'state_of_land.has_community_water_shed',
+    'state_of_land.type_of_vegetation_description',
+    'state_of_land.has_activity_in_park',
+    'state_of_land.is_on_private_land',
+    'state_of_land.has_auth_lieutenant_gov_council',
     'state_of_land.has_fn_cultural_heritage_sites_in_area',
-    'state_of_land.fn_engagement_activities', 'state_of_land.cultural_heritage_description',
-    'state_of_land.recreational_trail_use_description', 'state_of_land.has_shared_info_with_fn',
-    'state_of_land.has_archaeology_sites_affected', 'first_aid_equipment_on_site',
-    'first_aid_cert_level', 'exploration_access.reclamation_description',
-    'exploration_access.reclamation_cost', 'blasting_operation.has_storage_explosive_on_site',
-    'blasting_operation.explosive_permit_issued', 'blasting_operation.explosive_permit_expiry_date',
-    'blasting_operation.explosive_permit_number', 'camp.has_fuel_stored', 'camp.volume_fuel_stored',
-    'camp.has_fuel_stored_in_bulk', 'camp.has_fuel_stored_in_barrels',
-    'camp.reclamation_description', 'camp.reclamation_cost',
+    'state_of_land.fn_engagement_activities',
+    'state_of_land.cultural_heritage_description',
+    'state_of_land.recreational_trail_use_description',
+    'state_of_land.has_shared_info_with_fn',
+    'state_of_land.has_archaeology_sites_affected',
+    'first_aid_equipment_on_site',
+    'first_aid_cert_level',
+    'exploration_access.reclamation_description',
+    'exploration_access.reclamation_cost',
+    'blasting_operation.has_storage_explosive_on_site',
+    'blasting_operation.explosive_permit_issued',
+    'blasting_operation.explosive_permit_expiry_date',
+    'blasting_operation.explosive_permit_number',
+    'camp.has_fuel_stored',
+    'camp.volume_fuel_stored',
+    'camp.has_fuel_stored_in_bulk',
+    'camp.has_fuel_stored_in_barrels',
+    'camp.reclamation_description',
+    'camp.reclamation_cost',
     'cut_lines_polarization_survey.reclamation_description',
     'cut_lines_polarization_survey.reclamation_cost',
-    'mechanical_trenching.reclamation_description', 'mechanical_trenching.reclamation_cost',
-    'placer_operation.is_underground', 'placer_operation.is_hand_operation',
-    'placer_operation.total_disturbed_area', 'placer_operation.reclamation_description',
-    'placer_operation.reclamation_cost', 'sand_and_gravel.average_overburden_depth',
-    'sand_and_gravel.average_top_soil_depth', 'sand_and_gravel.stability_measures_description',
-    'sand_and_gravel.is_agricultural_land_reserve', 'sand_and_gravel.land_use_zoning',
-    'sand_and_gravel.agri_lnd_rsrv_permit_application_number', 'sand_and_gravel.proposed_land_use',
-    'sand_and_gravel.community_plan', 'sand_and_gravel.has_local_soil_removal_bylaw',
-    'sand_and_gravel.total_mineable_reserves', 'sand_and_gravel.total_annual_extraction',
-    'sand_and_gravel.reclamation_description', 'sand_and_gravel.reclamation_cost',
-    'settling_pond.is_ponds_recycled', 'settling_pond.is_ponds_exfiltrated',
-    'settling_pond.is_ponds_discharged', 'settling_pond.reclamation_description',
-    'settling_pond.reclamation_cost', 'surface_bulk_sample.processing_method_description',
-    'surface_bulk_sample.reclamation_description', 'surface_bulk_sample.reclamation_cost',
+    'mechanical_trenching.reclamation_description',
+    'mechanical_trenching.reclamation_cost',
+    'placer_operation.is_underground',
+    'placer_operation.is_hand_operation',
+    'placer_operation.total_disturbed_area',
+    'placer_operation.reclamation_description',
+    'placer_operation.reclamation_cost',
+    'sand_and_gravel.average_overburden_depth',
+    'sand_and_gravel.average_top_soil_depth',
+    'sand_and_gravel.stability_measures_description',
+    'sand_and_gravel.is_agricultural_land_reserve',
+    'sand_and_gravel.land_use_zoning',
+    'sand_and_gravel.agri_lnd_rsrv_permit_application_number',
+    'sand_and_gravel.proposed_land_use',
+    'sand_and_gravel.community_plan',
+    'sand_and_gravel.has_local_soil_removal_bylaw',
+    'sand_and_gravel.total_mineable_reserves',
+    'sand_and_gravel.total_annual_extraction',
+    'sand_and_gravel.reclamation_description',
+    'sand_and_gravel.reclamation_cost',
+    'settling_pond.is_ponds_recycled',
+    'settling_pond.is_ponds_exfiltrated',
+    'settling_pond.is_ponds_discharged',
+    'settling_pond.reclamation_description',
+    'settling_pond.reclamation_cost',
+    'surface_bulk_sample.processing_method_description',
+    'surface_bulk_sample.reclamation_description',
+    'surface_bulk_sample.reclamation_cost',
     'exploration_surface_drilling.reclamation_core_storage',
     'exploration_surface_drilling.reclamation_description',
-    'exploration_surface_drilling.reclamation_cost', 'underground_exploration.total_ore_amount',
+    'exploration_surface_drilling.reclamation_cost',
+    'underground_exploration.total_ore_amount',
     'underground_exploration.total_waste_amount',
 ]
 
-UNIT_TYPE_CODE_FIELDS = ['estimate_rate_unit_type_code', 'length_unit_type_code', 'proposed_production_unit_type_code', 'reclamation_unit_type_code', 'average_overburden_depth_unit_type_code', 'average_top_soil_depth_unit_type_code', 'total_mineable_reserves_unit_type_code', 'total_annual_extraction_unit_type_code', 'total_ore_unit_type_code', 'total_waste_unit_type_code']
+UNIT_TYPE_CODE_FIELDS = [
+    'estimate_rate_unit_type_code', 'length_unit_type_code', 'proposed_production_unit_type_code',
+    'reclamation_unit_type_code', 'average_overburden_depth_unit_type_code',
+    'average_top_soil_depth_unit_type_code', 'total_mineable_reserves_unit_type_code',
+    'total_annual_extraction_unit_type_code', 'total_ore_unit_type_code',
+    'total_waste_unit_type_code'
+]
 
 
 class NOWApplicationExportResource(Resource, UserMixin):
@@ -176,7 +224,9 @@ class NOWApplicationExportResource(Resource, UserMixin):
         def get_reclamation_summary(now_application):
             summary = []
             activity_types = ActivityType.get_all()
-            reclamation_activity_types = (activity_type for activity_type in activity_types if activity_type.activity_type_code not in ['water_supply', 'blasting_operation'])
+            reclamation_activity_types = (
+                activity_type for activity_type in activity_types
+                if activity_type.activity_type_code not in ['water_supply', 'blasting_operation'])
             for activity_type in reclamation_activity_types:
                 if now_application.get(activity_type.activity_type_code):
                     summary.append({
@@ -191,30 +241,54 @@ class NOWApplicationExportResource(Resource, UserMixin):
                     })
             return summary
 
-        def get_applicable_now_activities(now_application):
-            conditional_sections = [
-                'sand_gravel_quarry_operation', 'surface_bulk_sample',
-                'cut_lines_polarization_survey', 'underground_exploration', 'placer_operation'
-            ]
-            now_type_conditional_sections = {
-                'QCA': ['sand_gravel_quarry_operation'],
-                'SAG': ['sand_gravel_quarry_operation'],
-                'QIM': ['sand_gravel_quarry_operation'],
-                'COL':
-                ['surface_bulk_sample', 'cut_lines_polarization_survey', 'underground_exploration'],
-                'MIN':
-                ['surface_bulk_sample', 'cut_lines_polarization_survey', 'underground_exploration'],
-                'PLA':
-                ['placer_operation', 'cut_lines_polarization_survey', 'underground_exploration']
-            }
-            applicable = {}
-            activity_types = ActivityType.get_all()
-            notice_of_work_type_code = now_application['notice_of_work_type_code']
-            for activity_type in activity_types:
-                if activity_type.activity_type_code in conditional_sections and activity_type.activity_type_code in now_type_conditional_sections[
-                        notice_of_work_type_code]:
-                    applicable[activity_type.activity_type_code] = True
-            return applicable
+        def get_renderable_now_sections(now_application):
+            def get_applicable_now_sections(now_application):
+                conditional_sections = [
+                    'sand_gravel_quarry_operation', 'surface_bulk_sample',
+                    'cut_lines_polarization_survey', 'underground_exploration', 'placer_operation'
+                ]
+                now_type_conditional_sections = {
+                    'QCA': ['sand_gravel_quarry_operation'],
+                    'SAG': ['sand_gravel_quarry_operation'],
+                    'QIM': ['sand_gravel_quarry_operation'],
+                    'COL':
+                    ['surface_bulk_sample', 'cut_lines_polarization_survey', 'underground_exploration'],
+                    'MIN':
+                    ['surface_bulk_sample', 'cut_lines_polarization_survey', 'underground_exploration'],
+                    'PLA':
+                    ['placer_operation', 'cut_lines_polarization_survey', 'underground_exploration']
+                }
+                applicable = {}
+                activity_types = ActivityType.get_all()
+                notice_of_work_type_code = now_application['notice_of_work_type_code']
+                for activity_type in activity_types:
+                    if activity_type.activity_type_code in conditional_sections and activity_type.activity_type_code in now_type_conditional_sections[
+                            notice_of_work_type_code] and :
+                        applicable[activity_type.activity_type_code] = True
+                return applicable
+
+            def get_populated_hideable_now_sections(now_application):
+                hideable_now_sections = [
+                    'sand_gravel_quarry_operation', 'exploration_access', 'blasting_operation', 'camp', 'cut_lines_polarization_survey',
+                    'exploration_surface_drilling', 'mechanical_trenching', 'settling_pond', 'surface_bulk_sample',
+                    'underground_exploration', 'sand_and_gravel', 'placer_operation', 'water_supply'
+                ]
+                populated = {}
+                for section in hideable_now_sections:
+                    populated[section] = False
+                    for key in now_application.get(section, {}):
+                        if now_application[section][key] != None:
+                            populated[section] = True
+                            break
+                return populated
+
+            applicable_sections = get_applicable_now_sections(now_application)
+            populated_sections = get_populated_hideable_now_sections(now_application)
+
+            render_sections = applicable_sections
+            for section in populated_sections:
+                render_sections[section] = render_sections[section] and populated_sections[section]
+            return render_sections
 
         def transform_documents(now_application):
             included_docs = []
@@ -231,8 +305,12 @@ class NOWApplicationExportResource(Resource, UserMixin):
             return included_docs
 
         def transform_data(obj):
+            ignore_empty_fields = ['details', 'equipment']
+
             for key in obj:
-                if obj[key] is None and key not in IGNORE_EMPTY_FIELDS:
+                if not obj[key] and key in ignore_empty_fields:
+                    continue
+                if obj[key] is None:
                     obj[key] = EMPTY_FIELD
                 elif key in CURRENCY_FIELDS:
                     obj[key] = format_currency(obj[key])
@@ -294,8 +372,7 @@ class NOWApplicationExportResource(Resource, UserMixin):
             permit_type, now_application_json['application_permit_type_code'])
         now_application_json = transform_data(now_application_json)
 
-        # NOTE: This should be always after transform_data
-        now_application_json['render'] = get_applicable_now_activities(now_application_json)
+        now_application_json['render'] = get_renderable_now_sections(now_application_json)
 
         # Determine what fields have changed from the original application
         original_now_application_json = transform_data(original_now_application_json)
@@ -306,7 +383,7 @@ class NOWApplicationExportResource(Resource, UserMixin):
                 if not edited_fields.get(paths[0]):
                     edited_fields[paths[0]] = {}
                 current_value = now_application_json[paths[0]][paths[1]]
-                original_value = original_now_application_json[paths[0]][paths[1]]             
+                original_value = original_now_application_json[paths[0]][paths[1]]
                 if current_value != original_value:
                     edited_fields[paths[0]][paths[1]] = True
             else:
