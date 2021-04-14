f18f82d680f353fbfbc2b8b92b5ecfcc52957ecf
Luke Mitchell <lukegamitchell@gmail.com>
2021-04-14 14:51:23 -0700

    Adjust response model and add missing fields

--- services/core-api/app/api/now_applications/response_models.py
+++ services/core-api/app/api/now_applications/response_models.py
@@ -555,6 +555,8 @@ NOW_APPLICATION_MODEL_EXPORT = api.model(
         'directions_to_site': fields.String,
         'work_plan': fields.String,
         'type_of_application': fields.String,
+        'is_applicant_individual_or_company': fields.String,
+        'relationship_to_applicant': fields.String,
         'proposed_annual_maximum_tonnage': fields.Fixed(decimals=2),
         'adjusted_annual_maximum_tonnage': fields.Fixed(decimals=2),
         'crown_grant_or_district_lot_numbers': fields.String,
@@ -586,8 +588,7 @@ NOW_APPLICATION_MODEL_EXPORT = api.model(
         'security_received_date': Date,
         'security_not_required': fields.Boolean,
         'security_not_required_reason': fields.String,
-        'last_updated_date': Date,
-        'last_updated_by': fields.String,
+        'last_updated_date': Date
     })
 
 NOW_VIEW_MODEL = api.model(
@@ -645,9 +646,9 @@ NOW_ACTIVITY_TYPES = api.model('ActivityType', {
 })
 
 APPLICATION_TYPE_CODE = api.model('ApplicationTypeCode', {
-        'application_type_code': fields.String,
-        'description': fields.String,
-        'active_ind': fields.Boolean
+    'application_type_code': fields.String,
+    'description': fields.String,
+    'active_ind': fields.Boolean
 })
 
 NOW_APPLICATION_TYPES = api.model(
