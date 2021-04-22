0e5ea27966a2a6ab717f8de72b5eeb659adeac29
Luke Mitchell <lukegamitchell@gmail.com>
2021-04-21 19:23:51 -0700

    Update net core to 3.1

--- services/filesystem-provider/ej2-amazon-s3-aspcore-file-provider/Startup.cs
+++ services/filesystem-provider/ej2-amazon-s3-aspcore-file-provider/Startup.cs
@@ -24,10 +24,9 @@ namespace EJ2FileManagerService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
+            services.AddMvc(options => options.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
             services.AddCors(options =>
-            {
+            {   
                 options.AddPolicy("AllowAllOrigins", builder =>
                 {
                     builder.AllowAnyOrigin()
