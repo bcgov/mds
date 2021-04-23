fedbb52f9d86b34c6d0d42112b68eeff551cafd2
Meredith Mumby <meredith@freshworks.io>
2021-04-23 12:06:41 -0700

    Added map documents to the additional document upload section on the application tab, added ability to upload documents to the application export table on the administrative tab, added the application export table to the Admin Amendment view

--- services/core-web/src/components/noticeOfWork/applications/administrative/NOWApplicationAdministrative.js
+++ services/core-web/src/components/noticeOfWork/applications/administrative/NOWApplicationAdministrative.js
@@ -61,27 +61,26 @@ export const NOWApplicationAdministrative = (props) => {
           categoriesToShow={["GDO"]}
         />
       </ScrollContentWrapper>
-      {props.noticeOfWork.application_type_code === "NOW" && (
-        <ScrollContentWrapper id="generated-documents" title="Application Export Files">
-          <NOWDocuments
-            documents={props.noticeOfWork.documents.filter(
-              ({
-                now_application_document_sub_type_code,
-                now_application_document_type_code,
-                mine_document,
-              }) =>
-                now_application_document_sub_type_code === "AEF" &&
-                (now_application_document_type_code !== "PMT" ||
-                  now_application_document_type_code !== "PMA" ||
-                  mine_document.document_name.includes("DRAFT"))
-            )}
-            isViewMode
-            disclaimerText="This table shows PDFs generated from edited Notice of Work forms and Draft Permits."
-            categoriesToShow={["AEF"]}
-            addDescriptionColumn={false}
-          />
-        </ScrollContentWrapper>
-      )}
+      <ScrollContentWrapper id="generated-documents" title="Application Export Files">
+        <NOWDocuments
+          documents={props.noticeOfWork.documents.filter(
+            ({
+              now_application_document_sub_type_code,
+              now_application_document_type_code,
+              mine_document,
+            }) =>
+              now_application_document_sub_type_code === "AEF" &&
+              (now_application_document_type_code !== "PMT" ||
+                now_application_document_type_code !== "PMA" ||
+                mine_document.document_name.includes("DRAFT"))
+          )}
+          isViewMode={false}
+          isAdminView
+          disclaimerText="This table shows PDFs generated from edited Notice of Work forms and Draft Permits."
+          categoriesToShow={["AEF"]}
+          addDescriptionColumn={false}
+        />
+      </ScrollContentWrapper>
       <ScrollContentWrapper id="inspectors" title="Inspectors">
         <AssignInspectors
           inspectors={props.inspectors}
