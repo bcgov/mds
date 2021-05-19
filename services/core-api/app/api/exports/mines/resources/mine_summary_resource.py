e5c1d5ea903e99a4e2dc20618df653bc8149518f
Luke Mitchell <lukegamitchell@gmail.com>
2021-05-19 09:27:22 -0700

    Add marshal_with to mine summary resource, tidy

--- services/core-api/app/api/exports/mines/resources/mine_summary_resource.py
+++ services/core-api/app/api/exports/mines/resources/mine_summary_resource.py
@@ -1,5 +1,5 @@
 import json
-from flask import make_response, request
+
 from flask_restplus import Resource, marshal
 from ..models.mine_summary_view import MineSummaryView
 
@@ -11,17 +11,14 @@ from app.api.constants import MINE_DETAILS_JSON, TIMEOUT_60_MINUTES
 
 class MineSummaryResource(Resource):
     @api.doc(description='Returns a subset of mine data.')
+    @api.marshal_with(MINE_SUMMARY_MODEL, envelope='mines', code=200, as_list=True)
     @requires_role_view_all
     def get(self):
         json_string = cache.get(MINE_DETAILS_JSON)
         if not json_string:
             records = MineSummaryView.query.all()
             content_dict = marshal(records, MINE_SUMMARY_MODEL)
-            json_string = json.dumps({'mines': content_dict}, separators=(',', ':'))
+            json_string = json.dumps(content_dict, separators=(',', ':'))
             cache.set(MINE_DETAILS_JSON, json_string, timeout=TIMEOUT_60_MINUTES)
 
-        response = make_response(json_string)
-        response.headers['content-type'] = 'application/json'
-        response.make_conditional(request)
-
-        return response
+        return json.loads(json_string)
