2094c7b476f5b142c4dadc2002351c8a5754d834
Meredith Mumby <meredith@freshworks.io>
2021-05-27 12:56:28 -0700

    removed length restrictions on new db strings

--- migrations/sql/V2021.05.26.16.47__update_drilling_and_blasting_activity.sql
+++ migrations/sql/V2021.05.26.16.47__update_drilling_and_blasting_activity.sql
@@ -1,6 +1,6 @@
-ALTER TABLE exploration_surface_drilling ADD COLUMN drill_program character varying(100);
+ALTER TABLE exploration_surface_drilling ADD COLUMN drill_program varchar;
 
-ALTER TABLE now_submissions.application ADD COLUMN expsurfacedrillprogam character varying(100);
+ALTER TABLE now_submissions.application ADD COLUMN expsurfacedrillprogam varchar;
 
-ALTER TABLE now_submissions.application ADD COLUMN describeexplosivetosite character varying(4000);
-ALTER TABLE blasting_operation ADD COLUMN describe_explosives_to_site character varying(4000);
+ALTER TABLE now_submissions.application ADD COLUMN describeexplosivetosite varchar;
+ALTER TABLE blasting_operation ADD COLUMN describe_explosives_to_site varchar;
