ab8811a7967c25eb20c9c35ff241b334429b613d
Luke Mitchell <lukegamitchell@gmail.com>
2021-05-27 13:30:42 -0700

    Create V2021.05.25.15.10__create_mine_work_information_table.sql

---
+++ migrations/sql/V2021.05.25.15.10__create_mine_work_information_table.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS mine_work_information (
+  mine_work_information_id integer PRIMARY KEY,
+  mine_guid uuid NOT NULL,
+  work_start_date date,
+  work_stop_date date,
+  work_comments varchar,
+  created_by varchar NOT NULL,
+  created_timestamp timestamptz NOT NULL,
+  deleted_ind boolean,
+
+  create_user varchar(60) NOT NULL,
+  create_timestamp timestamptz DEFAULT now() NOT NULL,
+  update_user varchar(60) NOT NULL,
+  update_timestamp timestamptz DEFAULT now() NOT NULL,
+
+  FOREIGN KEY (mine_guid) REFERENCES mine(mine_guid) DEFERRABLE INITIALLY DEFERRED
+);
+
+ALTER TABLE mine_work_information OWNER TO mds;
