a680da0b8e0450351fc7e8b44804c466f4686a78
mhuseinov <mekhti.huseinov.ssi@gmail.com>
2021-05-31 15:03:09 -0700

    MDS-3749: updated migrations

--- migrations/sql/V2021.05.27.10.13__relink_equipment_to_now_application.sql
+++ migrations/sql/V2021.05.27.10.13__relink_equipment_to_now_application.sql
@@ -3,21 +3,20 @@ ALTER TABLE public.activity_equipment_xref ADD column IF NOT EXISTS now_applicat
 ALTER TABLE public.activity_equipment_xref ADD CONSTRAINT equipment_now_application_id_fkey FOREIGN KEY (now_application_id) REFERENCES public.now_application(now_application_id);
 
 ALTER TABLE public.activity_equipment_xref DROP CONSTRAINT IF EXISTS equipment_assignment_pkey;
--- TODO check if I need to manually remove NOT NULL constraint for this, (expected answer no)
-
-ALTER TABLE public.activity_equipment_xref ADD PRIMARY KEY (equipment_id, now_application_id);
 
 -- populate now_application_id based on summary id
-with query as (
-	select e.equipment_id, na.now_application_id from equipment e 
-	join activity_equipment_xref aex on e.equipment_id = aex.equipment_id
-	join activity_summary as2 on as2.activity_summary_id = aex.activity_summary_id 
-	join now_application na on na.now_application_id = as2.now_application_id 
+WITH query AS (
+	SELECT e.equipment_id, na.now_application_id FROM equipment e 
+	JOIN activity_equipment_xref aex ON e.equipment_id = aex.equipment_id
+	JOIN activity_summary as2 ON as2.activity_summary_id = aex.activity_summary_id 
+	JOIN now_application na ON na.now_application_id = as2.now_application_id 
 )
-update activity_equipment_xref
-set now_application_id=query.now_application_id
-from query
+UPDATE activity_equipment_xref
+SET now_application_id=query.now_application_id
+FROM query
 WHERE activity_equipment_xref.equipment_id = query.equipment_id;
+
+ALTER TABLE public.activity_equipment_xref ADD PRIMARY KEY (equipment_id, now_application_id);
 
 CREATE TABLE NOW_Submissions.application_equipment_xref (
 	MESSAGEID integer,
