76c0924c61ab98647a41795b2b4f28593a888755
mhuseinov <mekhti.huseinov.ssi@gmail.com>
2021-06-03 14:35:56 -0700

    MDS- updated migration with schema parameter

--- migrations/sql/V2021.06.03__add_serial_to_equipmentid_in_now_submissions.sql
+++ migrations/sql/V2021.06.03__add_serial_to_equipmentid_in_now_submissions.sql
@@ -1,9 +1,11 @@
-CREATE OR REPLACE FUNCTION make_into_serial(table_name TEXT, column_name TEXT) RETURNS INTEGER AS $$
+CREATE OR REPLACE FUNCTION make_into_serial(db_schema_name TEXT, db_table_name TEXT, column_name TEXT) RETURNS INTEGER AS $$
 DECLARE
     start_with INTEGER;
     sequence_name TEXT;
+    table_name TEXT;
 BEGIN
-    sequence_name := table_name || '_' || column_name || '_seq';
+    sequence_name := db_schema_name || '.' || db_table_name || '_' || column_name || '_seq';
+    table_name := db_schema_name || '.' || db_table_name;
     EXECUTE 'SELECT coalesce(max(' || column_name || '), 0) + 1 FROM ' || table_name
             INTO start_with;
     EXECUTE 'CREATE SEQUENCE ' || sequence_name ||
@@ -15,4 +17,4 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql VOLATILE;
 
-SELECT make_into_serial('now_submissions.equipment', 'equipmentid');
+SELECT make_into_serial('now_submissions','equipment', 'equipmentid');
