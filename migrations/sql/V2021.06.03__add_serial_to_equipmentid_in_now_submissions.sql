1d2093e090749048ddfd9dba9322ea824a9a849a
mhuseinov <mekhti.huseinov.ssi@gmail.com>
2021-06-03 15:55:50 -0700

    updated ownership for seq

--- migrations/sql/V2021.06.03__add_serial_to_equipmentid_in_now_submissions.sql
+++ migrations/sql/V2021.06.03__add_serial_to_equipmentid_in_now_submissions.sql
@@ -1,18 +1,24 @@
-CREATE OR REPLACE FUNCTION make_into_serial(db_schema_name TEXT, db_table_name TEXT, column_name TEXT) RETURNS INTEGER AS $$
+CREATE OR REPLACE FUNCTION make_into_serial(db_schema_name TEXT, table_name TEXT, column_name TEXT) RETURNS INTEGER AS $$
 DECLARE
     start_with INTEGER;
     sequence_name TEXT;
-    table_name TEXT;
 BEGIN
-    sequence_name := db_schema_name || '.' || db_table_name || '_' || column_name || '_seq';
-    table_name := db_schema_name || '.' || db_table_name;
-    EXECUTE 'SELECT coalesce(max(' || column_name || '), 0) + 1 FROM ' || table_name
+    sequence_name := table_name || '_' || column_name || '_seq';
+
+    EXECUTE 'SELECT coalesce(max(' || column_name || '), 0) + 1 FROM ' || db_schema_name || '.' || table_name
             INTO start_with;
     EXECUTE 'CREATE SEQUENCE ' || sequence_name ||
             ' START WITH ' || start_with ||
-            ' OWNED BY ' || table_name || '.' || column_name;
-    EXECUTE 'ALTER TABLE ' || table_name || ' ALTER COLUMN ' || column_name ||
-            ' SET DEFAULT nextVal(''' || sequence_name || ''')';
+            'INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1';
+           
+    EXECUTE 'ALTER TABLE ' || sequence_name || ' OWNER TO mds';
+   
+    EXECUTE 'ALTER SEQUENCE ' || sequence_name || ' SET SCHEMA ' || db_schema_name;
+    
+    EXECUTE 'ALTER SEQUENCE ' || db_schema_name || '.' || sequence_name || ' OWNED BY ' || db_schema_name || '.' || table_name || '.' || column_name;
+    
+    EXECUTE 'ALTER TABLE ' || db_schema_name || '.' || table_name || ' ALTER COLUMN ' || column_name ||
+            ' SET DEFAULT nextVal(''' || db_schema_name || '.' || sequence_name || ''')';
     RETURN start_with;
 END;
 $$ LANGUAGE plpgsql VOLATILE;
