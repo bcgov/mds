54f588fc5ce02a4c7756c9a49435921898aa3eb0
mhuseinov <mekhti.huseinov.ssi@gmail.com>
2021-06-09 08:19:57 -0700

    MDS-3725: WIP application documents

--- services/core-web/src/components/noticeOfWork/applications/manageDocuments/NOWApplicationManageDocuments.js
+++ services/core-web/src/components/noticeOfWork/applications/manageDocuments/NOWApplicationManageDocuments.js
@@ -4,6 +4,7 @@ import CustomPropTypes from "@/customPropTypes";
 import FinalPermitDocuments from "@/components/noticeOfWork/applications/FinalPermitDocuments";
 import NOWDocuments from "@/components/noticeOfWork/applications//NOWDocuments";
 import ScrollContentWrapper from "@/components/noticeOfWork/applications/ScrollContentWrapper";
+import NOWSubmissionDocuments from "@/components/noticeOfWork/applications//NOWSubmissionDocuments";
 
 /**
  * @class NOWApplicationManageDocuments- contains all information relating to the Administrative work on a Notice of Work Application
@@ -23,6 +24,7 @@ const defaultProps = { importNowSubmissionDocumentsJob: {}, isViewMode: true };
 
 export const NOWApplicationManageDocuments = (props) => {
   const isNoWApplication = props.noticeOfWork.application_type_code === "NOW";
+  const applicationFilesTypes = ["AAF", "AEF", "MDO", "SDO"];
   return (
     <div>
       <ScrollContentWrapper
@@ -37,22 +39,31 @@ export const NOWApplicationManageDocuments = (props) => {
           adminView
         />
       </ScrollContentWrapper>
-
       <ScrollContentWrapper
         id="additional-application-files"
-        title="Additional Application Files"
+        title="Application Documents"
         isLoaded={props.isLoaded}
       >
-        <NOWDocuments
+        {/*  TODO should add map here from documents to submission_documents */}
+        <NOWSubmissionDocuments
+          now_application_guid={props.now_application_guid}
+          documents={props.noticeOfWork.filtered_submission_documents.concat(
+            props.noticeOfWork.documents?.filter(({ now_application_document_sub_type_code }) =>
+              applicationFilesTypes.includes(now_application_document_sub_type_code)
+            )
+          )}
+          importNowSubmissionDocumentsJob={props.importNowSubmissionDocumentsJob}
+          displayTableDescription
+        />
+        {/* <NOWDocuments
           documents={props.noticeOfWork.documents?.filter(
             ({ now_application_document_sub_type_code }) =>
-              now_application_document_sub_type_code === "AAF" ||
-              now_application_document_sub_type_code === "MDO"
+              applicationFilesTypes.includes(now_application_document_sub_type_code)
           )}
           isViewMode={!props.isViewMode}
           disclaimerText="Attach any file revisions or new files requested from the proponent here."
-          categoriesToShow={["AAF", "MDO"]}
-        />
+          categoriesToShow={applicationFilesTypes}
+        /> */}
       </ScrollContentWrapper>
       <ScrollContentWrapper
         id="government-documents"
