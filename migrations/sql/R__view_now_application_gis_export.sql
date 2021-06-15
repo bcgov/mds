997b96292a4cae19fc25e2fdc1d3d13281a0db3a
Luke Mitchell <lukegamitchell@gmail.com>
2021-06-15 14:35:04 -0700

    Update R__view_now_application_gis_export.sql

--- migrations/sql/R__view_now_application_gis_export.sql
+++ migrations/sql/R__view_now_application_gis_export.sql
@@ -9,9 +9,9 @@ CREATE TABLE IF NOT EXISTS nris.inspection_type (
    inspection_type_id varchar,
    inspection_type_code varchar
 );
--- ALTER SCHEMA nris OWNER TO mds;
-ALTER TABLE nris.inspection OWNER TO mds_test;
-ALTER TABLE nris.inspection_type OWNER TO mds_test;
+-- ALTER SCHEMA nris OWNER TO nris;
+ALTER TABLE nris.inspection OWNER TO nris;
+ALTER TABLE nris.inspection_type OWNER TO nris;
 
 DROP VIEW IF EXISTS public.now_application_gis_export_view;
 
