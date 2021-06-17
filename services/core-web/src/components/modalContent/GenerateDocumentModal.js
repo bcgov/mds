85fa68841505c4d7f4a16eed2181663d98a34d8e
justinmacaulay <justinkmacaulay@gmail.com>
2021-06-17 11:11:09 -0700

    converting to class component.

--- services/core-web/src/components/modalContent/GenerateDocumentModal.js
+++ services/core-web/src/components/modalContent/GenerateDocumentModal.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Alert } from "antd";
 import { connect } from "react-redux";
@@ -10,6 +10,7 @@ const propTypes = {
   initialValues: PropTypes.objectOf(PropTypes.any).isRequired,
   documentType: PropTypes.objectOf(PropTypes.any).isRequired,
   onSubmit: PropTypes.func.isRequired,
+  preview: PropTypes.func.isRequired,
   formSyncErrors: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired,
   signature: PropTypes.string.isRequired,
   submitFailed: PropTypes.bool.isRequired,
@@ -22,41 +23,54 @@ const defaultProps = {
   allowDocx: false,
 };
 
-export const GenerateDocumentModal = (props) => {
-  const errorsLength = Object.keys(props.formSyncErrors).length;
-  const showErrors = props.submitFailed && errorsLength > 0;
-  return (
-    <div>
-      {!props.signature && (
-        <>
-          <Alert
-            message="Signature needed to generate."
-            description="The signature for the Issuing Inspector has not been provided."
-            type="error"
-            showIcon
-          />
-          <br />
-        </>
-      )}
-      <GenerateDocumentForm
-        {...props}
-        initialValues={{ ...props.initialValues, file_type: "PDF" }}
-        disabled={!props.signature}
-      />
-      {showErrors && (
-        <div className="error center">
-          <Alert
-            message={`You have ${errorsLength} ${
-              errorsLength === 1 ? "issue" : "issues"
-            } that must be fixed before proceeding.`}
-            type="error"
-            showIcon
-          />
-        </div>
-      )}
-    </div>
-  );
-};
+export class GenerateDocumentModal extends Component {
+  state = { previewGenerating: false };
+
+  handlePreview = (type, values) => {
+    this.setState({ previewGenerating: true });
+    return this.props
+      .preview(type, values)
+      .finally(() => this.setState({ previewGenerating: false }));
+  };
+
+  render() {
+    const errorsLength = Object.keys(this.props.formSyncErrors).length;
+    const showErrors = this.props.submitFailed && errorsLength > 0;
+    return (
+      <div>
+        {!this.props.signature && (
+          <>
+            <Alert
+              message="Signature needed to generate."
+              description="The signature for the Issuing Inspector has not been provided."
+              type="error"
+              showIcon
+            />
+            <br />
+          </>
+        )}
+        <GenerateDocumentForm
+          {...this.props}
+          preview={this.handlePreview}
+          previewGenerating={this.state.previewGenerating}
+          initialValues={{ ...this.props.initialValues, file_type: "PDF" }}
+          disabled={!this.props.signature}
+        />
+        {showErrors && (
+          <div className="error center">
+            <Alert
+              message={`You have ${errorsLength} ${
+                errorsLength === 1 ? "issue" : "issues"
+              } that must be fixed before proceeding.`}
+              type="error"
+              showIcon
+            />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 
 GenerateDocumentModal.propTypes = propTypes;
 GenerateDocumentModal.defaultProps = defaultProps;
