ae6cbee3bc42aeb998cf6e607d00decaac385f0c
Luke Mitchell <lukegamitchell@gmail.com>
2021-06-25 13:52:11 -0700

    More WIP docgen

--- services/core-api/app/api/document_generation/namespace.py
+++ services/core-api/app/api/document_generation/namespace.py
@@ -1,6 +1,8 @@
 from flask_restplus import Namespace
 from app.api.document_generation.resources.now_document import NoticeOfWorkDocumentResource
+from app.api.document_generation.resources.explosives_permit_document import ExplosivesPermitDocumentResource
 
 api = Namespace('documents', description='Generate documents for business objects.')
 
-api.add_resource(NoticeOfWorkDocumentResource, '')
+api.add_resource(NoticeOfWorkDocumentResource, 'notice-of-work')
+api.add_resource(ExplosivesPermitDocumentResource, 'explosives-permit')
