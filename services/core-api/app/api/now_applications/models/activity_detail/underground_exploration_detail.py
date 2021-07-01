4b75d537794acbd7eb35e89bdf2fc997a1a43b2d
Luke Mitchell <exzizt@users.noreply.github.com>
2021-06-30 21:10:28 -0700

    Fix bug where NoW activities weren't always returned (#1782)
    * Logging with foo
    
    * WIP stuff
    
    * Tidy + remove unused imports
    
    * BUG FIX + tidy
    
    * Remove debug
    
    * Revert

--- services/core-api/app/api/now_applications/models/activity_detail/underground_exploration_detail.py
+++ services/core-api/app/api/now_applications/models/activity_detail/underground_exploration_detail.py
@@ -1,18 +1,10 @@
-from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.schema import FetchedValue
-from sqlalchemy.ext.associationproxy import association_proxy
-
-from app.api.utils.models_mixins import Base
 from app.extensions import db
-
 from app.api.now_applications.models.activity_detail.activity_detail_base import ActivityDetailBase
 
 
 class UndergroundExplorationDetail(ActivityDetailBase):
     __tablename__ = 'underground_exploration_detail'
-    __mapper_args__ = {
-        'polymorphic_identity': 'underground_exploration', ## type code
-    }
+    __mapper_args__ = {'polymorphic_identity': 'underground_exploration'}
 
     activity_detail_id = db.Column(
         db.Integer, db.ForeignKey('activity_detail.activity_detail_id'), primary_key=True)
