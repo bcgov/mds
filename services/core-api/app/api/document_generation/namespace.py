9c85206ec47cc473bde5a0b412bba2618976df75
Luke Mitchell <lukegamitchell@gmail.com>
2021-07-05 11:30:11 -0700

    Fix resource paths

--- services/core-api/app/api/document_generation/namespace.py
+++ services/core-api/app/api/document_generation/namespace.py
@@ -4,5 +4,5 @@ from app.api.document_generation.resources.explosives_permit_document import Exp
 
 api = Namespace('documents', description='Generate documents for business objects.')
 
-api.add_resource(NoticeOfWorkDocumentResource, 'notice-of-work')
-api.add_resource(ExplosivesPermitDocumentResource, 'explosives-permit')
+api.add_resource(NoticeOfWorkDocumentResource, '/notice-of-work')
+api.add_resource(ExplosivesPermitDocumentResource, '/explosives-permit')
