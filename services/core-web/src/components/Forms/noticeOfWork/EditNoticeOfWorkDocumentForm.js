d586ef07b9e93e7e5d7762f057901004767f330d
JustinMacaulay <justinkmacaulay@gmail.com>
2021-07-09 13:30:53 -0700

    mds 3725 New Document management tab (#1779)
    * MDS-3725: added endpoint to update document details
    
    * MDS-3725: WIP UI for Manage documents
    
    * MDS-3275: tests
    
    * MDS-3725: WIP added referral, public comment, consultation documents
    
    * MDS-3725: WIP application documents
    
    * MDS-3725: mapping for application documents
    
    * MDS-3725: final application package
    
    * MDS-3725: fix for navigation, hide part of permit column for final app package
    
    * document changes on the manage documents table
    
    * new doc changes
    
    * styling and functionality for packages
    
    * test updates
    
    * missed snapshot change from dev merge
    
    * updated badge
    
    * updated logic for showing referral/consultation pencils if not in progress
    
    * updated the snapshots
    
    * Descriptions and some small tweaks.
    
    * updated linting
    
    * updated the administrative amendment view to not include submission documents
    
    * updated snapshot tests
    
    * added preamble_date
    
    * missed the dissable buttons in now_documents
    
    * new edit functionality
    
    * PR comments
    
    * added new snapshot tests
    
    * common folder update
    
    Co-authored-by: mhuseinov <mekhti.huseinov.ssi@gmail.com>
    Co-authored-by: Meredith Mumby <meredith@freshworks.io>

--- services/core-web/src/components/Forms/noticeOfWork/EditNoticeOfWorkDocumentForm.js
+++ services/core-web/src/components/Forms/noticeOfWork/EditNoticeOfWorkDocumentForm.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { compose } from "redux";
 import PropTypes from "prop-types";
-import { Field, reduxForm } from "redux-form";
+import { Field, reduxForm, formValueSelector } from "redux-form";
 import { Form } from "@ant-design/compatible";
 import "@ant-design/compatible/assets/index.css";
 import { Button, Col, Row, Popconfirm } from "antd";
@@ -22,15 +22,19 @@ const propTypes = {
   title: PropTypes.string.isRequired,
   now_application_guid: PropTypes.string.isRequired,
   submitting: PropTypes.bool.isRequired,
+  isEditMode: PropTypes.bool.isRequired,
+  isFinalAppPackage: PropTypes.bool.isRequired,
   dropdownNoticeOfWorkApplicationDocumentTypeOptions: PropTypes.arrayOf(
     CustomPropTypes.dropdownListItem
   ).isRequired,
   change: PropTypes.func.isRequired,
+  isInCompleteStatus: PropTypes.Bool,
   categoriesToShow: PropTypes.arrayOf(PropTypes.string),
 };
 
 const defaultProps = {
   categoriesToShow: [],
+  isInCompleteStatus: false,
 };
 
 export class EditNoticeOfWorkDocumentForm extends Component {
@@ -80,22 +84,24 @@ export class EditNoticeOfWorkDocumentForm extends Component {
       <Form layout="vertical" onSubmit={this.props.handleSubmit}>
         <Row gutter={16}>
           <Col span={24}>
-            <Form.Item>
-              <Field
-                id="now_application_document_type_code"
-                name="now_application_document_type_code"
-                label="Document Category*"
-                placeholder="Select a document type"
-                component={renderConfig.SELECT}
-                data={filteredDropDownOptions}
-                validate={[
-                  required,
-                  validateSelectOptions(
-                    this.props.dropdownNoticeOfWorkApplicationDocumentTypeOptions
-                  ),
-                ]}
-              />
-            </Form.Item>
+            {!this.props.isEditMode && (
+              <Form.Item>
+                <Field
+                  id="now_application_document_type_code"
+                  name="now_application_document_type_code"
+                  label="Document Category*"
+                  placeholder="Select a document type"
+                  component={renderConfig.SELECT}
+                  data={filteredDropDownOptions}
+                  validate={[
+                    required,
+                    validateSelectOptions(
+                      this.props.dropdownNoticeOfWorkApplicationDocumentTypeOptions
+                    ),
+                  ]}
+                />
+              </Form.Item>
+            )}
             <Form.Item>
               <Field
                 id="description"
@@ -105,37 +111,84 @@ export class EditNoticeOfWorkDocumentForm extends Component {
                 validate={[maxLength(280)]}
               />
             </Form.Item>
-            <Form.Item>
-              <Field
-                id="is_final_package"
-                name="is_final_package"
-                label="Part of final application package"
-                type="checkbox"
-                component={renderConfig.CHECKBOX}
-              />
-            </Form.Item>
-            <h5>Document Upload*</h5>
-            <p className="p-light">
-              All files uploaded will be classified using the selected Category. To upload other
-              file types, re-open this form after submitting the current files.
-            </p>
-            <br />
-            <Form.Item>
+            {!this.props.isInCompleteStatus && (
               <Form.Item>
                 <Field
-                  id="notice_of_work_file_upload"
-                  name="notice_of_work_file_upload"
-                  onFileLoad={this.onFileLoad}
-                  onRemoveFile={this.onRemoveFile}
-                  component={FileUpload}
-                  addFileStart={() => this.toggleDisabled(true)}
-                  uploadUrl={NOTICE_OF_WORK_DOCUMENT(this.props.now_application_guid)}
-                  acceptedFileTypesMap={{ ...DOCUMENT, ...EXCEL, ...IMAGE, ...SPATIAL }}
-                  allowMultiple
-                  allowRevert
+                  id="is_final_package"
+                  name="is_final_package"
+                  label="Part of permit package"
+                  type="checkbox"
+                  component={renderConfig.CHECKBOX}
                 />
               </Form.Item>
-            </Form.Item>
+            )}
+            {this.props.isFinalAppPackage && (
+              <>
+                <Row gutter={16}>
+                  <Col md={12} xs={24}>
+                    <Form.Item>
+                      <Field
+                        id="preamble_title"
+                        name="preamble_title"
+                        label="Title"
+                        placeholder="Title"
+                        component={renderConfig.FIELD}
+                      />
+                    </Form.Item>
+                  </Col>
+                  <Col md={12} xs={24}>
+                    <Form.Item>
+                      <Field
+                        id="preamble_author"
+                        name="preamble_author"
+                        label="Author"
+                        placeholder="Author"
+                        component={renderConfig.FIELD}
+                      />
+                    </Form.Item>
+                  </Col>
+                </Row>
+                <Row gutter={16}>
+                  <Col md={12} xs={24}>
+                    <Form.Item>
+                      <Field
+                        id="preamble_date"
+                        name="preamble_date"
+                        label="Date"
+                        placeholder="Date"
+                        component={renderConfig.DATE}
+                      />
+                    </Form.Item>
+                  </Col>
+                </Row>
+              </>
+            )}
+            {!this.props.isEditMode && (
+              <>
+                <h5>Document Upload*</h5>
+                <p className="p-light">
+                  All files uploaded will be classified using the selected Category. To upload other
+                  file types, re-open this form after submitting the current files.
+                </p>
+                <br />
+                <Form.Item>
+                  <Form.Item>
+                    <Field
+                      id="notice_of_work_file_upload"
+                      name="notice_of_work_file_upload"
+                      onFileLoad={this.onFileLoad}
+                      onRemoveFile={this.onRemoveFile}
+                      component={FileUpload}
+                      addFileStart={() => this.toggleDisabled(true)}
+                      uploadUrl={NOTICE_OF_WORK_DOCUMENT(this.props.now_application_guid)}
+                      acceptedFileTypesMap={{ ...DOCUMENT, ...EXCEL, ...IMAGE, ...SPATIAL }}
+                      allowMultiple
+                      allowRevert
+                    />
+                  </Form.Item>
+                </Form.Item>
+              </>
+            )}
           </Col>
         </Row>
         <div className="right center-mobile">
@@ -156,7 +209,9 @@ export class EditNoticeOfWorkDocumentForm extends Component {
             htmlType="submit"
             loading={this.props.submitting}
             disabled={
-              this.state.uploadedFiles.length === 0 || this.props.submitting || this.state.disabled
+              (this.state.uploadedFiles.length === 0 && !this.props.isEditMode) ||
+              this.props.submitting ||
+              this.state.disabled
             }
           >
             {this.props.title}
@@ -170,12 +225,17 @@ export class EditNoticeOfWorkDocumentForm extends Component {
 EditNoticeOfWorkDocumentForm.propTypes = propTypes;
 EditNoticeOfWorkDocumentForm.defaultProps = defaultProps;
 
+const selector = formValueSelector(FORM.EDIT_NOTICE_OF_WORK_DOCUMENT_FORM);
+
+const mapStateToProps = (state) => ({
+  isFinalAppPackage: selector(state, "is_final_package"),
+  dropdownNoticeOfWorkApplicationDocumentTypeOptions: getDropdownNoticeOfWorkApplicationDocumentTypeOptions(
+    state
+  ),
+});
+
 export default compose(
-  connect((state) => ({
-    dropdownNoticeOfWorkApplicationDocumentTypeOptions: getDropdownNoticeOfWorkApplicationDocumentTypeOptions(
-      state
-    ),
-  })),
+  connect(mapStateToProps),
   reduxForm({
     form: FORM.EDIT_NOTICE_OF_WORK_DOCUMENT_FORM,
     touchOnBlur: false,
---
+++ services/core-web/src/components/noticeOfWork/applications/PermitPackage.js
@@ -0,0 +1,277 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { Button, notification } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
+
+import { openModal, closeModal } from "@common/actions/modalActions";
+import { getDocumentDownloadToken } from "@common/utils/actionlessNetworkCalls";
+import { modalConfig } from "@/components/modalContent/config";
+import CustomPropTypes from "@/customPropTypes";
+import {
+  setNoticeOfWorkApplicationDocumentDownloadState,
+  updateNoticeOfWorkApplication,
+  fetchImportedNoticeOfWorkApplication,
+} from "@common/actionCreators/noticeOfWorkActionCreator";
+import {
+  getNoticeOfWorkReviews,
+  getNoticeOfWork,
+  getNOWProgress,
+} from "@common/selectors/noticeOfWorkSelectors";
+import { EDIT_OUTLINE_VIOLET, EDIT_OUTLINE } from "@/constants/assets";
+import * as Permission from "@/constants/permissions";
+import { getDropdownNoticeOfWorkApplicationReviewTypeOptions } from "@common/selectors/staticContentSelectors";
+import NOWActionWrapper from "@/components/noticeOfWork/NOWActionWrapper";
+
+/**
+ * @constant PermitPackage renders edit/view for the Permit Package review step
+ */
+
+const propTypes = {
+  noticeOfWork: CustomPropTypes.importedNOWApplication.isRequired,
+  importNowSubmissionDocumentsJob: PropTypes.objectOf(PropTypes.any),
+  updateNoticeOfWorkApplication: PropTypes.func.isRequired,
+  fetchImportedNoticeOfWorkApplication: PropTypes.func.isRequired,
+  closeModal: PropTypes.func.isRequired,
+  openModal: PropTypes.func.isRequired,
+  setNoticeOfWorkApplicationDocumentDownloadState: PropTypes.func.isRequired,
+  isAdminView: PropTypes.bool,
+  isTableHeaderView: PropTypes.bool,
+};
+
+const defaultProps = {
+  isTableHeaderView: false,
+  isAdminView: false,
+  importNowSubmissionDocumentsJob: {},
+};
+
+export class PermitPackage extends Component {
+  state = {
+    cancelDownload: false,
+  };
+
+  createFinalDocumentPackage = (selectedCoreRows, selectedSubmissionRows) => {
+    const documentsPayload = this.props.noticeOfWork.documents.map((document) => {
+      document.is_final_package = selectedCoreRows.includes(
+        document.now_application_document_xref_guid
+      );
+      return document;
+    });
+
+    const submissionDocumentsPayload = this.props.noticeOfWork.filtered_submission_documents.map(
+      (document) => {
+        document.is_final_package = selectedSubmissionRows.includes(document.mine_document_guid);
+        return document;
+      }
+    );
+
+    const payload = {
+      ...this.props.noticeOfWork,
+      documents: documentsPayload,
+      submission_documents: submissionDocumentsPayload,
+    };
+
+    const message = "Successfully updated the final application package.";
+
+    this.props
+      .updateNoticeOfWorkApplication(payload, this.props.noticeOfWork.now_application_guid, message)
+      .then(() => {
+        this.props
+          .fetchImportedNoticeOfWorkApplication(this.props.noticeOfWork.now_application_guid)
+          .then(() => {
+            this.props.closeModal();
+          });
+      });
+  };
+
+  cancelDownload = () => {
+    this.setState({ cancelDownload: true });
+  };
+
+  downloadDocument = (url) => {
+    const a = document.createElement("a");
+    a.href = url.url;
+    a.download = url.filename;
+    a.style.display = "none";
+    document.body.append(a);
+    a.click();
+    a.remove();
+  };
+
+  waitFor = (conditionFunction) => {
+    const poll = (resolve) => {
+      if (conditionFunction()) resolve();
+      else setTimeout(() => poll(resolve), 400);
+    };
+
+    return new Promise(poll);
+  };
+
+  downloadDocumentPackage = () => {
+    const docURLS = [];
+
+    const submissionDocs = this.props.noticeOfWork.filtered_submission_documents
+      .filter(({ is_final_package }) => is_final_package)
+      .map((doc) => ({
+        key: doc.mine_document_guid,
+        documentManagerGuid: doc.document_manager_guid,
+        filename: doc.filename,
+      }));
+
+    const coreDocs = this.props.noticeOfWork.documents
+      .filter(({ is_final_package }) => is_final_package)
+      .map((doc) => ({
+        key: doc.now_application_document_xref_guid,
+        documentManagerGuid: doc.mine_document.document_manager_guid,
+        filename: doc.mine_document.document_name,
+      }));
+
+    const totalFiles = submissionDocs.length + coreDocs.length;
+    if (totalFiles === 0) {
+      return;
+    }
+
+    submissionDocs.forEach((doc) =>
+      getDocumentDownloadToken(doc.documentManagerGuid, doc.filename, docURLS)
+    );
+
+    coreDocs.forEach((doc) =>
+      getDocumentDownloadToken(doc.documentManagerGuid, doc.filename, docURLS)
+    );
+
+    let currentFile = 0;
+    this.waitFor(() => docURLS.length === submissionDocs.length + coreDocs.length).then(
+      async () => {
+        // eslint-disable-next-line no-restricted-syntax
+        for (const url of docURLS) {
+          if (this.state.cancelDownload) {
+            this.setState({ cancelDownload: false });
+            this.props.setNoticeOfWorkApplicationDocumentDownloadState({
+              downloading: false,
+              currentFile: 0,
+              totalFiles: 1,
+            });
+            notification.success({
+              message: "Cancelled file downloads.",
+              duration: 10,
+            });
+            return;
+          }
+          currentFile += 1;
+          this.props.setNoticeOfWorkApplicationDocumentDownloadState({
+            downloading: true,
+            currentFile,
+            totalFiles,
+          });
+          this.downloadDocument(url);
+          // eslint-disable-next-line
+          await new Promise((resolve) => setTimeout(resolve, 2000));
+        }
+        notification.success({
+          message: `Successfully Downloaded: ${totalFiles} files.`,
+          duration: 10,
+        });
+
+        this.props.setNoticeOfWorkApplicationDocumentDownloadState({
+          downloading: false,
+          currentFile: 1,
+          totalFiles: 1,
+        });
+      }
+    );
+  };
+
+  openFinalDocumentPackageModal = (event) => {
+    event.preventDefault();
+
+    const isNoWApplication = this.props.noticeOfWork.application_type_code === "NOW";
+
+    const finalDocuments = this.props.noticeOfWork.documents
+      .filter(({ is_final_package }) => is_final_package)
+      .map(({ now_application_document_xref_guid }) => now_application_document_xref_guid);
+
+    const finalSubmissionDocuments = this.props.noticeOfWork.filtered_submission_documents
+      .filter(({ is_final_package }) => is_final_package)
+      .map(({ mine_document_guid }) => mine_document_guid);
+
+    this.props.openModal({
+      props: {
+        mineGuid: this.props.noticeOfWork.mine_guid,
+        noticeOfWorkGuid: this.props.noticeOfWork.now_application_guid,
+        noticeOfWork: this.props.noticeOfWork,
+        importNowSubmissionDocumentsJob: this.props.importNowSubmissionDocumentsJob,
+        submissionDocuments: this.props.noticeOfWork.filtered_submission_documents,
+        documents: this.props.noticeOfWork.documents,
+        finalDocuments,
+        finalSubmissionDocuments,
+        onSubmit: this.createFinalDocumentPackage,
+        title: "Create Final Application Package",
+        isNoWApplication,
+      },
+      content: modalConfig.EDIT_FINAL_PERMIT_DOC_PACKAGE,
+      width: "75vw",
+    });
+  };
+
+  render() {
+    return this.props.isTableHeaderView ? (
+      <NOWActionWrapper
+        permission={Permission.EDIT_PERMITS}
+        tab={this.props.isAdminView ? undefined : "REV"}
+      >
+        <Button ghost type="primary" size="small" onClick={this.openFinalDocumentPackageModal}>
+          <img name="remove" src={EDIT_OUTLINE_VIOLET} alt="Edit document" />
+        </Button>
+      </NOWActionWrapper>
+    ) : (
+      <div>
+        <Button
+          type="secondary"
+          className="full-mobile"
+          onClick={() => this.downloadDocumentPackage()}
+        >
+          <DownloadOutlined className="padding-sm--right icon-sm" />
+          Download All
+        </Button>
+        <NOWActionWrapper
+          permission={Permission.EDIT_PERMITS}
+          tab={this.props.isAdminView ? undefined : "REV"}
+        >
+          <Button
+            type="secondary"
+            className="full-mobile"
+            onClick={this.openFinalDocumentPackageModal}
+          >
+            <img src={EDIT_OUTLINE} title="Edit" alt="Edit" className="padding-md--right" />
+            Edit
+          </Button>
+        </NOWActionWrapper>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  noticeOfWork: getNoticeOfWork(state),
+  noticeOfWorkReviews: getNoticeOfWorkReviews(state),
+  noticeOfWorkReviewTypes: getDropdownNoticeOfWorkApplicationReviewTypeOptions(state),
+  progress: getNOWProgress(state),
+});
+
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators(
+    {
+      openModal,
+      closeModal,
+      setNoticeOfWorkApplicationDocumentDownloadState,
+      updateNoticeOfWorkApplication,
+      fetchImportedNoticeOfWorkApplication,
+    },
+    dispatch
+  );
+
+PermitPackage.propTypes = propTypes;
+PermitPackage.defaultProps = defaultProps;
+
+export default connect(mapStateToProps, mapDispatchToProps)(PermitPackage);
