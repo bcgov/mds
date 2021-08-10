261f4b41cc5cfe63ef11d2187b5718a22fa04a7b
areyeslo <areyeslo@users.noreply.github.com>
2021-08-09 17:01:35 -0700

    Sequence in standard permit condition table.

--- migrations/sql/V2021.08.04.16.04__create_sequence_standard_permit_condition.sql
+++ migrations/sql/V2021.08.04.16.04__create_sequence_standard_permit_condition.sql
@@ -1,15 +1,16 @@
-CREATE SEQUENCE IF NOT EXISTS public.permit_conditions_standard_permit_condition_id_seq
-    INCREMENT 1
-    START 900
-    MINVALUE 900
-    NO MAXVALUE
-    CACHE 1;
-
-ALTER SEQUENCE public.permit_conditions_standard_permit_condition_id_seq
-    OWNER TO mds;
-
-GRANT ALL ON SEQUENCE public.permit_conditions_standard_permit_condition_id_seq TO mds;
-
-SELECT setval('permit_conditions_standard_permit_condition_id_seq', (SELECT MAX(standard_permit_condition_id) FROM standard_permit_conditions));
-
-ALTER TABLE ONLY public.standard_permit_conditions ALTER COLUMN standard_permit_condition_id SET DEFAULT nextval('public.permit_conditions_standard_permit_condition_id_seq'::regclass);
+DO
+$$
+BEGIN
+   EXECUTE format('
+   CREATE SEQUENCE IF NOT EXISTS public.permit_conditions_standard_permit_condition_id_seq
+   INCREMENT 1
+   START %1$s
+   MINVALUE %1$s
+   NO MAXVALUE
+   CACHE 1;'
+ , (SELECT COALESCE ((SELECT MAX(standard_permit_condition_id) FROM standard_permit_conditions),0) +1));
+   ALTER SEQUENCE public.permit_conditions_standard_permit_condition_id_seq OWNER TO mds;
+   GRANT ALL ON SEQUENCE public.permit_conditions_standard_permit_condition_id_seq TO mds;
+   ALTER TABLE ONLY public.standard_permit_conditions ALTER COLUMN standard_permit_condition_id SET DEFAULT nextval('public.permit_conditions_standard_permit_condition_id_seq'::regclass);
+END
+$$;
