675f061e45d4abc4fd4f7b2ef51d2193dada7fb1
Meredith Mumby <meredith@freshworks.io>
2021-08-11 11:00:49 -0700

    updated migration

--- migrations/sql/V2021.08.04.16.04__create_sequence_standard_permit_condition.sql
+++ migrations/sql/V2021.08.04.16.04__create_sequence_standard_permit_condition.sql
@@ -2,13 +2,14 @@ DO
 $$
 BEGIN
    EXECUTE format('
+   DROP SEQUENCE IF EXISTS public.permit_conditions_standard_permit_condition_id_seq CASCADE;
    CREATE SEQUENCE IF NOT EXISTS public.permit_conditions_standard_permit_condition_id_seq
    INCREMENT 1
    START %1$s
    MINVALUE %1$s
    NO MAXVALUE
    CACHE 1;'
- , (SELECT COALESCE ((SELECT MAX(standard_permit_condition_id) FROM standard_permit_conditions),0) +1));
+ , (SELECT COALESCE ((SELECT MAX(standard_permit_condition_id) FROM standard_permit_conditions),0)));
    ALTER SEQUENCE public.permit_conditions_standard_permit_condition_id_seq OWNER TO mds;
    GRANT ALL ON SEQUENCE public.permit_conditions_standard_permit_condition_id_seq TO mds;
    ALTER TABLE ONLY public.standard_permit_conditions ALTER COLUMN standard_permit_condition_id SET DEFAULT nextval('public.permit_conditions_standard_permit_condition_id_seq'::regclass);
