8d3ca040c5598282c1325581b053e5ce2da7fa51
justinmacaulay <justinkmacaulay@gmail.com>
2021-08-24 15:25:56 -0700

    adding in a loading wrapper

--- services/minespace-web/src/components/dashboard/mine/overview/MineWorkInformation.js
+++ services/minespace-web/src/components/dashboard/mine/overview/MineWorkInformation.js
@@ -16,6 +16,7 @@ import { EDIT_PENCIL } from "@/constants/assets";
 import AddMineWorkInformationForm from "@/components/Forms/AddMineWorkInformationForm";
 import { getWorkInformationBadgeStatusType } from "@/constants/theme";
 import { formatDate } from "@/utils/helpers";
+import LoadingWrapper from "@/components/common/wrappers/LoadingWrapper";
 
 const propTypes = {
   mineGuid: PropTypes.string.isRequired,
@@ -30,7 +31,7 @@ const defaultProps = {};
 const { Paragraph, Title } = Typography;
 
 export class MineWorkInformation extends Component {
-  state = { isEditMode: false };
+  state = { isEditMode: false, isLoaded: true };
 
   submitAddEditMineWorkInformationForm = (mineWorkInformationGuid) => (values) => {
     const action = mineWorkInformationGuid
@@ -44,7 +45,12 @@ export class MineWorkInformation extends Component {
 
   editWorkInformation = () => this.setState((prevState) => ({ isEditMode: !prevState.isEditMode }));
 
-  componentDidMount = () => this.props.fetchMineWorkInformations(this.props.mineGuid);
+  componentDidMount = () => {
+    this.setState({ isLoaded: false });
+    this.props
+      .fetchMineWorkInformations(this.props.mineGuid)
+      .then(this.setState({ isLoaded: true }));
+  };
 
   render() {
     const info = this.props.mineWorkInformations[0];
@@ -73,127 +79,130 @@ export class MineWorkInformation extends Component {
         <Row gutter={16}>
           <Col span={24}>
             {!isEmpty(this.props.mineWorkInformations) || this.state.isEditMode ? (
-              <div>
-                <Row>
-                  {this.state.isEditMode ? (
-                    <AddMineWorkInformationForm
-                      initialValues={info}
-                      title={title}
-                      mineWorkInformationGuid={info?.mine_work_information_guid}
-                      onSubmit={this.submitAddEditMineWorkInformationForm(
-                        info?.mine_work_information_guid
-                      )}
-                      cancelEdit={this.editWorkInformation}
-                      isEditMode={this.state.isEditMode}
-                    />
-                  ) : (
-                    <div>
-                      <Row gutter={16}>
-                        <Col span={22}>
-                          <Descriptions column={5} colon={false}>
-                            <Descriptions.Item
-                              label={
-                                <>
-                                  <Tooltip
-                                    overlayClassName="minespace-tooltip"
-                                    title={
-                                      <>
-                                        <Typography.Text strong>
-                                          Health, Safety and Reclamation Code for Mines in British
-                                          Columbia
-                                        </Typography.Text>
-                                        <br />
-                                        <Typography.Text underline>
-                                          Notice To Start Work
-                                        </Typography.Text>
-                                        <br />
-                                        <Typography.Text>
-                                          6.2.1 The manager shall give 10 days’ notice to an
-                                          inspector of intention to start [any mining activity] in,
-                                          at, or about a mine, including seasonal reactivation.
-                                        </Typography.Text>
-                                      </>
-                                    }
-                                    placement="right"
-                                    mouseEnterDelay={0.3}
-                                  >
-                                    <Icon type="info-circle" className="padding-sm" />
-                                  </Tooltip>
-                                  Work Start Date
-                                </>
-                              }
-                            >
-                              {formatDate(info.work_start_date)}
-                            </Descriptions.Item>
-                            <Descriptions.Item
-                              label={
-                                <>
-                                  <Tooltip
-                                    overlayClassName="minespace-tooltip"
-                                    title={
-                                      <>
-                                        <Typography.Text strong>
-                                          Health, Safety and Reclamation Code for Mines in British
-                                          Columbia
-                                        </Typography.Text>
-                                        <br />
-                                        <Typography.Text underline>
-                                          Notice to Stop Work
-                                        </Typography.Text>
-                                        <br />
-                                        <Typography.Text>
-                                          6.2.2 The manager shall give notice to an inspector of
-                                          intention to stop [any mining activity] in, at, or about a
-                                          mine, permanently, indefinitely, or for a definite period
-                                          exceeding 30 days, and except in an emergency, the notice
-                                          shall be not less than seven days.
-                                        </Typography.Text>
-                                      </>
-                                    }
-                                    placement="right"
-                                    mouseEnterDelay={0.3}
-                                  >
-                                    <Icon type="info-circle" className="padding-sm" />
-                                  </Tooltip>
-                                  Work Stop Date
-                                </>
-                              }
-                            >
-                              {formatDate(info.work_stop_date)}
-                            </Descriptions.Item>
-                            <Descriptions.Item label="Work Status">
-                              <Badge
-                                status={getWorkInformationBadgeStatusType(info.work_status)}
-                                text={info.work_status}
-                              />
-                            </Descriptions.Item>
-                          </Descriptions>
-                        </Col>
-                      </Row>
-                      <Row gutter={16}>
-                        <Col span={24} />
-                      </Row>
-                    </div>
-                  )}
-                </Row>
-                <Row>
-                  {info && (
-                    <Col span={22}>
-                      {/* NOTE: Ant Design has no easy way to right-align "Descriptions" so plain HTML tags are used here. */}
-                      <span className="inline-flex between" style={{ float: "right" }}>
-                        <div className="padding-sm">
-                          <span className="field-title">Updated By: </span>
-                          <span>{info.updated_by}</span>
-                        </div>
-                        <div className="padding-sm">
-                          <span className="field-title">Last Updated: </span>
-                          <span>{formatDateTime(info.updated_timestamp)}</span>
-                        </div>
-                      </span>
-                    </Col>
-                  )}
-                </Row>
-              </div>
+              <LoadingWrapper isLoaded={this.state.isLoaded}>
+                <div>
+                  <Row>
+                    {this.state.isEditMode ? (
+                      <AddMineWorkInformationForm
+                        initialValues={info}
+                        title={title}
+                        mineWorkInformationGuid={info?.mine_work_information_guid}
+                        onSubmit={this.submitAddEditMineWorkInformationForm(
+                          info?.mine_work_information_guid
+                        )}
+                        cancelEdit={this.editWorkInformation}
+                        isEditMode={this.state.isEditMode}
+                      />
+                    ) : (
+                      <div>
+                        <Row gutter={16}>
+                          <Col span={22}>
+                            <Descriptions column={5} colon={false}>
+                              <Descriptions.Item
+                                label={
+                                  <>
+                                    <Tooltip
+                                      overlayClassName="minespace-tooltip"
+                                      title={
+                                        <>
+                                          <Typography.Text strong>
+                                            Health, Safety and Reclamation Code for Mines in British
+                                            Columbia
+                                          </Typography.Text>
+                                          <br />
+                                          <Typography.Text underline>
+                                            Notice To Start Work
+                                          </Typography.Text>
+                                          <br />
+                                          <Typography.Text>
+                                            6.2.1 The manager shall give 10 days’ notice to an
+                                            inspector of intention to start [any mining activity]
+                                            in, at, or about a mine, including seasonal
+                                            reactivation.
+                                          </Typography.Text>
+                                        </>
+                                      }
+                                      placement="right"
+                                      mouseEnterDelay={0.3}
+                                    >
+                                      <Icon type="info-circle" className="padding-sm" />
+                                    </Tooltip>
+                                    Work Start Date
+                                  </>
+                                }
+                              >
+                                {formatDate(info.work_start_date)}
+                              </Descriptions.Item>
+                              <Descriptions.Item
+                                label={
+                                  <>
+                                    <Tooltip
+                                      overlayClassName="minespace-tooltip"
+                                      title={
+                                        <>
+                                          <Typography.Text strong>
+                                            Health, Safety and Reclamation Code for Mines in British
+                                            Columbia
+                                          </Typography.Text>
+                                          <br />
+                                          <Typography.Text underline>
+                                            Notice to Stop Work
+                                          </Typography.Text>
+                                          <br />
+                                          <Typography.Text>
+                                            6.2.2 The manager shall give notice to an inspector of
+                                            intention to stop [any mining activity] in, at, or about
+                                            a mine, permanently, indefinitely, or for a definite
+                                            period exceeding 30 days, and except in an emergency,
+                                            the notice shall be not less than seven days.
+                                          </Typography.Text>
+                                        </>
+                                      }
+                                      placement="right"
+                                      mouseEnterDelay={0.3}
+                                    >
+                                      <Icon type="info-circle" className="padding-sm" />
+                                    </Tooltip>
+                                    Work Stop Date
+                                  </>
+                                }
+                              >
+                                {formatDate(info.work_stop_date)}
+                              </Descriptions.Item>
+                              <Descriptions.Item label="Work Status">
+                                <Badge
+                                  status={getWorkInformationBadgeStatusType(info.work_status)}
+                                  text={info.work_status}
+                                />
+                              </Descriptions.Item>
+                            </Descriptions>
+                          </Col>
+                        </Row>
+                        <Row gutter={16}>
+                          <Col span={24} />
+                        </Row>
+                      </div>
+                    )}
+                  </Row>
+                  <Row>
+                    {info && (
+                      <Col span={22}>
+                        {/* NOTE: Ant Design has no easy way to right-align "Descriptions" so plain HTML tags are used here. */}
+                        <span className="inline-flex between" style={{ float: "right" }}>
+                          <div className="padding-sm">
+                            <span className="field-title">Updated By: </span>
+                            <span>{info.updated_by}</span>
+                          </div>
+                          <div className="padding-sm">
+                            <span className="field-title">Last Updated: </span>
+                            <span>{formatDateTime(info.updated_timestamp)}</span>
+                          </div>
+                        </span>
+                      </Col>
+                    )}
+                  </Row>
+                </div>
+              </LoadingWrapper>
             ) : (
               <>
                 <br />
