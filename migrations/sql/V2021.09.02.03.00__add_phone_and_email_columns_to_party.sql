13a40fe5458faf52188cd54c7649677112f869ec
Dragan Colic <cola_584@yahoo.com>
2021-09-08 11:18:09 -0700

    adding if exists for add column

--- migrations/sql/V2021.09.02.03.00__add_phone_and_email_columns_to_party.sql
+++ migrations/sql/V2021.09.02.03.00__add_phone_and_email_columns_to_party.sql
@@ -1,10 +1,10 @@
 
-ALTER TABLE party ADD COLUMN phone_no_sec character varying(12) NULL;
+ALTER TABLE party ADD COLUMN phone_no_sec IF NOT EXISTS character varying(12) NULL;
 
-ALTER TABLE party ADD COLUMN phone_sec_ext character varying(4) NULL;
+ALTER TABLE party ADD COLUMN phone_sec_ext IF NOT EXISTS character varying(4) NULL;
 
-ALTER TABLE party ADD COLUMN phone_no_ter character varying(12) NULL;
+ALTER TABLE party ADD COLUMN phone_no_ter IF NOT EXISTS character varying(12) NULL;
 
-ALTER TABLE party ADD COLUMN phone_ter_ext character varying(4) NULL;
+ALTER TABLE party ADD COLUMN phone_ter_ext IF NOT EXISTS character varying(4) NULL;
 
-ALTER TABLE party ADD COLUMN email_sec character varying(254) NULL;
+ALTER TABLE party ADD COLUMN email_sec IF NOT EXISTS character varying(254) NULL;
