9f307c5330a7336c7e6c3a82160d788a678028d1
Dragan Colic <cola_584@yahoo.com>
2021-09-08 13:42:57 -0700

    fixing the sql and tests

--- migrations/sql/V2021.09.02.03.00__add_phone_and_email_columns_to_party.sql
+++ migrations/sql/V2021.09.02.03.00__add_phone_and_email_columns_to_party.sql
@@ -1,10 +1,9 @@
+ALTER TABLE party ADD COLUMN IF NOT EXISTS phone_no_sec varchar NULL;
 
-ALTER TABLE party ADD COLUMN IF NOT EXISTS phone_no_sec::varchar NULL;
+ALTER TABLE party ADD COLUMN IF NOT EXISTS phone_sec_ext varchar NULL;
 
-ALTER TABLE party ADD COLUMN IF NOT EXISTS phone_sec_ext::varchar NULL;
+ALTER TABLE party ADD COLUMN IF NOT EXISTS phone_no_ter varchar NULL;
 
-ALTER TABLE party ADD COLUMN IF NOT EXISTS phone_no_ter::varchar NULL;
+ALTER TABLE party ADD COLUMN IF NOT EXISTS phone_ter_ext varchar NULL;
 
-ALTER TABLE party ADD COLUMN IF NOT EXISTS phone_ter_ext::varchar NULL;
-
-ALTER TABLE party ADD COLUMN IF NOT EXISTS email_sec::varchar NULL;
+ALTER TABLE party ADD COLUMN IF NOT EXISTS email_sec varchar NULL;
