91412f610dfba07003ae598ea5eb986adf424521
Dragan Colic <cola_584@yahoo.com>
2021-09-08 13:26:31 -0700

    fixing the varchar

--- migrations/sql/V2021.09.02.03.00__add_phone_and_email_columns_to_party.sql
+++ migrations/sql/V2021.09.02.03.00__add_phone_and_email_columns_to_party.sql
@@ -1,10 +1,10 @@
 
-ALTER TABLE party ADD COLUMN IF NOT EXISTS phone_no_sec character varying(12) NULL;
+ALTER TABLE party ADD COLUMN IF NOT EXISTS phone_no_sec::varchar NULL;
 
-ALTER TABLE party ADD COLUMN IF NOT EXISTS phone_sec_ext character varying(4) NULL;
+ALTER TABLE party ADD COLUMN IF NOT EXISTS phone_sec_ext::varchar NULL;
 
-ALTER TABLE party ADD COLUMN IF NOT EXISTS phone_no_ter character varying(12) NULL;
+ALTER TABLE party ADD COLUMN IF NOT EXISTS phone_no_ter::varchar NULL;
 
-ALTER TABLE party ADD COLUMN IF NOT EXISTS phone_ter_ext character varying(4) NULL;
+ALTER TABLE party ADD COLUMN IF NOT EXISTS phone_ter_ext::varchar NULL;
 
-ALTER TABLE party ADD COLUMN IF NOT EXISTS email_sec character varying(254) NULL;
+ALTER TABLE party ADD COLUMN IF NOT EXISTS email_sec::varchar NULL;
