58ffce376d43dc0d6ebcb7a2fab77a9ea4e42fb7
areyeslo <areyeslo@users.noreply.github.com>
2021-09-30 10:19:31 -0700

    New incident not passing categories and mine_incident_document_type_code missing.

---
+++ services/minespace-web/src/components/Forms/incidents/AddIncidentDetailForm.js
@@ -0,0 +1,169 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Field, reduxForm } from "redux-form";
+import { Form } from "@ant-design/compatible";
+import { Col, Row } from "antd";
+import {
+  required,
+  maxLength,
+  wholeNumber,
+  dateNotInFuture,
+  validateSelectOptions,
+} from "@common/utils/Validate";
+import * as Strings from "@common/constants/strings";
+import * as FORM from "@/constants/forms";
+import CustomPropTypes from "@/customPropTypes";
+import { renderConfig } from "@/components/common/config";
+import IncidentFileUpload from "./IncidentFileUpload";
+
+const propTypes = {
+  incidentDeterminationOptions: CustomPropTypes.options.isRequired,
+  mineGuid: PropTypes.string.isRequired,
+  onFileLoad: PropTypes.func.isRequired,
+  onRemoveFile: PropTypes.func.isRequired,
+};
+
+const defaultProps = {};
+
+// eslint-disable-next-line react/prefer-stateless-function
+class AddIncidentDetailForm extends Component {
+  render() {
+    return (
+      <Form layout="vertical">
+        <Row gutter={48}>
+          <Col span={24}>
+            <h4>Incident Details</h4>
+            <Row gutter={16}>
+              <Col md={12} xs={24}>
+                <Form.Item>
+                  <Field
+                    id="incident_date"
+                    name="incident_date"
+                    label="Incident date"
+                    placeholder="Please select date"
+                    component={renderConfig.DATE}
+                    validate={[required, dateNotInFuture]}
+                  />
+                </Form.Item>
+              </Col>
+              <Col md={12} xs={24}>
+                <Form.Item>
+                  <Field
+                    id="incident_time"
+                    name="incident_time"
+                    label="Incident time"
+                    placeholder="Please select time"
+                    component={renderConfig.TIME}
+                    validate={[required]}
+                    fullWidth
+                  />
+                </Form.Item>
+              </Col>
+            </Row>
+            <Form.Item>
+              <Field
+                id="proponent_incident_no"
+                name="proponent_incident_no"
+                label="Proponent incident number (optional)"
+                component={renderConfig.FIELD}
+                validate={[maxLength(20)]}
+              />
+            </Form.Item>
+            <Row gutter={16}>
+              <Col md={12} xs={24}>
+                <Form.Item>
+                  <Field
+                    id="number_of_injuries"
+                    name="number_of_injuries"
+                    label="Number of injuries (optional)"
+                    component={renderConfig.FIELD}
+                    validate={[wholeNumber, maxLength(10)]}
+                  />
+                </Form.Item>
+              </Col>
+              <Col md={12} xs={24}>
+                <Form.Item>
+                  <Field
+                    id="number_of_fatalities"
+                    name="number_of_fatalities"
+                    label="Number of fatalities (optional)"
+                    component={renderConfig.FIELD}
+                    validate={[wholeNumber, maxLength(10)]}
+                  />
+                </Form.Item>
+              </Col>
+            </Row>
+            <Form.Item>
+              <Field
+                id="emergency_services_called"
+                name="emergency_services_called"
+                label="Were emergency services called? (optional)"
+                placeholder="Please choose one"
+                component={renderConfig.RADIO}
+              />
+            </Form.Item>
+            <Form.Item>
+              <Field
+                id="incident_description"
+                name="incident_description"
+                label="Description of incident"
+                placeholder="Provide a detailed description of the incident"
+                component={renderConfig.SCROLL_FIELD}
+                validate={[required]}
+              />
+            </Form.Item>
+            <h4>Dangerous Occurrence Determination</h4>
+            <Form.Item>
+              <Field
+                id="mine_determination_type_code"
+                name="mine_determination_type_code"
+                label="Mine's determination (optional)"
+                component={renderConfig.SELECT}
+                validate={[validateSelectOptions(this.props.incidentDeterminationOptions)]}
+                data={this.props.incidentDeterminationOptions.filter(
+                  ({ value }) => value !== Strings.INCIDENT_DETERMINATION_TYPES.pending
+                )}
+              />
+            </Form.Item>
+            <Form.Item>
+              <Field
+                id="mine_determination_representative"
+                name="mine_determination_representative"
+                label="Mine representative who made determination"
+                component={renderConfig.FIELD}
+                validate={[maxLength(255)]}
+              />
+            </Form.Item>
+            <h4>Initial Notification Documents</h4>
+            <Form.Item>
+              <Field
+                id="InitialIncidentFileUpload"
+                name="InitialIncidentFileUpload"
+                onFileLoad={(document_name, document_manager_guid) =>
+                  this.props.onFileLoad(
+                    document_name,
+                    document_manager_guid,
+                    Strings.INCIDENT_DOCUMENT_TYPES.initial
+                  )
+                }
+                onRemoveFile={this.props.onRemoveFile}
+                mineGuid={this.props.mineGuid}
+                component={IncidentFileUpload}
+              />
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+    );
+  }
+}
+
+AddIncidentDetailForm.propTypes = propTypes;
+AddIncidentDetailForm.defaultProps = defaultProps;
+
+export default reduxForm({
+  form: FORM.ADD_INCIDENT,
+  destroyOnUnmount: false,
+  touchOnBlur: true,
+  forceUnregisterOnUnmount: true,
+})(AddIncidentDetailForm);
