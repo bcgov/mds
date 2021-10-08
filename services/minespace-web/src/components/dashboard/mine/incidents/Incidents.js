860e2ac715b5071e6ef14f9be45b813516a1ba46
areyeslo <areyeslo@users.noreply.github.com>
2021-10-08 11:38:55 -0700

    Comment Record a mine incident.

--- services/minespace-web/src/components/dashboard/mine/incidents/Incidents.js
+++ services/minespace-web/src/components/dashboard/mine/incidents/Incidents.js
@@ -2,8 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { destroy } from "redux-form";
 import { bindActionCreators } from "redux";
-import { Row, Col, Typography, Button } from "antd";
-import { PlusCircleFilled } from "@ant-design/icons";
+import { Row, Col, Typography } from "antd";
 import * as Strings from "@common/constants/strings";
 import PropTypes from "prop-types";
 import { openModal, closeModal } from "@common/actions/modalActions";
@@ -84,14 +83,15 @@ export class Incidents extends Component {
     return (
       <Row>
         <Col span={24}>
-          <Button
+          {/* Disabled new Incident button, until getting confirmation to enable it. */}
+          {/* <Button
             style={{ display: "inline", float: "right" }}
             type="primary"
             onClick={(event) => this.openCreateIncidentModal(event)}
           >
             <PlusCircleFilled />
             Record a mine incident
-          </Button>
+          </Button> */}
           <Typography.Title level={4}>Incidents</Typography.Title>
           <Typography.Paragraph>
             This table shows your mine&apos;s history of&nbsp;
