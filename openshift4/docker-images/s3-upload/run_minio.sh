7b4acd3d2a60d8cd7303c8fa522c9e71629bb6a0
jon-funk <johnthomasfunk@gmail.com>
2021-10-13 13:53:41 -0700

    Update minio dockerfile with friendly permissions

--- openshift4/docker-images/s3-upload/run_minio.sh
+++ openshift4/docker-images/s3-upload/run_minio.sh
@@ -1,6 +1,6 @@
-mc config
-mc alias list
-mc alias remove play
-mc alias set s3 $S3_HOST $S3_ACCESS_KEY $S3_SECRET
-mc alias list
-mc cp $SRC s3/$DEST
+mc -C "/app/.mc" config
+mc -C "/app/.mc" alias list
+mc -C "/app/.mc" alias remove play
+mc -C "/app/.mc" alias set s3 $S3_HOST $S3_ACCESS_KEY $S3_SECRET
+mc -C "/app/.mc" alias list
+mc -C "/app/.mc" cp $SRC s3/$DEST
