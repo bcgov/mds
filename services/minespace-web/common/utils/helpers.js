58edc7ea35a4002145cefca6db751df50dcb2828
JustinMacaulay <justinkmacaulay@gmail.com>
2021-11-03 13:56:47 -0700

    NoW Delay total not calculating correctly. (#1886)

--- services/minespace-web/common/utils/helpers.js
+++ services/minespace-web/common/utils/helpers.js
@@ -374,16 +374,20 @@ export const getDurationTextInDays = (duration) => {
   if (Math.sign(duration._milliseconds) === -1) {
     return "N/A";
   }
+  const years = duration.years();
+  const months = duration.months();
   const days = duration.days();
   const hours = duration.hours();
   const minutes = duration.minutes();
   const seconds = duration.seconds();
 
+  const yearsText = getDurationTextOrDefault(years, "Year");
+  const monthsText = getDurationTextOrDefault(months, "Month");
   const daysText = getDurationTextOrDefault(days, "Day");
   const hourText = getDurationTextOrDefault(hours, "Hour");
   const minuteText = getDurationTextOrDefault(minutes, "Minute");
   const secondText = getDurationTextOrDefault(seconds, "Second");
-  const value = `${daysText} ${hourText} ${minuteText} ${secondText}`;
+  const value = `${yearsText} ${monthsText} ${daysText} ${hourText} ${minuteText} ${secondText}`;
   return value;
 };
 
