0620c207617642d84cec3c089e163dd45e7ea695
jon-funk <johnthomasfunk@gmail.com>
2021-11-16 15:28:49 -0800

    Adjust upload behaviour

--- openshift4/docker-images/s3-upload/run_upload.sh
+++ openshift4/docker-images/s3-upload/run_upload.sh
@@ -1,5 +1,3 @@
-aws sts assume-role --role-arn $ROLE_TO_ASSUME --role-session-name s3-cronjob
-
 JSON=$(aws sts assume-role --role-arn $ROLE_TO_ASSUME --role-session-name s3-cronjob)
 
 STS_KEY_ID=$(echo $JSON | jq -r '.Credentials.AccessKeyId' -)
@@ -12,4 +10,4 @@ export AWS_SESSION_TOKEN=$STS_TOKEN
 
 aws sts get-caller-identity
 
-aws s3 cp test_file.txt s3://mds-backups-dev/backups/test_file.txt
+aws s3 $S3_ACTION
