a3b1a53759da6f513a116059174f1528a5b6fac0
Ben Harrington <Benjamin.Harrington@nttdata.com>
2021-11-17 16:41:34 -0800

    Fixing copy-pasta on reducer.

--- services/minespace-web/common/selectors/projectSummarySelectors.js
+++ services/minespace-web/common/selectors/projectSummarySelectors.js
@@ -1,28 +1,7 @@
-import { createSelector } from "reselect";
 import * as projectSummaryReducer from "../reducers/projectSummaryReducer";
 
-export const { getProjectSummaries, getProjectSummary, getProjectSummaryPageData } = projectSummaryReducer;
-
-// export const getVarianceApplications = createSelector(
-//   [getVariances],
-//   (variances) =>
-//     variances.filter(
-//       ({ variance_application_status_code }) => variance_application_status_code !== "APP"
-//     )
-// );
-
-// export const getApprovedVariances = createSelector(
-//   [getVariances],
-//   (variances) =>
-//     variances.filter(
-//       ({ variance_application_status_code }) => variance_application_status_code === "APP"
-//     )
-// );
-
-// export const getVarianceApplicationsInReview = createSelector(
-//   [getVariances],
-//   (variances) =>
-//     variances.filter(
-//       ({ variance_application_status_code }) => variance_application_status_code === "REV"
-//     )
-// );
+export const {
+  getProjectSummary,
+  getProjectSummaries,
+  getProjectSummaryPageData,
+} = projectSummaryReducer;
