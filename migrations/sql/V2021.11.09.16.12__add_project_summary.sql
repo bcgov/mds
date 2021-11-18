b3d56ff3e969a57b84026e6cf9fddef351521419
Ben Harrington <Benjamin.Harrington@nttdata.com>
2021-11-17 16:36:28 -0800

    Fixing primary key and constraints.

--- migrations/sql/V2021.11.09.16.12__add_project_summary.sql
+++ migrations/sql/V2021.11.09.16.12__add_project_summary.sql
@@ -16,23 +16,23 @@ INSERT INTO project_summary_status_code(project_summary_status_code, description
 VALUES ('O', 'Open', 10, 'system-mds', 'system-mds'), ('C', 'Closed', 20, 'system-mds', 'system-mds'), ('W', 'Withdrawn', 30, 'system-mds', 'system-mds');
 
 CREATE TABLE IF NOT EXISTS project_summary (
-    project_summary_id              serial                                PRIMARY KEY,
-    project_summary_guid            uuid DEFAULT gen_random_uuid()           NOT NULL,
+    project_summary_guid            uuid DEFAULT gen_random_uuid()        PRIMARY KEY,
+    project_summary_id              serial                                   NOT NULL,
     project_summary_description     character varying(300)                           ,
     project_summary_date            timestamp with time zone                 NOT NULL,
     deleted_ind                     boolean DEFAULT false                    NOT NULL,
     status_code                     character varying(3)                     NOT NULL,
 
     mine_guid                       uuid                                     NOT NULL,
-    project_summary_lead_guid       uuid                                             ,
+    project_summary_lead_party_guid uuid                                             ,
     
     create_user                     character varying(60)                    NOT NULL,
     create_timestamp                timestamp with time zone DEFAULT now()   NOT NULL,
     update_user                     character varying(60)                    NOT NULL,
     update_timestamp                timestamp with time zone DEFAULT now()   NOT NULL,
 
-    CONSTRAINT project_summary_guid UNIQUE (project_summary_guid),
-    CONSTRAINT project_summary_lead_guid_fkey FOREIGN KEY (project_summary_lead) REFERENCES party(party_guid) ON UPDATE CASCADE ON DELETE SET NULL,
+    CONSTRAINT project_summary_id UNIQUE (project_summary_id),
+    CONSTRAINT project_summary_lead_party_guid_fkey FOREIGN KEY (project_summary_lead_party_guid) REFERENCES party(party_guid) ON UPDATE CASCADE ON DELETE SET NULL,
     CONSTRAINT mine_guid_fkey FOREIGN KEY (mine_guid) REFERENCES mine(mine_guid) DEFERRABLE INITIALLY DEFERRED,
     CONSTRAINT status_code_fkey FOREIGN KEY (status_code) REFERENCES project_summary_status_code(project_summary_status_code) DEFERRABLE INITIALLY DEFERRED
 );
@@ -57,7 +57,7 @@ INSERT INTO project_summary_document_type(project_summary_document_type_code, de
 VALUES ('GEN', 'General', 10, 'system-mds', 'system-mds');
 
 CREATE TABLE IF NOT EXISTS project_summary_document_xref (
-    project_summary_document_xref_guid         uuid                                 PRIMARY KEY,
+    project_summary_document_xref_guid         uuid DEFAULT gen_random_uuid()       PRIMARY KEY,
     mine_document_guid                         uuid                                    NOT NULL,
     project_summary_id                         smallint                                NOT NULL,
     project_summary_document_type_code         character varying(3)                    NOT NULL,
