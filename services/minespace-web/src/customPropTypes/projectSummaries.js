5d221c8f38376ea64c86986f9aadd5a61cd5607d
Ben Harrington <Benjamin.Harrington@nttdata.com>
2021-11-17 16:48:43 -0800

    Updating propTypes.

--- services/minespace-web/src/customPropTypes/projectSummaries.js
+++ services/minespace-web/src/customPropTypes/projectSummaries.js
@@ -2,7 +2,7 @@ import { PropTypes, shape } from "prop-types";
 import { mineDocument } from "@/customPropTypes/documents";
 
 export const projectSummary = shape({
-  project_summary_id: PropTypes.string,
+  project_summary_id: PropTypes.number,
   project_summary_guid: PropTypes.string,
   status_code: PropTypes.string,
   project_summary_lead: PropTypes.string,
