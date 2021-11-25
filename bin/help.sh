2ee16d4f8d9dff6b6d48f48c44df6b6617fdc44a
jon-funk <johnthomasfunk@gmail.com>
2021-11-24 16:28:47 -0800

    Update help message

--- bin/help.sh
+++ bin/help.sh
@@ -6,9 +6,24 @@
 # Global config
 # Color codes
 CGREEN="\e[0;92m"
-CRED="\e[0;91m"
 CRESET="\e[0m"
 HELP="${CGREEN}
-HEY!
+Welcome to the MDS Project - See below for suggested usage and how to get started developing on MDS\n\n
+
+Suggested:\n
+    If you're just starting on the project then run 'make valid' to check if you're missing any import tools or configurations.\n
+    This command will also include remediation steps for any failures!\n\n
+
+(Below commands assume 'make' before each command)\n
+Usage:\n
+\t    valid: checks your dev environment is setup correctly\n
+\t    lite: creates a minimum viable setup, use to start working asap\n
+\t    getdb: pulls a copy of the database in staging\n
+\t    seeddb: uses a pulled copy to insert into your containerized postgresql database\n
+\t    full: Stands up ALL services\n
+\n
+... and check the Makefile directly for other commands\n
+\n
+Happy mining!
 ${CRESET}"
-echo $HELP
+echo -e $HELP
