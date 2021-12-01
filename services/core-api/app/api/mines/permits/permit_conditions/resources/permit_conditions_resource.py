66a6cf5cfc0ee5792c0dc3dfc2322861b60da7d4
Dragan Colic <cola_584@yahoo.com>
2021-12-01 14:28:54 -0800

    adding condition

--- services/core-api/app/api/mines/permits/permit_conditions/resources/permit_conditions_resource.py
+++ services/core-api/app/api/mines/permits/permit_conditions/resources/permit_conditions_resource.py
@@ -23,6 +23,10 @@ class PermitConditionsListResource(Resource, UserMixin):
     def post(self, mine_guid, permit_guid, permit_amendment_guid):
         permit_amendment = get_permit_amendment(permit_amendment_guid)
 
+        if not (permit_amendment.is_generated_in_core and permit_amendment.permit_amendment_status_code != "DFT" or 
+            permit_amendment.is_generated_in_core == False and permit_amendment.permit_amendment_status_code == "DFT"):
+            raise BadRequest('Permit Conditions cannot be edited if the permit was issued in Core and is no longer a draft.')
+
         request.json['permit_condition'][
             'permit_amendment_id'] = permit_amendment.permit_amendment_id
 
@@ -71,6 +75,10 @@ class PermitConditionsResource(Resource, UserMixin):
     def put(self, mine_guid, permit_guid, permit_amendment_guid, permit_condition_guid):
 
         permit_amendment = get_permit_amendment(permit_amendment_guid)
+
+        if not (permit_amendment.is_generated_in_core and permit_amendment.permit_amendment_status_code != "DFT" or 
+            permit_amendment.is_generated_in_core == False and permit_amendment.permit_amendment_status_code == "DFT"):
+            raise BadRequest('Permit Conditions cannot be edited if the permit was issued in Core and is no longer a draft.')
 
         old_condition = PermitConditions.find_by_permit_condition_guid(permit_condition_guid)
         old_display_order = old_condition.display_order
