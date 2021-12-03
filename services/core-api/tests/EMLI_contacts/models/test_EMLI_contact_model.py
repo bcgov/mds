8832b3f15197349a4b15455bc5a2f089868d9c1a
areyeslo <areyeslo@users.noreply.github.com>
2021-12-02 16:06:43 -0800

    Update test case.

--- services/core-api/tests/EMLI_contacts/models/test_EMLI_contact_model.py
+++ services/core-api/tests/EMLI_contacts/models/test_EMLI_contact_model.py
@@ -3,6 +3,7 @@ import uuid, pytest
 from app.api.EMLI_contacts.models.EMLI_contact import EMLIContact
 from tests.factories import EMLIContactFactory
 
+
 def test_find_EMLI_contact(db_session):
     contact = EMLIContactFactory()
 
@@ -12,18 +13,21 @@ def test_find_EMLI_contact(db_session):
     assert emli_contact.emli_contact_type_code == contact.emli_contact_type_code
     assert emli_contact.mine_region_code == contact.mine_region_code
 
+
 def test_find_EMLI_contact_by_id(db_session):
     contact_id = EMLIContactFactory().contact_id
 
     emli_contact = EMLIContact.find_EMLI_contact_by_id(contact_id)
     assert emli_contact.contact_id == contact_id
 
+
 def test_find_EMLI_contacts_by_mine_region(db_session):
-    mine_region_code = EMLIContactFactory().mine_region_code
+    contact = EMLIContactFactory()
 
-    emli_contact = EMLIContact.find_EMLI_contacts_by_mine_region(mine_region_code)
-
-    assert emli_contact[0].mine_region_code == mine_region_code
+    emli_contact = EMLIContact.find_EMLI_contacts_by_mine_region(contact.mine_region_code,
+                                                                 contact.is_major_mine)
+    assert (region == contact.mine_region_code or region == None
+            for region in emli_contact[0].mine_region_code)
 
 
 def test_find_all(db_session):
