8fd500f91ad356bc28e692474e47291a384135d3
areyeslo <areyeslo@users.noreply.github.com>
2021-12-03 14:59:48 -0800

    Removing test_EMLI_contact_by_id for model.

--- services/core-api/tests/EMLI_contacts/models/test_EMLI_contact_model.py
+++ services/core-api/tests/EMLI_contacts/models/test_EMLI_contact_model.py
@@ -14,11 +14,11 @@ def test_find_EMLI_contact(db_session):
     assert emli_contact.mine_region_code == contact.mine_region_code
 
 
-def test_find_EMLI_contact_by_id(db_session):
-    contact = EMLIContactFactory()
+# def test_find_EMLI_contact_by_id(db_session):
+#     contact = EMLIContactFactory()
 
-    emli_contact = EMLIContact.find_EMLI_contact_by_id(contact.contact_id)
-    assert emli_contact.contact_id == contact.contact_id
+#     emli_contact = EMLIContact.find_EMLI_contact_by_id(contact.contact_id)
+#     assert emli_contact.contact_id == contact.contact_id
 
 
 def test_find_EMLI_contacts_by_mine_region(db_session):
