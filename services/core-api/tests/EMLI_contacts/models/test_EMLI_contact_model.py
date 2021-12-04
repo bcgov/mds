0aec26785ae96d373cff0b91cf477f5abfab3953
areyeslo <areyeslo@users.noreply.github.com>
2021-12-03 17:11:36 -0800

    Adding back EMLI test cases.

--- services/core-api/tests/EMLI_contacts/models/test_EMLI_contact_model.py
+++ services/core-api/tests/EMLI_contacts/models/test_EMLI_contact_model.py
@@ -1,40 +1,40 @@
-# import uuid, pytest
+import uuid, pytest
 
-# from app.api.EMLI_contacts.models.EMLI_contact import EMLIContact
-# from tests.factories import EMLIContactFactory
-
-
-# def test_find_EMLI_contact(db_session):
-#     contact = EMLIContactFactory()
-
-#     emli_contact = EMLIContact.find_EMLI_contact(contact.emli_contact_type_code,
-#                                                  contact.mine_region_code, contact.is_major_mine)
-
-#     assert emli_contact.emli_contact_type_code == contact.emli_contact_type_code
-#     assert emli_contact.mine_region_code == contact.mine_region_code
+from app.api.EMLI_contacts.models.EMLI_contact import EMLIContact
+from tests.factories import EMLIContactFactory
 
 
-# def test_find_EMLI_contact_by_id(db_session):
-#     contact = EMLIContactFactory()
+def test_find_EMLI_contact(db_session):
+    contact = EMLIContactFactory()
 
-#     emli_contact = EMLIContact.find_EMLI_contact_by_id(contact.contact_id)
-#     assert emli_contact.contact_id == contact.contact_id
+    emli_contact = EMLIContact.find_EMLI_contact(contact.emli_contact_type_code,
+                                                 contact.mine_region_code, contact.is_major_mine)
 
-
-# def test_find_EMLI_contacts_by_mine_region(db_session):
-#     contact = EMLIContactFactory()
-
-#     emli_contact = EMLIContact.find_EMLI_contacts_by_mine_region(contact.mine_region_code,
-#                                                                  contact.is_major_mine)
-#     assert (c.mine_region == None
-#             if c.emli_contact_type_code == 'MMO' else c.mine_region == contact.mine_region
-#             for c in emli_contact)
+    assert emli_contact.emli_contact_type_code == contact.emli_contact_type_code
+    assert emli_contact.mine_region_code == contact.mine_region_code
 
 
-# def test_find_all(db_session):
-#     batch_size = 3
-#     contacts = EMLIContactFactory.create_batch(size=batch_size)
+def test_find_EMLI_contact_by_id(db_session):
+    contact = EMLIContactFactory()
 
-#     all_contacts = EMLIContact.get_all()
+    emli_contact = EMLIContact.find_EMLI_contact_by_id(contact.contact_id)
+    assert emli_contact.contact_id == contact.contact_id
 
-#     assert len(all_contacts) == 31
+
+def test_find_EMLI_contacts_by_mine_region(db_session):
+    contact = EMLIContactFactory()
+
+    emli_contact = EMLIContact.find_EMLI_contacts_by_mine_region(contact.mine_region_code,
+                                                                 contact.is_major_mine)
+    assert (c.mine_region == None
+            if c.emli_contact_type_code == 'MMO' else c.mine_region == contact.mine_region
+            for c in emli_contact)
+
+
+def test_find_all(db_session):
+    batch_size = 3
+    contacts = EMLIContactFactory.create_batch(size=batch_size)
+
+    all_contacts = EMLIContact.get_all()
+
+    assert len(all_contacts) == 31
--- services/core-api/tests/EMLI_contacts/resources/test_EMLI_contact.py
+++ services/core-api/tests/EMLI_contacts/resources/test_EMLI_contact.py
@@ -26,48 +26,48 @@ def test_get_emli_contact_by_id(test_client, db_session, auth_headers):
 
 
 #PUT
-# def test_put_emli_contact_not_found(test_client, db_session, auth_headers):
-#     data = {"first_name": "First", "last_name": "Last", "phone_number": "111-111-1111"}
-#     put_resp = test_client.put(
-#         f'/EMLI-contacts/1', json=data, headers=auth_headers['full_auth_header'])
-#     put_data = json.loads(put_resp.data.decode())
-#     assert put_resp.status_code == 404
-#     assert 'not found' in put_data['message']
+def test_put_emli_contact_not_found(test_client, db_session, auth_headers):
+    data = {"first_name": "First", "last_name": "Last", "phone_number": "111-111-1111"}
+    put_resp = test_client.put(
+        f'/EMLI-contacts/1', json=data, headers=auth_headers['full_auth_header'])
+    put_data = json.loads(put_resp.data.decode())
+    assert put_resp.status_code == 404
+    assert 'not found' in put_data['message']
 
-# def test_put_emli_contact_success(test_client, db_session, auth_headers):
-#     contact = EMLIContactFactory()
-#     data = marshal(contact, EMLI_CONTACT_MODEL)
-#     data['first_name'] = 'Mike'
-#     data['last_name'] = 'Doe'
-#     data['phone_number'] = '111-333-4444'
-#     data['email'] = 'mike.doe@contactme.com'
-#     data['fax_number'] = '250 000 000'
-#     data['mailing_address_line_1'] = 'PO BOX 1234, Prov Test'
-#     data['mailing_address_line_2'] = 'Vancouver, B.C. VVV VVV'
-#     data['is_major_mine'] = False
-#     data['is_general_contact'] = False
+def test_put_emli_contact_success(test_client, db_session, auth_headers):
+    contact = EMLIContactFactory()
+    data = marshal(contact, EMLI_CONTACT_MODEL)
+    data['first_name'] = 'Mike'
+    data['last_name'] = 'Doe'
+    data['phone_number'] = '111-333-4444'
+    data['email'] = 'mike.doe@contactme.com'
+    data['fax_number'] = '250 000 000'
+    data['mailing_address_line_1'] = 'PO BOX 1234, Prov Test'
+    data['mailing_address_line_2'] = 'Vancouver, B.C. VVV VVV'
+    data['is_major_mine'] = False
+    data['is_general_contact'] = False
 
-#     put_resp = test_client.put(
-#         f'/EMLI-contacts/{contact.contact_id}', json=data, headers=auth_headers['full_auth_header'])
-#     put_data = json.loads(put_resp.data.decode())
-#     assert put_resp.status_code == 200
-#     assert put_data['first_name'] == data['first_name']
-#     assert put_data['last_name'] == data['last_name']
-#     assert put_data['phone_number'] == data['phone_number']
-#     assert put_data['email'] == data['email']
-#     assert put_data['fax_number'] == data['fax_number']
-#     assert put_data['mailing_address_line_1'] == data['mailing_address_line_1']
-#     assert put_data['mailing_address_line_2'] == data['mailing_address_line_2']
-#     assert put_data['is_major_mine'] == data['is_major_mine']
-#     assert put_data['is_general_contact'] == data['is_general_contact']
+    put_resp = test_client.put(
+        f'/EMLI-contacts/{contact.contact_id}', json=data, headers=auth_headers['full_auth_header'])
+    put_data = json.loads(put_resp.data.decode())
+    assert put_resp.status_code == 200
+    assert put_data['first_name'] == data['first_name']
+    assert put_data['last_name'] == data['last_name']
+    assert put_data['phone_number'] == data['phone_number']
+    assert put_data['email'] == data['email']
+    assert put_data['fax_number'] == data['fax_number']
+    assert put_data['mailing_address_line_1'] == data['mailing_address_line_1']
+    assert put_data['mailing_address_line_2'] == data['mailing_address_line_2']
+    assert put_data['is_major_mine'] == data['is_major_mine']
+    assert put_data['is_general_contact'] == data['is_general_contact']
 
 #DELETE
-# def test_soft_delete_emli_contact_by_id(test_client, db_session, auth_headers):
-#     contact = EMLIContactFactory()
+def test_soft_delete_emli_contact_by_id(test_client, db_session, auth_headers):
+    contact = EMLIContactFactory()
 
-#     delete_resp = test_client.delete(
-#         f'/EMLI-contacts/{contact.contact_id}', headers=auth_headers['full_auth_header'])
+    delete_resp = test_client.delete(
+        f'/EMLI-contacts/{contact.contact_id}', headers=auth_headers['full_auth_header'])
 
-#     get_data = json.loads(delete_resp.data.decode())
+    get_data = json.loads(delete_resp.data.decode())
 
-#     assert get_data['deleted_ind'] == True
+    assert get_data['deleted_ind'] == True
