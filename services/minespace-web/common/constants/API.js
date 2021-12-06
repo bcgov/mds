580960838965e64265893585889a1863488eb721
Meredith Mumby <meredith.mumby@gmail.com>
2021-12-06 14:53:28 -0800

    updated the API url

--- services/minespace-web/common/constants/API.js
+++ services/minespace-web/common/constants/API.js
@@ -44,7 +44,7 @@ export const PROVINCE_CODES = "/parties/sub-division-codes";
 // EMLI contacts
 export const EMLI_CONTACTS = "/EMLI-contacts";
 export const EMLI_CONTACTS_BY_REGION = (region, isMajorMine) =>
-  `/EMLI-contacts/${region}?is_major_mine=${isMajorMine}`;
+  `/EMLI-contacts/${region}/contacts?is_major_mine=${isMajorMine}`;
 export const EMLI_CONTACT = (guid) => `/EMLI-contacts/${guid}`;
 
 export const MINE_VERIFIED_STATUSES = (params = {}) =>
