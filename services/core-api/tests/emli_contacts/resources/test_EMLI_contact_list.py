3bf2f618c1dc9ace28e9cfec19790b2eabf05e2f
jon-funk <johnthomasfunk@gmail.com>
2021-12-07 10:57:03 -0800

    Adjust semantics of emli tests

--- services/core-api/tests/emli_contacts/resources/test_EMLI_contact_list.py
+++ services/core-api/tests/emli_contacts/resources/test_EMLI_contact_list.py
@@ -5,48 +5,62 @@ from tests.factories import EMLIContactFactory
 
 
 def test_get_all_emli_contacts(test_client, db_session, auth_headers):
+    get_resp = test_client.get(f'/EMLI-contacts', headers=auth_headers['full_auth_header'])
+    get_data = json.loads(get_resp.data.decode())
+    init_records_count = len(get_data['records'])
+
     batch_size = 3
     #contacts = EMLIContactFactory.create_batch(size=batch_size)
     contacts = list()
 
-    for i in range(batch_size-1):
+    for i in range(batch_size - 1):
         contacts.append(EMLIContactFactory())
 
     get_resp = test_client.get(f'/EMLI-contacts', headers=auth_headers['full_auth_header'])
     get_data = json.loads(get_resp.data.decode())
     assert get_resp.status_code == 200
-    assert len(get_data['records']) == batch_size
+    assert len(get_data['records']) == len(contacts) + init_records_count
 
 
 def test_get_emli_contacts_major_mines(test_client, db_session, auth_headers):
+    get_resp = test_client.get(
+        f'/EMLI-contacts?is_major_mine=true', headers=auth_headers['full_auth_header'])
+    get_data = json.loads(get_resp.data.decode())
+    init_records_count = len(get_data['records'])
+
     batch_size = 3
     # contacts = EMLIContactFactory.create_batch(size=batch_size, major_mine=True)
     contacts = list()
 
-    for i in range(batch_size-1):
+    for i in range(batch_size - 1):
         contacts.append(EMLIContactFactory(major_mine=True))
 
     get_resp = test_client.get(
         f'/EMLI-contacts?is_major_mine=true', headers=auth_headers['full_auth_header'])
     get_data = json.loads(get_resp.data.decode())
     assert get_resp.status_code == 200
-    assert len(get_data['records']) == batch_size
+    assert len(get_data['records']) == len(contacts) + init_records_count
 
 
 def test_get_emli_contacts_by_mine_region_code(test_client, db_session, auth_headers):
+    get_resp = test_client.get(
+        f'/EMLI-contacts/NE/contacts', headers=auth_headers['full_auth_header'])
+    get_data = json.loads(get_resp.data.decode())
+    init_records_count = len(get_data['records'])
+
     batch_size = 3
 
     # contacts = EMLIContactFactory.create_batch(size=batch_size, mine_region_code='NE')
     contacts = list()
 
-    for i in range(batch_size-1):
+    for i in range(batch_size - 1):
         contacts.append(EMLIContactFactory(mine_region_code='NE'))
 
     get_resp = test_client.get(
         f'/EMLI-contacts/NE/contacts', headers=auth_headers['full_auth_header'])
     get_data = json.loads(get_resp.data.decode())
     assert get_resp.status_code == 200
-    assert len(get_data['records']) == batch_size
+    assert len(get_data['records']) == len(contacts) + init_records_count
 
 
 def test_post_emli_contacts(test_client, db_session, auth_headers):
