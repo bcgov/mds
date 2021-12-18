b038ddf045955de238f2033339ce12400c479351
Ben Harrington <Benjamin.Harrington@nttdata.com>
2021-12-17 16:34:51 -0800

    Creating new support models.

---
+++ services/core-api/app/api/mines/project_summary/models/project_summary_permit_type.py
@@ -0,0 +1,16 @@
+from app.extensions import db
+from app.api.utils.models_mixins import AuditMixin, SoftDeleteMixin, Base
+
+
+class ProjectSummaryPermitType(SoftDeleteMixin, AuditMixin, Base):
+    __tablename__ = 'project_summary_permit_type'
+
+    project_summary_permit_type = db.Column(db.String(100), primary_key=True)
+    description = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f'{self.__class__.__name__} {self.project_summary_permit_type}'
+
+    @classmethod
+    def get_all(cls):
+        return cls.query.all()
