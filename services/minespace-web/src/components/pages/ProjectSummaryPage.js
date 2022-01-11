3f0bf6decbfe931465e3099a54ecfc84d12c0b92
Meredith Mumby <meredith.mumby@gmail.com>
2022-01-11 08:44:19 -0800

    updated

--- services/minespace-web/src/components/pages/ProjectSummaryPage.js
+++ services/minespace-web/src/components/pages/ProjectSummaryPage.js
@@ -200,7 +200,7 @@ export class ProjectSummaryPage extends Component {
                     this.state.isEditMode
                       ? this.props.formattedProjectSummary.summary
                       : {
-                          contacts: [{is_primary: true}],
+                          contacts: [{ is_primary: true }],
                         }
                   }
                   mineGuid={mineGuid}
