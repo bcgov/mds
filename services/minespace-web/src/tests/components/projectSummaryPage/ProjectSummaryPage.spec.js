c29c36c7e2ddda6bf0e33daa6d6cc95f2db6eabf
Meredith Mumby <meredith.mumby@gmail.com>
2022-01-12 17:29:07 -0800

    updated the snapshot tests

--- services/minespace-web/src/tests/components/projectSummaryPage/ProjectSummaryPage.spec.js
+++ services/minespace-web/src/tests/components/projectSummaryPage/ProjectSummaryPage.spec.js
@@ -9,12 +9,14 @@ const dispatchProps = {};
 const setupProps = () => {
   props.match = { params: { id: "18133c75-49ad-4101-85f3-a43e35ae989a" } };
   props.projectSummaryDocumentTypesHash = MOCK.PROJECT_SUMMARY_DOCUMENT_TYPES_HASH;
+  props.mines = {};
 };
 
 const setupDispatchProps = () => {
   dispatchProps.fetchProjectSummaryById = jest.fn(() => Promise.resolve());
   dispatchProps.createProjectSummary = jest.fn(() => Promise.resolve());
   dispatchProps.updateProjectSummary = jest.fn(() => Promise.resolve());
+  dispatchProps.fetchMineRecordById = jest.fn(() => Promise.resolve());
 };
 
 beforeEach(() => {
