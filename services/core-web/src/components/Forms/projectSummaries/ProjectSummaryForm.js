85b5afa11b7bf8970ea74418f772942207c044ad
Ben Harrington <Benjamin.Harrington@nttdata.com>
2022-01-24 15:24:22 -0800

    Create functions for section rendering

--- services/core-web/src/components/Forms/projectSummaries/ProjectSummaryForm.js
+++ services/core-web/src/components/Forms/projectSummaries/ProjectSummaryForm.js
@@ -1,21 +1,14 @@
 /* eslint-disable */
 import React from "react";
 import PropTypes from "prop-types";
-import { Field, reduxForm, getFormValues } from "redux-form";
-import { connect } from "react-redux";
-import { compose } from "redux";
+import { Field } from "redux-form";
 import { Form } from "@ant-design/compatible";
 import "@ant-design/compatible/assets/index.css";
-import { Button, Col, Row, Popconfirm, Typography } from "antd";
-// import { required, dateNotInFuture } from "@common/utils/Validate";
+import { Typography } from "antd";
 import CustomPropTypes from "@/customPropTypes";
-// import { resetForm, createDropDownList, formatDate } from "@common/utils/helpers";
-import * as FORM from "@/constants/forms";
+import { formatDate } from "@common/utils/helpers";
 import { renderConfig } from "@/components/common/config";
-// import {
-//   getProjectSummaryStatusCodesHash,
-//   getProjectSummaryDocumentTypesHash,
-// } from "@common/selectors/staticContentSelectors";
+import DocumentTable from "@/components/common/DocumentTable";
 
 const propTypes = {
   handleSubmit: PropTypes.func.isRequired,
@@ -31,117 +24,8 @@ const defaultProps = {
 };
 
 export const ProjectSummaryForm = (props) => {
-  const {
-    formattedProjectSummary: { contacts, documents },
-  } = props;
-
-  const renderContacts = (contacts) => {
+  const renderProjectDetails = () => {
     return (
-      <div id="project-contacts">
-        <Typography.Title level={4}>Project contacts</Typography.Title>
-        <Typography strong>Proponent contacts</Typography>
-        <Typography strong>Primary project contact</Typography>
-        <Field id="primary_contact_name" name="contacts[0].name" component={renderConfig.FIELD} />
-        {contacts[0].job_title && (
-          <Field
-            id="primary_contact_job_title"
-            name="contacts[0].job_title"
-            component={renderConfig.FIELD}
-          />
-        )}
-        {contacts[0].company_name && (
-          <Field
-            id="primary_contact_company_name"
-            name="contacts[0].company_name"
-            component={renderConfig.FIELD}
-          />
-        )}
-        <Field id="primary_contact_email" name="contacts[0].email" component={renderConfig.FIELD} />
-        <Field
-          id="primary_contact_phone_number"
-          name="contacts[0].phone_number"
-          component={renderConfig.FIELD}
-        />
-        {contacts.length > 1 && <Typography strong>Additional project contacts</Typography>}
-        {/* {contacts.length > 1 && contacts.slice(1).map(c => {
-          return (
-            <Field id="primary_contact_name" name="primary_contact_name" component={renderConfig.FIELD} />
-            <Field
-              id="primary_contact_job_title"
-              name="primary_contact_job_title"
-              component={renderConfig.FIELD}
-            />
-            <Field
-              id="primary_contact_company_name"
-              name="primary_contact_company_name"
-              component={renderConfig.FIELD}
-            />
-            <Field
-              id="primary_contact_email"
-              name="primary_contact_email"
-              component={renderConfig.FIELD}
-            />
-            <Field
-              id="primary_contact_email"
-              name="primary_contact_email"
-              component={renderConfig.FIELD}
-            />
-          )
-        })} */}
-      </div>
-    );
-  };
-
-  const renderProjectDates = () => {
-    return (
-      <div id="project-dates">
-        <Typography.Title level={3}>Project dates requested by proponent</Typography.Title>
-        <Typography>
-          These are the key dates the proponent hopes to target. A final schedule will be negotiated
-          during the pre-application review.
-        </Typography>
-        <Form.Item>
-          <Field
-            id="expected_draft_irt_submission_date"
-            name="expected_draft_irt_submission_date"
-            label="Anticipated draft IRT submission date"
-            component={renderConfig.FIELD}
-            disabled
-          />
-        </Form.Item>
-        <Form.Item>
-          <Field
-            id="expected_permit_application_date"
-            name="expected_permit_application_date"
-            label="Anticipated permit application submission date"
-            component={renderConfig.FIELD}
-            disabled
-          />
-        </Form.Item>
-        <Form.Item>
-          <Field
-            id="expected_permit_receipt_date"
-            name="expected_permit_receipt_date"
-            label="Desired permit issuance date"
-            component={renderConfig.FIELD}
-            disabled
-          />
-        </Form.Item>
-        <Form.Item>
-          <Field
-            id="expected_project_start_date"
-            name="expected_project_start_date"
-            label="Anticipated work start date"
-            component={renderConfig.FIELD}
-            disabled
-          />
-        </Form.Item>
-      </div>
-    );
-  };
-
-  return (
-    <Form layout="vertical" onSubmit={props.handleSubmit}>
       <div id="project-details">
         <Typography.Title level={3}>Project details</Typography.Title>
         <Form.Item>
@@ -163,6 +47,11 @@ export const ProjectSummaryForm = (props) => {
           />
         </Form.Item>
       </div>
+    );
+  };
+
+  const renderAuthorizationsInvolved = () => {
+    return (
       <div id="authorizations-involved">
         <Typography.Title level={3}>Authorizations involved</Typography.Title>
         <Typography>
@@ -170,28 +59,187 @@ export const ProjectSummaryForm = (props) => {
           Additional authorizations may be required.
         </Typography>
       </div>
-      {renderProjectDates()}
-      {/* {renderContacts(contacts)} */}
+    );
+  };
+
+  const renderContacts = (contacts) => {
+    return (
+      <div id="project-contacts">
+        <Typography.Title level={4}>Project contacts</Typography.Title>
+        <Typography strong>Proponent contacts</Typography>
+        <Typography strong>Primary project contact</Typography>
+        <Field
+          id="primary_contact_name"
+          name="contacts[0].name"
+          component={renderConfig.FIELD}
+          disabled
+        />
+        {contacts[0]?.job_title && (
+          <Field
+            id="primary_contact_job_title"
+            name="contacts[0].job_title"
+            component={renderConfig.FIELD}
+            disabled
+          />
+        )}
+        {contacts[0]?.company_name && (
+          <Field
+            id="primary_contact_company_name"
+            name="contacts[0].company_name"
+            component={renderConfig.FIELD}
+            disabled
+          />
+        )}
+        <Field
+          id="primary_contact_email"
+          name="contacts[0].email"
+          component={renderConfig.FIELD}
+          disabled
+        />
+        <Field
+          id="primary_contact_phone_number"
+          name="contacts[0].phone_number"
+          component={renderConfig.FIELD}
+          disabled
+        />
+        {contacts.length > 1 && <Typography strong>Additional project contacts</Typography>}
+        {contacts.length >= 1 &&
+          contacts.map((c, idx) => {
+            if (!c.is_primary) {
+              return (
+                <>
+                  <Field
+                    id={`contact_name_${idx}`}
+                    name={`contacts[${idx}].name`}
+                    component={renderConfig.FIELD}
+                    disabled
+                  />
+                  {contacts[idx]?.job_title && (
+                    <Field
+                      id={`contact_job_title_${idx}`}
+                      name={`contacts[${idx}].job_title`}
+                      component={renderConfig.FIELD}
+                      disabled
+                    />
+                  )}
+                  {contacts[idx]?.company_name && (
+                    <Field
+                      id={`contact_company_name_${idx}`}
+                      name={`contacts[${idx}].company_name`}
+                      component={renderConfig.FIELD}
+                      disabled
+                    />
+                  )}
+                  <Field
+                    id={`contact_email_${idx}`}
+                    name={`contacts[${idx}].email`}
+                    component={renderConfig.FIELD}
+                    disabled
+                  />
+                  <Field
+                    id={`contact_phone_number_${idx}`}
+                    name={`contacts[${idx}].phone_number`}
+                    component={renderConfig.FIELD}
+                    disabled
+                  />
+                </>
+              );
+            }
+          })}
+      </div>
+    );
+  };
+
+  const renderProjectDates = (projectSummary) => {
+    return (
+      <div id="project-dates">
+        <Typography.Title level={3}>Project dates requested by proponent</Typography.Title>
+        <Typography>
+          These are the key dates the proponent hopes to target. A final schedule will be negotiated
+          during the pre-application review.
+        </Typography>
+        <Form.Item>
+          <Field
+            id="expected_draft_irt_submission_date"
+            name="expected_draft_irt_submission_date"
+            label="Anticipated draft IRT submission date"
+            component={renderConfig.DATE}
+            disabled
+          />
+        </Form.Item>
+        <Form.Item>
+          <Field
+            id="expected_permit_application_date"
+            name="expected_permit_application_date"
+            label="Anticipated permit application submission date"
+            component={renderConfig.DATE}
+            disabled
+          />
+        </Form.Item>
+        <Form.Item>
+          <Field
+            id="expected_permit_receipt_date"
+            name="expected_permit_receipt_date"
+            label="Desired permit issuance date"
+            component={renderConfig.DATE}
+            disabled
+          />
+        </Form.Item>
+        <Form.Item>
+          <Field
+            id="expected_project_start_date"
+            name="expected_project_start_date"
+            label="Anticipated work start date"
+            component={renderConfig.DATE}
+            disabled
+          />
+        </Form.Item>
+      </div>
+    );
+  };
+
+  const renderDocuments = (documents) => {
+    return (
       <div id="documents">
         <Typography.Title level={4}>Documents</Typography.Title>
+        <DocumentTable
+          documents={documents.reduce(
+            (docs, doc) => [
+              {
+                key: doc.mine_document_guid,
+                mine_document_guid: doc.mine_document_guid,
+                document_manager_guid: doc.document_manager_guid,
+                name: doc.document_name,
+                category:
+                  props.projectSummaryDocumentTypesOptions[doc.project_summary_document_type_code],
+                uploaded: doc.created_at,
+              },
+              ...docs,
+            ],
+            []
+          )}
+          isViewOnly
+        />
       </div>
+    );
+  };
+
+  const {
+    formattedProjectSummary: { contacts, documents },
+  } = props;
+
+  return (
+    <Form layout="vertical" onSubmit={props.handleSubmit}>
+      {renderProjectDetails()}
+      {renderAuthorizationsInvolved()}
+      {renderProjectDates(props.formattedProjectSummary)}
+      {renderContacts(contacts)}
+      {renderDocuments(documents)}
     </Form>
   );
 };
 
 ProjectSummaryForm.propTypes = propTypes;
 ProjectSummaryForm.defaultProps = defaultProps;
-
-// const mapStateToProps = (state) => ({
-//   formValues: getFormValues(FORM.PROJECT_SUMMARY)(state),
-// });
-
-// export default compose(
-//   connect(mapStateToProps),
-//   reduxForm({
-//     form: FORM.PROJECT_SUMMARY,
-//     enableReinitialize: true,
-//   })
-// )(ProjectSummaryForm);
 
 export default ProjectSummaryForm;
