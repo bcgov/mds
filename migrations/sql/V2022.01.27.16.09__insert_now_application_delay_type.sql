4a5612f0dbcede11f02ae643f48d3d3a70dc5c6e
areyeslo <areyeslo@users.noreply.github.com>
2022-01-27 16:13:22 -0800

    Add a new NoW application delay type.

---
+++ migrations/sql/V2022.01.27.16.09__insert_now_application_delay_type.sql
@@ -0,0 +1,10 @@
+INSERT INTO now_application_delay_type (
+    delay_type_code,
+    description,
+    create_user,
+    update_user
+    )
+VALUES 
+    ('NRT', 'Nation Requests Additional Time','system-mds', 'system-mds')
+
+ON CONFLICT DO NOTHING;
