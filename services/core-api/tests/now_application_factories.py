569d3e8b9cccd8152b6e571cabf625d4cc9e9543
Dragan Colic <cola_584@yahoo.com>
2022-02-24 14:07:35 -0800

    adding two more missing fields

--- services/core-api/tests/now_application_factories.py
+++ services/core-api/tests/now_application_factories.py
@@ -458,6 +458,8 @@ class NOWApplicationFactory(BaseFactory):
     liability_adjustment = factory.fuzzy.FuzzyInteger(1, 10000)
     crown_grant_or_district_lot_numbers = factory.Faker('sentence', nb_words=100, variable_nb_words=True)
     adjusted_annual_maximum_tonnage = factory.fuzzy.FuzzyInteger(1, 10000)
+    first_aid_cert_level = factory.Faker('sentence', nb_words=100, variable_nb_words=True)
+    is_applicant_individual_or_company = factory.Faker('sentence', nb_words=100, variable_nb_words=True)
 
     access_operations = factory.RelatedFactory(AccessOperationsFactory, 'now_application')
     access_roads = factory.RelatedFactory(AccessRoadsFactory, 'now_application')
