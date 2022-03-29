860555ed6139f8c661d4b3dea1641a312a407b98
matbusby-fw <mat@freshworks.io>
2022-03-29 16:21:12 -0600

    Created basic nod tab

---
+++ services/minespace-web/src/components/dashboard/mine/nods/nods.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Col, Row, Typography } from "antd";
+
+// eslint-disable-next-line react/prefer-stateless-function
+export class Nods extends Component {
+  render() {
+    return (
+      <Row>
+        <Col span={24}>
+          <Typography.Title level={4}>Nods</Typography.Title>
+          <Typography.Paragraph>
+            The below table displays all of the&nbsp;
+            <Typography.Text className="color-primary" strong>
+              permit applications
+            </Typography.Text>
+            &nbsp;associated with this mine.
+          </Typography.Paragraph>
+        </Col>
+      </Row>
+    );
+  }
+}
+
+export default Nods;
