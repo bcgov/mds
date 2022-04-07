e9851da7bf8d932ad50fac230bc9511826e0d519
Hitankar Ray <hitankarray@MacBook-Pro.hitronhub.home>
2022-04-07 11:36:03 -0700

    refactor endpoints for nods and FE as well

--- services/core-api/tests/mines/notice_of_departure/test_nod_list_resource.py
+++ services/core-api/tests/mines/notice_of_departure/test_nod_list_resource.py
@@ -21,6 +21,23 @@ class TestNodListResource:
         assert get_resp.status_code == 200
         assert len(get_data['records']) == batch_size
 
+    def test_get_nods_for_a_mine_permit_combo(self, test_client, db_session, auth_headers):
+        """Should return the correct number of records and a 200 status code"""
+
+        batch_size = 3
+        mine = MineFactory(minimal=True)
+        permit = PermitFactory()
+        NoticeOfDepartureFactory.create_batch(size=batch_size, mine=mine, permit=permit)
+        NoticeOfDepartureFactory.create_batch(size=batch_size, mine=mine)
+
+        get_resp = test_client.get(
+            f'/mines/{mine.mine_guid}/notices-of-departure',
+            headers=auth_headers['full_auth_header'])
+        get_data = json.loads(get_resp.data.decode())
+        assert get_resp.status_code == 200
+        assert len(get_data['records']) == batch_size
+        assert len(get_data['records']) != 6
+
     def test_get_nods_non_existent_mine_guid(self, test_client, db_session, auth_headers):
         """Should return a 404 when the provided mine guid does not exist"""
 
