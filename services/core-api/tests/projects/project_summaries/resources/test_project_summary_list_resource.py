5320242d484b20ec986ff2c0a5932b69fc0c05c9
Ben Harrington <Benjamin.Harrington@nttdata.com>
2022-04-11 14:04:16 -0700

    Enable resource tests

--- services/core-api/tests/projects/project_summaries/resources/test_project_summary_list_resource.py
+++ services/core-api/tests/projects/project_summaries/resources/test_project_summary_list_resource.py
@@ -5,28 +5,20 @@ from tests.factories import MineFactory
 from tests.factories import ProjectSummaryFactory
 from tests.factories import ProjectFactory
 
-# def test_get_project_summaries_by_project_guid(test_client, db_session, auth_headers):
-#     mine = MineFactory(minimal=True)
-#     project = ProjectFactory.create_batch(mine=mine, size=1)
-#     project_summary = ProjectSummaryFactory.create_batch(project=project, size=1)
 
-#     get_resp = test_client.get(
-#         f'/projects/${project.project_guid}/project-summaries?mine_guid={str(mine.mine_guid)}',
-#         headers=auth_headers['full_auth_header'])
-#     get_data = json.loads(get_resp.data.decode())
+def test_get_project_summaries_by_project_guid(test_client, db_session, auth_headers):
+    mine = MineFactory(minimal=True)
+    project = ProjectFactory(mine=mine)
+    project_summary = ProjectSummaryFactory(project=project)
 
-#     assert get_resp.status_code == 200, get_resp.response
-#     assert len(get_data['records']) == 1
+    get_resp = test_client.get(
+        f'/projects/${project.project_guid}/project-summaries?mine_guid={mine.mine_guid}',
+        headers=auth_headers['full_auth_header'])
+    get_data = json.loads(get_resp.data.decode())
 
-# def test_get_project_summaries_by_invalid_project_guid(test_client, db_session, auth_headers):
-#     batch_size = 1
-#     ProjectSummaryFactory.create_batch(size=batch_size)
-
-#     get_resp = test_client.get(
-#         f'/projects/badProjectGuid123/project-summaries?mine_guid=123',
-#         headers=auth_headers['full_auth_header'])
-
-#     assert get_resp.status_code == 404
+    assert get_resp.status_code == 200, get_resp.response
+    # TODO: After ProjectSummaryListGetResource gets refactored for long term use, update this test
+    # assert len(get_data['records']) == 1
 
 
 def test_post_project_summary_minimum(test_client, db_session, auth_headers):
@@ -43,7 +35,6 @@ def test_post_project_summary_minimum(test_client, db_session, auth_headers):
     assert post_resp.status_code == 201
 
     post_data = json.loads(post_resp.data.decode())
-    print(f'post data: {post_data}')
     assert post_data['mine_guid'] == str(mine.mine_guid)
     assert post_data['project_summary_title'] == data['project_summary_title']
     assert post_data['status_code'] == data['status_code']
--- services/core-api/tests/projects/project_summaries/resources/test_project_summary_resource.py
+++ services/core-api/tests/projects/project_summaries/resources/test_project_summary_resource.py
@@ -5,33 +5,32 @@ from flask_restplus import marshal
 from app.api.projects.project_summary.response_models import PROJECT_SUMMARY_MODEL
 
 # def test_get_project_summary_by_project_summary_guid(test_client, db_session, auth_headers):
-#     project = ProjectFactory()
-#     project_summary = ProjectSummaryFactory(project=project)
+#     # project = ProjectFactory()
+#     project_summary = ProjectSummaryFactory()
 
 #     get_resp = test_client.get(
-#         f'/projects/{project.project_guid}/project-summaries/{project_summary.project_summary_guid}',
+#         f'/projects/{project_summary.project.project_guid}/project-summaries/{project_summary.project_summary_guid}',
 #         headers=auth_headers['full_auth_header'])
 #     get_data = json.loads(get_resp.data.decode())
 
-#     assert get_resp.status_code == 200
-#     assert get_data['project_guid'] == str(project.project_guid)
-#     assert get_data['project_summary_guid'] == str(project_summary.project_summary_guid)
-#     assert get_data['project_summary_title'] == str(project_summary.project_summary_title)
-#     assert get_data['status_code'] == str(project_summary.status_code)
+# assert get_resp.status_code == 200
+# assert get_data['project_guid'] == str(project_summary.project.project_guid)
+# assert get_data['project_summary_guid'] == str(project_summary.project_summary_guid)
+# assert get_data['project_summary_title'] == str(project_summary.project_summary_title)
+# assert get_data['status_code'] == str(project_summary.status_code)
 
 
 def test_put_project_summary(test_client, db_session, auth_headers):
-    mine = MineFactory(minimal=True)
-    project = ProjectFactory(mine=mine)
-    project_summary = ProjectSummaryFactory(project=project)
+    project_summary = ProjectSummaryFactory()
     data = marshal(project_summary, PROJECT_SUMMARY_MODEL)
+    print(f'PROS: {project_summary} PS_ID: {project_summary.project.project_guid}')
 
-    data['mine_guid'] = mine.mine_guid
+    data['mine_guid'] = project_summary.project.mine_guid
     data['project_summary_title'] = 'Test Title'
     data['status_code'] = 'SUB'
 
     put_resp = test_client.put(
-        f'/projects/{project.project_guid}/project-summaries/{project_summary.project_summary_guid}',
+        f'/projects/{project_summary.project.project_guid}/project-summaries/{project_summary.project_summary_guid}',
         headers=auth_headers['full_auth_header'],
         json=data)
     put_data = json.loads(put_resp.data.decode())
