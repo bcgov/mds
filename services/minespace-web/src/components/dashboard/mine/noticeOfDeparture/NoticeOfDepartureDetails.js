0820e5410a7e5e43cefc7a6fe9d593da9c130104
Mat Busby <mat@freshworks.io>
2022-04-14 15:28:15 -0600

    destructured props

--- services/minespace-web/src/components/dashboard/mine/noticeOfDeparture/NoticeOfDepartureDetails.js
+++ services/minespace-web/src/components/dashboard/mine/noticeOfDeparture/NoticeOfDepartureDetails.js
@@ -7,17 +7,15 @@ const propTypes = {
 };
 
 export const NoticeOfDepartureDetails = (props) => {
+  const { nod_title, permit, nod_guid } = props.noticeOfDeparture;
+
   return (
     <div>
       <Typography.Title level={4}>Basic Information</Typography.Title>
       <Descriptions colon={false} layout="vertical" column={1} size="middle">
-        <Descriptions.Item label="Departure Project Title">
-          {props.noticeOfDeparture.nod_title}
-        </Descriptions.Item>
-        <Descriptions.Item label="Permit #">
-          {props.noticeOfDeparture.permit.permit_no}
-        </Descriptions.Item>
-        <Descriptions.Item label="NOD #">{props.noticeOfDeparture.nod_guid}</Descriptions.Item>
+        <Descriptions.Item label="Departure Project Title">{nod_title}</Descriptions.Item>
+        <Descriptions.Item label="Permit #">{permit.permit_no}</Descriptions.Item>
+        <Descriptions.Item label="NOD #">{nod_guid}</Descriptions.Item>
       </Descriptions>
     </div>
   );
