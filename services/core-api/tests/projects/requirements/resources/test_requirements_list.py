cbbaac2fe23d67b7feb7d5e6e448e09a6f0e850f
areyeslo <areyeslo@users.noreply.github.com>
2022-04-17 22:11:33 -0700

    Requirements are seeded in migration, do not create from Factory.

--- services/core-api/tests/projects/requirements/resources/test_requirements_list.py
+++ services/core-api/tests/projects/requirements/resources/test_requirements_list.py
@@ -3,15 +3,16 @@ import json
 import uuid
 
 from app.api.projects.information_requirements_table.models.requirements import Requirements
-from tests.factories import RequirementsFactory
+# from tests.factories import RequirementsFactory
 
 
 def test_get_all_requirements(test_client, db_session, auth_headers):
-    requirements = RequirementsFactory()
+    # requirements = RequirementsFactory()
+    requirements = Requirements.get_all()
 
     get_resp = test_client.get(f'/projects/requirements', headers=auth_headers['full_auth_header'])
     get_data = json.loads(get_resp.data.decode())
 
     assert get_resp.status_code == 200
 
-    assert get_data['records'][0]['requirement_guid'] == str(requirements.requirement_guid)
+    assert len(get_data['records']) == len(requirements)
--- services/core-api/tests/projects/requirements/resources/test_requirements_resource.py
+++ services/core-api/tests/projects/requirements/resources/test_requirements_resource.py
@@ -6,51 +6,53 @@ from tests.factories import RequirementsFactory
 
 
 def test_get_requirements_by_requirement_guid(test_client, db_session, auth_headers):
-    requirement = RequirementsFactory()
+    requirement = Requirements.get_all()
 
     get_resp = test_client.get(
-        f'/projects/requirements/{requirement.requirement_guid}',
+        f'/projects/requirements/{requirement[0].requirement_guid}',
         headers=auth_headers['full_auth_header'])
     get_data = json.loads(get_resp.data.decode())
 
     assert get_resp.status_code == 200
-    assert get_data['requirement_guid'] == str(requirement.requirement_guid)
+    assert get_data['requirement_guid'] == str(requirement[0].requirement_guid)
 
 
 def test_put_requirement(test_client, db_session, auth_headers):
-    requirement = RequirementsFactory()
+    #requirement = RequirementsFactory()
+    requirements = Requirements.get_all()
 
     data = {
-        "requirement_guid": requirement.requirement_guid,
-        "requirement_id": requirement.requirement_id,
+        "requirement_guid": requirements[0].requirement_guid,
+        "requirement_id": requirements[0].requirement_id,
         "parent_requirement_id": None,
         "description": "Updated Description for Test",
         "display_order": 1
     }
 
     put_resp = test_client.put(
-        f'/projects/requirements/{requirement.requirement_guid}',
+        f'/projects/requirements/{requirements[0].requirement_guid}',
         headers=auth_headers['full_auth_header'],
         json=data)
     put_data = json.loads(put_resp.data.decode())
 
-    updated_requirement = Requirements.find_by_requirement_guid(requirement.requirement_guid)
+    updated_requirement = Requirements.find_by_requirement_guid(requirements[0].requirement_guid)
 
     assert put_resp.status_code == 200
-    assert put_data['records']['requirement_guid'] == str(requirement.requirement_guid)
-    assert put_data['records']['requirement_id'] == requirement.requirement_id
+    assert put_data['records']['requirement_guid'] == str(requirements[0].requirement_guid)
+    assert put_data['records']['requirement_id'] == requirements[0].requirement_id
     assert put_data['records']['description'] == updated_requirement.description
     assert put_data['records']['display_order'] == updated_requirement.display_order
 
 
 def test_delete_requirement(test_client, db_session, auth_headers):
-    requirement = RequirementsFactory()
+    # requirement = RequirementsFactory()
+    requirements = Requirements.get_all()
 
     del_resp = test_client.delete(
-        f'/projects/requirements/{requirement.requirement_guid}',
+        f'/projects/requirements/{requirements[0].requirement_guid}',
         headers=auth_headers['full_auth_header'])
 
-    updated_requirement = Requirements.find_by_requirement_guid(requirement.requirement_guid)
+    updated_requirement = Requirements.find_by_requirement_guid(requirements[0].requirement_guid)
 
     assert del_resp.status_code == 204
     assert updated_requirement == None
