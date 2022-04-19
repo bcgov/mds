03b030fdf71bd04059a47bd5fcda0d77b3b8b6a2
Hitankar Ray <hitankar@freshworks.io>
2022-04-19 12:54:58 -0700

    add fe tests

--- services/core-web/src/components/mine/NoticeOfDeparture/MineNoticeOfDeparture.js
+++ services/core-web/src/components/mine/NoticeOfDeparture/MineNoticeOfDeparture.js
@@ -60,7 +60,6 @@ export class MineNoticeOfDeparture extends Component {
         isLoaded={this.state.isLoaded}
         nods={this.props.nods}
         mine={mine}
-        isApplication
       />
       <br />
     </div>
