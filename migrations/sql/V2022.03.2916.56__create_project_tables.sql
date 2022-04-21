77eecb8c29cc36db0ed64023d9993ff8befcd27c
Ben Harrington <Benjamin.Harrington@nttdata.com>
2022-04-21 08:36:33 -0700

    Move datafix to separate migration

--- migrations/sql/V2022.03.2916.56__create_project_tables.sql
+++ migrations/sql/V2022.03.2916.56__create_project_tables.sql
@@ -50,29 +50,3 @@ COMMENT ON TABLE project_contact IS 'Project contact details related to a projec
 -- Add foreign keys to project on children(Summary)
 ALTER TABLE project_summary ADD COLUMN IF NOT EXISTS project_guid uuid;
 ALTER TABLE project_summary ADD CONSTRAINT project_guid_fkey FOREIGN KEY (project_guid) REFERENCES project(project_guid);
-
--- Data Fix:
--- Copy data from project_summary to project table(create a project for every project summary)
-INSERT INTO project (mine_guid, project_title, project_lead_party_guid, proponent_project_id, create_user,update_user)
-  SELECT ps.mine_guid, ps.project_summary_title, ps.project_summary_lead_party_guid, ps.proponent_project_id, 'system-mds','system-mds'
-  FROM project_summary ps
-  WHERE mine_guid not in (select mine_guid
-								   from project);
-
--- Update project_summary to include FK to newly created projects
-UPDATE project_summary ps SET project_guid = (SELECT project_guid FROM project WHERE mine_guid = ps.mine_guid LIMIT 1);
-								  
--- Copy data from project_summary_contact to project_contact by project_guid (copied in the previous query)								   
-INSERT INTO project_contact(project_guid, name, job_title, company_name, email, phone_number, phone_extension, is_primary, deleted_ind, create_user, update_user)
-  SELECT pr.project_guid, psc.name, psc.job_title, psc.company_name, psc.email, psc.phone_number, psc.phone_extension, psc.is_primary, psc.deleted_ind, psc.create_user, psc.update_user
-  FROM project_summary_contact psc, project pr, project_summary ps
-  WHERE psc.project_summary_guid = ps.project_summary_guid and pr.project_guid = ps.project_guid;
-
--- Drop legacy tables, columns, and constraints
-ALTER TABLE project_summary
-DROP CONSTRAINT IF EXISTS mine_guid_fkey,
-DROP COLUMN IF EXISTS mine_guid,
-DROP CONSTRAINT IF EXISTS project_summary_lead_party_guid_fkey,
-DROP COLUMN IF EXISTS project_summary_lead_party_guid,
-DROP COLUMN IF EXISTS project_summary_title,
-DROP COLUMN IF EXISTS proponent_project_id;
