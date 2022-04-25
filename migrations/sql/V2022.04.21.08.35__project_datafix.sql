5a85be8f9f3656962b20c6c84477cce5e3fcd0d6
areyeslo <areyeslo@users.noreply.github.com>
2022-04-25 11:18:30 -0700

    Separation of concerns: Removed dropping columns and include in a following migration. Update project_summary table.

--- migrations/sql/V2022.04.21.08.35__project_datafix.sql
+++ migrations/sql/V2022.04.21.08.35__project_datafix.sql
@@ -2,7 +2,7 @@
 INSERT INTO project (mine_guid, project_title, project_lead_party_guid, proponent_project_id, create_user,update_user)
   SELECT ps.mine_guid, ps.project_summary_title, ps.project_summary_lead_party_guid, ps.proponent_project_id, 'system-mds','system-mds'
   FROM project_summary ps
-  WHERE mine_guid not in (select mine_guid
+  WHERE ps.mine_guid not in (select mine_guid
 								   from project);
 
 -- Update project_summary to include FK to newly created projects
@@ -13,12 +13,3 @@ INSERT INTO project_contact(project_guid, name, job_title, company_name, email, 
   SELECT pr.project_guid, psc.name, psc.job_title, psc.company_name, psc.email, psc.phone_number, psc.phone_extension, psc.is_primary, psc.deleted_ind, psc.create_user, psc.update_user
   FROM project_summary_contact psc, project pr, project_summary ps
   WHERE psc.project_summary_guid = ps.project_summary_guid and pr.project_guid = ps.project_guid;
-
--- Drop legacy tables, columns, and constraints
-ALTER TABLE project_summary
-DROP CONSTRAINT IF EXISTS mine_guid_fkey,
-DROP COLUMN IF EXISTS mine_guid,
-DROP CONSTRAINT IF EXISTS project_summary_lead_party_guid_fkey,
-DROP COLUMN IF EXISTS project_summary_lead_party_guid,
-DROP COLUMN IF EXISTS project_summary_title,
-DROP COLUMN IF EXISTS proponent_project_id;
