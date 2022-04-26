a4de13d924e42f40b8c724d8bbeeb27071e53dfa
Ben Harrington <Benjamin.Harrington@nttdata.com>
2022-04-26 10:48:41 -0700

    Remove troublesome migrations temporarily

--- migrations/sql/V2022.04.25.11.52__drop_columns_project_summary.sql
+++
@@ -1,8 +0,0 @@
--- Drop legacy tables, columns, and constraints
-ALTER TABLE project_summary
-DROP CONSTRAINT IF EXISTS mine_guid_fkey,
-DROP COLUMN IF EXISTS mine_guid,
-DROP CONSTRAINT IF EXISTS project_summary_lead_party_guid_fkey,
-DROP COLUMN IF EXISTS project_summary_lead_party_guid,
-DROP COLUMN IF EXISTS project_summary_title,
-DROP COLUMN IF EXISTS proponent_project_id;
