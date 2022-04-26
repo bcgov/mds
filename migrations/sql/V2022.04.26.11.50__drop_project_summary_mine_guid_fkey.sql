4b8b81d3c66a8b291e8e2569d2425a8fa04c53f3
areyeslo <areyeslo@users.noreply.github.com>
2022-04-26 12:59:24 -0700

    Dropping additional columns in project summary.

--- migrations/sql/V2022.04.26.11.50__drop_project_summary_mine_guid_fkey.sql
+++ migrations/sql/V2022.04.26.11.50__drop_project_summary_mine_guid_fkey.sql
@@ -2,4 +2,5 @@
 -- We need to disable this and change the column to NULLABLE to move forward.
 ALTER TABLE project_summary
   ALTER COLUMN mine_guid DROP NOT NULL,
-  DROP CONSTRAINT IF EXISTS mine_guid_fkey;
+  DROP CONSTRAINT IF EXISTS mine_guid_fkey,
+  ALTER COLUMN project_summary_title DROP NOT NULL;
