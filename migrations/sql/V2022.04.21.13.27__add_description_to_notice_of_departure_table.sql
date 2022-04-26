78e9f439ee6a2f213db6a3b32b5b66adac4c78bf
Mat Busby <mat@freshworks.io>
2022-04-26 14:51:35 -0600

    Removed first not null

--- migrations/sql/V2022.04.21.13.27__add_description_to_notice_of_departure_table.sql
+++ migrations/sql/V2022.04.21.13.27__add_description_to_notice_of_departure_table.sql
@@ -1,4 +1,4 @@
-ALTER TABLE notice_of_departure ADD nod_description character varying(3000) NOT NULL;
+ALTER TABLE notice_of_departure ADD nod_description character varying(3000);
 
 UPDATE notice_of_departure SET nod_description = ' ' WHERE nod_description IS NULL;
 
