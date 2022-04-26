1f412b7b7a52d1ed8491c830de321c260eaf5bf9
Ben Harrington <Benjamin.Harrington@nttdata.com>
2022-04-26 12:03:34 -0700

    Add small fix to migration

--- migrations/sql/V2022.04.26.11.50__drop_project_summary_mine_guid_fkey.sql
+++ migrations/sql/V2022.04.26.11.50__drop_project_summary_mine_guid_fkey.sql
@@ -1,3 +1,5 @@
 -- The project summary factories have been updated in the code base to not include mine_guid anymore but the schema still has a NOT NULL FKEY. 
--- We need to disable this to move forward.
-ALTER TABLE project_summary DROP CONSTRAINT IF EXISTS mine_guid_fkey;
+-- We need to disable this and change the column to NULLABLE to move forward.
+ALTER TABLE project_summary
+  ALTER COLUMN mine_guid DROP NOT NULL,
+  DROP CONSTRAINT IF EXISTS mine_guid_fkey;
