7ac634f75537c42bda393ba1c5236562e6a76160
areyeslo <areyeslo@users.noreply.github.com>
2022-04-26 12:59:24 -0700

    Dropping additional columns in project summary.

---
+++ migrations/sql/V2022.04.26.12.25__drop_additional_columns_project_summary.sql
@@ -0,0 +1,5 @@
+ALTER TABLE project_summary
+DROP CONSTRAINT IF EXISTS project_summary_lead_party_guid_fkey,
+DROP COLUMN IF EXISTS project_summary_lead_party_guid,
+DROP COLUMN IF EXISTS project_summary_title,
+DROP COLUMN IF EXISTS proponent_project_id;
