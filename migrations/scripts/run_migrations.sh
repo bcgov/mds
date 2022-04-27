a18d60f19703275bfac86740e1a0463a895f76b9
v-y-a-s <10957095+v-y-a-s@users.noreply.github.com>
2022-04-26 21:10:26 -0700

    [fix] run test db migrations if not in k8s

--- migrations/scripts/run_migrations.sh
+++ migrations/scripts/run_migrations.sh
@@ -5,12 +5,12 @@ set -exv -o pipefail
 cd $FLYWAY_HOME
 
 # Substitute environment variables in template files
-if ls $FLYWAY_HOME/sql/*.tmpl > /dev/null 2>&1 ; then
-echo "---> substituting environment variables in templates"
-for file in $FLYWAY_HOME/sql/*.tmpl; do
-    target_file="$FLYWAY_HOME/sql/$(basename "$file" .tmpl)"
-    envsubst < "$file" > "$target_file"
-done
+if ls $FLYWAY_HOME/sql/*.tmpl >/dev/null 2>&1; then
+    echo "---> substituting environment variables in templates"
+    for file in $FLYWAY_HOME/sql/*.tmpl; do
+        target_file="$FLYWAY_HOME/sql/$(basename "$file" .tmpl)"
+        envsubst <"$file" >"$target_file"
+    done
 fi
 
 # Make flyway binary discoverable
@@ -19,5 +19,8 @@ export PATH="$FLYWAY_HOME:$PATH"
 # Run migrations for mds database
 flyway migrate
 
-# Run migrations for mds_test database
-flyway -url=jdbc:postgresql://$FLYWAY_DB_HOST/mds_test migrate
+Run migrations for mds_test database
+
+if [ "$PLATFORM" != "K8S" ]; then
+    flyway -url=jdbc:postgresql://$FLYWAY_DB_HOST/mds_test migrate
+fi
