9ce7f23bdd0006c2d6f2017eb4107c30a095a598
Hitankar Ray <hitankar@freshworks.io>
2022-04-27 13:18:58 -0700

    add namespace

--- services/core-api/app/api/mines/namespace.py
+++ services/core-api/app/api/mines/namespace.py
@@ -18,6 +18,7 @@ from app.api.mines.permits.permit_amendment.resources.permit_amendment import Pe
 from app.api.mines.permits.permit_amendment.resources.permit_amendment_document import PermitAmendmentDocumentListResource, PermitAmendmentDocumentResource
 from app.api.mines.permits.permit_amendment.resources.permit_amendment_vc import PermitAmendmentVCResource
 from app.api.mines.notice_of_departure.resources.notice_of_departure_list import NoticeOfDepartureListResource
+from app.api.mines.notice_of_departure.resources.notice_of_departure_document import MineNoticeOfDepartureDocumentUploadResource
 from app.api.mines.permits.permit.resources.permit_document_upload import PermitDocumentUploadInitializationResource
 from app.api.mines.explosives_permit.resources.explosives_permit import ExplosivesPermitResource
 from app.api.mines.explosives_permit.resources.explosives_permit_list import ExplosivesPermitListResource
@@ -163,6 +164,9 @@ api.add_resource(
 )
 
 api.add_resource(NoticeOfDepartureListResource, '/<string:mine_guid>/notices-of-departure')
+
+api.add_resource(MineNoticeOfDepartureDocumentUploadResource,
+                 '/<string:mine_guid>/notices-of-departure/<string:nod_guid>/documents')
 
 api.add_resource(ExplosivesPermitResource,
                  '/<string:mine_guid>/explosives-permits/<string:explosives_permit_guid>')
