41f18cd885fde9b3287ffeb4d1aea5c8709b09a0
vyasworks <vyas@freshworks.io>
2022-05-02 09:53:42 -0700

    [fix] use mds' repo's hash!

--- gitops/commit.sh
+++ gitops/commit.sh
@@ -7,6 +7,7 @@ TARGET_ENV=${3?"Enter Target Env Name !"}
 ACTOR_NAME=${4?"Enter Actor Name !"}
 GITHUB_TOKEN=${5?"Enter Target Env Name !"}
 
+MDS_GIT_HASH=$(git rev-parse --verify HEAD)
 REPO_URL="https://github.com/bcgov/mds/commit"
 REPO_LOCATION=$(git rev-parse --show-toplevel)
 TIMESTAMP=$(date +"%y-%m-%d-%H-%M-%S")
@@ -24,22 +25,21 @@ cd $REPO_LOCATION/gitops/tenant-gitops-4c2ba9
 
 if [ $TARGET_ENV == 'dev' ]; then
     echo "$TARGET_ENV Environment, updating git hash in deployment env vars"
-    GIT_HASH=$(git rev-parse --verify HEAD)
 
     # Replace the commit sha in env vars in overlay patch
-    sed -i "s^git-commit.*^git-commit-$GIT_HASH-TS-$TIMESTAMP^" $TARGET_APP/overlays/$TARGET_ENV/deployment.patch.yaml
+    sed -i "s^git-commit.*^git-commit-$MDS_GIT_HASH-TS-$TIMESTAMP^" $TARGET_APP/overlays/$TARGET_ENV/deployment.patch.yaml
     git add .
-    git commit -m "[$TARGET_APP] Pushed to $TARGET_ENV by $ACTOR_NAME - $REPO_URL/$GIT_HASH"
+    git commit -m "[$TARGET_APP] Pushed to $TARGET_ENV by $ACTOR_NAME - $REPO_URL/$MDS_GIT_HASH"
 else
     echo "$TARGET_ENV Environment, Trigger deployment by promoting image from $SOURCE_ENV env"
     # copy git has from dev into test / prod overlay patch!
     # promotion of images are based off the same git hash!
-    GIT_HASH= $(cat core-api/overlays/$SOURCE_ENV/deployment.patch.yaml | grep git-commit -m1 | cut -f2 -d ":" | xargs)
+    MDS_GIT_HASH= $(cat core-api/overlays/$SOURCE_ENV/deployment.patch.yaml | grep git-commit -m1 | cut -f2 -d ":" | xargs)
 
     # Replace the commit sha in env vars in overlay patch
-    sed -i "s^git-commit.*^git-commit-$GIT_HASH-TS-$TIMESTAMP^" $TARGET_APP/overlays/$TARGET_ENV/deployment.patch.yaml
+    sed -i "s^git-commit.*^git-commit-$MDS_GIT_HASH-TS-$TIMESTAMP^" $TARGET_APP/overlays/$TARGET_ENV/deployment.patch.yaml
     git add .
-    git commit -m "[$TARGET_APP] Pushed from $SOURCE_ENV to $TARGET_ENV by $ACTOR_NAME - $REPO_URL/$GIT_HASH"
+    git commit -m "[$TARGET_APP] Pushed from $SOURCE_ENV to $TARGET_ENV by $ACTOR_NAME - $REPO_URL/$MDS_GIT_HASH"
 fi
 
 # push to gitops repo
