8e427562314e97a943a5a1cf7fbe523c3ed2af97
vyasworks <vyas@freshworks.io>
2022-05-02 13:37:06 -0700

    [fix] better commit message for promotion

--- gitops/commit.sh
+++ gitops/commit.sh
@@ -14,6 +14,14 @@ TIMESTAMP=$(date +"%y-%m-%d-%H-%M-%S")
 
 git config --global user.name $ACTOR_NAME
 
+function commit() {
+    git add -A
+    if ! git diff-index --quiet HEAD; then
+        git commit -m "$@"
+        git push origin main
+    fi
+}
+
 if [ ! -d $REPO_LOCATION/gitops/tenant-gitops-4c2ba9 ]; then
     git clone https://v-y-a-s:$GITHUB_TOKEN@github.com/bcgov-c/tenant-gitops-4c2ba9 $REPO_LOCATION/gitops/tenant-gitops-4c2ba9
 fi
@@ -27,9 +35,8 @@ if [ $TARGET_ENV == 'dev' ]; then
     echo "$TARGET_ENV Environment, updating git hash in deployment env vars"
 
     # Replace the commit sha in env vars in overlay patch
-    sed -i "s^git-commit.*^git-commit-$MDS_GIT_HASH-TS-$TIMESTAMP^" $TARGET_APP/overlays/$TARGET_ENV/deployment.patch.yaml
-    git add .
-    git commit -m "[$TARGET_APP] Pushed to $TARGET_ENV by $ACTOR_NAME - $REPO_URL/$MDS_GIT_HASH"
+    sed -i "s^git-commit.*^git-commit-$MDS_GIT_HASH^" $TARGET_APP/overlays/$TARGET_ENV/deployment.patch.yaml
+    commit "[$TARGET_APP] Pushed to $TARGET_ENV by $ACTOR_NAME - $REPO_URL/$MDS_GIT_HASH"
 else
     echo "$TARGET_ENV Environment, Trigger deployment by promoting image from $SOURCE_ENV env"
     # copy git has from dev into test / prod overlay patch!
@@ -37,10 +44,7 @@ else
     MDS_GIT_HASH=$(cat core-api/overlays/$SOURCE_ENV/deployment.patch.yaml | grep git-commit -m1 | cut -f2 -d ":" | xargs)
 
     # Replace the commit sha in env vars in overlay patch
-    sed -i "s^git-commit.*^git-commit-$MDS_GIT_HASH-TS-$TIMESTAMP^" $TARGET_APP/overlays/$TARGET_ENV/deployment.patch.yaml
-    git add .
-    git commit -m "[$TARGET_APP] Pushed from $SOURCE_ENV to $TARGET_ENV by $ACTOR_NAME - $REPO_URL/$MDS_GIT_HASH"
-fi
+    sed -i "s^git-commit.*^git-commit-$MDS_GIT_HASH^" $TARGET_APP/overlays/$TARGET_ENV/deployment.patch.yaml
+    commit "[$TARGET_APP] Pushed from $SOURCE_ENV to $TARGET_ENV by $ACTOR_NAME - $REPO_URL/$MDS_GIT_HASH"
 
-# push to gitops repo
-git push
+fi
