8b58efacfe320e5ef2aef4e51762502826e45599
v-y-a-s <10957095+v-y-a-s@users.noreply.github.com>
2022-05-02 08:03:36 -0700

    [chore] add actor name to gitops, link to commit

--- gitops/commit.sh
+++ gitops/commit.sh
@@ -3,8 +3,10 @@ set -e
 
 TARGET_APP=${1?"Enter App Name !"}
 TARGET_ENV=${2?"Enter Target Env Name !"}
-GITHUB_TOKEN=${3?"Enter Target Env Name !"}
+ACTOR_NAME=${3?"Enter Actor Name !"}
+GITHUB_TOKEN=${4?"Enter Target Env Name !"}
 
+REPO_URL="https://github.com/bcgov/mds/commit"
 REPO_LOCATION=$(git rev-parse --show-toplevel)
 GIT_HASH=$(git rev-parse --verify HEAD)
 
@@ -16,9 +18,9 @@ if [ ! -d $REPO_LOCATION/gitops/tenant-gitops-4c2ba9 ]; then
 fi
 
 # Replace the commit id with new commit id of latest push
-sed -i "s/git-commit.*/git-commit-$GIT_HASH/" $REPO_LOCATION/gitops/tenant-gitops-4c2ba9/$TARGET_APP/overlays/$TARGET_ENV/deployment.patch.yaml
+sed -i "s/git-commit.*/git-commit-$REPO_URL/$GIT_HASH" $REPO_LOCATION/gitops/tenant-gitops-4c2ba9/$TARGET_APP/overlays/$TARGET_ENV/deployment.patch.yaml
 
 cd $REPO_LOCATION/gitops/tenant-gitops-4c2ba9
 git add .
-git commit -m "Update $TARGET_APP for $TARGET_ENV with $GIT_HASH"
+git commit -m "[$TARGET_APP] Pushed to $TARGET_ENV by $ACTOR_NAME - $REPO_URL/$GIT_HASH"
 git push
