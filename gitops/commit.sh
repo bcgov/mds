51c3e794487c29c1dc49b4bc6e40a32239b5b861
vyasworks <vyas@freshworks.io>
2022-05-02 15:57:53 -0700

    [fix] naming commits

--- gitops/commit.sh
+++ gitops/commit.sh
@@ -16,10 +16,11 @@ git config --global user.name $ACTOR_NAME
 
 function commit() {
     git add -A
-    if ! git diff-index --quiet HEAD; then
-        git commit -m "$@"
-        git push origin main
-    fi
+
+    # if ! git diff-index --quiet HEAD; then
+    #     git commit -m "$@"
+    #     git push origin main
+    # fi
 }
 
 if [ ! -d $REPO_LOCATION/gitops/tenant-gitops-4c2ba9 ]; then
@@ -44,7 +45,7 @@ else
     MDS_GIT_HASH=$(cat core-api/overlays/$SOURCE_ENV/deployment.patch.yaml | grep git-commit -m1 | cut -f2 -d ":" | xargs)
 
     # Replace the commit sha in env vars in overlay patch
-    sed -i "s^git-commit.*^git-commit-$MDS_GIT_HASH^" $TARGET_APP/overlays/$TARGET_ENV/deployment.patch.yaml
+    sed -i "s^git-commit.*^$MDS_GIT_HASH^" $TARGET_APP/overlays/$TARGET_ENV/deployment.patch.yaml
     commit "[$TARGET_APP] Pushed from $SOURCE_ENV to $TARGET_ENV by $ACTOR_NAME - $REPO_URL/$MDS_GIT_HASH"
 
 fi
