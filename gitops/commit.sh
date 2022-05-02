2b8bab865b255ad89ddce9f880de435dd3eb1b3a
gh-promoter <automation@mds.gov.bc.ca>
2022-05-02 08:44:43 -0700

    [chore] add timestamp to deployment commits

--- gitops/commit.sh
+++ gitops/commit.sh
@@ -9,6 +9,7 @@ GITHUB_TOKEN=${4?"Enter Target Env Name !"}
 REPO_URL="https://github.com/bcgov/mds/commit"
 REPO_LOCATION=$(git rev-parse --show-toplevel)
 GIT_HASH=$(git rev-parse --verify HEAD)
+TIMESTAMP=$(date +"%y-%m-%d-%H-%M-%S")
 
 git config --global user.email "automation@mds.gov.bc.ca"
 git config --global user.name "gh-promoter"
@@ -20,7 +21,7 @@ fi
 # Replace the commit id with new commit id of latest push
 # ^ is the delimiter here since repo url has many forward slashes!
 
-sed -i "s^git-commit.*^git-commit-${REPO_URL}/$GIT_HASH^" $REPO_LOCATION/gitops/tenant-gitops-4c2ba9/$TARGET_APP/overlays/$TARGET_ENV/deployment.patch.yaml
+sed -i "s^git-commit.*^git-commit-${REPO_URL}/$GIT_HASH-TS-$TIMESTAMP^" $REPO_LOCATION/gitops/tenant-gitops-4c2ba9/$TARGET_APP/overlays/$TARGET_ENV/deployment.patch.yaml
 
 cd $REPO_LOCATION/gitops/tenant-gitops-4c2ba9
 git add .
