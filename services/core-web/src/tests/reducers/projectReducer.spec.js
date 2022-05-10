b6a49e97bafefeb7230a9ef191a616750b173063
Ben Harrington <Benjamin.Harrington@nttdata.com>
2022-05-10 14:13:55 -0700

    Fix reference from refactor

--- services/core-web/src/tests/reducers/projectReducer.spec.js
+++ services/core-web/src/tests/reducers/projectReducer.spec.js
@@ -1,5 +1,5 @@
-import { projectSummaryReducer } from "@common/reducers/projectSummaryReducer";
-import { storeProjectSummaries, storeProjectSummary } from "@common/actions/projectSummaryActions";
+import { projectReducer } from "@common/reducers/projectReducer";
+import { storeProjectSummaries, storeProjectSummary } from "@common/actions/projectActions";
 import * as MOCK from "@/tests/mocks/dataMocks";
 
 const baseExpectedValue = {
@@ -11,10 +11,10 @@ const baseExpectedValue = {
 // Creates deep copy of javascript object instead of setting a reference
 const getBaseExpectedValue = () => JSON.parse(JSON.stringify(baseExpectedValue));
 
-describe("projectSummaryReducer", () => {
+describe("projectReducer", () => {
   it("receives undefined", () => {
     const expectedValue = getBaseExpectedValue();
-    const result = projectSummaryReducer(undefined, {});
+    const result = projectReducer(undefined, {});
     expect(result).toEqual(expectedValue);
   });
 
@@ -22,14 +22,14 @@ describe("projectSummaryReducer", () => {
     const expectedValue = getBaseExpectedValue();
     expectedValue.projectSummaries = MOCK.PROJECT_SUMMARIES.records;
     expectedValue.projectSummaryPageData = MOCK.PROJECT_SUMMARIES;
-    const result = projectSummaryReducer(undefined, storeProjectSummaries(MOCK.PROJECT_SUMMARIES));
+    const result = projectReducer(undefined, storeProjectSummaries(MOCK.PROJECT_SUMMARIES));
     expect(result).toEqual(expectedValue);
   });
 
   it("receives STORE_PROJECT_SUMMARY", () => {
     const expectedValue = getBaseExpectedValue();
     expectedValue.projectSummary = MOCK.PROJECT_SUMMARY;
-    const result = projectSummaryReducer(undefined, storeProjectSummary(MOCK.PROJECT_SUMMARY));
+    const result = projectReducer(undefined, storeProjectSummary(MOCK.PROJECT_SUMMARY));
     expect(result).toEqual(expectedValue);
   });
 });
