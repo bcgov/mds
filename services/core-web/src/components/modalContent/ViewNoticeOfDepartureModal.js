5ada22e1e6699e0bfda075c2981d85e7a465172b
Mat Busby <83598933+matbusby-fw@users.noreply.github.com>
2022-05-16 15:31:25 -0600

    Mds bugfix edit nods (#2086)
    * addressed QA NOD edit comments
    
    * Updated nodviewmodal snapshot

--- services/core-web/src/components/modalContent/ViewNoticeOfDepartureModal.js
+++ services/core-web/src/components/modalContent/ViewNoticeOfDepartureModal.js
@@ -1,13 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Button, Col, Row } from "antd";
+import { Button, Col, Popconfirm, Row } from "antd";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import {
   EMPTY_FIELD,
   NOTICE_OF_DEPARTURE_DOCUMENT_TYPE,
-  NOTICE_OF_DEPARTURE_TYPE,
   NOTICE_OF_DEPARTURE_STATUS,
+  NOTICE_OF_DEPARTURE_TYPE,
 } from "@common/constants/strings";
 import CustomPropTypes from "@/customPropTypes";
 import CoreTable from "@/components/common/CoreTable";
@@ -16,8 +16,8 @@ import LinkButton from "@/components/common/buttons/LinkButton";
 import { downloadFileFromDocumentManager } from "@common/utils/actionlessNetworkCalls";
 import { formatDate } from "@common/utils/helpers";
 import {
+  fetchDetailedNoticeOfDeparture,
   removeFileFromDocumentManager,
-  fetchDetailedNoticeOfDeparture
 } from "@common/actionCreators/noticeOfDepartureActionCreator";
 import { getNoticeOfDeparture } from "@common/selectors/noticeOfDepartureSelectors";
 
@@ -25,7 +25,7 @@ const propTypes = {
   closeModal: PropTypes.func.isRequired,
   noticeOfDeparture: CustomPropTypes.noticeOfDeparture.isRequired,
   fetchDetailedNoticeOfDeparture: PropTypes.func.isRequired,
-  mine: CustomPropTypes.mine.isRequired
+  mine: CustomPropTypes.mine.isRequired,
 };
 
 export const ViewNoticeOfDepartureModal = (props) => {
@@ -39,61 +39,76 @@ export const ViewNoticeOfDepartureModal = (props) => {
     (doc) => doc.document_type !== NOTICE_OF_DEPARTURE_DOCUMENT_TYPE.CHECKLIST
   );
 
-
-  const handleDeleteANoticeOfDepartureDocument = async(document) => {
-
+  const handleDeleteANoticeOfDepartureDocument = async (document) => {
     await removeFileFromDocumentManager(document);
-    
-    await props.fetchDetailedNoticeOfDeparture(
-      mine.mine_guid,
-      nod_guid
-    );
 
+    await props.fetchDetailedNoticeOfDeparture(mine.mine_guid, nod_guid);
   };
 
-  const fileColumns = [
-    {
-      title: "Name",
-      dataIndex: "document_name",
-      sortField: "document_name",
-      sorter: (a, b) => (a.document_name > b.document_name ? -1 : 1),
-      render: (text) => (
-        <div className="nod-table-link">
-          {text ? (
-            <LinkButton onClick={() => downloadFileFromDocumentManager(checklist[0])} title="Download">
-              {text}
-            </LinkButton>
-          ) : (
-            <div>{EMPTY_FIELD}</div>
-          )}
-        </div>
-      ),
-    },
-    {
-      title: "Category",
-      dataIndex: "document_category",
-      sortField: "document_category",
-      sorter: (a, b) => (a.document_category > b.document_category ? -1 : 1),
-      render: (text) => <div title="Id">{text || EMPTY_FIELD}</div>,
-    },
-    {
-      title: "Uploaded",
-      dataIndex: "create_timestamp",
-      sortField: "create_timestamp",
-      sorter: (a, b) => (a.create_timestamp > b.create_timestamp ? -1 : 1),
-      render: (text) => <div title="Type">{formatDate(text) || EMPTY_FIELD}</div>,
-    },
-    {
-      dataIndex: "actions",
-      render: () => (
-        <div className="btn--middle flex">
-          <button type="button" onClick={() => handleDeleteANoticeOfDepartureDocument({mine_guid: mine.mine_guid, nod_guid, ...checklist[0]})}>
-            <img name="remove" src={TRASHCAN} alt="Remove Document" />
-          </button>
-        </div>
-      ),
-    },
-  ];
+  const fileColumns = (isSortable) => {
+    return [
+      {
+        title: "Name",
+        dataIndex: "document_name",
+        sortField: "document_name",
+        sorter: isSortable ? (a, b) => a.document_name.localeCompare(b.document_name) : false,
+        render: (text) => (
+          <div className="nod-table-link">
+            {text ? (
+              <LinkButton
+                onClick={() => downloadFileFromDocumentManager(checklist[0])}
+                title="Download"
+              >
+                {text}
+              </LinkButton>
+            ) : (
+              <div>{EMPTY_FIELD}</div>
+            )}
+          </div>
+        ),
+      },
+      {
+        title: "Category",
+        dataIndex: "document_category",
+        sortField: "document_category",
+        sorter: isSortable
+          ? (a, b) => a.document_category.localeCompare(b.document_category)
+          : false,
+        render: (text) => <div title="Id">{text || EMPTY_FIELD}</div>,
+      },
+      {
+        title: "Uploaded",
+        dataIndex: "create_timestamp",
+        sortField: "create_timestamp",
+        sorter: isSortable ? (a, b) => a.create_timestamp.localeCompare(b.create_timestamp) : false,
+        render: (text) => <div title="Type">{formatDate(text) || EMPTY_FIELD}</div>,
+      },
+      {
+        dataIndex: "actions",
+        render: () => (
+          <div className="btn--middle flex">
+            <Popconfirm
+              placement="topRight"
+              title="Are you sure you want to cancel?"
+              onConfirm={() =>
+                handleDeleteANoticeOfDepartureDocument({
+                  mine_guid: mine.mine_guid,
+                  nod_guid,
+                  ...checklist[0],
+                })
+              }
+              okText="Yes"
+              cancelText="No"
+            >
+              <button type="button">
+                <img name="remove" src={TRASHCAN} alt="Remove Document" />
+              </button>
+            </Popconfirm>
+          </div>
+        ),
+      },
+    ];
+  };
 
   return (
     <div>
@@ -134,14 +149,14 @@ export const ViewNoticeOfDepartureModal = (props) => {
         <h4 className="nod-modal-section-header padding-md--top">Self-Assessment Form</h4>
         <CoreTable
           condition
-          columns={fileColumns}
+          columns={fileColumns(false)}
           dataSource={checklist}
           tableProps={{ pagination: false }}
         />
         <h4 className="nod-modal-section-header padding-md--top">Application Documentation</h4>
         <CoreTable
           condition
-          columns={fileColumns}
+          columns={fileColumns(true)}
           dataSource={otherDocuments}
           tableProps={{ pagination: false }}
         />
@@ -201,7 +216,7 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = (dispatch) =>
   bindActionCreators(
     {
-      fetchDetailedNoticeOfDeparture
+      fetchDetailedNoticeOfDeparture,
     },
     dispatch
   );
