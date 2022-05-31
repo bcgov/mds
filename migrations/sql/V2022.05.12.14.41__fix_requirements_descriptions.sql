e17a291f759a21fd7e6124439ad5923833e793e9
Ben Harrington <Benjamin.Harrington@nttdata.com>
2022-05-31 15:17:00 -0700

    Create requirements fix migration

--- migrations/sql/V2022.05.12.14.41__fix_requirements_descriptions.sql
+++ migrations/sql/V2022.05.12.14.41__fix_requirements_descriptions.sql
@@ -1,7 +1,12 @@
--- Add missing category
-INSERT INTO requirements(requirement_id, parent_requirement_id, description, display_order, create_user, update_user)
-VALUES  (158, 2, 'Water Quality', 7, 'system-mds', 'system-mds');
+-- Update display_order for parent section 2 before adding new category
+UPDATE requirements r SET display_order = r.display_order + 1 WHERE display_order >= 7 and parent_requirement_id = 2;
 
--- Update description and assign new category as parent
-UPDATE requirements SET description = 'Surface Water Quality', parent_requirement_id = 158, display_order = 1 WHERE description = 'Surface Water Quality Model';
-UPDATE requirements SET description = 'Groundwater Quality', parent_requirement_id = 158, display_order = 2 WHERE description = 'Groundwater Model';
+-- Add missing section 2 category
+INSERT INTO requirements(requirement_id, parent_requirement_id, description, display_order, create_user, update_user)
+VALUES (158, 2, 'Water Quality', 7, 'system-mds', 'system-mds');
+
+-- Add child records to newly added missing category
+INSERT INTO requirements(requirement_id, parent_requirement_id, description, display_order, create_user, update_user)
+VALUES
+  (159, (SELECT requirement_id FROM requirements WHERE description = 'Water Quality' LIMIT 1), 'Surface Water Quality', 1, 'system-mds', 'system-mds'),
+  (160, (SELECT requirement_id FROM requirements WHERE description = 'Water Quality' LIMIT 1), 'Groundwater Quality', 2, 'system-mds', 'system-mds');
