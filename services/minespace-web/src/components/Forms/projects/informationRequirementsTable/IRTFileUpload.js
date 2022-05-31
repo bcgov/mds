1782f17f5d7e996268e4e9fe1f52a3aec6c54a50
Ben Harrington <Benjamin.Harrington@nttdata.com>
2022-05-31 13:59:08 -0700

    Add afterSuccess payload

--- services/minespace-web/src/components/Forms/projects/informationRequirementsTable/IRTFileUpload.js
+++ services/minespace-web/src/components/Forms/projects/informationRequirementsTable/IRTFileUpload.js
@@ -1,13 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Field } from "redux-form";
-import { INFORMATION_REQUIREMENTS_TABLE } from "@common/constants/API";
+import { INFORMATION_REQUIREMENTS_TABLE_DOCUMENTS } from "@common/constants/API";
 import FileUpload from "@/components/common/FileUpload";
 
 const propTypes = {
   onFileLoad: PropTypes.func.isRequired,
   onRemoveFile: PropTypes.func.isRequired,
-  importIrtSpreadsheet: PropTypes.func.isRequired,
+  createInformationRequirementsTable: PropTypes.func.isRequired,
   acceptedFileTypesMap: PropTypes.objectOf(PropTypes.string).isRequired,
   projectGuid: PropTypes.string.isRequired,
 };
@@ -17,13 +17,16 @@ export const IRTFileUpload = (props) => (
     id="fileUpload"
     name="fileUpload"
     component={FileUpload}
-    uploadUrl={INFORMATION_REQUIREMENTS_TABLE(props.projectGuid)}
+    uploadUrl={INFORMATION_REQUIREMENTS_TABLE_DOCUMENTS(props.projectGuid)}
     acceptedFileTypesMap={props.acceptedFileTypesMap}
     onFileLoad={props.onFileLoad}
     onRemoveFile={props.onRemoveFile}
-    importIrtSpreadsheet={props.importIrtSpreadsheet}
-    projectGuid={props.projectGuid}
     allowRevert
+    allowMultiple={false}
+    afterSuccess={{
+      action: props.createInformationRequirementsTable,
+      actionGuid: props.projectGuid,
+    }}
   />
 );
 
