0c7ad5898713cde903c11a266642f2b63cb08658
Ben Harrington <Benjamin.Harrington@nttdata.com>
2022-06-01 15:20:49 -0700

    Extend customPropTypes for project

---
+++ services/core-web/src/customPropTypes/projects.js
@@ -0,0 +1,13 @@
+import { PropTypes } from "prop-types";
+import { mineDocument } from "./documents";
+
+export const projectSummary = PropTypes.shape({
+  project_summary_id: PropTypes.number,
+  project_summary_guid: PropTypes.string,
+  status_code: PropTypes.string,
+  documents: PropTypes.arrayOf(mineDocument),
+});
+
+export const project = PropTypes.shape({
+  project_guid: PropTypes.string,
+});
