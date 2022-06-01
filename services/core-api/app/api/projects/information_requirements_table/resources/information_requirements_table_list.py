b8175b539620058c9a65b60407c74208f18b4fc4
Ben Harrington <Benjamin.Harrington@nttdata.com>
2022-06-01 16:46:49 -0700

    Re-enable creation check again

--- services/core-api/app/api/projects/information_requirements_table/resources/information_requirements_table_list.py
+++ services/core-api/app/api/projects/information_requirements_table/resources/information_requirements_table_list.py
@@ -142,9 +142,9 @@ class InformationRequirementsTableListResource(Resource, UserMixin):
             if project is None:
                 raise BadRequest('Cannot import IRT, the project supplied cannot be found')
 
-            # existing_irt = InformationRequirementsTable.find_by_project_guid(project_guid)
-            # if existing_irt and existing_irt.status_code == 'REC':
-            #     raise BadRequest('Cannot import IRT, this project already has one imported')
+            existing_irt = InformationRequirementsTable.find_by_project_guid(project_guid)
+            if existing_irt and existing_irt.status_code == 'REC':
+                raise BadRequest('Cannot import IRT, this project already has one imported')
 
             new_information_requirements_table = self.build_irt_payload_from_excel(
                 import_file, project_guid)
