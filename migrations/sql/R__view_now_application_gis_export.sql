34f93e1e76b91e3efdb5908ddfef5038827d82d1
Will Sonnex <will@freshworks.io>
2022-06-02 09:34:53 -0700

    Added indexes and increased working memory

--- migrations/sql/R__view_now_application_gis_export.sql
+++ migrations/sql/R__view_now_application_gis_export.sql
@@ -1,5 +1,34 @@
  DROP VIEW IF EXISTS public.now_application_gis_export_view;
- DROP MATERIALIZED VIEW IF EXISTS public.now_application_gis_export_view;
+
+CREATE INDEX IF NOT EXISTS idx_activity_summary_now_application_id
+ON activity_summary (now_application_id);
+
+CREATE INDEX IF NOT EXISTS idx_now_submissions_application_messageid
+ON now_submissions.application (messageid);
+
+CREATE INDEX IF NOT EXISTS idx_mine_mine_guid
+ON mine (mine_guid);
+
+CREATE INDEX IF NOT EXISTS idx_now_application_identity_mine_guid
+ON now_application_identity (mine_guid);
+
+CREATE INDEX IF NOT EXISTS idx_now_application_identity_now_application_id
+ON now_application_identity (now_application_id);
+
+CREATE INDEX IF NOT EXISTS idx_now_application_identity_now_application_guid
+ON now_application_identity (now_application_guid);
+
+CREATE INDEX IF NOT EXISTS idx_mms_now_submissions_application_mms_cid
+ON mms_now_submissions.application (mms_cid);
+
+CREATE INDEX IF NOT EXISTS idx_mine_status_mine_guid_effective_date
+ON mine_status (mine_guid, effective_date DESC);
+
+CREATE INDEX IF NOT EXISTS idx_now_application_identity_mms_cid
+ON now_application_identity (mms_cid);
+
+-- Moves the top-level sort into memory
+SET LOCAL work_mem = "400MB";
 
  CREATE MATERIALIZED VIEW IF NOT EXISTS public.now_application_gis_export_view
  AS SELECT nai.now_application_guid::character varying AS now_application_guid,
