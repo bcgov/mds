fdd1b9a9442bee4ef94200d4d4131336df47690f
areyeslo <arturo_reyes_lopez@hotmail.com>
2022-06-02 11:22:59 -0700

    Merging IRT requirements to general requirements and adding some styling when displaying data

--- services/minespace-web/src/components/Forms/projects/informationRequirementsTable/ReviewSubmitInformationRequirementsTable.js
+++ services/minespace-web/src/components/Forms/projects/informationRequirementsTable/ReviewSubmitInformationRequirementsTable.js
@@ -29,20 +29,39 @@ function ReviewSubmitInformationRequirementsTable(props) {
 const Subrequirement = (props) => (
   <>
     <Col style={{ width: "100%" }} span={24}>
-      <p>
-        <Highlight>{`${props.sub_requirement.step} ${props.sub_requirement.description}`}</Highlight>
-      </p>
-      <p>
-        <Checkbox checked={props.sub_requirement.required}>Required</Checkbox>
-        <Checkbox checked={props.sub_requirement.methods}>Methods</Checkbox>
-      </p>
-      <p>
-        <Input.TextArea
-          rows={4}
-          value={props.sub_requirement.comment ? props.sub_requirement.comment : ""}
-          name="comment"
-        />
-      </p>
+      <span
+        id="description"
+        style={
+          props.sub_requirement.step.match(/\./g).length === 2
+            ? { fontWeight: "bold", fontSize: "16px" }
+            : props.sub_requirement.step.match(/\./g).length === 3
+            ? { fontWeight: "bold", fontSize: "14px" }
+            : { fontSize: "14px" }
+        }
+      >
+        {`${props.sub_requirement.step} ${props.sub_requirement.description}`}
+      </span>
+
+      {props.sub_requirement.required === undefined &&
+      props.sub_requirement.methods === undefined ? null : (
+        <>
+          <p>
+            <Checkbox checked={props.sub_requirement.required.toLowerCase() === "true"}>
+              Required
+            </Checkbox>
+            <Checkbox checked={props.sub_requirement.methods.toLowerCase() === "true"}>
+              Methods
+            </Checkbox>
+          </p>
+          <p>
+            <Input.TextArea
+              rows={4}
+              value={props.sub_requirement.comment ? props.sub_requirement.comment : ""}
+              name="comment"
+            />
+          </p>
+        </>
+      )}
     </Col>
     {props.sub_requirement.sub_requirements &&
       renderSubrequirement(props.sub_requirement.sub_requirements)}
