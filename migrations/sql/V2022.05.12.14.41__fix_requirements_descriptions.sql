48f60214dd3b527c22d866f506744781cea35fd2
Ben Harrington <Benjamin.Harrington@nttdata.com>
2022-06-02 10:19:05 -0700

    Fix sequence issue with migration/test

--- migrations/sql/V2022.05.12.14.41__fix_requirements_descriptions.sql
+++ migrations/sql/V2022.05.12.14.41__fix_requirements_descriptions.sql
@@ -2,12 +2,12 @@
 UPDATE requirements r SET display_order = r.display_order + 1 WHERE display_order >= 7 and parent_requirement_id = 2;
 
 -- Add missing section 2 category
-INSERT INTO requirements(requirement_id, parent_requirement_id, description, display_order, create_user, update_user)
-VALUES (158, 2, 'Water Quality', 7, 'system-mds', 'system-mds');
+INSERT INTO requirements(parent_requirement_id, description, display_order, create_user, update_user)
+VALUES (2, 'Water Quality', 7, 'system-mds', 'system-mds');
 
 -- Add child records to newly added missing category
-INSERT INTO requirements(requirement_id, parent_requirement_id, description, display_order, create_user, update_user)
+INSERT INTO requirements(parent_requirement_id, description, display_order, create_user, update_user)
 VALUES
-  (159, (SELECT requirement_id FROM requirements WHERE description = 'Water Quality' LIMIT 1), 'Surface Water Quality', 1, 'system-mds', 'system-mds'),
-  (160, (SELECT requirement_id FROM requirements WHERE description = 'Water Quality' LIMIT 1), 'Groundwater Quality', 2, 'system-mds', 'system-mds'),
-  (161, 5, 'Summary', 1, 'system-mds', 'system-mds');
+  ((SELECT requirement_id FROM requirements WHERE description = 'Water Quality' LIMIT 1), 'Surface Water Quality', 1, 'system-mds', 'system-mds'),
+  ((SELECT requirement_id FROM requirements WHERE description = 'Water Quality' LIMIT 1), 'Groundwater Quality', 2, 'system-mds', 'system-mds'),
+  (5, 'Summary', 1, 'system-mds', 'system-mds');
--- services/core-api/tests/projects/requirements/resources/test_requirements_list.py
+++ services/core-api/tests/projects/requirements/resources/test_requirements_list.py
@@ -18,8 +18,7 @@ def test_post_requirement(test_client, db_session, auth_headers):
         "requirement": {
             "description": "New Requirement",
             "parent_requirement_id": 1,
-            "display_order": 6,
-            "requirement_id": 161
+            "display_order": 6
         }
     }
 
