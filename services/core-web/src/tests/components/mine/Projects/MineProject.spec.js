29b83b2f6e055eb98de9832e69106ee16334c4b9
Ben Harrington <Benjamin.Harrington@nttdata.com>
2022-06-03 12:20:36 -0700

    Create/update tests and snapshots

--- services/core-web/src/tests/components/mine/Projects/MineProject.spec.js
+++ services/core-web/src/tests/components/mine/Projects/MineProject.spec.js
@@ -7,15 +7,14 @@ const props = {};
 const dispatchProps = {};
 
 const setupProps = () => {
-  props.mineGuid = "18145c75-49ad-0101-85f3-a43e45ae989a";
   props.mines = MOCK.MINES.mines;
-  props.projectSummaries = [];
+  props.mineGuid = "18145c75-49ad-0101-85f3-a43e45ae989a";
+  props.projects = MOCK.PROJECTS.records;
   props.projectSummaryStatusCodesHash = MOCK.PROJECT_SUMMARY_STATUS_CODES_HASH;
-  props.userRoles = [];
 };
 
 const setupDispatchProps = () => {
-  dispatchProps.fetchProjectSummariesByMine = jest.fn(() => Promise.resolve());
+  dispatchProps.fetchProjectsByMine = jest.fn(() => Promise.resolve());
 };
 
 beforeEach(() => {
