e77e2dcc7ae4d21965a1f555ffd665ac5fb72581
areyeslo <arturo_reyes_lopez@hotmail.com>
2022-06-03 21:40:56 -0700

    Adding snapshots and data mock.

--- services/core-web/src/tests/reducers/projectReducer.spec.js
+++ services/core-web/src/tests/reducers/projectReducer.spec.js
@@ -1,5 +1,10 @@
 import { projectReducer } from "@common/reducers/projectReducer";
-import { storeProjectSummaries, storeProjectSummary } from "@common/actions/projectActions";
+import {
+  storeProjectSummaries,
+  storeProjectSummary,
+  storeRequirements,
+  storeInformationRequirementsTable,
+} from "@common/actions/projectActions";
 import * as MOCK from "@/tests/mocks/dataMocks";
 
 const baseExpectedValue = {
@@ -9,7 +14,8 @@ const baseExpectedValue = {
   projects: [],
   project: {},
   projectPageData: {},
-  informationRequirementsTable: {},
+  informationRequirementsTable: [],
+  requirements: [],
 };
 
 // Creates deep copy of javascript object instead of setting a reference
@@ -34,6 +40,23 @@ describe("projectReducer", () => {
     const expectedValue = getBaseExpectedValue();
     expectedValue.projectSummary = MOCK.PROJECT_SUMMARY;
     const result = projectReducer(undefined, storeProjectSummary(MOCK.PROJECT_SUMMARY));
+    expect(result).toEqual(expectedValue);
+  });
+
+  it("receives STORE_REQUIREMENTS", () => {
+    const expectedValue = getBaseExpectedValue();
+    expectedValue.requirements = MOCK.REQUIREMENTS.records;
+    const result = projectReducer(undefined, storeRequirements(MOCK.REQUIREMENTS));
+    expect(result).toEqual(expectedValue);
+  });
+
+  it("receives STORE_INFORMATION_REQUIREMENTS_TABLE", () => {
+    const expectedValue = getBaseExpectedValue();
+    expectedValue.informationRequirementsTable = MOCK.INFORMATION_REQUIREMENTS_TABLE;
+    const result = projectReducer(
+      undefined,
+      storeInformationRequirementsTable(MOCK.INFORMATION_REQUIREMENTS_TABLE)
+    );
     expect(result).toEqual(expectedValue);
   });
 });
