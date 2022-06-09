ac0606a1e33a028fbae7d9732ebe781e2632e9cd
areyeslo <arturo_reyes_lopez@hotmail.com>
2022-06-08 22:22:15 -0700

    Filter Director of Abandoned Mines and Manager of Crown Sites when role is not abandoned mines.

---
+++ migrations/sql/V2022.06.08.17.00__insert_mine_party_appt_type_codes.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.mine_party_appt_type_code
+(mine_party_appt_type_code, description, display_order, active_ind, person, organization, grouping_level, create_user, update_user)
+VALUES
+('DAM', 'Director of Abandoned Mines',5,true,true,false,3,'system-mds','system-mds'),
+('CCS','Manager, Crown Contaminated Sites',6,true,true,false,3,'system-mds','system-mds');
