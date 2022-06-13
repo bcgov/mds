ef3aeea24a714931bd18c59d70cd771bb7b34b52
Ben Harrington <Benjamin.Harrington@nttdata.com>
2022-06-13 12:18:16 -0700

    Create/update tests and snapshots

---
+++ services/core-web/src/tests/components/Forms/informationRequirementsTable/UpdateInformationRequirementsTableForm.spec.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { UpdateInformationRequirementsTableForm } from "@/components/Forms/informationRequirementsTable/UpdateInformationRequirementsTableForm";
+import * as MOCK from "@/tests/mocks/dataMocks";
+
+const props = {};
+
+const setupProps = () => {
+  props.handleSubmit = jest.fn();
+  props.formValues = { status_code: "REC" };
+  props.initialValues = {
+    status_code: "REC",
+  };
+  props.displayValues = {
+    statusCode: "REC",
+    updateUser: "test",
+    updateDate: "Jun 13 2022",
+    informationRequirementsTableStatusCodesHash:
+      MOCK.INFORMATION_REQUIREMENTS_TABLE_STATUS_CODES_HASH,
+  };
+  props.pristine = true;
+  props.dropdownInformationRequirementsTableStatusCodes =
+    MOCK.INFORMATION_REQUIREMENTS_TABLE_STATUS_CODES_DROPDOWN;
+};
+
+beforeEach(() => {
+  setupProps();
+});
+
+describe("UpdateInformationRequirementsTableForm", () => {
+  it("renders properly", () => {
+    const component = shallow(<UpdateInformationRequirementsTableForm {...props} />);
+    expect(component).toMatchSnapshot();
+  });
+});
