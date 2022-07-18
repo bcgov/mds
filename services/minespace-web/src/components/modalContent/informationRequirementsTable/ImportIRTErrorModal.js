b5280a9bc63acf41a2ab07be0d4232a17ec8cc2b
Ben Harrington <Benjamin.Harrington@nttdata.com>
2022-07-18 11:25:33 -0700

    Add handling for different error codes

--- services/minespace-web/src/components/modalContent/informationRequirementsTable/ImportIRTErrorModal.js
+++ services/minespace-web/src/components/modalContent/informationRequirementsTable/ImportIRTErrorModal.js
@@ -5,55 +5,68 @@ import Callout from "@/components/common/Callout";
 
 const propTypes = {
   closeModal: PropTypes.func.isRequired,
-  errors: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)),
+  errors: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired,
+  isBadRequestError: PropTypes.bool.isRequired,
 };
 
 export const ImportIRTErrorModal = (props) => {
+  const [copySuccess, setCopySuccess] = useState(false);
   const copyErrorToClipboard = (error) => {
     navigator.clipboard.writeText(
       error.map((e) => `Section ${e.section} Row ${e.row_number}`).join("\n")
     );
     setCopySuccess(true);
   };
-  const [copySuccess, setCopySuccess] = useState(false);
 
+  if (props.isBadRequestError) {
+    return (
+      <div>
+        <Callout
+          style={{ marginTop: 0 }}
+          severity="warning"
+          message={
+            <>
+              <b>Warnings</b>
+              <p>
+                {`"Comments" fields required when "Methods" or "Required" are checked. Please update
+                and import again.`}
+              </p>
+            </>
+          }
+        />
+        <h3 style={{ color: "black" }}>
+          <b>Required sections</b>
+        </h3>
+        <b>Check below section row number(s) on your IRT file.</b>
+        {props.errors &&
+          props.errors?.map((e) => (
+            <p>
+              Section {e.section} Row {e.row_number}
+            </p>
+          ))}
+        <br />
+        <div className="ant-modal-footer">
+          <Button
+            type="secondary"
+            style={{ float: "left" }}
+            onClick={() => copyErrorToClipboard(props.errors)}
+          >
+            Copy list to clipboard
+          </Button>
+          {copySuccess && (
+            <p style={{ color: "red", float: "left", paddingLeft: "5px" }}>Copied to clipboard.</p>
+          )}
+          <Button type="primary" onClick={props.closeModal}>
+            Close
+          </Button>
+        </div>
+      </div>
+    );
+  }
   return (
     <div>
-      <Callout
-        style={{ marginTop: 0 }}
-        severity="warning"
-        message={
-          <>
-            <b>Warnings</b>
-            <p>
-              "Comments" fields required when "Methods" or "Required" are checked. Please update and
-              import again.
-            </p>
-          </>
-        }
-      />
-      <h3 style={{ color: "black" }}>
-        <b>Required sections</b>
-      </h3>
-      <b>Check below section row number(s) on your IRT file.</b>
-      {props.errors &&
-        props.errors?.map((e) => (
-          <p>
-            Section {e.section} Row {e.row_number}
-          </p>
-        ))}
-      <br />
+      <p>{props.errors}</p>
       <div className="ant-modal-footer">
-        <Button
-          type="secondary"
-          style={{ float: "left" }}
-          onClick={(e) => copyErrorToClipboard(props.errors)}
-        >
-          Copy list to clipboard
-        </Button>
-        {copySuccess && (
-          <p style={{ color: "red", float: "left", paddingLeft: "5px" }}>Copied to clipboard.</p>
-        )}
         <Button type="primary" onClick={props.closeModal}>
           Close
         </Button>
