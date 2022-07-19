2f0202f479b465536d66d9bdb6835a366ac490c1
Ben Harrington <Benjamin.Harrington@nttdata.com>
2022-07-19 12:41:08 -0700

    Create entry page and update navigation

---
+++ services/minespace-web/src/tests/components/project/informationRequirementsTableEntryTab/InformationRequirementsTableEntryTab.spec.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { InformationRequirementsTableEntryTab } from "@/components/pages/Project/InformationRequirementsTableEntryTab";
+import * as MOCK from "@/tests/mocks/dataMocks";
+
+const props = {};
+
+const setupProps = () => {
+  props.irt = MOCK.INFORMATION_REQUIREMENTS_TABLE;
+  props.match = { params: { projectGuid: "1234-5678-x" } };
+  props.history = { push: jest.fn() };
+};
+
+beforeEach(() => {
+  setupProps();
+});
+
+describe("InformationRequirementsTableEntryTab", () => {
+  it("renders properly", () => {
+    const component = shallow(<InformationRequirementsTableEntryTab {...props} />);
+    expect(component).toMatchSnapshot();
+  });
+});
