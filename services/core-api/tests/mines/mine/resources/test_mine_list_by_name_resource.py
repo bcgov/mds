01ce72852a14f7a38b24bd6cd48fb9b25e2ff753
Mat Busby <mat@freshworks.io>
2022-07-21 16:18:43 -0600

    updated tests

--- services/core-api/tests/mines/mine/resources/test_mine_list_by_name_resource.py
+++ services/core-api/tests/mines/mine/resources/test_mine_list_by_name_resource.py
@@ -9,8 +9,6 @@ def test_get_mines_by_list(test_client, db_session, auth_headers):
     get_resp = test_client.get('/mines/search', headers=auth_headers['full_auth_header'])
     get_data = json.loads(get_resp.data.decode())
     context = {
-        'deleted_ind': mine.deleted_ind,
-        'major_mine_ind': mine.major_mine_ind,
         'mine_guid': str(mine.mine_guid),
         'mine_name': mine.mine_name,
         'mine_no': mine.mine_no,
@@ -18,8 +16,9 @@ def test_get_mines_by_list(test_client, db_session, auth_headers):
         'longitude': str(mine.longitude),
         'mine_location_description': mine.mine_location_description,
     }
+
     assert get_resp.status_code == 200
-    assert get_data['mines'] == []
+    assert get_data['mines'][0] == context
 
 
 def test_get_mines_by_list_search_by_name(test_client, db_session, auth_headers):
@@ -29,8 +28,6 @@ def test_get_mines_by_list_search_by_name(test_client, db_session, auth_headers)
         f'/mines/search?name={mine.mine_name}', headers=auth_headers['full_auth_header'])
     get_data = json.loads(get_resp.data.decode())
     context = {
-        'deleted_ind': mine.deleted_ind,
-        'major_mine_ind': mine.major_mine_ind,
         'mine_guid': str(mine.mine_guid),
         'mine_name': mine.mine_name,
         'mine_no': mine.mine_no,
@@ -39,7 +36,7 @@ def test_get_mines_by_list_search_by_name(test_client, db_session, auth_headers)
         'mine_location_description': mine.mine_location_description,
     }
     assert get_resp.status_code == 200
-    assert get_data['mines'] == []
+    assert get_data['mines'][0] == context
     
 
 
