914f3d4a3c4564c47af505076f4623ed61adfaa3
Ben Harrington <Benjamin.Harrington@nttdata.com>
2022-07-25 08:46:03 -0700

    Update copyErrorClipboard to support multiple error types

--- services/minespace-web/src/components/modalContent/informationRequirementsTable/ImportIRTErrorModal.js
+++ services/minespace-web/src/components/modalContent/informationRequirementsTable/ImportIRTErrorModal.js
@@ -3,6 +3,9 @@ import PropTypes from "prop-types";
 import { Button } from "antd";
 import Callout from "@/components/common/Callout";
 
+const METHOD_TRUE_NEED_COMMENTS = "METHOD_TRUE_NEED_COMMENTS";
+const INFORMATION_CELL_INVALID = "INFORMATION_CELL_INVALID";
+
 const propTypes = {
   closeModal: PropTypes.func.isRequired,
   errors: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired,
@@ -11,27 +14,44 @@ const propTypes = {
 
 export const ImportIRTErrorModal = (props) => {
   const [copySuccess, setCopySuccess] = useState(false);
-  const copyErrorToClipboard = (error) => {
-    navigator.clipboard.writeText(
-      error.map((e) => `Section ${e.section} Row ${e.row_number}`).join("\n")
-    );
+  const copyErrorToClipboard = (errors) => {
+    const informationCellInvalidErrors = errors.filter((e) => e.error === INFORMATION_CELL_INVALID);
+    const informationCellErrorMapMessage = informationCellInvalidErrors
+      .map((e) => `Section ${e.section} Row ${e.row_number}`)
+      .join("\n");
+    const commentsNeededErrors = errors.filter((e) => e.error === METHOD_TRUE_NEED_COMMENTS);
+    const commentsNeededErrorMapMessage = commentsNeededErrors
+      .map((e) => `Section ${e.section} Row ${e.row_number}`)
+      .join("\n");
+    const commentsNeededFormattedError = commentsNeededErrors.length
+      ? `Required fields - Comments are required when "Methods are checked:\n\n${commentsNeededErrorMapMessage}`
+      : "";
+    let informationCellFormattedError = informationCellInvalidErrors.length
+      ? `Additional fields - Remove self added fields:\n\n${informationCellErrorMapMessage}`
+      : "";
+    if (informationCellInvalidErrors.length && commentsNeededErrors.length) {
+      informationCellFormattedError = `\n\n${informationCellFormattedError}`;
+    }
+    const formattedErrorMessage = commentsNeededFormattedError + informationCellFormattedError;
+
+    navigator.clipboard.writeText(formattedErrorMessage);
     setCopySuccess(true);
   };
 
   const renderErrorSection = (section, errors = []) => {
     let title = null;
     let description = null;
-    let errorSection = errors.filter((e) => e.error === section);
+    const errorSection = errors.filter((e) => e.error === section);
 
-    if (section === "INFORMATION_CELL_INVALID") {
+    if (section === INFORMATION_CELL_INVALID) {
       title = "Additional Fields";
-      description = "";
-    } else if (section === "METHOD_TRUE_NEEDS_COMMENTS") {
+      description = "Remove self added fields.";
+    } else if (section === METHOD_TRUE_NEED_COMMENTS) {
       title = "Required fields";
       description = "Comments are required when 'Methods' are checked.";
     }
 
-    if (!errorSection.length) {
+    if (errorSection.length === 0) {
       return null;
     }
     return (
@@ -43,7 +63,7 @@ export const ImportIRTErrorModal = (props) => {
         <br />
         {errorSection.map((es) => (
           <p>
-            Section {e.section} Row {e.row_number}
+            Section {es.section} Row {es.row_number}
           </p>
         ))}
       </>
@@ -63,8 +83,8 @@ export const ImportIRTErrorModal = (props) => {
             </>
           }
         />
-        {renderErrorSection("METHOD_TRUE_NEEDS_COMMENTS", props.errors)}
-        {renderErrorSection("INFORMATION_CELL_INVALID", props.errors)}
+        {renderErrorSection(METHOD_TRUE_NEED_COMMENTS, props.errors)}
+        {renderErrorSection(INFORMATION_CELL_INVALID, props.errors)}
         <br />
         <div className="ant-modal-footer">
           <Button
