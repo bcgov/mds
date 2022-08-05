28d273fb8bc142a661334b4308a9c3c83e6693c1
Ben Harrington <Benjamin.Harrington@nttdata.com>
2022-08-05 11:39:52 -0700

    Create review page component

---
+++ services/minespace-web/src/components/Forms/projects/majorMineApplication/MajorMineApplicationReviewSubmit.js
@@ -0,0 +1,162 @@
+import React from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { CALLOUT_SEVERITY } from "@common/constants/strings";
+import { Checkbox, Row, Col, Typography, Descriptions, Card, Input } from "antd";
+import PropTypes from "prop-types";
+import * as Strings from "@common/constants/strings";
+import {
+  removeDocumentFromMajorMineApplication,
+  fetchProjectById,
+} from "@common/actionCreators/projectActionCreator";
+import Callout from "@/components/common/Callout";
+import DocumentTable from "@/components/common/DocumentTable";
+import CustomPropTypes from "@/customPropTypes";
+
+const propTypes = {
+  project: CustomPropTypes.project.isRequired,
+  confirmedSubmission: PropTypes.bool.isRequired,
+  removeDocumentFromMajorMineApplication: PropTypes.func.isRequired,
+  fetchProjectById: PropTypes.func.isRequired,
+  setConfirmedSubmission: PropTypes.func.isRequired,
+};
+
+const inputStyle = { width: "100%" };
+
+export const MajorMineApplicationReviewSubmit = ({
+  project,
+  removeDocumentFromMajorMineApplication: removeDocument,
+  fetchProjectById: fetchProject,
+  confirmedSubmission,
+  setConfirmedSubmission,
+}) => {
+  const handleDeleteDocument = async ({
+    projectGuid,
+    majorMineApplicationGuid,
+    mineDocumentGuid,
+  }) => {
+    await removeDocument(projectGuid, majorMineApplicationGuid, mineDocumentGuid);
+    return fetchProject(projectGuid);
+  };
+
+  const {
+    project_guid: projectGuid,
+    contacts,
+    major_mine_application: { major_mine_application_guid: majorMineApplicationGuid },
+  } = project;
+
+  const primaryContact = contacts?.find((c) => c.is_primary) || {};
+
+  const primaryDocuments = project.major_mine_application?.documents?.filter(
+    (d) => d.major_mine_application_document_type_code === "PRM"
+  );
+  const spatialDocuments = project.major_mine_application?.documents?.filter(
+    (d) => d.major_mine_application_document_type_code === "SPT"
+  );
+  const supportDocuments = project.major_mine_application?.documents?.filter(
+    (d) => d.major_mine_application_document_type_code === "SPR"
+  );
+
+  return (
+    <>
+      <Callout
+        message={
+          <div>
+            <h4 style={{ fontWeight: "bold" }}>Confirm your Submission</h4>
+            <p>
+              Please confirm the contents of your submission below. When you are happy with your
+              review click the submit button to begin the review process.
+            </p>
+          </div>
+        }
+        severity={CALLOUT_SEVERITY.warning}
+      />
+      <br />
+      <Typography.Title level={4}>Basic Information</Typography.Title>
+      <Row>
+        <Col xs={24} md={12}>
+          <Descriptions layout="vertical" colon={false}>
+            <Descriptions.Item
+              style={inputStyle}
+              span={12}
+              label="Primary Contact"
+              className="vertical-description"
+            >
+              <Input value={primaryContact?.name || Strings.EMPTY_FIELD} disabled />
+            </Descriptions.Item>
+          </Descriptions>
+        </Col>
+        <Col xs={24} md={12}>
+          <Descriptions layout="vertical" colon={false}>
+            <Descriptions.Item
+              style={inputStyle}
+              span={12}
+              label="Mine Name"
+              className="vertical-description"
+            >
+              <Input value={project?.mine_name || Strings.EMPTY_FIELD} disabled />
+            </Descriptions.Item>
+          </Descriptions>
+        </Col>
+        <Col span={24}>
+          <Typography.Title level={4}>Primary Document</Typography.Title>
+          <DocumentTable
+            documents={primaryDocuments}
+            documentCategoryOptionsHash={{}}
+            documentParent="Major Mine Application"
+            categoryDataIndex="major_mine_application_document_type_code"
+            uploadDateIndex="upload_date"
+            handleDeleteDocument={handleDeleteDocument}
+            deletePayload={{ projectGuid, majorMineApplicationGuid }}
+          />
+          <Typography.Title level={4}>Spatial Components</Typography.Title>
+          <DocumentTable
+            documents={spatialDocuments}
+            documentCategoryOptionsHash={{}}
+            documentParent="Major Mine Application"
+            categoryDataIndex="major_mine_application_document_type_code"
+            uploadDateIndex="upload_date"
+            handleDeleteDocument={handleDeleteDocument}
+            deletePayload={{ projectGuid, majorMineApplicationGuid }}
+          />
+          <Typography.Title level={4}>Supporting Documents</Typography.Title>
+          <DocumentTable
+            documents={supportDocuments}
+            documentCategoryOptionsHash={{}}
+            documentParent="Major Mine Application"
+            categoryDataIndex="major_mine_application_document_type_code"
+            uploadDateIndex="upload_date"
+            handleDeleteDocument={handleDeleteDocument}
+            deletePayload={{ projectGuid, majorMineApplicationGuid }}
+          />
+        </Col>
+        <Col span={24}>
+          <Card>
+            <>
+              <p>
+                <b>Confirmation of Submission</b>
+              </p>
+              <p>
+                <span>
+                  <Checkbox checked={confirmedSubmission} onChange={setConfirmedSubmission} />
+                  &nbsp;&nbsp;
+                </span>
+                I understand that this application and supporting files are submitted on behalf of
+                the owner, agent or mine manager of this project.
+                <span style={{ color: "red" }}>*</span>
+              </p>
+            </>
+          </Card>
+          <br />
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+MajorMineApplicationReviewSubmit.propTypes = propTypes;
+
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({ removeDocumentFromMajorMineApplication, fetchProjectById }, dispatch);
+
+export default connect(null, mapDispatchToProps)(MajorMineApplicationReviewSubmit);
--- services/minespace-web/src/routes/Routes.js
+++ services/minespace-web/src/routes/Routes.js
@@ -4,7 +4,6 @@ import { ExclamationCircleOutlined } from "@ant-design/icons";
 import { Result, Button, Typography } from "antd";
 import AuthenticationGuard from "@/HOC/AuthenticationGuard";
 import * as routes from "@/constants/routes";
-import * as Permission from "@/constants/permissions";
 
 const Routes = () => (
   <Switch>
@@ -60,10 +59,12 @@ const Routes = () => (
       component={AuthenticationGuard()(routes.EDIT_MAJOR_MINE_APPLICATION.component)}
     />
     <Route
+      path={routes.REVIEW_MAJOR_MINE_APPLICATION.route}
+      component={AuthenticationGuard()(routes.REVIEW_MAJOR_MINE_APPLICATION.component)}
+    />
+    <Route
       path={routes.MAJOR_MINE_APPLICATION_SUCCESS.route}
-      component={AuthenticationGuard(Permission.IN_TESTING)(
-        routes.MAJOR_MINE_APPLICATION_SUCCESS.component
-      )}
+      component={AuthenticationGuard()(routes.MAJOR_MINE_APPLICATION_SUCCESS.component)}
     />
     <Route
       path={routes.EDIT_PROJECT.route}
