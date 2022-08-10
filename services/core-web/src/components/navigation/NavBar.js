60467dd5cd88e4154af4784848d46a47a695e038
Mat Busby <mat@freshworks.io>
2022-08-10 16:36:28 -0600

    Added feature flag and missing import to notification drawer

--- services/core-web/src/components/navigation/NavBar.js
+++ services/core-web/src/components/navigation/NavBar.js
@@ -21,6 +21,7 @@ import AuthorizationWrapper from "@/components/common/wrappers/AuthorizationWrap
 import SearchBar from "@/components/search/SearchBar";
 import { LOGO, HAMBURGER, CLOSE, SUCCESS_CHECKMARK, YELLOW_HAZARD } from "@/constants/assets";
 import NotificationDrawer from "@/components/navigation/NotificationDrawer";
+import { detectProdEnvironment as IN_PROD } from "@common/utils/environmentUtils";
 
 /**
  * @class NavBar - fixed and responsive navigation
@@ -477,7 +478,7 @@ export class NavBar extends Component {
               </Button>
             </MediaQuery>
             <MediaQuery minWidth={fullNavMinWidth}>{this.renderFullNav()}</MediaQuery>
-            <NotificationDrawer />
+            {!IN_PROD() && <NotificationDrawer />}
           </div>
         </div>
         <MediaQuery maxWidth={fullNavMinWidth - 1}>{this.renderHamburgerNav()}</MediaQuery>
