f518346b2326253a89b95aaf432e9bbb79bb2e64
Ben Harrington <Benjamin.Harrington@nttdata.com>
2022-10-06 10:00:54 -0700

    Add prop support, update uploadUrl

--- services/core-web/src/components/Forms/incidents/IncidentFileUpload.js
+++ services/core-web/src/components/Forms/incidents/IncidentFileUpload.js
@@ -3,33 +3,41 @@ import PropTypes from "prop-types";
 import { Field } from "redux-form";
 import { Form } from "@ant-design/compatible";
 import "@ant-design/compatible/assets/index.css";
-import { MINE_INCIDENT } from "@common/constants/API";
+import { MINE_INCIDENT_DOCUMENTS } from "@common/constants/API";
 import FileUpload from "@/components/common/FileUpload";
-import { DOCUMENT, EXCEL } from "@/constants/fileTypes";
+import { DOCUMENT, EXCEL, SPATIAL } from "@/constants/fileTypes";
 
 const propTypes = {
   onFileLoad: PropTypes.func.isRequired,
   onRemoveFile: PropTypes.func.isRequired,
   mineGuid: PropTypes.string.isRequired,
-  allowMultiple: PropTypes.bool.isRequired,
+  labelIdle: PropTypes.string,
 };
+
+const defaultProps = {
+  labelIdle: null,
+};
+
+const acceptedFileTypesMap = { ...DOCUMENT, ...EXCEL, ...SPATIAL };
 
 export const IncidentFileUpload = (props) => (
   <Form.Item>
     <Field
       id="fileUpload"
       name="fileUpload"
+      labelIdle={props?.labelIdle}
       component={FileUpload}
-      uploadUrl={`${MINE_INCIDENT()}/documents?mine_guid=${props.mineGuid}`}
-      acceptedFileTypesMap={{ ...DOCUMENT, ...EXCEL }}
+      uploadUrl={MINE_INCIDENT_DOCUMENTS(props.mineGuid)}
+      acceptedFileTypesMap={acceptedFileTypesMap}
       onFileLoad={props.onFileLoad}
       onRemoveFile={props.onRemoveFile}
       allowRevert
-      allowMultiple={props.allowMultiple}
+      allowMultiple
     />
   </Form.Item>
 );
 
 IncidentFileUpload.propTypes = propTypes;
+IncidentFileUpload.defaultProps = defaultProps;
 
 export default IncidentFileUpload;
