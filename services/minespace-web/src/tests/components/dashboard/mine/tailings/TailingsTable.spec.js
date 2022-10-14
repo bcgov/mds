d9c84b3201b04154245de66f0964cf39a0d0e8b9
Mat Busby <83598933+matbusby-fw@users.noreply.github.com>
2022-10-14 10:33:04 -0600

    [MDS-4697] - Update TSF Table (#2295)
    updated tsf table with nested dams

--- services/minespace-web/src/tests/components/dashboard/mine/tailings/TailingsTable.spec.js
+++ services/minespace-web/src/tests/components/dashboard/mine/tailings/TailingsTable.spec.js
@@ -1,13 +1,24 @@
-import React from "react";
-import { shallow } from "enzyme";
-import { TailingsTable } from "@/components/dashboard/mine/tailings/TailingsTable";
 import * as MOCK from "@/tests/mocks/dataMocks";
 
-const props = {};
-const dispatchProps = {};
+import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import { TailingsTable } from "@/components/dashboard/mine/tailings/TailingsTable";
+import { shallow } from "enzyme";
+
+const props = {
+  editTailings: jest.fn(),
+  itrmExemptionStatusCodeHash: {},
+  handleEditTailings: jest.fn(),
+  openEditTailingsModal: jest.fn(),
+};
+const dispatchProps = {
+  storeDam: jest.fn(),
+};
 
 const setupProps = () => {
-  props.tailings = MOCK.MINES.mines["18133c75-49ad-4101-85f3-a43e35ae989a"].mine_tailings_storage_facilities;
+  props.match = { params: { id: "18133c75-49ad-4101-85f3-a43e35ae989a" } };
+  props.tailings =
+    MOCK.MINES.mines["18133c75-49ad-4101-85f3-a43e35ae989a"].mine_tailings_storage_facilities;
   props.TSFOperatingStatusCodeHash = {};
   props.consequenceClassificationStatusCodeHash = {};
 };
@@ -18,7 +29,11 @@ beforeEach(() => {
 
 describe("TailingsTable", () => {
   it("renders properly", () => {
-    const component = shallow(<TailingsTable {...props} {...dispatchProps} />);
+    const component = shallow(
+      <BrowserRouter>
+        <TailingsTable {...props} {...dispatchProps} />
+      </BrowserRouter>
+    );
     expect(component).toMatchSnapshot();
   });
 });
