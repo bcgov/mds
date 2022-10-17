85accd0d328701fec3a64adb1f20abeb43c48a8f
Simen Fivelstad Smaaberg <simen@freshworks.io>
2022-10-17 16:18:26 -0700

    MDS-4625 Changed location of tsfs in core to under Permits & Approvals tab

---
+++ services/core-web/src/components/mine/Tailings/MineReportTailingsInfo.js
@@ -0,0 +1,6 @@
+import React from "react";
+import MineTailingsInfoTabs from "./MineTailingsInfoTabs";
+
+const MineReportTailingsInfo = () => <MineTailingsInfoTabs enabledTabs={["reports", "map"]} />;
+
+export default MineReportTailingsInfo;
