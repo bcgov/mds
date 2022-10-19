6bb9478b7ab8cb9b09e48bc8501662981c0a7d02
Mat Busby <mat.busby@ca.ey.com>
2022-10-19 13:24:08 -0600

    refined query to delete_now

--- services/core-api/sql/delete_now_record.sql
+++ services/core-api/sql/delete_now_record.sql
@@ -20,22 +20,22 @@ CREATE OR REPLACE FUNCTION delete_now(_now_number varchar) RETURNS VOID AS $$
 
 DECLARE
     -- Declare the variables.
-    _now_application_id bigint;
+_now_application_id bigint;
     _now_application_guid uuid;
-	now_application_ids integer[];
-	activity_summary_ids integer[];
+    now_application_ids integer[];
+    activity_summary_ids integer[];
 
 BEGIN
 
--- Get the now_application_id.
+    -- Get the now_application_id.
 SELECT now_application_id INTO _now_application_id
-      FROM now_application_identity
-      WHERE now_number = _now_number;
+FROM now_application_identity
+WHERE now_number = _now_number;
 
 -- Get the now_application_guid.
 SELECT now_application_guid INTO _now_application_guid
-    FROM now_application_identity
-    WHERE now_application_id = _now_application_id;
+FROM now_application_identity
+WHERE now_application_id = _now_application_id;
 
 -- Delete the records associated with Notices of Work.
 RAISE NOTICE 'Deleting records associated with Notices of Work';
@@ -59,7 +59,22 @@ WHERE now_application_id = _now_application_id;
 DELETE FROM settling_pond
 WHERE activity_summary_id = ANY(activity_summary_ids);
 
-DELETE FROM activity_summary
+DELETE FROM camp
+WHERE activity_summary_id = ANY(activity_summary_ids);
+
+DELETE FROM exploration_surface_drilling
+WHERE activity_summary_id = ANY(activity_summary_ids);
+
+DELETE FROM exploration_access
+WHERE activity_summary_id = ANY(activity_summary_ids);
+
+DELETE FROM sand_gravel_quarry_operation
+WHERE activity_summary_id = ANY(activity_summary_ids);
+
+DELETE FROM surface_bulk_sample
+WHERE activity_summary_id = ANY(activity_summary_ids);
+
+DELETE FROM underground_exploration
 WHERE activity_summary_id = ANY(activity_summary_ids);
 
 DELETE FROM state_of_land
@@ -77,16 +92,22 @@ WHERE now_application_id = _now_application_id;
 DELETE FROM now_application_review
 WHERE now_application_id = _now_application_id;
 
+DELETE FROM now_application_identity
+WHERE now_application_guid = _now_application_guid;
+
 DELETE FROM now_application
 WHERE now_application_id = _now_application_id;
+
+DELETE FROM activity_summary
+WHERE activity_summary_id = ANY(activity_summary_ids);
+
 
 -- Delete the record.
 RAISE NOTICE 'Deleting the record';
 
-DELETE FROM now_application_identity
-WHERE now_application_guid = _now_application_guid;
 
-RAISE NOTICE 'Successfully deleted all records';
+
+    RAISE NOTICE 'Successfully deleted all records';
 END;
 
 $$ LANGUAGE PLPGSQL;
@@ -94,6 +115,8 @@ $$ LANGUAGE PLPGSQL;
 -- Call the function.
 -- NOTE: Manually check/add the records to delete here before running this script.
 -- SELECT delete_now('1640544-2021-01');
+
+select delete_now('11');
 
 -- Drop the function.
 DROP FUNCTION delete_now(varchar, varchar);
