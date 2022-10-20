e7005b2bff64b7eae6b8715c309028517e039b20
wilson496 <Cameron.Wilson@nttdata.com>
2022-10-20 13:26:58 -0700

    [FIX]  - add GHA token

--- gitops/clear_caches.sh
+++ gitops/clear_caches.sh
@@ -3,9 +3,11 @@ set -e
 
 # Run this to clear 100 cache entries in GHA
 
+GH_TOKEN=${1?"Enter GitHub Actions Token !"}
+
 curl \
     -H "Accept: application/vnd.github.v3+json" \
-    -H "Authorization: token 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" \
+    -H "Authorization: token $GH_TOKEN" \
     https://api.github.com/repos/bcgov/mds/actions/caches?per_page=100 | jq '.["actions_caches"][] | .id' \
     | 
     while IFS= read -r cache_id; do
