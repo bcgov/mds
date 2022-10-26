73cf4ba156647060875a31faf08fb69986b4507c
Vyas <67800715+vyasworks@users.noreply.github.com>
2022-10-26 16:00:19 -0700

    [MDS-4745] Minespace Version Fixes - Part 2 (#2306)

--- services/minespace-web/src/tests/components/Forms/incidents/IncidentForm.spec.js
+++ services/minespace-web/src/tests/components/Forms/incidents/IncidentForm.spec.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
-import IncidentForm from "@/components/Forms/incidents/IncidentForm";
+import { IncidentForm } from "@/components/Forms/incidents/IncidentForm";
 import * as MOCK from "@/tests/mocks/dataMocks";
 
 const dispatchProps = {};
@@ -11,6 +11,7 @@ const setupProps = () => {
   props.incidentCategoryCodeOptions = MOCK.INCIDENT_CATEGORY_CODE_OPTIONS;
   props.formValues = { ...MOCK.INCIDENT };
   props.handlers = { deleteDocument: jest.fn(() => Promise.resolve()) };
+  props.match = { params: { mine_guid: "18133c75-49ad-4101-85f3-a43e35ae989a" } };
 };
 
 const setupDispatchProps = () => {
