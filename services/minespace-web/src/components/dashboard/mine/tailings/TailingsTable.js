e2462044efc0a4ce4dc659bc7340ae393140d378
Mat Busby <83598933+matbusby-fw@users.noreply.github.com>
2022-10-27 14:40:04 -0600

    [MDS-4815] TSF Bugs (#2319)
    * Addressed QA bugs for tsfs in minespace and core
    
    * Removed unused-vars flags and restructured props

--- services/minespace-web/src/components/dashboard/mine/tailings/TailingsTable.js
+++ services/minespace-web/src/components/dashboard/mine/tailings/TailingsTable.js
@@ -1,20 +1,15 @@
-import * as Strings from "@/constants/strings";
-
 import { Button, Table, Typography } from "antd";
 import {
   CONSEQUENCE_CLASSIFICATION_CODE_HASH,
   DAM_OPERATING_STATUS_HASH,
+  EMPTY_FIELD,
 } from "@common/constants/strings";
 import {
-  getConsequenceClassificationStatusCodeOptionsHash,
   getITRBExemptionStatusCodeOptionsHash,
   getTSFOperatingStatusCodeOptionsHash,
 } from "@common/selectors/staticContentSelectors";
 import { useHistory, useParams } from "react-router-dom";
 
-import AuthorizationWrapper from "@/components/common/wrappers/AuthorizationWrapper";
-import { EDIT_DAM } from "@/constants/routes";
-import { EDIT_PENCIL } from "@/constants/assets";
 import { detectProdEnvironment as IN_PROD } from "@common/utils/environmentUtils";
 import PropTypes from "prop-types";
 import React from "react";
@@ -23,18 +18,15 @@ import { connect } from "react-redux";
 import { getHighestConsequence } from "@common/utils/helpers";
 import { storeDam } from "@common/actions/damActions";
 import { storeTsf } from "@common/actions/tailingsActions";
+import { EDIT_PENCIL } from "@/constants/assets";
+import { EDIT_DAM } from "@/constants/routes";
+import AuthorizationWrapper from "@/components/common/wrappers/AuthorizationWrapper";
 
 const propTypes = {
   tailings: PropTypes.arrayOf(PropTypes.any).isRequired,
-  // eslint-disable-next-line react/no-unused-prop-types
   openEditTailingsModal: PropTypes.func.isRequired,
-  // eslint-disable-next-line react/no-unused-prop-types
   handleEditTailings: PropTypes.func.isRequired,
-  // eslint-disable-next-line react/no-unused-prop-types
   TSFOperatingStatusCodeHash: PropTypes.objectOf(PropTypes.string).isRequired,
-  // eslint-disable-next-line react/no-unused-prop-types
-  consequenceClassificationStatusCodeHash: PropTypes.objectOf(PropTypes.string).isRequired,
-  // eslint-disable-next-line react/no-unused-prop-types
   itrmExemptionStatusCodeHash: PropTypes.objectOf(PropTypes.string).isRequired,
   editTailings: PropTypes.func.isRequired,
   storeDam: PropTypes.func.isRequired,
@@ -45,7 +37,14 @@ export const TailingsTable = (props) => {
   const history = useHistory();
   const { id: mineGuid } = useParams();
   const [expandedRows, setExpandedRows] = React.useState([]);
-  const { editTailings, tailings } = props;
+  const {
+    editTailings,
+    tailings,
+    openEditTailingsModal,
+    handleEditTailings,
+    TSFOperatingStatusCodeHash,
+    itrmExemptionStatusCodeHash,
+  } = props;
 
   const handleEditDam = (event, dam) => {
     event.preventDefault();
@@ -84,9 +83,7 @@ export const TailingsTable = (props) => {
       title: "Operating Status",
       dataIndex: "tsf_operating_status_code",
       render: (text) => (
-        <div title="Operating Status">
-          {props.TSFOperatingStatusCodeHash[text] || Strings.EMPTY_FIELD}
-        </div>
+        <div title="Operating Status">{TSFOperatingStatusCodeHash[text] || EMPTY_FIELD}</div>
       ),
       sorter: (a, b) => (a.tsf_operating_status_code > b.tsf_operating_status_code ? -1 : 1),
     },
@@ -104,7 +101,7 @@ export const TailingsTable = (props) => {
       dataIndex: "itrb_exemption_status_code",
       render: (text) => (
         <div title="Has Independent Tailings Review Board?">
-          {props.itrmExemptionStatusCodeHash[text] || Strings.EMPTY_FIELD}
+          {itrmExemptionStatusCodeHash[text] || EMPTY_FIELD}
         </div>
       ),
       sorter: (a, b) => (a.itrb_exemption_status_code > b.itrb_exemption_status_code ? -1 : 1),
@@ -119,31 +116,38 @@ export const TailingsTable = (props) => {
       title: "Engineer of Record",
       dataIndex: "engineer_of_record",
       render: (text) => (
-        <div title="Engineer of Record">{text ? text.party.name : Strings.EMPTY_FIELD}</div>
+        <div title="Engineer of Record">{text ? text.party.name : EMPTY_FIELD}</div>
       ),
       sorter: (a, b) => (a.engineer_of_record > b.engineer_of_record ? -1 : 1),
     },
     {
+      title: "Qualified Person",
+      dataIndex: "qualified_person",
+      render: (text) => <div title="Qualified Person">{text ? text.party.name : EMPTY_FIELD}</div>,
+      sorter: (a, b) => (a.qualified_person > b.qualified_person ? -1 : 1),
+    },
+    {
       title: "Latitude",
       dataIndex: "latitude",
-      render: (text) => <div title="Latitude">{text || Strings.EMPTY_FIELD}</div>,
+      render: (text) => <div title="Latitude">{text || EMPTY_FIELD}</div>,
       sorter: (a, b) => (a.latitude > b.latitude ? -1 : 1),
     },
     {
       title: "Longitude",
       dataIndex: "longitude",
-      render: (text) => <div title="Longitude">{text || Strings.EMPTY_FIELD}</div>,
+      render: (text) => <div title="Longitude">{text || EMPTY_FIELD}</div>,
       sorter: (a, b) => (a.longitude > b.longitude ? -1 : 1),
     },
     {
       title: "Notes",
       dataIndex: "notes",
-      render: (text) => <div title="Notes">{text || Strings.EMPTY_FIELD}</div>,
+      render: (text) => <div title="Notes">{text || EMPTY_FIELD}</div>,
       sorter: (a, b) => (a.notes > b.notes ? -1 : 1),
     },
     {
       title: "",
       dataIndex: "edit",
+      fixed: "right",
       render: (text, record) => {
         return (
           <div title="" align="right">
@@ -155,9 +159,7 @@ export const TailingsTable = (props) => {
               ) : (
                 <Button
                   type="link"
-                  onClick={(event) =>
-                    props.openEditTailingsModal(event, props.handleEditTailings, record)
-                  }
+                  onClick={(event) => openEditTailingsModal(event, handleEditTailings, record)}
                 >
                   <img src={EDIT_PENCIL} alt="Edit" />
                 </Button>
@@ -190,6 +192,7 @@ export const TailingsTable = (props) => {
       },
       {
         title: "",
+        fixed: "right",
         dataIndex: "edit",
         render: (text, record) => {
           return (
@@ -246,7 +249,6 @@ const mapDispatchToProps = (dispatch) => bindActionCreators({ storeDam, storeTsf
 
 const mapStateToProps = (state) => ({
   TSFOperatingStatusCodeHash: getTSFOperatingStatusCodeOptionsHash(state),
-  consequenceClassificationStatusCodeHash: getConsequenceClassificationStatusCodeOptionsHash(state),
   itrmExemptionStatusCodeHash: getITRBExemptionStatusCodeOptionsHash(state),
 });
 
