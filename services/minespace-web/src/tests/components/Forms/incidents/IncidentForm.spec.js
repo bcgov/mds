cbe8effde58156fd5556031966daa09800e3c624
Ben Harrington <Benjamin.Harrington@nttdata.com>
2022-11-15 15:08:37 -0800

    Update test spec and snap

--- services/minespace-web/src/tests/components/Forms/incidents/IncidentForm.spec.js
+++ services/minespace-web/src/tests/components/Forms/incidents/IncidentForm.spec.js
@@ -12,6 +12,10 @@ const setupProps = () => {
   props.formValues = { ...MOCK.INCIDENT };
   props.handlers = { deleteDocument: jest.fn(() => Promise.resolve()) };
   props.match = { params: { mine_guid: "18133c75-49ad-4101-85f3-a43e35ae989a" } };
+  props.inspectorOptions = [
+    { groupName: "Active", opt: [] },
+    { groupName: "Inactive", opt: [] },
+  ];
 };
 
 const setupDispatchProps = () => {
