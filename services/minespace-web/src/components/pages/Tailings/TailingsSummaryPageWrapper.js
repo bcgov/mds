6a24f2bb364213a8cd856ae15d2bb248e92ed4b7
Simen Fivelstad Smaaberg <66635118+simensma-fresh@users.noreply.github.com>
2022-12-05 07:49:20 -0800

    [MDS-4610] Fixed loading / refresh of EOR history list (#2381)
    * MDS-4610 Fixed loading / refresh of EOR history list
    
    * MDS-4610 Fixed liniting issues

--- services/minespace-web/src/components/pages/Tailings/TailingsSummaryPageWrapper.js
+++ services/minespace-web/src/components/pages/Tailings/TailingsSummaryPageWrapper.js
@@ -3,6 +3,7 @@ import React from "react";
 import TailingsProvider from "@common/components/tailings/TailingsProvider";
 import ContactDetails from "@common/components/ContactDetails";
 import TailingsSummaryPage from "@common/components/tailings/TailingsSummaryPage";
+import PropTypes from "prop-types";
 import { renderConfig } from "@/components/common/config";
 import LinkButton from "@/components/common/LinkButton";
 import { modalConfig } from "@/components/modalContent/config";
@@ -13,17 +14,17 @@ import {
   EDIT_TAILINGS_STORAGE_FACILITY,
   MINE_DASHBOARD,
 } from "@/constants/routes";
-import PropTypes from "prop-types";
 
 const propTypes = {
   match: PropTypes.shape({
     params: PropTypes.shape({
       id: PropTypes.string,
+      mineGuid: PropTypes.string,
       tailingsStorageFacilityGuid: PropTypes.string,
       tab: PropTypes.string,
     }),
   }).isRequired,
-}
+};
 
 export const TailingsSummaryPageWrapper = (props) => {
   const { match } = props;
@@ -45,15 +46,16 @@ export const TailingsSummaryPageWrapper = (props) => {
       renderConfig={renderConfig}
       addContactModalConfig={modalConfig.ADD_CONTACT}
       tsfFormName={FORM.ADD_TAILINGS_STORAGE_FACILITY}
+      tsfGuid={match.params.tailingsStorageFacilityGuid}
       canAssignEor
       routes={routes}
       eorHistoryColumns={["name", "status", "dates", "letters"]}
     >
       <TailingsSummaryPage
-          form={FORM.ADD_TAILINGS_STORAGE_FACILITY}
-          mineGuid={match.params.mineGuid}
-          tsfGuid={match.params.tailingsStorageFacilityGuid}
-          tab={match.params.tab}
+        form={FORM.ADD_TAILINGS_STORAGE_FACILITY}
+        mineGuid={match.params.mineGuid}
+        tsfGuid={match.params.tailingsStorageFacilityGuid}
+        tab={match.params.tab}
       />
     </TailingsProvider>
   );
