c3d576f37df5c39728731752a9ce06641dac2900
Tara Epp <102187683+taraepp@users.noreply.github.com>
2023-01-05 23:03:52 +0000

    removed conflicting data from 'after migrate' script, updated data mocks to have correct consequence classifications too

--- migrations/sql/afterMigrate__1__seed_data.sql
+++ migrations/sql/afterMigrate__1__seed_data.sql
@@ -1571,23 +1571,6 @@ VALUES
 	('LIS', 'List Item', true, 30, 'system-mds', 'system-mds')
 on conflict do nothing;
 
-INSERT INTO consequence_classification_status
-    (
-    consequence_classification_status_code,
-    description,
-    display_order,
-    create_user,
-    update_user
-    )
-VALUES
-    ('LOW', 'Low', 10, 'system-mds', 'system-mds'),
-    ('SIG', 'Significant', 20, 'system-mds', 'system-mds'),
-    ('HIG', 'High', 30, 'system-mds', 'system-mds'),
-    ('VHIG', 'Very High', 35, 'system-mds', 'system-mds'),
-    ('EXT', 'Extreme', 50, 'system-mds', 'system-mds'),
-    ('NOD', 'N/A (No Dam)', 60, 'system-mds', 'system-mds')
-ON CONFLICT DO NOTHING;
-
 INSERT INTO tsf_operating_status
     (
     tsf_operating_status_code,
