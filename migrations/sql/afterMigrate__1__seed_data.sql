e592d127096065760e327467ec414d4dd3f750c8
Tara Epp <102187683+taraepp@users.noreply.github.com>
2023-01-06 12:43:11 -0600

    [MDS-4883] consequence classification order (#2400)
    * Change order of consequence in FE enums
    
    * removed migration from afterMigrate script that was undoing my migration (it's possible that this has been adding back a value we've intended to remove) added the display_order to come from the BE for CC so we can potentially get all our info from one place. But also updated some FE enums that were used to determine the severity scale
    
    * migrating NOD --> NRT for tsf records
    
    * fixed data mocks for related tests
    
    * update test snapshot to match data

--- migrations/sql/afterMigrate__1__seed_data.sql
+++ migrations/sql/afterMigrate__1__seed_data.sql
@@ -1571,30 +1571,6 @@ VALUES
 	('LIS', 'List Item', true, 30, 'system-mds', 'system-mds')
 on conflict do nothing;
 
-INSERT INTO consequence_classification_status
-    (
-    consequence_classification_status_code,
-    description,
-    display_order,
-    create_user,
-    update_user
-    )
-VALUES
-    ('LOW', 'Low', 10, 'system-mds', 'system-mds'),
-    ('SIG', 'Significant', 20, 'system-mds', 'system-mds'),
-    ('HIG', 'High', 30, 'system-mds', 'system-mds'),
-    ('VHIG', 'Very High', 35, 'system-mds', 'system-mds'),
-    ('EXT', 'Extreme', 50, 'system-mds', 'system-mds'),
-    ('NOD', 'N/A (No Dam)', 60, 'system-mds', 'system-mds')
-ON CONFLICT DO NOTHING;
-
-UPDATE consequence_classification_status SET display_order = 60 WHERE consequence_classification_status_code = 'SIG';
-UPDATE consequence_classification_status SET display_order = 50 WHERE consequence_classification_status_code = 'NOD';
-UPDATE consequence_classification_status SET display_order = 40 WHERE consequence_classification_status_code = 'LOW';
-UPDATE consequence_classification_status SET display_order = 30 WHERE consequence_classification_status_code = 'HIG';
-UPDATE consequence_classification_status SET display_order = 20 WHERE consequence_classification_status_code = 'VHIG';
-UPDATE consequence_classification_status SET display_order = 10 WHERE consequence_classification_status_code = 'EXT';
-
 INSERT INTO tsf_operating_status
     (
     tsf_operating_status_code,
