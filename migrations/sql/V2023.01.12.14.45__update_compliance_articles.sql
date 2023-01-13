6f1668e8e64c9b5116d2cff95f9ec644ec9290e9
Mat Busby <mat.busby@ca.ey.com>
2023-01-13 12:12:06 -0700

    reverted change to 6.19.1

--- migrations/sql/V2023.01.12.14.45__update_compliance_articles.sql
+++ migrations/sql/V2023.01.12.14.45__update_compliance_articles.sql
@@ -3,6 +3,23 @@ SET
     expiry_date = '2022-11-30'
 WHERE
     section = '2'
-  AND sub_section = '11'
-  AND paragraph = '9'
-  AND description = 'Toilet Facilities';
+    AND sub_section = '11'
+    AND paragraph = '9'
+    AND description = 'Toilet Facilities';
+
+UPDATE
+    compliance_article
+SET
+    expiry_date = '9999-12-31'
+WHERE
+    section = '6'
+    AND sub_section = '19'
+    AND paragraph = '1'
+    AND description = 'Operator''s Responsibility';
+
+DELETE from compliance_article
+WHERE
+    section = '6'
+    AND sub_section = '19'
+    AND paragraph = '1'
+    AND description = 'Remote Control Operator''s Responsibility';
