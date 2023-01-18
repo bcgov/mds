96103169d8c801271dcd76819361ea0af7a1e96d
Mat Busby <83598933+matbusby-fw@users.noreply.github.com>
2023-01-18 16:42:51 -0700

    [MDS-5000] - updates to celery worker command (#2410)
    updates to celery worker command

--- services/core-api/celery.sh
+++ services/core-api/celery.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-cd /app
+# -n is the number of tasks to consume
+# -A is the name of the app to run
+# -Q is the name of the queue to consume from
+# -concurrency is the number of child processes processing the queue
+# -B is the Beat
+# --scheduler is the scheduler class to use
+# -E Enable sending task-related events that can be captured by monitors
 
-celery worker -n core_tasks@%h -A app.tasks.celery_entrypoint -Q core_tasks --loglevel=debug --concurrency=1 -B --scheduler redbeat.RedBeatScheduler -E
+cd /app || exit
+
+celery worker -n core_tasks@%h -A app.tasks.celery_entrypoint --pidfile=/tmp/celery/celery.pid -Q core_tasks --loglevel=debug --concurrency=1 -B --scheduler redbeat.RedBeatScheduler -E
