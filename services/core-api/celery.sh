63e46cb8e1a3a56874b2eca85634fc02e4bdbd83
Mat Busby <83598933+matbusby-fw@users.noreply.github.com>
2023-01-18 09:33:58 -0700

    [MDS-5000] Celery Log Level (#2408)
    changed celery loglevel to debug

--- services/core-api/celery.sh
+++ services/core-api/celery.sh
@@ -2,4 +2,4 @@
 
 cd /app
 
-celery worker -n core_tasks@%h -A app.tasks.celery_entrypoint -Q core_tasks --loglevel=info --concurrency=1 -B --scheduler redbeat.RedBeatScheduler -E
+celery worker -n core_tasks@%h -A app.tasks.celery_entrypoint -Q core_tasks --loglevel=debug --concurrency=1 -B --scheduler redbeat.RedBeatScheduler -E
