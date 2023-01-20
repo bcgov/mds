f53fe27ec93d6344b2ba8ef120d36cd95d9250c3
Mat Busby <83598933+matbusby-fw@users.noreply.github.com>
2023-01-20 11:40:34 -0700

    [MDS-5000] CELERY!! ✊ ✊  (#2411)
    Updateds to celery.sh command for core-api-celery
    
    Co-authored-by: Mat Busby <matbusby@gmail.com>

--- services/core-api/celery.sh
+++ services/core-api/celery.sh
@@ -6,8 +6,11 @@
 # -concurrency is the number of child processes processing the queue
 # -B is the Beat
 # --scheduler is the scheduler class to use
+# -s Path to the schedule database.
 # -E Enable sending task-related events that can be captured by monitors
+# --pidfile is the location of the pid file
+
 
 cd /app || exit
 
-celery worker -n core_tasks@%h -A app.tasks.celery_entrypoint --pidfile=/tmp/celery/celery.pid -Q core_tasks --loglevel=debug --concurrency=1 -B --scheduler redbeat.RedBeatScheduler -E
+celery worker -n core_tasks@%h -A app.tasks.celery_entrypoint -Q core_tasks --loglevel=debug --concurrency=1 -B --scheduler redbeat.RedBeatScheduler -E --pidfile /tmp/celerybeat.pid -s /tmp/celerybeat-schedule
