e6e40c0597c8a3ad5b1039c70adcd676df3713de
Mat Busby <83598933+matbusby-fw@users.noreply.github.com>
2023-01-26 18:01:15 +0000

    added temp auto refresh and refactored app.js

--- services/minespace-web/src/index.js
+++ services/minespace-web/src/index.js
@@ -7,13 +7,13 @@ import { Provider } from "react-redux";
 
 import { MatomoProvider, createInstance } from "@datapunt/matomo-tracker-react";
 import { ENVIRONMENT } from "@mds/common";
-import { ReactKeycloakProvider} from "@react-keycloak/web";
+import { ReactKeycloakProvider } from "@react-keycloak/web";
 import App from "./App";
 import "antd/dist/antd.less";
 import "./styles/index.scss";
 import fetchEnv from "./fetchEnv";
 import configureStore from "./store/configureStore";
-import keycloak, {keycloakInitConfig} from "./keycloak";
+import keycloak, { keycloakInitConfig } from "./keycloak";
 
 export const store = configureStore();
 
@@ -34,13 +34,19 @@ export class Index extends Component {
       this.setState({ environment: true });
     });
   }
-  
 
   render() {
-    console.log(keycloakInitConfig, 'jjjj');
+    console.log(keycloakInitConfig, "jjjj");
     if (this.state.environment) {
       return (
-        <ReactKeycloakProvider authClient={keycloak} initOptions={keycloakInitConfig}>
+        <ReactKeycloakProvider
+          authClient={keycloak}
+          initOptions={keycloakInitConfig}
+          onTokens={(token) => {
+            console.log(token, "token");
+            localStorage.setItem("token", token);
+          }}
+        >
           <Provider store={store}>
             <MatomoProvider value={instance}>
               <App />
