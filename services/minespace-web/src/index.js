be955d3c42575121975061135994b7055a4a28a5
Tara Epp <102187683+taraepp@users.noreply.github.com>
2023-01-30 23:13:09 +0000

    updating progress so Mat can see- changed back to interval, extracted a function, fixed some numbers

--- services/minespace-web/src/index.js
+++ services/minespace-web/src/index.js
@@ -17,8 +17,11 @@ import { useIdleTimer } from 'react-idle-timer';
 export const store = configureStore();
 
 const idleTimeout = 60_000; // 1 minute?
+const bufferSeconds = 12 * 60;
 
 const Index = () => {
+  // const initialToken = keycloak?.tokenParsed?.exp ? keycloak.tokenParsed.exp * 1000 : null;
+  // console.log(keycloak);
   const [environment, setEnvironment] = useState(false);
   const [tokenExpiryTime, setTokenExpiryTime] = useState(null)
 
@@ -47,26 +50,52 @@ const Index = () => {
     ]
   });
 
-  useEffect(() => {
+  const handleAuthErrors = () => {
+    keycloak.clearToken();
+  }
+
+  const handleUpdateToken = () => {
+    // const tokenExpiryTime = keycloak?.tokenParsed?.exp ? keycloak.tokenParsed.exp * 1000 : null;
     if (tokenExpiryTime) {
-      const bufferSeconds = 10;
+      console.log(keycloak);
+      console.log('tokenExpiryTime', new Date(tokenExpiryTime));
+      console.log('tokenExpiryTimeFromKeyCloak', new Date(keycloak.tokenParsed?.exp * 1000))
+      console.log('refreshExpiryTime', new Date(keycloak.refreshTokenParsed?.exp * 1000))
+      let bufferSeconds = 14 * 60; // 12 minutes * 60 seconds per minute
       const timeToLive = tokenExpiryTime - Date.now() - (bufferSeconds * 1000);
       const isActive = getRemainingTime() > 0;
 
       console.log('timeToLive', timeToLive)
 
-      const updateTimeout = setTimeout(() => {
+      const updateInterval = setInterval(() => {
+        console.log('updateTimeout callback')
         if (isActive) {
           console.log('is active! Update token')
-          keycloak.updateToken(bufferSeconds)
+          console.log(keycloak.tokenParsed)
+          // updatetoken takes minValidity in seconds
+          // so does isTokenExpired
+          // console.log(keycloak.isTokenExpired(bufferSeconds + 1));
+          keycloak.updateToken(bufferSeconds + 1)
+            .then((wasUpdated) => {
+              console.log('token updated!!!', wasUpdated)
+              console.log(keycloak.tokenParsed);
+            })
+            .catch((err="") => {
+              console.log('failed to refresh token', err);
+              console.log(keycloak.refreshTokenParsed);
+            }) // returns a promise
+          console.log(keycloak.tokenParsed)
         }        
       }, timeToLive)
 
       return () => {
-        clearTimeout(updateTimeout)
+        clearInterval(updateInterval)
       }
     }
-    
+  }
+
+  useEffect(() => {
+    handleUpdateToken();    
   }, [tokenExpiryTime])
 
   return environment ? (
@@ -79,14 +108,20 @@ const Index = () => {
               localStorage.setItem("id_token", token.idToken);
               localStorage.setItem("refresh_token", token.refreshToken);
               localStorage.setItem("token", token.token);
+              let accessTokenExpiry = keycloak.tokenParsed.exp * 1000;
               localStorage.setItem("refresh_token_expiry", keycloak.refreshTokenParsed.exp * 1000);
-              localStorage.setItem("access_token_expiry", keycloak.tokenParsed.exp * 1000);
+              localStorage.setItem("access_token_expiry", accessTokenExpiry);
 
-              setTokenExpiryTime(keycloak.tokenParsed.exp * 1000);
+              setTokenExpiryTime(accessTokenExpiry);
             }   
           }}
           onEvent={(event, err="") => {console.log('keycloak event!'); console.log(event, err); console.log(keycloak);}}
           onTokenExpired={(val) => console.log("token expired", val)}
+          onReady={(authenticated) => {
+            if (authenticated) {
+              handleUpdateToken();
+            }
+          }}
         >
           <Provider store={store}>
             <App />
