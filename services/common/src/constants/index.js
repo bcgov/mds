9e15a24fb22002801497dc94351685b370ee137b
Mat Busby <matbusby@gmail.com>
2023-02-02 23:34:33 -0700

    updated common index comment

--- services/common/src/constants/index.js
+++ services/common/src/constants/index.js
@@ -1,8 +1,9 @@
 /** This is the source of the eslint import errors.
  * These files contain many exports with identical names.
- * This will need some cleaning, but the commented out files are
- * not currently being imported anywhere, so this will at least
- * stop the errors for now.
+ * We'll need to fix this since the system will have no idea which
+ * export to use.  It'll likely require some renaming since it seems
+ * yarn workspaces requires the export to come in the form of an index
+ * as is currently set up.
  */
 export * from "./actionTypes";
 export * from "./API";
