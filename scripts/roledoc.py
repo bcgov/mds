150d4e5966be2dff85ca7c10167bc0a633d33ca4
Vyas <67800715+vyasworks@users.noreply.github.com>
2023-02-15 17:25:36 -0800

    Update to include all roles used.

--- scripts/roledoc.py
+++ scripts/roledoc.py
@@ -2,7 +2,7 @@ import ast
 import os
 import sys
 
-def find_decorated_functions(folder_path, decorator_name):
+def find_decorated_functions(folder_path, decorator_names):
     results = []
     for root, dirs, files in os.walk(folder_path):
         for file in files:
@@ -21,45 +21,80 @@ def find_decorated_functions(folder_path, decorator_name):
                         continue
 
                     for decorator_node in function_node.decorator_list:
-                        if not isinstance(decorator_node, ast.Call):
-                            continue
+                        if isinstance(decorator_node, ast.Call):
+                            if not isinstance(decorator_node.func, ast.Name):
+                                continue
 
-                        if not isinstance(decorator_node.func, ast.Name):
-                            continue
+                            if decorator_node.func.id not in decorator_names:
+                                continue
 
-                        if decorator_node.func.id != decorator_name:
-                            continue
+                            args = []
+                            for arg_node in decorator_node.args:
+                                if isinstance(arg_node, ast.List):
+                                    items = [item.id for item in arg_node.elts if isinstance(item, ast.Name)]
+                                    args.extend(items)
+                                elif isinstance(arg_node, ast.Name):
+                                    args.append(arg_node.id)
 
-                        args = []
-                        for arg_node in decorator_node.args:
-                            if isinstance(arg_node, ast.List):
-                                items = [item.id for item in arg_node.elts if isinstance(item, ast.Name)]
-                                args.extend(items)
-                            elif isinstance(arg_node, ast.Name):
-                                args.append(arg_node.id)
+                            result = {
+                                "class_name": class_node.name,
+                                "function_name": function_node.name,
+                                "decorator_name": decorator_node.func.id,
+                                "args": args
+                            }
+                            results.append(result)
+                        elif isinstance(decorator_node, ast.Name):
+                            if decorator_node.id not in decorator_names:
+                                continue
 
-                        result = {
-                            "class_name": class_node.name,
-                            "function_name": function_node.name,
-                            "args": args
-                        }
-                        results.append(result)
+                            result = {
+                                "class_name": class_node.name,
+                                "function_name": function_node.name,
+                                "decorator_name": decorator_node.id,
+                                "args": []
+                            }
+                            results.append(result)
 
         for dir_name in dirs:
             dir_path = os.path.join(root, dir_name)
-            find_decorated_functions(dir_path, decorator_name)
+            find_decorated_functions(dir_path, decorator_names)
 
     return results
 
 
-if len(sys.argv) != 2:
-    print("Usage: python script.py path/to/folder")
+# List of decorators to search for
+decorator_names = ["is_minespace_user",
+    "can_edit_now_dates",
+    "can_edit_mines",
+    "requires_role_edit_emli_contacts",
+    "requires_role_edit_project_summaries",
+    "requires_role_edit_incidents",
+    "requires_role_view_all",
+    "requires_role_mine_edit",
+    "requires_role_mine_admin",
+    "requires_role_edit_party",
+    "requires_role_edit_permit",
+    "requires_role_edit_explosives_permit",
+    "requires_role_edit_standard_permit_conditions",
+    "requires_role_edit_report",
+    "requires_role_edit_do",
+    "requires_role_close_permit",
+    "requires_role_edit_submissions",
+    "requires_role_edit_securities",
+    "requires_role_mds_administrative_users",
+    "requires_role_edit_now_dates",
+    "requires_role_edit_tsf",
+    "requires_role_edit_requirements",
+    "requires_any_of"]
+
+# Get folder path from command-line argument
+if len(sys.argv) < 2:
+    print("Usage: python script.py folder_path")
     sys.exit(1)
 
 folder_path = sys.argv[1]
-decorator_name = "requires_any_of"
 
-results = find_decorated_functions(folder_path, decorator_name)
+results = find_decorated_functions(folder_path, decorator_names)
 
 # Group the results by class name
 grouped_results = {}
@@ -73,4 +108,8 @@ for result in results:
 for class_name, class_results in grouped_results.items():
     print(class_name)
     for result in class_results:
-        print(f"  {result['function_name']}: {result['args']}")
+        if result["args"]:
+            args_str = ", ".join(result["args"])
+            print(f"  {result['function_name']} ({result['decorator_name']}): {args_str}")
+        else:
+            print(f"  {result['function_name']} ({result['decorator_name']})")
