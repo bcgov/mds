f3b07bf9ece55abce22ed0202387516d403dc37c
Henry Oforeh <henry.oforeh@aot-technologies.com>
2023-03-17 11:09:17 -0600

    [MDS-4995] update memp document (#2461)
    update memp document

--- services/core-web/src/constants/assets.js
+++ services/core-web/src/constants/assets.js
@@ -41,7 +41,7 @@ export { default as YELLOW_HAZARD } from "../assets/images/Hazard.svg";
 export { default as BACKGROUND } from "../assets/images/Background.svg";
 export { default as TRASHCAN } from "../assets/images/TrashCan.svg";
 export { default as HSRC_PDF } from "../assets/downloads/health_safety_and_reclamation_code_nov2022.pdf";
-export { default as MEMP_PDF } from "../assets/downloads/Mine_Emergency_Management_Plan_Final_20210420.pdf";
+export { default as MEMP_PDF } from "../assets/downloads/7810_Mine_Emergency_Management_Plan_V4_DJ edits_20221031.pdf";
 export { default as FLUSH_SOUND } from "../assets/downloads/flush.mp3";
 export { default as WATER_SOUND } from "../assets/downloads/water.mp3";
 export { default as MINESPACE_ICON } from "../assets/images/minespaceIcon.svg";
