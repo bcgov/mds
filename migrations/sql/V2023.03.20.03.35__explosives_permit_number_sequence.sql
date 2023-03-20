1d1346f7898bd2e2ca565b6123b3f882a77637e6
Tara Epp <102187683+taraepp@users.noreply.github.com>
2023-03-20 22:44:03 +0000

    fix off by 1 error

--- migrations/sql/V2023.03.20.03.35__explosives_permit_number_sequence.sql
+++ migrations/sql/V2023.03.20.03.35__explosives_permit_number_sequence.sql
@@ -5,7 +5,7 @@ OWNED BY explosives_permit.permit_number;
 
 -- find the highest value matching BC-100XX and start the sequence at the next one, or 10000 if no results
 SELECT setval('explosives_permit_number_sequence', 
-    COALESCE((SELECT 1+(
+    COALESCE((
         SELECT SUBSTRING(permit_number FROM 4)::int FROM explosives_permit WHERE permit_number LIKE '%BC-100%' ORDER BY SUBSTRING(permit_number FROM 4)::int DESC LIMIT 1
-        )), 9999)
+        ), 9999)
 );
