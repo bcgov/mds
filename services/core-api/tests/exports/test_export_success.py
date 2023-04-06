44dfe07bb21b27641373f01bf0a9432fde27e427
Tara Epp <102187683+taraepp@users.noreply.github.com>
2023-04-06 15:41:37 -0600

    [MDS-4668] Pagination for mine-summary endpoint (#2487)
    * changed reponse of mine-summary endpoint to include total, per_page, and current_page, set page defaults, updated swagger, added test to suite

--- services/core-api/tests/exports/test_export_success.py
+++ services/core-api/tests/exports/test_export_success.py
@@ -1,4 +1,6 @@
 import json, pytest, uuid
+from app.api.exports.mines.models.mine_summary_view import MineSummaryView
+from app.api.exports.mines.resources.mine_summary_resource import PAGE_DEFAULT, PER_PAGE_DEFAULT
 
 
 def test_mine_summary_export_success(test_client, db_session, auth_headers):
@@ -7,6 +9,18 @@ def test_mine_summary_export_success(test_client, db_session, auth_headers):
 
     assert get_resp.status_code == 200
 
+def test_mine_summary_export_paginated(test_client, db_session, auth_headers):
+    get_resp = test_client.get(
+        f'/exports/mine-summary', headers=auth_headers['full_auth_header'])
+
+    assert get_resp.status_code == 200
+
+    get_data = json.loads(get_resp.data.decode()).get('data')
+
+    assert len(get_data['mines']) <= PER_PAGE_DEFAULT
+    assert get_data['per_page'] == PER_PAGE_DEFAULT
+    assert get_data['current_page'] == PAGE_DEFAULT
+    assert get_data['total'] == MineSummaryView.query.count()
 
 def test_core_static_content_success(test_client, db_session, auth_headers):
     get_resp = test_client.get(
