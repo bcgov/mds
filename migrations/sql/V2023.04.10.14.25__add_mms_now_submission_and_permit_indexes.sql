797ea5fd470f8b3d4d32e086d3902ab7d1a39beb
Mat Busby <mat.busby@ca.ey.com>
2023-04-12 09:56:48 -0600

    Remove unnecessary NoW Index

--- migrations/sql/V2023.04.10.14.25__add_mms_now_submission_and_permit_indexes.sql
+++ migrations/sql/V2023.04.10.14.25__add_mms_now_submission_and_permit_indexes.sql
@@ -1,3 +1,1 @@
-CREATE INDEX IF NOT EXISTS application_pkey ON mms_now_submissions.application (id);
-
 CREATE INDEX IF NOT EXISTS permit_conditions_parent_permit_condition_idx ON permit_conditions USING btree (parent_permit_condition_id);
