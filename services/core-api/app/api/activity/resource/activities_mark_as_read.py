8e723ec19765731212a31a8811f142c1150426ac
Henry Oforeh <henry.oforeh@aot-technologies.com>
2023-04-21 14:19:20 -0600

    update permission.

--- services/core-api/app/api/activity/resource/activities_mark_as_read.py
+++ services/core-api/app/api/activity/resource/activities_mark_as_read.py
@@ -1,12 +1,12 @@
 from flask_restplus import Resource, reqparse
 
 from app.api.activity.models.activity_notification import ActivityNotification
-from app.api.utils.access_decorators import EDIT_DO, requires_any_of, MINESPACE_PROPONENT
+from app.api.utils.access_decorators import VIEW_ALL, requires_any_of, MINESPACE_PROPONENT
 from app.api.utils.resources_mixins import UserMixin
 
 
 class ActivityMarkAsReadResource(Resource, UserMixin):
-    @requires_any_of([EDIT_DO, MINESPACE_PROPONENT])
+    @requires_any_of([VIEW_ALL, MINESPACE_PROPONENT])
     def patch(self):
         parser = reqparse.RequestParser()
         parser.add_argument(
