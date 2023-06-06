f37c5861ef218754a7d1d201a1391c73bb1fb88b
Henry Oforeh <henry.oforeh@aot-technologies.com>
2023-06-05 18:58:55 -0600

    cypress login screen with keycloak.

---
+++ services/core-web/cypress/e2e/index.cy.ts
@@ -0,0 +1,29 @@
+describe("Login Page", () => {
+  const username = Cypress.env("test-user");
+  const password = Cypress.env("test-pwd");
+  const url = Cypress.env("url");
+
+  beforeEach(() => {
+    cy.visit(url);
+  });
+
+  it("should successfully log in", () => {
+    cy.url({ timeout: 10000 }).should("include", "test.loginproxy.gov.bc.ca");
+    cy.get("a#social-idir").click();
+    cy.url({ timeout: 10000 }).should("include", "logontest7.gov.bc.ca");
+
+    cy.get("#user").type(username);
+    cy.get("#password").type(password);
+
+    cy.get('[name="btnSubmit"]').click();
+
+    // Assert that landing on the home page is successful
+    cy.url().should("include", "/home");
+
+    // Wait for the home page to load
+    cy.wait(10000); // Adjust the wait time as needed
+
+    // Verify that the home page contains the welcome heading
+    cy.contains("Welcome!").should("be.visible");
+  });
+});
