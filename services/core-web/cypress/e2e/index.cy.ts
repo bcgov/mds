0f443c78c4bc95fc48af5ea256eabcbf52b9bee4
Henry Oforeh <henry.oforeh@aot-technologies.com>
2023-06-15 11:55:12 -0600

    update cypress test.

--- services/core-web/cypress/e2e/index.cy.ts
+++ services/core-web/cypress/e2e/index.cy.ts
@@ -1,22 +1,11 @@
-describe("Login Page", () => {
-  const username = Cypress.env("test-user");
-  const password = Cypress.env("test-pwd");
+describe("Home Page", () => {
   const url = Cypress.env("url");
-
   beforeEach(() => {
-    cy.visit(url);
+    cy.login();
   });
 
-  it("should successfully log in", () => {
-    cy.url({ timeout: 10000 }).should("include", "test.loginproxy.gov.bc.ca");
-    cy.get("a#social-idir").click();
-    cy.url({ timeout: 10000 }).should("include", "logontest7.gov.bc.ca");
-
-    cy.get("#user").type(username);
-    cy.get("#password").type(password);
-
-    cy.get('[name="btnSubmit"]').click();
-
+  it("should navigate to the home page successfully", () => {
+    cy.visit(`${url}/home`);
     // Assert that landing on the home page is successful
     cy.url().should("include", "/home");
   });
