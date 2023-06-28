86086b92139b1a6c2d7beb2053d7d4b2bb50bb84
Sinn <Alex.Sinn@gov.bc.ca>
2023-06-28 13:47:18 -0700

    adjustment to file name column based on pr comment

--- services/core-web/src/components/common/DocumentTable.js
+++ services/core-web/src/components/common/DocumentTable.js
@@ -67,18 +67,17 @@ const renderFileType = (file) => {
 };
 
 const parseFiles = (versions, documentType) =>
-  versions.map((version, index) =>
-  ({
+  versions.map((version, index) => ({
     document: version,
     key: version.mine_document_version_guid,
     fileName: version.document_name,
     fileLocation: renderFileLocation(documentType) || Strings.EMPTY_FIELD,
     fileType: renderFileType(version.document_name) || Strings.EMPTY_FIELD,
-    lastModified: (version.update_timestamp && formatDate(version.update_timestamp)) || Strings.EMPTY_FIELD,
+    lastModified:
+      (version.update_timestamp && formatDate(version.update_timestamp)) || Strings.EMPTY_FIELD,
     createdBy: version.create_user,
-    numberOfVersions: (index === 0 ? (versions.length) - 1 : 0),
-  })
-  );
+    numberOfVersions: index === 0 ? versions.length - 1 : 0,
+  }));
 
 const transformRowData = (document) => {
   const files = parseFiles(document.versions, document.major_mine_application_document_type);
@@ -94,7 +93,7 @@ const transformRowData = (document) => {
 const defaultProps = {
   documents: [],
   isViewOnly: false,
-  removeDocument: () => { },
+  removeDocument: () => {},
   excludedColumnKeys: [],
   additionalColumnProps: [],
   documentColumns: null,
@@ -127,12 +126,27 @@ const openArchiveModal = (event, props, documents) => {
   });
 };
 
-const withTag = (text, elem) => {
+const withTag = (record, elem, title) => {
   return (
-    <div className="inline-flex flex-between">
+    <div className="inline-flex flex-between file-name-container" title={title}>
       {elem}
 
-      <Tag>{text}</Tag>
+      <span className="file-history-container">
+        {record?.numberOfVersions > 0 ? (
+          <span>
+            <Tooltip
+              title={`This file has ${record.numberOfVersions} previous versions`}
+              placement="top"
+              mouseEnterDelay={1}
+            >
+              <Tag icon={<ClockCircleOutlined />} color="#5E46A1" className="file-version-amount">
+                {record.numberOfVersions}
+              </Tag>
+            </Tooltip>
+          </span>
+        ) : null}
+        {record?.is_archived ? <Tag>{"Archived"}</Tag> : null}
+      </span>
     </div>
   );
 };
@@ -141,175 +155,166 @@ export const DocumentTable = (props) => {
   const isMinimalView = props.view === "minimal";
   let columns = props.expandable
     ? [
-      {
-        className: "file-name-column",
-        title: "File Name",
-        dataIndex: "fileName",
-        key: "fileName",
-        render: (text, record) => {
-          return (
-            <div className="file-name-container" title="File Name">
-              <span className="file-name-text" style={!record.numberOfVersions ? { marginLeft: "23px" } : {}}>{text}</span>
-              {record?.numberOfVersions > 0 ?
-                (<span className="file-history-container">
-                  <span>
-                    <Tooltip
-                      title={`This file has ${record.numberOfVersions} previous versions`}
-                      placement="top"
-                      mouseEnterDelay={1}
-                    >
-                      <span className="file-version-amount">
-                        <ClockCircleOutlined />
-                        <span>{record.numberOfVersions}</span>
-                      </span>
-                    </Tooltip>
-                  </span>
-                </span>) : null
-              }
-            </div>
-          )
-        },
-      },
-      {
-        title: "File Location",
-        dataIndex: "fileLocation",
-        key: "fileLocation",
-        render: (text) => <div title="File Location">{text}</div>,
-      },
-      {
-        title: "File Type",
-        dataIndex: "fileType",
-        key: "fileType",
-        render: (text) => <div title="File Type">{text}</div>,
-      },
-      {
-        title: "Last Modified",
-        dataIndex: "lastModified",
-        key: "lastModified",
-        render: (text) => <div title="Last Modified">{text}</div>,
-      },
-      {
-        title: "Created By",
-        dataIndex: "createdBy",
-        key: "createdBy",
-        render: (text) => <div title="Created By">{text}</div>,
-      },
-      {
-        title: "",
-        dataIndex: "operations",
-        key: "operations",
-        align: "right",
-        render: (text, record) => {
-          return (
-            <DocumentActions
-              openDocument
-              document={
-                {
-                  documentName: record.document.document_name,
-                  documentMangerGuid: record.document.document_manager_guid,
-                }
-              } />
-          );
-        },
-      },
-    ]
-    : [
-      {
-        title: "Name",
-        key: "name",
-        dataIndex: "name",
-        sorter: !isMinimalView && nullableStringSorter("name"),
-        render: (text, record) => {
-          let content = record.name;
-
-          if (!isMinimalView) {
-            content = (
-              <div key={record.key} title="Name">
-                <DocumentLink
-                  documentManagerGuid={record.document_manager_guid}
-                  documentName={record.name}
-                  truncateDocumentName={false}
-                />
+        {
+          className: "file-name-column",
+          title: "File Name",
+          dataIndex: "fileName",
+          key: "fileName",
+          render: (text, record) => {
+            let content = (
+              <div
+                className="file-name-text"
+                style={record?.numberOfVersions === 0 ? { marginLeft: "38px" } : {}}
+              >
+                {text}
               </div>
             );
-          }
 
-          return record.is_archived ? withTag("Archived", content) : content;
+            return record?.numberOfVersions > 0 || record?.is_archived ? (
+              withTag(record, content, "File Name")
+            ) : (
+              <div title="File Name">{content}</div>
+            );
+          },
         },
-      },
-      {
-        title: "Dated",
-        key: "dated",
-        dataIndex: "dated",
-        sorter: !isMinimalView && dateSorter("dated"),
-        defaultSortOrder: "descend",
-        render: (text) => <div title="Dated">{formatDate(text)}</div>,
-      },
-      {
-        title: "Category",
-        key: "category",
-        dataIndex: "category",
-        sorter: !isMinimalView && nullableStringSorter("category"),
-        render: (text) => <div title="Category">{text}</div>,
-      },
-      {
-        title: "Uploaded",
-        key: "uploaded",
-        dataIndex: "uploaded",
-        sorter: !isMinimalView && dateSorter("uploaded"),
-        defaultSortOrder: "descend",
-        render: (text) => <div title="Uploaded">{formatDate(text)}</div>,
-      },
-      {
-        key: "remove",
-        className: props.isViewOnly || !props.removeDocument ? "column-hide" : "",
-        render: (text, record) => (
-          <div className={props.isViewOnly || !props.removeDocument ? "column-hide" : ""}>
-            <Popconfirm
-              placement="topLeft"
-              title={`Are you sure you want to delete ${record.name}?`}
-              onConfirm={(event) => props.removeDocument(event, record.key, props?.documentParent)}
-              okText="Delete"
-              cancelText="Cancel"
-            >
-              <Button ghost type="primary" size="small">
-                <img src={TRASHCAN} alt="Remove" />
-              </Button>
-            </Popconfirm>
-          </div>
-        ),
-      },
-      isFeatureEnabled(Feature.MAJOR_PROJECT_ARCHIVE_FILE) && {
-        key: "archive",
-        className: props.isViewOnly || !props.canArchiveDocuments ? "column-hide" : "",
-        render: (text, record) => (
-          <div
-            className={
-              !record?.mine_document_guid || props.isViewOnly || !props.canArchiveDocuments
-                ? "column-hide"
-                : ""
-            }
-          >
-            <Button
-              ghost
-              type="primary"
-              size="small"
-              onClick={(event) => openArchiveModal(event, props, [record])}
-            >
-              Archive
-            </Button>
-          </div>
-        ),
-      },
-    ].filter(Boolean);
+        {
+          title: "File Location",
+          dataIndex: "fileLocation",
+          key: "fileLocation",
+          render: (text) => <div title="File Location">{text}</div>,
+        },
+        {
+          title: "File Type",
+          dataIndex: "fileType",
+          key: "fileType",
+          render: (text) => <div title="File Type">{text}</div>,
+        },
+        {
+          title: "Last Modified",
+          dataIndex: "lastModified",
+          key: "lastModified",
+          render: (text) => <div title="Last Modified">{text}</div>,
+        },
+        {
+          title: "Created By",
+          dataIndex: "createdBy",
+          key: "createdBy",
+          render: (text) => <div title="Created By">{text}</div>,
+        },
+        {
+          title: "",
+          dataIndex: "operations",
+          key: "operations",
+          align: "right",
+          render: (text, record) => {
+            return (
+              <DocumentActions
+                openDocument
+                document={{
+                  documentName: record.document.document_name,
+                  documentMangerGuid: record.document.document_manager_guid,
+                }}
+              />
+            );
+          },
+        },
+      ]
+    : [
+        {
+          title: "Name",
+          key: "name",
+          dataIndex: "name",
+          sorter: !isMinimalView && nullableStringSorter("name"),
+          render: (text, record) => {
+            let content = record.name;
 
+            if (!isMinimalView) {
+              content = (
+                <div key={record.key} title="Name">
+                  <DocumentLink
+                    documentManagerGuid={record.document_manager_guid}
+                    documentName={record.name}
+                    truncateDocumentName={false}
+                  />
+                </div>
+              );
+            }
+
+            return record.is_archived ? withTag(record, content, "Name") : content;
+          },
+        },
+        {
+          title: "Dated",
+          key: "dated",
+          dataIndex: "dated",
+          sorter: !isMinimalView && dateSorter("dated"),
+          defaultSortOrder: "descend",
+          render: (text) => <div title="Dated">{formatDate(text)}</div>,
+        },
+        {
+          title: "Category",
+          key: "category",
+          dataIndex: "category",
+          sorter: !isMinimalView && nullableStringSorter("category"),
+          render: (text) => <div title="Category">{text}</div>,
+        },
+        {
+          title: "Uploaded",
+          key: "uploaded",
+          dataIndex: "uploaded",
+          sorter: !isMinimalView && dateSorter("uploaded"),
+          defaultSortOrder: "descend",
+          render: (text) => <div title="Uploaded">{formatDate(text)}</div>,
+        },
+        {
+          key: "remove",
+          className: props.isViewOnly || !props.removeDocument ? "column-hide" : "",
+          render: (text, record) => (
+            <div className={props.isViewOnly || !props.removeDocument ? "column-hide" : ""}>
+              <Popconfirm
+                placement="topLeft"
+                title={`Are you sure you want to delete ${record.name}?`}
+                onConfirm={(event) =>
+                  props.removeDocument(event, record.key, props?.documentParent)
+                }
+                okText="Delete"
+                cancelText="Cancel"
+              >
+                <Button ghost type="primary" size="small">
+                  <img src={TRASHCAN} alt="Remove" />
+                </Button>
+              </Popconfirm>
+            </div>
+          ),
+        },
+        isFeatureEnabled(Feature.MAJOR_PROJECT_ARCHIVE_FILE) && {
+          key: "archive",
+          className: props.isViewOnly || !props.canArchiveDocuments ? "column-hide" : "",
+          render: (text, record) => (
+            <div
+              className={
+                !record?.mine_document_guid || props.isViewOnly || !props.canArchiveDocuments
+                  ? "column-hide"
+                  : ""
+              }
+            >
+              <Button
+                ghost
+                type="primary"
+                size="small"
+                onClick={(event) => openArchiveModal(event, props, [record])}
+              >
+                Archive
+              </Button>
+            </div>
+          ),
+        },
+      ].filter(Boolean);
 
   const currentRowData = props.expandable
     ? props.documents?.map((document) => {
-      return transformRowData(
-        document,
-      )
-    })
+        return transformRowData(document);
+      })
     : null;
 
   if (!some(props.documents, "dated")) {
@@ -330,33 +335,31 @@ export const DocumentTable = (props) => {
     });
   }
 
-  return (
-    (
-      props.expandable ?
-        (<CoreTable
-          condition={props.isLoaded}
-          dataSource={currentRowData}
-          columns={columns}
-          recordType="document history"
-          tableProps={{
-            className: "nested-table",
-            rowClassName: "table-row-align-middle pointer fade-in expandable-table-rows",
-            align: "left",
-            pagination: false,
-            indentSize: 0,
-            expandable: true,
-          }}
-        />)
-        : (<Table
-          pagination={false}
-          columns={props?.documentColumns ?? columns}
-          locale={{ emptyText: "No Data Yet" }}
-          dataSource={props.documents}
-          showHeader={!isMinimalView}
-          size={isMinimalView ? "small" : undefined}
-          rowClassName={isMinimalView ? "ant-table-row-minimal" : undefined}
-        />)
-    )
+  return props.expandable ? (
+    <CoreTable
+      condition={props.isLoaded}
+      dataSource={currentRowData}
+      columns={columns}
+      recordType="document history"
+      tableProps={{
+        className: "nested-table",
+        rowClassName: "table-row-align-middle pointer fade-in expandable-table-rows",
+        align: "left",
+        pagination: false,
+        indentSize: 0,
+        expandable: true,
+      }}
+    />
+  ) : (
+    <Table
+      pagination={false}
+      columns={props?.documentColumns ?? columns}
+      locale={{ emptyText: "No Data Yet" }}
+      dataSource={props.documents}
+      showHeader={!isMinimalView}
+      size={isMinimalView ? "small" : undefined}
+      rowClassName={isMinimalView ? "ant-table-row-minimal" : undefined}
+    />
   );
 };
 
