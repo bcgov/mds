b820467e8a12e04f9acfab2265370060f181501e
Henry Oforeh <henry.oforeh@aot-technologies.com>
2023-07-13 16:09:09 -0600

    implement cypress e2e for minespace.

---
+++ services/minespace-web/cypress/e2e/index.cy.ts
@@ -0,0 +1,12 @@
+describe("Mines Page", () => {
+  const url = Cypress.env("CYPRESS_CORE_WEB_TEST_URL");
+  beforeEach(() => {
+    cy.login();
+  });
+
+  it("should navigate to the mines page successfully", () => {
+    cy.visit(`${url}/mines`);
+    // Assert that landing on the home page is successful
+    cy.url({ timeout: 10000 }).should("include", "/mines");
+  });
+});
---
+++ services/minespace-web/cypress/support/commands.ts
@@ -0,0 +1,33 @@
+Cypress.Commands.add("login", () => {
+  const url = Cypress.env("CYPRESS_CORE_WEB_TEST_URL");
+  const environmentUrl = `${url}/env`;
+  const commonVariables = "bceid";
+
+  const response = {
+    backend: Cypress.env("CYPRESS_BACKEND"),
+    apiUrl: Cypress.env("CYPRESS_API_URL"),
+    docManUrl: Cypress.env("CYPRESS_DOC_MAN_URL"),
+    matomoUrl: Cypress.env("CYPRESS_MATOMO_URL"),
+    filesystemProviderUrl: Cypress.env("CYPRESS_FILE_SYSTEM_PROVIDER_URL"),
+    keycloak_clientId: Cypress.env("CYPRESS_KEYCLOAK_CLIENT_ID"),
+    keycloak_resource: Cypress.env("CYPRESS_KEYCLOAK_RESOURCE"),
+    keycloak_url: Cypress.env("CYPRESS_KEYCLOAK_URL"),
+    keycloak_idir_idpHint: commonVariables,
+    keycloak_bceid_idpHint: commonVariables,
+    keycloak_vcauthn_idpHint: commonVariables,
+    siteminder_url: Cypress.env("CYPRESS_KEYCLOAK_URL"),
+    environment: Cypress.env("CYPRESS_ENVIRONMENT"),
+    vcauthn_pres_req_conf_id: "minespace-access-0.1-dev",
+  };
+
+  cy.intercept("GET", environmentUrl, (req) => {
+    req.reply(response);
+  });
+
+  cy.visit(url);
+  cy.url({ timeout: 10000 }).should("include", "localhost");
+  cy.contains("Log in with BCeID").click();
+  cy.get("#username").type(Cypress.env("CYPRESS_TEST_USER"));
+  cy.get("#password").type(Cypress.env("CYPRESS_TEST_PASSWORD"));
+  cy.get("#kc-login").click();
+});
