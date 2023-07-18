7a39b2410fce2e276d10c33ee8f3287691f1c533
Henry Oforeh <henry.oforeh@aot-technologies.com>
2023-07-18 12:20:21 -0700

    [MDS-5337] Setup Cypress for MineSpace (#2591)
    * implement cypress e2e for minespace.
    
    * update test
    
    * update realm-export
    
    * update readme markdown files
    
    * update docker-compose.yaml file.
    
    * fix bug in workflow for minespace.
    
    * fix bug on build
    
    * add to documentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,8 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   ignorePatterns: [
     "services/minespace-web/webpack.config.ts",
+    "services/minespace-web/cypress.config.ts",
+    "services/minespace-web/cypress/**",
     "services/core-web/webpack.config.ts",
     "services/core-web/cypress.config.ts",
     "services/core-web/cypress/**",
