724d086fb4d6f73d2c78d9fdf3f3db562bb71ac5
Sinn <Alex.Sinn@gov.bc.ca>
2023-08-24 10:26:02 -0700

    trying to fix redux common error

--- services/core-web/src/components/mine/Projects/MajorMineApplicationTab.js
+++ services/core-web/src/components/mine/Projects/MajorMineApplicationTab.js
@@ -155,8 +155,8 @@ export class MajorMineApplicationTab extends Component {
             ),
           ]}
           isLoaded={this.state.isLoaded}
-          showVersionHistory={true}
-          enableBulkActions={true}
+          showVersionHistory={false}
+          enableBulkActions={false}
         />
       </div>
     );
