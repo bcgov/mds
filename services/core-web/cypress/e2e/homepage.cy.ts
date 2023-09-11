cd77e261cfaf34e58b4adabfc75b9ac45346f2b9
Simen Fivelstad Smaaberg <simen.smaaberg@ca.ey.com>
2023-09-11 09:55:02 -0700

    Re-added timeout

--- services/core-web/cypress/e2e/homepage.cy.ts
+++ services/core-web/cypress/e2e/homepage.cy.ts
@@ -5,7 +5,7 @@ describe("Home Page", () => {
   });
 
   it("should navigate to the home page successfully", () => {
-    cy.url().should("include", "/home");
+    cy.url({ timeout: 10000 }).should("include", "/home");
     cy.get("h1").should("have.text", "Welcome!");
   });
 });
