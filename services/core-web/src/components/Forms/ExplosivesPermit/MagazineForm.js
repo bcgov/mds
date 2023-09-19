edab53c73ed7e8a460ce6701df0ea137e429e7b1
Isuru Gunawardana <118843449+isuru-aot@users.noreply.github.com>
2023-09-19 14:03:13 -0700

    [MDS-5477] ESUP Field Validation (#2673)
    * fixing es-lint issues
    
    * fixing compare common failure
    
    * updating quantity validation
    
    * fixing common folder failures
    
    * adding tests for positiveNumber validation
    
    * updating validation for longitude

--- services/core-web/src/components/Forms/ExplosivesPermit/MagazineForm.js
+++ services/core-web/src/components/Forms/ExplosivesPermit/MagazineForm.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { FieldArray, Field } from "redux-form";
 import { Form } from "@ant-design/compatible";
-import { required, maxLength, number, lat, lon } from "@common/utils/Validate";
+import { required, maxLength, number, lat, lon, lonNegative, positiveNumber } from "@common/utils/Validate";
 
 import "@ant-design/compatible/assets/index.css";
 import { Collapse, Button, Popconfirm, Col, Row, Divider } from "antd";
@@ -108,7 +108,7 @@ export class MagazineForm extends Component {
                 id={`${field}quantity`}
                 name={`${field}quantity`}
                 component={renderConfig.FIELD}
-                validate={[number, required]}
+                validate={[positiveNumber, required]}
                 disabled={this.props.isProcessed}
               />
             </Form.Item>
@@ -134,7 +134,7 @@ export class MagazineForm extends Component {
                 label="Longitude *"
                 id={`${field}longitude`}
                 name={`${field}longitude`}
-                validate={[number, maxLength(12), lon, required]}
+                validate={[number, maxLength(12), lon, lonNegative, required]}
                 component={renderConfig.FIELD}
                 disabled={this.props.isProcessed}
               />
@@ -165,7 +165,7 @@ export class MagazineForm extends Component {
                 id={`${field}distance_road`}
                 name={`${field}distance_road`}
                 component={renderConfig.FIELD}
-                validate={[number, required]}
+                validate={[positiveNumber, required]}
                 disabled={this.props.isProcessed}
               />
             </Form.Item>
@@ -177,7 +177,7 @@ export class MagazineForm extends Component {
                 id={`${field}distance_dwelling`}
                 name={`${field}distance_dwelling`}
                 component={renderConfig.FIELD}
-                validate={[number, required]}
+                validate={[positiveNumber, required]}
                 disabled={this.props.isProcessed}
               />
             </Form.Item>
@@ -190,7 +190,7 @@ export class MagazineForm extends Component {
               id={`${field}length`}
               name={`${field}length`}
               component={renderConfig.FIELD}
-              validate={[number, required]}
+              validate={[positiveNumber, required]}
               disabled={this.props.isProcessed}
             />
           </Col>
@@ -200,7 +200,7 @@ export class MagazineForm extends Component {
               id={`${field}width`}
               name={`${field}width`}
               component={renderConfig.FIELD}
-              validate={[number, required]}
+              validate={[positiveNumber, required]}
               disabled={this.props.isProcessed}
             />
           </Col>
@@ -210,7 +210,7 @@ export class MagazineForm extends Component {
               id={`${field}height`}
               name={`${field}height`}
               component={renderConfig.FIELD}
-              validate={[number, required]}
+              validate={[positiveNumber, required]}
               disabled={this.props.isProcessed}
             />
           </Col>
