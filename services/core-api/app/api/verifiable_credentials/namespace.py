bb5b76d197a47b7c0430f11a5c93fe6c8c872959
Jason Syrotuck <jasyrotuck@gmail.com>
2023-09-27 12:43:32 -0700

    [CHORE] - Feature/mines act permit cred offer (#2698)
    * update state based on webhooks
    
    * don't use enum inheritance
    
    * missing migration. placeholder table for now
    
    * trailing slash
    
    * add trailing slash
    
    * endpoint to collect and offer credential, no local record
    
    * fix request.arg parsing in webhook handler
    
    * env example
    
    * add local storage of what was issued
    
    * psql syntax

--- services/core-api/app/api/verifiable_credentials/namespace.py
+++ services/core-api/app/api/verifiable_credentials/namespace.py
@@ -3,9 +3,11 @@ from flask_restplus import Namespace
 from app.api.verifiable_credentials.resources.verifiable_credential import VerifiableCredentialResource
 from app.api.verifiable_credentials.resources.verifiable_credential_connections import VerifiableCredentialConnectionResource
 from app.api.verifiable_credentials.resources.verifiable_credential_webhook import VerifiableCredentialWebhookResource
+from app.api.verifiable_credentials.resources.verifiable_credential_map import VerifiableCredentialMinesActPermitResource
 
 api = Namespace('verifiable-credentials', description='Variances actions/options')
 
 api.add_resource(VerifiableCredentialResource, '')
 api.add_resource(VerifiableCredentialWebhookResource, '/webhook/topic/<string:topic>/')
 api.add_resource(VerifiableCredentialConnectionResource, '/oob-invitation/<string:party_guid>')
+api.add_resource(VerifiableCredentialMinesActPermitResource, '/mines-act-permits')
--- services/core-api/app/api/verifiable_credentials/resources/verifiable_credential_webhook.py
+++ services/core-api/app/api/verifiable_credentials/resources/verifiable_credential_webhook.py
@@ -8,22 +8,34 @@ from app.api.utils.resources_mixins import UserMixin
 from app.api.services.traction_service import TractionService
 
 from app.api.verifiable_credentials.models.connection import PartyVerifiableCredentialConnection
+from app.api.verifiable_credentials.models.credentials import PartyVerifiableCredentialMinesActPermit
 
 PRESENT_PROOF = "present_proof"
 CONNECTIONS = "connections"
+CREDENTIAL_OFFER = "credential_offer"
 
 class VerifiableCredentialWebhookResource(Resource, UserMixin):
     @api.doc(description='Endpoint to recieve webhooks from Traction.', params={})
     def post(self, topic):
         current_app.logger.warning(f"TRACTION WEBHOOK: {request.__dict__}")
-        invitation_id = request["invi_msg_id"]
         if topic == CONNECTIONS:
+            invitation_id = request.args.get("invi_msg_id")
             vc_conn = PartyVerifiableCredentialConnection.find_by_invitation_id(invitation_id)
             assert vc_conn, f"{invitation_id} not found"
-            new_state = request["state"]
+            new_state = request.args["state"]
             if new_state != vc_conn.connection_state:
                 vc_conn.connection_state=new_state
                 vc_conn.save()
                 current_app.logger.debug(f"Updated party_vc_conn invitation_id={invitation_id} with state={new_state}")
+        if topic == CREDENTIAL_OFFER:
+            cred_exch_id = request.args.get("cred_exch_id")
+            cred_exch_record = PartyVerifiableCredentialMinesActPermit.find_by_cred_exch_id(cred_exch_id=cred_exch_id)
+            assert cred_exch_record
+            new_state = request.args["state"]
+            if new_state != cred_exch_record.cred_exch_state:
+                cred_exch_record.cred_exch_state=new_state
+                cred_exch_record.save()
+                current_app.logger.debug(f"Updated cred_exch_record cred_exch_id={cred_exch_id} with state={new_state}")
+
         else:
             current_app.logger.info(f"unknown topic={topic} received, payload ={request}")
