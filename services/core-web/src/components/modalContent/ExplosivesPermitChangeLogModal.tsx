f66fe6e625d049327d0d4568515ac9707b1b136d
matbusby <matbusby@gmail.com>
2023-10-11 14:42:12 -0600

    made delete button clickable

---
+++ services/core-web/src/components/modalContent/ExplosivesPermitChangeLogModal.tsx
@@ -0,0 +1,8 @@
+import { Modal } from "antd";
+import React from "react";
+
+const explosivesPermitChangeLogModal = () => {
+  return <Modal></Modal>;
+};
+
+export default explosivesPermitChangeLogModal;
