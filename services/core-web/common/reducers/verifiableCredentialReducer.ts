38df0321e46b29bd9cbc543b22c93a45209c43a2
Jason Syrotuck <jasyrotuck@gmail.com>
2023-10-25 15:54:09 -0700

    [FIX] feature flag condition and typescript conversion (#2745)
    * convert to TS and correct condition
    
    * commo folder sync

--- services/core-web/common/reducers/verifiableCredentialReducer.ts
+++ services/core-web/common/reducers/verifiableCredentialReducer.ts
@@ -1,13 +1,19 @@
 import * as actionTypes from "../constants/actionTypes";
 import { VERIFIABLE_CREDENTIALS } from "../constants/reducerTypes";
+import { RootState } from "@/App";
+import { IVCInvitation, LOADING_STATUS } from "@mds/common";
 
 /**
  * @file verifiableCredentialReducer.js
  * all data associated with verificable credential records.
  */
 
+interface verifiableCredentialState {
+  vcWalletConnectionInvitation: IVCInvitation;
+}
+
 const initialState = {
-  vcWalletConnectionInvitation: {},
+  vcWalletConnectionInvitation: {} as IVCInvitation,
 };
 
 const verifiableCredentialReducer = (state = initialState, action) => {
@@ -26,7 +32,7 @@ const verifiableCredentialReducerObject = {
   [VERIFIABLE_CREDENTIALS]: verifiableCredentialReducer,
 };
 
-export const getVCWalletConnectionInvitation = (state) =>
+export const getVCWalletConnectionInvitation = (state: RootState) =>
   state[VERIFIABLE_CREDENTIALS].vcWalletConnectionInvitation;
 
 export default verifiableCredentialReducerObject;
