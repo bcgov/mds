eb2aa1adb2960ef89fcf033cc3a5f0d0e79dc47b
Jason Syrotuck <jasyrotuck@gmail.com>
2023-10-26 15:45:33 -0700

    el if, and look up connection by invitation_id
    Signed-off-by: Jason Syrotuck <jasyrotuck@gmail.com>

--- services/core-api/app/api/verifiable_credentials/resources/verifiable_credential_webhook.py
+++ services/core-api/app/api/verifiable_credentials/resources/verifiable_credential_webhook.py
@@ -23,27 +23,27 @@ class VerifiableCredentialWebhookResource(Resource, UserMixin):
         webhook_body = request.get_json()
         current_app.logger.warning(f"TRACTION WEBHOOK <topic={topic}>: {webhook_body}")
         if topic == CONNECTIONS:
-            connection_id = webhook_body['connection_id']
-            vc_conn = PartyVerifiableCredentialConnection.query.unbound_unsafe().filter_by(connection_id=connection_id).first()
-            assert vc_conn, f"{connection_id} not found"
+            invitation_id = webhook_body['invitation_id']
+            vc_conn = PartyVerifiableCredentialConnection.query.unbound_unsafe().filter_by(invitation_id=invitation_id).first()
+            assert vc_conn, f"connection.invitation_id={invitation_id} not found"
             new_state = webhook_body["state"]
             if new_state != vc_conn.connection_state:
                 vc_conn.connection_state=new_state
                 vc_conn.save()
-                current_app.logger.debug(f"Updated party_vc_conn connection_id={connection_id} with state={new_state}")
-        if topic == OUT_OF_BAND:
+                current_app.logger.debug(f"Updated party_vc_conn connection_id={vc_conn.connection_id} with state={new_state}")
+        elif topic == OUT_OF_BAND:
             invitation_id = webhook_body["invi_msg_id"]
             vc_conn = PartyVerifiableCredentialConnection.query.unbound_unsafe().filter_by(invitation_id=invitation_id).first()
-            assert vc_conn, f"{invitation_id} not found"
+            assert vc_conn, f"out_of_band.invi_msg_id={invitation_id} not found"
             new_state = webhook_body["state"]
             if new_state != vc_conn.connection_state:
                 vc_conn.connection_state=new_state
                 vc_conn.save()
                 current_app.logger.debug(f"Updated party_vc_conn invitation_id={invitation_id} with state={new_state}")
-        if topic == CREDENTIAL_OFFER:
+        elif topic == CREDENTIAL_OFFER:
             cred_exch_id = webhook_body["credential_exchange_id"]
             cred_exch_record = PartyVerifiableCredentialMinesActPermit.query.unbound_unsafe().filter_by(cred_exch_id=cred_exch_id).first()
-            assert cred_exch_record
+            assert cred_exch_record, f"issue_credential.credential_exchange_id={invitation_id} not found"
             new_state = webhook_body["state"]
             if new_state != cred_exch_record.cred_exch_state:
                 cred_exch_record.cred_exch_state=new_state
@@ -51,4 +51,4 @@ class VerifiableCredentialWebhookResource(Resource, UserMixin):
                 current_app.logger.debug(f"Updated cred_exch_record cred_exch_id={cred_exch_id} with state={new_state}")
 
         else:
-            current_app.logger.info(f"unknown topic={topic}")
+            current_app.logger.info(f"unknown topic '{topic}'")
