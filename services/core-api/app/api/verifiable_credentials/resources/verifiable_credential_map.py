013e61f289394eedd769575decf6b5e0e95e0281
Jason Syrotuck <jasyrotuck@gmail.com>
2023-11-02 11:53:41 -0700

    new schema fixed typo
    S7S2wzcF2giKuwxdeLBk69:3:CL:115414:mds-dev-revok

--- services/core-api/app/api/verifiable_credentials/resources/verifiable_credential_map.py
+++ services/core-api/app/api/verifiable_credentials/resources/verifiable_credential_map.py
@@ -70,7 +70,7 @@ class VerifiableCredentialMinesActPermitResource(Resource, UserMixin):
         credential_attrs["longitude"] = permit_amendment.mine.longitude
         credential_attrs["bond_total"] = permit_amendment.permit.active_bond_total
         credential_attrs["tsf_operation_count"] = len([tsf for tsf in permit_amendment.mine.mine_tailings_storage_facilities if tsf.tsf_operating_status_code == "OPT"])
-        credential_attrs["tsf_care_and_maintainence_count"] = len([tsf for tsf in permit_amendment.mine.mine_tailings_storage_facilities if tsf.tsf_operating_status_code == "CAM"])
+        credential_attrs["tsf_care_and_maintenance_count"] = len([tsf for tsf in permit_amendment.mine.mine_tailings_storage_facilities if tsf.tsf_operating_status_code == "CAM"])
 
         # offer credential
         attributes = [{
