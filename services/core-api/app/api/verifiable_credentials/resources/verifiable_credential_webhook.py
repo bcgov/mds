20354fa452723033ad6c4cd68950e4a5920e7ff8
Jason Syrotuck <jasyrotuck@gmail.com>
2023-11-02 11:14:31 -0700

    clean up logging

--- services/core-api/app/api/verifiable_credentials/resources/verifiable_credential_webhook.py
+++ services/core-api/app/api/verifiable_credentials/resources/verifiable_credential_webhook.py
@@ -26,24 +26,28 @@ class VerifiableCredentialWebhookResource(Resource, UserMixin):
         if topic == CONNECTIONS:
             invitation_id = webhook_body['invitation_msg_id']
             vc_conn = PartyVerifiableCredentialConnection.query.unbound_unsafe().filter_by(invitation_id=invitation_id).first()
+            assert vc_conn, f"connection.invitation_msg_id={invitation_id} not found. webhook_body={webhook_body}"
             vc_conn.connection_id = webhook_body["connection_id"]
-            assert vc_conn, f"connection.invitation_msg_id={invitation_id} not found"
             new_state = webhook_body["state"]
             if new_state != vc_conn.connection_state:
                 vc_conn.connection_state=new_state
                 vc_conn.save()
                 current_app.logger.info(f"Updated party_vc_conn connection_id={vc_conn.connection_id} with state={new_state}")
+                # 'completed' or 'active' should both be considered successful
+                 
+
         elif topic == OUT_OF_BAND:
                 current_app.logger.info(f"out-of-band message invi_msg_id={webhook_body['invi_msg_id']}, state={webhook_body['state']}")
         elif topic == CREDENTIAL_OFFER:
             cred_exch_id = webhook_body["credential_exchange_id"]
             cred_exch_record = PartyVerifiableCredentialMinesActPermit.query.unbound_unsafe().filter_by(cred_exch_id=cred_exch_id).first()
-            assert cred_exch_record, f"issue_credential.credential_exchange_id={cred_exch_id} not found"
+            assert cred_exch_record, f"issue_credential.credential_exchange_id={cred_exch_id} not found. webhook_body={webhook_body}"
             new_state = webhook_body["state"]
             if new_state != cred_exch_record.cred_exch_state:
                 cred_exch_record.cred_exch_state=new_state
                 cred_exch_record.save()
                 current_app.logger.info(f"Updated cred_exch_record cred_exch_id={cred_exch_id} with state={new_state}")
+                # 'deleted' or 'credential_acked' should both be considered successful
         elif topic == PING:
                 current_app.logger.info(f"TrustPing received={request.get_json()}")
         else:
