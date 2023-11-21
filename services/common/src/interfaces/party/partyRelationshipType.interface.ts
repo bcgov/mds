91c257a5863eb9b92fdddad5eda1a87d47e77b76
asinn134 <127789479+asinn134@users.noreply.github.com>
2023-11-21 10:38:32 -0800

    [MDS-5606] Typescript conversions within Summary and Variance sections (#2808)
    * some javascript to typescript conversions for summary and variance sections
    
    * adjustments based on pr comments
    
    * removing vc actionType from pr as Henry's pr contains this already
    
    * scratch the last commit, just adding it back anyways
    
    * adjustment to fix test error
    
    * removing comment
    
    * removing property in permit interface

---
+++ services/common/src/interfaces/party/partyRelationshipType.interface.ts
@@ -0,0 +1,9 @@
+export interface IPartyRelationshipType {
+  active_ind: boolean;
+  description: string;
+  display_order: number;
+  grouping_level: number;
+  mine_party_appt_type_code: string;
+  organization: boolean;
+  person: boolean;
+}
