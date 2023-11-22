95b0432bc15099ead1e19ac2ef9cc2b23f0e5567
Jason Syrotuck <jasyrotuck@gmail.com>
2023-11-22 10:58:31 -0800

    [CHORE] - Developer Documentation for VC Functionality (#2792)
    * first draft.
    
    * styling and explain api_key
    
    * add webhook race condition issue
    
    * filename
    
    * avoid duplication and link to gov documentation
    
    * try and avoid race condition reversing active value
    
    * add dev var
    
    * update to 1.1.1 and remove security as that is solved
    
    * add info to top level readme.
    
    * Update verifiable_credentials.md
    
    * add final key
    
    * logging
    
    * comment with issue
    
    * endpoint security
    
    ---------
    
    Co-authored-by: Tara Epp <102187683+taraepp@users.noreply.github.com>

--- services/core-api/app/api/verifiable_credentials/resources/verifiable_credential_connections.py
+++ services/core-api/app/api/verifiable_credentials/resources/verifiable_credential_connections.py
@@ -2,7 +2,7 @@ from flask import current_app
 from flask_restplus import Resource
 from werkzeug.exceptions import NotFound
 from app.extensions import api
-from app.api.utils.access_decorators import requires_any_of, VIEW_ALL, MINESPACE_PROPONENT
+from app.api.utils.access_decorators import requires_any_of, EDIT_PARTY, MINESPACE_PROPONENT
 
 from app.api.parties.party.models.party import Party
 from app.api.verifiable_credentials.models.connection import PartyVerifiableCredentialConnection
@@ -12,7 +12,7 @@ from app.api.utils.resources_mixins import UserMixin
 
 class VerifiableCredentialConnectionResource(Resource, UserMixin):
     @api.doc(description='Create a connection invitation for a party by guid', params={})
-    @requires_any_of([VIEW_ALL, MINESPACE_PROPONENT])
+    @requires_any_of([EDIT_PARTY, MINESPACE_PROPONENT])
     def post(self, party_guid: str):
         #mine_guid will be param. just easy this way for development
         party = Party.find_by_party_guid(party_guid)
@@ -29,7 +29,7 @@ class VerifiableCredentialConnectionResource(Resource, UserMixin):
  
 
     @api.doc(description='Create a connection invitation for a party by guid', params={})
-    @requires_any_of([VIEW_ALL, MINESPACE_PROPONENT])
+    @requires_any_of([EDIT_PARTY, MINESPACE_PROPONENT])
     @api.marshal_with(PARTY_VERIFIABLE_CREDENTIAL_CONNECTION, code=200, envelope='records')
     def get(self, party_guid: str):
         party_vc_conn = PartyVerifiableCredentialConnection.find_by_party_guid(party_guid=party_guid)
