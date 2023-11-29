2785ee880a507649b611f39a99247699dea307e5
Tara Epp <102187683+taraepp@users.noreply.github.com>
2023-11-29 17:14:29 +0000

    create wrapper to supply redux store + custom initial state. Use it to make dashboard test do something.

--- services/minespace-web/src/tests/components/dashboard/UserDashboard.spec.js
+++ services/minespace-web/src/tests/components/dashboard/UserDashboard.spec.js
@@ -1,15 +1,40 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import { Provider } from "react-redux";
-import { store } from "@/App";
 import { MinesPage } from "@/components/pages/MinesPage";
+import { AUTHENTICATION, USER_MINE_INFO } from "@/constants/reducerTypes";
+import { USER_ROLES } from "@mds/common";
+import { ReduxWrapper } from "@/tests/utils/ReduxWrapper";
+import { MINE_RESPONSE } from "@/tests/mocks/dataMocks";
+
+const initialState = {
+  [USER_MINE_INFO]: {
+    userMineInfo: {
+      records: MINE_RESPONSE.mines,
+      current_page: 1,
+      items_per_page: 10,
+      total: MINE_RESPONSE.mines.length,
+      total_pages: 1,
+    },
+    mineDocuments: [],
+    mine: null,
+  },
+  [AUTHENTICATION]: {
+    isAuthenticated: true,
+    userAccessData: [USER_ROLES.role_minespace_proponent],
+    userInfo: {
+      preferred_username: "test@bceid",
+    },
+    redirect: false,
+    isProponent: true,
+  },
+};
 
 describe("UserDashboard", () => {
   it("renders properly", () => {
     const { container } = render(
-      <Provider store={store}>
+      <ReduxWrapper initialState={initialState}>
         <MinesPage />
-      </Provider>
+      </ReduxWrapper>
     );
     expect(container.firstChild).toMatchSnapshot();
   });
