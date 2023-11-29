35e1f3cd69c10726c978946cd6cdb4b771f86df3
Tara Epp <102187683+taraepp@users.noreply.github.com>
2023-11-29 19:58:49 +0000

    add pagination component instead of next/prev buttons, make Skeleton fancier

--- services/minespace-web/src/components/pages/MinesPage.tsx
+++ services/minespace-web/src/components/pages/MinesPage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import EnvironmentOutlined from "@ant-design/icons/EnvironmentOutlined";
-import { Row, Col, Divider, Typography, Button, Skeleton } from "antd";
+import { Row, Col, Divider, Typography, Skeleton, Pagination } from "antd";
 import { Link } from "react-router-dom";
 import { getUserInfo, isProponent } from "@mds/common/redux/selectors/authenticationSelectors";
 import { getUserMinePageData } from "@/selectors/userMineSelectors";
@@ -20,12 +20,12 @@ export const MinesPage = () => {
   const minePageData = useSelector(getUserMinePageData);
   const userIsProponent = useSelector(isProponent);
   const userInfo = useSelector(getUserInfo);
+  const { mines, current_page, total, items_per_page } = minePageData;
 
   const [isLoaded, setIsLoaded] = useState(false);
   const [desiredPage, setDesiredPage] = useState(DEFAULT_MINE_PAGE);
+  const [desiredPageSize, setDesiredPageSize] = useState(DEFAULT_MINES_PER_PAGE);
 
-  const per_page = DEFAULT_MINES_PER_PAGE;
-  const { mines, current_page, total, total_pages } = minePageData;
   const isProdOrTest = !detectDevelopmentEnvironment();
   const isUnauthenticated = !userIsProponent && isProdOrTest;
 
@@ -33,7 +33,7 @@ export const MinesPage = () => {
     // for "Can't perform a React state update on an unmounted component."
     let isMounted = true;
     setIsLoaded(false);
-    const queryParams = `?page=${desiredPage}&per_page=${per_page}`;
+    const queryParams = `?page=${desiredPage}&per_page=${desiredPageSize}`;
     dispatch(fetchUserMineInfo(queryParams)).then(() => {
       if (isMounted) {
         setIsLoaded(true);
@@ -43,26 +43,71 @@ export const MinesPage = () => {
     return () => {
       isMounted = false;
     };
-  }, [desiredPage]);
+  }, [desiredPage, desiredPageSize]);
 
-  return isUnauthenticated ? (
-    <UnauthenticatedNotice />
-  ) : (
+  const handlePaginationChange = (page, pageSize) => {
+    setDesiredPage(page);
+    setDesiredPageSize(pageSize);
+  };
+
+  // MAKE H1 ON MINESPACE 36px
+  const renderSkeleton = () => {
+    return (
+      <div className="link-card">
+        <ul>
+          {new Array(DEFAULT_MINES_PER_PAGE).fill({}).map((_, i) => (
+            <li key={`skeleton-row-${i}`}>
+              <Skeleton loading={!isLoaded} active paragraph={{ rows: 1 }}></Skeleton>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+
+  const renderNoMines = () => {
+    return (
+      <Row>
+        <Col span={24}>
+          <Typography.Paragraph>
+            You are not authorized to manage information for any mines. Please contact&nbsp;
+            <a className="underline" href={Strings.MDS_EMAIL}>
+              {Strings.MDS_EMAIL}
+            </a>
+            &nbsp;for assistance.
+          </Typography.Paragraph>
+        </Col>
+      </Row>
+    );
+  };
+
+  if (isUnauthenticated) {
+    return <UnauthenticatedNotice />;
+  }
+  return (
     <Row>
       <Col span={24}>
         <Typography.Title>My Mines</Typography.Title>
         <Divider />
-
         <Typography.Title level={4}>Welcome, {userInfo.preferred_username}.</Typography.Title>
 
-        <Skeleton loading={!isLoaded}>
-          {(mines && mines.length > 0 && (
-            <Row gutter={32}>
-              <Col xl={12} lg={14} sm={24}>
-                <Typography.Paragraph>
-                  You are authorized to submit information for the following mines:
-                </Typography.Paragraph>
+        {isLoaded && mines.length === 0 && renderNoMines()}
+
+        {(!isLoaded || (isLoaded && mines?.length > 0)) && (
+          <Row gutter={32}>
+            <Col xl={12} lg={14} sm={24}>
+              <Typography.Paragraph>
+                You are authorized to submit information for the following mines:
+              </Typography.Paragraph>
+
+              {!isLoaded ? (
+                renderSkeleton()
+              ) : (
                 <div className="link-card">
+                  <Typography.Paragraph>
+                    Showing {(current_page - 1) * items_per_page + 1}-
+                    {Math.min(total, current_page * items_per_page)} of {total} mines
+                  </Typography.Paragraph>
                   <ul>
                     {mines
                       .sort((a, b) => (a.mine_name > b.mine_name ? 1 : -1))
@@ -81,7 +126,9 @@ export const MinesPage = () => {
                       ))}
                   </ul>
                 </div>
-              </Col>
+              )}
+            </Col>
+            {isLoaded && mines.length > 0 && (
               <Col xl={12} lg={10} sm={0}>
                 <div style={{ height: "400px", marginTop: "-32px" }}>
                   <Map
@@ -95,42 +142,22 @@ export const MinesPage = () => {
                   &nbsp;for assistance.
                 </Typography.Paragraph>
               </Col>
-            </Row>
-          )) || (
-            <Row>
-              <Col span={24}>
-                <Typography.Paragraph>
-                  You are not authorized to manage information for any mines. Please contact&nbsp;
-                  <a className="underline" href={Strings.MDS_EMAIL}>
-                    {Strings.MDS_EMAIL}
-                  </a>
-                  &nbsp;for assistance.
-                </Typography.Paragraph>
-              </Col>
-            </Row>
-          )}
-        </Skeleton>
-        {true && (
-          <>
-            <Row justify="space-between">
-              <Button
-                onClick={() => setDesiredPage(current_page - 1)}
-                disabled={desiredPage <= DEFAULT_MINE_PAGE}
-                loading={!isLoaded}
-              >
-                Previous
-              </Button>
-              <Button
-                disabled={desiredPage >= total_pages}
-                onClick={() => setDesiredPage(current_page + 1)}
-                loading={!isLoaded}
-              >
-                Next
-              </Button>
-            </Row>
-            Viewing page {current_page} of {total_pages} ({total} mines)
-          </>
+            )}
+          </Row>
         )}
+
+        <Pagination
+          disabled={!isLoaded}
+          onChange={handlePaginationChange}
+          hideOnSinglePage={items_per_page === DEFAULT_MINES_PER_PAGE}
+          current={current_page}
+          pageSize={items_per_page}
+          pageSizeOptions={[10, 20, 50, 100]}
+          showSizeChanger
+          defaultPageSize={DEFAULT_MINES_PER_PAGE}
+          defaultCurrent={DEFAULT_MINE_PAGE}
+          total={total}
+        />
       </Col>
     </Row>
   );
