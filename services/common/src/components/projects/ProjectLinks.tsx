b4b55ca0cf6113eb9fca314cff7e1c43d2d915de
Tara Epp <102187683+taraepp@users.noreply.github.com>
2023-12-08 18:12:46 +0000

    move columns to belong with table, make component be able to display form on form pages and only table otherwise

--- services/common/src/components/projects/ProjectLinks.tsx
+++ services/common/src/components/projects/ProjectLinks.tsx
@@ -1,133 +1,131 @@
-import React, { FC } from "react";
-import { getProject } from "@mds/common/redux/selectors/projectSelectors";
-import { useSelector, connect } from "react-redux";
-// import { Field } from "redux-form";
-// import ProjectLinksTable from "@mds/common/components/projects/ProjectLinksTable";
+import React, { FC, useEffect, useState } from "react";
+import { getProject, getProjects } from "@mds/common/redux/selectors/projectSelectors";
+import { useSelector, useDispatch } from "react-redux";
+import { Field } from "redux-form";
+import ProjectLinksTable from "@mds/common/components/projects/ProjectLinksTable";
 import { ILinkedProject, IProject } from "@mds/common/interfaces";
-import FileOutlined from "@ant-design/icons/FileOutlined";
+
+import { Button, Col, Row, Typography } from "antd";
+import { USER_ROLES } from "@mds/common/constants";
 import {
-  ITableAction,
-  renderActionsColumn,
-  renderDateColumn,
-  renderTextColumn,
-} from "@mds/common/components/common/CoreTableCommonColumns";
-import { ColumnsType } from "antd/es/table";
-import { Typography } from "antd";
-import { useHistory } from "react-router-dom";
-import { USER_ROLES, getStatusDescription } from "@mds/common/constants";
-import { getSystemFlag, userHasRole } from "@mds/common/redux/reducers/authenticationReducer";
-// import { renderConfig } from "@mds/common/components/forms/config";
+  getSystemFlag,
+  isProponent,
+  userHasRole,
+} from "@mds/common/redux/reducers/authenticationReducer";
+import { renderConfig } from "@mds/common/components/forms/config";
+import {
+  createProjectLinks,
+  fetchProjectsByMine,
+} from "@mds/common/redux/actionCreators/projectActionCreator";
+import { dateSorter } from "@mds/common/redux/utils/helpers";
 
 interface ProjectLinksProps {
   viewProjectLink: (record) => string;
+  tableOnly?: boolean;
 }
 
-// interface ReduxProps {
-//   project: any;
-//   systemFlag: any;
-//   // getProject: any;
-//   // getSystemFlag: any;
-//   // user
-// }
+// outside of component to sneak past "hooks can't be rendered conditionally"
+const ProjectLinkInput = ({ unrelatedProjects, mineGuid, projectGuid }) => {
+  const dispatch = useDispatch();
+  const [currentSelection, setCurrentSelection] = useState([]);
 
-const ProjectLinks: FC<ProjectLinksProps> = ({ viewProjectLink }) => {
-  console.log(React);
-
-  console.log("getProject", getProject);
-  console.log("getSystemFlag", getSystemFlag);
-  console.log("userHasRole", userHasRole);
-  console.log("useSelector", useSelector);
-  // console.log(project, systemFlag);
-
-  const history = useHistory();
-  const project = useSelector(getProject);
-  // const systemFlag = useSelector(getSystemFlag);
-  // const isProponent = useSelector((state) => userHasRole(state, USER_ROLES.role_minespace_proponent));
-  // const canEditCore = useSelector((state) => userHasRole(state, USER_ROLES.role_edit_project_summaries));
-  // const hasModifyPermission = isProponent || canEditCore;
-
-  // console.log(isProponent, canEditCore);
-
-  // console.log(systemFlag);
-
-  const hasModifyPermission = true;
-
-  const getLinkedProjects = (data: IProject): ILinkedProject[] => {
-    return data.project_links.flatMap((link) => {
-      const isSameProject = link.project.project_guid === data.project_guid;
-      const isSameRelatedProject = link.related_project.project_guid === data.project_guid;
-
-      const transformProject = (project) => ({
-        ...project,
-        status_code: getStatusDescription(
-          project.project_summary.status_code,
-          project.major_mine_application.status_code,
-          project.information_requirements_table.status_code
-        ),
-        primary_contact: project.contacts.find((c: any) => c.name)?.name || "",
-      });
-
-      return [
-        !isSameProject ? transformProject(link.project) : null,
-        !isSameRelatedProject ? transformProject(link.related_project) : null,
-      ].filter(Boolean) as ILinkedProject[];
+  const addRelatedProjects = () => {
+    console.log("add is clicked!", currentSelection);
+    dispatch(createProjectLinks(mineGuid, projectGuid, currentSelection)).then((resp) => {
+      console.log(resp);
     });
   };
 
-  const actions = [
-    {
-      key: "view",
-      label: "View Project",
-      icon: <FileOutlined />,
-      clickFunction: (_event, record: ILinkedProject) => {
-        history.push(viewProjectLink(record));
-      },
-    },
-    {
-      key: "remove",
-      label: "Remove Linked-project",
-      icon: <FileOutlined />,
-      clickFunction: (_event, record: ILinkedProject) => {
-        console.log("remove linked project");
-      },
-    },
-  ];
-
-  const filterActions = (tableActions: ITableAction[]) => {
-    return hasModifyPermission
-      ? tableActions
-      : tableActions.filter((a) => ["view"].includes(a.key));
+  const handleChange = (args) => {
+    const newValues = args[0];
+    setCurrentSelection(newValues);
   };
 
-  const columns: ColumnsType<ILinkedProject> = [
-    renderTextColumn("project_title", "Project Title", true),
-    renderTextColumn("proponent_project_id", "Project #", true),
-    renderTextColumn("status_code", "Status", true),
-    renderTextColumn("primary_contact", "Contact", true),
-    renderDateColumn("update_timestamp", "Last Updated", true),
-    renderActionsColumn({ actions, recordActionsFilter: filterActions }),
-  ];
+  return (
+    <Row align="bottom" justify="start">
+      <Col>
+        <Field
+          id="linked-projects"
+          name="linked-projects"
+          component={renderConfig.MULTI_SELECT}
+          label="Select one or more related projects (optional)"
+          data={unrelatedProjects}
+          // this produces a TS error "no overload matches this call". But it is in fact fine.
+          onChange={(...args) => handleChange(args)}
+        />
+      </Col>
+      <Col>
+        <Button type="primary" onClick={addRelatedProjects}>
+          Add
+        </Button>
+      </Col>
+    </Row>
+  );
+};
+
+const ProjectLinks: FC<ProjectLinksProps> = ({ viewProjectLink, tableOnly = false, ...props }) => {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [unrelatedProjects, setUnrelatedProjects] = useState([]);
+  const project = useSelector(getProject);
+  const mineProjects = useSelector(getProjects);
+  const systemFlag = useSelector(getSystemFlag);
+  const isUserProponent = useSelector(isProponent);
+  const canEditProjects = useSelector((state) =>
+    userHasRole(state, USER_ROLES.role_edit_project_summaries)
+  );
+  const hasModifyPermission = isUserProponent || canEditProjects;
+  console.log(project);
+
+  const transformUnrelatedProjects = (projects) => {
+    const unrelated = projects
+      .filter((p) => p.project_guid !== project.project_guid)
+      .sort(dateSorter("update_timestamp", false))
+      .map((p) => ({
+        value: p.project_guid,
+        label: `${p.project_title} ${new Date(p.update_timestamp).toDateString()}`,
+      }));
+    console.log(unrelated);
+    return unrelated;
+  };
+
+  useEffect(() => {
+    let isMounted = true;
+
+    if (project?.mine_guid) {
+      dispatch(fetchProjectsByMine({ mineGuid: project.mine_guid })).then(() => {
+        if (isMounted) {
+          setIsLoaded(true);
+        }
+      });
+    }
+    return () => (isMounted = false);
+  }, [project]);
+
+  useEffect(() => {
+    setUnrelatedProjects(transformUnrelatedProjects(mineProjects));
+  }, [mineProjects]);
 
   return (
     <>
-      <Typography.Title level={3}>Related Projects</Typography.Title>
+      <Typography.Title level={3}>Related Project Applications</Typography.Title>
       <Typography.Paragraph>
-        Link related projects to help with communication with your team and the ministry.
+        Description of related major project applications for this mine are listed below.
       </Typography.Paragraph>
-      {/* <Field 
-        name={'linked-projects'}
-        component={renderConfig}
-        label="Select one or more related projects (optional)"
-      /> */}
-      {/* <ProjectLinksTable linkedProjects={getLinkedProjects(project)} tableColumns={columns} /> */}
+      {!tableOnly && (
+        <ProjectLinkInput
+          unrelatedProjects={unrelatedProjects}
+          mineGuid={project.mine_guid}
+          projectGuid={project.project_guid}
+        />
+      )}
+      <ProjectLinksTable
+        project={project}
+        hasModifyPermission={hasModifyPermission}
+        isLoaded={isLoaded}
+      />
     </>
   );
 };
 
-// const mapStateToProps = (state) => ({
-//   project: getProject(state),
-//   systemFlag: getSystemFlag(state),
-// });
-
-// export default connect(mapStateToProps, null)(ProjectLinks);
 export default ProjectLinks;
--- services/common/src/components/projects/ProjectLinksTable.tsx
+++ services/common/src/components/projects/ProjectLinksTable.tsx
@@ -1,21 +1,96 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+
+import { ColumnsType } from "antd/es/table";
+import FileOutlined from "@ant-design/icons/FileOutlined";
 
 import CoreTable from "@mds/common/components/common/CoreTable";
-import { ILinkedProject } from "@mds/common/interfaces";
-import { ColumnsType } from "antd/es/table";
+import { ILinkedProject, IProject } from "@mds/common/interfaces";
+import {
+  ITableAction,
+  renderActionsColumn,
+  renderDateColumn,
+  renderTextColumn,
+} from "../common/CoreTableCommonColumns";
+import { isProponent, getSystemFlag } from "@mds/common/redux/selectors/authenticationSelectors";
+import { userHasRole } from "@mds/common/redux/reducers/authenticationReducer";
+import { USER_ROLES, getStatusDescription } from "../..";
 
 interface IProjectLinksTableProps {
-  linkedProjects: ILinkedProject[];
-  tableColumns: ColumnsType<ILinkedProject>;
+  project: IProject;
+  hasModifyPermission: boolean;
+  isLoaded: boolean;
 }
 
-const ProjectLinksTable: React.FC<IProjectLinksTableProps> = ({ linkedProjects, tableColumns }) => {
-  const coreTableProps = {
-    dataSource: linkedProjects,
-    columns: tableColumns,
+const ProjectLinksTable: React.FC<IProjectLinksTableProps> = ({
+  project,
+  hasModifyPermission,
+  isLoaded,
+}) => {
+  const history = useHistory();
+  const systemFlag = useSelector(getSystemFlag);
+
+  const getLinkedProjects = (data: IProject): ILinkedProject[] => {
+    return data.project_links.flatMap((link) => {
+      const isSameProject = link.project.project_guid === data.project_guid;
+      const isSameRelatedProject = link.related_project.project_guid === data.project_guid;
+
+      const transformProject = (project) => ({
+        ...project,
+        status_code: getStatusDescription(
+          project.project_summary.status_code,
+          project.major_mine_application.status_code,
+          project.information_requirements_table.status_code
+        ),
+        primary_contact: project.contacts.find((c: any) => c.name)?.name || "",
+      });
+
+      return [
+        !isSameProject ? transformProject(link.project) : null,
+        !isSameRelatedProject ? transformProject(link.related_project) : null,
+      ].filter(Boolean) as ILinkedProject[];
+    });
   };
 
-  return <CoreTable {...coreTableProps} />;
+  const actions = [
+    {
+      key: "view",
+      label: "View Project",
+      icon: <FileOutlined />,
+      clickFunction: (_event, record: ILinkedProject) => {
+        console.log("viewing project link!");
+        // history.push(viewProjectLink(record));
+      },
+    },
+    {
+      key: "remove",
+      label: "Remove Linked-project",
+      icon: <FileOutlined />,
+      clickFunction: (_event, record: ILinkedProject) => {
+        console.log("remove linked project");
+      },
+    },
+  ];
+
+  const filterActions = (tableActions: ITableAction[]) => {
+    return hasModifyPermission
+      ? tableActions
+      : tableActions.filter((a) => ["view"].includes(a.key));
+  };
+
+  const columns: ColumnsType<ILinkedProject> = [
+    renderTextColumn("project_title", "Project Title", true),
+    renderTextColumn("proponent_project_id", "Project #", true),
+    renderTextColumn("status_code", "Status", true),
+    renderTextColumn("primary_contact", "Contact", true),
+    renderDateColumn("update_timestamp", "Last Updated", true),
+    renderActionsColumn({ actions, recordActionsFilter: filterActions }),
+  ];
+
+  return (
+    <CoreTable dataSource={getLinkedProjects(project)} columns={columns} condition={isLoaded} />
+  );
 };
 
 export default ProjectLinksTable;
