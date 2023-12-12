47dbca3868564b65a93e41ce2beab49931183b07
Henry Oforeh <henry.oforeh@aot-technologies.com>
2023-12-12 10:25:05 -0700

    remove comment.

--- services/core-web/cypress/e2e/majorprojects.cy.ts
+++ services/core-web/cypress/e2e/majorprojects.cy.ts
@@ -78,12 +78,6 @@ describe("Major Projects", () => {
       force: true,
     });
 
-    // cy.get("#search", { timeout: 10000 }).type("Brenda mine", { force: true });
-
-    // cy.get('[data-cy="apply-filter-button"]', { timeout: 10000 }).click({ force: true });
-
-    // cy.wait(10000)
-
     cy.get('[data-cy="mine-link"]', { timeout: 10000 })
       .eq(1)
       .click({
