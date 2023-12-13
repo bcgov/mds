d29cb514e49fc08de3abf4f0736020751d20c036
Simen Fivelstad Smaaberg <simen.smaaberg@ca.ey.com>
2023-12-13 10:58:07 -0800

    Update Tailings context

--- services/core-web/common/components/tailings/TailingsSummaryPage.tsx
+++ services/core-web/common/components/tailings/TailingsSummaryPage.tsx
@@ -6,7 +6,7 @@ import {
   addPartyRelationship,
   fetchPartyRelationships,
 } from "@mds/common/redux/actionCreators/partiesActionCreator";
-//asasde
+//as
 import { bindActionCreators, compose } from "redux";
 import { clearTsf, storeTsf } from "@mds/common/redux/actions/tailingsActions";
 import {
