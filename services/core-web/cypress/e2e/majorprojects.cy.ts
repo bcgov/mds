c56b352a8869ff8f3563320c97c656b7103a9849
Henry Oforeh <henry.oforeh@aot-technologies.com>
2023-12-13 06:09:01 -0700

    move test to a new file.

--- services/core-web/cypress/e2e/majorprojects.cy.ts
+++ services/core-web/cypress/e2e/majorprojects.cy.ts
@@ -1,10 +1,6 @@
 describe("Major Projects", () => {
   beforeEach(() => {
     cy.login();
-  });
-
-  it("should upload and download a document successfully", () => {
-    const fileName = "dummy.pdf";
 
     cy.get('[data-cy="home-link-button-major-projects"]', { timeout: 10000 }).click({
       force: true,
@@ -17,6 +13,10 @@ describe("Major Projects", () => {
       .click({ force: true });
 
     cy.get('a[data-cy="project-description-view-link"]', { timeout: 5000 }).click();
+  });
+
+  it("should upload and download a document successfully", () => {
+    const fileName = "dummy.pdf";
 
     cy.get("#project-summary-submit").then(($button) => {
       $button[0].click();
@@ -69,60 +69,5 @@ describe("Major Projects", () => {
       // Check that the download request was made successfully
       expect(interception.response.statusCode).to.equal(301);
     });
-  });
-
-  it("should add a new major project successfully", () => {
-    const uniqueProjectName = `Cypress_${Date.now()}`;
-
-    // Navigate to the mines page
-    cy.get('[data-cy="mines-button"]', { timeout: 10000 }).click({ force: true });
-
-    // Navigate to the second mine
-    cy.get('[data-cy="mine-link"]', { timeout: 10000 })
-      .eq(4)
-      .click({ force: true });
-
-    // Hover over the permits and approvals menu
-    cy.get('[data-menu-id^="rc-menu-"][data-menu-id$="permits-and-approvals"]', { timeout: 15000 })
-      .scrollIntoView()
-      .trigger("mouseover", { force: true });
-
-    // Go to major projects
-    cy.get('[data-cy="major-projects-link"]').click({ force: true });
-
-    // Create a new project
-    cy.get('[data-cy="create-new-project"]', { timeout: 15000 }).click({ force: true });
-
-    // Fill in project details
-    cy.get("#project_summary_title", { timeout: 15000 }).type(uniqueProjectName, { force: true });
-    cy.get("#project_summary_description").type("This is just a Cypress test project description", {
-      force: true,
-    });
-    cy.get("#mrc_review_required")
-      .contains("No")
-      .click({ force: true });
-    cy.get("#contacts\\[0\\]\\.name").type("Cypress", { force: true });
-    cy.get("#contacts\\[0\\]\\.email").type("cypress@mds.com", { force: true });
-    cy.get("#contacts\\[0\\]\\.phone_number").type("1234567890", { force: true });
-
-    // Submit the project
-    cy.get('[data-cy="project-summary-submit-button"]').click({ force: true });
-    // wait for API to respond before navigating
-    cy.wait(15000);
-    // Navigate back to projects
-    cy.get('[data-cy="back-to-project-link"]').click({ force: true });
-    cy.wait(15000);
-    // Navigate back to major projects
-    cy.get('[data-cy="back-to-major-project-link"]').click({ force: true });
-    // wait for table to load data
-    cy.wait(15000);
-    // Find the newly created project in the table and assert
-    cy.get("[data-cy=project-name-column]", { timeout: 10000 })
-      .contains(uniqueProjectName)
-      .closest("tr")
-      .as("targetRow");
-
-    // Assert that the row contains the expected data
-    cy.get("@targetRow", { timeout: 10000 }).should("contain", uniqueProjectName);
   });
 });
