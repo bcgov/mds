a4e1d1f00bcfb31cb39edb82cf83c903bc9e79c6
Tara Epp <102187683+taraepp@users.noreply.github.com>
2023-12-18 19:02:32 +0000

    changes to add package redux toolkit, no luck yet

--- services/common/src/redux/rootState.ts
+++ services/common/src/redux/rootState.ts
@@ -1,25 +1,26 @@
 import { sharedReducer } from "@mds/common/redux/reducers/rootReducerShared";
 
 import { createStore, applyMiddleware, combineReducers, $CombinedState } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 import { loadingBarMiddleware } from "react-redux-loading-bar";
 
-const configureStore = () => {
-  if (process.env.NODE_ENV === "development") {
-    return createStore(
-      combineReducers(sharedReducer),
-      applyMiddleware(
-        thunk,
-        loadingBarMiddleware({
-          scope: "modal",
-        })
-      )
-    );
-  }
-  return createStore(
-    combineReducers(sharedReducer),
-    applyMiddleware(thunk, loadingBarMiddleware())
-  );
+const configureStore2 = () => {
+  // if (process.env.NODE_ENV === "development") {
+  //   return createStore(
+  //     combineReducers(sharedReducer),
+  //     applyMiddleware(
+  //       thunk,
+  //       loadingBarMiddleware({
+  //         scope: "modal",
+  //       })
+  //     )
+  //   );
+  // }
+  // return createStore(
+  //   combineReducers(sharedReducer),
+  //   applyMiddleware(thunk, loadingBarMiddleware())
+  // );
 };
 
 const store = configureStore();
