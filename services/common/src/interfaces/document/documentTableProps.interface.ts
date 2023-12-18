1ba2f595528633a3186e09bd143952ae02d2374d
matbusby <matbusby@gmail.com>
2023-12-18 16:46:11 -0700

    set document interface in table back to MineDocument (from IMineDocument)

--- services/common/src/interfaces/document/documentTableProps.interface.ts
+++ services/common/src/interfaces/document/documentTableProps.interface.ts
@@ -1,24 +1,23 @@
-import { FileOperations } from "@mds/common/models/documents/document";
+import { FileOperations, MineDocument } from "@mds/common/models/documents/document";
 import { ColumnType } from "antd/es/table";
-import { IMineDocument } from "@mds/common/interfaces";
 
 interface DocumentTableProps {
   additionalColumnProps?: { key: string; colProps: any }[];
-  additionalColumns?: ColumnType<IMineDocument>[];
+  additionalColumns?: ColumnType<MineDocument>[];
   archiveMineDocuments?: (mineGuid: string, mineDocumentGuids: string[]) => void;
   canArchiveDocuments?: boolean;
   defaultSortKeys?: string[];
   documentColumns?: ColumnType<unknown>[];
   documentParent?: string;
-  documents: IMineDocument[];
+  documents: MineDocument[];
   enableBulkActions?: boolean;
   excludedColumnKeys?: string[];
   fileOperationPermissionMap?: { operation: FileOperations; permission: string | boolean }[];
-  handleRowSelectionChange?: (arg1: IMineDocument[]) => void;
+  handleRowSelectionChange?: (arg1: MineDocument[]) => void;
   isLoaded?: boolean;
   isViewOnly?: boolean;
-  onArchivedDocuments?: (docs?: IMineDocument[]) => void;
-  onReplaceDocument?: (document: IMineDocument) => void;
+  onArchivedDocuments?: (docs?: MineDocument[]) => void;
+  onReplaceDocument?: (document: MineDocument) => void;
   openDocument?: any;
   removeDocument?: (event, doc_guid: string, mine_guid: string) => void;
   replaceAlertMessage?: string;
