978196e9f9ab08ba31a396b69d1d81a5f007f19b
Tara Epp <102187683+taraepp@users.noreply.github.com>
2023-12-19 19:57:18 +0000

    updated tests to use new methods. Snaps had a minor text change in some cases

--- services/core-web/src/tests/store/configureStore.spec.js
+++ services/core-web/src/tests/store/configureStore.spec.js
@@ -1,20 +1,19 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
 import { rootReducer } from "@/reducers/rootReducer";
-import configureStore from "@/store/configureStore";
+import { configureStore } from "@reduxjs/toolkit";
+import getStore from "@/store/configureStore";
 
-const mockDevStore = createStore(rootReducer, applyMiddleware(thunk));
-const mockProdStore = createStore(rootReducer, applyMiddleware(thunk));
+const mockDevStore = configureStore({ reducer: rootReducer });
+const mockProdStore = configureStore({ reducer: rootReducer });
 
 describe("configureStore", () => {
   it("returns production store", () => {
-    const store = configureStore();
+    const store = getStore();
     expect(store.getState()).toEqual(mockDevStore.getState());
   });
 
   it("returns dev store", () => {
     process.env.NODE_ENV = "development";
-    const store = configureStore();
+    const store = getStore();
     expect(store.getState()).toEqual(mockProdStore.getState());
   });
 });
