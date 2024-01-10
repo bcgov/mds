2cf6f42ec59a68e282b214c4358459200a8c6ccd
Isuru Gunawardana <isuru.gunawardana@aot-technologies.com>
2024-01-09 18:47:45 -0800

    adding download all as a zip

--- services/core-web/src/components/noticeOfWork/applications/referals/ReferralConsultationPackage.js
+++ services/core-web/src/components/noticeOfWork/applications/referals/ReferralConsultationPackage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
@@ -16,13 +16,21 @@ import {
   updateNoticeOfWorkApplication,
   fetchImportedNoticeOfWorkApplication,
 } from "@mds/common/redux/actionCreators/noticeOfWorkActionCreator";
-import { getNoticeOfWorkReviews, getNoticeOfWork } from "@mds/common/redux/selectors/noticeOfWorkSelectors";
+import {
+  getNoticeOfWorkReviews,
+  getNoticeOfWork,
+} from "@mds/common/redux/selectors/noticeOfWorkSelectors";
 import { getDropdownNoticeOfWorkApplicationReviewTypeOptions } from "@mds/common/redux/selectors/staticContentSelectors";
 import { modalConfig } from "@/components/modalContent/config";
 import CustomPropTypes from "@/customPropTypes";
 import { EDIT_OUTLINE_VIOLET } from "@/constants/assets";
 import NOWActionWrapper from "@/components/noticeOfWork/NOWActionWrapper";
 import * as Permission from "@/constants/permissions";
+import {
+  documentsCompression,
+  pollDocumentsCompressionProgress,
+} from "@mds/common/redux/actionCreators/documentActionCreator";
+import { downloadFileFromDocumentManager } from "@mds/core-web/common/utils/actionlessNetworkCalls";
 
 /**
  * @constant ReviewNOWApplication renders edit/view for the NoW Application review step
@@ -38,111 +46,93 @@ const propTypes = {
   fetchImportedNoticeOfWorkApplication: PropTypes.func.isRequired,
   type: PropTypes.string.isRequired,
   isTableHeaderView: PropTypes.bool,
+  documentsCompression: PropTypes.func,
+  pollDocumentsCompressionProgress: PropTypes.func,
+  documentType: PropTypes.string,
 };
 
 const defaultProps = {
   importNowSubmissionDocumentsJob: {},
   isTableHeaderView: false,
+  documentType: "all",
 };
 
 export const ReferralConsultationPackage = (props) => {
   const [cancelDownload, setCancelDownload] = useState(false);
+  const [zipFileToDownload, setZipFileToDownload] = useState("");
 
-  const waitFor = (conditionFunction) => {
-    const poll = (resolve) => {
-      if (conditionFunction()) resolve();
-      else setTimeout(() => poll(resolve), 400);
-    };
+  const progressRef = useRef(false);
 
-    return new Promise(poll);
-  };
-
-  const downloadDocument = (url) => {
-    const a = document.createElement("a");
-    a.href = url.url;
-    a.download = url.filename;
-    a.style.display = "none";
-    document.body.append(a);
-    a.click();
-    a.remove();
-  };
+  useEffect(() => {
+    if (zipFileToDownload) {
+      downloadFileFromDocumentManager({ document_manager_guid: zipFileToDownload });
+    }
+  }, [zipFileToDownload]);
 
   const downloadDocumentPackage = (selectedCoreRows, selectedSubmissionRows) => {
-    const docURLS = [];
+    const documentManagerGuids = [
+      ...props.noticeOfWork.filtered_submission_documents
+        .filter((doc) => selectedSubmissionRows.includes(doc.now_application_document_xref_guid))
+        .map((doc) => doc.document_manager_guid),
+      ...props.noticeOfWork.documents
+        .filter((doc) => selectedCoreRows.includes(doc.now_application_document_xref_guid))
+        .map((doc) => doc.mine_document.document_manager_guid),
+    ].filter(Boolean);
 
-    const submissionDocs = props.noticeOfWork.filtered_submission_documents
-      .map((doc) => ({
-        key: doc.mine_document_guid,
-        documentManagerGuid: doc.document_manager_guid,
-        filename: doc.filename,
-      }))
-      .filter((doc) => selectedSubmissionRows.includes(doc.key));
+    const handleCloseCompressionNotification = () => {
+      progressRef.current = false;
+      notification.close(props.documentType);
+      props.compressionInProgress?.(false);
+    };
 
-    const coreDocs = props.noticeOfWork.documents
-      .map((doc) => ({
-        key: doc.now_application_document_xref_guid,
-        documentManagerGuid: doc.mine_document.document_manager_guid,
-        filename: doc.mine_document.document_name,
-      }))
-      .filter((doc) => selectedCoreRows.includes(doc.key));
+    const displayCompressionNotification = (message, description, duration) => {
+      notification.warning({
+        key: props.documentType,
+        className: `progressNotification-${props.documentType}`,
+        message,
+        description,
+        duration,
+        placement: "topRight",
+        onClose: handleCloseCompressionNotification,
+        top: 85,
+      });
+    };
 
-    const totalFiles = submissionDocs.length + coreDocs.length;
-    if (totalFiles === 0) {
-      return;
-    }
+    props
+      .documentsCompression(props.noticeOfWork.mine_guid, documentManagerGuids)
+      .then((response) => {
+        const taskId = response.data && response.data.task_id ? response.data.task_id : null;
+        displayCompressionNotification("Downloading...", "Compressing files to download", 0);
 
-    submissionDocs.forEach((doc) =>
-      getDocumentDownloadToken(doc.documentManagerGuid, doc.filename, docURLS)
-    );
-
-    coreDocs.forEach((doc) =>
-      getDocumentDownloadToken(doc.documentManagerGuid, doc.filename, docURLS)
-    );
-
-    const currentFile = 0;
-    waitFor(() => docURLS.length === totalFiles).then(async () => {
-      // eslint-disable-next-line no-restricted-syntax
-      for (const url of docURLS) {
-        if (cancelDownload) {
-          setCancelDownload(false);
-          props.closeModal();
-          props.setNoticeOfWorkApplicationDocumentDownloadState({
-            downloading: false,
-            currentFile: 0,
-            totalFiles: 1,
-          });
-          downloadDocument(url);
-          // eslint-disable-next-line
-          await new Promise((resolve) => setTimeout(resolve, 2000));
+        if (!taskId) {
+          setTimeout(() => {
+            displayCompressionNotification(
+              "Error starting file compression",
+              "An invalid task id was provided",
+              10
+            );
+          }, 2000);
+        } else {
+          progressRef.current = true;
+          props.compressionInProgress?.(true);
+          const poll = async () => {
+            const { data } = await props.pollDocumentsCompressionProgress(taskId);
+            if (data.state !== "SUCCESS" && progressRef.current) {
+              setTimeout(poll, 2000);
+            } else if (data.state === "SUCCESS" && data?.error.length > 0) {
+              displayCompressionNotification(
+                "Error compressing files",
+                "Unable to retrieve files",
+                10
+              );
+            } else {
+              setZipFileToDownload(data.success_docs[0]);
+              handleCloseCompressionNotification();
+            }
+          };
+          poll();
         }
-        // dispatch toast message
-        notification.success({
-          message: `Successfully Downloaded: ${submissionDocs.length + coreDocs.length} files.`,
-          duration: 10,
-        });
-
-        props.closeModal();
-        props.setNoticeOfWorkApplicationDocumentDownloadState({
-          downloading: true,
-          currentFile,
-          totalFiles,
-        });
-        downloadDocument(url);
-        // eslint-disable-next-line
-        await new Promise((resolve) => setTimeout(resolve, 2000));
-      }
-      notification.success({
-        message: `Successfully Downloaded: ${totalFiles} files.`,
-        duration: 10,
       });
-
-      props.setNoticeOfWorkApplicationDocumentDownloadState({
-        downloading: false,
-        currentFile: 1,
-        totalFiles: 1,
-      });
-      props.closeModal();
-    });
   };
 
   const openDownloadPackageModal = (event) => {
@@ -251,6 +241,8 @@ const mapDispatchToProps = (dispatch) =>
       setNoticeOfWorkApplicationDocumentDownloadState,
       updateNoticeOfWorkApplication,
       fetchImportedNoticeOfWorkApplication,
+      documentsCompression,
+      pollDocumentsCompressionProgress,
     },
     dispatch
   );
