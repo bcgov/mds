c74f91e9b23fc85d0b298890fab450e4cdc95a15
Tara Epp <102187683+taraepp@users.noreply.github.com>
2024-01-15 21:07:53 +0000

    move submitter_name & email out of submission and into report

--- services/minespace-web/src/components/dashboard/mine/reports/ReportPage.tsx
+++ services/minespace-web/src/components/dashboard/mine/reports/ReportPage.tsx
@@ -58,18 +58,13 @@ const ReportPage = () => {
     const submissionCount = report?.mine_report_submissions?.length ?? 0;
     if (submissionCount > 0) {
       const latestSubmission = report.mine_report_submissions[submissionCount - 1];
-      // TODO: submitter_name is dummy data
-      const {
-        mine_report_submission_status_code,
-        submission_date,
-        submitter_name = "Steve",
-      } = latestSubmission;
+
+      const { mine_report_submission_status_code, submission_date } = latestSubmission;
       return {
         ...report,
         mine_report_submission_status_code,
         status: MINE_REPORT_STATUS_HASH[mine_report_submission_status_code],
         submission_date,
-        submitter_name,
       };
     }
     return {
@@ -94,6 +89,7 @@ const ReportPage = () => {
   };
 
   const transformedReportData = transformData(mineReport);
+  console.log(transformedReportData);
   return (
     (loaded && (
       <div>
