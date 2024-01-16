0d1f001707acc52b123e590f890450123109995e
Simen Fivelstad Smaaberg <66635118+simensma-fresh@users.noreply.github.com>
2024-01-16 22:35:43 +0000

    [MDS-5684] Move keycloak sso notification logic into separate file

---
+++ services/minespace-web/public/notify-parent.js
@@ -0,0 +1,3 @@
+// This script is used to notify the parent window of the current URL and is used 
+// by Keycloak in Minespace to seamlessly check if the user is logged in.
+parent.postMessage(location.href, location.origin);
