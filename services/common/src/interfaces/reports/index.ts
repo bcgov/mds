489425da2767a326888b8eb18ca38ac199f4278d
matbusby <matbusby@gmail.com>
2024-01-17 14:12:58 -0700

    update report interface location

--- services/common/src/interfaces/reports/index.ts
+++ services/common/src/interfaces/reports/index.ts
@@ -1,3 +1,4 @@
 export * from "./mineReportSubmission.interface";
 export * from "./mineReportContact.interface";
 export * from "./mineReport.interface";
+export * from "./mineReportDefinition.interface";
