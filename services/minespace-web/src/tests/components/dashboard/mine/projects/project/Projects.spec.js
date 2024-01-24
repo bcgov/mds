4c627a934367ce124a386481960f7576b6dcbb4d
matbusby <matbusby@gmail.com>
2024-01-24 10:06:36 -0700

    fix inproperly setup tests

--- services/minespace-web/src/tests/components/dashboard/mine/projects/project/Projects.spec.js
+++ services/minespace-web/src/tests/components/dashboard/mine/projects/project/Projects.spec.js
@@ -8,13 +8,13 @@ const dispatchProps = {};
 
 const setupProps = () => {
   props.match = { params: { id: "18133c75-49ad-4101-85f3-a43e35ae989a" } };
-  props.mines = MOCK.MINES.mines[MOCK.MINES.mineIds[0]];
+  props.mines = { [props.match.params.id]: MOCK.MINES.mines[MOCK.MINES.mineIds[0]] };
   props.projects = MOCK.PROJECTS;
 };
 
 const setupDispatchProps = () => {
   dispatchProps.fetchMineRecordById = jest.fn(() => Promise.resolve());
-  dispatchProps.fetchProjectsByMine = jest.fn(() => Promise.resolve());
+  dispatchProps.fetchProjectsByMine = jest.fn(({ mineGuid }) => Promise.resolve({}));
 };
 
 beforeEach(() => {
