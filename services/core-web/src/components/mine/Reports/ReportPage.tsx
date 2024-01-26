3b9e679a37cb2a52a2120f5ead174bf04ee0b1d1
Tara Epp <102187683+taraepp@users.noreply.github.com>
2024-01-26 22:21:19 +0000

    copied handleScroll logic into its own component instead of my page, tweaked it a bit

--- services/core-web/src/components/mine/Reports/ReportPage.tsx
+++ services/core-web/src/components/mine/Reports/ReportPage.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { getFormSubmitErrors, getFormValues, isDirty } from "redux-form";
 
@@ -11,15 +11,17 @@ import { fetchMineReport } from "@mds/common/redux/actionCreators/reportActionCr
 
 import ReportDetailsForm from "@mds/common/components/reports/ReportDetailsForm";
 import Loading from "@/components/common/Loading";
-import { Button, Row, Typography } from "antd";
-import ScrollSideMenu from "@mds/common/components/common/ScrollSideMenu";
+import { Button, Row, Tag, Typography } from "antd";
+import { ScrollSideMenuProps } from "@mds/common/components/common/ScrollSideMenu";
+import ScrollSidePageWrapper from "@mds/common/components/common/ScrollSidePageWrapper";
 
 import * as routes from "@/constants/routes";
+import { ArrowLeftOutlined } from "@ant-design/icons";
 
 const ReportPage: FC = () => {
   const dispatch = useDispatch();
   const { mineGuid, reportGuid } = useParams<{ mineGuid: string; reportGuid: string }>();
-  const mineReport = useSelector((state) => getMineReportById(state, reportGuid));
+  const mineReport: IMineReport = useSelector((state) => getMineReportById(state, reportGuid));
   const mine = useSelector((state) => getMineById(state, mineGuid));
 
   const [loaded, setIsLoaded] = useState(Boolean(mineReport && mine));
@@ -31,7 +33,7 @@ const ReportPage: FC = () => {
   const isFormDirty = useSelector(isDirty(FORM.VIEW_EDIT_REPORT));
 
   useEffect(() => {
-    if (mineGuid && (!mine || mine.mine_guid !== mineGuid)) {
+    if (!mine || mine.mine_guid !== mineGuid) {
       dispatch(fetchMineRecordById(mineGuid));
     }
     if (
@@ -54,13 +56,13 @@ const ReportPage: FC = () => {
 
   const getToggleEditButton = () => {
     return isEditMode ? (
-      <Row justify="space-between">
+      <Row justify="end">
         <Button onClick={() => setIsEditMode(false)} disabled={isFormDirty}>
           View Report
         </Button>
-        <Button htmlType="submit" type="primary">
+        {/* <Button htmlType="submit" type="primary">
           Save Changes
-        </Button>
+        </Button> */}
       </Row>
     ) : (
       <Row justify="end">
@@ -75,7 +77,9 @@ const ReportPage: FC = () => {
     url: routes.REPORT_VIEW_EDIT,
     params: [mineGuid, reportGuid],
   };
-  const scrollSideMenuProps = {
+  const headerHeight = 140;
+
+  const scrollSideMenuProps: ScrollSideMenuProps = {
     menuOptions: [
       { href: "report-type", title: "Report Type" },
       { href: "report-information", title: "Report Information" },
@@ -85,32 +89,52 @@ const ReportPage: FC = () => {
     featureUrlRoute: sideBarRoute.url.hashRoute,
     featureUrlRouteArguments: sideBarRoute.params,
   };
+
+  const HeaderContent = (
+    <>
+      <div className="padding-lg">
+        <Typography.Title level={1}>
+          Report Name
+          <span>
+            <Tag title="mine name">tag content</Tag>
+          </span>
+        </Typography.Title>
+        <Link to="https://www.google.ca">
+          <ArrowLeftOutlined />
+          Back to: Reports
+        </Link>
+      </div>
+    </>
+  );
+
+  const PageContent = (
+    <>
+      <div style={{ backgroundColor: "green", width: "100%", height: "40px" }}>test alignment</div>
+      {getToggleEditButton()}
+      <ReportDetailsForm
+        mineGuid={mineGuid}
+        initialValues={mineReport}
+        handleSubmit={(values) => console.log(values)}
+        isEditMode={isEditMode}
+        formButtons={
+          isEditMode ? (
+            <Button htmlType="submit" type="primary">
+              Save Changes
+            </Button>
+          ) : null
+        }
+      />
+      {!isEditMode && getToggleEditButton()}
+    </>
+  );
+
   return loaded ? (
-    <div className="page">
-      <div className="padding-lg view--header">
-        <Typography.Title level={1}>Report Name</Typography.Title>
-      </div>
-      <div className="side-menu">
-        <ScrollSideMenu {...scrollSideMenuProps} />
-      </div>
-      <div className="side-menu--content">
-        {getToggleEditButton()}
-        <ReportDetailsForm
-          mineGuid={mineGuid}
-          initialValues={mineReport}
-          handleSubmit={(values) => console.log(values)}
-          isEditMode={isEditMode}
-          formButtons={
-            isEditMode ? (
-              <Button htmlType="submit" type="primary">
-                Save Changes
-              </Button>
-            ) : null
-          }
-        />
-        {!isEditMode && getToggleEditButton()}
-      </div>
-    </div>
+    <ScrollSidePageWrapper
+      menuProps={scrollSideMenuProps}
+      headerHeight={headerHeight}
+      header={HeaderContent}
+      content={PageContent}
+    />
   ) : (
     <Loading />
   );
