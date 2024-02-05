e4dc4a96f51c1b9b43543a09f4ac416e173c6ef1
Simen Fivelstad Smaaberg <66635118+simensma-fresh@users.noreply.github.com>
2024-02-05 10:06:58 -0800

    [MDS-5409] Fixed startup command of Core Celery (#2933)
    MDS-5409 Fixed startup command of Core Celery

--- services/core-api/celery.sh
+++ services/core-api/celery.sh
@@ -13,4 +13,4 @@
 
 cd /app || exit
 
-celery worker -n core_tasks@%h -A app.tasks.celery_entrypoint -Q core_tasks --loglevel=debug --concurrency=1 -B --scheduler redbeat.RedBeatScheduler -E --pidfile /tmp/celerybeat.pid -s /tmp/celerybeat-schedule
+celery -A app.tasks.celery_entrypoint worker -n core_tasks@%h -Q core_tasks --loglevel=debug --concurrency=1 -B --scheduler redbeat.RedBeatScheduler -E --pidfile /tmp/celerybeat.pid -s /tmp/celerybeat-schedule
