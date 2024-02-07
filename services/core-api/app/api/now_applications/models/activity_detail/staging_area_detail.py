dced424fb41b13d4679fc133c3f6ababc28d4792
Simen Fivelstad Smaaberg <66635118+simensma-fresh@users.noreply.github.com>
2024-02-07 23:30:10 +0000

    MDS-5730 Made staging area and building detail polymorphic identity unique

--- services/core-api/app/api/now_applications/models/activity_detail/staging_area_detail.py
+++ services/core-api/app/api/now_applications/models/activity_detail/staging_area_detail.py
@@ -2,7 +2,6 @@ from app.api.now_applications.models.activity_detail.activity_detail_base import
 
 
 class StagingAreaDetail(ActivityDetailBase):
-    __mapper_args__ = {'polymorphic_identity': 'camp'}
-
+    __mapper_args__ = {'polymorphic_identity': 'staging_area'}
     def __repr__(self):
         return f'<{self.__class__.__name__} {self.activity_detail_id}>'
