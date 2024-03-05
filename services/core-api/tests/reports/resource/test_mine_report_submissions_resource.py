c5d50c4ef6d4eed3ce563e127e4577916ef56545
Tara Epp <102187683+taraepp@users.noreply.github.com>
2024-03-05 17:48:05 +0000

    add BE test for GET && POST requested report

--- services/core-api/tests/reports/resource/test_mine_report_submissions_resource.py
+++ services/core-api/tests/reports/resource/test_mine_report_submissions_resource.py
@@ -1,9 +1,5 @@
 import json
-import uuid
-import pytest
-from datetime import datetime
 
-from app.api.mines.mine.models.mine import Mine
 from app.api.mines.reports.models.mine_report import MineReport
 from app.api.mines.reports.models.mine_report_submission import MineReportSubmission
 from app.api.mines.reports.models.mine_report_definition import MineReportDefinition
@@ -24,6 +20,20 @@ def test_get_all_mine_report_submissions_for_report(test_client, db_session, aut
     assert len(mine_report.mine_report_submissions) == len(get_data)
     assert str(mine_report.mine_report_guid) == get_data[0]['mine_report_guid']
     assert get_resp.status_code == 200
+
+def test_get_mine_report_with_no_submissions(test_client, db_session, auth_headers):
+    mine_report = MineReportFactory(mine_report_submissions=0)
+
+    params = f'?latest_submission=true&mine_report_guid={mine_report.mine_report_guid}'
+    get_resp = test_client.get(
+        f'mines/reports/submissions{params}', headers=auth_headers['full_auth_header'])
+    get_data = json.loads(get_resp.data.decode())
+
+    assert get_resp.status_code == 200
+    assert get_data['mine_report_guid'] == str(mine_report.mine_report_guid)
+    assert get_data['mine_report_submission_status_code'] == "NON"
+    assert get_data['mine_report_submission_guid'] == None    
+    assert get_data['documents'] == None 
 
 def test_get_latest_mine_report_submission_for_report(test_client, db_session, auth_headers):
     mine_report = MineReportFactory()
@@ -82,6 +92,38 @@ def test_post_initial_crr_mine_report_submission(test_client, db_session, auth_h
     assert num_submissions == 1
     assert latest_submission['mine_report_submission_status_code'] == "INI"
 
+def test_post_requested_report(test_client, db_session, auth_headers):
+    mine_report = MineReportFactory(mine_report_submissions=0)
+
+    new_document_data = MineDocumentFactory()
+    new_document = {
+        'document_name': new_document_data.document_name,
+        'document_manager_guid': new_document_data.document_manager_guid
+    }
+    mine_report_definition = MineReportDefinition.get_all()[0]
+
+    submission_data = {
+        'description_comment': "description comment",
+        'documents': [new_document],
+        'due_date': "2024-02-11",
+        'mine_guid': mine_report.mine_guid,
+        'mine_report_definition_guid': str(mine_report_definition.mine_report_definition_guid),
+        'received_date': "2024-02-09",
+        'submission_year': "2024",
+        'submitter_email': "email@email.com",
+        'submitter_name': "Submitter Name",        
+    }
+
+    # post from Minespace
+    post_resp = test_client.post(
+        f'mines/reports/submissions', headers=auth_headers['proponent_only_auth_header'], json=submission_data)
+    post_data = json.loads(post_resp.data.decode())
+
+    assert post_resp.status_code == 201
+    assert post_data['mine_report_submission_status_code'] == "INI"
+    assert post_data['mine_report_submission_guid'] is not None
+    assert post_data['mine_report_definition_guid'] == str(mine_report_definition.mine_report_definition_guid)
+
 def test_post_additional_mine_report_submission(test_client, db_session, auth_headers):
     mine_report = MineReportFactory(mine_report_submissions=1)
     first_submission = mine_report.mine_report_submissions[0]
@@ -122,7 +164,6 @@ def test_post_additional_mine_report_submission(test_client, db_session, auth_he
 
     post_resp = test_client.post(
         f'mines/reports/submissions', headers=auth_headers['full_auth_header'], json=submission_data)
-    post_data = json.loads(post_resp.data.decode())
 
     updated_report = MineReport.find_by_mine_report_guid(str(mine_report.mine_report_guid))
     latest_submission = MineReportSubmission.find_latest_by_mine_report_guid(str(mine_report.mine_report_guid)).json()
