9064494134ceb65f110d0a48c9a4b2623ca32ab5
Henry Oforeh <henry.oforeh@aot-technologies.com>
2024-03-07 15:02:36 -0700

    data clean update query

---
+++ migrations/sql/V2024.03.07.01.24__update_mine_report_definition_compliance_article_xref_prod.sql
@@ -0,0 +1,169 @@
+/** The report name in mine_report_definition is 'Terrain Incident Report' 
+ The update query here links the compliance_article with id '1060' 
+ and description 'Terrain incident report'. 
+ guid id for the mine_report_definition_compliance_article_xref = '2ec6602d-1628-4f73-ae9b-5a6bbfbc5102'
+ **/
+UPDATE
+    mine_report_definition_compliance_article_xref
+SET
+    compliance_article_id = 1060
+WHERE
+    mine_report_definition_compliance_article_xref_guid = '2ec6602d-1628-4f73-ae9b-5a6bbfbc5102';
+
+/** The report name in mine_report_definition is 'TSF Emergency Preparedness and Response Plan' 
+ The update query here links the compliance_article with id '1073' 
+ and description 'TSF Emergency Preparedness and Response Plan'
+ guid id for the mine_report_definition_compliance_article_xref = '0519667f-0313-442f-9147-af5544926c52'
+ **/
+UPDATE
+    mine_report_definition_compliance_article_xref
+SET
+    compliance_article_id = 1073
+WHERE
+    mine_report_definition_compliance_article_xref_guid = '0519667f-0313-442f-9147-af5544926c52';
+
+/** The report name in mine_report_definition is 'Annual Reclamation Report' 
+ The update query here links the compliance_article with id '1080' 
+ and description 'Annual Summary of Work and Reclamation Report'
+ guid id for the mine_report_definition_compliance_article_xref = 'd0063abe-40bb-4c68-8c65-450a117e27b1'
+ **/
+UPDATE
+    mine_report_definition_compliance_article_xref
+SET
+    compliance_article_id = 1080
+WHERE
+    mine_report_definition_compliance_article_xref_guid = 'd0063abe-40bb-4c68-8c65-450a117e27b1';
+
+/** The report name in mine_report_definition is 'Annual DSI' 
+ The update query here links the compliance_article with id '1075' 
+ and description 'Annual Dam Safety Inspection'
+ guid id for the mine_report_definition_compliance_article_xref = '1ca2ae04-d1a7-43fe-82ff-4800c05c2463'
+ **/
+UPDATE
+    mine_report_definition_compliance_article_xref
+SET
+    compliance_article_id = 1075
+WHERE
+    mine_report_definition_compliance_article_xref_guid = '1ca2ae04-d1a7-43fe-82ff-4800c05c2463';
+
+/** The report name in mine_report_definition is 'ITRB Activities Report' 
+ The update query here links the compliance_article with id '1076' 
+ and description 'Independent Tailings Review Board Activities Report'
+ guid id for the mine_report_definition_compliance_article_xref = '7a894f36-3e9f-4601-bc9d-f4d45995b675'
+ **/
+UPDATE
+    mine_report_definition_compliance_article_xref
+SET
+    compliance_article_id = 1076
+WHERE
+    mine_report_definition_compliance_article_xref_guid = '7a894f36-3e9f-4601-bc9d-f4d45995b675';
+
+/** The report name in mine_report_definition is 'Summary of TSF or Dam Safety Recommendations' 
+ The update query here links the compliance_article with id '1077' 
+ and description 'Summary of Tailings Storage Facility or Dam Safety Recommendations'
+ guid id for the mine_report_definition_compliance_article_xref = 'c6dba2c5-7002-43c6-8187-8606474d26f1'
+ **/
+UPDATE
+    mine_report_definition_compliance_article_xref
+SET
+    compliance_article_id = 1077
+WHERE
+    mine_report_definition_compliance_article_xref_guid = 'c6dba2c5-7002-43c6-8187-8606474d26f1';
+
+/** The report name in mine_report_definition is 'Performance of High Risk Dumps' 
+ The update query here links the compliance_article with id '1078' 
+ and description 'Performance of High Risk Dumps'
+ guid id for the mine_report_definition_compliance_article_xref = 'ebd59f37-f189-4c13-8f4d-08a5071553d2'
+ **/
+UPDATE
+    mine_report_definition_compliance_article_xref
+SET
+    compliance_article_id = 1078
+WHERE
+    mine_report_definition_compliance_article_xref_guid = 'ebd59f37-f189-4c13-8f4d-08a5071553d2';
+
+/** The report name in mine_report_definition is 'Dam Safety Review' 
+ The update query here links the compliance_article with id '1083' 
+ and description 'Dam Safety Review'
+ guid id for the mine_report_definition_compliance_article_xref = 'd7e70e4b-07aa-465c-bcdb-3b4e20f35946'
+ **/
+UPDATE
+    mine_report_definition_compliance_article_xref
+SET
+    compliance_article_id = 1083
+WHERE
+    mine_report_definition_compliance_article_xref_guid = 'd7e70e4b-07aa-465c-bcdb-3b4e20f35946';
+
+/** The report name in mine_report_definition is 'Annual Reconciliation of Water Balance and Water Management Plans' 
+ The update query here links the compliance_article with id '1069' 
+ and long description 'This section of the Code does not trigger a report submission requirement. The reconciled Water Balance and Water Management Plan for Tailings Storage Facilities can be submitted under 10.4.5(a) of the Code. Water Balance and Water Quality Models and Water Management and Monitoring Plans for mine sites should be submitted through the Applications feature in MineSpace, or as a permit required report in MineSpace.'
+ compliance_article with id = 1069 is one of the recently updated reports and the long description best describes the report name
+ better than the previous compliance_articles with similar short descriptions and completely different long descriptions.
+ guid id for the mine_report_definition_compliance_article_xref = '7aab58fa-0c54-4f99-aa49-aaed24c7b3ac'
+ **/
+UPDATE
+    mine_report_definition_compliance_article_xref
+SET
+    compliance_article_id = 1069
+WHERE
+    mine_report_definition_compliance_article_xref_guid = '7aab58fa-0c54-4f99-aa49-aaed24c7b3ac';
+
+/** The report name in mine_report_definition is 'Tailings Management System' 
+ The update query here links the compliance_article with id '1071' 
+ and description 'Tailings Management System'
+ guid id for the mine_report_definition_compliance_article_xref = 'e3422d98-d868-4ce4-a8b1-960174e58850'
+ **/
+UPDATE
+    mine_report_definition_compliance_article_xref
+SET
+    compliance_article_id = 1071
+WHERE
+    mine_report_definition_compliance_article_xref_guid = 'e3422d98-d868-4ce4-a8b1-960174e58850';
+
+/** The report name in mine_report_definition is 'TSF Risk Assessment' 
+ The update query here links the compliance_article with id '1072' 
+ and description 'TSF Risk Assessment'
+ guid id for the mine_report_definition_compliance_article_xref = 'eabebba5-ae0c-4a54-9b81-88c14729c9c6'
+ **/
+UPDATE
+    mine_report_definition_compliance_article_xref
+SET
+    compliance_article_id = 1072
+WHERE
+    mine_report_definition_compliance_article_xref_guid = 'eabebba5-ae0c-4a54-9b81-88c14729c9c6';
+
+/** The report name in mine_report_definition is 'TSF and Dam Registry Updates' 
+ The update query here links the compliance_article with id '1079' 
+ and description 'Tailings Storage Facility and Dam Registry'
+ guid id for the mine_report_definition_compliance_article_xref = 'e639ac0c-a419-4913-864c-b1edfa0aba36'
+ **/
+UPDATE
+    mine_report_definition_compliance_article_xref
+SET
+    compliance_article_id = 1079
+WHERE
+    mine_report_definition_compliance_article_xref_guid = 'e639ac0c-a419-4913-864c-b1edfa0aba36';
+
+/** The report name in mine_report_definition is 'Monthly Medical Aid Report' 
+ The update query here links the compliance_article with id '1021' 
+ and description 'Monthly Medical Aid Report'
+ guid id for the mine_report_definition_compliance_article_xref = 'da3c976f-c869-40fd-bab6-4cb7a1c27058'
+ **/
+UPDATE
+    mine_report_definition_compliance_article_xref
+SET
+    compliance_article_id = 1021
+WHERE
+    mine_report_definition_compliance_article_xref_guid = 'da3c976f-c869-40fd-bab6-4cb7a1c27058';
+
+/** The report name in mine_report_definition is 'Annual Summary of Work and Reclamation Report' 
+ The update query here links the compliance_article with id '1080'
+ and description 'Annual Summary of Work and Reclamation Report'
+ guid id for the mine_report_definition_compliance_article_xref = '1bd90be8-bc5b-40fa-8c9d-3fb878e1c8d9'
+ **/
+UPDATE
+    mine_report_definition_compliance_article_xref
+SET
+    compliance_article_id = 1080
+WHERE
+    mine_report_definition_compliance_article_xref_guid = '1bd90be8-bc5b-40fa-8c9d-3fb878e1c8d9';
