56832580d5ee0424097ffaa72b4289cdf55c0939
Jason Syrotuck <jasyrotuck@gmail.com>
2024-03-15 14:56:42 -0700

    [MDS-5835] Re-organize Minespace VC UI (#3001)
    * file separations
    
    * container fetches record, change language
    
    * remove unused import
    
    * remove unused, combine imports
    
    * remove unused match
    
    * change condition
    
    * call function, don't just refer to ti

--- services/minespace-web/src/components/dashboard/mine/permits/Permits.tsx
+++ services/minespace-web/src/components/dashboard/mine/permits/Permits.tsx
@@ -31,10 +31,7 @@ export const Permits: FC<PermitsProps> = ({ mine, permits, explosivesPermits, ..
 
   useEffect(() => {
     if (!isLoaded) {
-      Promise.all([
-        props.fetchPermits(mine.mine_guid),
-        props.fetchExplosivesPermits(mine.mine_guid),
-      ]).then(() => {
+      Promise.all([props.fetchExplosivesPermits(mine.mine_guid)]).then(() => {
         setIsLoaded(true);
       });
     }
@@ -58,93 +55,6 @@ export const Permits: FC<PermitsProps> = ({ mine, permits, explosivesPermits, ..
     });
   };
 
-  const DigitalWalletSection = () => {
-    // list of permittees from open permits with no duplicates
-    const permittees: any[] = [];
-    permits
-      .filter((p) => p.permit_status_code === "O")
-      .forEach((permit) => {
-        if (!permittees.find((p) => p.current_permittee_guid === permit.current_permittee_guid)) {
-          const permittee = {
-            name: permit.current_permittee,
-            current_permittee_guid: permit.current_permittee_guid,
-            status: permit.current_permittee_digital_wallet_connection_state,
-          };
-          permittees.push(permittee);
-        }
-      });
-    const colourMap = {
-      Inactive: "#D8292F",
-      Pending: "#F1C21B",
-      Active: "#45A776",
-    };
-
-    const showDigitalWalletInfo = permittees.some(
-      (p) => VC_CONNECTION_STATES[p.status] !== VC_CONNECTION_STATES.active
-    );
-    return (
-      <>
-        {showDigitalWalletInfo && (
-          <div className="light-grey-background padding-md">
-            <Typography.Title level={5}>Generate your digital wallet connection</Typography.Title>
-            <Typography.Paragraph>
-              A digital wallet is a digital version of a physical wallet that enables organizations
-              to store, send and receive digital credentials. Digital credentials are
-              cryptographically protected to prevent the data from being altered and are exchanged
-              with digital wallets. Digital credentials enable mines to share data, certifications,
-              and credentials with investors, purchasers, regulators and more securely and in
-              seconds.{" "}
-              <a
-                href="https://digital.gov.bc.ca/learning/case-studies/energy-mines-digital-trust-pilot/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn more
-              </a>
-            </Typography.Paragraph>
-          </div>
-        )}
-
-        <Typography.Title level={5}>Permittee Digital Wallet Connection Status</Typography.Title>
-        {permittees.map((permittee) => (
-          <Row
-            style={{ maxWidth: 690, padding: "16px 8px" }}
-            key={permittee.current_permittee_guid}
-          >
-            <Col span={8}>{permittee.name}</Col>
-            <Col span={6}>
-              <Badge
-                color={colourMap[VC_CONNECTION_STATES[permittee.status]]}
-                text={VC_CONNECTION_STATES[permittee.status]}
-              />
-            </Col>
-            <Col span={10}>
-              {VC_CONNECTION_STATES[permittee.status] === VC_CONNECTION_STATES.active ? (
-                <Button disabled type="primary">
-                  Digital Wallet Connection: Active
-                </Button>
-              ) : (
-                <Button
-                  type="primary"
-                  onClick={(e) =>
-                    openVCWalletInvitationModal(
-                      e,
-                      permittee.current_permittee_guid,
-                      permittee.name,
-                      permittee.status
-                    )
-                  }
-                >
-                  Generate Connection Invitation
-                </Button>
-              )}
-            </Col>
-          </Row>
-        ))}
-      </>
-    );
-  };
-
   return (
     <Row>
       <Col span={24}>
@@ -153,16 +63,6 @@ export const Permits: FC<PermitsProps> = ({ mine, permits, explosivesPermits, ..
         <Typography.Paragraph>
           The below table displays all of the <strong>permit applications</strong> associated with
           this mine.
-          {mine.major_mine_ind && isFeatureEnabled(Feature.VERIFIABLE_CREDENTIALS) && (
-            <>
-              <Typography.Text>
-                {" "}
-                Major mines operators in B.C. can now use digital credentials to prove that they
-                hold a valid Mines Act Permit from the Government of B.C.
-              </Typography.Text>
-              {isFeatureEnabled(Feature.VERIFIABLE_CREDENTIALS) && <DigitalWalletSection />}
-            </>
-          )}
         </Typography.Paragraph>
         <PermitsTable
           isLoaded={isLoaded}
