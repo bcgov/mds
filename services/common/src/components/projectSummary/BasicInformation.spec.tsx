4d740550f85c54160aea8542dff87297d021d15b
Tara Epp <102187683+taraepp@users.noreply.github.com>
2024-03-19 15:43:06 +0000

    remove commented out code

--- services/common/src/components/projectSummary/BasicInformation.spec.tsx
+++ services/common/src/components/projectSummary/BasicInformation.spec.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { render } from "@testing-library/react";
-// import { BasicInformation } from "@/components/Forms/projects/projectSummary/BasicInformation";
-// import { ReduxWrapper } from "@/tests/utils/ReduxWrapper";
 import FormWrapper from "@mds/common/components/forms/FormWrapper";
 import { FORM } from "@mds/common";
 import { PROJECTS } from "@mds/common/constants/reducerTypes";
 import { ReduxWrapper } from "@mds/common/tests/utils/ReduxWrapper";
 import { PROJECT } from "@mds/common/tests/mocks/dataMocks";
 import BasicInformation from "./BasicInformation";
-// import { PROJECT } from "@/tests/mocks/dataMocks";
 
 const initialState = {
   [PROJECTS]: {
