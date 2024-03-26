4a06adacc5a5d797b5dba42e740a8636c5a90c22
Isuru Gunawardana <isuru.gunawardana@aot-technologies.com>
2024-03-26 13:30:11 -0700

    fixing report name

--- services/core-api/app/api/mines/reports/models/mine_report.py
+++ services/core-api/app/api/mines/reports/models/mine_report.py
@@ -171,11 +171,11 @@ class MineReport(SoftDeleteMixin, AuditMixin, Base):
           "ms_report_page_link": ms_report_page_link
         }
 
-        trigger_notification(f'Your ({self.mine_report_definition_report_name}) report has been recieved',
+        trigger_notification(f'Your ({self.report_name}) report has been recieved',
                               ActivityType.mine_report_submitted, self.mine,
                               'MineReportSubmission', self.mine_report_guid, recipients=ActivityRecipients.minespace_users)
 
-        trigger_notification(f'A ({self.mine_report_definition_report_name}) report has been recieved',
+        trigger_notification(f'A ({self.report_name}) report has been recieved',
                               ActivityType.mine_report_submitted, self.mine,
                               'MineReportSubmission', self.mine_report_guid, recipients=ActivityRecipients.core_users)
 
