c271966a4fa16cce5de3c45bea6fbe9472d4ebac
Tara Epp <102187683+taraepp@users.noreply.github.com>
2024-04-04 23:14:55 +0000

    missed change to cypress test

--- services/core-web/cypress/e2e/createproject.cy.ts
+++ services/core-web/cypress/e2e/createproject.cy.ts
@@ -39,6 +39,9 @@ describe("Major Projects", () => {
     cy.get("#contacts\\[0\\]\\.email").type("cypress@mds.com", { force: true });
     cy.get("#contacts\\[0\\]\\.phone_number").type("1234567890", { force: true });
     cy.get('[data-cy="checkbox-authorization-OTHER"]').click({ force: true });
+    cy.get(
+      '[name="authorizations.OTHER[0].authorization_description"]'
+    ).type("legislation description", { force: true });
 
     // Submit the project
     cy.get('[data-cy="project-summary-submit-button"]').click({ force: true });
