0e570799dd961247fdbf0824ea961385ce88f94f
matbusby <matbusby@gmail.com>
2024-04-05 16:46:51 -0600

    updated content of modal to use new DigitalCredentialDetails component

--- services/core-web/src/components/mine/DigitalPermitCredential/CredentialContentModal.tsx
+++ services/core-web/src/components/mine/DigitalPermitCredential/CredentialContentModal.tsx
@@ -1,16 +1,29 @@
-import { Col, Row, Typography } from "antd";
-import React, { useEffect } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   fetchCredentialExchangeDetails,
   getCredentialExchangeDetails,
 } from "@mds/common/redux/slices/verifiableCredentialsSlice";
+import { ICredentialExchange, IMine } from "@mds/common";
+import DigitalCredentialDetails from "@/components/mine/DigitalPermitCredential/DigitalCredentialDetails";
 
-const CredentialContentModal = ({ partyGuid, credExchId }) => {
-  const { Paragraph } = Typography;
+interface CredentialContentModalProps {
+  partyGuid: string;
+  credExchId: string;
+  mine: IMine;
+}
+
+const CredentialContentModal: FC<CredentialContentModalProps> = ({
+  partyGuid,
+  credExchId,
+  mine,
+}) => {
+  const [details, setDetails] = useState();
   const dispatch = useDispatch();
-  const credentialExchangeDetails: any[] = useSelector(getCredentialExchangeDetails);
-  const credentialExchangeDetail: any = credentialExchangeDetails.find(
+  const credentialExchangeDetails: ICredentialExchange[] = useSelector(
+    getCredentialExchangeDetails
+  );
+  const credentialExchangeDetail: ICredentialExchange = credentialExchangeDetails.find(
     (ced) => ced.credential_exchange_id === credExchId
   );
 
@@ -18,25 +31,45 @@ const CredentialContentModal = ({ partyGuid, credExchId }) => {
     dispatch(fetchCredentialExchangeDetails({ partyGuid, credentialExchangeGuid: credExchId }));
   }, []);
 
-  return (
-    <>
-      {credentialExchangeDetail &&
-        credentialExchangeDetail.credential_proposal_dict.credential_proposal.attributes.map(
-          (attr) => {
-            return (
-              <Row key={attr.name} className="margin-large--bottom">
-                <Col span={12}>
-                  <Paragraph strong>{attr.name}</Paragraph>
-                </Col>
-                <Col span={12}>
-                  <Paragraph>{attr.value}</Paragraph>
-                </Col>
-              </Row>
-            );
-          }
-        )}
-    </>
-  );
+  const formatTractionDate = (dateString: string) => {
+    const year = dateString.slice(0, 4);
+    const month = dateString.slice(4, 6);
+    const day = dateString.slice(6, 8);
+
+    return `${year}-${month}-${day}T00:00:00`;
+  };
+
+  const convertAttributesToObject = (attributes: { name: string; value: string }[]) => {
+    const attributesObject: any = {};
+    attributes.forEach((attr) => {
+      if (attr.name === "issue_date") {
+        attributesObject[attr.name] = formatTractionDate(attr.value);
+      } else {
+        attributesObject[attr.name] = attr.value;
+      }
+    });
+    attributesObject.active_bond_total = attributesObject.bond_total;
+    attributesObject.current_permittee = attributesObject.permittee_name;
+    attributesObject.site_properties = {
+      mine_tenure_type_code: "",
+      mine_commodity_code: [attributesObject.mine_commodity],
+      mine_disturbance_code: [attributesObject.mine_disturbance],
+    };
+
+    return attributesObject;
+  };
+
+  useEffect(() => {
+    if (credentialExchangeDetail) {
+      setDetails(
+        convertAttributesToObject(
+          credentialExchangeDetail.credential_proposal_dict.credential_proposal.attributes
+        )
+      );
+    }
+  }, [credentialExchangeDetail]);
+
+  return <DigitalCredentialDetails permitRecord={details} mine={mine} />;
 };
 
 export default CredentialContentModal;
