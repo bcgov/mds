9bfc15c493ee8e7a50066c7c2d078d2de7c48a7e
Henry Oforeh <henry.oforeh@aot-technologies.com>
2024-04-19 06:00:12 -0600

    refactor get, implement create

--- services/core-api/app/api/compliance/models/compliance_article.py
+++ services/core-api/app/api/compliance/models/compliance_article.py
@@ -42,6 +42,50 @@ class ComplianceArticle(AuditMixin, Base):
         return cls.query.all()
 
     @classmethod
+    def filter_or_get_all(cls, article_act_code=None, section=None, description=None, long_description=None,
+                          sub_section=None, paragraph=None, sub_paragraph=None, effective_date=None, expiry_date=None):
+        if expiry_date is None:
+            expiry_date = '9999-12-31'
+
+        if any(param is not None for param in
+               [article_act_code, section, description, long_description, sub_section, paragraph, sub_paragraph,
+                effective_date, expiry_date]):
+            query = cls.query
+            if article_act_code is not None:
+                query = query.filter_by(article_act_code=article_act_code)
+            if section is not None:
+                query = query.filter_by(section=section)
+            if description is not None:
+                query = query.filter_by(description=description)
+            if long_description is not None:
+                query = query.filter_by(long_description=long_description)
+            if sub_section is not None:
+                query = query.filter_by(sub_section=sub_section)
+            if paragraph is not None:
+                query = query.filter_by(paragraph=paragraph)
+            if sub_paragraph is not None:
+                query = query.filter_by(sub_paragraph=sub_paragraph)
+            if effective_date is not None:
+                query = query.filter_by(effective_date=effective_date)
+            if expiry_date is not None:
+                query = query.filter_by(expiry_date=expiry_date)
+            return query.all()
+        else:
+            return cls.get_all()
+
+    @classmethod
+    def find_existing_compliance_article(cls, article_act_code, section, sub_section=None, paragraph=None,
+                                         sub_paragraph=None):
+        query = cls.query.filter_by(article_act_code=article_act_code, section=section)
+        if sub_section is not None:
+            query = query.filter_by(sub_section=sub_section)
+        if paragraph is not None:
+            query = query.filter_by(paragraph=paragraph)
+        if sub_paragraph is not None:
+            query = query.filter_by(sub_paragraph=sub_paragraph)
+        return query.first()
+
+    @classmethod
     def create(cls,
                article_act_code,
                section,
