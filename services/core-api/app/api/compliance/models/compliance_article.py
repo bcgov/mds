866ad2d8a66d664d6ec8433c3e334e229a688f83
Henry Oforeh <henry.oforeh@aot-technologies.com>
2024-04-19 15:36:02 -0600

    fix unit test

--- services/core-api/app/api/compliance/models/compliance_article.py
+++ services/core-api/app/api/compliance/models/compliance_article.py
@@ -3,7 +3,7 @@ from app.extensions import db
 from sqlalchemy.schema import FetchedValue
 
 from app.api.utils.models_mixins import AuditMixin, Base
-
+from sqlalchemy import func
 
 class ComplianceArticle(AuditMixin, Base):
     __tablename__ = 'compliance_article'
@@ -39,3 +39,118 @@ class ComplianceArticle(AuditMixin, Base):
     @classmethod
     def get_all(cls):
         return cls.query.all()
+
+    @classmethod
+    def filter_or_get_all(cls, article_act_code=None, section=None, description=None, long_description=None,
+                          sub_section=None, paragraph=None, sub_paragraph=None, effective_date=None,
+                          expiry_date='9999-12-31'):
+
+        filters = []
+
+        if article_act_code:
+            filters.append(
+                func.lower(cls.article_act_code).contains(
+                    func.lower(article_act_code)))
+        if section:
+            filters.append(cls.section == section)
+        if description:
+            filters.append(
+                func.lower(cls.description).contains(
+                    func.lower(description)))
+        if long_description:
+            filters.append(
+                func.lower(cls.long_description).contains(
+                    func.lower(long_description)))
+        if sub_section:
+            filters.append(cls.sub_section == sub_section)
+        if paragraph:
+            filters.append(cls.paragraph == paragraph)
+        if sub_paragraph:
+            filters.append(cls.sub_paragraph == sub_paragraph)
+        if effective_date:
+            filters.append(cls.effective_date == effective_date)
+        if expiry_date:
+            filters.append(cls.expiry_date == expiry_date)
+
+        base_query = cls.query
+        if filters:
+            base_query = base_query.filter(*filters)
+
+        return base_query.all()
+
+    @classmethod
+    def find_existing_compliance_article(cls, article_act_code, section, sub_section=None, paragraph=None,
+                                         sub_paragraph=None, expiry_date='9999-12-31'):
+        query = cls.query.filter_by(article_act_code=article_act_code, section=section, expiry_date=expiry_date)
+        if sub_section is not None:
+            query = query.filter_by(sub_section=sub_section)
+        if paragraph is not None:
+            query = query.filter_by(paragraph=paragraph)
+        if sub_paragraph is not None:
+            query = query.filter_by(sub_paragraph=sub_paragraph)
+        return query.first()
+
+    @classmethod
+    def create(cls,
+               article_act_code,
+               section,
+               sub_section,
+               paragraph,
+               sub_paragraph,
+               description,
+               long_description,
+               effective_date,
+               expiry_date,
+               help_reference_link,
+               cim_or_cpo,
+               add_to_session=True):
+
+        compliance_article = cls(article_act_code=article_act_code,
+                                 section=section,
+                                 sub_section=sub_section,
+                                 paragraph=paragraph,
+                                 sub_paragraph=sub_paragraph,
+                                 description=description,
+                                 long_description=long_description,
+                                 effective_date=effective_date,
+                                 expiry_date=expiry_date,
+                                 help_reference_link=help_reference_link,
+                                 cim_or_cpo=cim_or_cpo)
+        if add_to_session:
+            compliance_article.save(commit=False)
+        return compliance_article
+
+    def update(self,
+               article_act_code,
+               description,
+               long_description,
+               effective_date,
+               expiry_date,
+               section=None,
+               sub_section=None,
+               paragraph=None,
+               sub_paragraph=None,
+               help_reference_link=None,
+               cim_or_cpo=None,
+               add_to_session=True):
+        self.article_act_code = article_act_code
+        self.description = description
+        self.long_description = long_description
+        self.effective_date = effective_date
+        self.expiry_date = expiry_date
+        if section is not None:
+            self.section = section
+        if sub_section is not None:
+            self.sub_section = sub_section
+        if paragraph is not None:
+            self.paragraph = paragraph
+        if sub_paragraph is not None:
+            self.sub_paragraph = sub_paragraph
+        if help_reference_link is not None:
+            self.help_reference_link = help_reference_link
+        if cim_or_cpo is not None:
+            self.cim_or_cpo = cim_or_cpo
+
+        if add_to_session:
+            self.save(commit=False)
+        return self
--- services/core-api/app/api/compliance/resources/compliance_article.py
+++ services/core-api/app/api/compliance/resources/compliance_article.py
@@ -1,6 +1,6 @@
 from flask_restx import Resource, fields
 from app.extensions import api
-from app.api.utils.access_decorators import (requires_any_of, VIEW_ALL, MINESPACE_PROPONENT)
+from app.api.utils.access_decorators import (requires_any_of, VIEW_ALL, MINESPACE_PROPONENT, EDIT_CODE)
 from app.api.utils.resources_mixins import UserMixin
 from app.api.compliance.models.compliance_article import ComplianceArticle
 from app.api.compliance.response_models import COMPLIANCE_ARTICLE_MODEL
@@ -10,7 +10,7 @@ class ComplianceArticleResource(Resource, UserMixin):
     @api.doc(description=
              'This endpoint returns a list of all possible compliance codes and their descriptions.'
              )
-    @requires_any_of([VIEW_ALL, MINESPACE_PROPONENT])
+    @requires_any_of([VIEW_ALL, MINESPACE_PROPONENT, EDIT_CODE])
     @api.marshal_with(COMPLIANCE_ARTICLE_MODEL, code=200, envelope='records')
     def get(self):
         records = ComplianceArticle.get_all()
