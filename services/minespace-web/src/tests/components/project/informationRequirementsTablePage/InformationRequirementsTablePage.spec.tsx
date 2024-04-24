f7c99542cd3db32d4c8213b70e4c9f365a182afc
matbusby <matbusby@gmail.com>
2024-04-23 18:14:10 -0600

    update InformationRequirementsTablePage.spec.tsx to give a more useful snap

--- services/minespace-web/src/tests/components/project/informationRequirementsTablePage/InformationRequirementsTablePage.spec.tsx
+++ services/minespace-web/src/tests/components/project/informationRequirementsTablePage/InformationRequirementsTablePage.spec.tsx
@@ -1,26 +1,68 @@
 import React from "react";
-import { shallow } from "enzyme";
 import { InformationRequirementsTablePage } from "@/components/pages/Project/InformationRequirementsTablePage";
 import * as MOCK from "@/tests/mocks/dataMocks";
 import { ReduxWrapper } from "@mds/common/tests/utils/ReduxWrapper";
-import { PROJECTS } from "@mds/common/constants/reducerTypes";
+import * as reducerTypes from "@mds/common/constants/reducerTypes";
+import { AUTHENTICATION, PROJECTS } from "@mds/common/constants/reducerTypes";
+import { render } from "@testing-library/react";
+import { USER_ROLES } from "@mds/common";
+import { Provider } from "react-redux";
+import { store } from "@/App";
+import * as projectActionCreator from "@mds/common/redux/actionCreators/projectActionCreator";
+import { AppThunk } from "@mds/common/interfaces/appThunk.type";
+import { success } from "@mds/common/redux/actions/genericActions";
+import * as projectActions from "@mds/common/redux/actions/projectActions";
+import { BrowserRouter } from "react-router-dom";
 
 const initialState = {
   [PROJECTS]: {
+    projects: [MOCK.PROJECT],
     project: MOCK.PROJECT,
     requirements: MOCK.INFORMATION_REQUIREMENTS_TABLE.requirements,
     informationRequirementsTableDocumentTypesHash:
       MOCK.INFORMATION_REQUIREMENTS_TABLE_STATUS_CODES_HASH,
   },
+  [AUTHENTICATION]: {
+    userAccessData: [USER_ROLES.role_minespace_proponent],
+  },
 };
 
+function mockFunction() {
+  const original = jest.requireActual("react-router-dom");
+  const projectGuid = "8132462392222";
+  const irtGuid = "c16afb82-144c-4138-9a36-ba5c24c43d8a";
+  return {
+    ...original,
+    useParams: jest.fn().mockReturnValue({
+      projectGuid,
+      irtGuid,
+    }),
+    useLocation: jest.fn().mockReturnValue({
+      state: { current: 0 },
+    }),
+  };
+}
+
+jest.mock("react-router-dom", () => mockFunction());
+jest.spyOn(projectActionCreator, "fetchProjectById").mockImplementation(
+  (): AppThunk<Promise<any>> => (dispatch) => {
+    dispatch(success(reducerTypes.GET_PROJECT));
+    dispatch(projectActions.storeProject(MOCK.PROJECT));
+    return Promise.resolve(MOCK.PROJECT);
+  }
+);
 describe("InformationRequirementsTablePage", () => {
   it("renders properly", () => {
-    const component = shallow(
-      <ReduxWrapper initialState={initialState}>
-        <InformationRequirementsTablePage />
-      </ReduxWrapper>
+    const { container } = render(
+      <BrowserRouter>
+        <ReduxWrapper initialState={initialState}>
+          <Provider store={store}>
+            <InformationRequirementsTablePage />
+          </Provider>
+        </ReduxWrapper>
+      </BrowserRouter>
     );
-    expect(component).toMatchSnapshot();
+
+    expect(container).toMatchSnapshot();
   });
 });
