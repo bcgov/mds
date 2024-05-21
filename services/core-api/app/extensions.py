b7c00cd9176bf4e99ed63c3ec4066ca968d6e619
matbusby-fw <mat.busby@ca.ey.com>
2024-05-21 15:52:24 -0600

    Update check for test jwt secrets in core-api to not crash on NONETYPE

--- services/core-api/app/extensions.py
+++ services/core-api/app/extensions.py
@@ -76,7 +76,7 @@ def getJwtManager():
         raise AuthError({'code': 'auth_fail',
                     'description': 'Token issuer oidc.gov.bc.ca is no longer supported. Please contact the mds team.'}, 401)
 
-    if test_config.JWT_OIDC_TEST_ISSUER is not None:
+    if test_config.JWT_OIDC_TEST_ISSUER is not None and test_config.JWT_OIDC_TEST_ISSUER in iss:
         jwt_result = jwt
     else:
         jwt_result = get_jwt_by_audience(aud)
