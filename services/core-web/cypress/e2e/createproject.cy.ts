ccdb035e871016d4539404c84f2cee4ca52a26dd
Tara Epp <102187683+taraepp@users.noreply.github.com>
2024-06-07 22:28:28 +0000

    try fixing a cypress test

--- services/core-web/cypress/e2e/createproject.cy.ts
+++ services/core-web/cypress/e2e/createproject.cy.ts
@@ -30,22 +30,39 @@ describe("Major Projects", () => {
     // Create a new project
     cy.get('[data-cy="create-new-project"]', { timeout: 15000 }).click({ force: true });
 
+    // turn to edit mode
+    cy.contains("Edit Project Description").click({ force: true });
     // Fill in project details
     cy.get("#project_summary_title", { timeout: 15000 }).type(uniqueProjectName, { force: true });
     cy.get("#project_summary_description").type("This is just a Cypress test project description", {
       force: true,
     });
-    cy.get("#contacts\\[0\\]\\.first_name").type("Cypress", { force: true });
-    cy.get("#contacts\\[0\\]\\.last_name").type("Test", { force: true });
-    cy.get("#contacts\\[0\\]\\.email").type("cypress@mds.com", { force: true });
-    cy.get("#contacts\\[0\\]\\.phone_number").type("1234567890", { force: true });
+
+    // SAVE & CONTINUE - skip to Purpose & Authorization
+    cy.contains("Save & Continue").click({ force: true });
+    cy.contains("Purpose & Authorization", { timeout: 10000 }).click({ force: true });
+
     cy.get('[data-cy="checkbox-authorization-OTHER"]').click({ force: true });
     cy.get(
       '[name="authorizations.OTHER[0].authorization_description"]'
     ).type("legislation description", { force: true });
 
+    // SAVE & CONTINUE - direct to Project Contacts
+    cy.contains("Save & Continue").click({ force: true });
+
+    cy.get("#contacts\\[0\\]\\.first_name").type("Cypress", { force: true });
+    cy.get("#contacts\\[0\\]\\.last_name").type("Test", { force: true });
+    cy.get("#contacts\\[0\\]\\.email").type("cypress@mds.com", { force: true });
+    cy.get("#contacts\\[0\\]\\.phone_number").type("1234567890", { force: true });
+
+    // SAVE & CONTINUE - skip to Declaration
+    cy.contains("Save & Continue").click({ force: true });
+    cy.contains("Declaration", { timeout: 10000 }).click({ force: true });
+
+    cy.get("input", { timeout: 10000 }).click({ force: true });
+
     // Submit the project
-    cy.get('[data-cy="project-summary-submit-button"]').click({ force: true });
+    cy.contains("Submit").click({ force: true });
     // wait for API to respond before navigating
     cy.wait(15000);
     // Navigate back to projects
