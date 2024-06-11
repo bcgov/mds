6133eae956c665ffa4af1b58af5faabfb1e7afed
Tara Epp <102187683+taraepp@users.noreply.github.com>
2024-06-11 17:50:35 +0000

    scroll

--- services/core-web/cypress/e2e/createproject.cy.ts
+++ services/core-web/cypress/e2e/createproject.cy.ts
@@ -110,10 +110,11 @@ describe("Major Projects", () => {
 
     // SAVE & CONTINUE - Location, Access and Land Use
     cy.contains("Save & Continue").click({ force: true });
+    cy.scrollTo(0, 0);
     cy.get(`[name="is_legal_land_owner"]`, { timeout: 10000 })
       .first()
       .scrollIntoView()
-      .click(); // click yes
+      .click({ force: true }); // click yes
     cy.get(`[name="facility_latitude"]`).type("48", { force: true });
     cy.get(`[name="facility_longitude"]`).type("-114", { force: true });
 
