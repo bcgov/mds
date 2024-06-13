f039f7526aa7ffd031d0a44b86d8a52c166021d2
Henry Oforeh <henry.oforeh@aot-technologies.com>
2024-06-13 09:59:09 -0600

    update cypress test

--- services/core-web/cypress/e2e/createproject.cy.ts
+++ services/core-web/cypress/e2e/createproject.cy.ts
@@ -104,7 +104,10 @@ describe("Major Projects", () => {
 
     // SAVE & CONTINUE - Agent
     cy.contains("Save & Continue").click({ force: true });
-    cy.contains("Are you an agent applying on behalf of the applicant?", { timeout: 10000 });
+    cy.contains("Are you an agent applying on behalf of the applicant?", {
+      timeout: 15000,
+      force: true,
+    });
 
     cy.contains("No").click({ force: true });
 
