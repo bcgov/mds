40e2f482c41e53ce2a80723067bf81b01848a600
Jason Syrotuck <jasyrotuck@gmail.com>
2024-06-17 11:30:34 -0700

    [MDS-5920] Feature/fix anoncred commodity and disturbance codes (#3151)
    * ok, use permit minetype and not mine minetype.
    
    * use existing relationship instead of new db call
    
    * handle empty
    
    * similar condition
    
    * more null handling

--- services/core-api/tests/verifiable_credentials/test_manager.py
+++ services/core-api/tests/verifiable_credentials/test_manager.py
@@ -1,19 +1,35 @@
-
 from app.api.verifiable_credentials.manager import VerifiableCredentialManager
-
+from app.api.mines.mine.models.mine_type import MineType
 from tests.factories import create_mine_and_permit, PartyFactory, MinePartyAppointmentFactory
 
 
 class TestVerifiableCredentialManager:
     """test MinesActPermit 1.1.1 attributes"""
-    def test_collect_attributes_for_mines_act_permit_111(self, test_client, db_session, auth_headers):
+
+    def test_collect_attributes_for_mines_act_permit_111(self, test_client, db_session,
+                                                         auth_headers):
         mine, permit = create_mine_and_permit()
         permittee_appt = MinePartyAppointmentFactory(permittee=True)
 
         pa = permit.permit_amendments[0]
         permit.bonds[0].bond_status_code = "ACT"
-        mine_disturbance_list = [mtd.mine_disturbance_literal for mtd in pa.mine.mine_type[0].mine_type_detail if mtd.mine_disturbance_code]
-        mine_commodity_list = [mtd.mine_commodity_literal for mtd in pa.mine.mine_type[0].mine_type_detail if mtd.mine_commodity_code]  
+
+        mine_type = [mt for mt in pa.permit.site_properties if mt.mine_guid == pa.permit.mine_guid
+                     ][0] if pa.permit.site_properties else None
+        mine_disturbance_list = []
+        mine_commodity_list = []
+
+        if mine_type:
+            mine_disturbance_list = [
+                mtd.mine_disturbance_literal for mtd in mine_type.mine_type_detail
+                if mtd.mine_disturbance_code
+            ]
+
+            mine_commodity_list = [
+                mtd.mine_commodity_literal for mtd in mine_type.mine_type_detail
+                if mtd.mine_commodity_code
+            ]
+
         mine_status_xref = pa.mine.mine_status[0].mine_status_xref
 
         assert pa.permit_no
@@ -32,11 +48,14 @@ class TestVerifiableCredentialManager:
         assert attributes["permit_no"] == pa.permit_no
         assert attributes["permit_status"] == permit.permit_status_code_description
         # assert attributes["permittee_name"] == permit.current_permittee
-        assert attributes["mine_operation_status"] == mine_status_xref.mine_operation_status.description
+        assert attributes[
+            "mine_operation_status"] == mine_status_xref.mine_operation_status.description
         if mine_status_xref.mine_operation_status_reason:
-            assert attributes["mine_operation_status_reason"] == mine_status_xref.mine_operation_status_reason.description 
+            assert attributes[
+                "mine_operation_status_reason"] == mine_status_xref.mine_operation_status_reason.description
         if mine_status_xref.mine_operation_status_sub_reason:
-            assert attributes["mine_operation_status_sub_reason"] == mine_status_xref.mine_operation_status_sub_reason.description
+            assert attributes[
+                "mine_operation_status_sub_reason"] == mine_status_xref.mine_operation_status_sub_reason.description
         if mine_disturbance_list:
             assert attributes["mine_disturbance"]
         if mine_commodity_list:
