52b73b8383f2993039a4203977ed37f23910d494
Simen Fivelstad Smaaberg <66635118+simensma-fresh@users.noreply.github.com>
2024-07-04 23:28:03 +0000

    Updated permit search to use haystack 2.0 functionality

--- services/permits/app/app.py
+++ services/permits/app/app.py
@@ -2,23 +2,9 @@ from dotenv import find_dotenv, load_dotenv
 from fastapi import FastAPI
 
 from .openid_connect_middleware import OpenIdConnectMiddleware
-# from .pipelines import indexing_pipeline, query_pipeline, request_limiter, document_store
-from .permit_conditions.resources.permit_condition_resource import router
-
-# Configure default query and indexing pipelines for Haystack to use
-# rest_api.utils.pipelines = {
-#     'query_pipeline': query_pipeline(),
-#     'document_store': document_store,
-#     'concurrency_limiter': request_limiter(),
-#     'indexing_pipeline': indexing_pipeline(),
-# }
+from .permit_conditions.resources.permit_condition_resource import router as permit_condition_router
+from .permit_search.resources.permit_search_resource import router as permit_search_router
 
 mds = FastAPI()
-mds.include_router(router)
-# mds.add_middleware(OpenIdConnectMiddleware)
-
-# mds.add_route
-
-# haystack_app = get_app()
-# # Include the Haystack REST API in our app
-# mds.mount('/haystack', haystack_app)
+mds.include_router(permit_condition_router)
+mds.include_router(permit_search_router)
