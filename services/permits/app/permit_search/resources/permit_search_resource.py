194c76862709edf5d0cafebf39e4ad51c779a83b
Simen Fivelstad Smaaberg <66635118+simensma-fresh@users.noreply.github.com>
2024-07-04 23:28:03 +0000

    Updated permit search to use haystack 2.0 functionality

---
+++ services/permits/app/permit_search/resources/permit_search_resource.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, UploadFile, File
+from pydantic import BaseModel
+from fastapi.responses import StreamingResponse
+
+from app.permit_conditions.pipelines.permit_condition_pipeline import permit_condition_pipeline
+import tempfile
+
+from fastapi import APIRouter
+
+from app.helpers.temporary_file import store_temporary
+from app.permit_search.pipelines.pipelines import indexing_pipeline, query_pipeline
+from pydantic import BaseModel
+
+router = APIRouter()
+
+class SearchRequest(BaseModel):
+    query: str
+
+@router.post("/permit/index")
+async def index_permit(file: UploadFile = File(...)):
+    tmp = store_temporary(file)
+    
+    try:
+        pipeline = indexing_pipeline()
+
+        return pipeline.run({"PDFConverter": {"sources": [tmp.name]}})
+    finally:
+        tmp.close()
+
+@router.post("/permit/query")
+async def search_permit(req: SearchRequest):
+    pipeline = query_pipeline()
+
+    res = pipeline.run({"retriever": {"query": req.query}})
+
+    return res['ranker']
