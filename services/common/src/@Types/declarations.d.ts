c9d2d7e8a50f1cf979c234907b1ab1df9c30e4cc
Sinn <Alex.Sinn@gov.bc.ca>
2024-07-16 10:24:08 -0700

    making common package loader display correct styling based on system

--- services/common/src/@Types/declarations.d.ts
+++ services/common/src/@Types/declarations.d.ts
@@ -12,3 +12,8 @@ declare module "*.gif" {
   const value: any;
   export default value;
 }
+
+declare module "*.json" {
+  const value: any;
+  export default value;
+}
--- services/common/src/components/common/Loading.tsx
+++ services/common/src/components/common/Loading.tsx
@@ -2,13 +2,26 @@
  * @constant Loading is a full page loading spinner
  */
 import React from "react";
-import { LOADER } from "@mds/common/constants/assets";
+import { useSelector } from "react-redux";
+import { getSystemFlag } from "@mds/common/redux/selectors/authenticationSelectors";
+import { SystemFlagEnum } from "@mds/common/constants/enums";
+import Lottie from "react-lottie";
+
+import coreLoader from "@mds/common/assets/coreLoader.json";
+import mineSpaceLoader from "@mds/common/assets/mineSpaceLoader.json";
 
 const Loading = () => {
+  const systemFlag = useSelector(getSystemFlag);
+  const isCore = systemFlag === SystemFlagEnum.core;
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: isCore ? coreLoader : mineSpaceLoader,
+  };
   return (
     <div id="loading-screen">
       <div id="loader">
-        <img src={LOADER} alt="Loading" />
+        <Lottie options={defaultOptions} />
       </div>
     </div>
   );
