a4cfb7e94b512254f7539295d50be114cd3483b9
Mat Busby <83598933+matbusby-fw@users.noreply.github.com>
2024-07-17 10:49:23 -0600

    [MDS-6074] - BE spatial bugfixes (#3184)
    * migration to update mine_document_bundle columns and mine_document columns, creation of `tmp/spatial` file in docman in the init file.
    
    * added association proxies to two documentxref tables to successfully return spatial bundle info.
    
    * updated tmp location
    
    * changed response model to only return the bundle id rather than the whole bundle

--- services/core-api/app/api/mines/documents/models/mine_document_bundle.py
+++ services/core-api/app/api/mines/documents/models/mine_document_bundle.py
@@ -7,8 +7,19 @@ from app.api.utils.models_mixins import SoftDeleteMixin, AuditMixin, Base
 class MineDocumentBundle(SoftDeleteMixin, AuditMixin, Base):
     __tablename__ = 'mine_document_bundle'
 
-    bundle_id = db.Column(UUID(as_uuid=True), primary_key=True)
+    bundle_id = db.Column(db.Integer, primary_key=True)
     bundle_guid = db.Column(UUID(as_uuid=True), nullable=False)
     name = db.Column(db.String(300), nullable=False)
     geomark_id = db.Column(db.String(300), nullable=True)
-    docman_bundle_guid = db.Column(UUID(as_uuid=True), nullable=True)
+    docman_bundle_guid = db.Column(UUID(as_uuid=True))
+
+    bundle_documents = db.relationship('MineDocument', back_populates='mine_document_bundle')
+
+    def json(self):
+        return {
+            'bundle_id': str(self.bundle_id),
+            'bundle_guid': str(self.bundle_guid),
+            'name': self.name,
+            'geomark_id': self.geomark_id,
+            'docman_bundle_guid': str(self.docman_bundle_guid)
+        }
