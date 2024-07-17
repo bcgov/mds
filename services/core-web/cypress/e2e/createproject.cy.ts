d4e7ff874deaa7a5e34254eda345ac463f7adc15
Sinn <Alex.Sinn@gov.bc.ca>
2024-07-17 13:18:37 -0700

    cypress error test

--- services/core-web/cypress/e2e/createproject.cy.ts
+++ services/core-web/cypress/e2e/createproject.cy.ts
@@ -38,7 +38,7 @@ describe("Major Projects", () => {
 
     // SAVE & CONTINUE - skip to Purpose & Authorization
     cy.contains("Save & Continue").click({ force: true });
-    cy.contains("Project Lead (optional)", { timeout: 10000 });
+    cy.contains("Ministry Contact", { timeout: 10000 });
     cy.contains("Purpose and Authorization", { timeout: 10000 }).click({ force: true });
     cy.contains("Regulatory Approval Type", { timeout: 10000 });
 
