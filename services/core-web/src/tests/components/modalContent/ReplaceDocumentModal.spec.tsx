476929204208c7444a94cafb44815c31e51d47e2
Henry Oforeh <henry.oforeh@aot-technologies.com>
2024-07-31 12:50:33 -0600

    update snapshot.

--- services/core-web/src/tests/components/modalContent/ReplaceDocumentModal.spec.tsx
+++ services/core-web/src/tests/components/modalContent/ReplaceDocumentModal.spec.tsx
@@ -1,33 +1,33 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { store } from "@/App";
+import configureStore from "redux-mock-store";
+import ReplaceDocumentModal from "@mds/common/components/documents/ReplaceDocumentModal";
 import { MINEDOCUMENTS } from "@/tests/mocks/dataMocks";
 import matchMedia from "@/tests/mocks/matchMedia";
-import ReplaceDocumentModal from "@mds/common/components/documents/ReplaceDocumentModal";
 import { shallow } from "enzyme";
+
+const mockStore = configureStore([]);
+const store = mockStore({});
 
 const props = {
   document: MINEDOCUMENTS[0],
   handleSubmit: jest.fn().mockReturnValue(Promise.resolve()),
   closeModal: jest.fn(),
   postNewDocumentVersion: jest.fn().mockReturnValue(Promise.resolve()),
+  alertMessage: "This is a test alert message.",
 };
-
-beforeEach(() => {
-  props.document = MINEDOCUMENTS;
-});
 
 beforeAll(() => {
   window.matchMedia = matchMedia;
 });
 
 describe("ReplaceDocumentModal", () => {
-  it("renders properly", () => {
-    const { container } = shallow(
+  it("renders correctly and matches the snapshot", () => {
+    const wrapper = shallow(
       <Provider store={store}>
         <ReplaceDocumentModal {...props} />
       </Provider>
     );
-    expect(container).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
   });
 });
