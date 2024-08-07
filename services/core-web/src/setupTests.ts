536e97747527cef5c01111d8fb9b3285b7f830f7
Tara Epp <102187683+taraepp@users.noreply.github.com>
2024-08-07 18:57:56 +0000

    make tests work

--- services/core-web/src/setupTests.ts
+++ services/core-web/src/setupTests.ts
@@ -9,10 +9,18 @@ require("jest-localstorage-mock");
 Enzyme.configure({ adapter: new Adapter() });
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-global.REQUEST_HEADER = require(path.resolve(__dirname, "../common/utils/RequestHeaders.js"));
+(<any>global).REQUEST_HEADER = require(path.resolve(__dirname, "../common/utils/RequestHeaders.js"));
 
-global.requestAnimationFrame = (callback) => {
-  setTimeout(callback, 0);
+
+(<any>global).requestAnimationFrame = (callback: any) => {
+  setTimeout(callback, 0); // eslint-disable-line @typescript-eslint/no-implied-eval
+};
+
+(<any>global).GLOBAL_ROUTES = {
+  MINE_DASHBOARD: {
+    route: "test",
+    dynamicRoute: () => "test",
+  },
 };
 
 beforeAll(() => {
