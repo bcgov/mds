87b5f83598b7812f58f57421300a155eab825282
Tara Epp <102187683+taraepp@users.noreply.github.com>
2024-08-19 18:38:01 +0000

    see if adding a default geomark URL helps Index.js pass, and if adding an equivalent rule on MS helps the build error

--- services/common/src/components/documents/DocumentTable.spec.tsx
+++ services/common/src/components/documents/DocumentTable.spec.tsx
@@ -1,18 +1,9 @@
 import React from "react";
-import {
-  render,
-  fireEvent,
-  waitFor,
-  waitForElementToBeRemoved,
-  queryByAttribute,
-  screen,
-} from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import * as MOCK from "@mds/common/tests/mocks/dataMocks";
 import DocumentTable from "./DocumentTable";
 import { MineDocument } from "@mds/common/models/documents/document";
 import { ReduxWrapper } from "@mds/common/tests/utils/ReduxWrapper";
-import { FORM } from "@mds/common/constants";
-import { openModal } from "@mds/common/redux/actions/modalActions";
 import * as modalActions from "@mds/common/redux/actions/modalActions";
 
 const documents = MOCK.PROJECT_SUMMARY.documents.map((d) => new MineDocument(d));
@@ -24,7 +15,7 @@ describe("DocumentTable", () => {
 
   const openModalSpy = jest.spyOn(modalActions, "openModal");
   it("renders properly", async () => {
-    const { container, getByText, getAllByText, findByText, findByTestId } = render(
+    const { container, getAllByText, findByTestId } = render(
       <ReduxWrapper>
         <DocumentTable
           documents={documents}
@@ -39,19 +30,8 @@ describe("DocumentTable", () => {
     expect(container).toMatchSnapshot();
     const actionsButton = getAllByText("Actions")[0];
     fireEvent.mouseEnter(actionsButton);
-    // fireEvent.click(actionsButton);
-    const archiveAction = await findByTestId("action-button-archive"); //view, download, replace, download-all
-    // const viewAction = await findByText(/Download file/i);
+    const archiveAction = await findByTestId("action-button-archive");
     fireEvent.click(archiveAction);
     expect(openModalSpy).toHaveBeenCalledTimes(1);
-    // screen.debug();
-    // await waitFor(() => expect(findByText(/about to archive the following file/)).toBeInTheDocument());
-    // const archiveButton = await findByText(/Archive/);
-    // fireEvent.click(archiveButton);
-    // screen.debug()
-    // await waitFor(() => { expect(queryByAttribute("id", container, "loading-screen")).toBeInTheDocument() })
-    // // await waitFor(() => container.querySelector(`#${FORM.ARCHIVE_DOCUMENT}`).)
-    // await waitForElementToBeRemoved(() => container.querySelector(`#loading-screen`))
-    // expect(onArchiveFunc).toHaveBeenCalledTimes(1);
   });
 });
