f716dc134b1efd9d160ea14f1fb14924f5c6b2e5
matbusby-fw <mat.busby@ca.ey.com>
2024-09-05 16:20:22 -0600

    fixed pytest

--- services/core-api/tests/reports/resource/test_mine_reports_resource.py
+++ services/core-api/tests/reports/resource/test_mine_reports_resource.py
@@ -1,6 +1,8 @@
-import datetime
 import json
 import uuid
+from datetime import datetime, timedelta, date
+
+from flask import current_app
 
 from app.api.mines.mine.models.mine import Mine
 from app.api.mines.reports.models.mine_report_definition import MineReportDefinition
@@ -21,15 +23,6 @@ def test_get_code_required_reports_for_mine(test_client, db_session, auth_header
     assert len(get_data['records']) == THREE_REPORTS
     assert get_resp.status_code == 200
 
-def test_get_permit_required_reports_for_mine(test_client, db_session, auth_headers):
-    mine = MineFactory(mine_reports=THREE_REPORTS)
-    mine_reports = MineReportFactory(mine = mine, permit_required_reports = True)
-    get_resp = test_client.get(
-        f'/mines/{mine.mine_guid}/reports?mine_reports_type={MINE_REPORT_TYPE["PERMIT REQUIRED REPORTS"]}', headers=auth_headers['full_auth_header'])
-    get_data = json.loads(get_resp.data.decode())
-    assert len(get_data['records']) == 1
-    assert get_resp.status_code == 200
-
     # Test with pagination
     get_resp = test_client.get(
         f'/mines/{mine.mine_guid}/reports?page=1&per_page=2',
@@ -41,20 +34,21 @@ def test_get_permit_required_reports_for_mine(test_client, db_session, auth_head
 
     # Test sort by due_date in ascending order
     get_resp = test_client.get(
-        f'/mines/{mine.mine_guid}/reports?sort_field=due_date&sort_dir=asc',
+        f'/mines/{mine.mine_guid}/reports?mine_reports_type=CRR&sort_field=due_date&sort_dir=asc',
         headers=auth_headers['full_auth_header']
     )
     get_data = json.loads(get_resp.data.decode())
     assert get_resp.status_code == 200
-    assert len(get_data['records']) == THREE_REPORTS
+    assert len(get_data['records']) == len(mine.mine_reports)
     for i in range(len(get_data['records']) - 1):
         assert datetime.strptime(get_data['records'][i]['due_date'], '%Y-%m-%d') <= datetime.strptime(
             get_data['records'][i + 1]['due_date'], '%Y-%m-%d')
 
     # Test filter by a specific report name
-    specific_report_name = mine.mine_reports[0].report_name
+    specific_report_name = mine.mine_reports[0].mine_report_guid
+    current_app.logger.debug(specific_report_name)
     get_resp = test_client.get(
-        f'/mines/{mine.mine_guid}/reports?report_name={specific_report_name}',
+        f'/mines/{mine.mine_guid}/reports?mine_reports_type=CRR&report_name={specific_report_name}',
         headers=auth_headers['full_auth_header']
     )
     get_data = json.loads(get_resp.data.decode())
@@ -62,17 +56,32 @@ def test_get_permit_required_reports_for_mine(test_client, db_session, auth_head
     assert all(report['report_name'] == specific_report_name for report in get_data['records'])
 
     # Test filter by received date range
-    start_date = '2023-01-01'
-    end_date = '2023-12-31'
+    start_date = mine.mine_reports[0].received_date - timedelta(days=1)
+    end_date = mine.mine_reports[0].received_date + timedelta(days=1)
+
     get_resp = test_client.get(
-        f'/mines/{mine.mine_guid}/reports?received_date_after={start_date}&received_date_before={end_date}',
+        f'/mines/{mine.mine_guid}/reports?mine_reports_type=CRR&due_date_start={start_date.strftime("%Y-%m-%d")}&due_date_end={end_date.strftime("%Y-%m-%d")}',
         headers=auth_headers['full_auth_header']
     )
     get_data = json.loads(get_resp.data.decode())
     assert get_resp.status_code == 200
+
     for report in get_data['records']:
         received_date = datetime.strptime(report['received_date'], '%Y-%m-%d')
-        assert datetime.strptime(start_date, '%Y-%m-%d') <= received_date <= datetime.strptime(end_date, '%Y-%m-%d')
+
+        assert (start_date <= received_date.date())
+        assert (received_date.date() <= end_date)
+
+def test_get_permit_required_reports_for_mine(test_client, db_session, auth_headers):
+    mine = MineFactory(mine_reports=THREE_REPORTS)
+    mine_reports = MineReportFactory(mine = mine, permit_required_reports = True)
+    get_resp = test_client.get(
+        f'/mines/{mine.mine_guid}/reports?mine_reports_type={MINE_REPORT_TYPE["PERMIT REQUIRED REPORTS"]}', headers=auth_headers['full_auth_header'])
+    get_data = json.loads(get_resp.data.decode())
+    assert len(get_data['records']) == 1
+    assert get_resp.status_code == 200
+
+
 
 def test_get_a_report_for_a_mine(test_client, db_session, auth_headers):
     mine = MineFactory(mine_reports=ONE_REPORT)
