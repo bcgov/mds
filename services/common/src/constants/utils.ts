8477fcaf28313e1514e84ee42756b069b9772d14
Henry Oforeh <henry.oforeh@aot-technologies.com>
2024-09-12 07:36:29 -0600

    update unit tests snapshot.

--- services/common/src/constants/utils.ts
+++ services/common/src/constants/utils.ts
@@ -77,7 +77,7 @@ export const isFieldDisabled = (
   const disabledStatuses = new Set(["WDN", "COM"]);
   const enabledStatusMapping = {
     [SystemFlagEnum.ms]: new Set(["DFT", "CHR"]),
-    [SystemFlagEnum.core]: new Set(["ASG", "UNR", "CHR", "OHD"]),
+    [SystemFlagEnum.core]: new Set(["DFT", "ASG", "UNR", "CHR", "OHD"]),
   };
 
   if (disabledStatuses.has(projectSummaryStatusCode)) return true;
@@ -97,7 +97,7 @@ export const isDocumentFieldDisabled = (systemFlag, projectSummaryStatusCode = "
   const disabledStatuses = new Set(["WDN", "COM"]);
   const enabledStatusMapping = {
     [SystemFlagEnum.ms]: new Set(["DFT", "SUB", "ASG", "CHR"]),
-    [SystemFlagEnum.core]: new Set(["ASG", "UNR", "CHR", "OHD"]),
+    [SystemFlagEnum.core]: new Set(["DFT", "ASG", "UNR", "CHR", "OHD"]),
   };
 
   if (disabledStatuses.has(projectSummaryStatusCode)) return true;
