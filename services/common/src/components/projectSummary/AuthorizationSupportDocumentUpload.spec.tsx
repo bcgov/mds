ace4d0111738464a798fe03b81dc8e492ec07870
Henry Oforeh <henry.oforeh@aot-technologies.com>
2024-09-13 08:50:57 -0600

    update test.

--- services/common/src/components/projectSummary/AuthorizationSupportDocumentUpload.spec.tsx
+++ services/common/src/components/projectSummary/AuthorizationSupportDocumentUpload.spec.tsx
@@ -1,18 +1,26 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { ReduxWrapper } from "@mds/common/tests/utils/ReduxWrapper";
-import { PROJECTS, STATIC_CONTENT } from "@mds/common/constants/reducerTypes";
+import { PROJECTS, STATIC_CONTENT, AUTHENTICATION } from "@mds/common/constants/reducerTypes";
 import { FORM } from "../..";
 import FormWrapper from "../forms/FormWrapper";
 import * as MOCK from "@mds/common/tests/mocks/dataMocks";
 import AuthorizationSupportDocumentUpload from "./AuthorizationSupportDocumentUpload";
 
 const initialState = {
+  form: {
+    [FORM.ADD_EDIT_PROJECT_SUMMARY]: {
+      values: {},
+    },
+  },
   [PROJECTS]: {
     projectSummary: MOCK.PROJECT_SUMMARY,
   },
   [STATIC_CONTENT]: {
     provinceOptions: MOCK.BULK_STATIC_CONTENT_RESPONSE.provinceOptions,
+  },
+  [AUTHENTICATION]: {
+    systemFlag: "ms",
   },
 };
 
@@ -20,7 +28,11 @@ describe("AuthorizationSupportDocumentUpload Component", () => {
   it("should render the component with expected fields", () => {
     const { container } = render(
       <ReduxWrapper initialState={initialState}>
-        <FormWrapper name={FORM.ADD_EDIT_PROJECT_SUMMARY} initialValues={{}} onSubmit={() => {}}>
+        <FormWrapper
+          name={FORM.ADD_EDIT_PROJECT_SUMMARY}
+          initialValues={initialState}
+          onSubmit={() => {}}
+        >
           <AuthorizationSupportDocumentUpload
             code={"code"}
             mineGuid={"mine_guid"}
