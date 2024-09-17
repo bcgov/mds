2337ce7d9d4eba271e3d57b1b22b241d9a0a798d
matbusby-fw <mat.busby@ca.ey.com>
2024-09-17 09:16:41 -0600

    Added ProjectSummaryForm.spec.tsx

--- services/common/src/components/projectSummary/ProjectSummaryForm.spec.tsx
+++ services/common/src/components/projectSummary/ProjectSummaryForm.spec.tsx
@@ -2,29 +2,23 @@ import React from "react";
 import { render } from "@testing-library/react";
 import { AUTHENTICATION, PROJECTS, STATIC_CONTENT } from "@mds/common/constants/reducerTypes";
 import { FORM, SystemFlagEnum } from "@mds/common/index";
-import FormWrapper from "@mds/common/components/forms/FormWrapper";
-import { ProjectManagement } from "@mds/common/components/projectSummary/ProjectManagement";
 import * as MOCK from "@mds/common/tests/mocks/dataMocks";
 import { USER_ACCESS_DATA } from "@mds/common/tests/mocks/dataMocks";
 import { ReduxWrapper } from "@mds/common/tests/utils/ReduxWrapper";
+import ProjectSummaryForm from "@mds/common/components/projectSummary/ProjectSummaryForm";
 
 const initialState = {
   form: {
     [FORM.ADD_EDIT_PROJECT_SUMMARY]: {
-      values: {
-        status_code: "ASG",
-        project_summary_guid: MOCK.PROJECT_SUMMARY.project_summary_guid,
-        project_lead_party_guid: MOCK.PROJECT_SUMMARY.project_lead_party_guid,
-      },
+      values: MOCK.PROJECT_SUMMARY,
     },
   },
   [PROJECTS]: {
     projectSummary: MOCK.PROJECT_SUMMARY,
-    projectSummaryMinistryComments: MOCK.PROJECT_SUMMARY_MINISTRY_COMMENTS,
   },
   [STATIC_CONTENT]: {
-    provinceOptions: MOCK.BULK_STATIC_CONTENT_RESPONSE.provinceOptions,
-    projectSummaryStatusCodes: MOCK.BULK_STATIC_CONTENT_RESPONSE.projectSummaryStatusCodes,
+    projectSummaryAuthorizationTypes:
+      MOCK.BULK_STATIC_CONTENT_RESPONSE.projectSummaryAuthorizationTypes,
   },
   [AUTHENTICATION]: {
     systemFlag: SystemFlagEnum.core,
@@ -32,22 +26,21 @@ const initialState = {
   },
 };
 
+const asyncSave = () => new Promise<void>(() => {});
+
 describe("Project Management", () => {
   it("renders properly", async () => {
-    const { container, findAllByText } = render(
+    const { container } = render(
       <ReduxWrapper initialState={initialState}>
-        <FormWrapper
-          name={FORM.ADD_EDIT_PROJECT_SUMMARY}
+        <ProjectSummaryForm
           initialValues={initialState.form[FORM.ADD_EDIT_PROJECT_SUMMARY].values}
-          onSubmit={() => {}}
-        >
-          <ProjectManagement />
-        </FormWrapper>
+          isEditMode={false}
+          handleSaveData={asyncSave}
+          handleTabChange={() => {}}
+          activeTab={"basic-information"}
+        />
       </ReduxWrapper>
     );
-
-    const commentText = await findAllByText(/this is a comment/i);
-    expect(commentText[0]).toBeInTheDocument();
 
     expect(container).toMatchSnapshot();
   });
