5b22b0df79d649f97ea48ef5419fa2d85635af6f
Tara Epp <102187683+taraepp@users.noreply.github.com>
2024-10-01 21:10:03 +0000

    added rich text editor

---
+++ services/common/src/components/forms/RenderRichTextEditor.tsx
@@ -0,0 +1,76 @@
+import React, { FC, useContext, useMemo } from "react";
+import { Input, Form } from "antd";
+import { BaseInputProps, BaseViewInput, getFormItemLabel } from "./BaseInput";
+import { FormConsumer, FormContext } from "./FormWrapper";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import parse from "html-react-parser";
+
+const RenderRichTextEditor: FC<BaseInputProps> = ({
+  label,
+  labelSubtitle,
+  meta,
+  input,
+  disabled,
+  help,
+  required,
+  defaultValue,
+  id,
+  placeholder,
+  allowClear,
+}) => {
+  const { isEditMode } = useContext(FormContext);
+
+  const handleAddImage = (data) => {
+    console.log("image data", data);
+  };
+  const toolbarOptions = isEditMode
+    ? [
+        ["bold", "italic", "underline", "strike"],
+        ["blockquote"],
+        ["color", "background", "font", "align"],
+        ["link", "image", "video", "formula"],
+      ]
+    : [];
+
+  const modules = useMemo(
+    () => ({
+      toolbar: {
+        container: toolbarOptions,
+      },
+      // handlers: { image: handleAddImage }
+    }),
+    []
+  );
+
+  const handleChange = (newValue) => {
+    console.log(newValue);
+    input.onChange(newValue);
+  };
+
+  if (!isEditMode) {
+    return parse(input.value);
+  }
+
+  return (
+    <Form.Item
+      id={id}
+      getValueProps={() => ({ value: input.value })}
+      name={input.name}
+      required={required}
+      validateStatus={meta.touched ? (meta.error && "error") || (meta.warning && "warning") : ""}
+      help={
+        meta.touched &&
+        ((meta.error && <span>{meta.error}</span>) || (meta.warning && <span>{meta.warning}</span>))
+      }
+      label={getFormItemLabel(label, required, labelSubtitle)}
+    >
+      <>
+        <ReactQuill theme="snow" value={input.value} onChange={handleChange} modules={modules} />
+        {help && <div className={`form-item-help ${input.name}-form-help`}>{help}</div>}
+      </>
+    </Form.Item>
+  );
+};
+
+export default RenderRichTextEditor;
--- services/minespace-web/src/HOC/AppHelp.tsx
+++ services/minespace-web/src/HOC/AppHelp.tsx
@@ -5,96 +5,88 @@ import { useSelector, useDispatch } from "react-redux";
 import { Route, Switch, useParams } from "react-router-dom";
 import * as routes from "@/constants/routes";
 import FormWrapper from "@mds/common/components/forms/FormWrapper";
-import RenderAutoSizeField from "@mds/common/components/forms/RenderAutoSizeField";
 import { Field } from "redux-form";
 import RenderSubmitButton from "@mds/common/components/forms/RenderSubmitButton";
 import RenderCheckbox from "@mds/common/components/forms/RenderCheckbox";
+import RenderRichTextEditor from "@mds/common/components/forms/RenderRichTextEditor";
+import parse from "html-react-parser";
 
 interface AppHelpContentProps {
-    helpKey: string;
+  helpKey: string;
 }
 const AppHelpContent: FC<AppHelpContentProps> = ({ helpKey }) => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const params = useParams<any>();
-    const { tab = "", activeTab = "" } = params;
-    const tabKey = activeTab ?? tab;
-    const keyToUse = tabKey ? `${helpKey}_${tabKey}` : helpKey;
-    const [editMode, setEditMode] = useState(false);
-    const appHelp = useSelector(getHelpByKey(helpKey, tabKey));
-    const hasPermission = true;
-    console.log(appHelp)
+  const params = useParams<any>();
+  const { tab = "", activeTab = "" } = params;
+  const tabKey = activeTab ?? tab;
+  const keyToUse = tabKey ? `${helpKey}_${tabKey}` : helpKey;
+  const [editMode, setEditMode] = useState(false);
+  const appHelp = useSelector(getHelpByKey(helpKey, tabKey));
+  const hasPermission = true;
+  console.log(appHelp);
 
-    const onSubmit = async (values) => {
-        console.log('keyToUse', keyToUse)
-        const { create_fallback } = values;
-        const key = create_fallback ? helpKey : keyToUse
-        await dispatch(createHelp({ helpKey: key, data: values }));
-        setEditMode(false);
-    }
+  const onSubmit = async (values) => {
+    console.log("keyToUse", keyToUse);
+    const { create_fallback } = values;
+    const key = create_fallback ? helpKey : keyToUse;
+    await dispatch(createHelp({ helpKey: key, data: values }));
+    setEditMode(false);
+  };
 
-    if (hasPermission && (!appHelp || editMode)) {
-        return <FormWrapper
-            name={`EDIT_APP_HELP-${keyToUse}`}
-            onSubmit={onSubmit}
-            initialValues={appHelp}
-            reduxFormConfig={{ enableReinitialize: true }}
-        >
-            Editing page help for key: <b>{keyToUse}</b>
-            <Field
-                label="Enter content for page help"
-                name="content"
-                maximumCharacters={4000}
-                component={RenderAutoSizeField}
-            />
-            <Field
-                component={RenderCheckbox}
-                name="is_draft"
-                label="Save as draft"
-            />
-            {tabKey && <Field
-                component={RenderCheckbox}
-                name="create_fallback"
-                label="For all tabs"
-            />}
-            <RenderSubmitButton />
-        </FormWrapper>
-    }
-    if (appHelp && !appHelp.is_draft) {
-        return (
-            <>
-                <div>{appHelp.content}</div>
-                <div>
-                    <Button onClick={() => setEditMode(true)}>Edit</Button>
-                </div>
-            </>
-        );
-    }
-    return <div>this is a placeholder- there seems to be no help on this page</div>;
-}
+  if (hasPermission && (!appHelp || editMode)) {
+    return (
+      <FormWrapper
+        name={`EDIT_APP_HELP-${keyToUse}`}
+        onSubmit={onSubmit}
+        initialValues={appHelp}
+        isEditMode={editMode}
+        reduxFormConfig={{ enableReinitialize: true }}
+      >
+        Editing page help for key: <b>{keyToUse}</b>
+        <Field label="React Quill editor" name="content" component={RenderRichTextEditor} />
+        <Field component={RenderCheckbox} name="is_draft" label="Save as draft" />
+        {tabKey && <Field component={RenderCheckbox} name="create_fallback" label="For all tabs" />}
+        {!editMode && <Button onClick={() => setEditMode(true)}>Edit</Button>}
+        <RenderSubmitButton />
+      </FormWrapper>
+    );
+  }
+  if (appHelp && !appHelp.is_draft) {
+    return (
+      <>
+        <div>{parse(appHelp.content)}</div>
+        <div>
+          <Button onClick={() => setEditMode(true)}>Edit</Button>
+        </div>
+      </>
+    );
+  }
+  return <div>this is a placeholder- there seems to be no help on this page</div>;
+};
 
 export const AppHelp: FC = () => {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-    const showDrawer = () => setOpen(true);
-    const hideDrawer = () => setOpen(false);
+  const showDrawer = () => setOpen(true);
+  const hideDrawer = () => setOpen(false);
 
-    return (
-        <Switch>
-            {(Object.entries(routes)).map(([routeName, route]) => {
-                return <Route
-                    key={routeName}
-                    exact={true}
-                    path={route.route}
-                >
-                    {route.helpKey &&
-                        <>
-                            <Button onClick={showDrawer}>?</Button>
-                            <Drawer placement="right" onClose={hideDrawer} open={open}>
-                                <AppHelpContent helpKey={route.helpKey} /></Drawer>
-                        </>}
-                </Route>
-            })}
-        </Switch>
-    );
-}
+  return (
+    <Switch>
+      {Object.entries(routes).map(([routeName, route]) => {
+        return (
+          <Route key={routeName} exact={true} path={route.route}>
+            {route.helpKey && (
+              <>
+                <Button onClick={showDrawer}>?</Button>
+                <Drawer placement="right" onClose={hideDrawer} open={open}>
+                  <AppHelpContent helpKey={route.helpKey} />
+                </Drawer>
+              </>
+            )}
+          </Route>
+        );
+      })}
+    </Switch>
+  );
+};
