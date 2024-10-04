e04ee3c9cf377f5ed863bd99f29b0a60c42fc098
Tara Epp <102187683+taraepp@users.noreply.github.com>
2024-10-04 20:07:02 +0000

    add to CORE header. Refactor CORE navbar

---
+++ services/core-web/src/tests/components/navigation/NavBar.spec.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { render } from "@testing-library/react";
+import { ReduxWrapper } from "@mds/common/tests/utils/ReduxWrapper";
+import { NavBar } from "@/components/navigation/NavBar";
+import { AUTHENTICATION, MINES } from "@mds/common/constants/reducerTypes";
+
+const initialState = {
+  [AUTHENTICATION]: {
+    userInfo: {},
+  },
+  [MINES]: {
+    currentUserVerifiedMines: [],
+    currentUserUnverifiedMinesMines: [],
+  },
+};
+
+describe("NavBar", () => {
+  it("renders properly", () => {
+    const { container } = render(
+      <ReduxWrapper initialState={initialState}>
+        <BrowserRouter>
+          <NavBar activeButton="" isMenuOpen={false} toggleHamburgerMenu={jest.fn()} />
+        </BrowserRouter>
+      </ReduxWrapper>
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
