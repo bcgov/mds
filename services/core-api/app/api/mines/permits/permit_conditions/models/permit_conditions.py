5157962721a922c66066d615b34258ce4d297080
Simen Fivelstad Smaaberg <66635118+simensma-fresh@users.noreply.github.com>
2024-10-08 14:20:02 -0700

    Remove unused import

--- services/core-api/app/api/mines/permits/permit_conditions/models/permit_conditions.py
+++ services/core-api/app/api/mines/permits/permit_conditions/models/permit_conditions.py
@@ -6,7 +6,7 @@ from app.api.utils.list_lettering_helpers import num_to_letter, num_to_roman
 from app.api.utils.models_mixins import AuditMixin, Base, SoftDeleteMixin
 from app.extensions import db
 from marshmallow import fields, validate
-from sqlalchemy.dialects.postgresql import JSON, UUID
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import backref, validates
 from sqlalchemy.schema import FetchedValue
