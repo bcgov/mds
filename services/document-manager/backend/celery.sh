5618e172fbc20b31b273fea6980e3e11b42fce1f
Sinn <Alex.Sinn@gov.bc.ca>
2024-10-08 13:54:14 -0700

    testing change to docman celery worker

--- services/document-manager/backend/celery.sh
+++ services/document-manager/backend/celery.sh
@@ -2,6 +2,6 @@
 
 cd /app
 
-celery -A app.tasks.celery worker --loglevel=info --logfile=/tmp/celery/celery.log --pidfile=/tmp/celery/celery.pid --concurrency=1
+celery -A app.tasks.celery worker --loglevel=info --logfile=/tmp/celery/celery.log --pidfile=/tmp/celery/celery.pid --concurrency=1 -n doc_man@%h
 
 celery -A app.tasks.celery flower --detach --basic_auth=${FLOWER_USER}:${FLOWER_USER_PASSWORD}
