c78bdbaeeecba511225f3628d10982eabc752d07
Simen Fivelstad Smaaberg <66635118+simensma-fresh@users.noreply.github.com>
2024-10-08 15:51:36 -0700

    [MDS-6118] Fixed numbering of permit conditions when missing (#3259)
    * MDS-6118 Fixed numbering of permit conditions when missing
    
    * Fixed create_certs error
    
    * Update .env-example
    
    * Remove unused import

--- services/core-api/app/api/mines/permits/permit_extraction/models/permit_extraction_task.py
+++ services/core-api/app/api/mines/permits/permit_extraction/models/permit_extraction_task.py
@@ -8,14 +8,19 @@ from sqlalchemy.schema import FetchedValue
 class PermitExtractionTask(AuditMixin, Base):
     __tablename__ = 'permit_extraction_task'
 
+    # This is a unique identifier for the task
     permit_extraction_task_id = db.Column(
         UUID(as_uuid=True), primary_key=True, server_default=db.FetchedValue())
+    
+    # This is the task_id for the actual extraction task as returned by the permit service
     task_id = db.Column(db.String(255), nullable=False)
     task_status = db.Column(db.String(255), nullable=False)
     task_meta = db.Column(db.JSON, nullable=True)
     task_result = db.Column(db.JSON, nullable=True)
 
+    # This is the task_id for the core celery task that is responsible for updating the status of the extraction task
     core_status_task_id = db.Column(db.String(255), nullable=True)
+    
     permit_amendment_guid = db.Column(
         UUID(as_uuid=True), db.ForeignKey('permit_amendment.permit_amendment_guid'), nullable=False)
     permit_amendment_document_guid = db.Column(
