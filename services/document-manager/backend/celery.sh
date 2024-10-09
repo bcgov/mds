bae496a2d1edc48b9e87fbaca8b6833f6e8ebc9f
asinn134 <127789479+asinn134@users.noreply.github.com>
2024-10-09 14:33:39 -0700

    [MDS-6153 ] Testing Docman log issues (#3262)
    * testing change to docman celery worker
    
    * name change
    
    * reverting docman celery.sh change
    
    * adding back name and detach to worker
    
    * testing out detach in worker

--- services/document-manager/backend/celery.sh
+++ services/document-manager/backend/celery.sh
@@ -2,5 +2,5 @@
 
 cd /app
 
-celery -A app.tasks.celery worker --detach --loglevel=info --logfile=/tmp/celery/celery.log --pidfile=/tmp/celery/celery.pid --concurrency=1 -n document_manager_service@%h
-celery -A app.tasks.celery flower --basic_auth=${FLOWER_USER}:${FLOWER_USER_PASSWORD}
+celery -A app.tasks.celery worker --loglevel=info --logfile=/tmp/celery/celery.log --pidfile=/tmp/celery/celery.pid --concurrency=1 -n document_manager_service@%h
+celery -A app.tasks.celery flower --detach --basic_auth=${FLOWER_USER}:${FLOWER_USER_PASSWORD}
