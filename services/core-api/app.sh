a33c6d12d25302d581bb63aa6cc66da1ca7f3c6a
Simen Fivelstad Smaaberg <66635118+simensma-fresh@users.noreply.github.com>
2024-11-07 23:51:31 +0000

    [MDS-6244] Moved gunicorn config to a file

--- services/core-api/app.sh
+++ services/core-api/app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run the app with 3 workers and a request timeout of 200 seconds
-gunicorn -w 3  --timeout 200 --access-logfile - --error-logfile - --log-level debug --capture-output 'app:create_app()' -b 0.0.0.0:5000 -c app/gunicorn_config.py
+gunicorn -c app/gunicorn_config.py 'app:create_app()'  
---
+++ services/core-api/app/gunicorn_config.py
@@ -0,0 +1,22 @@
+import os
+import traceback
+
+
+def opt_env(var_name):
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        return None
+
+# Print stack trace if a worker is killed (e.g. from a timeout)
+def worker_abort(worker):
+    pid = worker.pid
+    print("worker is being killed - {}".format(pid))
+    traceback.print_stack()
+
+bind = "0.0.0.0:5000"
+timeout = opt_env("GUNICORN_TIMEOUT") or 200
+workers = opt_env("GUNICORN_WORKERS") or 3
+errorlog = opt_env("GUNICORN_ERROR_LOG") or "-"
+loglevel = opt_env("GUNICORN_LOG_LEVEL") or "debug"
+accesslog = opt_env("GUNICORN_ACCESS_LOG") or "-"
