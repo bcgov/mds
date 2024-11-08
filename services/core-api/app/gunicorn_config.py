a5ab8fa39be8ca1f9364fd19d8a51668acf61c8b
Simen Fivelstad Smaaberg <66635118+simensma-fresh@users.noreply.github.com>
2024-11-08 00:19:28 +0000

    Added gunicorn max requests

--- services/core-api/app/gunicorn_config.py
+++ services/core-api/app/gunicorn_config.py
@@ -21,3 +21,8 @@ workers = opt_env("GUNICORN_WORKERS") or 3
 errorlog = opt_env("GUNICORN_ERROR_LOG") or "-"
 loglevel = opt_env("GUNICORN_LOG_LEVEL") or "debug"
 accesslog = opt_env("GUNICORN_ACCESS_LOG") or "-"
+
+# Restart a unicorn worker process after it has handled 500 - 600 requests (random to not restart all at once)
+# to protect against slow memory leaks
+max_requests = opt_env("GUNICORN_MAX_REQUESTS") or 500
+max_requests_jitter = opt_env("GUNICORN_MAX_REQUESTS_JITTER") or 100
