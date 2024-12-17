a417e93cf13eed25c786345da14df5e30cbb7398
Sinn <Alex.Sinn@gov.bc.ca>
2024-12-17 15:03:03 -0800

    addressing pr comment

--- services/core-api/tests/mines/explosives_permit_amendment/models/test_explosives_permit_amendment_model.py
+++ services/core-api/tests/mines/explosives_permit_amendment/models/test_explosives_permit_amendment_model.py
@@ -1,4 +1,4 @@
-from tests.factories import ExplosivesPermitAmendmentFactory
+from tests.factories import (ExplosivesPermitAmendmentFactory, ExplosivesPermitFactory)
 
 from app.api.mines.explosives_permit_amendment.models.explosives_permit_amendment import ExplosivesPermitAmendment
 
@@ -7,3 +7,9 @@ def test_explosives_permit_amendment_find_by_explosives_permit_amendment_guid(db
     explosives_permit_amendment_guid = explosives_permit_amendment.explosives_permit_amendment_guid
     explosives_permit_amendment = ExplosivesPermitAmendment.find_by_explosives_permit_amendment_guid(str(explosives_permit_amendment_guid))
     assert explosives_permit_amendment.explosives_permit_amendment_guid == explosives_permit_amendment_guid
+
+def test_explosives_permit_amendment_find_by_explosives_permit_id(db_session):
+    explosives_permit_amendment = ExplosivesPermitAmendmentFactory()
+    explosives_permit_id = explosives_permit_amendment.explosives_permit_id
+    explosives_permit_amendments = ExplosivesPermitAmendment.find_by_explosives_permit_id(explosives_permit_id)
+    assert any(amendment.explosives_permit_id == explosives_permit_id for amendment in explosives_permit_amendments)
