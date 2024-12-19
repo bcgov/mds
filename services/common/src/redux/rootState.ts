b65262f6d22279c375c6c93d1570586f649fa378
Simen Fivelstad Smaaberg <66635118+simensma-fresh@users.noreply.github.com>
2024-12-19 21:54:52 +0000

    [CHORE] Provide types for react-redux hooks

--- services/common/src/redux/rootState.ts
+++ services/common/src/redux/rootState.ts
@@ -2,6 +2,9 @@ import { sharedReducer } from "@mds/common/redux/reducers/rootReducerShared";
 import { configureStore } from "@reduxjs/toolkit";
 import { loadingBarReducer } from "react-redux-loading-bar";
 
+import type { TypedUseSelectorHook } from 'react-redux'
+import { useDispatch, useSelector, useStore } from 'react-redux'
+
 export const getStore = (preloadedState = {}) =>
   configureStore({
     reducer: {
@@ -14,6 +17,16 @@ export const getStore = (preloadedState = {}) =>
 
 export const store = getStore();
 
+// Provide typed versions for global redux functions
 type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
+export type AppStore = typeof store;
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppStore: () => AppStore = useStore;
+
 
 export { RootState };
