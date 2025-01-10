891283dced24bd4179912b546dd36d2a1265dd67
Sinn <Alex.Sinn@gov.bc.ca>
2025-01-09 17:26:38 -0800

    testing

--- services/common/src/components/project/ProjectDescriptionTab.spec.tsx
+++ services/common/src/components/project/ProjectDescriptionTab.spec.tsx
@@ -7,10 +7,36 @@ import { PROJECTS, PERMITS } from "@mds/common/constants/reducerTypes";
 
 const initialState = {
   [PROJECTS]: {
-    project: MOCK.PROJECT,
+    project: {
+      ...MOCK.PROJECT,
+      project_summary: {
+        ...MOCK.PROJECT.project_summary,
+        authorizations: [...MOCK.PROJECT.project_summary.authorizations,
+        {
+          project_summary_authorization_guid: "c47161c0-8860-4125-8e68-fac3d98cf429",
+          project_summary_guid: "8b4b9781-2e59-43ef-8164-4cc3b964417a",
+          project_summary_permit_type: ["AMENDMENT"],
+          project_summary_authorization_type: "REFUSE_DISCHARGE_PERMIT",
+          existing_permits_authorizations: ['1234'],
+          amendment_changes: null,
+          amendment_severity: null,
+          is_contaminated: null,
+          new_type: "PER",
+          authorization_description: "asdf",
+          exemption_reason: "asdasdas",
+          amendment_documents: [],
+          exemption_requested: false,
+          ams_tracking_number: "0",
+          ams_outcome: "Some error",
+          ams_status_code: "400",
+          ams_submission_timestamp: "2024-05-24T19:17:09.212499+00:00",
+        }
+        ]
+      }
+    },
   },
   [PERMITS]: { permits: MOCK.PERMITS },
-};
+}
 
 describe("ProjectDescriptionTab", () => {
   it("renders properly", () => {
