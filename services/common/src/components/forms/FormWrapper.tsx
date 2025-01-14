8e0bc3ebfe0a848e8bccff2ba8006d94481c025b
Simen Fivelstad Smaaberg <66635118+simensma-fresh@users.noreply.github.com>
2025-01-14 19:29:08 +0000

    Updated snapshots

--- services/common/src/components/forms/FormWrapper.tsx
+++ services/common/src/components/forms/FormWrapper.tsx
@@ -68,4 +68,4 @@ export default compose(
   reduxForm({})
 )(FormWrapper as any) as FC<FormWrapperProps>;
 
-export { FormContext } from "./BaseFormWrapper";
+export { FormContext, useFormContext, FormConsumer } from "./BaseFormWrapper";
