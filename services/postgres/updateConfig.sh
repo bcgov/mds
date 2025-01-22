ee91a5603209bc60bc7d7345ed22e3353154760c
sggerard <sggerard1997@gmail.com>
2025-01-22 15:55:48 -0800

    Update dockerfile, add updatedb command

--- services/postgres/updateConfig.sh
+++ services/postgres/updateConfig.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-cat /tmp/postgresql.conf >/var/lib/postgresql/data/pgdata/postgresql.conf
+cat /tmp/postgresql.conf >/var/lib/postgresql/data/postgresql.conf
+cat /tmp/pg_hba.conf >/var/lib/postgresql/data/pg_hba.conf
