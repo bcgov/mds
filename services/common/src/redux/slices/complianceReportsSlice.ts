b03c71ccae0c86ad332fe47b5d63afc2f2af3a17
Tara Epp <102187683+taraepp@users.noreply.github.com>
2025-01-24 23:01:31 +0000

    get the data in the table and pagination working properly with BE pagination. TODO: I'm missing data fields, and the sort only applies to data currently on the page

--- services/common/src/redux/slices/complianceReportsSlice.ts
+++ services/common/src/redux/slices/complianceReportsSlice.ts
@@ -3,7 +3,7 @@ import { ENVIRONMENT } from "@mds/common/constants/environment";
 import { createAppSlice, rejectHandler } from "@mds/common/redux/createAppSlice";
 import CustomAxios from "@mds/common/redux/customAxios";
 import * as API from "@mds/common/constants/API";
-import { IMineReportDefinition, ItemMap } from "@mds/common/interfaces";
+import { IMineReportDefinition, IPageData, ItemMap } from "@mds/common/interfaces";
 import { createItemMap } from "@mds/common/redux/utils/helpers";
 
 
@@ -11,10 +11,18 @@ export const complianceReportReducerType = 'complianceReports'
 
 interface ComplianceReportState {
     complianceReportMap: ItemMap<IMineReportDefinition>;
+    reportPageData: IPageData<IMineReportDefinition>,
 };
 
 const initialState: ComplianceReportState = {
     complianceReportMap: null,
+    reportPageData: {
+        records: [],
+        current_page: 0,
+        items_per_page: 0,
+        total: 0,
+        total_pages: 0
+    }
 };
 
 const createRequestHeader = REQUEST_HEADER.createRequestHeader;
@@ -31,13 +39,16 @@ const complianceReportSlice = createAppSlice({
                     errorToastMessage: "Failed to load compliance reports",
                 }).get(`${ENVIRONMENT.apiUrl}${API.MINE_REPORT_DEFINITIONS(searchParams)}`, headers);
                 thunkApi.dispatch(hideLoading());
+                console.log(resp);
                 return resp.data;
             },
             {
                 fulfilled: (state: ComplianceReportState, action) => {
                     const records: IMineReportDefinition[] = action.payload.records ?? [];
+                    const { current_page, items_per_page, total, total_pages } = action.payload;
                     const itemMap = createItemMap(records, "mine_report_definition_guid")
                     state.complianceReportMap = itemMap;
+                    state.reportPageData = { records, current_page, items_per_page, total, total_pages };
                 },
                 rejected: (_state: ComplianceReportState, action) => {
                     rejectHandler(action);
@@ -48,6 +59,12 @@ const complianceReportSlice = createAppSlice({
     selectors: {
         getComplianceReports: (state): ItemMap<IMineReportDefinition> => {
             return state.complianceReportMap;
+        },
+        getComplianceReportsAsList: (state): IMineReportDefinition[] => {
+            return Object.values(state.complianceReportMap);
+        },
+        getComplianceReportPageData: (state): IPageData<IMineReportDefinition> => {
+            return state.reportPageData;
         }
     }
 });
@@ -55,7 +72,7 @@ const complianceReportSlice = createAppSlice({
 export const {
     fetchComplianceReports
 } = complianceReportSlice.actions;
-export const { getComplianceReports } = complianceReportSlice.selectors;
+export const { getComplianceReports, getComplianceReportsAsList, getComplianceReportPageData } = complianceReportSlice.selectors;
 
 const complianceReportReducer = complianceReportSlice.reducer;
 export default complianceReportReducer;
--- services/core-web/src/components/admin/complianceCodes/ComplianceReportManagement.tsx
+++ services/core-web/src/components/admin/complianceCodes/ComplianceReportManagement.tsx
@@ -1,17 +1,68 @@
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { Button, Row, Typography } from "antd";
 import PlusOutlined from "@ant-design/icons/PlusOutlined";
 import CoreTable from "@mds/common/components/common/CoreTable";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchComplianceReports, getComplianceReportPageData } from "@mds/common/redux/slices/complianceReportsSlice";
+import { renderActionsColumn, renderTextColumn } from "@mds/common/components/common/CoreTableCommonColumns";
+import { IMineReportDefinition } from "@mds/common/interfaces";
 
 
 const ComplianceReportManagement: FC = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const codeRequiredReports = [];
-    const columns = [];
+    const dispatch = useDispatch();
+    const reportPageData = useSelector(getComplianceReportPageData);
+    const reportDefinitions = reportPageData?.records ?? [];
+    const defaultLoaded = reportPageData.total > 0;
+    console.log(reportPageData);
+    const [isLoading, setIsLoading] = useState(defaultLoaded);
+
+
+    const fetchData = (page = 1, per_page = 50) => {
+        setIsLoading(true);
+        dispatch(fetchComplianceReports({ page, per_page })).then(setIsLoading(false))
+    }
+
+    useEffect(() => {
+        if (!isLoading && !defaultLoaded) {
+            fetchData();
+        }
+    }, []);
 
     const openAddModal = () => {
         console.log('not implemented');
     };
+
+    const openViewModal = (record) => {
+        console.log('record', record)
+    };
+
+    const actions = [{
+        key: "view",
+        label: "View",
+        clickFunction: (_, record) => openViewModal(record)
+    }];
+
+    // TODO: the sorting for report name only sorts the data that is currently on the page
+    // and also I don't have data for the columns that are currently commented out
+
+    const columns = [
+        renderTextColumn("report_name", "Report Name", true), // sort
+        // renderTextColumn("section", "Section"), // sort, filter
+        renderTextColumn("report_type", "Report Type"), // filter
+        // renderTextColumn("regulatory_authority", "Regulatory Authority"), // sort, filter
+        // renderTextColumn("office", "Office"), // sort, filter
+        renderActionsColumn({ actions })
+    ];
+
+    const transformData = (reports: IMineReportDefinition[]) => {
+        return reports.map((r) => {
+            const report_type = r.is_prr_only ? "Permit Required Report" : "Core Required Report";
+            return {
+                ...r,
+                report_type
+            };
+        })
+    }
 
     return (
         <div>
@@ -29,14 +80,15 @@ const ComplianceReportManagement: FC = () => {
             </Row>
             <CoreTable
                 loading={isLoading}
-                dataSource={codeRequiredReports}
+                dataSource={transformData(reportDefinitions)}
                 columns={columns}
-                rowKey="key"
-                pagination={{
-                    total: codeRequiredReports.length,
+                rowKey="mine_report_definition_guid"
+                pagination={!isLoading && {
+                    total: reportPageData.total,
                     defaultPageSize: 50,
                     position: ['bottomCenter'],
-                    disabled: isLoading
+                    disabled: isLoading,
+                    onChange: fetchData
                 }}
             />
 
