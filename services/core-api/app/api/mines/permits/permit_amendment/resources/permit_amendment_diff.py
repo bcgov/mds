6b76359e029052abdfa1a61c99eb84cb78d8f99a
Simen Fivelstad Smaaberg <66635118+simensma-fresh@users.noreply.github.com>
2025-02-05 17:07:48 +0000

    MDS-6029 Added condition guid to api docs

--- services/core-api/app/api/mines/permits/permit_amendment/resources/permit_amendment_diff.py
+++ services/core-api/app/api/mines/permits/permit_amendment/resources/permit_amendment_diff.py
@@ -13,8 +13,7 @@ from app.api.mines.permits.permit_conditions.services.permit_condition_comparer 
 from app.api.utils.access_decorators import requires_role_view_all
 from app.api.utils.resources_mixins import UserMixin
 from app.extensions import api
-from flask import request
-from flask_restx import Resource
+from flask_restx import Resource, reqparse
 from werkzeug.exceptions import BadRequest, NotFound
 
 from .response_models import PERMIT_CONDITION_DIFF_LIST_MODEL
@@ -31,10 +30,20 @@ def _find_previous_amendment(permit_amendment, all_permit_amendments):
     return previous_amendment
 
 class PermitAmendmentDiffResource(Resource, UserMixin):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'permit_condition_guid',
+        type=str,
+        help='Permit Condition in Amendment to use as source for comparison',
+        location='args',
+        required=False,
+        store_missing=False)
+
 
     @api.response(200, "Returns the diff between the given permit amendment and the previous one")
     @requires_role_view_all
     @api.marshal_with(PERMIT_CONDITION_DIFF_LIST_MODEL, code=200)
+    @api.expect(parser)
     def get(self, mine_guid, permit_guid, permit_amendment_guid):
         mine = Mine.find_by_mine_guid(mine_guid)
         
@@ -54,7 +63,9 @@ class PermitAmendmentDiffResource(Resource, UserMixin):
         if str(permit_amendment.permit_guid) != str(permit_guid):
             raise BadRequest("Permit Amendment does not belong to the given permit")
 
-        permit_condition_guid = request.args.get('permit_condition_guid')
+        data = self.parser.parse_args()
+
+        permit_condition_guid = data.get('permit_condition_guid')
         permit_condition = None
         
         if permit_condition_guid is not None:
