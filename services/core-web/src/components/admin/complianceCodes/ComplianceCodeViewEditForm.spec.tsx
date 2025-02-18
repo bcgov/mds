2b2db7a5f742b2853e0eb1dfe83dcad0544c6bd5
Tara Epp <102187683+taraepp@users.noreply.github.com>
2025-02-18 20:57:28 +0000

    add a test

---
+++ services/core-web/src/components/admin/complianceCodes/ComplianceCodeViewEditForm.spec.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { ReduxWrapper } from "@/tests/utils/ReduxWrapper";
+import ComplianceCodeViewEditForm from "./ComplianceCodeViewEditForm";
+import { COMPLIANCE_CODES } from "@/tests/mocks/dataMocks";
+import { complianceCodeReducerType } from "@mds/common/redux/slices/complianceCodesSlice";
+
+const initialState = {
+    [complianceCodeReducerType]: {
+        complianceCodeMap: {}
+    }
+};
+
+describe("ComplianceCodeViewEditForm", () => {
+    it("renders properly", async () => {
+        const { container } = render(
+            <ReduxWrapper initialState={initialState}>
+                <ComplianceCodeViewEditForm initialValues={null} isEditMode onSave={jest.fn()} />
+                <ComplianceCodeViewEditForm initialValues={COMPLIANCE_CODES[0]} isEditMode={false} onSave={jest.fn()} />
+            </ReduxWrapper>
+        );
+        expect(container).toMatchSnapshot()
+    });
+})
