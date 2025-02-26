d0d5d960fe80228b774f6b64d4769d84d5fb900b
Tara Epp <102187683+taraepp@users.noreply.github.com>
2025-02-26 17:38:49 +0000

    mock keycloak

--- services/minespace-web/src/tests/utils/authenticationHelpers.spec.js
+++ services/minespace-web/src/tests/utils/authenticationHelpers.spec.js
@@ -7,15 +7,15 @@ import {
 } from "@/constants/environment";
 
 // TODO: FIX SSO MIGRATION TEST
-describe("`signOutFromSSO` function", () => {
-  jest.spyOn(keycloak, "logout");
-  it.skip("opens a new window with given url", () => {
-    signOutFromSSO();
-    expect(keycloak.logout).toHaveBeenCalledWith({
-      redirectUri: KEYCLOAK_LOGOUT_REDIRECT_URI,
-    });
-  });
-});
+// describe("`signOutFromSSO` function", () => {
+//   jest.spyOn(keycloak, "logout");
+//   it.skip("opens a new window with given url", () => {
+//     signOutFromSSO();
+//     expect(keycloak.logout).toHaveBeenCalledWith({
+//       redirectUri: KEYCLOAK_LOGOUT_REDIRECT_URI,
+//     });
+//   });
+// });
 
 describe("`signOutFromSiteMinder` function", () => {
   jest.spyOn(window, "open");
