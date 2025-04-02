cd6043d1adf509202cdc37a907cb4cd81bc4c381
sggerard <sggerard1997@gmail.com>
2025-04-02 12:46:43 -0700

    Mock formatDateTimeUserTz

--- services/common/src/components/tailings/dam/DamDiffModal.spec.tsx
+++ services/common/src/components/tailings/dam/DamDiffModal.spec.tsx
@@ -4,7 +4,17 @@ import { ReduxWrapper } from "@mds/common/tests/utils/ReduxWrapper";
 import { DAM_WITH_HISTORY } from "@mds/common/tests/mocks/dataMocks";
 import { damReducerType } from "@mds/common/redux/slices/damSlice";
 import DamDiffModal from "./DamDiffModal";
+import moment from "moment";
+import { DATETIME_TZ_FORMAT, DEFAULT_TIMEZONE } from "@mds/common/constants/strings";
 
+jest.mock("@mds/common/redux/utils/helpers", () => {
+    const helpers = jest.requireActual("@mds/common/redux/utils/helpers");
+    return {
+        ...helpers,
+        formatDateTimeUserTz: jest.fn((dateTime) => moment(dateTime, true).isValid() && 
+        moment.tz(dateTime, DEFAULT_TIMEZONE).format(DATETIME_TZ_FORMAT))
+    }
+  });
 
 const initialState = {
     [damReducerType]: {
