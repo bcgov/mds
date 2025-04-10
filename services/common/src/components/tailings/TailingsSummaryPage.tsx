687ceead5b733364e5598071c3f29767c248478d
Tara Epp <102187683+taraepp@users.noreply.github.com>
2025-04-10 20:19:58 +0000

    appease the sonarcloud

--- services/common/src/components/tailings/TailingsSummaryPage.tsx
+++ services/common/src/components/tailings/TailingsSummaryPage.tsx
@@ -193,7 +193,7 @@ export const TailingsSummaryPage: FC = () => {
     if (newActiveTab) {
       history.push(
         GLOBAL_ROUTES?.EDIT_TAILINGS_STORAGE_FACILITY.dynamicRoute(
-          newTsf?.data.mine_tailings_storage_facility_guid || tsfGuid,
+          newTsf?.data.mine_tailings_storage_facility_guid ?? tsfGuid,
           mineGuid,
           newActiveTab,
           isUserActionEdit
