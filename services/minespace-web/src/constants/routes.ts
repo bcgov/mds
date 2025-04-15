2dab3b102a6ce4b71c73eff2536466e3f33c13b7
sggerard <sggerard1997@gmail.com>
2025-04-15 15:44:56 -0700

    allow subTab to be null

--- services/minespace-web/src/constants/routes.ts
+++ services/minespace-web/src/constants/routes.ts
@@ -213,7 +213,7 @@ const getQueryString = (filterParams?) => {
 
 export const MINE_DASHBOARD = {
   route: "/mines/:id/:activeTab/:subTab?",
-  dynamicRoute: (id, activeTab = "overview", subTab, filterParams?: any) =>
+  dynamicRoute: (id, activeTab = "overview", subTab?, filterParams?: any) =>
     `/mines/${id}/${activeTab}${subTab ? `/${subTab}` : ""}${getQueryString(filterParams)}`,
   component: MineDashboard,
   helpKey: "Mine-Dashboard",
