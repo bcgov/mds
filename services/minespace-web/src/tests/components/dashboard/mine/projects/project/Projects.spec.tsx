0c5a1535f40edcc597edf8812ed41af963e4805c
sggerard <sggerard1997@gmail.com>
2025-04-16 11:11:13 -0700

    Update test, snaps

--- services/minespace-web/src/tests/components/dashboard/mine/projects/project/Projects.spec.tsx
+++ services/minespace-web/src/tests/components/dashboard/mine/projects/project/Projects.spec.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Projects } from "@/components/dashboard/mine/projects/Projects";
 import * as MOCK from "@mds/common/tests/mocks/dataMocks";
 import { render } from "@testing-library/react";
 import { ReduxWrapper } from "@/tests/utils/ReduxWrapper";
 import { PROJECTS } from "@mds/common/constants/reducerTypes";
 import { SidebarProvider } from "@mds/common/components/common/SidebarWrapper";
 import { BrowserRouter } from "react-router-dom";
+import ViewProjects from "@/components/dashboard/mine/projects/ViewProjects";
 
 const initialState = {
   [PROJECTS]: { projects: MOCK.PROJECTS.records },
@@ -14,12 +14,25 @@ const initialState = {
 const mine = MOCK.MINES.mines[MOCK.MINES.mineIds[0]];
 
 describe("Projects", () => {
-  it("renders properly", () => {
+  it("renders major mines application tab properly", () => {
     const { container } = render(
       <BrowserRouter>
         <ReduxWrapper initialState={initialState}>
-          <SidebarProvider value={{ mine } as any}>
-            <Projects />
+          <SidebarProvider value={{ mine, subTab: "major-mine" } as any}>
+            <ViewProjects />
+          </SidebarProvider>
+        </ReduxWrapper>
+      </BrowserRouter>
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  it("renders now application tab properly", () => {
+    const { container } = render(
+      <BrowserRouter>
+        <ReduxWrapper initialState={initialState}>
+          <SidebarProvider value={{ mine, subTab: "notice-of-work" } as any}>
+            <ViewProjects />
           </SidebarProvider>
         </ReduxWrapper>
       </BrowserRouter>
