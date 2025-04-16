279ef51c5c54ed0730ef902d18df9746a9261238
sggerard <sggerard1997@gmail.com>
2025-04-16 15:16:28 -0700

    Add NOW data to test

--- services/minespace-web/src/tests/components/dashboard/mine/projects/project/Projects.spec.tsx
+++ services/minespace-web/src/tests/components/dashboard/mine/projects/project/Projects.spec.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import * as MOCK from "@mds/common/tests/mocks/dataMocks";
 import { render } from "@testing-library/react";
 import { ReduxWrapper } from "@/tests/utils/ReduxWrapper";
-import { PROJECTS } from "@mds/common/constants/reducerTypes";
+import { NOTICE_OF_WORK, PROJECTS } from "@mds/common/constants/reducerTypes";
 import { SidebarProvider } from "@mds/common/components/common/SidebarWrapper";
 import { BrowserRouter } from "react-router-dom";
 import ViewProjects from "@/components/dashboard/mine/projects/ViewProjects";
 
 const initialState = {
   [PROJECTS]: { projects: MOCK.PROJECTS.records },
+  [NOTICE_OF_WORK]: { noticeOfWorkList: MOCK.NOW.applications },
 };
 
 const mine = MOCK.MINES.mines[MOCK.MINES.mineIds[0]];
