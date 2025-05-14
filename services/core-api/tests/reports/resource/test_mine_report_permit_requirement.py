4f342fa76d78d34807297353aabbb61b512d2e5a
Tara Epp <102187683+taraepp@users.noreply.github.com>
2025-05-14 14:46:13 +0000

    add a little BE test

--- services/core-api/tests/reports/resource/test_mine_report_permit_requirement.py
+++ services/core-api/tests/reports/resource/test_mine_report_permit_requirement.py
@@ -104,3 +104,48 @@ def test_update_mine_report_permit_requirement(test_client, db_session, auth_hea
     assert update_data['ministry_recipient'] == report_requirement.ministry_recipient
     assert update_data['permit_condition_ids'] == permit_condition_ids
     assert update_data['mine_report_permit_requirement_id'] == report_requirement.mine_report_permit_requirement_id
+
+def test_update_mine_report_permit_requirement_remove_condition(test_client, db_session, auth_headers):
+    mine, permit = create_mine_and_permit()
+    amendment = permit.permit_amendments[0]
+
+    report_requirement = MineReportPermitRequirementFactory(
+        permit_amendment=amendment
+    )
+
+    permit_conditions = [amendment.conditions[0], amendment.conditions[1]]
+    
+    for c in permit_conditions:
+        MineReportReqPermitConditionXrefFactory(
+            permit_condition=c,
+            mine_report_permit_requirement=report_requirement
+        )
+
+    # Only keep the first permit condition
+    updated_permit_condition_ids = [permit_conditions[0].permit_condition_id]
+
+    submission_data = {
+        'due_date_period_months': 6,
+        'initial_due_date': date.today().strftime('%Y-%m-%d'),
+        'cim_or_cpo': 'CIM',
+        'ministry_recipient': report_requirement.ministry_recipient,
+        'permit_condition_ids': updated_permit_condition_ids,
+        'permit_amendment_id': report_requirement.permit_amendment_id,
+        'mine_report_permit_requirement_id': report_requirement.mine_report_permit_requirement_id,
+    }
+
+    update_resp = test_client.put(
+        f'/mines/{mine.mine_guid}/reports/permit-requirements',
+        headers=auth_headers['full_auth_header'],
+        json=submission_data,
+    )
+
+    update_data = json.loads(update_resp.data.decode())
+
+    assert update_resp.status_code == 200
+    assert update_data['due_date_period_months'] == 6
+    assert update_data['initial_due_date'] == date.today().strftime('%Y-%m-%d')
+    assert update_data['cim_or_cpo'] == 'CIM'
+    assert update_data['ministry_recipient'] == report_requirement.ministry_recipient
+    assert update_data['permit_condition_ids'] == updated_permit_condition_ids
+    assert update_data['mine_report_permit_requirement_id'] == report_requirement.mine_report_permit_requirement_id
