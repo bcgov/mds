594b2f6456f287481bd87d4683676a2f768470e9
sggerard <sggerard1997@gmail.com>
2025-08-27 10:57:55 -0700

    Remove unnessasary css rule

--- services/common/src/components/permits/VariableConditionMenu.tsx
+++ services/common/src/components/permits/VariableConditionMenu.tsx
@@ -144,7 +144,7 @@ const VariableConditionMenu: FC<VariableConditionMenuProps> = ({
             onOpenChange={() => {}}
           >
             <div className="fa-icon-container ant-primary" style={{color: "white"}}>
-              <Typography.Text style={{ fontWeight: 600 }}>Condition Data Variables </Typography.Text>
+              <Typography.Text style={{ fontWeight: 600, color: "white" }}>Condition Data Variables </Typography.Text>
               <DownOutlined />
               <Tooltip title={"Hover your mouse over the menus until you find the variable data you'd like to enter. Put it into your edited Permit Condition by clicking on it. This will populate the edited condition with a variable in the Draft permit screen. The Data from the Application tab will show up correctly in the PDF Draft permit in place of the variable. Please ensure all variable data fields you select have the correct information in the Application tab before adding these fields to your draft permit."}
                 placement="right" mouseEnterDelay={0.3} overlayClassName="core-tooltip" getPopupContainer={triggerNode => triggerNode.parentNode as HTMLElement}>
