bad9164fe1646a6dcad69d86074c302ee83617ff
sggerard <sggerard1997@gmail.com>
2025-09-10 12:51:32 -0700

    Don't show Menu unless NOW or StandardConditionEditor

--- services/common/src/components/permits/PermitConditionForm.tsx
+++ services/common/src/components/permits/PermitConditionForm.tsx
@@ -322,7 +322,7 @@ const PermitConditionForm: FC<PermitConditionFormProps> = ({
                 </Col>
                 <Col className="condition-column" {...editableProps}>
                     <Col className="condition-editor">
-                        <VariableConditionMenu inputRef={conditionInputRef} isManagementView={Boolean(standardConditionType)} conditionForm={editingFormName}/>
+                        {stepEditDisabled && <VariableConditionMenu inputRef={conditionInputRef} isManagementView={Boolean(standardConditionType)} conditionForm={editingFormName}/>}
                         <Field
                             name="condition"
                             component={RenderAutoSizeField}
