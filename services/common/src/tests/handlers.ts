37464af3e2858557a03b847b10142d8d261f3c47
Simen Fivelstad Smaaberg <simen.smaaberg@ca.ey.com>
2025-09-17 12:14:18 -0700

    MDS-6525 Fixed Failing NoW contact test

--- services/common/src/tests/handlers.ts
+++ services/common/src/tests/handlers.ts
@@ -47,6 +47,10 @@ const projectHandlers = [
   http.get("/%3CAPI_URL%3E/projects/35633148-57f8-4967-be35-7f89abfbd02e", async () => {
     return HttpResponse.json(PROJECT);
   }),
+  // Requirements list used by InformationRequirementsTable and other project views
+  http.get("/%3CAPI_URL%3E/projects/requirements", async () => {
+    return HttpResponse.json(MOCK.REQUIREMENTS);
+  }),
   http.get(
     "/%3CAPI_URL%3E/projects/70414192-ca71-4d03-93a5-630491e9c554/ministry-comments",
     async () => {
