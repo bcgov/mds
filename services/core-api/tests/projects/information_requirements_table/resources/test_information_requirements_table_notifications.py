d3a63089dbb5d419ea9d1d2b9037a8de0aea9f43
mat <matbusby@gmail.com>
2025-10-09 21:11:30 -0600

    fixed irt test

--- services/core-api/tests/projects/information_requirements_table/resources/test_information_requirements_table_notifications.py
+++ services/core-api/tests/projects/information_requirements_table/resources/test_information_requirements_table_notifications.py
@@ -51,15 +51,23 @@ def test_information_requirements_table_notifications(mock_send_template_email, 
     expected_calls = [
         call(
             subject,
-            minespace_recipients,
-            "email/projects/minespace_project_section_email.html",
-            context
+            [MAJOR_MINES_OFFICE_EMAIL, project.project_lead.email],
+            "email/projects/ministry_project_section_email.html",
+            context,
+            reference_id=irt.irt_guid,
+            reference_table='information_requirements_table',
+            email_template_name='ministry_irt_status_notification',
+            create_tracking_record=True
         ),
         call(
             subject,
-            [MAJOR_MINES_OFFICE_EMAIL, project.project_lead.email],
-            "email/projects/ministry_project_section_email.html",
-            context
+            minespace_recipients,
+            "email/projects/minespace_project_section_email.html",
+            context,
+            reference_id=irt.irt_guid,
+            reference_table='information_requirements_table',
+            email_template_name='minespace_irt_status_notification',
+            create_tracking_record=True
         )
     ]
 
