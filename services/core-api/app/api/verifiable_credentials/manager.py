ce23dca585201d8dd7dcc6da2d1a620be6047acb
Jason Syrotuck <177293@nttdata.com>
2025-10-15 15:08:18 -0700

    always include regional

--- services/core-api/app/api/verifiable_credentials/manager.py
+++ services/core-api/app/api/verifiable_credentials/manager.py
@@ -56,31 +56,6 @@ permit_amendments_for_orgbook_query = """
     and mpa.deleted_ind = false
     and mpa.start_date <= pa.issue_date
     and (mpa.end_date > pa.issue_date OR mpa.end_date is null or mpa.end_date = '9999-12-31')
-    and m.major_mine_ind = true
-    and pa.deleted_ind = false
-    and pmt.permit_status_code = 'O'
-    and substring(pmt.permit_no,2,1) != 'X'
-
-    group by pa.permit_amendment_guid, p.party_guid, pa.description, pa.issue_date, pa.permit_amendment_status_code, pmt.permit_no, mpa.permit_id, poe.party_guid, p.party_name, poe.name_text, poe.registration_id, m.mine_name, mine_party_appt_type_code
-    order by pmt.permit_no, pa.issue_date;
-"""
-
-## for testing, exact same as above but includes regional mines.
-permit_amendments_for_orgbook_query_with_regional = """
-    select pa.permit_amendment_guid, p.party_guid, pmt.permit_no
- 
-    from party_orgbook_entity poe
-    inner join party p on poe.party_guid = p.party_guid
-    inner join mine_party_appt mpa on p.party_guid = mpa.party_guid
-    inner join permit pmt on pmt.permit_id = mpa.permit_id
-    inner join permit_amendment pa on pa.permit_id = pmt.permit_id
-    inner join mine m on pa.mine_guid = m.mine_guid
-    
-    where mpa.permit_id is not null
-    and mpa.mine_party_appt_type_code = 'PMT'
-    and mpa.deleted_ind = false
-    and mpa.start_date <= pa.issue_date
-    and (mpa.end_date > pa.issue_date OR mpa.end_date is null or mpa.end_date = '9999-12-31')
     and pa.deleted_ind = false
     and pmt.permit_status_code = 'O'
     and substring(pmt.permit_no,2,1) != 'X'
@@ -290,20 +265,13 @@ def forward_all_pending_untp_vc_to_orgbook():
 
 
 @celery.task()
-def push_untp_map_data_to_publisher(include_regional: bool = False):
+def push_untp_map_data_to_publisher():
     ## This is a different process that passes the data to the publisher.
     ## the publisher structures the data and sends it to the orgbook.
     ## the publisher also manages the BitStringStatusLists.
     query = permit_amendments_for_orgbook_query
-    
-    if include_regional:
-        current_app.logger.info("including regional mines in the push to publisher")
-        query = permit_amendments_for_orgbook_query_with_regional
-
     permit_amendment_query_results = db.session.execute(
         query).fetchall()
-
-
 
     failed_credentials: List[Tuple[str, str | None]] = []
     success_count = 0
