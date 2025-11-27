df62b8f7a6551ceb0ff3e9926ff56ab877472494
Sinn <Alex.Sinn@gov.bc.ca>
2025-11-27 12:06:34 -0800

    adjustment to canada postal code regex for more flexible entries

--- services/common/src/redux/utils/Validate.ts
+++ services/common/src/redux/utils/Validate.ts
@@ -11,7 +11,7 @@ class Validator {
   // eslint-disable-next-line no-control-regex
   ASCII_REGEX = /^[\x00-\x7F\s]*$/;
 
-  CAN_POSTAL_CODE_REGEX = /^[ABCEGHJ-NPRSTVXY]\d[ABCEGHJ-NPRSTV-Z]\d[ABCEGHJ-NPRSTV-Z]\d$/;
+  CAN_POSTAL_CODE_REGEX = /^[ABCEGHJ-NPRSTVXY]\d[ABCEGHJ-NPRSTV-Z][ -]?\d[ABCEGHJ-NPRSTV-Z]\d$/i;
 
   US_POSTAL_CODE_REGEX = /(^\d{5}$)|(^\d{9}$)|(^\d{5}-\d{4}$)/;
 
@@ -156,11 +156,11 @@ export const spatialDocumentBundle = (files: any[]) => {
 export const notnone = (value) => (value === "None" ? "Please select an item" : undefined);
 
 export const maxLength = memoize((max) => (value) =>
-    value && value.length > max ? `Must be ${max} characters or less` : undefined
+  value && value.length > max ? `Must be ${max} characters or less` : undefined
 );
 
 export const minLength = memoize((min) => (value) =>
-    value && value.length < min ? `Must be ${min} characters or more` : undefined
+  value && value.length < min ? `Must be ${min} characters or more` : undefined
 );
 
 export const exactLength = memoize((min) => (value) =>
@@ -204,6 +204,10 @@ export const phoneNumber = (value) =>
   value && !Validate.checkPhone(value) ? "Invalid phone number e.g. xxx-xxx-xxxx" : undefined;
 
 export const postalCodeWithCountry = memoize((address_type_code = "CAN") => (value) => {
+  console.log("inside postalCodeWithCountry!");
+  console.log(address_type_code)
+  console.log(value)
+
   const code_type = address_type_code === "USA" ? "zip code" : "postal code";
   if (!["CAN", "USA"].includes(address_type_code)) {
     return undefined;
@@ -223,9 +227,9 @@ export const currency = (value) =>
   value && !Validate.checkCurrency(value) ? "Invalid dollar amount" : undefined;
 
 export const validateStartDate = memoize((previousStartDate) => (value) =>
-    value <= previousStartDate
-      ? "New manager's start date cannot be on or before the previous manager's start date."
-      : undefined
+  value <= previousStartDate
+    ? "New manager's start date cannot be on or before the previous manager's start date."
+    : undefined
 );
 
 export const alertStartDateNotBeforeHistoric = memoize((mineAlerts) => (value) => {
@@ -244,9 +248,9 @@ export const max = memoize((maxValue) => (value) =>
 );
 
 export const alertNotInFutureIfCurrentActive = memoize((mineAlert) => (value) =>
-    value && mineAlert.start_date && new Date(value) >= new Date()
-      ? "Start date cannot be in the future if there is a current active alert.  Please update or remove current alert first"
-      : undefined
+  value && mineAlert.start_date && new Date(value) >= new Date()
+    ? "Start date cannot be in the future if there is a current active alert.  Please update or remove current alert first"
+    : undefined
 );
 
 export const dateNotInFuture = (value) =>
@@ -266,12 +270,12 @@ export const dateInFuture = (value) =>
 
 // NOTE: modified from version in CORE- change from <= to <
 export const dateNotBeforeOther = memoize((other: string, otherLabel?: string) => (value: string) => {
-    const invalid = value && other && value < other;
-    if (!invalid) {
-      return undefined;
-    }
-    const otherFormattedDate = moment(other).format("ddd MMM D YYYY");
-    const otherDateText = otherLabel ? `${otherLabel} (${otherFormattedDate})` : otherFormattedDate;
+  const invalid = value && other && value < other;
+  if (!invalid) {
+    return undefined;
+  }
+  const otherFormattedDate = moment(other).format("ddd MMM D YYYY");
+  const otherDateText = otherLabel ? `${otherLabel} (${otherFormattedDate})` : otherFormattedDate;
   return `Date cannot be before ${otherDateText}`
 }, (other, otherLabel) => `${other}_${otherLabel}`);
 
@@ -286,23 +290,23 @@ export const dateNotBeforeStrictOther = memoize(
 export const timeNotBeforeOther = memoize(
   (comparableDate, baseDate, baseTime) => (comparableTime) =>
     baseTime &&
-    baseDate &&
-    comparableDate &&
-    comparableTime &&
-    baseDate === comparableDate &&
-    comparableTime < baseTime
+      baseDate &&
+      comparableDate &&
+      comparableTime &&
+      baseDate === comparableDate &&
+      comparableTime < baseTime
       ? `Time cannot be before ${baseTime.format("H:mm")} hrs.`
       : undefined
 );
 
 // NOTE: modified from version in CORE- change from >= to >
 export const dateNotAfterOther = memoize((other: string, otherLabel?: string) => (value: string) => {
-    const invalid = value && other && value > other;
-    if (!invalid) {
-      return undefined;
-    }
-    const otherFormattedDate = moment(other).format("ddd MMM D YYYY");
-    const otherDateText = otherLabel ? `${otherLabel} (${otherFormattedDate})` : otherFormattedDate;
+  const invalid = value && other && value > other;
+  if (!invalid) {
+    return undefined;
+  }
+  const otherFormattedDate = moment(other).format("ddd MMM D YYYY");
+  const otherDateText = otherLabel ? `${otherLabel} (${otherFormattedDate})` : otherFormattedDate;
 
   return `Date cannot be after ${otherDateText}`
 }, (other, otherLabel) => `${other}_${otherLabel}`);
@@ -311,9 +315,9 @@ export const yearNotInFuture = (value) =>
   value && value > new Date().getFullYear() ? "Year cannot be in the future" : undefined;
 
 export const validateIncidentDate = memoize((reportedDate) => (value) =>
-    value <= reportedDate
-      ? "Incident date and time cannot occur before reporting occurence."
-      : undefined
+  value <= reportedDate
+    ? "Incident date and time cannot occur before reporting occurence."
+    : undefined
 );
 
 export const decimalPlaces = memoize((places) => (value) => {
@@ -404,7 +408,7 @@ export const validateIfApplicationTypeCorrespondsToPermitNumber = (
       Strings.APPLICATION_TYPES_BY_PERMIT_PREFIX[permit.permit_prefix].includes(applicationType)
       ? undefined
       : `The ${isAdminAmendment ? "Type of Administrative Amendment" : "Type of Notice of Work"
-        } does not match to the selected permit.`;
+      } does not match to the selected permit.`;
   }
   return undefined;
 };
