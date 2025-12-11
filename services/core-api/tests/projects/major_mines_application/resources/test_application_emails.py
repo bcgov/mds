6de07cbc2491bae1eb8736d7abf043d0521679a3
Tara Epp <102187683+taraepp@users.noreply.github.com>
2025-12-11 22:52:04 +0000

    add auth save back in

--- services/core-api/tests/projects/major_mines_application/resources/test_application_emails.py
+++ services/core-api/tests/projects/major_mines_application/resources/test_application_emails.py
@@ -152,11 +152,12 @@ def test_send_mma_submit_email(mock_send_template_email, test_client,
     major_mine_application = MajorMineApplicationFactory(status_code='DFT')
     
     project_summary = ProjectSummaryFactory(project=major_mine_application.project)
-    ProjectSummaryAuthorizationFactory(
+    auth = ProjectSummaryAuthorizationFactory(
         project_summary=project_summary,
         project_summary_authorization_type='MINES_ACT_PERMIT'
     )
 
+    auth.save()
     db_session.refresh(major_mine_application)
 
     # Add various document types to test document grouping
