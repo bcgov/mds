13fb992dfcaf864fa2e0de9f1b2b5a0aabd890d5
Simen Fivelstad Smaaberg <simen.smaaberg@ca.ey.com>
2025-12-22 14:47:08 -0800

    Added missing changes

--- services/common/src/redux/selectors/searchSelectors.js
+++ services/common/src/redux/selectors/searchSelectors.js
@@ -2,7 +2,9 @@ import * as searchReducer from "../reducers/searchReducer";
 
 export const {
   getSearchResults,
+  getSearchFacets,
   getSearchTerms,
   getSearchBarResults,
+  getSearchBarFacets,
   getSearchSubsetResults,
 } = searchReducer;
