4f5a243532ddf5965c4229894964d71467138aab
Jsyro <jasyrotuck@gmail.com>
2026-07-27 13:57:22 -0700

    be test

--- services/core-api/tests/services/test_issue_to_orgbook_service.py
+++ services/core-api/tests/services/test_issue_to_orgbook_service.py
@@ -26,9 +26,10 @@ class MockResponse():
 def test_issue_vc_with_orgbook_association_sends_post(test_client, db_session, auth_headers):
     mine, permit = create_mine_and_permit()
     pmte = MinePartyAppointmentFactory(permit=permit, permittee=True)
-    pmte.permittee_orgbook_entity = PartyBCRegistration.create('Bx12345', True, datetime.now(),
-                                                               12345, 'big company', 12345,
-                                                               pmte.party_guid)
+    pmte.permittee_orgbook_entity = PartyBCRegistration.create(pmte.party_guid,
+                                                               'Bx12345', 'big company', True,
+                                                               datetime.now(), 12345, 12345,
+                                                               'big company')
 
     with current_app.test_request_context() as a, patch(
             'app.api.services.issue_to_orgbook_service.requests.post',
