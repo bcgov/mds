25a312b9cf3cb9b301f408dcd82536a0817743f5
asinn134 <127789479+asinn134@users.noreply.github.com>
2026-09-15 11:16:05 -0700

    [MDS-7015] red boundary box not showing in pdf viewer for permit conditions (#3991)
    * fix for red boundary box not showing in PDF viewer for permit conditions
    
    * removing random text
    
    * adding tests
    
    * more tests
    
    * test changes
    
    * addressing pr comments

--- services/common/src/components/syncfusion/pdfViewerAnnotations.ts
+++ services/common/src/components/syncfusion/pdfViewerAnnotations.ts
@@ -15,17 +15,23 @@ export const addAnnotationToPDFViewer = (
   page?: number,
   boundingBox?: IPdfViewerBoundingBox
 ) => {
-  pdfViewer.navigation.goToPage(page || 0);
-  if (boundingBox) {
+  pdfViewer.annotation.clear();
+
+  const resolvedPage = page || 0;
+
+  if (resolvedPage) {
+    pdfViewer.navigation.goToPage(resolvedPage);
+  }
+
+  if (boundingBox && resolvedPage) {
     const { top, right, bottom, left } = boundingBox;
     const topPx = top * 96;
     const rightPx = right * 96;
     const bottomPx = bottom * 96;
     const leftPx = left * 96;
-    pdfViewer.annotation.clear();
     pdfViewer.annotation.addAnnotation("Polygon", {
       offset: { x: 0, y: 0 },
-      pageNumber: page || 0,
+      pageNumber: resolvedPage,
       vertexPoints: [
         { x: leftPx, y: topPx },
         { x: rightPx, y: topPx },
@@ -33,6 +39,7 @@ export const addAnnotationToPDFViewer = (
         { x: leftPx, y: bottomPx },
         { x: leftPx, y: topPx },
       ],
+      isLock: true,
     });
   }
 };
